8ec96db53156932ea01e2bb1141ed1356d290fe9
beep boop <githubbot@vadim.me>
2024-06-25 19:01:42 +0200

    110 (0.0). One of the ED nurses has called you to see a patient that she is worried about.
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    One of the ED nurses has called you to see a patient that she is worried about.
    
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    One of the ED nurses has called you to see a patient that she is worried about.
    
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    What's more, if John's sats fall below 65% or if his mean arterial pressure falls below 20mmHg, he will go into cardiac arrest.
    
    Good luck! 
    DoNothing
    ExamineAirway
    John is conscious and talking. The airway seems to be fine. 
    AirwayClear
    Finish
    Thanks for playing! Here's how you did:
    
    Sadly, you didn't manage to stabilise John on this occasion.  He is hypotensive.  He has an unstable tachyarrhythmia, which is an indication for cardioversion. 
    
    Don't worry if you're not perfect yet, though. That's exactly what we're here for! 
    
    Final reward: -100.10000000149012

--- metanurse.py
+++ metanurse.py
@@ -3,72 +3,40 @@ while True:
         observations = input().strip().split()
         observations = list(map(float, observations))
 
-        # Extracting observations
-        # Event occurrences
-        airway_clear = observations[3]
-        airway_vomit = observations[4]
-        airway_blood = observations[5]
-        airway_tongue = observations[6]
-        breathing_none = observations[7]
-        breathing_snoring = observations[8]
-        breathing_see_saw = observations[9]
-        
-        # Measurement times and values
-        resp_rate_time = observations[40]
-        resp_rate = observations[46]
-        sats_time = observations[39]
-        sats = observations[45]
-        map_time = observations[41]
-        map_value = observations[47]
-        
-        # Heart rhythms
-        heart_rhythm_svt = observations[29]
-        heart_rhythm_af = observations[30]
-        heart_rhythm_vt = observations[32]
+        # Observation indices
+        response_verbal = observations[0]
+        breathless = observations[1]
+        clear_airway = observations[3]
+        oxygen_sats = observations[52]
+        breathing_rate = observations[51]
+        map_measure = observations[50]
 
-        # Critical life-threatening conditions
-        if (sats_time > 0 and sats < 65) or (map_time > 0 and map_value < 20):
+        # Conditions for checking cardiac arrest
+        if oxygen_sats < 65 or map_measure < 20:
             print(17)  # StartChestCompression
             continue
 
-        # Airway management
-        if airway_clear == 0:
+        # Manage the airway
+        if clear_airway > 0:
+            print(0)  # DoNothing if airway is clear
+        else:
             print(3)  # ExamineAirway
             continue
-        elif airway_blood > 0 or airway_vomit > 0 or airway_tongue > 0:
-            print(31)  # UseYankeurSuctionCatheter
-            continue
 
-        # Breathing management
-        if breathing_none > 0:
+        # Manage breathing
+        if breathing_rate < 8:
             print(29)  # UseBagValveMask
             continue
-        elif breathing_snoring > 0 or breathing_see_saw > 0:
-            print(29)  # UseBagValveMask
-            continue
-        if resp_rate_time > 0 and resp_rate < 8:
-            print(29)  # UseBagValveMask
-            continue
-
-        # Oxygen management
-        if sats_time > 0 and sats < 88:
+        elif 88 > oxygen_sats >= 65:
             print(30)  # UseNonRebreatherMask
             continue
 
-        # Circulation management
-        if map_time > 0 and map_value < 60:
+        # Check circulation
+        if map_measure < 60:
             print(15)  # GiveFluids
             continue
 
-        # Heart rhythm management
-        if heart_rhythm_svt > 0:
-            print(9)  # GiveAdenosine
-            continue
-        elif heart_rhythm_af > 0 or heart_rhythm_vt > 0:
-            print(28)  # AttachDefibPads
-            continue
-
-        # If all conditions are stabilized
+        # Assuming patient stabilized if we reach here
         print(48)  # Finish
         break
 
