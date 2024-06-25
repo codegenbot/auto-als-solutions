a93f07895afb875ae084d7636f0360f6e8c197e6
beep boop <githubbot@vadim.me>
2024-06-25 12:41:00 +0200

    17 (0.0). One of the ED nurses has called you to see a patient that she is worried about.
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    What's more, if John's sats fall below 65% or if his mean arterial pressure falls below 20mmHg, he will go into cardiac arrest.
    
    Good luck! 
    DoNothing
    ExamineAirway
    John is conscious and talking. The airway seems to be fine. 
    AirwayClear
    Finish
    Thanks for playing! Here's how you did:
    
    Sadly, you didn't manage to stabilise John on this occasion.  He is hypotensive.  He has an unstable tachyarrhythmia, which is an indication for cardioversion. 
    
    Don't worry if you're not perfect yet, though. That's exactly what we're here for! 
    
    Final reward: -100.10000000149012

--- metanurse.py
+++ metanurse.py
@@ -3,21 +3,16 @@ while True:
         observations = input().strip().split()
         observations = list(map(float, observations))
 
-        # Timed events
+        # Observations
         timed_meas_sats = observations[39]
         timed_meas_map = observations[40]
-        timed_meas_resp_rate = observations[41]
-
-        # Measurements
         measured_sats = observations[46]
         measured_map = observations[45]
-        measured_resp_rate = observations[47]
 
-        # Events
         airway_clear = observations[3]
         breathing_none = observations[7]
 
-        # Critical conditions that need immediate action
+        # Decision making based on timely measurements
         if timed_meas_sats > 0 and measured_sats < 65:
             print(17)  # StartChestCompression
             continue
@@ -25,36 +20,19 @@ while True:
             print(17)  # StartChestCompression
             continue
 
-        # Regular Checkup for each ABCDE category
-        # A: Airway
+        # Assessing and Reacting to Airway
         if airway_clear == 0:
             print(3)  # ExamineAirway
             continue
-
-        # B: Breathing
-        if breathing_none > 0:
-            print(29)  # UseBagValveMask
-            continue
-
-        # C: Circulation
-        if timed_meas_map == 0 or measured_map < 60:
-            print(38)  # TakeBloodPressure
-            continue
-
-        # D: Disability/Consciousness
-        print(6)  # ExamineDisability
-
-        # E: Exposure
-        print(7)  # ExamineExposure
 
         # Handling Insufficient Sats or MAP
         if (timed_meas_sats > 0 and measured_sats < 88) or (
             timed_meas_map > 0 and measured_map < 60
         ):
-            if airway_clear > 0 and measured_resp_rate >= 8:
+            if airway_clear > 0:
                 print(30)  # UseNonRebreatherMask
             else:
-                print(29)  # UseBagValveMask
+                print(3)  # ExamineAirway
             continue
 
         # All conditions good, can end the game
