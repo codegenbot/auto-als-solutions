379649d40382cff4b583816e90dbbb13fc7d1616
beep boop <githubbot@vadim.me>
2024-06-25 16:45:56 +0200

    77 (0.0). One of the ED nurses has called you to see a patient that she is worried about.
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    What's more, if John's sats fall below 65% or if his mean arterial pressure falls below 20mmHg, he will go into cardiac arrest.
    
    Good luck! 
    DoNothing
    ExamineAirway
    John is conscious and talking. The airway seems to be fine. 
    AirwayClear
    Finish
    Thanks for playing! Here's how you did:
    
    Sadly, you didn't manage to stabilise John on this occasion.  He is hypotensive.  He has an unstable tachyarrhythmia, which is an indication for cardioversion. 
    
    Don't worry if you're not perfect yet, though. That's exactly what we're here for! 
    
    Final reward: -100.10000000149012

--- metanurse.py
+++ metanurse.py
@@ -3,88 +3,62 @@ while True:
         observations = input().strip().split()
         observations = list(map(float, observations))
 
-        response_verbal = observations[0]
-        response_groan = observations[1]
-        response_none = observations[2]
-        airway_clear = observations[3]
-        airway_obstruction = max(observations[4], observations[5], observations[6])
-        heart_rhythm = observations[27:39]
-        heart_rhythm_measure = observations[33]
-        measured_hr = observations[39]
-        measured_rr = observations[40]
-        measured_cg = observations[41]
-        measured_temp = observations[42]
-        measured_map = observations[43]
-        measured_sats = observations[44]
-        measured_resps = observations[45]
-        hr = observations[46]
-        rr = observations[47]
-        cg = observations[48]
-        temp = observations[49]
-        map_value = observations[50]
-        sats = observations[51]
-        resps = observations[52]
+        # Parsing inputs
+        # Event relevance
+        response_verbal = observations[1]
+        response_groan = observations[2]
+        response_none = observations[3]
+        airway_clear = observations[4]
+        airway_obstructed = observations[5] + observations[6] + observations[7]
+        
+        # Vital signs observations
+        measured_hr_time = observations[39]
+        measured_hr = observations[46]
+        measured_rr_time = observations[40]
+        measured_rr = observations[47]
+        measured_map_time = observations[41]
+        measured_map = observations[48]
+        measured_sats_time = observations[42]
+        measured_sats = observations[49]
 
-        # Critical instant lifesaving checks
-        if measured_sats > 0 and sats < 65:
+        # Critical conditions check
+        if measured_sats_time > 0 and measured_sats < 65:
             print(17)  # StartChestCompression
             continue
-        if measured_map > 0 and map_value < 20:
+        if measured_map_time > 0 and measured_map < 20:
             print(17)  # StartChestCompression
-            continue
-
-        # Attach devices and examine essentials
-        if measured_map == 0:
-            print(27)  # UseBloodPressureCuff
-            continue
-        if measured_hr == 0:
-            print(24)  # UseMonitorPads
-            continue
-        if measured_hr > 0 and measured_map > 0:
-            print(5)  # ExamineCirculation
-            continue
-
-        # Check heart rhythm issues
-        if any([v > 0 for v in heart_rhythm[5:9]]) or any(
-            [v > 0 for v in heart_rhythm[1:2]]
-        ):  # problematic rhythms
-            print(11)  # GiveAmiodarone
-            continue
-        if heart_rhythm[1] > 0:  # SVT
-            print(9)  # GiveAdenosine
             continue
 
         # Airway management
-        if response_none > 0 or airway_obstruction > 0:
-            if measured_sats == 0:
-                print(25)  # UseSatsProbe
-                continue
+        if airway_clear == 0:
             print(3)  # ExamineAirway
+            continue
+        
+        # Clearing Airway Obstructions
+        if airway_obstructed > 0:
+            print(31)  # UseYankeurSucionCatheter
             continue
 
         # Breathing management
-        if measured_rr == 0:
-            print(4)  # ExamineBreathing
-            continue
-        if measured_rr > 0 and rr < 8:
+        if measured_rr_time > 0 and measured_rr < 8:
             print(29)  # UseBagValveMask
             continue
-
-        # Circulation stabilization
-        if map_value < 60:
+        
+        # Circulation management
+        if measured_map_time > 0 and measured_map < 60:
             print(15)  # GiveFluids
             continue
 
-        # O2 Saturation handling
-        if sats < 88:
+        # Oxygen management
+        if measured_sats_time > 0 and measured_sats < 88:
             if airway_clear > 0:
                 print(30)  # UseNonRebreatherMask
             else:
-                print(3)  # ExamineAirway
+                print(36)  # PerformHeadTiltChinLift
             continue
-
-        # Final check before finishing
-        if airway_clear > 0 and sats >= 88 and rr >= 8 and map_value >= 60:
+        
+        # Checking vital signs are stabilized
+        if measured_sats_time > 0 and measured_sats >= 88 and measured_rr_time > 0 and measured_rr >= 8 and measured_map_time > 0 and measured_map >= 60:
             print(48)  # Finish
             break
 
