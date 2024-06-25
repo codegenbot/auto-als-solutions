8e780e2ceb331858b236ea55356b5f79049f87b6
beep boop <githubbot@vadim.me>
2024-06-25 15:42:04 +0200

    62 (0.0). One of the ED nurses has called you to see a patient that she is worried about.
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    What's more, if John's sats fall below 65% or if his mean arterial pressure falls below 20mmHg, he will go into cardiac arrest.
    
    Good luck! 
    DoNothing
    ExamineAirway
    John is conscious and talking. The airway seems to be fine. 
    AirwayClear
    Finish
    Thanks for playing! Here's how you did:
    
    Sadly, you didn't manage to stabilise John on this occasion.  He is hypotensive.  He has an unstable tachyarrhythmia, which is an indication for cardioversion. 
    
    Don't worry if you're not perfect yet, though. That's exactly what we're here for! 
    
    Final reward: -100.10000000149012

--- metanurse.py
+++ metanurse.py
@@ -4,22 +4,24 @@ while True:
         observations = list(map(float, observations))
 
         # Parsing inputs
-        airway_clear = observations[3]
-        airway_vomit = observations[4]
-        airway_blood = observations[5]
-        breathing_none = observations[7]
-        breathing_rate_timed = observations[40]
-        breathing_rate = observations[46]
-        oxygen_sats_timed = observations[39]
-        oxygen_sats = observations[45]
-        map_timed = observations[41]
-        map_measure = observations[47]
-        heart_rhythm_svt = observations[29]
-        heart_rhythm_af = observations[30]
-        heart_rhythm_vt = observations[32]
+        event_types = observations[:39]
+        measurement_timeliness = observations[39:46]
+        measurements = observations[46:]
+
+        airway_clear = event_types[3]
+        breathing_none = event_types[7]
+        measured_resps_relevance = measurement_timeliness[6]
+        measured_resps = measurements[6]
+        measured_sats_relevance = measurement_timeliness[5]
+        measured_sats = measurements[5]
+        measured_map_relevance = measurement_timeliness[4]
+        measured_map = measurements[4]
 
         # Critical instant lifesaving checks
-        if (oxygen_sats_timed > 0 and oxygen_sats < 65) or (map_timed > 0 and map_measure < 20):
+        if measured_sats_relevance > 0 and measured_sats < 65:
+            print(17)  # StartChestCompression
+            continue
+        if measured_map_relevance > 0 and measured_map < 20:
             print(17)  # StartChestCompression
             continue
 
@@ -27,34 +29,26 @@ while True:
         if airway_clear == 0:
             print(3)  # ExamineAirway
             continue
-        elif airway_blood > 0 or airway_vomit > 0:
-            print(31)  # UseYankeurSuctionCatheter
-            continue
 
         # Breathing management
         if breathing_none > 0:
             print(29)  # UseBagValveMask
             continue
-        elif breathing_rate_timed > 0 and breathing_rate < 8:
+        if measured_resps_relevance > 0 and measured_resps < 8:
             print(29)  # UseBagValveMask
             continue
 
-        # Check oxygenation and apply oxygen if needed
-        if oxygen_sats_timed > 0 and oxygen_sats < 88:
-            print(30)  # UseNonRebreatherMask
-            continue
-
         # Circulation stabilization
-        if map_timed > 0 and map_measure < 60:
+        if measured_map_relevance > 0 and measured_map < 60:
             print(15)  # GiveFluids
             continue
-        
-        # Heart rhythm management
-        if heart_rhythm_svt > 0:
-            print(9)  # GiveAdenosine
-            continue
-        elif heart_rhythm_af > 0 or heart_rhythm_vt > 0:
-            print(28)  # AttachDefibPads
+
+        # Check oxygenation and apply oxygen if needed
+        if measured_sats_relevance > 0 and measured_sats < 88:
+            if airway_clear > 0:
+                print(30)  # UseNonRebreatherMask
+            else:
+                print(3)  # ExamineAirway
             continue
 
         # If all stabilizing conditions are met
