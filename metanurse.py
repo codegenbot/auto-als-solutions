91c3dd422c2be75a882486d737ec60aba78fdc48
beep boop <githubbot@vadim.me>
2024-06-27 02:31:08 +0200

    7 (0.0). FATAL Program exited unexpectedly Final reward: -5000

--- metanurse.py
+++ metanurse.py
@@ -1,93 +1,72 @@
 import math
 
-def advanced_life_support(observations):
-    actions = [
-        "DoNothing",
-        "CheckSignsOfLife",
-        "CheckRhythm",
-        "ExamineAirway",
-        "ExamineBreathing",
-        "ExamineCirculation",
-        "ExamineDisability",
-        "ExamineExposure",
-        "ExamineResponse",
-        "GiveAdenosine",
-        "GiveAdrenaline",
-        "GiveAmiodarone",
-        "GiveAtropine",
-        "GiveMidazolam",
-        "UseVenflonIVCatheter",
-        "GiveFluids",
-        "ViewMonitor",
-        "StartChestCompression",
-        "OpenAirwayDrawer",
-        "OpenBreathingDrawer",
-        "OpenCirculationDrawer",
-        "OpenDrugsDrawer",
-        "BagDuringCPR",
-        "ResumeCPR",
-        "UseMonitorPads",
-        "UseSatsProbe",
-        "UseAline",
-        "UseBloodPressureCuff",
-        "AttachDefibPads",
-        "UseBagValveMask",
-        "UseNonRebreatherMask",
-        "UseYankeurSucionCatheter",
-        "UseGuedelAirway",
-        "TakeBloodForArtherialBloodGas",
-        "TakeRoutineBloods",
-        "PerformAirwayManoeuvres",
-        "PerformHeadTiltChinLift",
-        "PerformJawThrust",
-        "TakeBloodPressure",
-        "TurnOnDefibrillator",
-        "DefibrillatorCharge",
-        "DefibrillatorCurrentUp",
-        "DefibrillatorCurrentDown",
-        "DefibrillatorPace",
-        "DefibrillatorPacePause",
-        "DefibrillatorRateUp",
-        "DefibrillatorRateDown",
-        "DefibrillatorSync",
-        "Finish"
-    ]
+def solve(observations):
+    airway_events = observations[:33]
+    vital_signs_events = observations[33:40]
+    vital_signs_values = observations[40:]
 
-    observations = [float(x) for x in observations.split()]
-    event_observations = observations[:33]
-    vital_sign_observations = observations[33:40]
-    vital_sign_values = observations[40:]
+    airway_clear = airway_events[3] > 0
+    airway_vomit = airway_events[4] > 0
+    airway_blood = airway_events[5] > 0
+    airway_tongue = airway_events[6] > 0
 
-    step = 0
-    while step < 350:
-        if max(vital_sign_observations[5], 0) < 0.65 or max(vital_sign_observations[6], 0) < 20:
-            return print(actions.index("StartChestCompression"))
+    breathing_none = airway_events[7] > 0
+    breathing_snoring = airway_events[8] > 0
+    breathing_seesaw = airway_events[9] > 0
+    breathing_equal_expansion = airway_events[10] > 0
+    breathing_bibasal_crepitations = airway_events[11] > 0
+    breathing_wheeze = airway_events[12] > 0
+    breathing_coarse_crepitations = airway_events[13] > 0
+    breathing_pneumothorax_symptoms = airway_events[14] > 0
 
-        if min(vital_sign_values[5], 1) < 0.88 or min(vital_sign_values[6], 1) < 8:
-            if event_observations[3] > 0:  # AirwayClear
-                return print(actions.index("ExamineBreathing"))
-            else:
-                return print(actions.index("ExamineAirway"))
+    ventilation_resistance = airway_events[15] > 0
 
-        if min(vital_sign_values[4], 1) < 60:
-            return print(actions.index("ExamineCirculation"))
+    radial_pulse_palpable = airway_events[16] > 0
+    radial_pulse_nonpalpable = airway_events[17] > 0
 
-        if event_observations[7] > 0:  # BreathingNone
-            return print(actions.index("ExamineResponse"))
+    heart_sounds_muffled = airway_events[18] > 0
+    heart_sounds_normal = airway_events[19] > 0
 
-        if event_observations[0] > 0:  # ResponseVerbal
-            return print(actions.index("CheckSignsOfLife"))
+    avpu_a = airway_events[20] > 0
+    avpu_u = airway_events[21] > 0
+    avpu_v = airway_events[22] > 0
 
-        if event_observations[1] > 0:  # ResponseGroan
-            return print(actions.index("CheckSignsOfLife"))
+    pupils_pinpoint = airway_events[23] > 0
+    pupils_normal = airway_events[24] > 0
 
-        if event_observations[2] > 0:  # ResponseNone
-            return print(actions.index("CheckSignsOfLife"))
+    exposure_rash = airway_events[25] > 0
+    exposure_peripherally_shutdown = airway_events[26] > 0
+    exposure_stained_underwear = airway_events[27] > 0
 
-        step += 1
+    heart_rhythm_nsr = airway_events[28] > 0
+    heart_rhythm_svt = airway_events[29] > 0
+    heart_rhythm_af = airway_events[30] > 0
+    heart_rhythm_atrial_flutter = airway_events[31] > 0
+    heart_rhythm_vt = airway_events[32] > 0
 
-    return print(actions.index("Finish"))
+    measured_heart_rate = vital_signs_events[0] > 0 and vital_signs_values[0] or 0
+    measured_resp_rate = vital_signs_events[1] > 0 and vital_signs_values[1] or 0
+    measured_capillary_glucose = vital_signs_events[2] > 0 and vital_signs_values[2] or 0
+    measured_temperature = vital_signs_events[3] > 0 and vital_signs_values[3] or 0
+    measured_map = vital_signs_events[4] > 0 and vital_signs_values[4] or 0
+    measured_sats = vital_signs_events[5] > 0 and vital_signs_values[5] or 0
+    measured_resps = vital_signs_events[6] > 0 and vital_signs_values[6] or 0
 
-while True:
-    observations = input()
-    advanced_life_support(observations)
+    if measured_sats < 65 or measured_map < 20:
+        return 17  # StartChestCompression
+
+    # Airway assessment
+    if not airway_clear:
+        if airway_vomit or airway_blood:
+            return 31  # UseYankeurSucionCatheter
+        elif airway_tongue:
+            return 32  # UseGuedelAirway
+        else:
+            return 3  # ExamineAirway
+
+    # Breathing assessment
+    if breathing_none or breathing_seesaw or breathing_pneumothorax_symptoms:
+        return 29  # UseBagValveMask
+    elif breathing_snoring:
+        return 36  # PerformHeadTiltChinLift
+    elif breathing_bibasal_crepitations or breathing_wheeze or breathing_
