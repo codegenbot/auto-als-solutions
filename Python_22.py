f3c79044f2352e4e39d9bde9af029b388b86857d
beep boop <githubbot@vadim.me>
2024-07-05 22:14:43 +0200

    Solution 16 of Python/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/m3a4lqhh Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c19fdd98-3238-47f9-a6b7-9155a4b51 2f1.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_22.py
+++ Python_22.py
@@ -1,16 +1,4 @@
 from typing import List, Any
 
-
 def filter_integers(values: List[Any]) -> List[int]:
-    while True:
-        try:
-            input_values = list(
-                map(
-                    int,
-                    input("Enter space-separated integers (or 'q' to quit): ").split(),
-                )
-            )
-            return [value for value in input_values if isinstance(value, int)]
-        except ValueError:
-            if input("Type 'q' to quit: ") == "q":
-                break
+    return [value for value in values if isinstance(value, int) and value]
