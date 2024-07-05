bd74154677ed10d11483ac5178c22d9656bc582d
beep boop <githubbot@vadim.me>
2024-07-06 00:57:28 +0200

    Solution 88 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/2mhktc53 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d7bed9d4-ff7a-4033-8388-9a6d90552 08b.py", line 9, in <module>     check(sort_even)           ^^^^^^^^^ NameError: name 'sort_even' is not defined

--- Python_37.py
+++ Python_37.py
@@ -1,9 +1,4 @@
-def sort_even():
-    numbers = list(
-        map(int, input("Enter even and odd numbers (space separated): ").split())
-    )
-    print(sorted(numbers))
-
-
+```
 def check():
-    sort_even()
+    nums = list(map(int, input("Enter even and odd numbers (space separated): ").split()))
+    print(sorted([num for num in nums if num % 2 == 0]))
