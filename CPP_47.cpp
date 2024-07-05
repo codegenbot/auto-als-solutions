7ff1098f068a9960f82c0a4f0d79e26669b3f06c
beep boop <githubbot@vadim.me>
2024-07-06 01:35:43 +0200

    Solution 15 of CPP/47, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/imoylgvn Prompt: 1266391d-3947-41e9-aaf7-f7d0a1d3c890.cpp: In function ‘int main()’: 1266391d-3947-41e9-aaf7-f7d0a1d3c890.cpp:18:5: error: ‘assert’ was not declared in this scope    18 |     assert (std::abs(result - 7)<1e-4 );       |     ^~~~~~ 1266391d-3947-41e9-aaf7-f7d0a1d3c890.cpp:4:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     3 | #include <algorithm>   +++ |+#include <cassert>     4 | 1266391d-3947-41e9-aaf7-f7d0a1d3c890.cpp: At global scope: 1266391d-3947-41e9-aaf7-f7d0a1d3c890.cpp:23:5: error: redefinition of ‘int main( )’    23 | int main(){       |     ^~~~ 1266391d-3947-41e9-aaf7-f7d0a1d3c890.cpp:15:5: note: ‘int main()’ previously def ined here    15 | int main() {       |     ^~~~

--- CPP_47.cpp
+++ CPP_47.cpp
@@ -17,7 +17,5 @@ float median(std::vector<float> l) {
 int main() {
     std::vector<float> numbers = {8, 1, 3, 9, 9, 2, 7};
     float result = median(numbers);
-    assert(std::abs(result - 5.0) < 1e-4);
-    std::cout << "The median is: " << result << std::endl;
-    return 0;
+    assert(std::abs(result - 6.5) < 1e-4);
 }
