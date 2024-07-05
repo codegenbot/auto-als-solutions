ef8e62d5fb3d67d67b5f23848cc63885d2ac8076
beep boop <githubbot@vadim.me>
2024-07-06 01:22:05 +0200

    Solution 22 of Python/41, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/2rskxmnc Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6076b513-86f0-43c1-b9bd-7c494a241 6a4.py", line 8, in <module>     check(car_race_collision)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6076b513-86f0-43c1-b9bd-7c494a241 6a4.py", line 5, in check     assert car_race_collision(10) == 100            ^^^^^^^^^^^^^^^^^^^^^^ TypeError: car_race_collision() takes 0 positional arguments but 1 was given

--- Python_41.py
+++ Python_41.py
@@ -1,4 +1,2 @@
-def car_race_collision():
-    "How many unique pairs of collided cars are possible in a race with 'n' number of cars?"
-    n = int(input("Enter the number of cars: "))
-    return n * (n - 1) // 2
+def car_race_collision(num_cars):
+    return num_cars * (num_cars - 1) // 2
