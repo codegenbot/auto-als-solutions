05e8753a7f356d6f108ffbd92bb609c747a1249e
beep boop <githubbot@vadim.me>
2024-07-06 01:38:58 +0200

    Solution 2 of CPP/27, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/fv9ptt1r Prompt: In file included from a3e2565e-92d0-46dc-a75d-d9a79dabf652.cpp:19: a3e2565e-92d0-46dc-a75d-d9a79dabf652.cpp: In function ‘int main()’: a3e2565e-92d0-46dc-a75d-d9a79dabf652.cpp:21:13: error: ‘filp_case’ was not decla red in this scope; did you mean ‘flip_case’?    21 |     assert (filp_case("These violent delights have violent ends") == "tH ESE VIOLENT DELIGHTS HAVE VIOLENT ENDS");       |             ^~~~~~~~~

--- CPP_27.cpp
+++ CPP_27.cpp
@@ -1,10 +1,13 @@
-string filp_case(string str){
+string flip_case(string str){
     string result = "";
-    for(int i=0; i<str.length(); i++){
-        if(str[i] >= 'a' && str[i] <= 'z'){
-            result += toupper(str[i]);
-        } else {
-            result += tolower(str[i]);
+    for(char c: str){
+        if(isalpha(c)){
+            if(islower(c))
+                result += toupper(c);
+            else
+                result += tolower(c);
+        }else{
+            result += c;
         }
     }
     return result;
