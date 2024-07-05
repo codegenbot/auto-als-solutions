36dbe8e252cf368cd828fd74205e15be7cc3c8ca
beep boop <githubbot@vadim.me>
2024-07-06 01:44:27 +0200

    Solution 39 of CPP/44, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/vm7zlok5 Prompt: _Traits = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:539:14:   required from ‘std::__cxx11::b asic_string<_CharT, _Traits, _Alloc>::basic_string(const _CharT*, const _Alloc&)  [with <template-parameter-2-1> = std::allocator<char>; _CharT = char; _Traits =  std::char_traits<char>; _Alloc = std::allocator<char>]’ ce55b86d-fea9-4a1c-88be-e25262edb2fd.cpp:7:16:   required from here /usr/include/c++/11/bits/stl_iterator.h:1015:57: error: no type named ‘differenc e_type’ in ‘struct std::iterator_traits<const char*>’  1015 |       typedef typename __traits_type::difference_type   difference_type;       |                                                         ^~~~~~~~~~~~~~~ /usr/include/c++/11/bits/stl_iterator.h: In instantiation of ‘class __gnu_cxx::_ _normal_iterator<char*, std::__cxx11::basic_string<char> >’: /usr/include/c++/11/bits/basic_string.tcc:225:25:   required from ‘void std::__c xx11::basic_string<_CharT, _Traits, _Alloc>::_M_construct(_InIterator, _InIterat or, std::forward_iterator_tag) [with _FwdIterator = const char*; _CharT = char; _Traits = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:539:14:   required from ‘std::__cxx11::b asic_string<_CharT, _Traits, _Alloc>::basic_string(const _CharT*, const _Alloc&)  [with <template-parameter-2-1> = std::allocator<char>; _CharT = char; _Traits =  std::char_traits<char>; _Alloc = std::allocator<char>]’ ce55b86d-fea9-4a1c-88be-e25262edb2fd.cpp:7:16:   required from here /usr/include/c++/11/bits/stl_iterator.h:1015:57: error: no type named ‘differenc e_type’ in ‘struct std::iterator_traits<char*>’

--- CPP_44.cpp
+++ CPP_44.cpp
@@ -1,22 +1,18 @@
+```cpp
 #include <string>
 using namespace std;
 
 string change_base(int x, int base) {
     char* res = new char[1000];  
     string s = "";
-    int i = 0;
     while (x > 0) {
         int rem = x % base;
-        if(rem < 10)
-            res[i++] = '0' + rem;
-        else if(rem <= 35)
-            res[i++] = 'A' + rem - 10;
-        else 
-            res[i++] = '-';
+        char c = rem < 10 ? '0' + rem : rem > 9 ? 'A' + rem - 10 : '-';
+        s.push_back(c);
         x /= base;
     }
-    res[i] = '\0';
-    return string(res);
+    delete[] res; // Don't forget to deallocate the memory
+    return s;
 }
 
 int main() {
@@ -25,7 +21,5 @@ int main() {
     cin >> num;
     cout << "Enter the base: ";
     cin >> base;
-    cout << "The conversion result is: " << change_base(num, base) << endl;
-    
-    delete[] new char[1000];
+    cout << "The conversion result is: " << change_base(num, base);
 }
