414d848f01df7c11cd88aae873305b3016b8cc32
beep boop <githubbot@vadim.me>
2024-07-06 00:46:35 +0200

    Solution 37 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/vl4vgx5a Prompt:       |                             ^~~~~~ ac70b043-620c-4703-ad65-71194160c149.cpp:27:36: error: expected primary-expressi on before ‘float’    27 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ ac70b043-620c-4703-ad65-71194160c149.cpp:35:5: error: redefinition of ‘int main( )’    35 | int main(){       |     ^~~~ ac70b043-620c-4703-ad65-71194160c149.cpp:21:5: note: ‘int main()’ previously def ined here    21 | int main() {       |     ^~~~ In file included from ac70b043-620c-4703-ad65-71194160c149.cpp:26: ac70b043-620c-4703-ad65-71194160c149.cpp: In function ‘int main()’: ac70b043-620c-4703-ad65-71194160c149.cpp:36:20: error: ‘get_positive’ was not de clared in this scope    36 |     assert (issame(get_positive({}) , {}));       |                    ^~~~~~~~~~~~ ac70b043-620c-4703-ad65-71194160c149.cpp:36:19: error: ‘issame’ cannot be used a s a function    36 |     assert (issame(get_positive({}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 using namespace std;
 
-bool issame(vector<float> a, vector<float> b){
+bool issame(vector<float> a, vector<float> b) {
     if(a.size() != b.size())
         return false;
     for(int i = 0; i<a.size(); i++)
