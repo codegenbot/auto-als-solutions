af7c82759f234511f5269e9dae6c861e8d954563
beep boop <githubbot@vadim.me>
2024-07-06 01:47:34 +0200

    Solution 18 of Python/5, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/l0cmfi1n Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/39bdca43-2cb8-42ee-b65a-5fcd43772 6db.py", line 10, in <module>     check(intersperse)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/39bdca43-2cb8-42ee-b65a-5fcd43772 6db.py", line 7, in check     assert intersperse([2, 2, 2], 2) == [2, 2, 2, 2, 2]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_5.py
+++ Python_5.py
@@ -1,5 +1,9 @@
-def intersperse(numbers: list[int], delimeter: int) -> list[int]:
-    result = [numbers[0]]
-    for i in range(1, len(numbers)):
-        result.extend([result[-1], delimeter, numbers[i]])
+def intersperse(numbers: list[int], delimiter: int) -> list[int]:
+    result = []
+    if len(numbers) > 1:
+        result.append(numbers[0])
+        for i in range(1, len(numbers)):
+            result.extend([result[-1], delimiter, numbers[i]])
+    else:
+        result = [numbers[0]]
     return result
