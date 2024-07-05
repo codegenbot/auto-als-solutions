495f06e45ce78c222157c7f841d1a867d03ba61e
beep boop <githubbot@vadim.me>
2024-07-06 01:19:05 +0200

    Solution 8 of CPP/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/1j5b47pf Prompt:    11 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ dafbd054-e475-4382-8d7c-4660cb3dd793.cpp:11:44: error: ‘b’ was not declared in t his scope    11 | bool issame(vector<string> a,vector<string>b){       |                                            ^ dafbd054-e475-4382-8d7c-4660cb3dd793.cpp:11:45: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from dafbd054-e475-4382-8d7c-4660cb3dd793.cpp:10: dafbd054-e475-4382-8d7c-4660cb3dd793.cpp: In function ‘int main()’: dafbd054-e475-4382-8d7c-4660cb3dd793.cpp:20:20: error: ‘filter_by_substring’ was  not declared in this scope    20 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |                    ^~~~~~~~~~~~~~~~~~~ dafbd054-e475-4382-8d7c-4660cb3dd793.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_7.cpp
+++ CPP_7.cpp
@@ -1,13 +1,10 @@
-```cpp
+#include <vector>
+#include <string>
+
 bool issame(vector<string> a, vector<string> b) {
     if (a.size() != b.size()) return false;
     for (int i = 0; i < a.size(); i++) {
-        if (find(a[i].begin(), a[i].end(), b[i]) == a[i].end())
-            return false;
+        if (filter_by_substring(a, b[i]).size() != 1) return false;
     }
     return true;
-}
-
-int main() {
-    assert(issame(filter_by_substring({"grunt", "trumpet", "prune", "gruesome"}, "run"), {"grunt", "prune"}));
 }
