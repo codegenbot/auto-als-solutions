95cea18cc896f0d60a5a4d774c821de5757ec7c4
beep boop <githubbot@vadim.me>
2024-07-06 01:44:29 +0200

    Solution 28 of CPP/47, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/imoylgvn Prompt: w_allocator<_Tp>::allocate(int, const void*) [with _Tp = float]’: /usr/include/c++/11/bits/alloc_traits.h:464:28:   required from ‘static _Tp* std ::allocator_traits<std::allocator<_CharT> >::allocate(std::allocator_traits<std: :allocator<_CharT> >::allocator_type&, int) [with _Tp = float; std::allocator_tr aits<std::allocator<_CharT> >::pointer = float*; std::allocator_traits<std::allo cator<_CharT> >::allocator_type = std::allocator<float>]’ /usr/include/c++/11/bits/stl_vector.h:346:33:   required from ‘std::_Vector_base <_Tp, _Alloc>::pointer std::_Vector_base<_Tp, _Alloc>::_M_allocate(size_t) [with  _Tp = float; _Alloc = std::allocator<float>; std::_Vector_base<_Tp, _Alloc>::po inter = float*; size_t = long unsigned int]’ /usr/include/c++/11/bits/stl_vector.h:1582:25:   required from ‘void std::vector <_Tp, _Alloc>::_M_range_initialize(_ForwardIterator, _ForwardIterator, std::forw ard_iterator_tag) [with _ForwardIterator = const float*; _Tp = float; _Alloc = s td::allocator<float>]’ /usr/include/c++/11/bits/stl_vector.h:629:21:   required from ‘std::vector<_Tp, _Alloc>::vector(std::initializer_list<_Tp>, const allocator_type&) [with _Tp = f loat; _Alloc = std::allocator<float>; std::vector<_Tp, _Alloc>::allocator_type =  std::allocator<float>]’ 5c917041-1918-496d-b40e-2aec463af8d6.cpp:33:5:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<float>’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- CPP_47.cpp
+++ CPP_47.cpp
@@ -15,7 +15,14 @@ float median(std::vector<float> l) {
 }
 
 int main() {
-    std::vector<float> numbers = {8, 1, 3, 9, 9, 2, 7};
+    std::vector<float> numbers;
+    numbers.push_back(8);
+    numbers.push_back(1);
+    numbers.push_back(3);
+    numbers.push_back(9);
+    numbers.push_back(9);
+    numbers.push_back(2);
+    numbers.push_back(7);
     float result = median(numbers);
     assert (std::abs(result - 7)<1e-4 );
     return 0;
