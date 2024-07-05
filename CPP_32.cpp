01f8cb7729b695a4438786002a8e2c723d08c7a3
beep boop <githubbot@vadim.me>
2024-07-06 00:51:02 +0200

    Solution 76 of CPP/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/ntqeeobx Prompt: in this scope    30 |     assert(abs(poly(coeffs, solution))< 1e-3);       |     ^~~~~~ 644f78aa-1d02-4c53-9d10-f4a2746276cb.cpp:3:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     2 | #include <vector>   +++ |+#include <cassert>     3 | 644f78aa-1d02-4c53-9d10-f4a2746276cb.cpp: At global scope: 644f78aa-1d02-4c53-9d10-f4a2746276cb.cpp:36:5: error: redefinition of ‘int main( )’    36 | int main(){       |     ^~~~ 644f78aa-1d02-4c53-9d10-f4a2746276cb.cpp:26:5: note: ‘int main()’ previously def ined here    26 | int main() {       |     ^~~~ 644f78aa-1d02-4c53-9d10-f4a2746276cb.cpp: In function ‘int main()’: 644f78aa-1d02-4c53-9d10-f4a2746276cb.cpp:51:20: error: ‘find_zero’ was not decla red in this scope; did you mean ‘findZero’?    51 |         solution = find_zero(coeffs);       |                    ^~~~~~~~~       |                    findZero

--- CPP_32.cpp
+++ CPP_32.cpp
@@ -28,7 +28,7 @@ int main() {
     vector<double> coeffs = {1.0, -7.0, 12.0, -6.0}; 
     double solution = findZero(coeffs);
 
-    assert(abs(poly(coeffs, solution))< 1e-3);
+    assert(abs(poly(coeffs, solution)) < 1e-3);
 
     return 0;
 }
