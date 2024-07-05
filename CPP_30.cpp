c4d3043a6f76db115429daf816a24eaf0e3ebd6b
beep boop <githubbot@vadim.me>
2024-07-06 00:52:34 +0200

    Solution 58 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/vl4vgx5a Prompt:       |                                    ^~~~~ a8153be9-7885-4a48-902c-fd5e678ac68e.cpp:27:43: error: expression list treated a s compound expression in initializer [-fpermissive]    27 | bool issame(vector<float> a,vector<float>b){       |                                           ^ a8153be9-7885-4a48-902c-fd5e678ac68e.cpp:35:5: error: redefinition of ‘int main( )’    35 | int main(){       |     ^~~~ a8153be9-7885-4a48-902c-fd5e678ac68e.cpp:21:5: note: ‘int main()’ previously def ined here    21 | int main() {       |     ^~~~ In file included from a8153be9-7885-4a48-902c-fd5e678ac68e.cpp:26: a8153be9-7885-4a48-902c-fd5e678ac68e.cpp: In function ‘int main()’: a8153be9-7885-4a48-902c-fd5e678ac68e.cpp:36:20: error: ‘get_positive’ was not de clared in this scope    36 |     assert (issame(get_positive({}) , {}));       |                    ^~~~~~~~~~~~ a8153be9-7885-4a48-902c-fd5e678ac68e.cpp:36:19: error: ‘issame’ cannot be used a s a function    36 |     assert (issame(get_positive({}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -1,18 +1,16 @@
-```cpp
-#include <initializer_list>
 #include <vector>
 
-bool issame(std::vector<float> a, std::vector<float> b) {
+bool same(vector<float> a, vector<float> b) {
     if (a.size() != b.size())
         return false;
     for (int i = 0; i<a.size(); i++)
-        if (std::abs(a[i] - b[i]) > 1e-9)
+        if (abs(a[i] - b[i]) > 1e-9)
             return false;
     return true;
 }
 
-std::vector<float> get_positive(std::vector<float> l) {
-    std::vector<float> result;
+vector<float> get_positives(vector<float> l) {
+    vector<float> result;
     for (float x : l) {
         if (x > 0)
             result.push_back(x);
@@ -20,7 +18,7 @@ std::vector<float> get_positive(std::vector<float> l) {
     return result;
 }
 
-int main_test() {
-    assert(issame(get_positive({}), {}));
+int test() {
+    assert(same(get_positives({}), {}));
     return 0;
 }
