803905a44833787298155408122d1384634c4e8c
beep boop <githubbot@vadim.me>
2024-07-06 00:47:38 +0200

    Solution 56 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/2mhktc53 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/76804fd8-6f56-4497-8f26-b88efb6e5 ed5.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_37.py
+++ Python_37.py
@@ -1,2 +1,5 @@
-```
-return [i if i % 2 == 0 else next(j) for i, j in zip(even, [iter(odd)] * len(even))])
+def sort_even(l: list):
+    even = sorted([x for x in l if x % 2 == 0])
+    odd = sorted([x for x in l if x % 2 != 0])
+
+    return [i if i % 2 == 0 else next(j) for i, j in zip(even, iter(odd))]
