aeba92f53806ec74118a7e60cd749819d2c0946c
beep boop <githubbot@vadim.me>
2024-07-06 01:56:59 +0200

    Solution 53 of CPP/44, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/vm7zlok5 Prompt: 6e0e6f5c-70b3-4067-a5af-10d12d11651a.cpp:1:1: error: stray ‘`’ in program     1 | ```       | ^ 6e0e6f5c-70b3-4067-a5af-10d12d11651a.cpp:1:2: error: stray ‘`’ in program     1 | ```       |  ^ 6e0e6f5c-70b3-4067-a5af-10d12d11651a.cpp:1:3: error: stray ‘`’ in program     1 | ```       |   ^ 6e0e6f5c-70b3-4067-a5af-10d12d11651a.cpp:27:5: error: redefinition of ‘int main( )’    27 | int main(){       |     ^~~~ 6e0e6f5c-70b3-4067-a5af-10d12d11651a.cpp:17:5: note: ‘int main()’ previously def ined here    17 | int main() {       |     ^~~~

--- CPP_44.cpp
+++ CPP_44.cpp
@@ -2,22 +2,23 @@
 using namespace std;
 
 string change_base(int x, int base) {
-    char res[1000];
     string s = "";
     while (x > 0) {
         int rem = x % base;
-        char c = rem < 10 ? '0' + rem : rem > 9 ? 'A' + rem - 10 : '-';
-        s.push_back(c);
+        if(rem < 10)
+            s.push_back('0' + rem);
+        else if(rem <= 35)
+            s.push_back('A' + rem - 10);
+        else
+            s.push_back('-');
         x /= base;
     }
-    return s;
+    return string reversal(s);
 }
 
-int main() {
-    int num, base;
-    cout << "Enter a number: ";
-    cin >> num;
-    cout << "Enter the base: ";
-    cin >> base;
-    cout << "The conversion result is: " << change_base(num, base);
+string reversal(string str) {
+    int n = str.length();
+    for(int i=n-1; i>=0; i--) {
+        cout << str[i];
+    }
 }
