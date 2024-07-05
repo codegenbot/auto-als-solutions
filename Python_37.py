ab6471b1633e22a02ceeb014cf5f9802a8e8f42f
beep boop <githubbot@vadim.me>
2024-07-06 00:52:41 +0200

    Solution 71 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/2mhktc53 Prompt: [6, 7, 8, 9, 10, 1, 2, 3, 4, 5] Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4862700c-3919-4119-8484-7bd4d2ca6 f9b.py", line 16, in <module>     check(sort_even)           ^^^^^^^^^ NameError: name 'sort_even' is not defined. Did you mean: 'sort_even_odd'?

--- Python_37.py
+++ Python_37.py
@@ -3,7 +3,7 @@ def sort_even_odd(even, odd):
     odd.sort()
     return odd + even
 
-even_numbers = [1, 2, 3, 4, 5]
-odd_numbers = [6, 7, 8, 9, 10]
+even_numbers = [4, 2, 6, 1]
+odd_numbers = [3, 7, 5, 10]
 
 print(sort_even_odd(even_numbers, odd_numbers))
