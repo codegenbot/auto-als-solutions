d94735f84f537fce6a8fcc0150ce1e6f2a1b4c99
beep boop <githubbot@vadim.me>
2024-07-06 00:47:53 +0200

    Solution 42 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/vl4vgx5a Prompt:       |             ^~~~~~ 4968bcf8-c5f0-41c3-b7b0-82c717d3d154.cpp:29:36: error: expected primary-expressi on before ‘float’    29 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 4968bcf8-c5f0-41c3-b7b0-82c717d3d154.cpp:37:5: error: redefinition of ‘int main( )’    37 | int main(){       |     ^~~~ 4968bcf8-c5f0-41c3-b7b0-82c717d3d154.cpp:23:5: note: ‘int main()’ previously def ined here    23 | int main() {       |     ^~~~ In file included from 4968bcf8-c5f0-41c3-b7b0-82c717d3d154.cpp:28: 4968bcf8-c5f0-41c3-b7b0-82c717d3d154.cpp: In function ‘int main()’: 4968bcf8-c5f0-41c3-b7b0-82c717d3d154.cpp:38:20: error: ‘get_positive’ was not de clared in this scope    38 |     assert (issame(get_positive({}) , {}));       |                    ^~~~~~~~~~~~ 4968bcf8-c5f0-41c3-b7b0-82c717d3d154.cpp:38:19: error: ‘issame’ cannot be used a s a function    38 |     assert (issame(get_positive({}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -1,26 +1,25 @@
-#include <vector>
 #include <initializer_list>
+#include <vector>
 
-bool issame(std::vector<float> a, std::vector<float> b){
-    if(a.size() != b.size())
+bool issame(vector<float> a, vector<float> b) {
+    if (a.size() != b.size())
         return false;
-    for(int i = 0; i<a.size(); i++)
-        if(abs(a[i] - b[i]) > 1e-9)
+    for (int i = 0; i<a.size(); i++)
+        if (abs(a[i] - b[i]) > 1e-9)
             return false;
     return true;
 }
 
-std::vector<float> get_positive(std::vector<float> l) {
-    std::vector<float> result;
+vector<float> get_positive(vector<float> l) {
+    vector<float> result;
     for (float x : l) {
         if (x > 0)
             result.push_back(x);
     }
     return result;
-
 }
 
-int main_entry() {
+int main_test() {
     assert(issame(get_positive({}), {}));
     return 0;
 }
