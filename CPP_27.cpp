0720971c9697882c1ae42e2c773d14457dcaa19a
beep boop <githubbot@vadim.me>
2024-07-06 01:46:06 +0200

    Solution 7 of CPP/27, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/fv9ptt1r Prompt: bdd0afe1-9f9d-43bb-bfb7-74a344ae4b93.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string filp_case(string str){       | ^~~~~~ In file included from bdd0afe1-9f9d-43bb-bfb7-74a344ae4b93.cpp:13: bdd0afe1-9f9d-43bb-bfb7-74a344ae4b93.cpp: In function ‘int main()’: bdd0afe1-9f9d-43bb-bfb7-74a344ae4b93.cpp:15:13: error: ‘filp_case’ was not decla red in this scope    15 |     assert (filp_case("These violent delights have violent ends") == "tH ESE VIOLENT DELIGHTS HAVE VIOLENT ENDS");       |             ^~~~~~~~~

--- CPP_27.cpp
+++ CPP_27.cpp
@@ -4,9 +4,9 @@ string filp_case(string str){
     string result = "";
     for(int i=0; i<str.length(); i++){
         if(str[i] >= 'a' && str[i] <= 'z'){
-            result += toupper((char)str[i]);
+            result += toupper(str[i]);
         } else {
-            result += tolower((char)str[i]);
+            result += tolower(str[i]);
         }
     }
     return result;
