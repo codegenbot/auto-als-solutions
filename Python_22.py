be1efe4bcadcf3beec0c804ab9448aacbb161e4d
beep boop <githubbot@vadim.me>
2024-07-05 22:21:42 +0200

    Solution 22 of Python/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/m3a4lqhh Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/341463b5-95f9-4a72-8f27-b46f79601 317.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_22.py
+++ Python_22.py
@@ -1,8 +1,10 @@
+````
 from typing import List, Any
 def filter_integers(values: List[Any]) -> List[int]:
     return [value for value in values if isinstance(value, int)]
 
 user_input = input("Enter values separated by commas: ")
-values_list = [int(value.strip()) for value in user_input.split(",")]
+values_list = [value.strip() for value in user_input.split(",")]
 filtered_values = filter_integers(values_list)
 print(filtered_values)
+```
