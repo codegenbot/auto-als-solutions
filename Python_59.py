4d0327f55b990d3090788a63a31bea58b31a8525
beep boop <githubbot@vadim.me>
2024-07-06 01:57:14 +0200

    Solution 4 of Python/59, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/0fm496c2 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a98aad03-08ff-41fe-af7c-9093658b0 b57.py", line 14, in <module>     check(largest_prime_factor)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a98aad03-08ff-41fe-af7c-9093658b0 b57.py", line 11, in check     assert largest_prime_factor(13195) == 29            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_59.py
+++ Python_59.py
@@ -1,19 +1,10 @@
 ```
 def largest_prime_factor(n: int):
-    def is_prime(k: int) -> bool:
-        if k < 2:
-            return False
-        for i in range(2, int(k**0.5) + 1):
-            if k % i == 0:
-                return False
-        return True
-
-    i = n // 2
+    i = n - 1
     while i > 1:
         if n % i == 0:
-            if is_prime(i):
-                j = i
-                while j <= i and n % j == 0:
-                    j += 1
-                return i if j == i else j
+            j = i
+            while j <= i and n % j == 0:
+                j += 1
+            return i if j == i else j
         i -= 1
