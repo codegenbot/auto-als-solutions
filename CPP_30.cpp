d1878cac143669da85cc42e2de403f1bef60ba46
beep boop <githubbot@vadim.me>
2024-07-06 00:57:56 +0200

    Solution 72 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/vl4vgx5a Prompt:       |           ^~~~~~ In file included from dd5c0b8a-aab3-4379-aac5-83e7101d89ae.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ dd5c0b8a-aab3-4379-aac5-83e7101d89ae.cpp:28:36: error: expected primary-expressi on before ‘float’    28 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ dd5c0b8a-aab3-4379-aac5-83e7101d89ae.cpp:28:43: error: expression list treated a s compound expression in initializer [-fpermissive]    28 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from dd5c0b8a-aab3-4379-aac5-83e7101d89ae.cpp:27: dd5c0b8a-aab3-4379-aac5-83e7101d89ae.cpp: In function ‘int main()’: dd5c0b8a-aab3-4379-aac5-83e7101d89ae.cpp:37:20: error: ‘get_positive’ was not de clared in this scope    37 |     assert (issame(get_positive({}) , {}));       |                    ^~~~~~~~~~~~ dd5c0b8a-aab3-4379-aac5-83e7101d89ae.cpp:37:19: error: ‘issame’ cannot be used a s a function    37 |     assert (issame(get_positive({}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -1,17 +1,17 @@
 #include <vector>
 #include <cmath>
 
-bool same(vector<float> a, vector<float> b) {
+bool issame(std::vector<float> a, std::vector<float> b) {
     if (a.size() != b.size())
         return false;
     for (int i = 0; i<a.size(); i++)
-        if (abs(a[i] - b[i]) > 1e-9)
+        if (std::abs(a[i] - b[i]) > 1e-9)
             return false;
     return true;
 }
 
-vector<float> get_positives(vector<float> l) {
-    vector<float> result;
+std::vector<float> get_positive(std::vector<float> l) {
+    std::vector<float> result;
     for (float x : l) {
         if (x > 0)
             result.push_back(x);
@@ -20,6 +20,6 @@ vector<float> get_positives(vector<float> l) {
 }
 
 int main() {
-    assert(same(get_positives({}), {}));
+    assert(issame(get_positive({}), {}));
     return 0;
 }
