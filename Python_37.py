f35f0333a5978c36e01ab53bcbde863b7c29976a
beep boop <githubbot@vadim.me>
2024-07-06 00:46:08 +0200

    Solution 46 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/2mhktc53 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/850a7466-1499-4d73-b360-fde571d8d 8d9.py", line 12, in <module>     check(sort_even)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/850a7466-1499-4d73-b360-fde571d8d 8d9.py", line 7, in check     assert tuple(sort_even([5, 8, -12, 4, 23, 2, 3, 11, 12, -10])) == tuple(                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/850a7466-1499-4d73-b360-fde571d8d 8d9.py", line 5, in sort_even     return [i[0] if i else j.pop(0) for i, j in zip(even, [iter(odd)] * len(even ))]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/850a7466-1499-4d73-b360-fde571d8d 8d9.py", line 5, in <listcomp>     return [i[0] if i else j.pop(0) for i, j in zip(even, [iter(odd)] * len(even ))]             ~^^^ TypeError: 'int' object is not subscriptable

--- Python_37.py
+++ Python_37.py
@@ -1,5 +1,6 @@
+```
 def sort_even(l: list):
     even = sorted([x for x in l if x % 2 == 0])
     odd = sorted([x for x in l if x % 2 != 0])
 
-    return [i[0] if i else next(j) for i, j in zip(even, (iter(odd) for _ in even))]
+    return [i for pair in zip(even, odd) for i in pair]
