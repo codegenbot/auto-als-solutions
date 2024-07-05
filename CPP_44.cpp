bd32605c4f65ed47b5eb692139744d40b8522e7f
beep boop <githubbot@vadim.me>
2024-07-06 01:19:12 +0200

    Solution 7 of CPP/44, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/vm7zlok5 Prompt: f625a64f-1d44-4fa1-9373-44fdaf309127.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string change_base(int x, int base) {       | ^~~~~~ In file included from f625a64f-1d44-4fa1-9373-44fdaf309127.cpp:13: f625a64f-1d44-4fa1-9373-44fdaf309127.cpp: In function ‘int main()’: f625a64f-1d44-4fa1-9373-44fdaf309127.cpp:17:17: error: ‘change_base’ was not dec lared in this scope    17 |         assert (change_base(x, x + 1) == to_string(x));       |                 ^~~~~~~~~~~ f625a64f-1d44-4fa1-9373-44fdaf309127.cpp:17:42: error: ‘to_string’ was not decla red in this scope    17 |         assert (change_base(x, x + 1) == to_string(x));       |                                          ^~~~~~~~~

--- CPP_44.cpp
+++ CPP_44.cpp
@@ -1,13 +1,15 @@
 #include <string>
+#include <bits/stdc++.h>
 
-std::string change_base(int x, int base) {
+using namespace std;
+
+string change_base(int x, int base) {
     if (x < base) {
-        return std::to_string(x);
+        return to_string(x);
     }
-    std::string res = "";
+    string res = "";
     while (x > 0) {
-        res = std::to_string(x % base) + res;
+        res = to_string(x % base) + res;
         x /= base;
     }
     return res;
-}
