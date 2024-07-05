bea4c4bc82fea5e69e06e29c20da03dd09ca9893
beep boop <githubbot@vadim.me>
2024-07-05 05:47:47 +0200

    Solution 6 of substitution-cipher, ratio 0.02 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-4o-2024-05-13-run1/runs/1hfwwp5c Prompt:    10 |     string decipheredMessage;       |           ^~~~~~~~~~~~~~~~~~       |           ; b1bd1c73-0eef-43e2-9fd1-6eaa0547e501.cpp:12:9: error: ‘decipheredMessage’ was no t declared in this scope    12 |         decipheredMessage += cipherMap[ch];       |         ^~~~~~~~~~~~~~~~~ b1bd1c73-0eef-43e2-9fd1-6eaa0547e501.cpp:12:30: error: ‘cipherMap’ was not decla red in this scope    12 |         decipheredMessage += cipherMap[ch];       |                              ^~~~~~~~~ b1bd1c73-0eef-43e2-9fd1-6eaa0547e501.cpp:15:5: error: ‘cout’ was not declared in  this scope    15 |     cout << decipheredMessage << endl;       |     ^~~~ b1bd1c73-0eef-43e2-9fd1-6eaa0547e501.cpp:15:13: error: ‘decipheredMessage’ was n ot declared in this scope    15 |     cout << decipheredMessage << endl;       |             ^~~~~~~~~~~~~~~~~ b1bd1c73-0eef-43e2-9fd1-6eaa0547e501.cpp:15:34: error: ‘endl’ was not declared i n this scope    15 |     cout << decipheredMessage << endl;       |                                  ^~~~

--- substitution-cipher.cpp
+++ substitution-cipher.cpp
@@ -8,16 +8,16 @@ int main() {
     string cipher1, cipher2, message;
     cin >> cipher1 >> cipher2 >> message;
 
-    map<char, char> cipher_map;
+    map<char, char> cipherMap;
     for (size_t i = 0; i < cipher1.size(); ++i) {
-        cipher_map[cipher1[i]] = cipher2[i];
+        cipherMap[cipher1[i]] = cipher2[i];
     }
 
-    string deciphered_message;
-    for (char c : message) {
-        deciphered_message += cipher_map[c];
+    string decipheredMessage;
+    for (char ch : message) {
+        decipheredMessage += cipherMap[ch];
     }
 
-    cout << deciphered_message << endl;
+    cout << decipheredMessage << endl;
     return 0;
 }
