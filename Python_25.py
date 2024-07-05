51be65a41a1684716ce69819c70986ad652c152d
beep boop <githubbot@vadim.me>
2024-07-05 22:17:21 +0200

    Solution 5 of Python/25, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/ofllo3it Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1b83eaca-4711-4a12-bfb5-e2f1f6f9f 30d.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_25.py
+++ Python_25.py
@@ -1,15 +1,18 @@
 from typing import List
-import math
 
 
-def find_prime_numbers(n: int) -> List[int]:
-    prime_numbers = []
-    for num in range(2, n + 1):
-        is_prime = True
-        for i in range(2, int(math.sqrt(num)) + 1):
-            if num % i == 0:
-                is_prime = False
-                break
-        if is_prime:
-            prime_numbers.append(num)
-    return prime_numbers
+def factorize(n: int) -> List[int]:
+    factors = []
+    i = 2
+    while i * i <= n:
+        if n % i:
+            i += 1
+        else:
+            count = 0
+            while n % i == 0:
+                n //= i
+                count += 1
+            factors.extend([i] * count)
+    if n > 1:
+        factors.append(n)
+    return factors
