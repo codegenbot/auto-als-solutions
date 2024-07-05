95721bb6f07d1a6ea0bd3498cd75c267949bc9f1
beep boop <githubbot@vadim.me>
2024-07-06 01:55:43 +0200

    Solution 13 of CPP/23, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/13ocpli8 Prompt:     1 | ```int str_len(string str){ return str.length(); }```       |                                                   ^ 6806900c-201c-42e8-a5c4-5da90b56a128.cpp:1:52: error: stray ‘`’ in program     1 | ```int str_len(string str){ return str.length(); }```       |                                                    ^ 6806900c-201c-42e8-a5c4-5da90b56a128.cpp:1:53: error: stray ‘`’ in program     1 | ```int str_len(string str){ return str.length(); }```       |                                                     ^ 6806900c-201c-42e8-a5c4-5da90b56a128.cpp:1:16: error: ‘string’ was not declared in this scope     1 | ```int str_len(string str){ return str.length(); }```       |                ^~~~~~ In file included from 6806900c-201c-42e8-a5c4-5da90b56a128.cpp:3: 6806900c-201c-42e8-a5c4-5da90b56a128.cpp: In function ‘int main()’: 6806900c-201c-42e8-a5c4-5da90b56a128.cpp:5:13: error: ‘strlen’ was not declared in this scope     5 |     assert (strlen("asdasnakj") == 9);       |             ^~~~~~ 6806900c-201c-42e8-a5c4-5da90b56a128.cpp:4:1: note: ‘strlen’ is defined in heade r ‘<cstring>’; did you forget to ‘#include <cstring>’?     3 | #include<assert.h>   +++ |+#include <cstring>     4 | int main(){

--- CPP_23.cpp
+++ CPP_23.cpp
@@ -1,9 +1,9 @@
-```cpp
+```
 #include <string>
 #include <cassert>
 
 int str_len(string str){ return str.length(); }
 
 int main() {
-    assert(str_len("asdasnakj") == 12); 
+    assert(str_len("asdasnakj") == 12);
 }
