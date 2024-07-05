54d5263b79b524f1016d97f078ddeadefb0d60a4
beep boop <githubbot@vadim.me>
2024-07-06 01:05:44 +0200

    Solution 87 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/vl4vgx5a Prompt:       |             ^~~~~~ 336db725-6627-442c-8dbb-bd1b30e80ed8.cpp:28:36: error: expected primary-expressi on before ‘float’    28 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 336db725-6627-442c-8dbb-bd1b30e80ed8.cpp:36:5: error: redefinition of ‘int main( )’    36 | int main(){       |     ^~~~ 336db725-6627-442c-8dbb-bd1b30e80ed8.cpp:22:5: note: ‘int main()’ previously def ined here    22 | int main() {       |     ^~~~ In file included from 336db725-6627-442c-8dbb-bd1b30e80ed8.cpp:27: 336db725-6627-442c-8dbb-bd1b30e80ed8.cpp: In function ‘int main()’: 336db725-6627-442c-8dbb-bd1b30e80ed8.cpp:37:20: error: ‘get_positive’ was not de clared in this scope    37 |     assert (issame(get_positive({}) , {}));       |                    ^~~~~~~~~~~~ 336db725-6627-442c-8dbb-bd1b30e80ed8.cpp:37:19: error: ‘issame’ cannot be used a s a function    37 |     assert (issame(get_positive({}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -1,11 +1,10 @@
-#include <vector>
 #include <assert.h>
 
 bool issame(std::vector<float> a, std::vector<float> b) {
     if (a.size() != b.size())
         return false;
     for (int i = 0; i<a.size(); i++)
-        if (abs(a[i] - b[i]) > 1e-9)
+        if (std::abs(a[i] - b[i]) > 1e-9)
             return false;
     return true;
 }
