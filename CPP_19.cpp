facb6f9979e1a2465ee5741d2cfc837cd02d592f
beep boop <githubbot@vadim.me>
2024-07-06 18:37:08 +0200

    Solution 5 of CPP/19, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/nabjymve Prompt: Complete the following code given the task description and function signature.

--- CPP_19.cpp
+++ CPP_19.cpp
@@ -1,32 +1,39 @@
-#include <algorithm>
+#include<stdio.h>
+#include<string>
+#include<map>
 using namespace std;
 
-string sort_numbers(string numbers) {
-    map<string, int> numMap = {{"zero", 0}, {"one", 1}, {"two", 2}, {"three", 3},
-                               {"four", 4}, {"five", 5}, {"six", 6}, {"seven", 7},
-                               {"eight", 8}, {"nine", 9}};
-    vector<string> numVec;
+string sort_numbers(string numbers){
+    map<string,int> num_map;
+    num_map["zero"] = 0;
+    num_map["one"] = 1;
+    num_map["two"] = 2;
+    num_map["three"] = 3;
+    num_map["four"] = 4;
+    num_map["five"] = 5;
+    num_map["six"] = 6;
+    num_map["seven"] = 7;
+    num_map["eight"] = 8;
+    num_map["nine"] = 9;
+
+    vector<string> vec;
     string temp;
-
-    for (int i = 0; i < numbers.length(); i++) {
-        if (numbers[i] == ' ') {
-            continue;
-        }
-        temp = "";
-        while (i < numbers.length() && numbers[i] != ' ') {
+    for(int i=0; i < numbers.length(); i++){
+        if(numbers[i] != ' ')
             temp += numbers[i];
-            i++;
+        else{
+            vec.push_back(temp);
+            temp = "";
         }
-        numVec.push_back(temp);
     }
+    vec.push_back(temp);
 
-    sort(numVec.begin(), numVec.end(), [&numMap](string a, string b) {
-        return to_string(numMap[a]) < to_string(numMap[b]);
-    });
+    sort(vec.begin(),vec.end());
 
     string result = "";
-    for (const auto &num : numVec) {
-        result += num + " ";
+    for(int i=0; i<vec.size(); i++){
+        result += vec[i] + " ";
     }
-    return result.substr(0, result.length() - 1);
+
+    return result;
 }
