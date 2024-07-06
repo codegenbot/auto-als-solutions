5f55453b2a02520d3fcd8046fa5457fcdfd8c7c3
beep boop <githubbot@vadim.me>
2024-07-06 02:11:17 +0200

    Solution 26 of Python/28, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/o6qupeag Prompt: Program did not receive expected input

--- Python_28.py
+++ Python_28.py
@@ -1,10 +1,2 @@
-from typing import List
-
-
-def concatenate(strings: List[str]) -> str:
-    result = ''.join(strings)
-    return result
-
-
 strings = input("Enter strings separated by space : ").split()
-print(concatenate(strings))
+print(' '.join(filter(None, strings)))
