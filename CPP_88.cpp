36bfb12bca93bd2cf81638cad46f1c1e6b0fd59e
beep boop <githubbot@vadim.me>
2024-07-06 06:26:45 +0200

    Solution 63 of CPP/88, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/cgpdw3dk Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from 1ea1d332-49b6-4140-8639-e9303d65ed65.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_88.cpp
+++ CPP_88.cpp
@@ -14,21 +14,13 @@ int main() {
     std::vector<int> array1 = {21, 14, 23, 11};
     std::vector<int> array2 = {23, 21, 14, 11};
     assert (issame(array1, array2));
-    
-    void sort_array(std::vector<int>& array) { 
+    void sort_array(std::vector<int>& array) {
         std::sort(array.begin(), array.end());
     }
-    sort_array(array1);
-    sort_array(array2);
-    
-    if (!issame(array1, array2)) {
-        for(int i = 0; i < array1.size(); i++) {
-            if (array1[i] > array2[i]) {
-                std::cout << "First different element is: " << array1[i] << "\n";
-                return;
-            }
-        }
-    } else {
-        std::cout << "Arrays are the same\n";
+    std::vector<int> array3 = array1;
+    sort_array(array3);
+    for(int i : array3) {
+        std::cout << i << " ";
     }
+    std::cout << "\n";
 }
