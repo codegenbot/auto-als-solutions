1fba9c5263b8566904b3d776e9af79eab76db53d
beep boop <githubbot@vadim.me>
2024-07-06 05:14:17 +0200

    Solution 34 of Python/159, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/w7dch2ke Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/57041667-d936-45e1-87b4-6836128ed 5e4.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_159.py
+++ Python_159.py
@@ -1,11 +1,3 @@
-```
-def eat(number, need, remaining):
-    if number < need and number < remaining:
-        eaten = number
-        return [eaten, 0]
-    elif need > remaining:
-        eaten = remaining
-        return [eaten, 0]
-    else:
-        eaten = min(number, need, remaining)
-        return [eaten, max(0, need - eaten)]
+def eat(hungry, needed, remaining):
+    eaten = min(needed, min(hungry, remaining))
+    return [eaten, needed - eaten]
