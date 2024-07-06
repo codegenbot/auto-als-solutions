ee7aa2f90e7558aa7217178a4985e585a327faf7
beep boop <githubbot@vadim.me>
2024-07-06 02:34:35 +0200

    Solution 28 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/ivx00olu Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from 804c5ba8-9c8d-494c-adef-6b313e3d5351.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,27 +1,25 @@
 ```cpp
-#include <iostream>
 #include <vector>
 #include <algorithm>
 
 using namespace std;
 
-vector<int> common(vector<int> a, vector<int> b){
+vector<int> common(vector<int> a, vector<int> b) {
+    set<int> bSet(b.begin(), b.end());
     vector<int> result;
-    for(int i=0; i<a.size();i++){
-        if(find(b.begin(),b.end(),a[i])!=b.end())result.push_back(a[i]);
+    for (int i = 0; i < a.size(); i++) {
+        if (bSet.count(a[i])) {
+            result.push_back(a[i]);
+            bSet.erase(a[i]);
+        }
     }
     return result;
 }
 
 bool issame(vector<int> a, vector<int> b) {
-    if (a.size() != b.size())
-        return false;
-    for (int i = 0; i < a.size(); i++)
-        if (find(b.begin(), b.end(), a[i]) == b.end())
-            return false;
-    return true;
+    return is_permutation(a.begin(), a.end(), b.begin(), b.end());
 }
 
-int main(){
+int main() {
     assert(common({4, 3, 2, 8}, {}) == {});
 }
