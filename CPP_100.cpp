fa93cdc66f8ed83d72d4c417994f6997be70d320
beep boop <githubbot@vadim.me>
2024-07-06 04:41:37 +0200

    Solution 37 of CPP/100, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/b41o8swj Prompt: 4344ce1b-11fa-4dca-9ac8-42333675b3a4.cpp:11:39: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<int> a,vector<int>b){       |                                       ^ 4344ce1b-11fa-4dca-9ac8-42333675b3a4.cpp:19:5: error: redefinition of ‘int main( )’    19 | int main(){       |     ^~~~ 4344ce1b-11fa-4dca-9ac8-42333675b3a4.cpp:8:6: note: ‘int main()’ previously defi ned here     8 | void main() { assert(same(make_a_pile(8), {1, 2, 4, 6, 8, 10, 12, 14}));  }       |      ^~~~ In file included from 4344ce1b-11fa-4dca-9ac8-42333675b3a4.cpp:10: 4344ce1b-11fa-4dca-9ac8-42333675b3a4.cpp: In function ‘int main()’: 4344ce1b-11fa-4dca-9ac8-42333675b3a4.cpp:20:20: error: ‘make_a_pile’ was not dec lared in this scope    20 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |                    ^~~~~~~~~~~ 4344ce1b-11fa-4dca-9ac8-42333675b3a4.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_100.cpp
+++ CPP_100.cpp
@@ -2,10 +2,16 @@
 #include <algorithm>
 
 bool same(vector<int> a, vector<int> b) { 
-    return a.size() == b.size() && all_of(a.begin(), a.end(), [b](int x) { return find(b.begin(), b.end(), x) != b.end(); }); }
+    return a.size() == b.size() && all_of(a.begin(), a.end(), [b](int x) { 
+        return find(b.begin(), b.end(), x) != b.end(); 
+    }); 
+} 
 
 vector<int> make_a_pile(int n) { 
-    vector<int> pile; for (int i = 1; i <= n; i++) pile.push_back(i % 2 ? i * 2 - 1 : i * 2); return pile; } 
+    vector<int> pile; 
+    for (int i = 1; i <= n; i++) 
+        pile.push_back(i % 2 ? i * 2 - 1 : i * 2); 
+    return pile; } 
 
 int main() {
     assert(same(make_a_pile(8), {1, 2, 4, 6, 8, 10, 12, 14}));
