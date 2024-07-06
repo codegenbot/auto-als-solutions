9728eda8faecb18f8847d08b143804a3a5cdaa46
beep boop <githubbot@vadim.me>
2024-07-06 09:06:44 +0200

    Solution 2 of CPP/157, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/phx6jum3 Prompt: 93963b06-68ca-495b-855b-1d6c8616394d.cpp:1:1: error: ‘Here’ does not name a type     1 | Here is the completed code:       | ^~~~ In file included from 93963b06-68ca-495b-855b-1d6c8616394d.cpp:10: 93963b06-68ca-495b-855b-1d6c8616394d.cpp: In function ‘int main()’: 93963b06-68ca-495b-855b-1d6c8616394d.cpp:12:13: error: ‘right_angle_triangle’ wa s not declared in this scope    12 |     assert (right_angle_triangle(2, 2, 10) == false);       |             ^~~~~~~~~~~~~~~~~~~~

--- CPP_157.cpp
+++ CPP_157.cpp
@@ -1,8 +1,11 @@
-Here is the solution:
-
 bool right_angle_triangle(float a, float b, float c) {
-    if (a*a + b*b == c*c || a*a + c*c == b*b || b*b + c*c == a*a)
+    if (a * a + b * b == c * c || a * a + c * c == b * b || b * b + c * c == a * a)
         return true;
     else
         return false;
+
+}
+
+int main() {
+    assert(right_angle_triangle(2, 2, 10) == false);
 }
