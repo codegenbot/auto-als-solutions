1199d015896f37521a90d09ab42fe06a05f0fe5c
beep boop <githubbot@vadim.me>
2024-07-06 05:09:29 +0200

    Solution 22 of CPP/110, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/bg482d9l Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- CPP_110.cpp
+++ CPP_110.cpp
@@ -1,9 +1,36 @@
-string exchange(vector<int> lst1, vector<int> lst2) {
-    int oddCount = 0;
-    for (int num : lst1) {
-        if (num % 2 != 0) {
-            oddCount++;
+```cpp
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    string exchange(vector<int> lst1, vector<int> lst2) {
+        int oddCount = 0;
+        for (int num : lst1) {
+            if (num % 2 != 0) {
+                oddCount++;
+            }
+        }
+        return (oddCount == 0 ? "NO" : "YES");
+    }
+
+    string oddOrEven(vector<int> vec) {
+        int count = 0;
+        for (int num : vec) {
+            if (num % 2 != 0) {
+                count++;
+            }
+        }
+        if (count > vec.size() / 2) {
+            return "YES";
+        } else {
+            return "NO";
         }
     }
-    return (oddCount == 0 ? "YES" : "NO"); 
-}
+
+    int main() {
+        cout << oddOrEven({100, 200}) << endl;
+        cout << "Test cases passed." << endl;
+        return 0;
+    }
+};
