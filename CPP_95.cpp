dc69a0ba07f4f2fb14fcba7fccb99f03eb70ad8a
beep boop <githubbot@vadim.me>
2024-07-06 22:52:17 +0200

    Solution 53 of CPP/95, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/4h6at1j3 Prompt:       |                      ^~~ 065e0421-8e74-4a3d-bd3b-6e7e3d929ae1.cpp:1:26: error: ‘string’ was not declared in this scope     1 | bool check_dict_case(map<string,string> dict){       |                          ^~~~~~ 065e0421-8e74-4a3d-bd3b-6e7e3d929ae1.cpp:1:33: error: ‘string’ was not declared in this scope     1 | bool check_dict_case(map<string,string> dict){       |                                 ^~~~~~ 065e0421-8e74-4a3d-bd3b-6e7e3d929ae1.cpp:1:41: error: ‘dict’ was not declared in  this scope     1 | bool check_dict_case(map<string,string> dict){       |                                         ^~~~ 065e0421-8e74-4a3d-bd3b-6e7e3d929ae1.cpp:1:45: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | bool check_dict_case(map<string,string> dict){       |                                             ^ In file included from 065e0421-8e74-4a3d-bd3b-6e7e3d929ae1.cpp:9: 065e0421-8e74-4a3d-bd3b-6e7e3d929ae1.cpp: In function ‘int main()’: 065e0421-8e74-4a3d-bd3b-6e7e3d929ae1.cpp:11:28: error: ‘check_dict_case’ cannot be used as a function    11 |     assert (check_dict_case({}) == false);       |             ~~~~~~~~~~~~~~~^~~~

--- CPP_95.cpp
+++ CPP_95.cpp
@@ -1,9 +1,9 @@
 #include <string>
 #include <map>
 
-bool check_dict_case(std::map<std::string, std::string> dict){
-    for(auto &pair : dict) {
-        if(pair.first.length() > 0 && (!isupper(pair.first[0]) || !islower(pair.first))) 
+bool check_dict_case(std::map<std::string, std::string> dict) {
+    for (auto &pair : dict) {
+        if (pair.first.length() > 0 && (!isupper(pair.first[0]) || !islower(pair.first))) 
             return false;
     }
     return true;
