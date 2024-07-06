2ee8d1f64b43bd1f4bf0f6b15d77d33eb37655f8
beep boop <githubbot@vadim.me>
2024-07-06 22:02:48 +0200

    Solution 10 of CPP/90, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/4d0ckwdj Prompt: Complete the following code given the task description and function signature.

--- CPP_90.cpp
+++ CPP_90.cpp
@@ -1,11 +1,5 @@
-int next_smallest(vector<int> lst) {
-    if (lst.empty()) return -1; // None in Python equivalent
-    vector<int> sorted = lst;
-    sort(sorted.begin(), sorted.end());
-    for (int i = 0; i < sorted.size() - 1; i++) {
-        if (sorted[i] != sorted[i + 1]) {
-            return sorted[i + 1];
-        }
-    }
-    return -1; // None in Python equivalent
+vector<int>::iterator it = min_element(lst.begin(), lst.end());
+if(it != lst.end() && ++it != lst.end()) {
+    return *it;
 }
+return -1; // or return None if that's what you want
