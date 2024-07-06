d3ff2ec6152143527224334e8090d19bf49ca5e6
beep boop <githubbot@vadim.me>
2024-07-07 00:47:32 +0200

    Solution 6 of CPP/43, ratio 0.7777777777777778 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/gx7bp187 Prompt: Complete the following code given the task description and function signature.

--- CPP_43.cpp
+++ CPP_43.cpp
@@ -3,12 +3,12 @@
 
 bool pairs_sum_to_zero(std::vector<int> l) {
     std::sort(l.begin(), l.end());
-    for (int i = 0; i < l.size() - 1; i++) {
+    for (int i = 0; i < l.size() - 1; ++i) {
         if (std::abs(l[i]) + l[i] == 0) return true;
         int j = i + 1;
         while (j < l.size() && l[j] <= std::abs(l[i])) {
             if (l[i] + l[j] == 0) return true;
-            j++;
+            ++j;
         }
     }
     return false;
