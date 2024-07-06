543f4575275a8030b2d04f4506b00448192dc994
beep boop <githubbot@vadim.me>
2024-07-06 23:47:09 +0200

    Solution 10 of CPP/144, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/q7aqeoe1 Prompt: Complete the following code given the task description and function signature.

--- CPP_144.cpp
+++ CPP_144.cpp
@@ -1,39 +1,16 @@
-#include <string>
-#include <sstream>
-
+#include<string>
 using namespace std;
 
 bool simplify(string x, string n) {
-    int numerator1 = 0, denominator1 = 0;
-    int numerator2 = 0, denominator2 = 0;
+    int a = stoi(split(x)[0]);
+    int b = stoi(split(x)[2]);
+    int c = stoi(split(n)[0]);
+    int d = stoi(split(n)[2]);
 
-    // Extract numerator and denominator from both fractions
-    size_t pos = x.find('/');
-    numerator1 = stoi(x.substr(0, pos));
-    denominator1 = stoi(x.substr(pos + 1));
-
-    pos = n.find('/');
-    numerator2 = stoi(n.substr(0, pos));
-    denominator2 = stoi(n.substr(pos + 1));
-
-    // Calculate the least common multiple of both denominators
-    int lcm = (denominator1 * denominator2) / __gcd(denominator1, denominator2);
-
-    // Calculate the new numerators and denominators
-    int numerator3 = numerator1 * (lcm / denominator1);
-    int denominator3 = lcm;
-
-    int numerator4 = numerator2 * (lcm / denominator2);
-    int denominator4 = lcm;
-
-    // Check if both fractions are equal
-    return numerator3 == numerator4;
+    return (a*d) == (b*c);
 }
 
-// Function to calculate the greatest common divisor of two numbers
-int __gcd(int a, int b) {
-    if (b == 0)
-        return a;
-    else
-        return __gcd(b, a % b);
+string split(string s) {
+    size_t found = s.find('/');
+    return s.substr(0,found);
 }
