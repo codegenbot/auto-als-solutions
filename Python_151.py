b5aa831406e30924aa0a13f4dafe508d359477e8
beep boop <githubbot@vadim.me>
2024-07-06 14:10:08 +0200

    Solution 44 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/dt00hj12 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7bed9a62-be36-4bbc-89fd-1ba083b1b c89.py", line 12, in <module>     check(double_the_difference)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7bed9a62-be36-4bbc-89fd-1ba083b1b c89.py", line 8, in check     double_the_difference(lst) == odd_sum                           ^^^ NameError: name 'lst' is not defined. Did you mean: 'list'?

--- Python_151.py
+++ Python_151.py
@@ -1,5 +1,3 @@
-def double_the_sum_of_squares(numbers):
-    if len(numbers) < 2:
-        raise ValueError("List should contain at least two elements.")
-    numbers = sorted([num for num in numbers if isinstance(num, int) and num > 0])
-    return (numbers[0] - numbers[-1]) * 2
+def double_the_difference(lst):
+    result = sum(i**2 for i in lst if isinstance(i, int) and i > 0)
+    return result * 2
