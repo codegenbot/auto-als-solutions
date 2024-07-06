be592178750fa170bf6bcb1ec18701b0b8b83c61
beep boop <githubbot@vadim.me>
2024-07-07 00:52:55 +0200

    Solution 69 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/sllk2064 Prompt:    27 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ ca4eb7e3-1f6d-4a0d-8e49-86ef38cb1508.cpp:27:44: error: ‘b’ was not declared in t his scope    27 | bool issame(vector<string> a,vector<string>b){       |                                            ^ ca4eb7e3-1f6d-4a0d-8e49-86ef38cb1508.cpp:27:45: error: expression list treated a s compound expression in initializer [-fpermissive]    27 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from ca4eb7e3-1f6d-4a0d-8e49-86ef38cb1508.cpp:26: ca4eb7e3-1f6d-4a0d-8e49-86ef38cb1508.cpp: In function ‘int main()’: ca4eb7e3-1f6d-4a0d-8e49-86ef38cb1508.cpp:36:20: error: ‘sorted_list_sum’ was not  declared in this scope    36 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ ca4eb7e3-1f6d-4a0d-8e49-86ef38cb1508.cpp:36:19: error: ‘issame’ cannot be used a s a function    36 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -4,28 +4,6 @@ bool issame(vector<string> a,vector<string>b){
 
 int main() {
     assert(issame({{"aaaa", "bbbb", "dd", "cc"}}, {{"cc", "dd", "aaaa", "bbbb"}}));
-    // Call the function here
     vector<string> result = sorted_list_sum({"aaaa", "bbbb", "dd", "cc"});
     return 0;
-}
-
-vector<string> sorted_list_sum(vector<string> lst) {
-    vector<string> result;
-
-    for (const string& str : lst) {
-        if (str.length() % 2 == 0) {
-            result.push_back(str);
-        }
-    }
-
-    sort(result.begin(), result.end(),
-         [](const string& a, const string& b) {
-             if (a.length() != b.length()) {
-                 return a.length() < b.length();
-             } else {
-                 return a < b;
-             }
-         });
-
-    return result;
 }
