7b430bbcf4d30f4d84ec7e5f612eba0095108b64
beep boop <githubbot@vadim.me>
2024-07-06 05:09:06 +0200

    Solution 52 of Python/119, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/629l2qg8 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f8f2c2d4-5191-4297-8f3b-a2497deeb b86.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_119.py
+++ Python_119.py
@@ -1,4 +1,5 @@
 def match_parens(lst):
+    """Complete the following code given the task description and function signature.."""
     for string in lst:
         open_count = 0
         close_count = 0
@@ -9,11 +10,10 @@ def match_parens(lst):
                 open_count += 1
             elif char == ')':
                 close_count += 1
-                
+        
         if open_count != close_count:
             matching = False
-            
-    if not any(matching):
-        return "No"
-    else:
-        return "Yes"
+        
+        if not matching:
+            return "No"
+    return "Yes"
