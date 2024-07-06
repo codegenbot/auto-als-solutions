2381b698cd700b6985a0dab49601ae6925c2e366
beep boop <githubbot@vadim.me>
2024-07-06 04:29:29 +0200

    Solution 6 of CPP/99, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/nskabmp9 Prompt: bc0b34cc-49e8-48bb-b055-ca5b0879d768.cpp:3:21: error: ‘string’ was not declared in this scope     3 | int closest_integer(string value) {       |                     ^~~~~~ bc0b34cc-49e8-48bb-b055-ca5b0879d768.cpp:3:21: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from bc0b34cc-49e8-48bb-b055-ca5b0879d768.cpp:1: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from bc0b34cc-49e8-48bb-b055-ca5b0879d768.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ In file included from bc0b34cc-49e8-48bb-b055-ca5b0879d768.cpp:8: bc0b34cc-49e8-48bb-b055-ca5b0879d768.cpp: In function ‘int main()’: bc0b34cc-49e8-48bb-b055-ca5b0879d768.cpp:10:28: error: ‘closest_integer’ cannot be used as a function    10 |     assert (closest_integer("0") == 0);       |             ~~~~~~~~~~~~~~~^~~~~

--- CPP_99.cpp
+++ CPP_99.cpp
@@ -1,2 +1,7 @@
-double num = stod(value);
-return (num >= 0) ? ceil(num) : floor(num);
+#include <string>
+#include <cmath>
+
+int closest_integer(std::string value) {
+    double num = stod(value);
+    return (num > 0) ? ceil(num) : floor(num);
+}
