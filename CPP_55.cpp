c8062c1734ae6f8f586d4c2d06bb68bf795cbe2b
beep boop <githubbot@vadim.me>
2024-07-06 03:33:25 +0200

    Solution 52 of CPP/55, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/sa7iw70j Prompt:  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘std::basic_ostream <_CharT, _Traits>& std::operator<<(std::basic_ostream<_CharT, _Traits>&, const s td::__cxx11::basic_string<_CharT, _Traits, _Allocator>&) [with _CharT = char; _T raits = std::char_traits<char>; _Alloc = std::allocator<char>]’: 675c8643-61a4-4bd7-b2eb-60501d0dc878.cpp:24:13:   required from here /usr/include/c++/11/bits/basic_string.h:6536:57: error: ‘const class std::__cxx1 1::basic_string<char>’ has no member named ‘size’  6536 |       return __ostream_insert(__os, __str.data(), __str.size());       |                                                   ~~~~~~^~~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- CPP_55.cpp
+++ CPP_55.cpp
@@ -1,6 +1,5 @@
-```
+```cpp
 #include <iostream>
-#include <sstream>
 
 using namespace std;
 
@@ -21,8 +20,5 @@ int main()
     int n;
     cout << "Enter the value of n: ";
     cin >> n;
-    ostringstream oss;
-    oss << "The " << to_string(n) << "-th Fibonacci number is: " << to_string(fib(n));
-    cout << oss.str() << endl;
+    cout << "The " << n << "-th Fibonacci number is: " << fib(n) << endl;
     return 0;
-}
