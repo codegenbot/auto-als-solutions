f0175c29843445d6199cf1cfd39de0a559fc98dd
beep boop <githubbot@vadim.me>
2024-07-06 16:01:18 +0200

    Solution 40 of Python/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/j3m9w7h7 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f41ab336-7a22-47b6-8ad6-606eeb6f3 840.py", line 29, in <module>     check(minPath)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f41ab336-7a22-47b6-8ad6-606eeb6f3 840.py", line 26, in check     assert minPath([[1, 3], [3, 2]], 10) == [1, 3, 1, 3, 1, 3, 1, 3, 1, 3]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_129.py
+++ Python_129.py
@@ -4,15 +4,8 @@ def minPath(grid, k):
     queue = [(0, 0, [grid[0][0]])]
     result = []
 
-    k_count = 0
     while queue:
         x, y, path = queue.pop(0)
-        for p in path[1:]:
-            k_count += grid[x][y] == int(p)
-        if k_count > k:
-            return []
-        k_count = 0
-
         if len(path) > k:
             continue
         if len(path) == k:
@@ -24,5 +17,16 @@ def minPath(grid, k):
             if 0 <= nx < N and 0 <= ny < N and not visited[nx][ny]:
                 visited[nx][ny] = True
                 queue.append((nx, ny, path + [grid[nx][ny]]))
-
+        else:
+            for dx, dy in sorted(
+                [
+                    (dx, dy)
+                    for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]
+                    if 0 <= x + dx < N
+                    and 0 <= y + dy < N
+                    and not visited[x + dx][y + dy]
+                ]
+            ):
+                nx, ny = x + dx, y + dy
+                queue.append((nx, ny, path + [grid[nx][ny]]))
     return []
