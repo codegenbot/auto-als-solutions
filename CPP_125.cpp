2af07ea374c949bb0c6ece4e7832b88228fba177
beep boop <githubbot@vadim.me>
2024-07-06 08:14:13 +0200

    Solution 36 of CPP/125, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/g4r9w7pd Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from d820285c-1bc6-4785-9a63-5a7dfa113170.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_125.cpp
+++ CPP_125.cpp
@@ -1,15 +1,15 @@
-```
 #include <vector>
 #include <string>
+#include <initializer_list>
 
 bool issame(const std::vector<std::string>& a, const std::vector<std::string>& b) {
     return a == b;
 }
 
-std::vector<std::string> split_words(std::string txt) {
-    std::vector<std::string> result;
+std::vector<std::string> split_words(string txt) {
+    vector<string> result;
     size_t pos = 0;
-    while ((pos = txt.find(' ')) != std::string::npos) {
+    while ((pos = txt.find(' ')) != string::npos) {
         result.push_back(txt.substr(0, pos));
         txt.erase(0, pos + 1);
     }
@@ -21,6 +21,7 @@ std::vector<std::string> split_words(std::string txt) {
 }
 
 int main() {
+    #define _CRT_SECURE_NO_WARNINGS
     std::string user_input;
     std::cout << "Enter a string: ";
     std::getline(std::cin, user_input);
