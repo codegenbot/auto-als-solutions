177fcbd5e98d02de747613a8d28a927ed24322d8
beep boop <githubbot@vadim.me>
2024-07-06 08:01:12 +0200

    Solution 84 of Python/103, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/fw0szpvy Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/213dde0c-6fb5-4cfe-b1f0-494a41600 274.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_103.py
+++ Python_103.py
@@ -1,4 +1,2 @@
-def rounded_avg(n, m):
-    import math
-    avg = math.ceil((n + m) / 2)
-    return bin(avg)[2:].zfill(math.floor(math.log2(avg)) + 1)
+def average_score(grades):
+    return sum(grades) / len(grades)
