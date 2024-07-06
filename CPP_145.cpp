4ce88a6b3d854870ef384cc095000fd580394837
beep boop <githubbot@vadim.me>
2024-07-06 11:59:53 +0200

    Solution 71 of CPP/145, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/pmi4gz2f Prompt: /usr/include/c++/11/bits/stl_algobase.h:2069:14:   required from ‘_RandomAccessI terator std::__find_if(_RandomAccessIterator, _RandomAccessIterator, _Predicate,  std::random_access_iterator_tag) [with _RandomAccessIterator = __gnu_cxx::__nor mal_iterator<int*, std::vector<int> >; _Predicate = __gnu_cxx::__ops::_Iter_nega te<sortByDigitSum(std::vector<int>)::<lambda(int)> >]’ /usr/include/c++/11/bits/stl_algo.h:106:28:   required from ‘_InputIterator std: :__find_if_not(_InputIterator, _InputIterator, _Predicate) [with _InputIterator = __gnu_cxx::__normal_iterator<int*, std::vector<int> >; _Predicate = __gnu_cxx: :__ops::_Iter_pred<sortByDigitSum(std::vector<int>)::<lambda(int)> >]’ /usr/include/c++/11/bits/stl_algo.h:1586:35:   required from ‘_ForwardIterator s td::__stable_partition(_ForwardIterator, _ForwardIterator, _Predicate) [with _Fo rwardIterator = __gnu_cxx::__normal_iterator<int*, std::vector<int> >; _Predicat e = __gnu_cxx::__ops::_Iter_pred<sortByDigitSum(std::vector<int>)::<lambda(int)>  >]’ /usr/include/c++/11/bits/stl_algo.h:1634:37:   required from ‘_BIter std::stable _partition(_BIter, _BIter, _Predicate) [with _BIter = __gnu_cxx::__normal_iterat or<int*, std::vector<int> >; _Predicate = sortByDigitSum(std::vector<int>)::<lam bda(int)>]’ 5a5c3fdc-5a88-499a-9b87-2f9508d27e5b.cpp:11:26:   required from here /usr/include/c++/11/bits/predefined_ops.h:395:19: error: invalid cast from type ‘std::tuple<int, bool>’ to type ‘bool’   395 |         { return !bool(_M_pred(*__it)); }       |                   ^~~~~~~~~~~~~~~~~~~~

--- CPP_145.cpp
+++ CPP_145.cpp
@@ -7,7 +7,7 @@ bool isSame(const std::vector<int>& a, const std::vector<int>& b) {
 
 std::vector<int> sortByDigitSum(std::vector<int> numbers) {
     auto lambda = [](int x) { return abs(x); };
-    std::stable_sort(numbers.begin(), numbers.end(), [lambda](int x, int y) { return lambda(x) < lambda(y); });
+    std::stable_sort(numbers.begin(), numbers.end(), [lambda](int x, int y) { return lambda(y) - lambda(x); });
     std::stable_partition(numbers.begin(), numbers.end(), [x](int x) { return (x > 0); });
     return numbers;
 }
