4ab9a1b082e04f6f77f2aa4c21fcf415491d7b67
beep boop <githubbot@vadim.me>
2024-07-06 03:28:41 +0200

    Solution 33 of CPP/73, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/s4xvbypn Prompt: 47d523c6-bbec-4e7a-9989-829ba65d72b0.cpp:1:21: error: ‘vector’ was not declared in this scope     1 | int smallest_change(vector<int> arr) {       |                     ^~~~~~ 47d523c6-bbec-4e7a-9989-829ba65d72b0.cpp:1:28: error: expected primary-expressio n before ‘int’     1 | int smallest_change(vector<int> arr) {       |                            ^~~ In file included from 47d523c6-bbec-4e7a-9989-829ba65d72b0.cpp:24: 47d523c6-bbec-4e7a-9989-829ba65d72b0.cpp: In function ‘int main()’: 47d523c6-bbec-4e7a-9989-829ba65d72b0.cpp:26:28: error: ‘smallest_change’ cannot be used as a function    26 |     assert (smallest_change({0, 1}) == 1);       |             ~~~~~~~~~~~~~~~^~~~~~~~

--- CPP_73.cpp
+++ CPP_73.cpp
@@ -1,4 +1,3 @@
-```cpp
 #include <vector>
 #include <cstddef>
 
@@ -17,7 +16,7 @@ size_t smallest_change(vector<int> arr) {
             if (arr[i] == arr[j]) {
                 dp[i][j].push_back(dp[i+1][j-1].size());
             } else {
-                dp[i][j].push_back(min(size_t(1), dp[i+1].size()) + 1);
+                dp[i][j].push_back(min(size_t(1), dp[i+1][n-1].size()) + 1);
             }
         }
     }
