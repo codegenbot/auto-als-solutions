806d50408132c657bf2587457e8456fd84283e90
beep boop <githubbot@vadim.me>
2024-07-06 09:35:28 +0200

    Solution 22 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/y0i7d1af Prompt: 95c6f3a8-c6be-4186-9f11-e2c3dfb21a80.cpp:6:1: note: ‘strlen’ is defined in heade r ‘<cstring>’; did you forget to ‘#include <cstring>’?     5 | #include <openssl/md5.h>   +++ |+#include <cstring>     6 | 95c6f3a8-c6be-4186-9f11-e2c3dfb21a80.cpp:15:15: warning: ‘int MD5_Update(MD5_CTX *, const void*, size_t)’ is deprecated: Since OpenSSL 3.0 [-Wdeprecated-declarat ions]    15 |     MD5_Update(&mdContext, ptr, len);       |     ~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~ In file included from 95c6f3a8-c6be-4186-9f11-e2c3dfb21a80.cpp:5: /usr/include/openssl/md5.h:50:27: note: declared here    50 | OSSL_DEPRECATEDIN_3_0 int MD5_Update(MD5_CTX *c, const void *data, size_ t len);       |                           ^~~~~~~~~~ 95c6f3a8-c6be-4186-9f11-e2c3dfb21a80.cpp:16:14: warning: ‘int MD5_Final(unsigned  char*, MD5_CTX*)’ is deprecated: Since OpenSSL 3.0 [-Wdeprecated-declarations]    16 |     MD5_Final(result, &mdContext);       |     ~~~~~~~~~^~~~~~~~~~~~~~~~~~~~ In file included from 95c6f3a8-c6be-4186-9f11-e2c3dfb21a80.cpp:5: /usr/include/openssl/md5.h:51:27: note: declared here    51 | OSSL_DEPRECATEDIN_3_0 int MD5_Final(unsigned char *md, MD5_CTX *c);       |                           ^~~~~~~~~

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -1,3 +1,7 @@
+#include <string>
+#include <sstream>
+#include <iomanip>
+#include <openssl/md5.h>
 #include <cstring>
 
 std::string string_to_md5(const char* text) {
@@ -5,15 +9,11 @@ std::string string_to_md5(const char* text) {
 
     unsigned char result[16];
     MD5_CTX mdContext;
-#ifndef OPENSSL_3_0_0_OR_LATER
     MD5_Init(&mdContext);
     const char* ptr = text;
     size_t len = strlen(text);
-    MD5_Update(&mdContext, ptr, len);
-    MD5_Final(result, &mdContext);
-#else
-    // Use new functions (not shown here)
-#endif
+    MD5_Update((void*) &mdContext, (const void*) ptr, len);
+    MD5_Final(result, (void*) &mdContext);
 
     std::ostringstream oss;
     for (int i = 0; i < 16; ++i) {
