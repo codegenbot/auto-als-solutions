0dec24580d90c01af9abe0cdf52c22d5de9b550b
beep boop <githubbot@vadim.me>
2024-07-06 23:14:00 +0200

    Solution 4 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/4p7xu0od Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,55 +1,30 @@
-#include<string>
-#include<boost/any.hpp>
-using namespace std;
-
-boost::any compare_one(boost::any a, boost::any b) {
-    if (a.type() == typeid(int) && b.type() == typeid(double)) {
-        return b;
-    }
-    else if (a.type() == typeid(double) && b.type() == typeid(int)) {
+if (a.type() == typeid(int) && b.type() == typeid(int)) {
+    int x = boost::any_cast<int>(a);
+    int y = boost::any_cast<int>(b);
+    if (x > y)
         return a;
-    }
-    else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-        string str_a = boost::any_cast<string>(a);
-        string str_b = boost::any_cast<string>(b);
-        if (str_a > str_b) {
-            return a;
-        }
-        else if (str_a < str_b) {
-            return b;
-        }
-        else {
-            return boost::any("None");
-        }
-    }
-    else if (a.type() == typeid(string) && b.type() == typeid(double)) {
-        string str_a = boost::any_cast<string>(a);
-        double num_b = boost::any_cast<double>(b);
-        if (stod(str_a) > num_b) {
-            return a;
-        }
-        else if (stod(str_a) < num_b) {
-            return b;
-        }
-        else {
-            return boost::any("None");
-        }
-    }
-    else if (a.type() == typeid(double) && b.type() == typeid(string)) {
-        double num_a = boost::any_cast<double>(a);
-        string str_b = boost::any_cast<string>(b);
-        if (num_a > stod(str_b)) {
-            return a;
-        }
-        else if (num_a < stod(str_b)) {
-            return b;
-        }
-        else {
-            return boost::any("None");
-        }
-    }
-    else {
-        // This line should never be reached, but it's here for completeness
-        return boost::any("None");
-    }
+    else if (y > x)
+        return b;
+    else
+        return "None";
+} else if (a.type() == typeid(float) && b.type() == typeid(float)) {
+    float x = boost::any_cast<float>(a);
+    float y = boost::any_cast<float>(b);
+    if (x > y)
+        return a;
+    else if (y > x)
+        return b;
+    else
+        return "None";
+} else if (a.type() == typeid(string) && b.type() == typeid(string)) {
+    string x = boost::any_cast<string>(a);
+    string y = boost::any_cast<string>(b);
+    if (stof(x) > stof(y))
+        return a;
+    else if (stof(y) > stof(x))
+        return b;
+    else
+        return "None";
+} else {
+    throw invalid_argument("Invalid types");
 }
