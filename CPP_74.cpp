44d72d2c5263bb6fdc94fe1fe1b466b949ff15de
beep boop <githubbot@vadim.me>
2024-07-06 08:24:01 +0200

    Solution 97 of CPP/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/k9lvoh5f Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from 696eb912-fa29-4d6c-9ef5-7283ac152a84.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -22,12 +22,12 @@ bool issame(vector<string> a, const vector<string>& b) {
 vector<string> total_match(vector<string> lst1, vector<string> lst2) {
     int sum1 = 0;
     for (const auto& str : lst1) {
-        sum1 += str.length();
+        sum1 += std::size_t(str.length());
     }
     
     int sum2 = 0;
     for (const auto& str : lst2) {
-        sum2 += str.length();
+        sum2 += std::size_t(str.length());
     }
 
     if (issame(lst1, lst2)) {
