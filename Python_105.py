21171ff47ccb92fdb3cdb2cae7b5876a6d045a80
beep boop <githubbot@vadim.me>
2024-07-06 03:58:22 +0200

    Solution 84 of Python/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/ubalkc5i Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d66ae36f-28a9-4dba-b2d6-f19b1ed37 15b.py", line 11, in <module>     check(by_length)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d66ae36f-28a9-4dba-b2d6-f19b1ed37 15b.py", line 8, in check     assert by_length([9, 4, 8]) == ["Nine", "Eight", "Four"]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_105.py
+++ Python_105.py
@@ -1,4 +1,5 @@
+```Python
 def by_length(arr):
-    mapping = {1: "One", 2: "Two", 3: "Three", 4: "Four", 5: "Five", 
-               6: "Six", 7: "Seven", 8: "Eight", 9: "Nine"}
-    return [mapping[i] for i in arr]
+    num_to_name = {1: "One", 2: "Two", 3: "Three", 4: "Four", 
+                   5: "Five", 6: "Six", 7: "Seven", 8: "Eight", 9: "Nine"}
+    return [num_to_name[i] for i in arr if len(num_to_name[str(i)]) >= 4]
