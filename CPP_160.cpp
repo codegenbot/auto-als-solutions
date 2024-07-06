1877fefc4df8cb2d83ee3d0a1b13230c659042f6
beep boop <githubbot@vadim.me>
2024-07-07 01:01:13 +0200

    Solution 11 of CPP/160, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/92a7m90d Prompt: Complete the following code given the task description and function signature.

--- CPP_160.cpp
+++ CPP_160.cpp
@@ -1,18 +1,23 @@
 int do_algebra(vector<string> operator_, vector<int> operand) {
-    int result = 0;
-    for (int i = 1; i < operand.size(); i++) {
-        if (*operator_.begin() == "+") {
-            result += operand[i];
-        } else if (*operator_.begin() == "-") {
-            result -= operand[i];
-        } else if (*operator_.begin() == "*") {
-            result *= operand[i];
-        } else if (*operator_.begin() == "//") {
-            result /= (operand[i] + 0.5);
-        } else if (*operator_.begin() == "**") {
-            result = pow(operand[0], operand[i]);
-        }
-        operator_.erase(operator_.begin());
+    int result = operand[0];
+    string opstr = "";
+    for (int i = 1; i < operator_.size(); i++) {
+        opstr += operator_[i];
+        result = eval(result, opstr, &operand[i]);
+        opstr.clear();
     }
     return result;
+}
+
+long long eval(int a, const string& op, int b) {
+    if (op == "+")
+        return a + b;
+    else if (op == "-")
+        return a - b;
+    else if (op == "*")
+        return a * b;
+    else if (op == "//")
+        return a / b;
+    else
+        return pow(a, b);
 }
