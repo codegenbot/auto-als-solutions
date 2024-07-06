96bb95b502c6bfaeed981e9bff1b213f51388577
beep boop <githubbot@vadim.me>
2024-07-06 03:08:58 +0200

    Solution 30 of Python/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/ubalkc5i Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a68f2b1a-a49f-4b84-86c6-9f73b6140 e82.py", line 9, in <module>     check(by_length)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a68f2b1a-a49f-4b84-86c6-9f73b6140 e82.py", line 6, in check     assert by_length([9, 4, 8]) == ["Nine", "Eight", "Four"]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_105.py
+++ Python_105.py
@@ -1,3 +1,2 @@
-```
 def by_length(arr):
-    return [str(num) + " - " + ("One" if num in range(1, 11) else "Unknown") for num in arr]
+    return [name for num, name in zip(map(str, arr), ["One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"]) if name[0] == '1']
