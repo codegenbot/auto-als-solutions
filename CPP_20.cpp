ebe252514f70f9fbf9013f41fa2748902fdc888e
beep boop <githubbot@vadim.me>
2024-07-07 00:06:57 +0200

    Solution 72 of CPP/20, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/yxbztcci Prompt:    23 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 2015a7fb-c70b-457b-9567-914408984290.cpp:23:36: error: expected primary-expressi on before ‘float’    23 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 2015a7fb-c70b-457b-9567-914408984290.cpp:23:43: error: expression list treated a s compound expression in initializer [-fpermissive]    23 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 2015a7fb-c70b-457b-9567-914408984290.cpp:22: 2015a7fb-c70b-457b-9567-914408984290.cpp: In function ‘int main()’: 2015a7fb-c70b-457b-9567-914408984290.cpp:32:20: error: ‘find_closest_elements’ w as not declared in this scope    32 |     assert (issame(find_closest_elements({1.1, 2.2, 3.1, 4.1, 5.1}) , {2 .2, 3.1}));       |                    ^~~~~~~~~~~~~~~~~~~~~ 2015a7fb-c70b-457b-9567-914408984290.cpp:32:19: error: ‘issame’ cannot be used a s a function    32 |     assert (issame(find_closest_elements({1.1, 2.2, 3.1, 4.1, 5.1}) , {2 .2, 3.1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~

--- CPP_20.cpp
+++ CPP_20.cpp
@@ -1,7 +1,14 @@
 bool issame(vector<float> a,vector<float>b){
-    if(a.size() != b.size())return false;
-    for(int i = 0; i < a.size(); ++i) {
-        if(abs(a[i] - b[i]) > 1e-6) return false;
+    if(a.size()!=b.size())
+        return false;
+    for(int i=0;i<a.size();i++){
+        if(abs(a[i]-b[i]) > numeric_limits<float>::epsilon()){
+            return false;
+        }
     }
     return true;
+}
+
+int main() {
+    assert (issame(find_closest_elements({1.1, 2.2, 3.1, 4.1, 5.1}) , {2.2, 3.1}));
 }
