d58dc1ed6c2d2a42c65cd721a34012bf8b652f78
beep boop <githubbot@vadim.me>
2024-07-06 18:51:20 +0200

    Solution 61 of CPP/11, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/jfi7nmbi Prompt: 16820a00-040e-4d1f-ba9d-64aa07cdbd1b.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string string_xor(string a, string b) {       | ^~~~~~ In file included from 16820a00-040e-4d1f-ba9d-64aa07cdbd1b.cpp:17: 16820a00-040e-4d1f-ba9d-64aa07cdbd1b.cpp: In function ‘int main()’: 16820a00-040e-4d1f-ba9d-64aa07cdbd1b.cpp:19:13: error: ‘string_xor’ was not decl ared in this scope    19 |     assert (string_xor("0101", "0000") == "0101");       |             ^~~~~~~~~~

--- CPP_11.cpp
+++ CPP_11.cpp
@@ -1,16 +1,16 @@
 #include <string>
 
-string string_xor(string a, string b) {
-    string result = "";
+std::string string_xor(std::string a, std::string b) {
+    std::string result = "";
     for(int i = 0; i < a.length(); i++) {
         char c1 = a[i];
         char c2 = (i < b.length()) ? b[i] : '0';
-        if(c1 == '0' && c2 == '0') {
+        if(c1 == '1' && c2 == '1') {
             result += '0';
-        } else if((c1 == '0' || c2 == '0')) {
+        } else if(c1 == '1' || c2 == '1') {
             result += '1';
         } else {
-            result += '1';
+            result += '0';
         }
     }
     return result;
