ef1a06554322a6ed742dc72e320f73ae3b84588d
beep boop <githubbot@vadim.me>
2024-07-06 18:58:29 +0200

    Solution 46 of CPP/20, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/p6t224qv Prompt:    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 38d68fd1-8a18-49f3-af09-f56e4bfdabb6.cpp:20:36: error: expected primary-expressi on before ‘float’    20 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 38d68fd1-8a18-49f3-af09-f56e4bfdabb6.cpp:20:43: error: expression list treated a s compound expression in initializer [-fpermissive]    20 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 38d68fd1-8a18-49f3-af09-f56e4bfdabb6.cpp:19: 38d68fd1-8a18-49f3-af09-f56e4bfdabb6.cpp: In function ‘int main()’: 38d68fd1-8a18-49f3-af09-f56e4bfdabb6.cpp:29:20: error: ‘find_closest_elements’ w as not declared in this scope    29 |     assert (issame(find_closest_elements({1.1, 2.2, 3.1, 4.1, 5.1}) , {2 .2, 3.1}));       |                    ^~~~~~~~~~~~~~~~~~~~~ 38d68fd1-8a18-49f3-af09-f56e4bfdabb6.cpp:29:19: error: ‘issame’ cannot be used a s a function    29 |     assert (issame(find_closest_elements({1.1, 2.2, 3.1, 4.1, 5.1}) , {2 .2, 3.1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~

--- CPP_20.cpp
+++ CPP_20.cpp
@@ -1,5 +1,16 @@
 #include <algorithm>
-#include <cmath>
+#include <numeric>
+#include <vector>
+
+bool issame(vector<float> a,vector<float>b){
+    if(a.size() != b.size())
+        return false;
+    for(int i = 0; i < a.size(); i++){
+        if(abs(a[i] - b[i]) > 1e-5)
+            return false;
+    }
+    return true;
+}
 
 vector<float> find_closest_elements(vector<float> numbers) {
     sort(numbers.begin(), numbers.end());
@@ -15,14 +26,4 @@ vector<float> find_closest_elements(vector<float> numbers) {
     }
     
     return closest;
-}
-
-bool issame(vector<float> a, vector<float> b){
-    if(a.size() != b.size())
-        return false;
-    for(int i = 0; i < a.size(); i++){
-        if(abs(a[i] - b[i]) > 1e-5)
-            return false;
-    }
-    return true;
 }
