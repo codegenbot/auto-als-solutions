1b0d77606fa310f3012fc02f988436648c89fb42
beep boop <githubbot@vadim.me>
2024-07-06 03:47:17 +0200

    Solution 93 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/y2phiwh4 Prompt: 72e58060-53d1-4223-929b-6a7bf407ba77.cpp:21:39: error: expression list treated a s compound expression in initializer [-fpermissive]    21 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 72e58060-53d1-4223-929b-6a7bf407ba77.cpp:20: 72e58060-53d1-4223-929b-6a7bf407ba77.cpp: In function ‘int main()’: 72e58060-53d1-4223-929b-6a7bf407ba77.cpp:30:35: error: cannot convert ‘<brace-en closed initializer list>’ to ‘int’    30 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |                    ~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~       |                                   |       |                                   <brace-enclosed initializer list> 72e58060-53d1-4223-929b-6a7bf407ba77.cpp:5:54: note:   initializing argument 1 o f ‘std::vector<int> filter_integers(int)’     5 | std::vector<int> filter_integers(std::list<std::any> values) {       |                                  ~~~~~~~~~~~~~~~~~~~~^~~~~~ In file included from 72e58060-53d1-4223-929b-6a7bf407ba77.cpp:20: 72e58060-53d1-4223-929b-6a7bf407ba77.cpp:30:19: error: ‘issame’ cannot be used a s a function    30 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -20,3 +20,8 @@ std::vector<int> filter_integers(std::list<std::any> values) {
     }
     return result;
 }
+
+int main() {
+    assert(issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}), {3, 3, 3}));
+    return 0;
+}
