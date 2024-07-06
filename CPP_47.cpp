88b349a9343778578f424ff1ce4e4232e4860e4f
beep boop <githubbot@vadim.me>
2024-07-07 00:47:47 +0200

    Solution 1 of CPP/47, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/2lgvr1o7 Prompt: Complete the following code given the task description and function signature.

--- CPP_47.cpp
+++ CPP_47.cpp
@@ -1,6 +1,7 @@
-sort(l.begin(), l.end());
+std::sort(l.begin(), l.end());
 int n = l.size();
-if(n%2==0)
-    return (l[n/2-1]+l[n/2])/2.0;
-else
+if(n % 2 == 0) {
+    return (l[n/2 - 1] + l[n/2]) / 2;
+} else {
     return l[n/2];
+}
