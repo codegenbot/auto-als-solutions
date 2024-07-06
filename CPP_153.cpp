287b95d53dae03c4e445da9bd538d6d787be227f
beep boop <githubbot@vadim.me>
2024-07-07 01:09:15 +0200

    Solution 45 of CPP/153, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/7ij0la7r Prompt:       |                         ^~~~~~ 87e15f70-3171-4e13-b7e4-e089e446b218.cpp:1:43: error: ‘vector’ was not declared in this scope     1 | int Strongest_Extension(string class_name,vector<string> extensions){       |                                           ^~~~~~ 87e15f70-3171-4e13-b7e4-e089e446b218.cpp:1:50: error: ‘string’ was not declared in this scope     1 | int Strongest_Extension(string class_name,vector<string> extensions){       |                                                  ^~~~~~ 87e15f70-3171-4e13-b7e4-e089e446b218.cpp:1:58: error: ‘extensions’ was not decla red in this scope     1 | int Strongest_Extension(string class_name,vector<string> extensions){       |                                                          ^~~~~~~~~~ 87e15f70-3171-4e13-b7e4-e089e446b218.cpp:1:68: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | int Strongest_Extension(string class_name,vector<string> extensions){       |                                                                    ^ In file included from 87e15f70-3171-4e13-b7e4-e089e446b218.cpp:21: 87e15f70-3171-4e13-b7e4-e089e446b218.cpp: In function ‘int main()’: 87e15f70-3171-4e13-b7e4-e089e446b218.cpp:23:32: error: ‘Strongest_Extension’ can not be used as a function    23 |     assert (Strongest_Extension("Sp", {"671235", "Bb"}) == "Sp.671235");       |             ~~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_153.cpp
+++ CPP_153.cpp
@@ -10,8 +10,8 @@ std::string Strongest_Extension(std::string class_name, std::vector<std::string>
     for(auto extension : extensions){
         int cap = 0, sm = 0;
         for(char c : extension){
-            if(isupper(c)) cap++;
-            else if(islower(c)) sm++;
+            if(std::isupper(c)) cap++;
+            else if(std::islower(c)) sm++;
         }
         int strength = cap - sm;
         if(strength > max_strength || (strength == max_strength && extension < strongest_extension)){
@@ -24,6 +24,6 @@ std::string Strongest_Extension(std::string class_name, std::vector<std::string>
 }
 
 int main() {
-    std::cout << Strongest_Extension("Sp", {"671235", "Bb"}) << std::endl;
+    assert(Strongest_Extension("Sp", {"671235", "Bb"}) == "Sp.671235");
     return 0;
 }
