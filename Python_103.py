1e5c16e6174380df2d5722dddd9b0a550710382c
beep boop <githubbot@vadim.me>
2024-07-06 03:32:51 +0200

    Solution 19 of Python/103, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/fw0szpvy Prompt: Program did not receive expected input

--- Python_103.py
+++ Python_103.py
@@ -1,10 +1,8 @@
 n = int(input("Enter the first number: "))
 m = int(input("Enter the second number: "))
 
-
 def rounded_avg(n, m):
     avg = (n + m) / 2
     return bin(int(round(avg)))[2:]
-
 
 print(rounded_avg(n, m))
