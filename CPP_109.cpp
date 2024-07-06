0626dbc25edf750d8d9874b78519f1c8fa5bbc54
beep boop <githubbot@vadim.me>
2024-07-06 05:15:34 +0200

    Solution 15 of CPP/109, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/yrwfogqa Prompt: in this scope     3 | bool move_one_ball(vector<int> arr) {       |                    ^~~~~~ 2e97ebba-25c7-4bbd-b9f7-109a64652c75.cpp:3:20: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 2e97ebba-25c7-4bbd-b9f7-109a64652c75.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 2e97ebba-25c7-4bbd-b9f7-109a64652c75.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 2e97ebba-25c7-4bbd-b9f7-109a64652c75.cpp:3:27: error: expected primary-expressio n before ‘int’     3 | bool move_one_ball(vector<int> arr) {       |                           ^~~ In file included from 2e97ebba-25c7-4bbd-b9f7-109a64652c75.cpp:14: 2e97ebba-25c7-4bbd-b9f7-109a64652c75.cpp: In function ‘int main()’: 2e97ebba-25c7-4bbd-b9f7-109a64652c75.cpp:16:26: error: ‘move_one_ball’ cannot be  used as a function    16 |     assert (move_one_ball({})==true);       |             ~~~~~~~~~~~~~^~~~

--- CPP_109.cpp
+++ CPP_109.cpp
@@ -6,10 +6,8 @@ bool move_one_ball(std::vector<int> arr) {
     int n = arr.size();
     if (n == 0) return true;
 
-    std::sort(arr.begin(), arr.end());
-    
     for (int i = 1; i < n; ++i) {
-        if (arr[i] < arr[0]) return false;
+        if (arr[i] < *arr.begin()) return false;
     }
 
     return true;
