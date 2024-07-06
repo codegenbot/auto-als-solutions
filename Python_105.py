4c26599cb205b60a0b16c3efdd964f00c96e722c
beep boop <githubbot@vadim.me>
2024-07-06 02:53:31 +0200

    Solution 13 of Python/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/ubalkc5i Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/df8df1d4-7176-4fb2-a3a7-16c22f6f1 fa0.py", line 15, in <module>     check(by_length)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/df8df1d4-7176-4fb2-a3a7-16c22f6f1 fa0.py", line 12, in check     assert by_length([9, 4, 8]) == ["Nine", "Eight", "Four"]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_105.py
+++ Python_105.py
@@ -1,18 +1,9 @@
+```
 def by_length(arr):
     result = sorted([i for i in arr if 1 <= i <= 9])
     return [
         name
-        for name in [
-            "One",
-            "Two",
-            "Three",
-            "Four",
-            "Five",
-            "Six",
-            "Seven",
-            "Eight",
-            "Nine",
-        ]
-        for num in result[::-1]
+        for name in ["One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"]
+        for num in result[::-1]  
         if str(num) == str(name)[0]
     ] or []
