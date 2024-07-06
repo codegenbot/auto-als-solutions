442873c5a9698844ed3fd3efa36fd82b2ac4442a
beep boop <githubbot@vadim.me>
2024-07-07 00:13:14 +0200

    Solution 40 of CPP/34, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/ye2pw82u Prompt: 575ecb22-6f1f-4efa-99c7-a78a9fdc555a.cpp:27:26: error: no matching function for call to ‘unique(<brace-enclosed initializer list>)’    27 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |                    ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 575ecb22-6f1f-4efa-99c7-a78a9fdc555a.cpp:9:4: note: candidate: ‘template<class I t> It unique(It, It)’     9 | It unique(It first, It last) {       |    ^~~~~~ 575ecb22-6f1f-4efa-99c7-a78a9fdc555a.cpp:9:4: note:   template argument deductio n/substitution failed: In file included from 575ecb22-6f1f-4efa-99c7-a78a9fdc555a.cpp:17: 575ecb22-6f1f-4efa-99c7-a78a9fdc555a.cpp:27:26: note:   candidate expects 2 argu ments, 1 provided    27 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |                    ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 575ecb22-6f1f-4efa-99c7-a78a9fdc555a.cpp:27:19: error: ‘issame’ cannot be used a s a function    27 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~

--- CPP_34.cpp
+++ CPP_34.cpp
@@ -3,6 +3,7 @@
 
 std::vector<int> unique(std::vector<int> l) {
     std::sort(l.begin(), l.end());
-    l.erase(std::unique(l.begin(), l.end()), l.end());
+    auto result_end = std::unique(l.begin(), l.end());
+    l.erase(result_end, l.end());
     return l;
 }
