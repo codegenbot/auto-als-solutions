369f1b48e29233c842b998645785c1b1f8b6b82e
beep boop <githubbot@vadim.me>
2024-07-07 00:05:36 +0200

    Solution 3 of CPP/31, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/30ik3o8e Prompt: Complete the following code given the task description and function signature.

--- CPP_31.cpp
+++ CPP_31.cpp
@@ -1,13 +1,21 @@
-#include <math.h>
-
-using namespace std;
+bool is_divisible(long long i) {
+    return i * i > n;
+}
 
 bool is_prime(long long n) {
     if (n <= 1)
         return false;
-    for (long long i = 2; i * i <= n; i++) {
+
+    if (n == 2 || n == 3)
+        return true;
+
+    if (n % 2 == 0)
+        return false;
+
+    for (long long i = 3; !is_divisible(i); i += 2) {
         if (n % i == 0)
             return false;
     }
+
     return true;
 }
