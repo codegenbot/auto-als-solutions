d81b3a898659e95bc6ba992031e7789b91be9596
beep boop <githubbot@vadim.me>
2024-07-06 11:54:53 +0200

    Solution 69 of CPP/145, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/pmi4gz2f Prompt:    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ c0d1149a-37ca-47d3-b517-c817583db8ea.cpp:26:34: error: expected primary-expressi on before ‘int’    26 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ c0d1149a-37ca-47d3-b517-c817583db8ea.cpp:26:39: error: expression list treated a s compound expression in initializer [-fpermissive]    26 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from c0d1149a-37ca-47d3-b517-c817583db8ea.cpp:25: c0d1149a-37ca-47d3-b517-c817583db8ea.cpp: In function ‘int main()’: c0d1149a-37ca-47d3-b517-c817583db8ea.cpp:35:20: error: ‘order_by_points’ was not  declared in this scope    35 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |                    ^~~~~~~~~~~~~~~ c0d1149a-37ca-47d3-b517-c817583db8ea.cpp:35:19: error: ‘issame’ cannot be used a s a function    35 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~

--- CPP_145.cpp
+++ CPP_145.cpp
@@ -1,9 +1,8 @@
 #include <vector>
 #include <algorithm>
-#include <set>
 
 std::vector<int> sortByDigitSum(std::vector<int> arr) {
-    sort(arr.begin(), arr.end(), [](int a, int b)->bool{
+    std::sort(arr.begin(), arr.end(), [](int a, int b)->bool{
         long long sum1 = 0, sum2 = 0;
         while(a > 0){
             sum1 += a % 10;
@@ -16,14 +15,4 @@ std::vector<int> sortByDigitSum(std::vector<int> arr) {
         return sum1 < sum2;
     });
     return arr;
-}
-
-int main() {
-    std::vector<int> arr = {0, 6, 6, -76, -21, 23, 4};
-    std::vector<int> sortedArr = sortByDigitSum(arr);
-    
-    for (int i : sortedArr) {
-        std::cout << i << " ";
-    }
-    return 0;
 }
