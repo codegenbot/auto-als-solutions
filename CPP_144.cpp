b422d2bd2fc950378de3f195a925824e7932d197
beep boop <githubbot@vadim.me>
2024-07-06 23:55:29 +0200

    Solution 15 of CPP/144, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/q7aqeoe1 Prompt: Complete the following code given the task description and function signature.

--- CPP_144.cpp
+++ CPP_144.cpp
@@ -1,34 +1,26 @@
 #include<string>
 using namespace std;
 
-bool simplify(string x,string n){
-    int numerator1 = 0, denominator1 = 0;
-    int numerator2 = 0, denominator2 = 0;
-
-    size_t pos1 = x.find('/');
-    size_t pos2 = n.find('/');
-
-    string num1 = x.substr(0, pos1);
-    string den1 = x.substr(pos1 + 1);
-    string num2 = n.substr(0, pos2);
-    string den2 = n.substr(pos2 + 1);
-
-    numerator1 = stoi(num1) * stoi(den2);
-    denominator1 = stoi(den1) * stoi(num2);
-
-    numerator2 = stoi(num1) * stoi(num2);
-    denominator2 = stoi(den1) * stoi(den2);
-
-    if (gcd(numerator1, denominator1) == 1)
-        return true;
-    else
-        return false;
-
+int gcd(int a,int b){
+    if(b==0)return a;
+    return gcd(b,a%b);
 }
 
-int gcd(int a, int b){
-    if (b == 0)
-        return a;
-    else
-        return gcd(b, a % b);
+bool simplify(string x,string n){
+    int numer1 = stoi(split(x)[0]);
+    int denom1 = stoi(split(x)[2]);
+
+    int numer2 = stoi(split(n)[0]);
+    int denom2 = stoi(split(n)[2]);
+
+    if(denom1*denom2 == 0) return false;
+
+    int greatest = gcd(gcd(numer1,denom1),gcd(numer2,denom2));
+
+    return (numer1/denom1)*(denom2/greatest) == (numer2/greatest);
+}
+
+string split(string s){
+    size_t found=s.find("/");
+    return s.substr(0(found));
 }
