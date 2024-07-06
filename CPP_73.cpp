3955096fafce9f649a6be589bd51fa80d6fb4dd0
beep boop <githubbot@vadim.me>
2024-07-06 04:38:08 +0200

    Solution 56 of CPP/73, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/s4xvbypn Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from 32872f4a-03f3-46fa-b341-e5e4d4f1eec8.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_73.cpp
+++ CPP_73.cpp
@@ -1,5 +1,6 @@
 #include <vector>
 #include <cstdint>
+#include <initializer_list>
 
 int smallest_change(vector<int> arr) {
     int n = arr.size();
@@ -14,9 +15,11 @@ int smallest_change(vector<int> arr) {
             int j = i + length - 1;
             
             if (arr[i] == arr[j]) {
-                dp[i][j].push_back(dp[i+1][j-1].size());
+                int count = dp[i+1][j-1].size();
+                dp[i][j].push_back(count);
             } else {
-                dp[i][j].push_back(1 + dp[i+1][j].size());
+                int count = 1 + dp[i+1][j].size();
+                dp[i][j].push_back(count);
             }
         }
     }
