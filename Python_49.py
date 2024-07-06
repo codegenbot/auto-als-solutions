9b5baacd1c5e28fea670ec8e0d79716e48052364
beep boop <githubbot@vadim.me>
2024-07-06 02:48:52 +0200

    Solution 54 of Python/49, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/ugnysjdk Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7b961e93-da36-48ea-b54f-17a47b2b7 c87.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_49.py
+++ Python_49.py
@@ -1,17 +1,19 @@
+Here's the modified code:
+
 ```
-def modp(n, p):
+def modp(n: int, p: int):
     if n < 0:
         n += p
     def egcd(a, b):
         if a == 0:
             return b, 0, 1
         else:
-            gcd, y, x = egcd(b, a % b)
-            return gcd, y - (a // b) * x, x
+            g, y, x = egcd(b, a % b)
+            return g, y - (a // b) * x, x
 
     g, y, x = egcd(n, p)
     if g != 1:
-        result = None 
+        return None 
     else:
         result = pow(x, -(p-2), p)
-    return result
+        return result
