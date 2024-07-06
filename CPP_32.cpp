671f510176e4535f78c040e9874abc2df8faff55
beep boop <githubbot@vadim.me>
2024-07-07 00:12:28 +0200

    Solution 98 of CPP/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/0ujhsbw6 Prompt:                  from /usr/include/x86_64-linux-gnu/bits/libc-header-start.h:33,                  from /usr/include/stdio.h:27,                  from aa5a9e3e-4275-4a2e-a2d3-8f5d707e99d5.cpp:1: /usr/include/x86_64-linux-gnu/bits/mathcalls.h:301:1: note: candidate: ‘double r ound(double)’   301 | __MATHCALLX (round,, (_Mdouble_ __x), (__const__));       | ^~~~~~~~~~~ /usr/include/x86_64-linux-gnu/bits/mathcalls.h:301:1: note:   candidate expects 1 argument, 2 provided In file included from /usr/include/c++/11/math.h:36,                  from aa5a9e3e-4275-4a2e-a2d3-8f5d707e99d5.cpp:2: /usr/include/c++/11/cmath:1760:3: note: candidate: ‘constexpr long double std::r ound(long double)’  1760 |   round(long double __x)       |   ^~~~~ /usr/include/c++/11/cmath:1760:3: note:   candidate expects 1 argument, 2 provid ed /usr/include/c++/11/cmath:1756:3: note: candidate: ‘constexpr float std::round(f loat)’  1756 |   round(float __x)       |   ^~~~~ /usr/include/c++/11/cmath:1756:3: note:   candidate expects 1 argument, 2 provid ed

--- CPP_32.cpp
+++ CPP_32.cpp
@@ -1,3 +1,7 @@
-double fractional_part = modf(x, &x);
-if(fractional_part > 0.005 || fractional_part < -0.005) x += fractional_part;
-return round(x*100.0)/100.0;
+double find_zero(vector<double> xs){
+    if(xs.size() % 2 != 0 || abs(xs[0]) < 1e-6) return -1; 
+    double x = (-xs[1]/xs[3]); 
+    double fractional_part = modf(x, &x);
+    if(fractional_part > 0.005 || fractional_part < -0.005) x += fractional_part;
+    return round(x*100.0)/100.0; 
+}
