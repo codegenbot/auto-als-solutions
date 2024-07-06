6fbf364c855a1346079dceb3da611c465de1a5a3
beep boop <githubbot@vadim.me>
2024-07-06 10:54:28 +0200

    Solution 41 of CPP/163, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/58xbmj1f Prompt: f ‘std::vector<int> generate_integers(int&, int&)’     6 | std::vector<int> generate_integers(int& a, int& b) {       |                                    ~~~~~^ 93c2f25f-67cf-4548-8464-b88ea6686964.cpp:21:5: error: ‘assert’ was not declared in this scope    21 |     assert(std::equal(generate_integers(17,89).begin(), generate_integer s(17,89).end(), {}));       |     ^~~~~~ 93c2f25f-67cf-4548-8464-b88ea6686964.cpp:3:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     2 | #include <algorithm>   +++ |+#include <cassert>     3 | In file included from 93c2f25f-67cf-4548-8464-b88ea6686964.cpp:25: 93c2f25f-67cf-4548-8464-b88ea6686964.cpp: In function ‘int main()’: 93c2f25f-67cf-4548-8464-b88ea6686964.cpp:35:38: error: cannot bind non-const lva lue reference of type ‘int&’ to an rvalue of type ‘int’    35 |     assert (issame(generate_integers(17,89) , {}));       |                                      ^~ 93c2f25f-67cf-4548-8464-b88ea6686964.cpp:6:41: note:   initializing argument 1 o f ‘std::vector<int> generate_integers(int&, int&)’     6 | std::vector<int> generate_integers(int& a, int& b) {       |                                    ~~~~~^

--- CPP_163.cpp
+++ CPP_163.cpp
@@ -19,6 +19,6 @@ bool issame(const vector<int>& a, const vector<int>&b){
 }
 
 int main_test() { 
-    assert(issame({}, generate_integers(17,89)));
+    assert(issame({}, generate_integers(17,89)) );
     return 0;
 }
