32afb0d0ea0a05c0212eb99d5d32e2d3044d1720
beep boop <githubbot@vadim.me>
2024-07-06 19:46:01 +0200

    Solution 3 of CPP/56, ratio 0.9166666666666666 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/9uog2x34 Prompt: Complete the following code given the task description and function signature.

--- CPP_56.cpp
+++ CPP_56.cpp
@@ -1,13 +1,16 @@
+#include <string>
+using namespace std;
+
 bool correct_bracketing(string brackets){
-    int open = 0;
+    int count = 0;
     for(int i=0; i<brackets.length(); i++){
         if(brackets[i] == '<'){
-            open++;
+            count++;
         }
         else if(brackets[i] == '>'){
-            if(open <= 0) return false;
-            open--;
+            if(count > 0)count--;
+            else return false;
         }
     }
-    return open == 0;
+    return count == 0;
 }
