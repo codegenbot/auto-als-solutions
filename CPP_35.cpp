34514361606d591aed08a5a5a526d6be01cc0853
beep boop <githubbot@vadim.me>
2024-07-07 00:39:25 +0200

    Solution 68 of CPP/35, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/dqp6iafu Prompt: 79777919-f2f8-4298-9a71-913b69401643.cpp:1:1: error: expected unqualified-id bef ore ‘return’     1 | return *max_element(l.begin(), l.end());       | ^~~~~~ In file included from 79777919-f2f8-4298-9a71-913b69401643.cpp:3: 79777919-f2f8-4298-9a71-913b69401643.cpp: In function ‘int main()’: 79777919-f2f8-4298-9a71-913b69401643.cpp:5:17: error: ‘max_element’ was not decl ared in this scope     5 |     assert (abs(max_element({5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10})- 12 4)<1e-4);       |                 ^~~~~~~~~~~ 79777919-f2f8-4298-9a71-913b69401643.cpp:5:13: error: ‘abs’ was not declared in this scope     5 |     assert (abs(max_element({5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10})- 12 4)<1e-4);       |             ^~~

--- CPP_35.cpp
+++ CPP_35.cpp
@@ -1,11 +1,15 @@
+```cpp
 #include <algorithm>
 #include <cmath>
 
-int maximumAbsoluteDifference(vector<int> l) {
-    int res = 0;
-    std::vector<int> temp(l);
-    std::sort(temp.begin(), temp.end());
-    if (temp.size() > 1)
-        res = std::abs(temp[temp.size()-1] - temp[0]);
-    return res;
+int maximumSumSubarray(int k, int n) {
+    if (k > n) 
+        return 0;
+    std::vector<int> l;
+    for (int i = 1; i <= k; ++i) {
+        int x;
+        std::cin >> x;
+        l.push_back(x);
+    }
+    return *std::max_element(l.begin(), l.end());
 }
