cfaa711c82fd94c3a01c7fd36507d7c518b472cc
beep boop <githubbot@vadim.me>
2024-07-07 00:01:03 +0200

    Solution 16 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/sllk2064 Prompt:    22 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ bf02fedd-6816-4262-b860-f0f6f6dfd020.cpp:22:44: error: ‘b’ was not declared in t his scope    22 | bool issame(vector<string> a,vector<string>b){       |                                            ^ bf02fedd-6816-4262-b860-f0f6f6dfd020.cpp:22:45: error: expression list treated a s compound expression in initializer [-fpermissive]    22 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from bf02fedd-6816-4262-b860-f0f6f6dfd020.cpp:21: bf02fedd-6816-4262-b860-f0f6f6dfd020.cpp: In function ‘int main()’: bf02fedd-6816-4262-b860-f0f6f6dfd020.cpp:31:20: error: ‘sorted_list_sum’ was not  declared in this scope    31 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ bf02fedd-6816-4262-b860-f0f6f6dfd020.cpp:31:19: error: ‘issame’ cannot be used a s a function    31 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -1,20 +1,17 @@
-#include <algorithm>
-#include <vector>
-#include <string>
-
 vector<string> sorted_list_sum(vector<string> lst) {
-    // Remove strings with odd lengths from the vector
-    lst.erase(remove_if(lst.begin(), lst.end(),
-        [](const string& str) { return str.length() % 2; }), lst.end());
-    
-    // Sort the remaining strings by length and then alphabetically
-    std::stable_sort(lst.begin(), lst.end(),
-        [](const string& str1, const string& str2) {
-            if (str1.length() == str2.length())
-                return str1 < str2;
-            else
-                return str1.length() < str2.length();
-        });
-    
-    return lst;
+    vector<string> result;
+    for (const auto& str : lst) {
+        if (str.length() % 2 == 0) {
+            result.push_back(str);
+        }
+    }
+    sort(result.begin(), result.end(), 
+         [](const string& a, const string& b) {
+             if (a.length() != b.length()) {
+                 return a.length() < b.length();
+             } else {
+                 return a < b;
+             }
+         });
+    return result;
 }
