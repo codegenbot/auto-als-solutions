6aff6d0fdbd474cc841de972090d7baf12abdec1
beep boop <githubbot@vadim.me>
2024-07-07 01:41:08 +0200

    Solution 17 of CPP/68, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/mz730308 Prompt:       |                    ^~~ 44e78875-5d37-46c2-a0f9-7a8a6e252dd9.cpp:19:27: error: ‘vector’ was not declared  in this scope    19 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 44e78875-5d37-46c2-a0f9-7a8a6e252dd9.cpp:19:34: error: expected primary-expressi on before ‘int’    19 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 44e78875-5d37-46c2-a0f9-7a8a6e252dd9.cpp:19:39: error: expression list treated a s compound expression in initializer [-fpermissive]    19 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 44e78875-5d37-46c2-a0f9-7a8a6e252dd9.cpp:18: 44e78875-5d37-46c2-a0f9-7a8a6e252dd9.cpp: In function ‘int main()’: 44e78875-5d37-46c2-a0f9-7a8a6e252dd9.cpp:28:20: error: ‘pluck’ was not declared in this scope    28 |     assert (issame(pluck({7, 9, 7, 1}) , {}));       |                    ^~~~~ 44e78875-5d37-46c2-a0f9-7a8a6e252dd9.cpp:28:19: error: ‘issame’ cannot be used a s a function    28 |     assert (issame(pluck({7, 9, 7, 1}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_68.cpp
+++ CPP_68.cpp
@@ -2,14 +2,16 @@
 #include <pair>
 
 bool issame(vector<int> a,vector<int>b){
-    if(a.size()!=b.size())return false;
+    if(a.size()!=b.size())
+        return false;
     for(int i=0;i<a.size();i++){
-        if(a[i]!=b[i])return false;
+        if(a[i]!=b[i])
+            return false;
     }
     return true;
 }
 
-vector<vector<int>> pluck(vector<int> arr) {
+vector<int> pluck(vector<int> arr) {
     vector<pair<int, int>> evenNodes;
     for (int i = 0; i < arr.size(); i++) {
         if (arr[i] % 2 == 0) {
@@ -23,12 +25,16 @@ vector<vector<int>> pluck(vector<int> arr) {
     
     sort(evenNodes.begin(), evenNodes.end());
     
-    vector<vector<int>> result;
+    vector<int> result;
     for(int i=0;i<arr.size();i++){
-        if(issame({evenNodes[0].first}, {arr[i]})){
-            result.push_back({evenNodes[0].first});
-            break;
+        bool found = false;
+        for(auto &node : evenNodes){
+            if(i==node.second){
+                found=true;break;
+            }
         }
+        if(!found)
+            result.push_back(arr[i]);
     }
     
     return result;
