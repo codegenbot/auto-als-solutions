39a8d8ab50443cd7b096da40d8c8f5fcb19df9a9
beep boop <githubbot@vadim.me>
2024-07-06 02:09:42 +0200

    Solution 22 of CPP/63, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/gzcd2ovm Prompt: 25f2af79-46da-4efe-bf8d-34b488982986.cpp: In function ‘int main()’: 25f2af79-46da-4efe-bf8d-34b488982986.cpp:25:5: error: ‘printf’ was not declared in this scope    25 |     printf("Enter the value of n: ");       |     ^~~~~~ 25f2af79-46da-4efe-bf8d-34b488982986.cpp:3:1: note: ‘printf’ is defined in heade r ‘<cstdio>’; did you forget to ‘#include <cstdio>’?     2 | #include <stdio.h>   +++ |+#include <cstdio>     3 | 25f2af79-46da-4efe-bf8d-34b488982986.cpp:26:5: error: ‘scanf’ was not declared i n this scope    26 |     scanf("%d", &n);       |     ^~~~~ 25f2af79-46da-4efe-bf8d-34b488982986.cpp: At global scope: 25f2af79-46da-4efe-bf8d-34b488982986.cpp:32:5: error: redefinition of ‘int main( )’    32 | int main(){       |     ^~~~ 25f2af79-46da-4efe-bf8d-34b488982986.cpp:23:5: note: ‘int main()’ previously def ined here    23 | int main() {       |     ^~~~

--- CPP_63.cpp
+++ CPP_63.cpp
@@ -1,5 +1,5 @@
-```c++
-#include <stdio.h>
+```
+#include <iostream>
 
 using namespace std;
 
@@ -14,7 +14,7 @@ int fibfib(int n) {
             int temp = a + b + c;
             a = b;
             b = c;
-            c = temp;
+            c = temp % 2 == 0 ? c : c - ((c/2)*(c/2));
         }
         return c;
     }
@@ -22,8 +22,8 @@ int fibfib(int n) {
 
 int main() {
     int n;
-    printf("Enter the value of n: ");
-    scanf("%d", &n);
-    printf("FibFib number at position %d is %d\n", n, fibfib(n));
+    cout << "Enter the value of n: ";
+    cin >> n;
+    cout << "FibFib number at position " << n << " is " << fibfib(n) << endl;
     return 0;
 }
