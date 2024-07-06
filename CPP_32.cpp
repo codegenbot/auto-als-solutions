3c0d595e775a0cffc3183df09b837a77dcc5db49
beep boop <githubbot@vadim.me>
2024-07-07 00:07:50 +0200

    Solution 77 of CPP/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/0ujhsbw6 Prompt: a223e73a-76d2-48f2-9b73-a8b3ae0b4dfd.cpp:20:13: error: ‘coeffs’ was not declared  in this scope; did you mean ‘coeff’?    20 |             coeffs.push_back(coeff);       |             ^~~~~~       |             coeff a223e73a-76d2-48f2-9b73-a8b3ae0b4dfd.cpp:23:30: error: ‘coeffs’ was not declared  in this scope; did you mean ‘ncoeff’?    23 |         solution = find_zero(coeffs);       |                              ^~~~~~       |                              ncoeff a223e73a-76d2-48f2-9b73-a8b3ae0b4dfd.cpp:23:36: error: ‘find_zero’ cannot be use d as a function    23 |         solution = find_zero(coeffs);       |                                    ^ In file included from a223e73a-76d2-48f2-9b73-a8b3ae0b4dfd.cpp:7: a223e73a-76d2-48f2-9b73-a8b3ae0b4dfd.cpp:24:21: error: ‘poly’ was not declared i n this scope    24 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                     ^~~~ a223e73a-76d2-48f2-9b73-a8b3ae0b4dfd.cpp:24:17: error: ‘abs’ was not declared in  this scope    24 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                 ^~~

--- CPP_32.cpp
+++ CPP_32.cpp
@@ -1,4 +1,3 @@
-```cpp
 #include <vector>
 #include <cmath>
 
@@ -11,11 +10,15 @@ double poly(const std::vector<double>& coeffs, double x) {
 }
 
 double find_zero(std::vector<double> coeffs, double solution){
-    double left = poly(coeffs, solution);
-    double right = poly(coeffs, -solution);
-    if (std::abs(left) < std::abs(right)) {
-        return -solution;
-    } else {
-        return solution;
+    double left = 1.0;
+    double right = 10000.0;
+    
+    while (abs(left - right) > 0.00001) {
+        double mid = (left + right) / 2;
+        if (poly(coeffs, mid) * poly(coeffs, solution) < 0)
+            right = mid;
+        else
+            left = mid;
     }
+    return (-solution / poly(coeffs, solution));
 }
