fe8c82d1a3fc8c1c6a35c1f7c333d7fa7273f2fa
beep boop <githubbot@vadim.me>
2024-07-06 10:26:00 +0200

    Solution 28 of CPP/163, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/58xbmj1f Prompt:       |             ^~~~~~ edb2e2ef-dcf5-4231-baca-3d3582657278.cpp:24:20: error: expected primary-expressi on before ‘int’    24 | bool issame(vector<int> a,vector<int>b){       |                    ^~~ edb2e2ef-dcf5-4231-baca-3d3582657278.cpp:24:27: error: ‘vector’ was not declared  in this scope    24 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ edb2e2ef-dcf5-4231-baca-3d3582657278.cpp:24:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from edb2e2ef-dcf5-4231-baca-3d3582657278.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from edb2e2ef-dcf5-4231-baca-3d3582657278.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ edb2e2ef-dcf5-4231-baca-3d3582657278.cpp:24:34: error: expected primary-expressi on before ‘int’    24 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~

--- CPP_163.cpp
+++ CPP_163.cpp
@@ -1,11 +1,9 @@
 #include <vector>
 #include <algorithm>
 
-using namespace std;
-
-std::vector<int> generate_integers(int& a, int& b) {
+std::vector<int> generate_integers(int a, int b) {
     std::vector<int> result;
-    for (int i = min(a, b); i <= max(a, b); i++) {
+    for (int i = std::min(a, b); i <= std::max(a, b); i++) {
         if (i % 2 == 0) {
             result.push_back(i);
         }
@@ -13,7 +11,7 @@ std::vector<int> generate_integers(int& a, int& b) {
     return result;
 }
 
-bool issame(const vector<int>& a, const vector<int>&b){
+bool issame(const std::vector<int>& a, const std::vector<int>&b){
     return (a==b);
 }
 
