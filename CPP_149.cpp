39a85f40bc36e7799c345ad6d08e608f18db872b
beep boop <githubbot@vadim.me>
2024-07-06 10:24:54 +0200

    Solution 33 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/tenvpvw5 Prompt: r> >)’ previously defined here     8 | bool issame(vector<string> a, vector<string> b) {       |      ^~~~~~ aee347d9-3dcb-4893-a991-8caea36add18.cpp:52:5: error: redefinition of ‘int std:: main()’    52 | int main(){       |     ^~~~ aee347d9-3dcb-4893-a991-8caea36add18.cpp:38:5: note: ‘int std::main()’ previousl y defined here    38 | int main() {       |     ^~~~ aee347d9-3dcb-4893-a991-8caea36add18.cpp:54:2: error: expected ‘}’ at end of inp ut    54 | }       |  ^ aee347d9-3dcb-4893-a991-8caea36add18.cpp:5:15: note: to match this ‘{’     5 | namespace std {       |               ^ aee347d9-3dcb-4893-a991-8caea36add18.cpp: In function ‘int std::main()’: aee347d9-3dcb-4893-a991-8caea36add18.cpp:54:1: warning: control reaches end of n on-void function [-Wreturn-type]    54 | }       | ^

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -37,6 +37,6 @@ vector<string> sorted_list_sum(vector<string> lst) {
 }
 
 int main() {
-    vector<string> lst = {"aaaa", "bbbb", "dd", "cc"};
-    assert(issame(sorted_list_sum(lst), {"cc", "dd", "aaaa", "bbbb"}));
+    assert(issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}), {"cc", "dd", "aaaa", "bbbb"}));
     return 0;
+}
