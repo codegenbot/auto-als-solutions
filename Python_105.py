604dd7e297e7f2d1dd176e6101544e45f62e1db7
beep boop <githubbot@vadim.me>
2024-07-06 03:41:53 +0200

    Solution 68 of Python/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/ubalkc5i Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/cf30c7e4-451b-4a91-ad8c-2f6143d87 335.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_105.py
+++ Python_105.py
@@ -1,3 +1,13 @@
 def by_length(arr):
-    mapping = {1: "One", 2: "Two", 3: "Three", 4: "Four", 5: "Five", 6: "Six", 7: "Seven", 8: "Eight", 9: "Nine"}
-    return [i for i in arr if len(mapping[i]) >= 4]
+    words = {
+        1: "One",
+        2: "To",
+        3: "Three",
+        4: "Four",
+        5: "Five",
+        6: "Six",
+        7: "Seven",
+        8: "Eight",
+        9: "Nine",
+    }
+    return [words[i] for i in arr if len(words.get(i, "")) >= 4]
