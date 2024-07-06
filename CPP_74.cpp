470867e6309eefa0abd41984e93cad288179eda0
beep boop <githubbot@vadim.me>
2024-07-06 05:10:25 +0200

    Solution 55 of CPP/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/k9lvoh5f Prompt: a17109d9-28da-4277-b71d-0bcd09fac58b.cpp:61:37: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from a17109d9-28da-4277-b71d-0bcd09fac58b.cpp:3: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from a17109d9-28da-4277-b71d-0bcd09fac58b.cpp:3: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ a17109d9-28da-4277-b71d-0bcd09fac58b.cpp:61:44: error: ‘b’ was not declared in t his scope    61 | bool issame(vector<string> a,vector<string>b){       |                                            ^ In file included from a17109d9-28da-4277-b71d-0bcd09fac58b.cpp:60: a17109d9-28da-4277-b71d-0bcd09fac58b.cpp: In function ‘int main()’: a17109d9-28da-4277-b71d-0bcd09fac58b.cpp:70:19: error: too many arguments to fun ction ‘bool issame(std::vector<std::__cxx11::basic_string<char> >)’    70 |     assert (issame(total_match({"this"}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ a17109d9-28da-4277-b71d-0bcd09fac58b.cpp:5:6: note: declared here     5 | bool issame(std::vector<std::string> a) {       |      ^~~~~~

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -1,9 +1,15 @@
+```cpp
 #include <vector>
 #include <algorithm>
 #include <string>
 
-bool issame(std::vector<std::string> a) {
-    return a.size() == 0;
+bool issame(std::vector<std::string> a, std::vector<std::string> b) {
+    for (size_t i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
 }
 
 std::vector<std::string> total_match(std::vector<std::string> lst1, std::vector<std::string> lst2) {
