7da1affd603435e9b2d297de9d799635d6bf8cf8
beep boop <githubbot@vadim.me>
2024-07-07 00:07:06 +0200

    Solution 73 of CPP/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/0ujhsbw6 Prompt: a223e73a-76d2-48f2-9b73-a8b3ae0b4dfd.cpp:20:13: error: ‘coeffs’ was not declared  in this scope; did you mean ‘coeff’?    20 |             coeffs.push_back(coeff);       |             ^~~~~~       |             coeff a223e73a-76d2-48f2-9b73-a8b3ae0b4dfd.cpp:23:30: error: ‘coeffs’ was not declared  in this scope; did you mean ‘ncoeff’?    23 |         solution = find_zero(coeffs);       |                              ^~~~~~       |                              ncoeff a223e73a-76d2-48f2-9b73-a8b3ae0b4dfd.cpp:23:36: error: ‘find_zero’ cannot be use d as a function    23 |         solution = find_zero(coeffs);       |                                    ^ In file included from a223e73a-76d2-48f2-9b73-a8b3ae0b4dfd.cpp:7: a223e73a-76d2-48f2-9b73-a8b3ae0b4dfd.cpp:24:21: error: ‘poly’ was not declared i n this scope    24 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                     ^~~~ a223e73a-76d2-48f2-9b73-a8b3ae0b4dfd.cpp:24:17: error: ‘abs’ was not declared in  this scope    24 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                 ^~~

--- CPP_32.cpp
+++ CPP_32.cpp
@@ -1,23 +1,23 @@
 #include <vector>
 #include <cmath>
 
-double poly(const std::vector<double>& coeffs, double x) {
+double poly(std::vector<double> coeffs, double x) {
     double result = 0;
-    for (int i = 0; i < coeffs.size(); ++i) {
+    for (int i = 0; i < coeffs.size(); i++) {
         result += coeffs[i] * pow(x, i);
     }
     return result;
 }
 
 double find_zero(std::vector<double> coeffs, double solution){
-    double left = 1e9;
-    double right = -1e9;
-    for (int i = 0; i < coeffs.size(); ++i) {
+    double left = 1e10;
+    double right = -1e10;
+    for (int i = 0; i < coeffs.size(); i++) {
         if (coeffs[i] > 0) {
-            right = min(right, pow(solution, i));
-        } else {
-            left = max(left, pow(solution, i));
+            left = min(left, (-coeffs[0]/coeffs[i]));
+        } else if (coeffs[i] < 0) {
+            right = max(right, (-coeffs[0]/coeffs[i]));
         }
     }
-    return (-left + right) / (2 * abs(left - right));
+    return (-left + right)/2;
 }
