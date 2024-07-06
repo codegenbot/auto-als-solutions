93dbbacd19299829a2524461d62c3a84616de1b8
beep boop <githubbot@vadim.me>
2024-07-07 01:57:12 +0200

    Solution 32 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/bx78xaav Prompt: In file included from de0c1629-7739-43a9-9da0-5981ba4aa24b.cpp:24: de0c1629-7739-43a9-9da0-5981ba4aa24b.cpp: In function ‘int main()’: de0c1629-7739-43a9-9da0-5981ba4aa24b.cpp:34:20: error: ‘strange_sort_list’ was n ot declared in this scope; did you mean ‘strange_sort_vector’?    34 |     assert (issame(strange_sort_list({111111}) , {111111}));       |                    ^~~~~~~~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -3,17 +3,20 @@
 
 using namespace std;
 
-bool issame(vector<int> v1, vector<int> v2) {
-    if(v1.size() != v2.size()) return false;
-    for(int i = 0; i < v1.size(); i++) {
-        if(v1[i] != v2[i]) return false;
+bool issame(vector<int> a, vector<int> b) {
+    if (a.size() != b.size()) 
+        return false;
+    
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i])
+            return false;
     }
+    
     return true;
 }
 
-bool isequal(vector<int> v1, vector<int> v2) {
-    if(v1.size() != v2.size()) return false;
-    sort(v1.begin(), v1.end());
-    sort(v2.begin(), v2.end());
-    return v1 == v2;
+bool isequal(vector<int> a, vector<int> b) {
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
+    return a == b;
 }
