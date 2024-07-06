677a90b3281e5db4cf9629438aedf1b06f8d4b76
beep boop <githubbot@vadim.me>
2024-07-06 23:19:41 +0200

    Solution 6 of CPP/19, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/rrmr6mrq Prompt: Complete the following code given the task description and function signature.

--- CPP_19.cpp
+++ CPP_19.cpp
@@ -1,30 +1,41 @@
 #include <algorithm>
+using namespace std;
 
 string sort_numbers(string numbers) {
     map<string, int> numMap = {{"zero", 0}, {"one", 1}, {"two", 2}, {"three", 3},
                                 {"four", 4}, {"five", 5}, {"six", 6}, {"seven", 7},
                                 {"eight", 8}, {"nine", 9}};
-    vector<string> numVec;
-    string temp;
+    vector<string> nums;
+    string num;
 
-    for (int i = 0; i < numbers.length(); i++) {
-        if (numbers[i] == ' ') {
-            continue;
-        }
-        temp += numbers[i];
-        if (i == numbers.length() - 1 || numbers[i + 1] != ' ') {
-            numVec.push_back(temp);
-            temp.clear();
+    for (string s : split(numbers, " ")) {
+        if (numMap.count(s)) {
+            nums.push_back(s);
         }
     }
 
-    sort(numVec.begin(), numVec.end(), [&numMap](const string& a, const string& b) {
-        return to_string(numMap.at(a)) < to_string(numMap.at(b));
-    });
+    sort(nums.begin(), nums.end());
+    return join(nums, " ");
+}
 
+vector<string> split(const string& str, char c) {
+    vector<string> tokens;
+    size_t pos = 0;
+    while ((pos = str.find(c)) != string::npos) {
+        tokens.push_back(str.substr(0, pos));
+        str.erase(0, pos + 1);
+    }
+    tokens.push_back(str);
+    return tokens;
+}
+
+string join(vector<string> nums, char c) {
     string result;
-    for (const auto& str : numVec) {
-        result += str + " ";
+    for (int i = 0; i < nums.size(); i++) {
+        if (i > 0) {
+            result += c;
+        }
+        result += nums[i];
     }
-    return result.substr(0, result.length() - 1);
+    return result;
 }
