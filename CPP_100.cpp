b51539da30c83eba9992a49088ca677c40bc2e79
beep boop <githubbot@vadim.me>
2024-07-06 05:33:45 +0200

    Solution 97 of CPP/100, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/b41o8swj Prompt:       |             ^~~~~~ a023e528-8550-4ba2-aafd-7e0854a38cba.cpp:21:34: error: expected primary-expressi on before ‘int’    21 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ a023e528-8550-4ba2-aafd-7e0854a38cba.cpp:21:39: error: expression list treated a s compound expression in initializer [-fpermissive]    21 | bool issame(vector<int> a,vector<int>b){       |                                       ^ a023e528-8550-4ba2-aafd-7e0854a38cba.cpp:29:5: error: redefinition of ‘int main( )’    29 | int main(){       |     ^~~~ a023e528-8550-4ba2-aafd-7e0854a38cba.cpp:16:5: note: ‘int main()’ previously def ined here    16 | int main() {       |     ^~~~ In file included from a023e528-8550-4ba2-aafd-7e0854a38cba.cpp:20: a023e528-8550-4ba2-aafd-7e0854a38cba.cpp: In function ‘int main()’: a023e528-8550-4ba2-aafd-7e0854a38cba.cpp:30:19: error: ‘issame’ cannot be used a s a function    30 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_100.cpp
+++ CPP_100.cpp
@@ -11,5 +11,5 @@ std::vector<int> make_a_pile(int n) {
     std::vector<int> pile;
     for (int i = 1; i <= n; i++)
         pile.push_back(i % 2 ? i * 2 - 1 : i * 2);
-    return pile; 
+    return pile;
 }
