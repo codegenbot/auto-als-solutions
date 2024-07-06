35fa923fc1b99b74532f8f9af208c9c069a8157c
beep boop <githubbot@vadim.me>
2024-07-06 19:12:13 +0200

    Solution 63 of CPP/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/uft3iac8 Prompt:    11 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ d8769c29-9093-4a7d-b192-bfaa4497f458.cpp:11:44: error: ‘b’ was not declared in t his scope    11 | bool issame(vector<string> a,vector<string>b){       |                                            ^ d8769c29-9093-4a7d-b192-bfaa4497f458.cpp:11:45: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from d8769c29-9093-4a7d-b192-bfaa4497f458.cpp:10: d8769c29-9093-4a7d-b192-bfaa4497f458.cpp: In function ‘int main()’: d8769c29-9093-4a7d-b192-bfaa4497f458.cpp:20:20: error: ‘filter_by_prefix’ was no t declared in this scope    20 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |                    ^~~~~~~~~~~~~~~~ d8769c29-9093-4a7d-b192-bfaa4497f458.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_29.cpp
+++ CPP_29.cpp
@@ -1,9 +1,20 @@
-bool issame(string a, string b) {
-    vector<string> filter(vector<string> strings, string prefix) {
-        vector<string> result;
-        for (string s : strings) {
-            if (s.find(prefix) == 0)
-                result.push_back(s);
-        }
-        return result;
+```
+bool issame(string a, string b){
+    bool result = true;
+    if(a.size() != b.size())
+        result = false;
+    else
+        for(int i=0; i<a.size(); i++)
+            if(a[i] != b[i])
+                result = false;
+    return result;
+}
+
+vector<string> filter_by_prefix(vector<string> strings, string prefix){
+    vector<string> result;
+    for(string s : strings){
+        if(s.find(prefix) == 0)
+            result.push_back(s);
     }
+    return result;
+}
