50ade919157bbe0a865a902fed1ddda75199df32
beep boop <githubbot@vadim.me>
2024-07-06 07:58:22 +0200

    Solution 2 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/ecknuzmn Prompt: 5a2f91d4-2c1c-4fb9-8bc1-7eef0bb6c1ca.cpp:3:10: fatal error: boost/any.hpp: No su ch file or directory     3 | #include <boost/any.hpp>       |          ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,37 +1,16 @@
+#include <iostream>
+#include <string>
 #include <boost/any.hpp>
 
+using namespace std;
+
 boost::any compare_one(boost::any a, boost::any b) {
-    if (a.type() == typeid(int) && b.type() == typeid(float)) {
+    if (any_cast<double>(a) > any_cast<double>(b))
+        return a;
+    else if (any_cast<string>(a) > any_cast<string>(b))
+        return a;
+    else if (any_cast<string>(b) > any_cast<string>(a))
         return b;
-    }
-    else if (a.type() == typeid(int) && b.type() == typeid(double)) {
-        return b;
-    }
-    else if (a.type() == typeid(float) && b.type() == typeid(double)) {
-        return b;
-    }
-    else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-        string str1 = a.convert<string>();
-        string str2 = b.convert<string>();
-        if (stod(str1) > stod(str2))
-            return a;
-        else if (stod(str1) < stod(str2))
-            return b;
-        else
-            return boost::any("None");
-    }
-    else if ((a.type() == typeid(int) && b.type() == typeid(string)) ||
-             (a.type() == typeid(float) && b.type() == typeid(string))) {
-        string str = a.convert<string>();
-        double num2 = stod(b.convert<string>());
-        if (stod(str) > num2)
-            return a;
-        else if (stod(str) < num2)
-            return b;
-        else
-            return boost::any("None");
-    }
-    else {
+    else
         return boost::any("None");
-    }
 }
