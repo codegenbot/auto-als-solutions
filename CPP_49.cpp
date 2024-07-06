a8279c93b291c0ef7bc78f7cbd4214479b5a47d5
beep boop <githubbot@vadim.me>
2024-07-07 01:09:17 +0200

    Solution 7 of CPP/49, ratio 0.7142857142857143 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/dxgt1y9t Prompt: Complete the following code given the task description and function signature.

--- CPP_49.cpp
+++ CPP_49.cpp
@@ -1,12 +1,14 @@
 int modp(int n, int p) {
-    if (n < 0) {
-        n = ((1 << 31) - 1) - n;
-        return modp(n, p);
-    }
+    if (n < 0) return modp(-n, p);
     long long res = 1;
-    while (n > 0) {
-        if (n & 1)
-            res = (long long)res * p % p;
+    for (int i = 0; i < __builtin_clz(n); i++) {
+        if (!(n & 1)) {
+            p = (long long)p * p % p;
+        }
+        n >>= 1;
+    }
+    while (n) {
+        if (n & 1) res = (long long)res * p % p;
         n >>= 1;
         p = (long long)p * p % p;
     }
