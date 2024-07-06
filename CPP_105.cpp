c08a595a8c399c2fbb21c29e1813a4c35e64dc41
beep boop <githubbot@vadim.me>
2024-07-06 05:44:05 +0200

    Solution 27 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/t3hkjq0a Prompt:       |                                     ^~~~~~ 4152b559-0be1-4080-bbb4-93d8e22bafa2.cpp:67:37: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from 4152b559-0be1-4080-bbb4-93d8e22bafa2.cpp:3: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 4152b559-0be1-4080-bbb4-93d8e22bafa2.cpp:3: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 4152b559-0be1-4080-bbb4-93d8e22bafa2.cpp:67:44: error: ‘b’ was not declared in t his scope    67 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 4152b559-0be1-4080-bbb4-93d8e22bafa2.cpp:75:5: error: redefinition of ‘int main( )’    75 | int main(){       |     ^~~~ 4152b559-0be1-4080-bbb4-93d8e22bafa2.cpp:61:5: note: ‘int main()’ previously def ined here    61 | int main() {       |     ^~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -16,7 +16,6 @@ std::vector<std::string> by_length(std::vector<int> arr) {
     for (int i : arr) {
         if (i >= 1 && i <= 9) {
             temp.push_back({(i == 1)? 1 : ((i == 2)? 2 : ((i == 3)? 3 : ((i == 4)? 4 : ((i == 5)? 5 : ((i == 6)? 6 : ((i == 7)? 7 : ((i == 8)? 8 : 9)))))))))), i});
-        }
     }
 
     std::sort(temp.begin(), temp.end());
