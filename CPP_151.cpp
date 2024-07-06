ba3c28e3d1730af96672e5d811ef0ddebb05e933
beep boop <githubbot@vadim.me>
2024-07-06 10:47:48 +0200

    Solution 36 of CPP/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/2iqw0vuf Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from 6c6bbf99-95d0-4150-aa6e-0ed62467395c.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_151.cpp
+++ CPP_151.cpp
@@ -1,11 +1,13 @@
+```cpp
 #include <vector>
 #include <cmath>
 #include <cassert>
+#include <initializer_list>
 using namespace std;
 
-long long double_the_difference(vector<double> lst) {
+long long double_the_difference(vector<float> lst) {
     long long sum = 0;
-    for (double num : lst) {
+    for (float num : lst) {
         if (num > 0 && floor(num) == num) {
             sum += pow(num, 2);
         }
@@ -14,7 +16,7 @@ long long double_the_difference(vector<double> lst) {
 }
 
 int main() {
-    vector<double> lst = {1.5, 2.5, -3.4, 4.7};
+    vector<float> lst = {1.5f, 2.5f, -3.4f, 4.7f};
     long long odd_sum = 0; 
     assert(double_the_difference(lst) == odd_sum); 
     return 0;
