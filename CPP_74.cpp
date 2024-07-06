694d0b6d8e5cddd1ba119c74f86a6b7468ce31f7
beep boop <githubbot@vadim.me>
2024-07-06 04:51:29 +0200

    Solution 50 of CPP/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/k9lvoh5f Prompt: 24ae0932-f2a0-4991-a6b8-87b717c1099b.cpp:61:37: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from 24ae0932-f2a0-4991-a6b8-87b717c1099b.cpp:3: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 24ae0932-f2a0-4991-a6b8-87b717c1099b.cpp:3: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 24ae0932-f2a0-4991-a6b8-87b717c1099b.cpp:61:44: error: ‘b’ was not declared in t his scope    61 | bool issame(vector<string> a,vector<string>b){       |                                            ^ In file included from 24ae0932-f2a0-4991-a6b8-87b717c1099b.cpp:60: 24ae0932-f2a0-4991-a6b8-87b717c1099b.cpp: In function ‘int main()’: 24ae0932-f2a0-4991-a6b8-87b717c1099b.cpp:70:19: error: too many arguments to fun ction ‘bool issame(std::vector<std::__cxx11::basic_string<char> >)’    70 |     assert (issame(total_match({"this"}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 24ae0932-f2a0-4991-a6b8-87b717c1099b.cpp:5:6: note: declared here     5 | bool issame(std::vector<std::string> a) {       |      ^~~~~~

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -1,12 +1,68 @@
-```cpp
 #include <vector>
 #include <algorithm>
 #include <string>
 
-bool issame(std::vector<std::string> a, std::vector<std::string> b, std::vector<std::string> c) {
-    return (a.size() == b.size()) && (a.size() == c.size());
+bool issame(std::vector<std::string> a, std::vector<std::string> b) {
+    if (a.size() != b.size()) {
+        return false;
+    }
+    
+    for (size_t i = 0; i < a.size(); ++i) {
+        if (a[i].compare(b[i]) != 0) {
+            return false;
+        }
+    }
+
+    return true;
 }
 
-int main() {
-    assert(issame({{"this"}}, {""}, {""}) == true);
+std::vector<std::string> total_match(std::vector<std::string> lst1, std::vector<std::string> lst2) {
+    int sum1 = 0;
+    for (const auto& str : lst1) {
+        sum1 += str.length();
+    }
+    
+    int sum2 = 0;
+    for (const auto& str : lst2) {
+        sum2 += str.length();
+    }
+
+    if (lst1 == lst2) {
+        return lst1;
+    } else if (sum1 < sum2) {
+        return lst1;
+    } else if (sum1 > sum2) {
+        return lst2;
+    }
+    
+    int sum1lst = 0, sum2lst = 0;
+    for (const auto& str : lst1) {
+        sum1lst += str.length();
+        for (char c : str) {
+            sum1lst += 1;
+        }
+    }
+
+    for (const auto& str : lst2) {
+        sum2lst += str.length();
+        for (char c : str) {
+            sum2lst += 1;
+        }
+    }
+
+    if (sum1lst < sum2lst) {
+        return lst1;
+    } else if (sum1lst > sum2lst) {
+        return lst2;
+    } else {
+        for (const auto& str : lst1) {
+            for (const auto& sub_str : lst2) {
+                if (str.find(sub_str) != std::string::npos || sub_str.find(str) != std::string::npos) {
+                    return lst1;
+                }
+            }
+        }
+
+        return lst1.size() < lst2.size() ? lst1 : lst2;
+    }
 }
