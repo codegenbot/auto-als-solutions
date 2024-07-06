d5ed6396250bc69793489469396eabdab281c766
beep boop <githubbot@vadim.me>
2024-07-06 12:26:30 +0200

    Solution 15 of Python/67, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/yjo5uju2 Prompt: Complete the following code given the task description and function signature.

--- Python_67.py
+++ Python_67.py
@@ -1,6 +1,10 @@
-def fruit_distribution(s,n):
+def fruit_distribution(s, n):
     words = s.split()
     for i in range(len(words)):
-        if words[i].endswith('apples') or words[i].endswith('oranges'):
-            n -= int(words[i-1]) + 1
-    return n
+        if words[i].lower() == "apples" or words[i].lower() == "oranges":
+            if words[i - 1].replace(".", "").isdigit():
+                return (
+                    n
+                    - int(words[i - 1])
+                    - int("".join(filter(str.isdigit, words[i + 2 :])))
+                )
