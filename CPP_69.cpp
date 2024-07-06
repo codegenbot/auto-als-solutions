06172603fd728868b1180e7e082e10fcfcab9880
beep boop <githubbot@vadim.me>
2024-07-06 12:04:09 +0200

    Solution 58 of CPP/69, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/qp49zkht Prompt: deduction/substitution failed: In file included from bf663dc9-4e1d-4f8b-a95c-99822dbfe69e.cpp:20: bf663dc9-4e1d-4f8b-a95c-99822dbfe69e.cpp:22:19: note:   candidate expects 6 argu ments, 1 provided    22 |     assert (search({3, 10, 10, 9, 2}) == -1);       |             ~~~~~~^~~~~~~~~~~~~~~~~~~ In file included from /usr/include/c++/11/algorithm:74,                  from bf663dc9-4e1d-4f8b-a95c-99822dbfe69e.cpp:1: /usr/include/c++/11/pstl/glue_algorithm_defs.h:117:1: note: candidate: ‘template <class _ExecutionPolicy, class _ForwardIterator1, class _ForwardIterator2> __pst l::__internal::__enable_if_execution_policy<_ExecutionPolicy, _ForwardIterator> std::search(_ExecutionPolicy&&, _ForwardIterator1, _ForwardIterator1, _ForwardIt erator2, _ForwardIterator2)’   117 | search(_ExecutionPolicy&& __exec, _ForwardIterator1 __first, _ForwardIte rator1 __last, _ForwardIterator2 __s_first,       | ^~~~~~ /usr/include/c++/11/pstl/glue_algorithm_defs.h:117:1: note:   template argument deduction/substitution failed: In file included from bf663dc9-4e1d-4f8b-a95c-99822dbfe69e.cpp:20: bf663dc9-4e1d-4f8b-a95c-99822dbfe69e.cpp:22:19: note:   candidate expects 5 argu ments, 1 provided    22 |     assert (search({3, 10, 10, 9, 2}) == -1);       |             ~~~~~~^~~~~~~~~~~~~~~~~~~

--- CPP_69.cpp
+++ CPP_69.cpp
@@ -3,7 +3,16 @@
 using namespace std;
 
 int main() {
-    vector<int> vec = {3, 10, 10, 9, 2};
-    assert(find(vec.begin(), vec.end(), 10) != vec.end()); 
+    vector<int> vec;
+    int n;
+    cout << "Enter the number of elements: ";
+    cin >> n;
+    vec.resize(n);
+    
+    cout << "Enter the elements: ";
+    for(int i = 0; i < n; i++)
+        cin >> vec[i];
+    
+    assert(std::find(vec.begin(), vec.end(), vec[0]) == vec.end());
     return 0;
 }
