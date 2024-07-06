3252e0e66382786ddefe7e6686788ee459f55083
beep boop <githubbot@vadim.me>
2024-07-06 02:15:07 +0200

    Solution 26 of Python/49, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/ugnysjdk Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f1433249-ea32-4d21-be9b-4f0489f44 1be.py", line 9, in <module>     check(modp)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f1433249-ea32-4d21-be9b-4f0489f44 1be.py", line 6, in check     assert modp(31, 5) == 3            ^^^^^^^^^^^^^^^^ AssertionError

--- Python_49.py
+++ Python_49.py
@@ -1,5 +1,16 @@
+```
 def modp(n: int, p: int):
-    """Calculate n^p-1 (mod p)"""
     if n < 0:
         n += p
-    return pow(n, p - 1, p)
+    def egcd(a, b):
+        if a == 0:
+            return b, 0, 1
+        else:
+            gcd, y, x = egcd(b, a % b)
+            return gcd, y - (a // b) * x, x
+
+    g, y, x = egcd(n, p)
+    if g != 1:
+        return None 
+    else:
+        return pow(x, p-2, p)
