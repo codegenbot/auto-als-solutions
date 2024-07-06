5744297840ca530fcd9527aeb941fe0741ec5495
beep boop <githubbot@vadim.me>
2024-07-07 01:57:13 +0200

    Solution 44 of CPP/68, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/mz730308 Prompt:       |                    ^~~ 35624182-dd4f-49aa-842e-5906ca36d449.cpp:18:27: error: ‘vector’ was not declared  in this scope    18 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 35624182-dd4f-49aa-842e-5906ca36d449.cpp:18:34: error: expected primary-expressi on before ‘int’    18 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 35624182-dd4f-49aa-842e-5906ca36d449.cpp:18:39: error: expression list treated a s compound expression in initializer [-fpermissive]    18 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 35624182-dd4f-49aa-842e-5906ca36d449.cpp:17: 35624182-dd4f-49aa-842e-5906ca36d449.cpp: In function ‘int main()’: 35624182-dd4f-49aa-842e-5906ca36d449.cpp:27:20: error: ‘pluck’ was not declared in this scope    27 |     assert (issame(pluck({7, 9, 7, 1}) , {}));       |                    ^~~~~ 35624182-dd4f-49aa-842e-5906ca36d449.cpp:27:19: error: ‘issame’ cannot be used a s a function    27 |     assert (issame(pluck({7, 9, 7, 1}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_68.cpp
+++ CPP_68.cpp
@@ -1,4 +1,19 @@
 #include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool issame(vector<int> a, vector<int> b) {
+    if (a.size() != b.size()) {
+        return false;
+    }
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
+}
 
 vector<int> pluck(vector<int> arr) {
     vector<int> result = {};
@@ -12,4 +27,17 @@ vector<int> pluck(vector<int> arr) {
     }
 
     return result;
+}
+
+int main() {
+    vector<int> a = {1, 2, 3, 4, 5};
+    vector<int> b = {1, 2, 3, 4, 5};
+    
+    if (issame(a, b)) {
+        cout << "Vectors are same." << endl;
+    } else {
+        cout << "Vectors are not same." << endl;
+    }
+    
+    return 0;
 }
