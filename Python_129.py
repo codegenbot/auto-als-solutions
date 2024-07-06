2f5c8d6911e30fa5fa48b70d0598afce3da89c1a
beep boop <githubbot@vadim.me>
2024-07-06 16:22:52 +0200

    Solution 76 of Python/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/j3m9w7h7 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/69c7fde3-af47-4843-bc79-e9c7f67db d60.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_129.py
+++ Python_129.py
@@ -8,7 +8,7 @@ def minPath(grid, k):
     def dfs(i, j, path):
         nonlocal res
         if len(path) == k:
-            res = sorted(set(path))
+            res = sorted(path)
             return True
         visited[i][j] = True
         for di, dj in directions:
