1d9c8542cbb2b362bdd9af19f567f4bbc7f34221
beep boop <githubbot@vadim.me>
2024-07-06 03:24:42 +0200

    Solution 71 of Python/28, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/o6qupeag Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/49eb5f7f-2ce1-408e-9d00-dbc795850 211.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_28.py
+++ Python_28.py
@@ -7,7 +7,7 @@ while True:
             if inp.lower() == 'stop':
                 break
             for s in inp.split():
-                if not set(s).issubset({'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' '}):
+                if not set(s).issubset({'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'}):
                     raise ValueError
             strings += [s.strip() for s in inp.split()]
         break
