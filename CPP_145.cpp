a14d6b327db62771371c58dc275580654a9a4797
beep boop <githubbot@vadim.me>
2024-07-06 10:50:27 +0200

    Solution 48 of CPP/145, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/pmi4gz2f Prompt:   759 |                                      __memcpyable<_BI2, _BI1>::__value,       |                                      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~   760 |                                      _Category>::__copy_move_b(__first,       |                                      ~~~~~~~~~~~~~~~~~~~~~~~~~^~~~~~~~~   761 |                                                                __last,       |                                                                ~~~~~~~   762 |                                                                __result) ;       |                                                                ~~~~~~~~~ In file included from /usr/include/c++/11/bits/stl_algobase.h:71,                  from /usr/include/c++/11/algorithm:61,                  from 84d718a5-b947-4382-83d2-cf2057134b47.cpp:1: /usr/include/c++/11/bits/predefined_ops.h: At global scope: /usr/include/c++/11/bits/predefined_ops.h:187:7: warning: ‘__gnu_cxx::__ops::_It er_comp_val<_Compare>::_Iter_comp_val(__gnu_cxx::__ops::_Iter_comp_iter<_Compare >&&) [with _Compare = sortByPoints(const std::vector<int>&)::<lambda(int, int)>] ’ used but never defined   187 |       _Iter_comp_val(_Iter_comp_iter<_Compare>&& __comp)       |       ^~~~~~~~~~~~~~

--- CPP_145.cpp
+++ CPP_145.cpp
@@ -1,22 +1,19 @@
 #include <algorithm>
 #include <vector>
-#include <initializer_list>
 
 bool same(std::vector<int> a, std::vector<int> b) {
     return (a.size() == b.size()) && (std::equal(a.begin(), a.end(), b.begin()));
 }
 
-auto comp = [](int a, int b) {
-    int sumA = 0, sumB = 0;
-    for (char c : std::to_string(a)) sumA += c - '0';
-    for (char c : std::to_string(b)) sumB += c - '0';
-    if (sumA == sumB)
-        return true;  
-    return sumA < sumB;
-};
-
 std::vector<int> sortByPoints(const std::vector<int>& nums) {
-    std::sort(nums.begin(), nums.end(), comp);
+    std::sort(nums.begin(), nums.end(), [&nums](int a, int b) {
+        int sumA = 0, sumB = 0;
+        for (char c : std::to_string(a)) sumA += c - '0';
+        for (char c : std::to_string(b)) sumB += c - '0';
+        if (sumA == sumB)
+            return nums.find(a) > nums.find(b);
+        return sumA < sumB;
+    });
     return nums;
 }
 
