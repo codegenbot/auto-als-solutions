05a044ce5f32954f33346dbd8425b6a34a866432
beep boop <githubbot@vadim.me>
2024-07-07 00:52:14 +0200

    Solution 20 of CPP/39, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/l6ltyzsi Prompt: Program failed

--- CPP_39.cpp
+++ CPP_39.cpp
@@ -1,8 +1,8 @@
 int prime_fib(int n) {
     int a = 0, b = 1, fib = 0;
     for (int i = 0; i < n; i++) {
-        if (i == n - 1)
-            return b;
+        if (i == n - 2)
+            return a;
         fib = a + b;
         a = b;
         b = fib;
