775ccc5806916578ea90552e3a902b86a986ceb3
beep boop <githubbot@vadim.me>
2024-07-06 14:08:58 +0200

    Solution 80 of Python/140, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/ewgln41s Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0cfcb1ac-bb98-4150-9d32-9660e57ed 312.py", line 16, in <module>     check(fix_spaces)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0cfcb1ac-bb98-4150-9d32-9660e57ed 312.py", line 12, in check     fix_spaces("   Exa 1 2 2 mple") == "-Exa_1_2_2_mple" AssertionError: This prints if this assert fails 4 (good for debugging!)

--- Python_140.py
+++ Python_140.py
@@ -1,11 +1,8 @@
 ```
 def fix_spaces(text):
-    new_text = ''
-    for char in text:
-        if char == ' ' and new_text[-1] == ' ':
-            new_text += '-'
-        elif char != ' ':
-            new_text += char
-        else:
-            new_text += '_'
-    return new_text
+    text = text.replace(" ", "_")
+    while "--" in text or "---" in text or "----" in text:
+        text = text.replace("--", "-")
+        text = text.replace("---", "-")
+        text = text.replace("----", "-")
+    return text.strip().replace("-", "-_").lower()
