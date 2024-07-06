799dcc4a0ad466071579ffa96d98ac0f5c69a9a4
beep boop <githubbot@vadim.me>
2024-07-06 02:46:39 +0200

    Solution 23 of Python/84, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/ui55ly4s Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/eed6aa86-7473-4bdc-bace-6bcbd15c1 e19.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_84.py
+++ Python_84.py
@@ -1,6 +1,6 @@
 def solve(N):
-    N += 1
-    count = 0
-    while (1 << count) <= N:
-        count += 1
-    return str(bin(N))[2:].zfill(count)
+    result = []
+    while N:
+        result.append(str(N % 2))
+        N //= 2
+    return "".join(result[::-1])
