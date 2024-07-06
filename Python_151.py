b6aec0434c498f97ed732de78cc4686bca8224a0
beep boop <githubbot@vadim.me>
2024-07-06 16:17:19 +0200

    Solution 54 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/metyua9h Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8f7ee744-0da5-4c5c-84c8-8c053d98a b0a.py", line 11, in <module>     check(double_the_difference)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8f7ee744-0da5-4c5c-84c8-8c053d98a b0a.py", line 7, in check     double_the_difference(lst) == odd_sum                           ^^^ NameError: name 'lst' is not defined. Did you mean: 'list'?

--- Python_151.py
+++ Python_151.py
@@ -1,10 +1,13 @@
 def double_the_difference(lst):
     odd_sum = sum(i for i in lst if isinstance(i, int) and i >= 0 and i % 2 != 0)
-    return sum(i**2 for i in lst if isinstance(i, int) and i >= 0 and i % 2 != 0) / (2 * odd_sum)
+    return sum(i**2 for i in lst if isinstance(i, int) and i >= 0 and i % 2 != 0) / (
+        2 * odd_sum
+    )
+
 
 def check():
     lst = [1, -2, 3, 4, 5]
-    return_value = double_the_difference(lst)
-    assert round(return_value, 2) == 4.0
+    assert round(double_the_difference(lst), 2) == 4.0
+
 
 check()
