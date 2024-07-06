82c57f854a2947fa91acbbeacaed415e82df8b18
beep boop <githubbot@vadim.me>
2024-07-06 02:25:30 +0200

    Solution 24 of CPP/20, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/8j2pv2eq Prompt: In file included from /usr/include/c++/11/functional:62,                  from /usr/include/c++/11/pstl/glue_algorithm_defs.h:13,                  from /usr/include/c++/11/algorithm:74,                  from fafc81b3-bfa9-4b5e-b5e8-a326e911dbb5.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ fafc81b3-bfa9-4b5e-b5e8-a326e911dbb5.cpp:26:36: error: expected primary-expressi on before ‘float’    26 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ fafc81b3-bfa9-4b5e-b5e8-a326e911dbb5.cpp:26:43: error: expression list treated a s compound expression in initializer [-fpermissive]    26 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from fafc81b3-bfa9-4b5e-b5e8-a326e911dbb5.cpp:25: fafc81b3-bfa9-4b5e-b5e8-a326e911dbb5.cpp: In function ‘int main()’: fafc81b3-bfa9-4b5e-b5e8-a326e911dbb5.cpp:35:19: error: ‘issame’ cannot be used a s a function    35 |     assert (issame(find_closest_elements({1.1, 2.2, 3.1, 4.1, 5.1}) , {2 .2, 3.1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~

--- CPP_20.cpp
+++ CPP_20.cpp
@@ -2,6 +2,15 @@
 #include <vector>
 #include <numeric>
 
+bool issame(vector<float> a,vector<float> b){
+    for(int i = 0; i<a.size(); i++){
+        if(a[i] != b[i]){
+            return false;
+        }
+    }
+    return true;
+}
+
 std::vector<std::pair<float, float>> find_closest_elements(std::vector<float> numbers) {
     std::vector<std::pair<float, float>> closest_pairs;
     float min_diff = numeric_limits<float>::max();
@@ -20,19 +29,4 @@ std::vector<std::pair<float, float>> find_closest_elements(std::vector<float> nu
     }
 
     return closest_pairs;
-}
-
-bool same(std::vector<float> a, std::vector<float> b) {
-    if (a.size() != b.size())
-        return false;
-    for (int i = 0; i < a.size(); i++) {
-        if (std::abs(a[i] - b[i]) > std::numeric_limits<float>::epsilon())
-            return false;
-    }
-    return true;
-}
-
-int main() {
-    assert(same(find_closest_elements({1.1, 2.2, 3.1, 4.1, 5.1}), {2.2, 3.1}));
-    // ... rest of your code
 }
