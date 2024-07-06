0b272aba27ed98cfd48944ed9db769f440a572a0
beep boop <githubbot@vadim.me>
2024-07-07 01:48:54 +0200

    Solution 83 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/8ma2o623 Prompt: 3a172f57-4cb0-419c-b2b9-d5fe78bb7659.cpp:4:1: error: ‘string’ does not name a ty pe; did you mean ‘stdin’?     4 | string string_to_md5(string text) {       | ^~~~~~       | stdin In file included from 3a172f57-4cb0-419c-b2b9-d5fe78bb7659.cpp:16: 3a172f57-4cb0-419c-b2b9-d5fe78bb7659.cpp: In function ‘int main()’: 3a172f57-4cb0-419c-b2b9-d5fe78bb7659.cpp:18:13: error: ‘string_to_md5’ was not d eclared in this scope; did you mean ‘string_to_hex’?    18 |     assert (string_to_md5("password") == "5f4dcc3b5aa765d61d8327deb882cf 99");       |             ^~~~~~~~~~~~~

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -3,7 +3,6 @@
 #include <openssl/x509v3.h>
 #include <openssl/evp.h>
 #include <string>
-#include <sstream>
 
 std::string string_to_md5(std::string text) {
     if (text.empty()) return "";
@@ -12,7 +11,7 @@ std::string string_to_md5(std::string text) {
     MD5((unsigned const char*)text.c_str(), text.size(), result);
     std::stringstream ss;
     for (int i = 0; i < 16; i++) {
-        ss << hex << setfill('0') << setw(2) << (int)result[i];
+        ss << std::hex << std::setfill('0') << std::setw(2) << (int)result[i];
     }
     return ss.str();
 }
