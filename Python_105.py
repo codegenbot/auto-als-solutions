2ce73fa961aa220f197610353c2a7662d1292640
beep boop <githubbot@vadim.me>
2024-07-06 02:47:01 +0200

    Solution 2 of Python/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/ubalkc5i Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7f17f2bd-2d66-4875-b539-d5b100d4a cc9.py", line 25, in <module>     check(by_length)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7f17f2bd-2d66-4875-b539-d5b100d4a cc9.py", line 22, in check     assert by_length([9, 4, 8]) == ["Nine", "Eight", "Four"]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_105.py
+++ Python_105.py
@@ -1,6 +1,8 @@
 def by_length(arr):
-    result = sorted([str(i) for i in arr if 1 <= i <= 9])
-    return [names[int(i) - 1] for i in reversed(result)]
-
-
-names = ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"]
+    result = sorted([i for i in arr if 1 <= i <= 9])
+    return [
+        name
+        for name in ["One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"]
+        for num in result
+        if str(num) == str(name)[0]
+    ] or []
