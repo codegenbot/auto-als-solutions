733bb7605f934a6cb890395f83a0305f785ff46f
beep boop <githubbot@vadim.me>
2024-07-06 08:05:31 +0200

    Solution 8 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/ecknuzmn Prompt: e03c7ad2-8803-4f7f-9885-f01e60119e8d.cpp:1:10: fatal error: boost/any.hpp: No su ch file or directory     1 | #include <boost/any.hpp>       |          ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,83 +1,41 @@
-#include <iostream>
+```cpp
+#include <boost/any.hpp>
 #include <string>
-#include <sstream>
-#include <vector>
 
 using namespace std;
 
-class Any {
-public:
-    enum Type { INT, FLOAT, DOUBLE, STRING };
-
-    template<typename T>
-    void insert(T value) {
-        if (value instanceof int) {
-            data.push_back(to_string(value));
-        }
-        else if (value instanceof float || value instanceof double) {
-            data.push_back(to_string(value));
-        }
-        else if (value instanceof string) {
-            data.push_back(string(value));
-        }
-    }
-
-    Type get_type() const {
-        return type;
-    }
-
-    vector<string> convert() const {
-        return data;
-    }
-
-private:
-    vector<string> data;
-    Type type;
-};
-
-Any compare_one(Any a, Any b) {
-    if (a.get_type() == Any::INT && b.get_type() == Any::FLOAT) {
+boost::any compare_one(boost::any a, boost::any b) {
+    if (a.type() == typeid(int) && b.type() == typeid(float)) {
         return b;
     }
-    else if (a.get_type() == Any::INT && b.get_type() == Any::DOUBLE) {
+    else if (a.type() == typeid(int) && b.type() == typeid(double)) {
         return b;
     }
-    else if (a.get_type() == Any::FLOAT && b.get_type() == Any::DOUBLE) {
+    else if (a.type() == typeid(float) && b.type() == typeid(double)) {
         return b;
     }
-    else if (a.get_type() == Any::STRING && b.get_type() == Any::STRING) {
-        vector<string> v1 = a.convert();
-        vector<string> v2 = b.convert();
-        double num1, num2;
-        for (const auto& s : v1) {
-            num1 = stod(s);
-        }
-        for (const auto& s : v2) {
-            num2 = stod(s);
-        }
-        if (num1 > num2)
+    else if (a.type() == typeid(string) && b.type() == typeid(string)) {
+        string str1 = a.convert<string>();
+        string str2 = b.convert<string>();
+        if (stod(str1) > stod(str2))
             return a;
-        else if (num1 < num2)
+        else if (stod(str1) < stod(str2))
             return b;
         else
-            return Any();
+            return boost::any("None");
     }
-    else if ((a.get_type() == Any::INT && b.get_type() == Any::STRING) ||
-             (a.get_type() == Any::FLOAT && b.get_type() == Any::STRING)) {
-        vector<string> v = a.convert();
-        double num2;
-        for (const auto& s : v) {
-            num2 = stod(s);
-        }
-        string str = b.convert()[0];
+    else if ((a.type() == typeid(int) && b.type() == typeid(string)) ||
+             (a.type() == typeid(float) && b.type() == typeid(string))) {
+        string str = a.convert<string>();
+        double num2 = stod(b.convert<string>());
         if (stod(str) > num2)
             return a;
         else if (stod(str) < num2)
             return b;
         else
-            return Any();
+            return boost::any("None");
     }
     else {
-        return Any();
+        return boost::any("None");
     }
 }
