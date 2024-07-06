be7d504d4531a8256ac1bf40b3c4da7b923eae50
beep boop <githubbot@vadim.me>
2024-07-06 02:59:36 +0200

    Solution 26 of Python/53, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/ju0ki0uh Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c9e0f837-3f16-4c8f-b21e-634d82253 aca.py", line 1     ```Python     ^ SyntaxError: invalid syntax

--- Python_53.py
+++ Python_53.py
@@ -1,12 +1,11 @@
-```
 def add(x: int, y: int) -> int:
-    while True:
+    got_valid_input = False
+
+    while not got_valid_input:
         try:
             x = int(input("Enter the first number: "))
             y = int(input("Enter the second number: "))
-            if input("Do you want to add another pair? (yes/no): ").lower() != 'no':
-                return x + y
-            else:
-                break
+            got_valid_input = True
+            return x + y
         except ValueError:
             print("Invalid input. Please enter a valid integer.")
