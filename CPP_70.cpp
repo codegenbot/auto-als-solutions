6a0e204c4533ed9b766d07b488c89a1224f36a27
beep boop <githubbot@vadim.me>
2024-07-06 05:24:05 +0200

    Solution 58 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/xmlozhdh Prompt: 28de18ff-0236-4600-861b-b053bb5bc360.cpp:35:5: note: ‘int main()’ previously def ined here    35 | int main() {       |     ^~~~ In file included from 28de18ff-0236-4600-861b-b053bb5bc360.cpp:42: 28de18ff-0236-4600-861b-b053bb5bc360.cpp: In function ‘int main()’: 28de18ff-0236-4600-861b-b053bb5bc360.cpp:52:37: error: cannot bind non-const lva lue reference of type ‘std::vector<int>&’ to an rvalue of type ‘std::vector<int> ’    52 |     assert (issame(strange_sort_list({111111}) , {111111}));       |                    ~~~~~~~~~~~~~~~~~^~~~~~~~~~ In file included from /usr/include/c++/11/vector:67,                  from 28de18ff-0236-4600-861b-b053bb5bc360.cpp:2: /usr/include/c++/11/bits/stl_vector.h:625:7: note:   after user-defined conversi on: ‘std::vector<_Tp, _Alloc>::vector(std::initializer_list<_Tp>, const allocato r_type&) [with _Tp = int; _Alloc = std::allocator<int>; std::vector<_Tp, _Alloc> ::allocator_type = std::allocator<int>]’   625 |       vector(initializer_list<value_type> __l,       |       ^~~~~~ 28de18ff-0236-4600-861b-b053bb5bc360.cpp:9:42: note:   initializing argument 1 o f ‘void strange_sort_list(std::vector<int>&)’     9 | void strange_sort_list(std::vector<int>& lst) {       |                        ~~~~~~~~~~~~~~~~~~^~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -33,15 +33,9 @@ void strange_sort_list(std::vector<int>& lst) {
 }
 
 int main() {
-    std::vector<int> input;
-    std::cout << "Enter elements of the vector separated by space: ";
-    for (int i = 0; i < 5; ++i) {
-        int num;
-        std::cin >> num;
-        input.push_back(num);
-    }
-
-    strange_sort_list(input);
+    std::vector<int> input = {1, 1, 1, 1, 1};
     
+    std::vector<int> inputCopy = input;
+    strange_sort_list(inputCopy);
     return 0;
 }
