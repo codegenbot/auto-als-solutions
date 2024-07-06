2f37dc7028c6397dce1f8b1d786f3291cf655e32
beep boop <githubbot@vadim.me>
2024-07-06 10:20:13 +0200

    Solution 24 of CPP/159, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/jmlpd852 Prompt: a0ba0e0d-1532-4893-81b7-989c8eae7ca1.cpp:61:5: error: redefinition of ‘int main( )’    61 | int main(){       |     ^~~~ a0ba0e0d-1532-4893-81b7-989c8eae7ca1.cpp:22:5: note: ‘int main()’ previously def ined here    22 | int main() {       |     ^~~~

--- CPP_159.cpp
+++ CPP_159.cpp
@@ -21,7 +21,7 @@ vector<int> eat(int number, int need, int remaining) {
 
 int main() {
     vector<int> output;
-
+    
     output = eat(5, 6, 10);
     for (int i : output) {
         cout << i << " ";
@@ -35,12 +35,6 @@ int main() {
     cout << endl;
     
     output = eat(1, 10, 10);
-    for (int i : output) {
-        cout << i << " ";
-    }
-    cout << endl;
-    
-    output = eat(2, 11, 5);
     for (int i : output) {
         cout << i << " ";
     }
