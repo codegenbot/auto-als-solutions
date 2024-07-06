d21f325195c84f740d7b61d1a7de5806a241d995
beep boop <githubbot@vadim.me>
2024-07-06 18:37:39 +0200

    Solution 6 of CPP/19, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/nabjymve Prompt: Complete the following code given the task description and function signature.

--- CPP_19.cpp
+++ CPP_19.cpp
@@ -1,38 +1,41 @@
-#include<stdio.h>
 #include<string>
 #include<map>
 using namespace std;
 
-string sort_numbers(string numbers){
-    map<string,int> num_map;
-    num_map["zero"] = 0;
-    num_map["one"] = 1;
-    num_map["two"] = 2;
-    num_map["three"] = 3;
-    num_map["four"] = 4;
-    num_map["five"] = 5;
-    num_map["six"] = 6;
-    num_map["seven"] = 7;
-    num_map["eight"] = 8;
-    num_map["nine"] = 9;
+string sort_numbers(string numbers) {
+    map<string, int> numMap = {{"zero", 0}, {"one", 1}, {"two", 2}, {"three", 3},
+                                {"four", 4}, {"five", 5}, {"six", 6}, {"seven", 7},
+                                {"eight", 8}, {"nine", 9}};
 
-    vector<string> vec;
-    string temp;
-    for(int i=0; i < numbers.length(); i++){
-        if(numbers[i] != ' ')
-            temp += numbers[i];
-        else{
-            vec.push_back(temp);
-            temp = "";
+    vector<string> numVec;
+    string numStr;
+
+    for (int i = 0; i < numbers.length(); i++) {
+        while (i < numbers.length() && !isdigit(numbers[i])) {
+            i++;
         }
+        if (i >= numbers.length()) break;
+        int j = i + 1;
+        while (j < numbers.length() && isdigit(numbers[j])) {
+            j++;
+        }
+        numStr = numbers.substr(i, j - i);
+        numVec.push_back(numMap[numStr]);
     }
-    vec.push_back(temp);
 
-    sort(vec.begin(),vec.end());
+    sort(numVec.begin(), numVec.end());
+    string result;
 
-    string result = "";
-    for(int i=0; i<vec.size(); i++){
-        result += vec[i] + " ";
+    for (int i = 0; i < numVec.size(); i++) {
+        for (auto it = numMap.begin(); it != numMap.end(); ++it) {
+            if (it->second == numVec[i]) {
+                result += it->first;
+                break;
+            }
+        }
+        if (i < numVec.size() - 1) {
+            result += " ";
+        }
     }
 
     return result;
