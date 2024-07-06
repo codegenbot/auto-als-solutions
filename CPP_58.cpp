b48fe20cd97c3bf528d8e0782d2de09aa22dbb2d
beep boop <githubbot@vadim.me>
2024-07-06 02:54:15 +0200

    Solution 40 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/ivx00olu Prompt: /usr/include/c++/11/ext/new_allocator.h: In instantiation of ‘_Tp* __gnu_cxx::ne w_allocator<_Tp>::allocate(int, const void*) [with _Tp = int]’: /usr/include/c++/11/bits/alloc_traits.h:464:28:   required from ‘static _Tp* std ::allocator_traits<std::allocator<_CharT> >::allocate(std::allocator_traits<std: :allocator<_CharT> >::allocator_type&, int) [with _Tp = int; std::allocator_trai ts<std::allocator<_CharT> >::pointer = int*; std::allocator_traits<std::allocato r<_CharT> >::allocator_type = std::allocator<int>]’ /usr/include/c++/11/bits/stl_vector.h:346:33:   required from ‘std::_Vector_base <_Tp, _Alloc>::pointer std::_Vector_base<_Tp, _Alloc>::_M_allocate(size_t) [with  _Tp = int; _Alloc = std::allocator<int>; std::_Vector_base<_Tp, _Alloc>::pointe r = int*; size_t = long unsigned int]’ /usr/include/c++/11/bits/vector.tcc:440:44:   required from ‘void std::vector<_T p, _Alloc>::_M_realloc_insert(std::vector<_Tp, _Alloc>::iterator, _Args&& ...) [ with _Args = {const int&}; _Tp = int; _Alloc = std::allocator<int>; std::vector< _Tp, _Alloc>::iterator = std::vector<int>::iterator]’ /usr/include/c++/11/bits/stl_vector.h:1198:21:   required from ‘void std::vector <_Tp, _Alloc>::push_back(const value_type&) [with _Tp = int; _Alloc = std::alloc ator<int>; std::vector<_Tp, _Alloc>::value_type = int]’ 25a086b3-ebd2-4ec6-84d1-eb664a5fa8c3.cpp:11:66:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<int>’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <cctype>
 
 using namespace std;
 
@@ -15,12 +16,43 @@ vector<int> common(vector<int> a, vector<int> b){
 bool issame(vector<int> a, vector<int> b) {
     if (a.size() != b.size())
         return false;
-    for (int i = 0; i < a.size();i++)
+    for (int i = 0; i < a.size(); i++)
         if (find(b.begin(), b.end(), a[i]) == b.end())
             return false;
     return true;
 }
 
 int main(){
-    assert(issame({4, 3, 2, 8}, common({4, 3, 2, 8}, {1, 7})));
+    vector<int> a, b;
+    int n1,n2;
+
+    cout << "Enter size of first array: ";
+    cin >> n1;
+    for(int i=0; i<n1;i++){
+        cout << "Enter element: ";
+        cin >> a[i];
+    }
+
+    cout << "Enter size of second array: ";
+    cin >> n2;
+    for(int i=0; i<n2;i++){
+        cout << "Enter element: ";
+        cin >> b[i];
+    }
+    
+    if(issame(a, b))
+        cout << "Both arrays are same." << endl;
+    else{
+        vector<int> res = common(a,b);
+        if(res.size() > 0)
+            cout << "Common elements in the two arrays are: ";
+        for(int i=0; i<res.size();i++){
+            cout << res[i] << " ";
+        }
+        cout << endl;
+        else
+            cout << "No common elements." << endl;
+    }
+
+    return 0;
 }
