295985bd0e957092747f5e65a2d178369b4d1def
beep boop <githubbot@vadim.me>
2024-07-06 11:55:48 +0200

    Solution 47 of Python/36, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/pxzlgmmy Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d8c82f62-c8c1-490f-977c-73bdebaef d9b.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_36.py
+++ Python_36.py
@@ -1,6 +1,20 @@
 def fizz_buzz(n: int):
-    count = 0
+    backticks = ""
     for i in range(1, n + 1):
-        if "7" in str(i):
-            count += 1
-    return count
+        if i % 11 == 0 and i % 13 == 0:
+            backticks += "Backtick"
+        elif i % 11 == 0:
+            backticks += "Quote"
+        elif i % 13 == 0:
+            backticks += "Tick"
+        else:
+            backticks += str(i)
+        if i % 3 == 0:
+            backticks += "Buzz"
+        elif i % 5 == 0:
+            backticks += "Fizz"
+        elif i % 7 == 0:
+            backticks += "Backtick"
+        else:
+            backticks += str(i) + " "
+    return backticks
