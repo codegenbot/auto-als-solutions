946066cfe918f6ec6486b391289b4157457943ed
beep boop <githubbot@vadim.me>
2024-07-06 02:13:25 +0200

    Solution 70 of CPP/44, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/vm7zlok5 Prompt: 1::basic_string<char>’ has no member named ‘size’  6236 |     { return (__lhs.size() == __rhs.size()       |               ~~~~~~^~~~ /usr/include/c++/11/bits/basic_string.h:6236:37: error: ‘const class std::__cxx1 1::basic_string<char>’ has no member named ‘size’  6236 |     { return (__lhs.size() == __rhs.size()       |                               ~~~~~~^~~~ /usr/include/c++/11/bits/basic_string.h:6238:59: error: ‘const class std::__cxx1 1::basic_string<char>’ has no member named ‘size’  6238 |                                                     __lhs.size())); }       |                                                     ~~~~~~^~~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- CPP_44.cpp
+++ CPP_44.cpp
@@ -2,15 +2,18 @@
 using namespace std;
 
 string change_base(int x, int base) {
-    char res[1000];
+    char res[1001];
     string s = "";
     size_t size = 0;
     while (x > 0) {
         int rem = x % base;
         char c = rem < 10 ? '0' + rem : rem > 9 ? 'A' + rem - 10 : '-';
-        res[size++] = c;
+        s.push_back(c);
+        x /= base;
+        size++;
     }
-    return string(res, size);
+    res[size] = '\0'; 
+    return string(res);
 }
 
 int main() {
@@ -19,5 +22,4 @@ int main() {
     cin >> num;
     cout << "Enter the base: ";
     cin >> base;
-    cout << "The conversion result is: " << change_base(num, base);
-}
+    cout << "The conversion result is: " << change_base(num, base) << endl;
