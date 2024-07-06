7c66c3a54fbac98d90fe82aac20b78680d19cc7f
beep boop <githubbot@vadim.me>
2024-07-07 01:14:48 +0200

    Solution 30 of CPP/39, ratio 0.7 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/l6ltyzsi Prompt: ba7aa48b-9366-4abb-ac0c-0d10eb6d5729.cpp: In function ‘int prime_fib(int)’: ba7aa48b-9366-4abb-ac0c-0d10eb6d5729.cpp:9:17: error: ‘isPrime’ was not declared  in this scope     9 |         while (!isPrime(fib))       |                 ^~~~~~~ ba7aa48b-9366-4abb-ac0c-0d10eb6d5729.cpp:12:1: warning: control reaches end of n on-void function [-Wreturn-type]    12 | }       | ^

--- CPP_39.cpp
+++ CPP_39.cpp
@@ -1,21 +1,18 @@
 #include <iostream>
 
-using namespace std;
-
 bool isPrime(int num) {
-    if (num <= 1)
-        return false;
-    for (int i = 2; i * i <= num; i++)
+    for (int i = 2; i * i <= num; i++) {
         if (num % i == 0)
             return false;
+    }
     return true;
 }
 
 int prime_fib(int n) {
     int a = 0, b = 1, fib = 0;
-    for (int i = 0; ; i++) {
+    for (int i = 0; i < n; i++) {
         if (i == n - 1)
-            return a;
+            return b;
         fib = a + b;
         a = b;
         b = fib;
