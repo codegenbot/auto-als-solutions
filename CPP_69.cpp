7305d09b0d8de4aa16f54f0c7ec156e323bcb5fa
beep boop <githubbot@vadim.me>
2024-07-06 07:21:18 +0200

    Solution 34 of CPP/69, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/qp49zkht Prompt: In file included from 4764e526-3c09-4029-936c-42e07f4df167.cpp:9: 4764e526-3c09-4029-936c-42e07f4df167.cpp: In function ‘int main()’: 4764e526-3c09-4029-936c-42e07f4df167.cpp:11:13: error: ‘search’ was not declared  in this scope; did you mean ‘std::search’?    11 |     assert (search({3, 10, 10, 9, 2}) == -1);       |             ^~~~~~ In file included from /usr/include/c++/11/algorithm:74,                  from 4764e526-3c09-4029-936c-42e07f4df167.cpp:1: /usr/include/c++/11/pstl/glue_algorithm_defs.h:117:1: note: ‘std::search’ declar ed here   117 | search(_ExecutionPolicy&& __exec, _ForwardIterator1 __first, _ForwardIte rator1 __last, _ForwardIterator2 __s_first,       | ^~~~~~

--- CPP_69.cpp
+++ CPP_69.cpp
@@ -1,25 +1,9 @@
-#include <bits/stdc++.h>
+#include <algorithm>
+#include <vector>
 
-using namespace std;
-
-int findMaxValue(vector<int> numbers) {
-    map<int, int> freqMap;
-    for (int num : numbers) {
-        if (freqMap.find(num) == freqMap.end()) {
-            freqMap[num] = 1;
-        } else {
-            freqMap[num]++;
-        }
-    }
-    for (auto it = freqMap.begin(); it != freqMap.end(); ++it) {
-        if (it->second >= it->first && it->first > 0) {
-            return it->first;
-        }
-    }
-    return -1;
+int findMaxValue(std::vector<int> numbers) {
+    int maxValue = *std::max_element(numbers.begin(), numbers.end());
+    return maxValue;
 }
 
-int main() {
-    assert(findMaxValue({3, 10, 10, 9, 2}) == -1);
-    return 0;
-}
+assert (std::search({3, 10, 10, 9, 2}, {3, 10, 10, 9, 2}) == -1);
