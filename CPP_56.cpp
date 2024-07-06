cfcb29b52d15d3bc56e3bf98ffac991f09f0883a
beep boop <githubbot@vadim.me>
2024-07-06 22:17:42 +0200

    Solution 68 of CPP/56, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/9uog2x34 Prompt: Program failed

--- CPP_56.cpp
+++ CPP_56.cpp
@@ -1,7 +1,3 @@
-````
-#include <string>
-using namespace std;
-
 bool correct_bracketing(string brackets){
     int opening = 0;
     for(char c : brackets) {
@@ -14,5 +10,3 @@ bool correct_bracketing(string brackets){
         }
     }
     return opening == 0;
-}
-```
