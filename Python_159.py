23ac3c03b99433f8f344b9b51a9c89bf18e61de8
beep boop <githubbot@vadim.me>
2024-07-06 16:01:24 +0200

    Solution 6 of Python/159, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/xo52ebw5 Prompt: Complete the following code given the task description and function signature.

--- Python_159.py
+++ Python_159.py
@@ -1,5 +1,3 @@
 def eat(number, need, remaining):
-    total_eaten = number + (
-        need - (remaining - need) if remaining >= need else remaining
-    )
-    return [total_eaten, remaining - total_eaten]
+    total_eaten = number + need - remaining if remaining >= need else number + remaining
+    return [total_eaten, abs(total_eaten - (number + need))]
