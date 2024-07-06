995a83cc58ed960c2e7d6359b33226ea1db9d2de
beep boop <githubbot@vadim.me>
2024-07-06 23:56:27 +0200

    Solution 20 of CPP/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/0ujhsbw6 Prompt: ed928f6f-281d-43e8-9a78-19f5d86d8218.cpp:22:13: error: ‘coeffs’ was not declared  in this scope; did you mean ‘coeff’?    22 |             coeffs.push_back(coeff);       |             ^~~~~~       |             coeff ed928f6f-281d-43e8-9a78-19f5d86d8218.cpp:25:30: error: ‘coeffs’ was not declared  in this scope; did you mean ‘ncoeff’?    25 |         solution = find_zero(coeffs);       |                              ^~~~~~       |                              ncoeff ed928f6f-281d-43e8-9a78-19f5d86d8218.cpp:25:36: error: ‘find_zero’ cannot be use d as a function    25 |         solution = find_zero(coeffs);       |                                    ^ In file included from ed928f6f-281d-43e8-9a78-19f5d86d8218.cpp:9: ed928f6f-281d-43e8-9a78-19f5d86d8218.cpp:26:21: error: ‘poly’ was not declared i n this scope    26 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                     ^~~~ ed928f6f-281d-43e8-9a78-19f5d86d8218.cpp:26:17: error: ‘abs’ was not declared in  this scope    26 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                 ^~~

--- CPP_32.cpp
+++ CPP_32.cpp
@@ -1,17 +1,18 @@
-```cpp
 vector<double> coeffs;
-double find_zero(vector<double> xs){
+int ncoeff;
+double poly(double x) {
     double sum = 0;
-    vector<double> poly(4);
-    for (int i = 2; i < xs.size(); i+=2) {
-        sum += xs[i] * pow(-sum / xs[i-1], i);
+    for (int i = 0; i < ncoeff; i++) {
+        sum += coeffs[i] * pow(x, i);
     }
-    return round(sum, 10);
+    return sum;
 }
-
-int main() {
-    vector<double> coeffs = {1.0, 3.0, -5.0, 4.0}; 
-    double solution = find_zero(coeffs); 
-    vector<double> poly = {1.0, 2.0, 3.0, 4.0}; 
-    assert(abs(poly[0] + poly[1]*solution + poly[2]*pow(solution, 2) + poly[3]*pow(solution, 3)) < 1e-3);
+double find_zero(vector<double> xs){
+    int ncoeff = xs.size();
+    vector<double> coeffs(xs.begin(), xs.end());
+    double solution = 0;
+    for (int i = 2; i < ncoeff; i+=2) {
+        solution += coeffs[i] * pow(-solution / coeffs[i-1], i);
+    }
+    return round(solution, 10);
 }
