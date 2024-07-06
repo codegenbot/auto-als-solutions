98d4e389be88de4a3b6a8aec0d726811eab58270
beep boop <githubbot@vadim.me>
2024-07-06 14:10:01 +0200

    Solution 38 of Python/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/x4etqnzo Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f21c8993-b5a9-45e7-a272-a7e305124 24b.py", line 1     Here is the solution:                 ^^^^^^^^ SyntaxError: invalid syntax

--- Python_32.py
+++ Python_32.py
@@ -5,6 +5,6 @@ def find_zero(xs: list):
     max_coeff = abs(max(xs))
     min_coeff = abs(min(xs))
     for x in range(-max_coeff, max_coeff+1):
-        if round(sum([a * x**i for i, a in enumerate(reversed(xs))]), 10) == 0:
+        if round(sum(i * j for i, j in zip(xs, [x**i for i in range(len(xs))])) , 10) == 0:
             return round(x, 2)
     return None
