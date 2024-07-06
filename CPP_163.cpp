97880b2c6d4ab383bc091c4640803decb295998a
beep boop <githubbot@vadim.me>
2024-07-06 12:48:59 +0200

    Solution 97 of CPP/163, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/58xbmj1f Prompt:       |             ^~~~~~ 000b82ed-b131-4f91-b367-440aecaababd.cpp:22:20: error: expected primary-expressi on before ‘int’    22 | bool issame(vector<int> a,vector<int>b){       |                    ^~~ 000b82ed-b131-4f91-b367-440aecaababd.cpp:22:27: error: ‘vector’ was not declared  in this scope    22 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 000b82ed-b131-4f91-b367-440aecaababd.cpp:22:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 000b82ed-b131-4f91-b367-440aecaababd.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 000b82ed-b131-4f91-b367-440aecaababd.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 000b82ed-b131-4f91-b367-440aecaababd.cpp:22:34: error: expected primary-expressi on before ‘int’    22 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~

--- CPP_163.cpp
+++ CPP_163.cpp
@@ -11,15 +11,14 @@ std::vector<int> generate_integers(int start, int end) {
 }
 
 bool operator==(const std::vector<int>& a, const std::vector<int>& b) {
-    if (a.size() != b.size())
-        return false;
-    for (int i = 0; i < a.size(); ++i)
-        if (a[i] != b[i])
-            return false;
+    if(a.size() != b.size()) return false;
+    for(int i = 0; i < a.size(); ++i) {
+        if(a[i] != b[i]) return false;
+    }
     return true;
 }
 
 int main_test() {
-    assert(generate_integers(17, 89) == generate_intements(17, 89));
+    assert(generate_integers(17, 89) == generate_intelines(17, 89));
     return 0;
 }
