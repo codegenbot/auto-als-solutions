e8dce1b92815fc49e02e600ee369c109dbcf8586
beep boop <githubbot@vadim.me>
2024-07-06 04:00:26 +0200

    Solution 40 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/xmlozhdh Prompt: In file included from /usr/include/c++/11/vector:67,                  from 0f23ac1d-a641-4741-bf17-de3a9317a627.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 0f23ac1d-a641-4741-bf17-de3a9317a627.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 0f23ac1d-a641-4741-bf17-de3a9317a627.cpp:34:34: error: expected primary-expressi on before ‘int’    34 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from 0f23ac1d-a641-4741-bf17-de3a9317a627.cpp:33: 0f23ac1d-a641-4741-bf17-de3a9317a627.cpp: In function ‘int main()’: 0f23ac1d-a641-4741-bf17-de3a9317a627.cpp:43:20: error: ‘strange_sort_list’ was n ot declared in this scope    43 |     assert (issame(strange_sort_list({111111}) , {111111}));       |                    ^~~~~~~~~~~~~~~~~ 0f23ac1d-a641-4741-bf17-de3a9317a627.cpp:43:19: error: ‘issame’ cannot be used a s a function    43 |     assert (issame(strange_sort_list({111111}) , {111111}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -1,26 +1,28 @@
 #include <vector>
 #include <algorithm>
 
-bool issame(vector<int> a, vector<int> b) {
+bool std::issame(std::vector<int> a, std::vector<int> b) {
     return a == b;
 }
 
-vector<int> strange_sort_list(vector<int> lst) {
-    vector<int> result;
+std::vector<int> strange_sort_list(std::vector<int> lst) {
+    std::vector<int> result;
     while (!lst.empty()) {
-        int min_val = *min_element(lst.begin(), lst.end());
-        result.push_back(min_val);
-        lst.erase(remove(lst.begin(), lst.end(), min_val), lst.end());
-        
+        int val = *lst.begin();
+        result.push_back(val);
+        lst.erase(remove(lst.begin(), lst.end(), val), lst.end());
         if (!lst.empty()) {
-            int max_val = *max_element(lst.begin(), lst.end());
-            result.push_back(max_val);
-            for(int i : lst) {
-                if(i == max_val) {
-                    lst.erase(remove(lst.begin(), lst.end(), i), lst.end());
+            vector<int> temp;
+            for(int i : lst){
+                if(i == *max_element(lst.begin(), lst.end())){
+                    temp.push_back(i);
                     break;
                 }
             }
+            for(int x:temp){
+                lst.erase(remove(lst.begin(), lst.end(), x), lst.end());
+            }
+            result.push_back(*max_element(lst.begin(), lst.end()));
         } else {
             result.push_back(*min_element(lst.begin(), lst.end()));
         }
