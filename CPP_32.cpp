43c7c7c53abceadf9f4098e7eb547daa73e3ddc7
beep boop <githubbot@vadim.me>
2024-07-07 00:03:11 +0200

    Solution 52 of CPP/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/0ujhsbw6 Prompt: 6f240b40-0a89-49ed-a018-db874597fa88.cpp:27:13: error: ‘coeffs’ was not declared  in this scope; did you mean ‘coeff’?    27 |             coeffs.push_back(coeff);       |             ^~~~~~       |             coeff 6f240b40-0a89-49ed-a018-db874597fa88.cpp:30:30: error: ‘coeffs’ was not declared  in this scope; did you mean ‘ncoeff’?    30 |         solution = find_zero(coeffs);       |                              ^~~~~~       |                              ncoeff 6f240b40-0a89-49ed-a018-db874597fa88.cpp:30:36: error: ‘find_zero’ cannot be use d as a function    30 |         solution = find_zero(coeffs);       |                                    ^ In file included from 6f240b40-0a89-49ed-a018-db874597fa88.cpp:14: 6f240b40-0a89-49ed-a018-db874597fa88.cpp:31:21: error: ‘poly’ was not declared i n this scope    31 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                     ^~~~ 6f240b40-0a89-49ed-a018-db874597fa88.cpp:31:17: error: ‘abs’ was not declared in  this scope    31 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                 ^~~

--- CPP_32.cpp
+++ CPP_32.cpp
@@ -1,21 +1,23 @@
 ```
 vector<double> coeffs = xs;
+double poly(vector<double> coeffs, double x) {
+    double result = 0.0;
+    for(int i=0; i<coeffs.size(); i++) {
+        result += coeffs[i] * pow(x, i);
+    }
+    return result;
+}
+
 double find_zero(vector<double> xs){
-    int n = xs.size();
-    double a = 0;
-    for(int i=0; i<n; i++){
+    vector<double> coeffs = xs;
+    int n = coeffs.size();
+    double a = 1.0;
+    for(int i=1; i<n; i++){
         a = 0;
-        for(int j=0; j<=i; j++)
+        for(int j=0; j<i; j++)
             a += coeffs[j] * pow(1.0, i-j);
         if(a == 0) return 1.0;
     }
     double x = -coeffs[1]/2*coeffs[2];
     return x;
-}
-double poly(vector<double> coeffs, double x){
-    int n = coeffs.size();
-    double a = 0;
-    for(int i=0; i<n; i++)
-        a += coeffs[i] * pow(x, i);
-    return a;
 }
