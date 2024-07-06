4fef30a5ed2b0499a61d4e6ac889f5358550d5ed
beep boop <githubbot@vadim.me>
2024-07-06 02:26:59 +0200

    Solution 82 of CPP/44, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/vm7zlok5 Prompt: e92d665f-8c5a-44bb-bcbe-001753db126b.cpp:1:1: error: stray ‘`’ in program     1 | ```       | ^ e92d665f-8c5a-44bb-bcbe-001753db126b.cpp:1:2: error: stray ‘`’ in program     1 | ```       |  ^ e92d665f-8c5a-44bb-bcbe-001753db126b.cpp:1:3: error: stray ‘`’ in program     1 | ```       |   ^ e92d665f-8c5a-44bb-bcbe-001753db126b.cpp:29:5: error: redefinition of ‘int main( )’    29 | int main(){       |     ^~~~ e92d665f-8c5a-44bb-bcbe-001753db126b.cpp:19:5: note: ‘int main()’ previously def ined here    19 | int main() {       |     ^~~~

--- CPP_44.cpp
+++ CPP_44.cpp
@@ -1,4 +1,3 @@
-```
 #include <iostream>
 using namespace std;
 
@@ -9,19 +8,18 @@ string change_base(int x, int base) {
     while (x > 0) {
         int rem = x % base;
         char c = rem < 10 ? '0' + rem : rem > 9 ? 'A' + rem - 10 : '-';
-        s.push_back(c);
+        s.insert(0, 1, c);
         x /= base;
         size++;
     }
-    return s.substr(0, size);
+    return s;
 }
 
-cout << "Enter a number: ";
-int num;
-cin >> num;
-
-cout << "Enter the base: ";
-int base;
-cin >> base;
-
-cout << "The conversion result is: " << change_base(num, base);
+int main() {
+    int num, base;
+    cout << "Enter a number: ";
+    cin >> num;
+    cout << "Enter the base: ";
+    cin >> base;
+    cout << "The conversion result is: " << change_base(num, base);
+}
