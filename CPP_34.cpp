9d978eb4329501fd9d6647373e817d8a04bd06a7
beep boop <githubbot@vadim.me>
2024-07-06 19:33:36 +0200

    Solution 80 of CPP/34, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/yxw68pfq Prompt:       | ^~~~~~~~~~~ /usr/include/c++/11/pstl/glue_algorithm_defs.h:238:1: note:   template argument deduction/substitution failed: 8e9865d1-a7cf-40ac-80ee-e882f5157576.cpp:6:46: note:   candidate expects 5 argum ents, 2 provided     6 |     vector<int> result(l.begin(), unique_copy(l.begin(), l.end()));       |                                   ~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~ In file included from /usr/include/c++/11/algorithm:74,                  from 8e9865d1-a7cf-40ac-80ee-e882f5157576.cpp:2: /usr/include/c++/11/pstl/glue_algorithm_defs.h:243:1: note: candidate: ‘template <class _ExecutionPolicy, class _ForwardIterator1, class _ForwardIterator2> __pst l::__internal::__enable_if_execution_policy<_ExecutionPolicy, _ForwardIterator2>  std::unique_copy(_ExecutionPolicy&&, _ForwardIterator1, _ForwardIterator1, _For wardIterator2)’   243 | unique_copy(_ExecutionPolicy&& __exec, _ForwardIterator1 __first, _Forwa rdIterator1 __last, _ForwardIterator2 __result);       | ^~~~~~~~~~~ /usr/include/c++/11/pstl/glue_algorithm_defs.h:243:1: note:   template argument deduction/substitution failed: 8e9865d1-a7cf-40ac-80ee-e882f5157576.cpp:6:46: note:   candidate expects 4 argum ents, 2 provided     6 |     vector<int> result(l.begin(), unique_copy(l.begin(), l.end()));       |                                   ~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~

--- CPP_34.cpp
+++ CPP_34.cpp
@@ -1,26 +1,9 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include<vector>
+#include<algorithm>
 using namespace std;
 
-vector<int> unique(vector<int> l) {
-    vector<int> result(l.begin(), unique_copy(l.begin(), l.end()));
+vector<int> unique(vector<int> l){
+    vector<int> result(l.begin(), l.end());
     sort(result.begin(), result.end());
     return result;
-}
-
-int main() {
-    vector<int> v = {5, 3, 5, 2, 3, 3, 9, 0, 123};
-    for (int x : v) {
-        cout << x << " ";
-    }
-    cout << endl;
-    
-    vector<int> unique_v = unique(v);
-    for (int x : unique_v) {
-        cout << x << " ";
-    }
-    cout << endl;
-
-    return 0;
 }
