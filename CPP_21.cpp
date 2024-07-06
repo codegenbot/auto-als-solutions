7a181b5afdc71b35d9a7f84f825c773a77bfe084
beep boop <githubbot@vadim.me>
2024-07-06 23:25:25 +0200

    Solution 9 of CPP/21, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/95zl2c2v Prompt: Complete the following code given the task description and function signature.

--- CPP_21.cpp
+++ CPP_21.cpp
@@ -1,4 +1,7 @@
-#include <vector>
+#include<stdio.h>
+#include<math.h>
+#include<vector>
+using namespace std;
 
 vector<float> rescale_to_unit(vector<float> numbers) {
     float minVal = *min_element(numbers.begin(), numbers.end());
