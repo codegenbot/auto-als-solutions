551023052a29703dc26e42bf000e89b962a3ae60
beep boop <githubbot@vadim.me>
2024-07-06 05:09:39 +0200

    Solution 57 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/xmlozhdh Prompt: 6d2aa5cc-ebc9-4b85-92d5-ad5edbd586e3.cpp:35:5: note: ‘int main()’ previously def ined here    35 | int main() {       |     ^~~~ In file included from 6d2aa5cc-ebc9-4b85-92d5-ad5edbd586e3.cpp:48: 6d2aa5cc-ebc9-4b85-92d5-ad5edbd586e3.cpp: In function ‘int main()’: 6d2aa5cc-ebc9-4b85-92d5-ad5edbd586e3.cpp:58:37: error: cannot bind non-const lva lue reference of type ‘std::vector<int>&’ to an rvalue of type ‘std::vector<int> ’    58 |     assert (issame(strange_sort_list({111111}) , {111111}));       |                    ~~~~~~~~~~~~~~~~~^~~~~~~~~~ In file included from /usr/include/c++/11/vector:67,                  from 6d2aa5cc-ebc9-4b85-92d5-ad5edbd586e3.cpp:2: /usr/include/c++/11/bits/stl_vector.h:625:7: note:   after user-defined conversi on: ‘std::vector<_Tp, _Alloc>::vector(std::initializer_list<_Tp>, const allocato r_type&) [with _Tp = int; _Alloc = std::allocator<int>; std::vector<_Tp, _Alloc> ::allocator_type = std::allocator<int>]’   625 |       vector(initializer_list<value_type> __l,       |       ^~~~~~ 6d2aa5cc-ebc9-4b85-92d5-ad5edbd586e3.cpp:9:42: note:   initializing argument 1 o f ‘void strange_sort_list(std::vector<int>&)’     9 | void strange_sort_list(std::vector<int>& lst) {       |                        ~~~~~~~~~~~~~~~~~~^~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -6,13 +6,13 @@ bool issame(std::vector<int> a, std::vector<int> b) {
     return a == b;
 }
 
-void strange_sort_list(const std::vector<int>& lst) {
+void strange_sort_list(std::vector<int>& lst) {
     std::vector<int> result;
     while (!lst.empty()) {
         int min_val = *std::min_element(lst.begin(), lst.end());
         result.push_back(min_val);
         lst.erase(std::remove(lst.begin(), lst.end(), min_val), lst.end());
-        
+
         if (!lst.empty()) {
             int max_val = *std::max_element(lst.begin(), lst.end());
             std::vector<int> temp;
@@ -40,7 +40,8 @@ int main() {
         std::cin >> num;
         input.push_back(num);
     }
-    
+
     strange_sort_list(input);
+    
     return 0;
 }
