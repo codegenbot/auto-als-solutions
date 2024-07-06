b1134485171ca4e0852a8cd4aabf0040d0d8c504
beep boop <githubbot@vadim.me>
2024-07-06 02:17:18 +0200

    Solution 34 of CPP/15, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/95movtiu Prompt: a0fd550f-12ca-4fc8-b8d4-7f4e6f88066f.cpp:1:1: error: stray ‘`’ in program     1 | ```       | ^ a0fd550f-12ca-4fc8-b8d4-7f4e6f88066f.cpp:1:2: error: stray ‘`’ in program     1 | ```       |  ^ a0fd550f-12ca-4fc8-b8d4-7f4e6f88066f.cpp:1:3: error: stray ‘`’ in program     1 | ```       |   ^ a0fd550f-12ca-4fc8-b8d4-7f4e6f88066f.cpp:23:5: error: redefinition of ‘int main( )’    23 | int main(){       |     ^~~~ a0fd550f-12ca-4fc8-b8d4-7f4e6f88066f.cpp:15:5: note: ‘int main()’ previously def ined here    15 | int main() {       |     ^~~~

--- CPP_15.cpp
+++ CPP_15.cpp
@@ -4,11 +4,12 @@
 
 std::string string_sequence(int n) {
     std::ostringstream oss;
-    for (int i = 0; i <= n; ++i) {
-        oss << i << " ";
+    for (int i = 1; i <= n; ++i) {
+        if(i != 1)
+            oss << " ";
+        oss << i;
     }
-    if(n != 0)
-        oss << std::endl; 
+    oss << std::endl; 
     return oss.str();
 }
 
@@ -16,9 +17,5 @@ int main() {
     int n;
     std::cout << "Enter a number: ";
     std::cin >> n;
-    if(n > 0) {
-        std::cout << string_sequence(n) << std::endl;
-    }
-    else
-        std::cout << "Error! Please enter a positive integer." << std::endl;
+    std::cout << string_sequence(n) << std::endl;
 }
