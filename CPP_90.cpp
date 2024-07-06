aeb8fb20974acecca2798711af16b00e33da0bcb
beep boop <githubbot@vadim.me>
2024-07-06 03:58:18 +0200

    Solution 19 of CPP/90, ratio 0.8333333333333334 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/q2kr7bww Prompt: 2ebe474c-d8fc-40d4-b95b-5bcc84d446ef.cpp:1:24: error: ‘vector’ is not a member o f ‘std’     1 | int next_smallest(std::vector<int> lst) {       |                        ^~~~~~ 2ebe474c-d8fc-40d4-b95b-5bcc84d446ef.cpp:1:1: note: ‘std::vector’ is defined in header ‘<vector>’; did you forget to ‘#include <vector>’?   +++ |+#include <vector>     1 | int next_smallest(std::vector<int> lst) { 2ebe474c-d8fc-40d4-b95b-5bcc84d446ef.cpp:1:31: error: expected primary-expressio n before ‘int’     1 | int next_smallest(std::vector<int> lst) {       |                               ^~~ In file included from 2ebe474c-d8fc-40d4-b95b-5bcc84d446ef.cpp:12: 2ebe474c-d8fc-40d4-b95b-5bcc84d446ef.cpp: In function ‘int main()’: 2ebe474c-d8fc-40d4-b95b-5bcc84d446ef.cpp:14:26: error: ‘next_smallest’ cannot be  used as a function    14 |     assert (next_smallest({-35, 34, 12, -45}) == -35);       |             ~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~

--- CPP_90.cpp
+++ CPP_90.cpp
@@ -2,11 +2,11 @@
 #include <limits>
 
 int next_smallest(std::vector<int> lst) {
-    if (lst.empty()) return 0; 
+    if (lst.empty()) return 0;
     int result = lst[0];
     for (int i = 1; i < lst.size(); i++) {
-        if (lst[i] < lst[0]) {
-            result = std::min(result, lst[i]);
+        if (lst[i] <= result) {
+            result = lst[i];
         }
     }
     return result;
