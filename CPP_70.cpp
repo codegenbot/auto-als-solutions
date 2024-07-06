9ed430dc8e39bc563535eba7a7fa0c9c30260576
beep boop <githubbot@vadim.me>
2024-07-06 20:25:07 +0200

    Solution 34 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/t59aw96g Prompt:                  from /usr/include/c++/11/algorithm:74,                  from cf425b02-094b-4229-90d5-ad38c640edb8.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ cf425b02-094b-4229-90d5-ad38c640edb8.cpp:18:34: error: expected primary-expressi on before ‘int’    18 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ cf425b02-094b-4229-90d5-ad38c640edb8.cpp:18:39: error: expression list treated a s compound expression in initializer [-fpermissive]    18 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from cf425b02-094b-4229-90d5-ad38c640edb8.cpp:17: cf425b02-094b-4229-90d5-ad38c640edb8.cpp: In function ‘int main()’: cf425b02-094b-4229-90d5-ad38c640edb8.cpp:27:20: error: ‘strange_sort_list’ was n ot declared in this scope    27 |     assert (issame(strange_sort_list({111111}) , {111111}));       |                    ^~~~~~~~~~~~~~~~~ cf425b02-094b-4229-90d5-ad38c640edb8.cpp:27:19: error: ‘issame’ cannot be used a s a function    27 |     assert (issame(strange_sort_list({111111}) , {111111}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -20,12 +20,14 @@ std::vector<int> strange_sort_list(std::vector<int> lst) {
 }
 
 int main() {
-    std::vector<int> input = {3, 2, 1};
+    std::vector<int> input = {1, 2, 3, 4, 5};
     std::vector<int> output = strange_sort_list(input);
-    if (issame(output, {1, 2, 3})) {
-        std::cout << "Success!" << std::endl;
+    
+    if (issame(output, {1, 5, 2, 4, 3})) {
+        std::cout << "Test passed." << std::endl;
     } else {
-        std::cout << "Failure!" << std::endl;
+        std::cout << "Test failed." << std::endl;
     }
+    
     return 0;
 }
