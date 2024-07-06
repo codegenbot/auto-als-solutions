1613bd68954d51241a60817295f5a0254caaafb4
beep boop <githubbot@vadim.me>
2024-07-06 07:29:45 +0200

    Solution 95 of CPP/73, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/s4xvbypn Prompt: t>::operator[](((std::vector<long unsigned int>::size_type)(k - 1)))’, which is of non-class type ‘__gnu_cxx::__alloc_traits<std::allocator<long unsigned int>, long unsigned int>::value_type’ {aka ‘long unsigned int’}    21 |                     size_t ops = 1 + dp[i][k-1].size() + dp[k+1][j].size ();       |                                                 ^~~~ cd2ce184-8fb7-4b8b-834a-282e201a12e7.cpp:21:69: error: request for member ‘size’  in ‘(& dp.std::vector<std::vector<long unsigned int> >::operator[](((std::vecto r<std::vector<long unsigned int> >::size_type)(k + 1))))->std::vector<long unsig ned int>::operator[](((std::vector<long unsigned int>::size_type)j))’, which is of non-class type ‘__gnu_cxx::__alloc_traits<std::allocator<long unsigned int>, long unsigned int>::value_type’ {aka ‘long unsigned int’}    21 |                 size_t ops = 1 + dp[i][k-1].size() + dp[k+1][j].size();       |                                                                 ^~~~ cd2ce184-8fb7-4b8b-834a-282e201a12e7.cpp:26:26: error: request for member ‘push_ back’ in ‘(& dp.std::vector<std::vector<long unsigned int> >::operator[](((std:: vector<std::vector<long unsigned int> >::size_type)i)))->std::vector<long unsign ed int>::operator[](((std::vector<long unsigned int>::size_type)j))’, which is o f non-class type ‘__gnu_cxx::__alloc_traits<std::allocator<long unsigned int>, l ong unsigned int>::value_type’ {aka ‘long unsigned int’}    26 |                 dp[i][j].push_back(min_ops);       |                          ^~~~~~~~~

--- CPP_73.cpp
+++ CPP_73.cpp
@@ -3,30 +3,26 @@
 
 int smallest_change(std::vector<int> arr) {
     int n = arr.size();
-    std::vector<std::vector<int>> dp(n, std::vector<int>(n-1));
+    std::vector<size_t> dp(n-1);
     
-    for (int i = 0; i < n; i++) {
-        dp[i][i] = 0;
+    for (int i = 0; i < n - 1; i++) {
+        dp[i] = INT_MAX;
     }
     
-    for (int length = 2; length <= n; length++) {
-        for (int i = 0; i < n - length + 1; i++) {
-            int j = i + length - 1;
-            
-            if (arr[i] == arr[j]) {
-                dp[i][j] = dp[i+1][j-1].size();
-            } else {
-                int min_ops = INT_MAX;
-                for (int k = i; k <= j; k++) {
-                    int ops = 1 + dp[i][k-1].size() + dp[k+1][j].size();
-                    if (ops < min_ops) {
-                        min_ops = ops;
-                    }
+    for (int i = 0; i < n - 1; i++) {
+        if (arr[i] == arr[i+1]) {
+            dp[i] = 0;
+        } else {
+            int min_ops = INT_MAX;
+            for (int k = 0; k <= 1; k++) {
+                size_t ops = 1 + (k==0 ? dp[i-1]:0) + (k==1?0:dp[i].size());
+                if (ops < min_ops) {
+                    min_ops = ops;
                 }
-                dp[i][j] = min_ops;
             }
+            dp[i] = min_ops;
         }
     }
     
-    return *std::max_element(dp[0].begin(), dp[0].end());
+    return *std::max_element(dp.begin(), dp.end())+1;
 }
