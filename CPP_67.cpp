0b0676f81d76519f98596005e04450c00328c768
beep boop <githubbot@vadim.me>
2024-07-06 19:59:49 +0200

    Solution 12 of CPP/67, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/juuz10le Prompt: Complete the following code given the task description and function signature.

--- CPP_67.cpp
+++ CPP_67.cpp
@@ -1,18 +1,32 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
 int fruit_distribution(string s, int n) {
-    size_t pos = 0;
-    string word;
-    int apples = 0, oranges = 0;
-
-    while ((pos = s.find(" and ")) != string::npos) {
-        word = s.substr(0, pos);
-        if (word.find("apples") != string::npos)
-            apples = stoi(word.substr(0, word.find(" ")));
-        else if (word.find("oranges") != string::npos)
-            oranges = stoi(word.substr(0, word.find(" ")));
-
-        s.erase(0, pos + 5);
+    size_t apple_count = 0, orange_count = 0;
+    for (size_t i = 0; i < s.size(); ++i) {
+        if (s[i] == 'a' && s[i+1] == 'p') {
+            while (i + 3 <= s.size() && s.substr(i + 3, 2) != "and" 
+                   && s.substr(i + 3, 5) != "apples") {
+                ++apple_count;
+                i += (s.substr(i + 3, 1) == ' ') ? 4 : 6;
+            }
+        } else if (s[i] == 'o' && s[i+1] == 'r') {
+            while (i + 3 <= s.size() && s.substr(i + 3, 2) != "and" 
+                   && s.substr(i + 3, 5) != "oranges") {
+                ++orange_count;
+                i += (s.substr(i + 3, 1) == ' ') ? 4 : 6;
+            }
+        }
     }
+    
+    return n - apple_count - orange_count;
+}
 
-    int mangoes = n - apples - oranges;
-    return mangoes;
+int main() {
+    string s = "5 apples and 6 oranges";
+    int n = 19;
+    cout << fruit_distribution(s, n);
+    return 0;
 }
