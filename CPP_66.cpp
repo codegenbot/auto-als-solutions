fc5dd2bdc874f7ddc8247554c75210f2eba87a65
beep boop <githubbot@vadim.me>
2024-07-06 19:42:58 +0200

    Solution 2 of CPP/66, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/8q0v1d66 Prompt: Complete the following code given the task description and function signature.

--- CPP_66.cpp
+++ CPP_66.cpp
@@ -1,11 +1,8 @@
-#include<string>
-using namespace std;
-
 int digitSum(string s){
     int sum = 0;
     for(char c : s){
         if(isupper(c))
-            sum += c;
+            sum += (int)c;
     }
     return sum;
 }
