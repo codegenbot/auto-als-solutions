d54558d8ad620e4bffcc6a7bd2ae7259f0524416
beep boop <githubbot@vadim.me>
2024-07-06 20:53:20 +0200

    Solution 71 of CPP/68, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/np75m44i Prompt:       |                    ^~~ ddac7f79-fb78-4df7-b210-eff089399c96.cpp:24:27: error: ‘vector’ was not declared  in this scope    24 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ ddac7f79-fb78-4df7-b210-eff089399c96.cpp:24:34: error: expected primary-expressi on before ‘int’    24 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ ddac7f79-fb78-4df7-b210-eff089399c96.cpp:24:39: error: expression list treated a s compound expression in initializer [-fpermissive]    24 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from ddac7f79-fb78-4df7-b210-eff089399c96.cpp:23: ddac7f79-fb78-4df7-b210-eff089399c96.cpp: In function ‘int main()’: ddac7f79-fb78-4df7-b210-eff089399c96.cpp:33:20: error: ‘pluck’ was not declared in this scope    33 |     assert (issame(pluck({7, 9, 7, 1}) , {}));       |                    ^~~~~ ddac7f79-fb78-4df7-b210-eff089399c96.cpp:33:19: error: ‘issame’ cannot be used a s a function    33 |     assert (issame(pluck({7, 9, 7, 1}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_68.cpp
+++ CPP_68.cpp
@@ -1,6 +1,6 @@
 #include <vector>
 
-std::vector<int> pluck(std::vector<int> arr){
+std::vector<int> pluck(std::vector<int> arr) {
     std::vector<int> result = {};
     int minEvenValueIndex = -1;
     int minEvenValue = INT_MAX;
@@ -18,4 +18,3 @@ std::vector<int> pluck(std::vector<int> arr){
     }
 
     return result;
-}
