9037d49f90ed056b671d9bea56cae09dc23d8230
beep boop <githubbot@vadim.me>
2024-07-06 21:25:13 +0200

    Solution 96 of CPP/67, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/juuz10le Prompt: Program failed

--- CPP_67.cpp
+++ CPP_67.cpp
@@ -1,16 +1,18 @@
-```cpp
 int fruit_distribution(string s, int n) {
     size_t pos = 0;
     int apples = 0, oranges = 0;
     
     while ((pos = s.find(" apples", pos)) != string::npos) {
-        apples += stoi(s.substr(pos + 7).erase(0, 1));
-        s.erase(0, pos + 8);
+        apples = stoi(s.substr(0, pos).erase().substr(1).erase());
+        pos += 7; 
     }
     
-    while ((pos = s.find(" oranges")) != string::npos) {
-        oranges += stoi(s.substr(pos + 8).erase(0, 1));
-        s.erase(0, pos + 9);
+    if (s.find(" oranges") != string::npos) {
+        size_t pos2 = s.find(" oranges");
+        while ((pos2 = s.find(" oranges", pos2)) != string::npos) {
+            oranges = stoi(s.substr(pos2 + 8).erase());
+            pos2 += 8; 
+        }
     }
     
     return n - apples - oranges;
