ca69adc04adeec7df3567b42a5cb6aa41086bd57
beep boop <githubbot@vadim.me>
2024-07-06 23:40:00 +0200

    Solution 5 of CPP/144, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/q7aqeoe1 Prompt: Complete the following code given the task description and function signature.

--- CPP_144.cpp
+++ CPP_144.cpp
@@ -1,17 +1,15 @@
 #include <string>
-#include <sstream>
-
 using namespace std;
 
 bool simplify(string x, string n) {
-    int a = 0, b = 1, c = 0, d = 1;
-    stringstream s(x);
-    s >> a >> b;
-    s.clear();
-    s.str(n);
-    s >> c >> d;
-    if (b*d == a*c)
+    int a = stoi(strtok((x.substr(1)).c_str(), "/"));
+    int b = stoi(strtok(NULL, "/"));
+    int c = stoi(strtok((n.substr(1)).c_str(), "/"));
+    int d = stoi(strtok(NULL, "/"));
+
+    if (a * c == b * d) {
         return true;
-    else
+    } else {
         return false;
+    }
 }
