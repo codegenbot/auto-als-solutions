19579e8b8eafb8705c69c25205aa6d7e2eff5c99
beep boop <githubbot@vadim.me>
2024-07-06 12:19:32 +0200

    Solution 65 of CPP/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/2iqw0vuf Prompt:     1 | ```       |  ^ 0dd95f5d-25fb-4290-b1bb-3bd843cefcbe.cpp:1:3: error: stray ‘`’ in program     1 | ```       |   ^ 0dd95f5d-25fb-4290-b1bb-3bd843cefcbe.cpp:22:5: error: redefinition of ‘int main( )’    22 | int main(){       |     ^~~~ 0dd95f5d-25fb-4290-b1bb-3bd843cefcbe.cpp:16:5: note: ‘int main()’ previously def ined here    16 | int main() {       |     ^~~~ In file included from 0dd95f5d-25fb-4290-b1bb-3bd843cefcbe.cpp:21: 0dd95f5d-25fb-4290-b1bb-3bd843cefcbe.cpp: In function ‘int main()’: 0dd95f5d-25fb-4290-b1bb-3bd843cefcbe.cpp:23:35: error: ‘lst’ was not declared in  this scope    23 |     assert (double_the_difference(lst) == odd_sum );       |                                   ^~~ 0dd95f5d-25fb-4290-b1bb-3bd843cefcbe.cpp:23:43: error: ‘odd_sum’ was not declare d in this scope    23 |     assert (double_the_difference(lst) == odd_sum );       |                                           ^~~~~~~

--- CPP_151.cpp
+++ CPP_151.cpp
@@ -14,7 +14,6 @@ long long double_the_difference(vector<double> lst) {
 
 int main() {
     vector<double> lst({1.5, 2.5, -3.4, 4.7});
-    long long odd_sum = 0;
-    assert(double_the_difference(lst) == odd_sum);
+    long long odd_sum = double_the_difference(lst);
     return 0;
 }
