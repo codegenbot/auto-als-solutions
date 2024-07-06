5cfb3917f996ed77f3dce04127b0a720bbac43ff
beep boop <githubbot@vadim.me>
2024-07-06 03:09:05 +0200

    Solution 16 of CPP/86, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/qsuhvrkf Prompt: 47034308-145f-49d8-8020-4308fba71704.cpp:3:1: error: ‘string’ does not name a ty pe; did you mean ‘stdin’?     3 | string anti_shuffle(string s) {       | ^~~~~~       | stdin In file included from 47034308-145f-49d8-8020-4308fba71704.cpp:21: 47034308-145f-49d8-8020-4308fba71704.cpp: In function ‘int main()’: 47034308-145f-49d8-8020-4308fba71704.cpp:23:13: error: ‘anti_shuffle’ was not de clared in this scope    23 |     assert (anti_shuffle("Hi. My name is Mister Robot. How are you?") ==  ".Hi My aemn is Meirst .Rboot How aer ?ouy");       |             ^~~~~~~~~~~~

--- CPP_86.cpp
+++ CPP_86.cpp
@@ -1,30 +1,18 @@
-#include <algorithm>
-#include <iostream>
-#include <sstream>
 #include <string>
 
-std::string anti_shuffle(std::string s) {
-    std::stringstream result;
-    std::string word;
+string anti_shuffle(string s) {
+    string result = "";
     for(int i=0; i<s.size(); i++){
         if(s[i] == ' '){
-            result << ' ';
+            result += ' ';
             continue;
         }
+        string word = "";
         for(int j=i; j<s.size() && s[j] != ' '; j++){
             word += s[j];
         }
-        std::sort(word.begin(), word.end());
-        while (!word.empty()) {
-            result << word.back();
-            word.pop_back();
-        }
+        i = j - 1;
+        sort(word.begin(), word.end());
+        result += word;
     }
-    return result.str();
-}
-
-int main() {
-    std::string str = "Hi. My name is Mister Robot. How are you?";
-    assert(anti_shuffle(str) == ".Hi My aemn is Meirst .Rboot How aer ?ouy");
-    return 0;
-}
+    return result;
