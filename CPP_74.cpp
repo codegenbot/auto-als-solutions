314cd6a3acb871670a3b326d79d35c4ead7bc63d
beep boop <githubbot@vadim.me>
2024-07-06 04:22:03 +0200

    Solution 44 of CPP/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/k9lvoh5f Prompt: d3926f04-33d4-444f-8a05-2fb0b5ad9476.cpp:61:37: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from d3926f04-33d4-444f-8a05-2fb0b5ad9476.cpp:3: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from d3926f04-33d4-444f-8a05-2fb0b5ad9476.cpp:3: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ d3926f04-33d4-444f-8a05-2fb0b5ad9476.cpp:61:44: error: ‘b’ was not declared in t his scope    61 | bool issame(vector<string> a,vector<string>b){       |                                            ^ In file included from d3926f04-33d4-444f-8a05-2fb0b5ad9476.cpp:60: d3926f04-33d4-444f-8a05-2fb0b5ad9476.cpp: In function ‘int main()’: d3926f04-33d4-444f-8a05-2fb0b5ad9476.cpp:70:19: error: too many arguments to fun ction ‘bool issame(std::vector<std::__cxx11::basic_string<char> >)’    70 |     assert (issame(total_match({"this"}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ d3926f04-33d4-444f-8a05-2fb0b5ad9476.cpp:5:6: note: declared here     5 | bool issame(std::vector<std::string> a) {       |      ^~~~~~

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -2,7 +2,11 @@
 #include <algorithm>
 #include <string>
 
-bool issame(std::vector<std::string> a) {
+bool issame(std::vector<std::string> a, std::vector<std::string> b) {
+    if (a.size() != b.size()) return false;
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) return false;
+    }
     return true;
 }
 
