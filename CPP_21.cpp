724ea4fe7b7201a04c0a439763f3bda79e4251a3
beep boop <githubbot@vadim.me>
2024-07-06 23:43:16 +0200

    Solution 31 of CPP/21, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/95zl2c2v Prompt: 1c8bca12-091f-47b6-8aa0-843104e4cd6b.cpp: In function ‘std::vector<float> rescal e_to_unit(std::vector<float>)’: 1c8bca12-091f-47b6-8aa0-843104e4cd6b.cpp:7:22: error: ‘min_element’ was not decl ared in this scope     7 |     float min_val = *min_element(numbers.begin(), numbers.end());       |                      ^~~~~~~~~~~ 1c8bca12-091f-47b6-8aa0-843104e4cd6b.cpp:8:22: error: ‘max_element’ was not decl ared in this scope     8 |     float max_val = *max_element(numbers.begin(), numbers.end());       |                      ^~~~~~~~~~~

--- CPP_21.cpp
+++ CPP_21.cpp
@@ -1,10 +1,8 @@
-#include<stdio.h>
-#include<math.h>
-#include<vector>
-#include<algorithm>
+#include <algorithm>
+
 using namespace std;
 
-vector<float> rescale_to_unit(vector<float> numbers){ 
+vector<float> rescale_to_unit(vector<float> numbers) {
     float min_val = *min_element(numbers.begin(), numbers.end());
     float max_val = *max_element(numbers.begin(), numbers.end());
 
@@ -13,4 +11,3 @@ vector<float> rescale_to_unit(vector<float> numbers){
         result.push_back((num - min_val) / (max_val - min_val));
     }
     return result;
-}
