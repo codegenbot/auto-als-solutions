753eeadc33dd6e494a7ce81139fd4a56cbcd9ac8
beep boop <githubbot@vadim.me>
2024-07-06 12:18:51 +0200

    Solution 58 of CPP/133, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/nokt3pfm Prompt: /usr/include/c++/11/ext/new_allocator.h: In instantiation of ‘_Tp* __gnu_cxx::ne w_allocator<_Tp>::allocate(int, const void*) [with _Tp = double]’: /usr/include/c++/11/bits/alloc_traits.h:464:28:   required from ‘static _Tp* std ::allocator_traits<std::allocator<_CharT> >::allocate(std::allocator_traits<std: :allocator<_CharT> >::allocator_type&, int) [with _Tp = double; std::allocator_t raits<std::allocator<_CharT> >::pointer = double*; std::allocator_traits<std::al locator<_CharT> >::allocator_type = std::allocator<double>]’ /usr/include/c++/11/bits/stl_vector.h:346:33:   required from ‘std::_Vector_base <_Tp, _Alloc>::pointer std::_Vector_base<_Tp, _Alloc>::_M_allocate(size_t) [with  _Tp = double; _Alloc = std::allocator<double>; std::_Vector_base<_Tp, _Alloc>:: pointer = double*; size_t = long unsigned int]’ /usr/include/c++/11/bits/vector.tcc:440:44:   required from ‘void std::vector<_T p, _Alloc>::_M_realloc_insert(std::vector<_Tp, _Alloc>::iterator, _Args&& ...) [ with _Args = {const double&}; _Tp = double; _Alloc = std::allocator<double>; std ::vector<_Tp, _Alloc>::iterator = std::vector<double>::iterator]’ /usr/include/c++/11/bits/stl_vector.h:1198:21:   required from ‘void std::vector <_Tp, _Alloc>::push_back(const value_type&) [with _Tp = double; _Alloc = std::al locator<double>; std::vector<_Tp, _Alloc>::value_type = double]’ e6142c65-e356-436b-8584-7542390c4cea.cpp:20:22:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<double>’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- CPP_133.cpp
+++ CPP_133.cpp
@@ -1,19 +1,20 @@
+```cpp
 #include <iostream>
-#include <vector>
+#include <vector<double> >
 #include <cmath>
 
-int sum_squares(const std::vector<float>& lst) {
+int sum_squares(const std::vector<double>& lst) {
     int result = 0;
-    for (float x : lst) {
-        int ceil_x = ceil(x);
+    for (double x : lst) {
+        double ceil_x = ceil(x);
         result += pow(ceil_x, 2);
     }
     return result;
 }
 
 int main() {
-    std::vector<float> lst;
-    float num;
+    std::vector<double> lst;
+    double num;
 
     while (std::cin >> num) {
         lst.push_back(num);
@@ -22,4 +23,5 @@ int main() {
     int sum = sum_squares(lst);
 
     std::cout << "Sum of squares: " << sum << std::endl;
+
 }
