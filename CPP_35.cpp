68ef385174412490a36571f60959d1e33a28bb5b
beep boop <githubbot@vadim.me>
2024-07-07 00:44:40 +0200

    Solution 85 of CPP/35, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/dqp6iafu Prompt: 7b546936-dbd7-4358-aad2-2c0f4d6abb41.cpp:1:1: error: expected unqualified-id bef ore ‘return’     1 | return *max_element(l.begin(), l.end());       | ^~~~~~ In file included from 7b546936-dbd7-4358-aad2-2c0f4d6abb41.cpp:3: 7b546936-dbd7-4358-aad2-2c0f4d6abb41.cpp: In function ‘int main()’: 7b546936-dbd7-4358-aad2-2c0f4d6abb41.cpp:5:17: error: ‘max_element’ was not decl ared in this scope     5 |     assert (abs(max_element({5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10})- 12 4)<1e-4);       |                 ^~~~~~~~~~~ 7b546936-dbd7-4358-aad2-2c0f4d6abb41.cpp:5:13: error: ‘abs’ was not declared in this scope     5 |     assert (abs(max_element({5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10})- 12 4)<1e-4);       |             ^~~

--- CPP_35.cpp
+++ CPP_35.cpp
@@ -1,12 +1,7 @@
 #include <algorithm>
 #include <cmath>
 
-int findMax(int n) {
-    vector<int> l;
-    for (int i = 0; i < n; i++) {
-        int num;
-        cin >> num;
-        l.push_back(num);
-    }
+int findMax(int arr[], int n) {
+    vector<int> l(arr, arr+n);
     return *max_element(l.begin(), l.end());
 }
