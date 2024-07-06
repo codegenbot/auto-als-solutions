11995190b1ccdca4b9bd91af9361661f4c2c6c28
beep boop <githubbot@vadim.me>
2024-07-06 04:13:16 +0200

    Solution 60 of CPP/86, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/qsuhvrkf Prompt:  this scope    32 |     getline(cin, str);       |             ^~~ 4725b09d-4438-48b2-b011-77c082311403.cpp:32:13: note: ‘std::cin’ is defined in h eader ‘<iostream>’; did you forget to ‘#include <iostream>’? 4725b09d-4438-48b2-b011-77c082311403.cpp:34:52: error: ‘endl’ was not declared i n this scope    34 |     cout << "Antishuffled sentence: " << output << endl;       |                                                    ^~~~ 4725b09d-4438-48b2-b011-77c082311403.cpp:3:1: note: ‘std::endl’ is defined in he ader ‘<ostream>’; did you forget to ‘#include <ostream>’?     2 | #include <algorithm>   +++ |+#include <ostream>     3 | using namespace std; 4725b09d-4438-48b2-b011-77c082311403.cpp: At global scope: 4725b09d-4438-48b2-b011-77c082311403.cpp:39:5: error: redefinition of ‘int main( )’    39 | int main(){       |     ^~~~ 4725b09d-4438-48b2-b011-77c082311403.cpp:29:5: note: ‘int main()’ previously def ined here    29 | int main(){       |     ^~~~

--- CPP_86.cpp
+++ CPP_86.cpp
@@ -1,6 +1,8 @@
-#include <iostream>
+```
 #include <string>
 #include <algorithm>
+#include <iostream>
+
 using namespace std;
 
 string anti_shuffle(string s){
@@ -20,31 +22,18 @@ string anti_shuffle(string s){
         for(int k=0; k<word.size(); k++){
             result += word[k];
         }
-        sort(&word[0], &word[0] + word.size());
+        sort(word.begin(), word.end());
         result += word;
         i = j;
     }
     return result;
 }
 
-string anti_shuffle(string s){
-    string result = "";
-    int i = 0;
-    while(i<s.size()){
-        if(s[i] == ' '){
-            result += ' ';
-            i++;
-            continue;
-        }
-        int j = i + 1;
-        for(;j<s.size() && s[j] != ''; j++){
-            
-        }
-        string word = s.substr(i, j - i);
-        result.append(word);
-        sort(&word[0], &word[0] + word.size());
-        result += word;
-        i = j;
-    }
-    return result;
+int main(){
+    string str;
+    cout << "Enter a sentence: ";
+    getline(cin, str);
+    string output = anti_shuffle(str);
+    cout << "Antishuffled sentence: " << output << endl;
+    return 0;
 }
