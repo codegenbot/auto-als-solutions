3ec19b5001e5747a5af11cb182762089287c7527
beep boop <githubbot@vadim.me>
2024-07-06 02:53:55 +0200

    Solution 26 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/2p90vrr5 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5aff7669-bcc8-4c8b-aa8f-bb2c57fac e69.py", line 20, in <module>     check(encode)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5aff7669-bcc8-4c8b-aa8f-bb2c57fac e69.py", line 16, in check     encode("I DoNt KnOw WhAt tO WrItE") == "k dQnT kNqW wHcT Tq wRkTg" AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_93.py
+++ Python_93.py
@@ -3,10 +3,8 @@ def encode(message):
     result = ''
     for char in message:
         if char.isalpha():
-            if char.isupper():
-                result += chr((ord(char) - ord('A') + 2) % 26 + ord('A'))
-            else:
-                result += chr((ord(char) - ord('a') + 2) % 26 + ord('a'))
+            ascii_offset = ord('A') if char.isupper() else ord('a')
+            result += chr((ord(char) - ascii_offset + 3) % 26 + ascii_offset)
         else:
             result += char
     return result
