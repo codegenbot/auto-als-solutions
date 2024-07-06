5652b1afdc688bc011c76fa8398e8baed41d12c3
beep boop <githubbot@vadim.me>
2024-07-07 00:07:39 +0200

    Solution 76 of CPP/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/0ujhsbw6 Prompt: a223e73a-76d2-48f2-9b73-a8b3ae0b4dfd.cpp:20:13: error: ‘coeffs’ was not declared  in this scope; did you mean ‘coeff’?    20 |             coeffs.push_back(coeff);       |             ^~~~~~       |             coeff a223e73a-76d2-48f2-9b73-a8b3ae0b4dfd.cpp:23:30: error: ‘coeffs’ was not declared  in this scope; did you mean ‘ncoeff’?    23 |         solution = find_zero(coeffs);       |                              ^~~~~~       |                              ncoeff a223e73a-76d2-48f2-9b73-a8b3ae0b4dfd.cpp:23:36: error: ‘find_zero’ cannot be use d as a function    23 |         solution = find_zero(coeffs);       |                                    ^ In file included from a223e73a-76d2-48f2-9b73-a8b3ae0b4dfd.cpp:7: a223e73a-76d2-48f2-9b73-a8b3ae0b4dfd.cpp:24:21: error: ‘poly’ was not declared i n this scope    24 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                     ^~~~ a223e73a-76d2-48f2-9b73-a8b3ae0b4dfd.cpp:24:17: error: ‘abs’ was not declared in  this scope    24 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                 ^~~

--- CPP_32.cpp
+++ CPP_32.cpp
@@ -1,16 +1,21 @@
+```cpp
 #include <vector>
 #include <cmath>
 
-double poly(vector<double> coeffs, double x) {
+double poly(const std::vector<double>& coeffs, double x) {
     double result = 0;
-    for (int i = 0; i < coeffs.size(); i++) {
+    for (int i = 0; i < coeffs.size(); ++i) {
         result += coeffs[i] * pow(x, i);
     }
-    return abs(result - 1);
+    return result;
 }
 
-double find_zero(vector<double> coeffs, double solution){
-    double left = coeffs[0];
-    double right = coeffs[coeffs.size() - 1];
-    return (-left / right);
+double find_zero(std::vector<double> coeffs, double solution){
+    double left = poly(coeffs, solution);
+    double right = poly(coeffs, -solution);
+    if (std::abs(left) < std::abs(right)) {
+        return -solution;
+    } else {
+        return solution;
+    }
 }
