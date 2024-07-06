cf0a40e157108dcace359478f9730176268bcf9b
beep boop <githubbot@vadim.me>
2024-07-06 19:52:34 +0200

    Solution 1 of CPP/73, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/mpli604o Prompt: Complete the following code given the task description and function signature.

--- CPP_73.cpp
+++ CPP_73.cpp
@@ -11,10 +11,8 @@ int smallest_change(vector<int> arr) {
             changes++;
             if (str[left] == str[right - 1]) {
                 right--;
-            } else if (str[right] == str[left + 1]) {
-                left++;
             } else {
-                break;
+                left++;
             }
         } else {
             left++;
