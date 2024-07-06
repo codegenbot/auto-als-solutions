9f263f86a5b58ae3d50900821eb05ad066d7aeca
beep boop <githubbot@vadim.me>
2024-07-07 00:57:39 +0200

    Solution 17 of CPP/141, ratio 0.6923076923076923 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/teabhwu8 Prompt: Program failed

--- CPP_141.cpp
+++ CPP_141.cpp
@@ -27,8 +27,10 @@ string file_name_check(string file_name){
     size_t dot_pos = 0; 
     for (int i=0; i<file_name.length(); i++) { 
         if (file_name[i] == '.') { 
-            dot_pos=i; break; } 
-    } 
+            dot_pos=i; 
+            break; 
+        } 
+    }
     string after_dot = file_name.substr(dot_pos+1);
     string allowed_extensions[]={"txt","exe","dll"};
     bool found=false;
