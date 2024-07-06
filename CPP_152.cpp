1cba8f8a23bfed60cc0c6afa720c6710f804326b
beep boop <githubbot@vadim.me>
2024-07-06 08:58:48 +0200

    Solution 4 of CPP/152, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/iiv03sks Prompt:       |                    ^~~ 55371042-f9a4-46dd-83c0-c8046e980bac.cpp:16:27: error: ‘vector’ was not declared  in this scope    16 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 55371042-f9a4-46dd-83c0-c8046e980bac.cpp:16:34: error: expected primary-expressi on before ‘int’    16 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 55371042-f9a4-46dd-83c0-c8046e980bac.cpp:16:39: error: expression list treated a s compound expression in initializer [-fpermissive]    16 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 55371042-f9a4-46dd-83c0-c8046e980bac.cpp:15: 55371042-f9a4-46dd-83c0-c8046e980bac.cpp: In function ‘int main()’: 55371042-f9a4-46dd-83c0-c8046e980bac.cpp:25:20: error: ‘compare’ was not declare d in this scope    25 |     assert (issame(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));       |                    ^~~~~~~ 55371042-f9a4-46dd-83c0-c8046e980bac.cpp:25:19: error: ‘issame’ cannot be used a s a function    25 |     assert (issame(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_152.cpp
+++ CPP_152.cpp
@@ -1,12 +1,7 @@
 #include <vector>
-#include <algorithm>
 
-bool issame(std::vector<int> a, std::vector<int> b) {
-    return a == b;
-}
-
-std::vector<int> compare(std::vector<int> game, std::vector<int> guess) {
-    std::vector<int> result;
+vector<int> compare(vector<int> game, vector<int> guess) {
+    vector<int> result;
     for (int i = 0; i < game.size(); i++) {
         if (game[i] == guess[i]) {
             result.push_back(0);
@@ -15,9 +10,3 @@ std::vector<int> compare(std::vector<int> game, std::vector<int> guess) {
         }
     }
     return result;
-}
-
-int main() {
-    assert(std::issame(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));
-    return 0;
-}
