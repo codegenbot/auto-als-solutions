059d307ff370306ddec77005669ecbd64823b49f
beep boop <githubbot@vadim.me>
2024-07-06 11:56:34 +0200

    Solution 55 of CPP/133, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/nokt3pfm Prompt: /usr/include/c++/11/ext/new_allocator.h: In instantiation of ‘_Tp* __gnu_cxx::ne w_allocator<_Tp>::allocate(int, const void*) [with _Tp = float]’: /usr/include/c++/11/bits/alloc_traits.h:464:28:   required from ‘static _Tp* std ::allocator_traits<std::allocator<_CharT> >::allocate(std::allocator_traits<std: :allocator<_CharT> >::allocator_type&, int) [with _Tp = float; std::allocator_tr aits<std::allocator<_CharT> >::pointer = float*; std::allocator_traits<std::allo cator<_CharT> >::allocator_type = std::allocator<float>]’ /usr/include/c++/11/bits/stl_vector.h:346:33:   required from ‘std::_Vector_base <_Tp, _Alloc>::pointer std::_Vector_base<_Tp, _Alloc>::_M_allocate(size_t) [with  _Tp = float; _Alloc = std::allocator<float>; std::_Vector_base<_Tp, _Alloc>::po inter = float*; size_t = long unsigned int]’ /usr/include/c++/11/bits/vector.tcc:440:44:   required from ‘void std::vector<_T p, _Alloc>::_M_realloc_insert(std::vector<_Tp, _Alloc>::iterator, _Args&& ...) [ with _Args = {const float&}; _Tp = float; _Alloc = std::allocator<float>; std::v ector<_Tp, _Alloc>::iterator = std::vector<float>::iterator]’ /usr/include/c++/11/bits/stl_vector.h:1198:21:   required from ‘void std::vector <_Tp, _Alloc>::push_back(const value_type&) [with _Tp = float; _Alloc = std::all ocator<float>; std::vector<_Tp, _Alloc>::value_type = float]’ 972e94b0-cb09-4a1e-ae9f-615ba136106c.cpp:20:22:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<float>’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- CPP_133.cpp
+++ CPP_133.cpp
@@ -1,26 +1,8 @@
-```cpp
-#include <iostream>
-#include <vector>
-#include <cmath>
-
-long long sum_squares(const std::vector<float>& lst) {
+int sum_squares(const std::vector<float>& lst) {
     long long result = 0;
     for (float x : lst) {
-        long long ceil_x = std::lround(x);
+        long long ceil_x = ceil(x);
         result += pow(ceil_x, 2);
     }
     return result;
-}
-
-int main() {
-    std::vector<float> lst;
-    float num;
-
-    while (std::cin >> num) {
-        lst.push_back(num);
-    }
-
-    long long sum = sum_squares(lst);
-
-    std::cout << "Sum of squares: " << sum << std::endl;
 }
