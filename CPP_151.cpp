f7d1ae32fbd3ba308f4a46df8e4c07d654347c32
beep boop <githubbot@vadim.me>
2024-07-06 12:39:52 +0200

    Solution 70 of CPP/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/2iqw0vuf Prompt: 6959115d-085a-4f8b-a7dc-70f86b26a53f.cpp:24:5: error: redefinition of ‘int main( )’    24 | int main(){       |     ^~~~ 6959115d-085a-4f8b-a7dc-70f86b26a53f.cpp:16:5: note: ‘int main()’ previously def ined here    16 | int main() {       |     ^~~~ In file included from 6959115d-085a-4f8b-a7dc-70f86b26a53f.cpp:23: 6959115d-085a-4f8b-a7dc-70f86b26a53f.cpp: In function ‘int main()’: 6959115d-085a-4f8b-a7dc-70f86b26a53f.cpp:25:35: error: ‘lst’ was not declared in  this scope    25 |     assert (double_the_difference(lst) == odd_sum );       |                                   ^~~ 6959115d-085a-4f8b-a7dc-70f86b26a53f.cpp:25:43: error: ‘odd_sum’ was not declare d in this scope    25 |     assert (double_the_difference(lst) == odd_sum );       |                                           ^~~~~~~

--- CPP_151.cpp
+++ CPP_151.cpp
@@ -1,6 +1,7 @@
+```cpp
 #include <vector>
 #include <cmath>
-#include <iostream>
+#include <iostream> 
 using namespace std;
 
 long long double_the_difference(vector<double> lst) {
@@ -14,17 +15,8 @@ long long double_the_difference(vector<double> lst) {
 }
 
 int main() {
-    vector<double> lst;
-    int n;
-    cout << "Enter the number of elements: ";
-    cin >> n;
-    for(int i=0; i<n; i++) {
-        double num;
-        cout << "Enter element " << i+1 << ": ";
-        cin >> num;
-        lst.push_back(num);
-    }
-    long long odd_sum = double_the_difference(lst);
-    cout << "The sum of squares of odd elements is: " << odd_sum;
+    vector<double> vec({1.5, 2.5, -3.4, 4.7});
+    long long odd_sum = double_the_difference(vec);
+    cout << odd_sum;
     return 0;
 }
