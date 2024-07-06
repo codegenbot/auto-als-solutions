735846b4ef4a4624f534cc54bb3cbddff3ec0f1c
beep boop <githubbot@vadim.me>
2024-07-06 18:35:50 +0200

    Solution 3 of CPP/19, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/nabjymve Prompt: Complete the following code given the task description and function signature.

--- CPP_19.cpp
+++ CPP_19.cpp
@@ -1,33 +1,34 @@
 #include <algorithm>
-#include <string>
-
 using namespace std;
 
 string sort_numbers(string numbers) {
-    map<string, int> numMap = {{"zero", 0}, {"one", 1}, {"two", 2}, {"three", 3},
-                                {"four", 4}, {"five", 5}, {"six", 6}, {"seven", 7},
-                                {"eight", 8}, {"nine", 9}};
-    vector<string> nums;
-    string temp;
+    map<string, int> numMap = {
+        {"zero", 0}, {"one", 1}, {"two", 2}, {"three", 3},
+        {"four", 4}, {"five", 5}, {"six", 6}, {"seven", 7},
+        {"eight", 8}, {"nine", 9}
+    };
+
+    vector<string> numVec;
+    string num;
 
     for (int i = 0; i < numbers.length(); i++) {
         if (numbers[i] == ' ') {
             continue;
         }
-        temp += numbers[i];
-        if (i == numbers.length() - 1 || numbers[i + 1] != ' ') {
-            nums.push_back(temp);
-            temp = "";
+        num += numbers[i];
+        if (i + 1 >= numbers.length() || numbers[i + 1] != ' ') {
+            numVec.push_back(num);
+            num = "";
         }
     }
 
-    sort(nums.begin(), nums.end(), [&](string a, string b) {
-        return numMap[a] < numMap[b];
+    sort(numVec.begin(), numVec.end(), [&numMap](const string& a, const string& b) {
+        return numMap.at(a) < numMap.at(b);
     });
 
     string result;
-    for (const auto &num : nums) {
-        result += num + " ";
+    for (string& str : numVec) {
+        result += str + " ";
     }
     return result.substr(0, result.length() - 1);
 }
