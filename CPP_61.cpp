3978a05d7d0598b0253ef482cdae8b8778f0903b
beep boop <githubbot@vadim.me>
2024-07-06 02:11:50 +0200

    Solution 4 of CPP/61, ratio 0.8333333333333334 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/xmu7e595 Prompt: f172e1bf-cbca-49e8-a800-2978dbb1e1f3.cpp:1:25: error: ‘string’ was not declared in this scope     1 | bool correct_bracketing(string brackets){       |                         ^~~~~~ In file included from f172e1bf-cbca-49e8-a800-2978dbb1e1f3.cpp:10: f172e1bf-cbca-49e8-a800-2978dbb1e1f3.cpp: In function ‘int main()’: f172e1bf-cbca-49e8-a800-2978dbb1e1f3.cpp:12:36: error: ‘correct_bracketing’ cann ot be used as a function    12 |     assert (not (correct_bracketing("()()(()())()))()")));       |                  ~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~

--- CPP_61.cpp
+++ CPP_61.cpp
@@ -1,9 +1,9 @@
 #include <string>
-
-bool correct_bracketing(string brackets) {
+bool correct_bracketing(std::string brackets){
     int count = 0;
     for(char c : brackets){
         if(c == '(')count++;
         else if(c == ')')if(count > 0)count--;else return false;
     }
     return count == 0;
+}
