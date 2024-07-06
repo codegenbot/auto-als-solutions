650ec2268dfb1a3ed9dc973ce42ed87e7ccdcb61
beep boop <githubbot@vadim.me>
2024-07-06 13:35:58 +0200

    Solution 88 of CPP/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/2iqw0vuf Prompt:     1 | ```       |  ^ dfc3a353-2c12-4afb-974e-7fcaaed2fd48.cpp:1:3: error: stray ‘`’ in program     1 | ```       |   ^ dfc3a353-2c12-4afb-974e-7fcaaed2fd48.cpp:26:5: error: redefinition of ‘int main( )’    26 | int main(){       |     ^~~~ dfc3a353-2c12-4afb-974e-7fcaaed2fd48.cpp:18:5: note: ‘int main()’ previously def ined here    18 | int main() {       |     ^~~~ In file included from dfc3a353-2c12-4afb-974e-7fcaaed2fd48.cpp:25: dfc3a353-2c12-4afb-974e-7fcaaed2fd48.cpp: In function ‘int main()’: dfc3a353-2c12-4afb-974e-7fcaaed2fd48.cpp:27:35: error: ‘lst’ was not declared in  this scope    27 |     assert (double_the_difference(lst) == odd_sum );       |                                   ^~~ dfc3a353-2c12-4afb-974e-7fcaaed2fd48.cpp:27:43: error: ‘odd_sum’ was not declare d in this scope    27 |     assert (double_the_difference(lst) == odd_sum );       |                                           ^~~~~~~

--- CPP_151.cpp
+++ CPP_151.cpp
@@ -1,8 +1,6 @@
-```cpp
 #include <vector>
 #include <cmath>
 #include <iostream>
-#include <initializer_list>
 using namespace std;
 
 long long double_the_difference(vector<double> lst) {
@@ -19,7 +17,6 @@ int main() {
     vector<double> lst({1.5, 2.5, -3.4, 4.7});
     long long odd_sum = double_the_difference(lst);
     cout << odd_sum;
-    
-    assert (double_the_difference(lst) == odd_sum );
+    assert(double_the_difference(lst) == odd_sum); 
     return 0;
 }
