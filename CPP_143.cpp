280707c48104f92ebb192d78a745bc90577b89f1
beep boop <githubbot@vadim.me>
2024-07-06 23:34:00 +0200

    Solution 15 of CPP/143, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/qcdkg1yi Prompt: Complete the following code given the task description and function signature.

--- CPP_143.cpp
+++ CPP_143.cpp
@@ -1,20 +1,28 @@
 string words_in_sentence(string sentence){
     string result = "";
-    for (int i = 0; i < sentence.size(); ++i) {
-        if (sentence[i] == ' ') continue;
-        int len = 1;
-        for (int j = i+1; j <= sentence.size() && j != i; ++j) {
-            if (sentence[j] != ' ') len++;
-            else break;
+    for(string word : split(sentence, ' ')){
+        if(is_prime(word.length())){
+            result += word + " ";
         }
-        bool isPrime = true;
-        for (int k = 2; k * k <= len; ++k) {
-            if (len % k == 0) {
-                isPrime = false;
-                break;
-            }
-        }
-        if (isPrime) result += sentence.substr(i, len) + " ";
     }
-    return result;
+    return result.substr(0, result.size()-1);
+}
+
+vector<string> split(string str, char ch) {
+    vector<string> ar;
+    size_t pos = 0;
+    while((pos = str.find(ch)) != string::npos) {
+        ar.push_back(str.substr(0, pos));
+        str.erase(0, pos + 1);
+    }
+    ar.push_back(str);
+    return ar;
+}
+
+bool is_prime(int num){
+    if(num <= 1) return false;
+    for(int i = 2; i*i <= num; i++){
+        if(num%i == 0) return false;
+    }
+    return true;
 }
