07b112459817ce1f750df4d9efbb0579c1040ad2
beep boop <githubbot@vadim.me>
2024-07-06 23:02:14 +0200

    Solution 40 of CPP/110, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/p3jebju1 Prompt: bca5d5a1-a9d2-42cf-938c-cb6c458c97e3.cpp:1:13: error: ‘vector’ was not declared in this scope     1 | bool hasOdd(vector<int>& vec) {       |             ^~~~~~ bca5d5a1-a9d2-42cf-938c-cb6c458c97e3.cpp:1:20: error: expected primary-expressio n before ‘int’     1 | bool hasOdd(vector<int>& vec) {       |                    ^~~ bca5d5a1-a9d2-42cf-938c-cb6c458c97e3.cpp:9:1: error: ‘string’ does not name a ty pe     9 | string exchange(vector<int> lst1, vector<int> lst2) {       | ^~~~~~ In file included from bca5d5a1-a9d2-42cf-938c-cb6c458c97e3.cpp:25: bca5d5a1-a9d2-42cf-938c-cb6c458c97e3.cpp: In function ‘int main()’: bca5d5a1-a9d2-42cf-938c-cb6c458c97e3.cpp:27:13: error: ‘exchange’ was not declar ed in this scope    27 |     assert (exchange({100, 200}, {200, 200}) == "YES");       |             ^~~~~~~~

--- CPP_110.cpp
+++ CPP_110.cpp
@@ -25,16 +25,3 @@ string exchange(vector<int> lst1, vector<int> lst2) {
     }
     return "YES";
 }
-
-int main() {
-    int n, m;
-    cin >> n >> m;
-    vector<int> v1(n), v2(m);
-    for (int i = 0; i < n; i++) {
-        cin >> v1[i];
-    }
-    for (int i = 0; i < m; i++) {
-        cin >> v2[i];
-    }
-    cout << exchange(v1, v2) << endl;
-}
