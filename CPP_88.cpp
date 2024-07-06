ec33c991f7b38024988f8b3d24bf5071aab6ae6f
beep boop <githubbot@vadim.me>
2024-07-06 07:16:22 +0200

    Solution 77 of CPP/88, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/cgpdw3dk Prompt: 620a743f-8eab-498f-9570-a80fee3428e1.cpp:18:5: note: ‘int main()’ previously def ined here    18 | int main() {       |     ^~~~ In file included from 620a743f-8eab-498f-9570-a80fee3428e1.cpp:25: 620a743f-8eab-498f-9570-a80fee3428e1.cpp: In function ‘int main()’: 620a743f-8eab-498f-9570-a80fee3428e1.cpp:35:30: error: cannot bind non-const lva lue reference of type ‘std::vector<int>&’ to an rvalue of type ‘std::vector<int> ’    35 |     assert (issame(sort_array({21, 14, 23, 11}) , {23, 21, 14, 11}));       |                    ~~~~~~~~~~^~~~~~~~~~~~~~~~~~ In file included from /usr/include/c++/11/vector:67,                  from 620a743f-8eab-498f-9570-a80fee3428e1.cpp:2: /usr/include/c++/11/bits/stl_vector.h:625:7: note:   after user-defined conversi on: ‘std::vector<_Tp, _Alloc>::vector(std::initializer_list<_Tp>, const allocato r_type&) [with _Tp = int; _Alloc = std::allocator<int>; std::vector<_Tp, _Alloc> ::allocator_type = std::allocator<int>]’   625 |       vector(initializer_list<value_type> __l,       |       ^~~~~~ 620a743f-8eab-498f-9570-a80fee3428e1.cpp:14:35: note:   initializing argument 1 of ‘void sort_array(std::vector<int>&)’    14 | void sort_array(std::vector<int>& array) {       |                 ~~~~~~~~~~~~~~~~~~^~~~~

--- CPP_88.cpp
+++ CPP_88.cpp
@@ -1,6 +1,6 @@
 int main() {
     std::vector<int> array1 = {21, 14, 23, 11};
     std::vector<int> array2 = {23, 21, 14, 11};
-    assert (issame(array1, array2));
+    assert(issame({21, 14, 23, 11}, sort_array({21, 14, 23, 11})));
     sort_array(array1);
 }
