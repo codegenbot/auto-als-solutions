d5509f2f32240d25bef8795a8fbc8ea90b0aa489
beep boop <githubbot@vadim.me>
2024-07-06 23:12:50 +0200

    Solution 2 of CPP/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/5msl6t64 Prompt: Complete the following code given the task description and function signature.

--- CPP_10.cpp
+++ CPP_10.cpp
@@ -1,4 +1,6 @@
+#include <iostream>
 #include <string>
+
 using namespace std;
 
 bool is_palindrome(string str) {
@@ -6,13 +8,19 @@ bool is_palindrome(string str) {
     return s==str;
 }
 
-string make_palindrome(string str) {
-    int i = 0, j = str.length() - 1;
-    while (i <= j && str[i] == str[j]) {
-        i++;
-        j--;
-    }
-    string rev = str.substr(i);
-    reverse(rev.begin(), rev.end());
-    return str + rev;
+string make_palindrome(string str){
+    int i=str.length()-1;
+    while(i>=0 && str[i]==str[0])
+        i--;
+    if(i<0)
+        return str+str;
+    return str.substr(0,i+1)+str.substr(i+1, str.length()-i-1).reverse();
+}
+
+int main() {
+    string s;
+    cout<<"Enter a string: ";
+    cin>>s;
+    cout << "The shortest palindrome that begins with the supplied string is: "<<make_palindrome(s)<<endl;
+    return 0;
 }
