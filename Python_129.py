8d87c8076463ea7f9a74e6bec9c5d5873a87aa2d
beep boop <githubbot@vadim.me>
2024-07-06 16:32:51 +0200

    Solution 92 of Python/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/j3m9w7h7 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a71f24e3-d080-4487-ac04-bc70132a6 0dc.py", line 28, in <module>     check(minPath)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a71f24e3-d080-4487-ac04-bc70132a6 0dc.py", line 25, in check     assert minPath([[1, 3], [3, 2]], 10) == [1, 3, 1, 3, 1, 3, 1, 3, 1, 3]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_129.py
+++ Python_129.py
@@ -3,20 +3,22 @@ def minPath(grid, k):
     n = len(grid)
     res = []
 
-    def dfs(i, j, path, sum_path):
+    def dfs(i, j, path, total):
         nonlocal res
         if len(path) == k:
-            if not res or sum_path < sum(res):
-                res = path
+            if not res or total < sum(res[-1]):
+                res = [path]
+            elif total == sum(res[-1]):
+                res.append(path)
             return
 
         for x, y in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
             ni, nj = i + x, j + y
             if 0 <= ni < n and 0 <= nj < n and (ni, nj) not in path:
-                dfs(ni, nj, path + [(n * i) + j], sum_path + grid[ni][nj])
+                dfs(ni, nj, path + [(n * i) + j], total + grid[ni][nj])
 
     for i in range(n):
         for j in range(n):
             dfs(i, j, [], 0)
 
-    return res
+    return res[0]
