4a062e132ce577341530d318823f68df98209f20
beep boop <githubbot@vadim.me>
2024-07-06 03:06:05 +0200

    Solution 97 of Python/50, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/8ei1er5a Prompt: Program did not receive expected input

--- Python_50.py
+++ Python_50.py
@@ -8,18 +8,25 @@ def decode_shift(s: str):
     for ch in s:
         if not ch.isalpha():
             decoded_str += ch
+            shift = 1
         elif 'a' <= ch <= 'z':
             if shift:
-                decoded_str += chr((ord(ch)-97+26-key)%26 + 97) if ch <= 'm' else chr((ord(ch)-101+26-key)%26 + 101)
+                if ord(ch)-97+key <= 26:
+                    decoded_str += chr((ord(ch)-97+key)%26 + 97)
+                else:
+                    decoded_str += chr(96+(ord(ch)-97+key)%26) 
+                shift = 0
             else:
-                decoded_str += chr(ord(ch))
-            shift = 0
+                decoded_str += ch
         elif 'A' <= ch <= 'Z':
             if shift:
-                decoded_str += chr((ord(ch)-65+26-key)%26 + 65) if ch <= 'M' else chr((ord(ch)-69+26-key)%26 + 69)
+                if ord(ch)-65+key <= 26:
+                    decoded_str += chr((ord(ch)-65+key)%26 + 65)
+                else:
+                    decoded_str += chr(90+(ord(ch)-65+key)%26) 
+                shift = 0
             else:
-                decoded_str += chr(ord(ch))
-            shift = 0
+                decoded_str += ch
         else: 
             decoded_str += ch
     
