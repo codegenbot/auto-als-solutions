5cf4f9064f6efb2def4111bf5369d549b25e58ae
beep boop <githubbot@vadim.me>
2024-07-06 02:21:30 +0200

    Solution 22 of CPP/72, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/bg9uit55 Prompt:                  from /usr/include/c++/11/bits/stl_algobase.h:64,                  from /usr/include/c++/11/bits/char_traits.h:39,                  from /usr/include/c++/11/string:40,                  from dc466c9f-a04d-450a-80de-9b0d85dcce85.cpp:2: /usr/include/c++/11/type_traits: In instantiation of ‘struct std::is_destructibl e<int>’: /usr/include/c++/11/bits/stl_construct.h:188:51:   required from ‘void std::_Des troy(_ForwardIterator, _ForwardIterator) [with _ForwardIterator = int*]’ /usr/include/c++/11/bits/alloc_traits.h:848:15:   required from ‘void std::_Dest roy(_ForwardIterator, _ForwardIterator, std::allocator<_T2>&) [with _ForwardIter ator = int*; _Tp = int]’ /usr/include/c++/11/bits/stl_vector.h:680:15:   required from ‘std::vector<_Tp, _Alloc>::~vector() [with _Tp = int; _Alloc = std::allocator<int>]’ dc466c9f-a04d-450a-80de-9b0d85dcce85.cpp:28:23:   required from here /usr/include/c++/11/type_traits:885:52: error: static assertion failed: template  argument must be a complete class or an unbounded array   885 |       static_assert(std::__is_complete_or_unbounded(__type_identity<_Tp> {}),       |                     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~ ~~~ /usr/include/c++/11/type_traits:885:52: note: ‘std::__is_complete_or_unbounded<s td::__type_identity<int> >((std::__type_identity<int>{}, std::__type_identity<in t>()))’ evaluates to false

--- CPP_72.cpp
+++ CPP_72.cpp
@@ -1,28 +1,40 @@
-```cpp
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <cassert>
 
 using namespace std;
-
-bool isPalindrome(string s) {
-    string rev = s;
-    reverse(rev.begin(), rev.end());
-    return s == rev;
-}
 
 bool will_it_fly(vector<int> q, int w) {
     string str = "";
     for (int i : q) {
         str += to_string(i);
     }
-    if (!isPalindrome(str)) return false;
-    int sum = 0;
+    long long sum = 0;
     for (int i : q) {
         sum += i;
     }
-    return sum <= w;
+    bool isRevSumPalindrome = false;
+    long long revSum = 0;
+    while(sum > 0) {
+        revSum = 0;
+        int temp = sum;
+        while(temp > 0) {
+            revSum = revSum * 10 + temp % 10;
+            temp /= 10;
+        }
+        if(revSum == sum) {
+            isRevSumPalindrome = true;
+            break;
+        }
+        sum = revSum;
+    }
+    return (isRevSumPalindrome && stoll(str) <= w);
+}
+
+bool isPalindrome(string s) {
+    string rev = s;
+    reverse(rev.begin(), rev.end());
+    return s == rev;
 }
 
 int main() {
