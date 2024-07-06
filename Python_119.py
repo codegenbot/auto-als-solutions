2c2d430f5fce081950a610479992c0ec0cd9d724
beep boop <githubbot@vadim.me>
2024-07-06 05:04:05 +0200

    Solution 49 of Python/119, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/629l2qg8 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/033097eb-8603-4193-a7fb-6b4cfe331 799.py", line 25, in <module>     check(match_parens)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/033097eb-8603-4193-a7fb-6b4cfe331 799.py", line 22, in check     assert match_parens([")", "("]) == "Yes"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_119.py
+++ Python_119.py
@@ -7,10 +7,10 @@ def match_parens(lst):
         for char in string:
             if char == '(':
                 open_count += 1
-                close_count = 0  
+                close_count = 0  # reset the count of closing parentheses
             elif char == ')':
                 close_count += 1
-                if close_count > open_count:  
+                if close_count > open_count:  # check if they are matched correctly
                     matching = False
                     break
         
