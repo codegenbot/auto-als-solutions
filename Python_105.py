9cd0a0db1769f0df3cd09b3ce289a88e12fbe9fb
beep boop <githubbot@vadim.me>
2024-07-06 03:40:36 +0200

    Solution 66 of Python/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/ubalkc5i Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/cf30c7e4-451b-4a91-ad8c-2f6143d87 335.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_105.py
+++ Python_105.py
@@ -1,5 +1,3 @@
-```
 def by_length(arr):
-    name_number = {1: "One", 2: "Two", 3: "Three", 4: "Four", 5: "Five", 
-                   6: "Six", 7: "Seven", 8: "Eight", 9: "Nine"}
-    return [name_number[i] for i in arr if str(i).startswith('1')]
+    mapping = {1: "One", 2: "Two", 3: "Three", 4: "Four", 5: "Five", 6: "Six", 7: "Seven", 8: "Eight", 9: "Nine"}
+    return [i for i in arr if len(mapping[i]) >= 4]
