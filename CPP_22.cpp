f7540b5389b8db5d60aecf620e90a113a1006522
beep boop <githubbot@vadim.me>
2024-07-06 02:41:24 +0200

    Solution 47 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/y2phiwh4 Prompt:  in this scope    13 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ cbd4952c-9207-4481-9f78-3b88e5d062bb.cpp:13:34: error: expected primary-expressi on before ‘int’    13 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ cbd4952c-9207-4481-9f78-3b88e5d062bb.cpp:13:39: error: expression list treated a s compound expression in initializer [-fpermissive]    13 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from cbd4952c-9207-4481-9f78-3b88e5d062bb.cpp:12: cbd4952c-9207-4481-9f78-3b88e5d062bb.cpp: In function ‘int main()’: cbd4952c-9207-4481-9f78-3b88e5d062bb.cpp:22:20: error: ‘filter_integers’ was not  declared in this scope    22 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |                    ^~~~~~~~~~~~~~~ cbd4952c-9207-4481-9f78-3b88e5d062bb.cpp:22:19: error: ‘issame’ cannot be used a s a function    22 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -1,37 +1,18 @@
 #include <boost/any.hpp>
-#include <iostream>
+#include <vector>
+#include <list>
 
-bool issame(const std::vector<int>& a, const std::vector<int>& b) {
-    if(a.size() != b.size())
-        return false;
-    for(int i=0; i<a.size(); i++) {
-        if(a[i] != b[i])
-            return false;
-    }
-    return true;
-}
-
-int main() {
-    // This will be the input values
-    std::list<boost::any> values;
-
-    // Your code to add values to 'values' list
-    
-    // Print out the result
-    std::vector<int> result = filter_integers(values);
-    for(int i: result) {
-        std::cout << i;
-    }
-    
-    return 0;
-}
+using boost::any_cast;
+typedef long long OInt;
 
 std::vector<int> filter_integers(std::list<boost::any> values) {
     std::vector<int> result;
     for (const auto& value : values) {
-        boost::any_cast<oint>(value).unwrap_or(0);
-        if(boost::any_cast<oint>(value)) {
-            result.push_back(boost::any_cast<int>(value));
+        if(any_cast<OInt>(value)) {
+            int num = any_cast<int>(value);
+            if(num != 0) {
+                result.push_back(num);
+            }
         }
     }
     return result;
