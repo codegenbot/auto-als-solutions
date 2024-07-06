68c0f50f22274506d126b503fde8db6d7b1feb60
beep boop <githubbot@vadim.me>
2024-07-06 19:58:34 +0200

    Solution 23 of CPP/64, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/2wtb9il4 Prompt: f0311122-033d-4ec9-9bd0-04ce55a6f983.cpp:1:18: error: ‘string’ was not declared in this scope     1 | int vowels_count(string s) {       |                  ^~~~~~ In file included from f0311122-033d-4ec9-9bd0-04ce55a6f983.cpp:13: f0311122-033d-4ec9-9bd0-04ce55a6f983.cpp: In function ‘int main()’: f0311122-033d-4ec9-9bd0-04ce55a6f983.cpp:15:25: error: ‘vowels_count’ cannot be used as a function    15 |     assert (vowels_count("ACEDY") == 3);       |             ~~~~~~~~~~~~^~~~~~~~~

--- CPP_64.cpp
+++ CPP_64.cpp
@@ -5,7 +5,7 @@ int vowels_count(std::string s) {
     for (char c : s) {
         if ((c == 'a') || (c == 'e') || (c == 'i') || (c == 'o') || (c == 'u')) {
             count++;
-        } else if (c == 'y' && std::find(s.begin(), s.end(), 'y') != s.end() && std::find(s.begin(), s.end(), 'y') != s.end()) {
+        } else if (c == 'y' && std::find(s.begin(), s.end(), 'y') != s.end() && std::distance(std::next(std::find(s.begin(), s.end(), 'y'), 0), s.end()) > 1) {
             count++;
         }
     }
