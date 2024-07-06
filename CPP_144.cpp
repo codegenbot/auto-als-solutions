543c51feb981911f3408649964d8d880277b80f5
beep boop <githubbot@vadim.me>
2024-07-07 00:01:50 +0200

    Solution 16 of CPP/144, ratio 0.7692307692307693 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/q7aqeoe1 Prompt: Program failed

--- CPP_144.cpp
+++ CPP_144.cpp
@@ -1,26 +1,18 @@
-#include<string>
+#include <string>
+#include <sstream>
+
 using namespace std;
 
-int gcd(int a,int b){
-    if(b==0)return a;
-    return gcd(b,a%b);
-}
-
-bool simplify(string x,string n){
-    int numer1 = stoi(split(x)[0]);
-    int denom1 = stoi(split(x)[2]);
-
-    int numer2 = stoi(split(n)[0]);
-    int denom2 = stoi(split(n)[2]);
-
-    if(denom1*denom2 == 0) return false;
-
-    int greatest = gcd(gcd(numer1,denom1),gcd(numer2,denom2));
-
-    return (numer1/denom1)*(denom2/greatest) == (numer2/greatest);
-}
-
-string split(string s){
-    size_t found=s.find("/");
-    return s.substr(0(found));
+bool simplify(string x, string n) {
+    int a = 0, b = 1, c = 0, d = 1;
+    stringstream s(x);
+    char temp;
+    s >> a >> temp >> b;
+    s.clear();
+    s.str(n);
+    s >> c >> temp >> d;
+    if (b*d == a*c)
+        return true;
+    else
+        return false;
 }
