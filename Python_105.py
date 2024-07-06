60ebd969d51a6106b0c855f33cb9992b674fca0e
beep boop <githubbot@vadim.me>
2024-07-06 03:14:48 +0200

    Solution 39 of Python/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/ubalkc5i Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/20af99c3-43c1-4fd0-a24b-e5ccd2785 986.py", line 13, in <module>     check(by_length)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/20af99c3-43c1-4fd0-a24b-e5ccd2785 986.py", line 10, in check     assert by_length([9, 4, 8]) == ["Nine", "Eight", "Four"]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_105.py
+++ Python_105.py
@@ -1,14 +1,17 @@
 number_names = {
-    1: "One",
-    2: "Two",
-    3: "Three",
-    4: "Four",
-    5: "Five",
-    6: "Six",
-    7: "Seven",
-    8: "Eight",
-    9: "Nine",
-    10: "Ten",
+    i: name
+    for i, name in [
+        (1, "One"),
+        (2, "Two"),
+        (3, "Three"),
+        (4, "Four"),
+        (5, "Five"),
+        (6, "Six"),
+        (7, "Seven"),
+        (8, "Eight"),
+        (9, "Nine"),
+        (10, "Ten"),
+    ]
 }
 
 
