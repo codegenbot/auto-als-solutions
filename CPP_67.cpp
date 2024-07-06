ebd6b2e6e96ced4aa6dfc29bcfe3f4d58ba45c40
beep boop <githubbot@vadim.me>
2024-07-06 19:41:54 +0200

    Solution 1 of CPP/67, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/juuz10le Prompt: Complete the following code given the task description and function signature.

--- CPP_67.cpp
+++ CPP_67.cpp
@@ -1,21 +1,29 @@
-#include <stdio.h>
-#include <string>
-using namespace std;
-
 int fruit_distribution(string s, int n) {
-    string input = s;
-    int total_fruits = n;
     size_t pos = 0;
-    int apples = 0;
-    int oranges = 0;
+    string apples = "apples";
+    string oranges = "oranges";
 
-    while ((pos = input.find(" ")) != string::npos) {
-        if (input.substr(0, pos).find("apples") != string::npos)
-            apples = stoi(input.substr(0, pos).substr(input.find(" ") + 1));
-        else if (input.substr(0, pos).find("oranges") != string::npos)
-            oranges = stoi(input.substr(0, pos).substr(input.find(" ") + 1));
-        input.erase(0, pos + 1);
+    // Find the number of apples
+    pos = s.find(apples);
+    int appleCount = 0;
+    if (pos != string::npos) {
+        int start = pos + apples.length();
+        while (start < s.length() && isdigit(s[start])) {
+            appleCount = appleCount * 10 + (s[start] - '0');
+            start++;
+        }
     }
 
-    return total_fruits - apples - oranges;
+    // Find the number of oranges
+    pos = s.find(oranges);
+    int orangeCount = 0;
+    if (pos != string::npos) {
+        int start = pos + oranges.length();
+        while (start < s.length() && isdigit(s[start])) {
+            orangeCount = orangeCount * 10 + (s[start] - '0');
+            start++;
+        }
+    }
+
+    return n - appleCount - orangeCount;
 }
