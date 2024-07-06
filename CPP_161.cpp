07629044ca572ffb419a4b5a2e77a6ef7cdfa601
beep boop <githubbot@vadim.me>
2024-07-06 11:10:40 +0200

    Solution 84 of CPP/161, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/yr1nyzw7 Prompt: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~ In file included from /usr/include/c++/11/bits/stl_algobase.h:67,                  from /usr/include/c++/11/bits/char_traits.h:39,                  from /usr/include/c++/11/string:40,                  from 49220723-3149-4b2e-afa2-be956986f217.cpp:2: /usr/include/c++/11/bits/stl_iterator.h: In instantiation of ‘class __gnu_cxx::_ _normal_iterator<char*, std::__cxx11::basic_string<char> >’: /usr/include/c++/11/bits/basic_string.tcc:225:25:   required from ‘void std::__c xx11::basic_string<_CharT, _Traits, _Alloc>::_M_construct(_InIterator, _InIterat or, std::forward_iterator_tag) [with _FwdIterator = const char*; _CharT = char; _Traits = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:539:14:   required from ‘std::__cxx11::b asic_string<_CharT, _Traits, _Alloc>::basic_string(const _CharT*, const _Alloc&)  [with <template-parameter-2-1> = std::allocator<char>; _CharT = char; _Traits =  std::char_traits<char>; _Alloc = std::allocator<char>]’ 49220723-3149-4b2e-afa2-be956986f217.cpp:24:5:   required from here /usr/include/c++/11/bits/stl_iterator.h:1015:57: error: no type named ‘differenc e_type’ in ‘struct std::iterator_traits<char*>’  1015 |       typedef typename __traits_type::difference_type   difference_type;       |                                                         ^~~~~~~~~~~~~~~

--- CPP_161.cpp
+++ CPP_161.cpp
@@ -1,22 +1,3 @@
-#include <iostream>
-#include <string>
-
-void solve(const char* input) {
-    std::cout << "Reversed: ";
-    for(int i = strlen(input)-1; i >=0 ; i--) {
-        std::cout << input[i];
-    }
-    std::cout << std::endl;
-}
-
-int main() {
-    std::cout << "Enter a string: ";
-    std::string input;
-    std::getline(std::cin, input);
-    if (input.empty()) {
-        std::cout << "Error! Please enter a valid input." << std::endl;
-    } else {
-        solve(input.c_str());
-    }
-    return 0;
+std::string solve(std::string input) {
+    return input;
 }
