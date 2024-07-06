7e47de532e4d50a6d9058935e3abc739c1ba5c42
beep boop <githubbot@vadim.me>
2024-07-07 01:33:39 +0200

    Solution 24 of CPP/141, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/teabhwu8 Prompt: Program failed

--- CPP_141.cpp
+++ CPP_141.cpp
@@ -1,47 +1,8 @@
-#include <cctype>
-#include<string>
-using namespace std;
-
-string file_name_check(string file_name){
-    int count_dot = 0;
-    bool starts_with_letter = false;
-    int digit_count = 0;
-    
-    for(int i=0; i<file_name.length(); i++){
-        if(file_name[i] == '.'){
-            count_dot++;
-        }
-        else if(isdigit(file_name[i])){
-            digit_count++;
-        }
-        else if(i==0){
-            starts_with_letter=true;
-        }
-        
-        if(count_dot > 1 || (starts_with_letter && !isalpha(file_name[0])) 
-          || digit_count > 3) return "No";
+size_t dot_pos = 0;
+for (int i=0; i<file_name.length(); i++) {
+    if (file_name[i] == '.') {
+        dot_pos=i;
+        break;
     }
-    
-    if(count_dot != 1) return "No";
-    
-    size_t dot_pos = 0; 
-    for (int i=0; i<file_name.length(); i++) { 
-        if (file_name[i] == '.') { 
-            dot_pos=i; 
-            break; 
-        } 
-    }
-    string after_dot = file_name.substr(dot_pos+1);
-    string allowed_extensions[]={"txt","exe","dll"};
-    bool found=false;
-    for(int i=0; i<3; i++){
-        if(after_dot == allowed_extensions[i]){
-            found=true;
-            break;
-        }
-    }
-    
-    if(!found) return "No";
-    
-    return "Yes";
 }
+string after_dot = file_name.substr(dot_pos+1);
