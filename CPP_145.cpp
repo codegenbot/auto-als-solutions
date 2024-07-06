4afdbd0baacf2bf3920fddee6f9d794b000c83ea
beep boop <githubbot@vadim.me>
2024-07-06 13:01:58 +0200

    Solution 93 of CPP/145, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/pmi4gz2f Prompt:    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 6f0f2c25-8573-4a26-89a5-88e4295d4cc0.cpp:22:34: error: expected primary-expressi on before ‘int’    22 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 6f0f2c25-8573-4a26-89a5-88e4295d4cc0.cpp:22:39: error: expression list treated a s compound expression in initializer [-fpermissive]    22 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 6f0f2c25-8573-4a26-89a5-88e4295d4cc0.cpp:21: 6f0f2c25-8573-4a26-89a5-88e4295d4cc0.cpp: In function ‘int main()’: 6f0f2c25-8573-4a26-89a5-88e4295d4cc0.cpp:31:20: error: ‘order_by_points’ was not  declared in this scope    31 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |                    ^~~~~~~~~~~~~~~ 6f0f2c25-8573-4a26-89a5-88e4295d4cc0.cpp:31:19: error: ‘issame’ cannot be used a s a function    31 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~

--- CPP_145.cpp
+++ CPP_145.cpp
@@ -1,10 +1,8 @@
-```cpp
 #include <vector>
 #include <algorithm>
 
 std::vector<int> sortByDigitSum(std::vector<int> arr) {
-    std::vector<int> res = arr;
-    sort(res.begin(), res.end(), [](int a, int b)->bool{
+    std::sort(arr.begin(), arr.end(), [](int a, int b)->bool{
         long long sum1 = 0, sum2 = 0;
         while(a > 0){
             sum1 += a % 10;
@@ -16,5 +14,5 @@ std::vector<int> sortByDigitSum(std::vector<int> arr) {
         }
         return sum1 < sum2;
     });
-    return res;
+    return arr;
 }
