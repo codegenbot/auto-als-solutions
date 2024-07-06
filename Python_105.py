c8cc2057b37a024a1371522c09c5008c123035f1
beep boop <githubbot@vadim.me>
2024-07-06 02:52:25 +0200

    Solution 11 of Python/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/ubalkc5i Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/df8df1d4-7176-4fb2-a3a7-16c22f6f1 fa0.py", line 15, in <module>     check(by_length)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/df8df1d4-7176-4fb2-a3a7-16c22f6f1 fa0.py", line 12, in check     assert by_length([9, 4, 8]) == ["Nine", "Eight", "Four"]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_105.py
+++ Python_105.py
@@ -1,2 +1,10 @@
 def by_length(arr):
-    return [name for num, name in zip(sorted(map(str, arr)), ["One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"]) if set(name[0]) == set(str(num)[0])]
+    result = sorted([i for i in arr if 1 <= i <= 9])
+    return [
+        name
+        for num, name in zip(
+            sorted(map(str, arr)),
+            ["One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"],
+        )
+        if set(name[0]) == set(str(num)[0])
+    ]
