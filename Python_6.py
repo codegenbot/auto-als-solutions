01c72dbef17e5ce396a6dbfaf530259f6e15f308
beep boop <githubbot@vadim.me>
2024-07-06 02:48:03 +0200

    Solution 63 of Python/6, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/6nuzx6oe Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7dfdbf4c-15c3-4de6-b6be-6faf2fa01 e45.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_6.py
+++ Python_6.py
@@ -2,8 +2,7 @@
 from typing import List
 
 def parse_nested_parens(paren_string: str) -> List[int]:
-"""Parse a string of nested parentheses into an integer list where '1' means opening parenthesis and '-1' means closing parenthesis.
-"""
+"""Return a list of integers representing the nesting level of parentheses in paren_string."""
     result = []
     level = 0
     for char in paren_string:
