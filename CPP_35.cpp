90bafa238cd5389ace0a0ba6ef9570f6fa8e371f
beep boop <githubbot@vadim.me>
2024-07-07 00:45:34 +0200

    Solution 88 of CPP/35, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/dqp6iafu Prompt: 7b546936-dbd7-4358-aad2-2c0f4d6abb41.cpp:1:1: error: expected unqualified-id bef ore ‘return’     1 | return *max_element(l.begin(), l.end());       | ^~~~~~ In file included from 7b546936-dbd7-4358-aad2-2c0f4d6abb41.cpp:3: 7b546936-dbd7-4358-aad2-2c0f4d6abb41.cpp: In function ‘int main()’: 7b546936-dbd7-4358-aad2-2c0f4d6abb41.cpp:5:17: error: ‘max_element’ was not decl ared in this scope     5 |     assert (abs(max_element({5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10})- 12 4)<1e-4);       |                 ^~~~~~~~~~~ 7b546936-dbd7-4358-aad2-2c0f4d6abb41.cpp:5:13: error: ‘abs’ was not declared in this scope     5 |     assert (abs(max_element({5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10})- 12 4)<1e-4);       |             ^~~

--- CPP_35.cpp
+++ CPP_35.cpp
@@ -1,6 +1,6 @@
 #include <algorithm>
 #include <cmath>
 
-int maxAbsoluteValue(vector<int> l) {
-    return *max_element(l.begin(), l.end());
+int largestAbsoluteValue(vector<int> l) {
+    return *max_element(l.begin(), l.end()) >= 0 ? *max_element(l.begin(), l.end()) : -(*max_element(l.begin(), l.end()));
 }
