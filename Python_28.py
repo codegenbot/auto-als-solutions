f5bbad94f9e974bfe13c603c9d08f6b15ce42dcb
beep boop <githubbot@vadim.me>
2024-07-06 03:58:24 +0200

    Solution 91 of Python/28, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/o6qupeag Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/fbef3f6a-16d4-412f-b36e-bf52e3049 343.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_28.py
+++ Python_28.py
@@ -5,7 +5,7 @@ while True:
     if inp.lower() == 'stop':
         break
     for s in inp.split():
-        if not set(s).issuperset({'!': 1, '?': 1, ',': 1}):
+        if not set(s).issuperset({'!', '?', ','}):
             continue
         strings += [s.strip()]
 print(' '.join(filter(None, strings)))
