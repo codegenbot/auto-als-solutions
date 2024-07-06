0158bf93c943265b54a547078b2b546e1236a8ca
beep boop <githubbot@vadim.me>
2024-07-06 03:36:43 +0200

    Solution 39 of CPP/86, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/qsuhvrkf Prompt: xx11::basic_string<char> >; _Compare = __gnu_cxx::__ops::_Iter_less_iter]’ /usr/include/c++/11/bits/stl_algo.h:1933:27:   required from ‘void std::__intros ort_loop(_RandomAccessIterator, _RandomAccessIterator, _Size, _Compare) [with _R andomAccessIterator = __gnu_cxx::__normal_iterator<char*, std::__cxx11::basic_st ring<char> >; _Size = long int; _Compare = __gnu_cxx::__ops::_Iter_less_iter]’ /usr/include/c++/11/bits/stl_algo.h:1954:25:   required from ‘void std::__sort(_ RandomAccessIterator, _RandomAccessIterator, _Compare) [with _RandomAccessIterat or = __gnu_cxx::__normal_iterator<char*, std::__cxx11::basic_string<char> >; _Co mpare = __gnu_cxx::__ops::_Iter_less_iter]’ /usr/include/c++/11/bits/stl_algo.h:4842:18:   required from ‘void std::sort(_RA Iter, _RAIter) [with _RAIter = __gnu_cxx::__normal_iterator<char*, std::__cxx11: :basic_string<char> >]’ e2501fdf-0314-4044-85b0-b9b6212550b9.cpp:18:13:   required from here /usr/include/c++/11/bits/stl_heap.h:257:9: error: no type named ‘value_type’ in ‘struct std::iterator_traits<__gnu_cxx::__normal_iterator<char*, std::__cxx11::b asic_string<char> > >’   257 |         _ValueType;       |         ^~~~~~~~~~ /usr/include/c++/11/bits/stl_heap.h:259:9: error: no type named ‘difference_type ’ in ‘struct std::iterator_traits<__gnu_cxx::__normal_iterator<char*, std::__cxx 11::basic_string<char> > >’   259 |         _DistanceType;       |         ^~~~~~~~~~~~~

--- CPP_86.cpp
+++ CPP_86.cpp
@@ -1,25 +1,22 @@
-```cpp
 #include <string>
 #include <algorithm>
 using namespace std;
 
 string anti_shuffle(string s){
     string result = "";
-    int i = 0;
-    while(i<s.size()){
+    for(int i=0; i<s.size(); i++){
         if(s[i] == ' '){
             result += ' ';
-            i++;
             continue;
         }
         string word = "";
         for(int j=i; j<s.size() && s[j] != ' '; j++){
             word += s[j];
         }
-        i = j;
+        i = j - 1;
         sort(word.begin(), word.end());
         result += s.substr(i, j - i);
-        i = j;
+        i++;
     }
     return result;
 }
