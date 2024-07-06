43d4c969ebc706d5a25c3dd02aac21a7a5595656
beep boop <githubbot@vadim.me>
2024-07-06 09:22:14 +0200

    Solution 8 of CPP/160, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/7o93lsij Prompt:       |                       ^~~~~~ eeb75008-3209-42a2-a9ef-1c2b9b7027b0.cpp:1:31: error: ‘operato’ was not declared  in this scope     1 | int do_algebra(vector<string> operato, vector<int> operand) {       |                               ^~~~~~~ eeb75008-3209-42a2-a9ef-1c2b9b7027b0.cpp:1:40: error: ‘vector’ was not declared in this scope     1 | int do_algebra(vector<string> operato, vector<int> operand) {       |                                        ^~~~~~ eeb75008-3209-42a2-a9ef-1c2b9b7027b0.cpp:1:47: error: expected primary-expressio n before ‘int’     1 | int do_algebra(vector<string> operato, vector<int> operand) {       |                                               ^~~ eeb75008-3209-42a2-a9ef-1c2b9b7027b0.cpp:1:59: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | int do_algebra(vector<string> operato, vector<int> operand) {       |                                                           ^ In file included from eeb75008-3209-42a2-a9ef-1c2b9b7027b0.cpp:17: eeb75008-3209-42a2-a9ef-1c2b9b7027b0.cpp: In function ‘int main()’: eeb75008-3209-42a2-a9ef-1c2b9b7027b0.cpp:19:23: error: ‘do_algebra’ cannot be us ed as a function    19 |     assert (do_algebra({"//", "*"}, {7, 3, 4}) == 8);       |             ~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_160.cpp
+++ CPP_160.cpp
@@ -1,23 +1,16 @@
 #include <vector>
-using namespace std;
-
-int do_algebra(vector<string> operato, vector<int> operand) {
-    int result = operand[0];
-    for (int i = 1; i <= operato.size(); i++) {
-        if (operato[i - 1] == "+") {
-            result += operand[i];
-        } else if (operato[i - 1] == "-") {
-            result -= operand[i];
-        } else if (operato[i - 1] == "*") {
-            result *= operand[i];
-        } else if (operato[i - 1] == "//") { 
-            result /= operand[i]; // this will perform integer division
+int do_algebra(vector<pair<string, int>> expressions) {
+    int result = expressions[0].second;
+    for (int i = 1; i < expressions.size(); i++) {
+        if (expressions[i].first == "+") {
+            result += expressions[i].second;
+        } else if (expressions[i].first == "-") {
+            result -= expressions[i].second;
+        } else if (expressions[i].first == "*") {
+            result *= expressions[i].second;
+        } else if (expressions[i].first == "/") {
+            result /= expressions[i].second; // this will perform integer division
         }
     }
     return result;
-}
-
-int main() {
-    assert(do_algebra({"//", "*"}, {7, 3, 4}) == 8);
-    return 0;
 }
