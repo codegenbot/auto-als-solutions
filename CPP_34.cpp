ea4fba24b01c7c516d9b9836ba5aa350549db210
beep boop <githubbot@vadim.me>
2024-07-07 00:05:24 +0200

    Solution 3 of CPP/34, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/ye2pw82u Prompt: Complete the following code given the task description and function signature.

--- CPP_34.cpp
+++ CPP_34.cpp
@@ -1,18 +1,20 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include<stdio.h>
+#include<vector>
+#include<algorithm>
 using namespace std;
 
 vector<int> unique(vector<int> l){
-    vector<int> result(l.begin(), unique(l.begin(), l.end()).end());
-    return result;
-}
-
-int main(){
-    vector<int> v = {5, 3, 5, 2, 3, 3, 9, 0, 123};
-    vector<int> res = unique(v);
-    for(int i:res){
-        cout << i << " ";
+    vector<int> result;
+    for(int i=0; i<l.size(); i++){
+        bool flag = true;
+        for(int j=0; j<result.size(); j++){
+            if(l[i] == result[j]){
+                flag = false;
+                break;
+            }
+        }
+        if(flag) result.push_back(l[i]);
     }
-    return 0;
+    sort(result.begin(), result.end());
+    return result;
 }
