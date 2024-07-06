571d569cd5c446e512316e50b3149b3febbf88e0
beep boop <githubbot@vadim.me>
2024-07-06 21:53:01 +0200

    Solution 2 of CPP/87, ratio 0.6666666666666666 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/tu4ouux4 Prompt: Complete the following code given the task description and function signature.

--- CPP_87.cpp
+++ CPP_87.cpp
@@ -1,11 +1,11 @@
-#include <iostream>
+#include <stdio.h>
 #include <vector>
 using namespace std;
 
 vector<vector<int>> get_row(vector<vector<int>> lst, int x) {
     vector<vector<int>> result;
     for (int i = 0; i < lst.size(); i++) {
-        if (x <= i) {
+        if (i == x) {
             for (int j = lst[i].size() - 1; j >= 0; j--) {
                 result.push_back({i, j});
             }
