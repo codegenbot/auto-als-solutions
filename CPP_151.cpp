3be8d8cc38afc7b8ee4fa9dc46e459ab6ac0747e
beep boop <githubbot@vadim.me>
2024-07-06 13:09:33 +0200

    Solution 79 of CPP/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/2iqw0vuf Prompt: r ‘<cassert>’; did you forget to ‘#include <cassert>’?     3 | #include <iostream>   +++ |+#include <cassert>     4 | using namespace std; 610fa34f-f04e-4221-aeb4-fef945ee0f7f.cpp: At global scope: 610fa34f-f04e-4221-aeb4-fef945ee0f7f.cpp:25:5: error: redefinition of ‘int main( )’    25 | int main(){       |     ^~~~ 610fa34f-f04e-4221-aeb4-fef945ee0f7f.cpp:16:5: note: ‘int main()’ previously def ined here    16 | int main() {       |     ^~~~ In file included from 610fa34f-f04e-4221-aeb4-fef945ee0f7f.cpp:24: 610fa34f-f04e-4221-aeb4-fef945ee0f7f.cpp: In function ‘int main()’: 610fa34f-f04e-4221-aeb4-fef945ee0f7f.cpp:26:35: error: ‘lst’ was not declared in  this scope    26 |     assert (double_the_difference(lst) == odd_sum );       |                                   ^~~ 610fa34f-f04e-4221-aeb4-fef945ee0f7f.cpp:26:43: error: ‘odd_sum’ was not declare d in this scope    26 |     assert (double_the_difference(lst) == odd_sum );       |                                           ^~~~~~~

--- CPP_151.cpp
+++ CPP_151.cpp
@@ -2,8 +2,7 @@
 #include <vector>
 #include <cmath>
 #include <iostream>
-#include <cassert>
-
+#include <cassert>  
 using namespace std;
 
 long long double_the_difference(vector<double> lst) {
@@ -18,7 +17,7 @@ long long double_the_difference(vector<double> lst) {
 
 int main() {
     vector<double> lst({1.5, 2.5, -3.4, 4.7});
-    long long odd_sum = double_the_difference(lst);
+    long long odd_sum = double_the_difference(lst);  
     assert(double_the_difference(lst) == odd_sum); 
     cout << odd_sum;
     return 0;
