41d78dbdb0a55d997a4c508658ef94873d3e45b0
beep boop <githubbot@vadim.me>
2024-07-06 23:20:05 +0200

    Solution 7 of CPP/19, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/rrmr6mrq Prompt: Complete the following code given the task description and function signature.

--- CPP_19.cpp
+++ CPP_19.cpp
@@ -5,37 +5,29 @@ string sort_numbers(string numbers) {
     map<string, int> numMap = {{"zero", 0}, {"one", 1}, {"two", 2}, {"three", 3},
                                 {"four", 4}, {"five", 5}, {"six", 6}, {"seven", 7},
                                 {"eight", 8}, {"nine", 9}};
-    vector<string> nums;
-    string num;
 
-    for (string s : split(numbers, " ")) {
-        if (numMap.count(s)) {
-            nums.push_back(s);
+    vector<string> numVec;
+    string temp;
+
+    for (int i = 0; i < numbers.length(); i++) {
+        if (numbers[i] == ' ') {
+            continue;
         }
-    }
-
-    sort(nums.begin(), nums.end());
-    return join(nums, " ");
-}
-
-vector<string> split(const string& str, char c) {
-    vector<string> tokens;
-    size_t pos = 0;
-    while ((pos = str.find(c)) != string::npos) {
-        tokens.push_back(str.substr(0, pos));
-        str.erase(0, pos + 1);
-    }
-    tokens.push_back(str);
-    return tokens;
-}
-
-string join(vector<string> nums, char c) {
-    string result;
-    for (int i = 0; i < nums.size(); i++) {
-        if (i > 0) {
-            result += c;
+        temp = "";
+        while (i < numbers.length() && numbers[i] != ' ') {
+            temp += numbers[i];
+            i++;
         }
-        result += nums[i];
+        numVec.push_back(temp);
     }
-    return result;
+
+    sort(numVec.begin(), numVec.end(), [&numMap](string a, string b) {
+        return numMap[a] < numMap[b];
+    });
+
+    string result = "";
+    for (auto &str : numVec) {
+        result += str + " ";
+    }
+    return result.substr(0, result.length() - 1);
 }
