d850c324f46fdcdd8325cc6ef67398ab47b963bc
beep boop <githubbot@vadim.me>
2024-07-06 04:20:07 +0200

    Solution 90 of Python/84, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/ui55ly4s Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ce7103fd-55c8-471d-b483-3cfc87c9d e7e.py", line 12, in <module>     check(solve)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ce7103fd-55c8-471d-b483-3cfc87c9d e7e.py", line 9, in check     assert solve(963) == "10010", "Error"            ^^^^^^^^^^^^^^^^^^^^^ AssertionError: Error

--- Python_84.py
+++ Python_84.py
@@ -1,5 +1,6 @@
+```
 def solve(N):
     if not isinstance(N, int):
         return "Error: Input must be an integer"
     binary = bin(N)[2:]
-    return ''.join('1' if c == '0' else '0' for c in binary)
+    return "".join("0" if c == "1" else "1" for c in binary)
