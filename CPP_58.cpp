2b93b197ea0c099606a5fac9af3f73f7bc1c8766
beep boop <githubbot@vadim.me>
2024-07-06 20:04:35 +0200

    Solution 50 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/hbhotxqy Prompt:       |         ^~~ b52e5cd3-1e1f-4b18-8864-8089436772a3.cpp:7:9: error: expected primary-expression  before ‘int’     7 |     set<int> s2(l2.begin(), l2.end());       |         ^~~ b52e5cd3-1e1f-4b18-8864-8089436772a3.cpp:9:9: error: expected primary-expression  before ‘int’     9 |     set<int> intersection;       |         ^~~ b52e5cd3-1e1f-4b18-8864-8089436772a3.cpp:10:22: error: ‘s1’ was not declared in this scope; did you mean ‘l1’?    10 |     set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(),       |                      ^~       |                      l1 b52e5cd3-1e1f-4b18-8864-8089436772a3.cpp:10:44: error: ‘s2’ was not declared in this scope; did you mean ‘l2’?    10 |     set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(),       |                                            ^~       |                                            l2 b52e5cd3-1e1f-4b18-8864-8089436772a3.cpp:11:32: error: ‘intersection’ was not de clared in this scope    11 |                       inserter(intersection, intersection.begin()));       |                                ^~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,17 +1,14 @@
-#include <vector>
-#include <algorithm>
-using namespace std;
-
 vector<int> common(vector<int> l1, vector<int> l2) {
     set<int> s1(l1.begin(), l1.end());
     set<int> s2(l2.begin(), l2.end());
 
-    set<int> resultIntersection;
-    if(s1.size() < 1 || s2.size() < 1)
-        return {"Error: At least one list is empty."}.push_back(0);
+    if (s1.empty() || s2.empty()) {
+        throw runtime_error("Error: Both lists must not be empty.");
+    }
 
-    set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(),
-                      inserter(resultIntersection, resultIntersection.begin()));
+    set<int> resultIntersection;
+    set_difference(s1.begin(), s1.end(), s2.begin(), s2.end(),
+                    inserter(resultIntersection, resultIntersection.begin()));
 
     vector<int> result(resultIntersection.begin(), resultIntersection.end());
     return result;
