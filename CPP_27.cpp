ca18d0e42deb72affb7f05d971d8d8368cdcf16a
beep boop <githubbot@vadim.me>
2024-07-06 02:58:10 +0200

    Solution 58 of CPP/27, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/fv9ptt1r Prompt: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~ In file included from /usr/include/c++/11/bits/stl_algobase.h:67,                  from /usr/include/c++/11/bits/char_traits.h:39,                  from /usr/include/c++/11/string:40,                  from a911026b-acad-4379-a551-9b3aede86af5.cpp:2: /usr/include/c++/11/bits/stl_iterator.h: In instantiation of ‘class __gnu_cxx::_ _normal_iterator<const char*, std::__cxx11::basic_string<char> >’: /usr/include/c++/11/bits/basic_string.tcc:225:25:   required from ‘void std::__c xx11::basic_string<_CharT, _Traits, _Alloc>::_M_construct(_InIterator, _InIterat or, std::forward_iterator_tag) [with _FwdIterator = const char*; _CharT = char; _Traits = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:539:14:   required from ‘std::__cxx11::b asic_string<_CharT, _Traits, _Alloc>::basic_string(const _CharT*, const _Alloc&)  [with <template-parameter-2-1> = std::allocator<char>; _CharT = char; _Traits =  std::char_traits<char>; _Alloc = std::allocator<char>]’ a911026b-acad-4379-a551-9b3aede86af5.cpp:6:26:   required from here /usr/include/c++/11/bits/stl_iterator.h:1015:57: error: no type named ‘differenc e_type’ in ‘struct std::iterator_traits<const char*>’  1015 |       typedef typename __traits_type::difference_type   difference_type;       |                                                         ^~~~~~~~~~~~~~~

--- CPP_27.cpp
+++ CPP_27.cpp
@@ -1,13 +1,8 @@
-int main
-{
-    std::string input;
-    std::cout << "Enter a string: ";
-    std::getline(std::cin, input);
-    std::cout << flip_case(input) << std::endl;
-    return 0;
+#include <string>
+#include <cctype>
 
 std::string flip_case(std::string str){
-    std::string result = "";
+    std::string result(str);
     for(char c: str){
         if(isalpha(c)){
             if(islower(c))
@@ -15,8 +10,16 @@ std::string flip_case(std::string str){
             else
                 result += tolower(c);
         }else{
-            result.append(1, c);
+            result.push_back(c); 
         }
     }
     return result;
+}
+
+int main(){
+    std::string input;
+    std::cout << "Enter a string: ";
+    std::getline(std::cin, input);
+    std::cout << flip_case(input) << std::endl;
+    return 0;
 }
