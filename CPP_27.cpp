8ccc009cee7630812cdf897db122aef7cc2845c1
beep boop <githubbot@vadim.me>
2024-07-06 19:30:32 +0200

    Solution 98 of CPP/27, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/9n7jt4bs Prompt: d81cac63-684f-42a9-86b0-46113209c023.cpp:2:1: error: ‘string’ does not name a ty pe     2 | string filp_case(string str){       | ^~~~~~ In file included from d81cac63-684f-42a9-86b0-46113209c023.cpp:14: d81cac63-684f-42a9-86b0-46113209c023.cpp: In function ‘int main()’: d81cac63-684f-42a9-86b0-46113209c023.cpp:16:13: error: ‘filp_case’ was not decla red in this scope    16 |     assert (filp_case("These violent delights have violent ends") == "tH ESE VIOLENT DELIGHTS HAVE VIOLENT ENDS");       |             ^~~~~~~~~

--- CPP_27.cpp
+++ CPP_27.cpp
@@ -2,15 +2,13 @@
 #include <string>
 using namespace std;
 
-int i = 0;
-std::string flip_case(std::string str){
-    while(i < str.length()){
+std::string filp_case(std::string str){
+    for(int i = 0; i < str.length(); i++){
         if(str[i] >= 'a' && str[i] <= 'z'){
-            str[i] -= 32; 
+            str[i] = toupper(str[i]); // Convert lowercase to uppercase
         }else if(str[i] >= 'A' && str[i] <= 'Z'){
-            str[i] += 32; 
+            str[i] = tolower(str[i]); // Convert uppercase to lowercase
         }
-        i++;
     }
     return str;
 }
