ed83f8460e14d6a3cde80298b53745ab581109b0
beep boop <githubbot@vadim.me>
2024-07-06 16:20:40 +0200

    Solution 86 of Python/140, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/jj5dpla6 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/889305fa-8f95-4968-8fb9-05754e972 adf.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_140.py
+++ Python_140.py
@@ -1,7 +1,4 @@
-input_str = "_2823_"
-output_str = "-2823_"
-
-for i in range(2823, 2911):
+input_str = "_-2823_"
+for i in range(2824, 2911):
     input_str = input_str.replace(f"_{i:04d_}", f"-{i:04d_}")
-
 print(input_str)
