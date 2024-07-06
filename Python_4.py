3ecde011fee94da823d050ca01bf83d9e1747f66
beep boop <githubbot@vadim.me>
2024-07-06 03:42:07 +0200

    Solution 79 of Python/4, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/znmm06im Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7b80d785-9d86-4e4e-8426-dbf90d839 f06.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_4.py
+++ Python_4.py
@@ -1,5 +1,15 @@
-def sum_of_squares(n: int) -> float:
-    total_sum = 0
-    for i in range(1, n + 1):
-        total_sum += i**2
-    return total_sum
+from typing import List
+
+
+def mean_absolute_deviation(numbers: List[float]) -> float:
+    if not numbers:
+        return 0
+    median = (
+        sorted([num for num in numbers if isinstance(num, (int, float))])[
+            len(numbers) // 2
+        ]
+        if len(numbers) % 2 != 0
+        else sum(sorted([num for num in numbers if isinstance(num, (int, float))]))
+        / len([num for num in numbers if isinstance(num, (int, float))])
+    )
+    return sum(abs(num - median) for num in numbers) / len(numbers)
