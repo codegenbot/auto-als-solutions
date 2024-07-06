0dab21693bc243f99b14892e60d1d0d0acc0af4b
beep boop <githubbot@vadim.me>
2024-07-06 04:30:56 +0200

    Solution 97 of Python/89, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/6733c3l5 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/819432c2-62ea-46f3-8e8d-ce7fd8ad2 e0a.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_89.py
+++ Python_89.py
@@ -4,7 +4,7 @@ def encrypt(s):
     for char in s:
         if char.isalpha():
             ascii_offset = 65 if char.isupper() else 97
-            value = ord('A') if char.isupper() else ord('a')
+            value = ord('a') if char.islower() else ord('A')
             result += chr((ord(char) - value + 3) % 26 + value)
         else:
             result += char
