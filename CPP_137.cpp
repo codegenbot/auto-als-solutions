99d26620835650f700969ab268838a4d90df7aeb
beep boop <githubbot@vadim.me>
2024-07-07 00:47:23 +0200

    Solution 94 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/4p7xu0od Prompt: 05680fab-bc74-4de4-9d9b-1ca0d90ba1b1.cpp:1:10: fatal error: boost/any.hpp: No su ch file or directory     1 | #include <boost/any.hpp>       |          ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,14 +1,15 @@
-#include <boost/any.hpp>
+#include <iostream>
 #include <string>
+#include <boost/any.hpp>
 
-using namespace boost;
+using namespace std;
 
 boost::any compare_one(boost::any a, boost::any b) {
     if (any_cast<int>(a) > any_cast<int>(b)) return a;
     else if (any_cast<double>(a) > any_cast<double>(b)) return a;
-    else if (any_cast<std::string>(a) > any_cast<std::string>(b)) return a;
+    else if (any_cast<string>(a) > any_cast<string>(b)) return a;
     else if (any_cast<int>(a) == any_cast<int>(b)) return "None";
     else if (any_cast<double>(a) == any_cast<double>(b)) return "None";
-    else if (any_cast<std::string>(a) == any_cast<std::string>(b)) return "None";
-    return b;
+    else if (any_cast<string>(a) == any_cast<string>(b)) return "None";
+    else return b;
 }
