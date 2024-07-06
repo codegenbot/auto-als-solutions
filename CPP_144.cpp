b8131e191954e1dba781908f9259bc02b08ddde1
beep boop <githubbot@vadim.me>
2024-07-06 23:38:33 +0200

    Solution 4 of CPP/144, ratio 0.8461538461538461 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/q7aqeoe1 Prompt: Complete the following code given the task description and function signature.

--- CPP_144.cpp
+++ CPP_144.cpp
@@ -1,32 +1,17 @@
-#include<string>
+#include <string>
+#include <sstream>
+
 using namespace std;
 
 bool simplify(string x, string n) {
-    int numerator1 = 0, denominator1 = 0, numerator2 = 0, denominator2 = 0;
-    stringstream ss(x);
-    ss >> numerator1 >> '/' >> denominator1;
-    stringstream s(n);
-    s >> numerator2 >> '/' >> denominator2;
-
-    if (denominator1 != denominator2) {
-        return false;
-    }
-
-    int commonDivisor = gcd(denominator1, numerator1);
-    int newNumerator1 = numerator1 / commonDivisor;
-    int newDenominator1 = denominator1 / commonDivisor;
-
-    if ((long long)newNumerator1 * (long long)denominator2 == (long long)numerator2 * (long long)newDenominator1)
+    int a = 0, b = 1, c = 0, d = 1;
+    stringstream s(x);
+    s >> a >> b;
+    s.clear();
+    s.str(n);
+    s >> c >> d;
+    if (b*d == a*c)
         return true;
     else
         return false;
-}
-
-int gcd(int a, int b) {
-    while (b != 0) {
-        int temp = b;
-        b = a % b;
-        a = temp;
-    }
-    return a;
 }
