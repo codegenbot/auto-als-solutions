c927d4897a31120ffc46096101f1b7656c352f43
beep boop <githubbot@vadim.me>
2024-07-06 04:49:00 +0200

    Solution 33 of Python/103, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/fw0szpvy Prompt: Program did not receive expected input

--- Python_103.py
+++ Python_103.py
@@ -4,5 +4,5 @@ m = float(input("Enter the second number: "))
 def rounded_avg(n, m):
     avg = (n + m) / 2
     if not avg.is_integer():
-        return str(int(round(avg)))
+        return str(int(avg))
     return bin(int(round(avg)))[2:]
