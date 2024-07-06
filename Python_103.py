7f8a20689b534fa2042e67787e73a1e303af65bc
beep boop <githubbot@vadim.me>
2024-07-06 02:44:43 +0200

    Solution 5 of Python/103, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/fw0szpvy Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c83c4586-1145-44f4-9f90-c216d444c 109.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_103.py
+++ Python_103.py
@@ -1,10 +1,2 @@
-def binary_average(n1, n2):
-    if n1 < 0 or n2 < 0:
-        return "Error: Numbers should be non-negative."
-    elif n1 > n2:
-        return -1
-    else:
-        total = n1 + n2
-        avg = round(total / 2)
-        binary_avg = bin(avg)[2:]
-        return binary_avg
+def average_rounding_score(scores):
+    return sum(map(round, scores)) / len(scores) if scores else -1
