e981c40d6c75e1424f692fab730406d54573e6ca
beep boop <githubbot@vadim.me>
2024-07-06 23:43:01 +0200

    Solution 7 of CPP/144, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/q7aqeoe1 Prompt: Complete the following code given the task description and function signature.

--- CPP_144.cpp
+++ CPP_144.cpp
@@ -1,11 +1,52 @@
-#include<string>
+#include <string>
+#include <vector>
+
 using namespace std;
 
 bool simplify(string x, string n) {
-    int a = stoi(strtok((x.substr(1,x.find('/'))).c_str(),"/"));
-    int b = stoi(x.substr(0,1));
-    int c = stoi(strtok((n.substr(1,n.find('/'))).c_str(),"/"));
-    int d = stoi(n.substr(0,1));
+    int a = 0, b = 1, c = 0, d = 1;
+    int numerator = 0, denominator = 0;
+    
+    // Convert the strings to integers
+    string str = "";
+    for(int i=1; i<x.size(); i++){
+        if(x[i] == '/'){
+            a = stoi(str);
+            break;
+        }
+        str += x[i];
+    }
+    str = "";
+    for(int i=x.size()-1; i>=0; i--){
+        if(x[i] == '/'){
+            break;
+        }
+        str = x[i] + str;
+    }
+    b = stoi(str);
 
-    return (a*d) == (b*c);
+    str = "";
+    for(int i=1; i<n.size(); i++){
+        if(n[i] == '/'){
+            c = stoi(str);
+            break;
+        }
+        str += n[i];
+    }
+    str = "";
+    for(int i=n.size()-1; i>=0; i--){
+        if(n[i] == '/'){
+            break;
+        }
+        str = n[i] + str;
+    }
+    d = stoi(str);
+
+    // Calculate the greatest common divisor
+    numerator = a * d;
+    denominator = b * c;
+
+    // Check if the result is an integer
+    int gcd = __gcd(numerator, denominator);
+    return gcd == 1;
 }
