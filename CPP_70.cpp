4f0dbd99c73d4c3e610caac427e4d0b59404bea6
beep boop <githubbot@vadim.me>
2024-07-06 21:46:10 +0200

    Solution 97 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/t59aw96g Prompt:       |                    ^~~ 3b4b66e2-eba8-49ea-b65e-cf2f5ffce93f.cpp:15:27: error: ‘vector’ was not declared  in this scope    15 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 3b4b66e2-eba8-49ea-b65e-cf2f5ffce93f.cpp:15:34: error: expected primary-expressi on before ‘int’    15 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 3b4b66e2-eba8-49ea-b65e-cf2f5ffce93f.cpp:15:39: error: expression list treated a s compound expression in initializer [-fpermissive]    15 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 3b4b66e2-eba8-49ea-b65e-cf2f5ffce93f.cpp:14: 3b4b66e2-eba8-49ea-b65e-cf2f5ffce93f.cpp: In function ‘int main()’: 3b4b66e2-eba8-49ea-b65e-cf2f5ffce93f.cpp:24:20: error: ‘strange_sort_list’ was n ot declared in this scope    24 |     assert (issame(strange_sort_list({111111}) , {111111}));       |                    ^~~~~~~~~~~~~~~~~ 3b4b66e2-eba8-49ea-b65e-cf2f5ffce93f.cpp:24:19: error: ‘issame’ cannot be used a s a function    24 |     assert (issame(strange_sort_list({111111}) , {111111}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -2,14 +2,26 @@
 #include <algorithm>
 
 bool issame(vector<int> a, vector<int> b) {
-    if(a.size() != b.size()) return false;
-    for(int i = 0; i < a.size(); i++) {
-        if(a[i] != b[i]) return false;
+    if (a.size() != b.size()) {
+        return false;
+    }
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) {
+            return false;
+        }
     }
     return true;
 }
 
-int main() {
-    assert(issame({strange_sort_list({111111})}, {111111}));
-    return 0;
+vector<int> strange_sort_list(vector<int> lst) {
+    vector<int> result;
+    while (!lst.empty()) {
+        int min = *min_element(lst.begin(), lst.end());
+        result.push_back(min);
+        lst.erase(remove(lst.begin(), lst.end(), min), lst.end());
+        if (!lst.empty())
+            result.push_back(*max_element(lst.begin(), lst.end()));
+        lst.erase(remove(lst.begin(), lst.end(), *max_element(lst.begin(), lst.end())), lst.end());
+    }
+    return result;
 }
