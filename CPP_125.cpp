379585037c19f4c05768a125d34e274c72dd0a8c
beep boop <githubbot@vadim.me>
2024-07-06 06:17:44 +0200

    Solution 4 of CPP/125, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/g4r9w7pd Prompt:       |                              ^~~~~~ 8123b065-b55b-43ff-b426-30246a80c2d5.cpp:16:37: error: ‘string’ was not declared  in this scope    16 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 8123b065-b55b-43ff-b426-30246a80c2d5.cpp:16:44: error: ‘b’ was not declared in t his scope    16 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 8123b065-b55b-43ff-b426-30246a80c2d5.cpp:16:45: error: expression list treated a s compound expression in initializer [-fpermissive]    16 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 8123b065-b55b-43ff-b426-30246a80c2d5.cpp:15: 8123b065-b55b-43ff-b426-30246a80c2d5.cpp: In function ‘int main()’: 8123b065-b55b-43ff-b426-30246a80c2d5.cpp:25:20: error: ‘split_words’ was not dec lared in this scope    25 |     assert (issame(split_words("") ,{"0"}));       |                    ^~~~~~~~~~~ 8123b065-b55b-43ff-b426-30246a80c2d5.cpp:25:19: error: ‘issame’ cannot be used a s a function    25 |     assert (issame(split_words("") ,{"0"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_125.cpp
+++ CPP_125.cpp
@@ -1,12 +1,14 @@
 #include <vector>
 #include <string>
 
-bool issame(const std::vector<std::string>& a, const std::vector<std::string>& b) {
-    return a == b;
+bool issame(vector<string> a,vector<string>b){
+    if(a.size() != b.size())return false;
+    for(int i=0;i<a.size();i++)if(a[i]!=b[i])return false;
+    return true;
 }
 
-std::vector<std::string> split_words(string txt) {
-    std::vector<string> result;
+vector<string> split_words(string txt) {
+    vector<string> result;
     size_t pos = 0;
     while ((pos = txt.find(' ')) != string::npos) {
         result.push_back(txt.substr(0, pos));
@@ -17,4 +19,9 @@ std::vector<std::string> split_words(string txt) {
     }
     result.push_back(txt);
     return result;
+}
+
+int main() {
+    assert (issame(split_words("") ,{"0"}));
+    return 0;
 }
