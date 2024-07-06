d4bb0b1d641589e34572292b8ff975ee5d04b674
beep boop <githubbot@vadim.me>
2024-07-06 02:20:16 +0200

    Solution 32 of Python/49, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/ugnysjdk Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9bdc4183-81e8-49c4-98fa-f3b30da67 8f7.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_49.py
+++ Python_49.py
@@ -1,9 +1,6 @@
-```
+````
 def modp(n: int, p: int):
     if n < 0:
         n += p
-    return pow(n, p - 1, p)
-# """
-# This function calculates modular exponentiation. It adds the modulus to 
-# negative numbers before performing the calculation.
-# """
+    return pow(n, p-1, p)
+```
