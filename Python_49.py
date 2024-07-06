a5a2d212c8fa581b51fd0f586db9595e96584070
beep boop <githubbot@vadim.me>
2024-07-06 03:38:10 +0200

    Solution 91 of Python/49, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/ugnysjdk Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2bf21847-102e-474e-89da-1332f7bfc c29.py", line 27, in <module>     check(modp)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2bf21847-102e-474e-89da-1332f7bfc c29.py", line 24, in check     assert modp(31, 5) == 3            ^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2bf21847-102e-474e-89da-1332f7bfc c29.py", line 20, in modp     result = pow(x, -(p-2), p)              ^^^^^^^^^^^^^^^^^ ValueError: base is not invertible for the given modulus

--- Python_49.py
+++ Python_49.py
@@ -1,15 +1,16 @@
+```
 def modp(*args):
     n = args[0]
     p = args[1]
     if n < 0:
         n += p
-
+    
     def egcd(a, b):
         if a == 0:
             return (b, 0, 1)
         else:
-            gcd, y, x = egcd(b, a % b)
-            return (gcd, x, y - (a // b) * x)
+            gcd, x, y = egcd(b, a % b)
+            return (gcd, y, x)
 
     g, _, _ = egcd(n, p)
     if g != 1:
