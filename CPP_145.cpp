cfb799adcf2d79e50f3c9d5a011ae429cf818ef7
beep boop <githubbot@vadim.me>
2024-07-06 11:16:18 +0200

    Solution 56 of CPP/145, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/pmi4gz2f Prompt:    28 | bool issame(vector<int> a,vector<int>b){       |                                       ^ cc7de7d6-22ce-4af7-bd80-2617c3baf4ae.cpp:36:5: error: redefinition of ‘int main( )’    36 | int main(){       |     ^~~~ cc7de7d6-22ce-4af7-bd80-2617c3baf4ae.cpp:22:5: note: ‘int main()’ previously def ined here    22 | int main() {       |     ^~~~ In file included from cc7de7d6-22ce-4af7-bd80-2617c3baf4ae.cpp:27: cc7de7d6-22ce-4af7-bd80-2617c3baf4ae.cpp: In function ‘int main()’: cc7de7d6-22ce-4af7-bd80-2617c3baf4ae.cpp:37:20: error: ‘order_by_points’ was not  declared in this scope    37 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |                    ^~~~~~~~~~~~~~~ cc7de7d6-22ce-4af7-bd80-2617c3baf4ae.cpp:37:19: error: ‘issame’ cannot be used a s a function    37 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~

--- CPP_145.cpp
+++ CPP_145.cpp
@@ -1,4 +1,3 @@
-```cpp
 #include <algorithm>
 #include <vector>
 #include <initializer_list>
@@ -7,7 +6,7 @@ bool same(std::vector<int> a, std::vector<int> b) {
     return (a.size() == b.size()) && (std::equal(a.begin(), a.end(), b.begin()));
 }
 
-std::vector<int> order_by_points(std::vector<int> nums) {
+std::vector<int> sortNumbersByPoints(std::vector<int> nums) {
     auto comp = [&nums](int a, int b) {
         int sumA = 0, sumB = 0;
         for (char c : std::to_string(a)) sumA += c - '0';
@@ -20,7 +19,6 @@ std::vector<int> order_by_points(std::vector<int> nums) {
     return nums;
 }
 
-int testMain() {
-    assert(same(order_by_points({0,6,6,-76,-21,23,4}), {-76, -21, 0, 4, 23, 6, 6}));
+int main() {
+    assert(same(sortNumbersByPoints({0,6,6,-76,-21,23,4}), {-76, -21, 0, 4, 23, 6, 6}));
     return 0;
-}
