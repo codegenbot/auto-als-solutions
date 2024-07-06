c2abe5b25aa4797a41039dff2726e14b8987be96
beep boop <githubbot@vadim.me>
2024-07-06 13:10:09 +0200

    Solution 97 of CPP/145, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/pmi4gz2f Prompt:    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ fba4ef6a-74b8-401a-b614-437c06b92ccd.cpp:22:34: error: expected primary-expressi on before ‘int’    22 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ fba4ef6a-74b8-401a-b614-437c06b92ccd.cpp:22:39: error: expression list treated a s compound expression in initializer [-fpermissive]    22 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from fba4ef6a-74b8-401a-b614-437c06b92ccd.cpp:21: fba4ef6a-74b8-401a-b614-437c06b92ccd.cpp: In function ‘int main()’: fba4ef6a-74b8-401a-b614-437c06b92ccd.cpp:31:20: error: ‘order_by_points’ was not  declared in this scope    31 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |                    ^~~~~~~~~~~~~~~ fba4ef6a-74b8-401a-b614-437c06b92ccd.cpp:31:19: error: ‘issame’ cannot be used a s a function    31 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~

--- CPP_145.cpp
+++ CPP_145.cpp
@@ -2,23 +2,19 @@
 #include <algorithm>
 #include <vector>
 
-bool issame(int a, int b) {
-    long long sum1 = 0, sum2 = 0;
-    while(a > 0){
-        sum1 += a % 10;
-        a /= 10;
-    }
-    while(b > 0){
-        sum2 += b % 10;
-        b /= 10;
-    }
-    return (sum1 == sum2);
-}
-
-std::vector<int> sortByDigitSum(std::vector<int> arr) {
-    std::sort(arr.begin(), arr.end(), [&](int a, int b)->bool{
-        if (!issame(a,b)) return (long long)(a) < (long long)(b);
-        else return (long long)(a) < (long long)(b);
+std::vector<int> sortByDigitSum(const std::initializer_list<int>& arr) {
+    std::vector<int> res(arr);
+    std::sort(res.begin(), res.end(), [&](int a, int b)->bool{
+        long long sum1 = 0, sum2 = 0;
+        while(a > 0){
+            sum1 += a % 10;
+            a /= 10;
+        }
+        while(b > 0){
+            sum2 += b % 10;
+            b /= 10;
+        }
+        return sum1 <= sum2;
     });
-    return arr;
+    return res;
 }
