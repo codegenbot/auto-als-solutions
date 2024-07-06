814c841f926b848f3e4b12a9b87aa3b11c12d55e
beep boop <githubbot@vadim.me>
2024-07-06 04:13:52 +0200

    Solution 7 of Python/148, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/e3xonwlv Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7969d1df-1aed-464a-920f-ac613d68d 256.py", line 7     return tuple(sorted([p for p in planets[i1+1:i2] if p != planet1 and p != pl anet2])))         ^ SyntaxError: unmatched ')'

--- Python_148.py
+++ Python_148.py
@@ -5,4 +5,4 @@ def bf(planet1, planet2):
         return ()
     i1 = planets.index(planet1)
     i2 = planets.index(planet2)
-    return tuple(sorted([p for p in planets[i1+1:i2] if p != planet1 and p != planet2]))
+    return tuple(sorted([p for p in planets[i1+1:i2] if p != planet1 and p != planet2])))
