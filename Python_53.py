5173dd7a252b19fe1c44542b7b3410b3fa8ba759
beep boop <githubbot@vadim.me>
2024-07-06 04:36:29 +0200

    Solution 58 of Python/53, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/ju0ki0uh Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/82c9c66b-578d-4291-844d-e2c2b7dff 5a0.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_53.py
+++ Python_53.py
@@ -1,8 +1,8 @@
-def add():
+def add(x: int, y: int) -> int:
     while True:
         try:
             x = int(input("Enter the first number: "))
             y = int(input("Enter the second number: "))
             return x + y
-        except ValueError:
+        except (ValueError, EOFError):
             print("Invalid input. Please enter a valid integer.")
