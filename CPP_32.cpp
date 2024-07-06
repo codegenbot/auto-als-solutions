3d841388f7f8cd3570effdb30e6a3c18c2bb958d
beep boop <githubbot@vadim.me>
2024-07-07 00:00:49 +0200

    Solution 40 of CPP/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/0ujhsbw6 Prompt: b3e73659-9f70-4595-a6b9-22c611e2c75d.cpp:22:13: error: ‘coeffs’ was not declared  in this scope; did you mean ‘coeff’?    22 |             coeffs.push_back(coeff);       |             ^~~~~~       |             coeff b3e73659-9f70-4595-a6b9-22c611e2c75d.cpp:25:30: error: ‘coeffs’ was not declared  in this scope; did you mean ‘ncoeff’?    25 |         solution = find_zero(coeffs);       |                              ^~~~~~       |                              ncoeff b3e73659-9f70-4595-a6b9-22c611e2c75d.cpp:25:36: error: ‘find_zero’ cannot be use d as a function    25 |         solution = find_zero(coeffs);       |                                    ^ In file included from b3e73659-9f70-4595-a6b9-22c611e2c75d.cpp:9: b3e73659-9f70-4595-a6b9-22c611e2c75d.cpp:26:21: error: ‘poly’ was not declared i n this scope    26 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                     ^~~~ b3e73659-9f70-4595-a6b9-22c611e2c75d.cpp:26:17: error: ‘abs’ was not declared in  this scope    26 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                 ^~~

--- CPP_32.cpp
+++ CPP_32.cpp
@@ -1,29 +1,12 @@
-#include <cmath>
+```cpp
 #include <vector>
+using namespace std;
 
-double abs(double num) {
-    return num > 0 ? num : -num;
-}
-
-double poly(const vector<double>& coeffs, double solution) {
-    double result = 0.0;
-    for (int i = 0; i < coeffs.size(); i++) {
-        result += coeffs[i] * pow(solution, i);
+double find_zero(vector<double> xs){
+    double a = 0, b = 0;
+    for (int i = 0; i < xs.size(); i++) {
+        if(i % 2 == 1) a += xs[i];
+        else b += xs[i];
     }
-    return result;
-}
-
-double find_zero(vector<double> xs) {
-    double a = xs[0], b = 0;
-    vector<double> coeffs;
-    for (int i = 1; i < xs.size(); i++) {
-        if (i % 2 == 0) {
-            coeffs.push_back(xs[i]);
-        } else {
-            coeffs.push_back(-xs[i]);
-        }
-    }
-    double solution = -b / a;
-    assert(abs(poly(coeffs, solution)) < 1e-3);
-    return solution;
+    return -b / a;
 }
