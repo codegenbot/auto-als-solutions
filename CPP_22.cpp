016c1e5188fcf420a8f44f002066adb54d812d2b
beep boop <githubbot@vadim.me>
2024-07-06 02:42:40 +0200

    Solution 48 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/y2phiwh4 Prompt:  in this scope    13 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ cbd4952c-9207-4481-9f78-3b88e5d062bb.cpp:13:34: error: expected primary-expressi on before ‘int’    13 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ cbd4952c-9207-4481-9f78-3b88e5d062bb.cpp:13:39: error: expression list treated a s compound expression in initializer [-fpermissive]    13 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from cbd4952c-9207-4481-9f78-3b88e5d062bb.cpp:12: cbd4952c-9207-4481-9f78-3b88e5d062bb.cpp: In function ‘int main()’: cbd4952c-9207-4481-9f78-3b88e5d062bb.cpp:22:20: error: ‘filter_integers’ was not  declared in this scope    22 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |                    ^~~~~~~~~~~~~~~ cbd4952c-9207-4481-9f78-3b88e5d062bb.cpp:22:19: error: ‘issame’ cannot be used a s a function    22 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -1,18 +1,13 @@
-#include <boost/any.hpp>
-#include <vector>
-#include <list>
-
-using boost::any_cast;
-typedef long long OInt;
+bool issame(std::vector<int> a, std::vector<int> b) {
+    return a == b;
+}
 
 std::vector<int> filter_integers(std::list<boost::any> values) {
     std::vector<int> result;
     for (const auto& value : values) {
-        if(any_cast<OInt>(value)) {
-            int num = any_cast<int>(value);
-            if(num != 0) {
-                result.push_back(num);
-            }
+        if(boost::holds_alternative<int>(value)) { 
+            int val = boost::get<int>(value); 
+            result.push_back(val);
         }
     }
     return result;
