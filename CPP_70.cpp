9439ae0c4f45c351046df50cac19a9545ff140cc
beep boop <githubbot@vadim.me>
2024-07-06 02:36:43 +0200

    Solution 12 of CPP/70, ratio 0.6666666666666666 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/xmlozhdh Prompt: d8379a8c-a1f6-4dac-b7b7-030c87be2464.cpp:39:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    39 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ d8379a8c-a1f6-4dac-b7b7-030c87be2464.cpp:5:6: note: ‘bool issame(std::vector<int >, std::vector<int>)’ previously defined here     5 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -2,7 +2,7 @@
 #include <algorithm>
 using namespace std;
 
-bool issame(vector<int> a,vector<int>b){
+bool same(vector<int> a,vector<int>b){
     if(a.size() != b.size()) return false;
     sort(a.begin(), a.end());
     sort(b.begin(), b.end());
