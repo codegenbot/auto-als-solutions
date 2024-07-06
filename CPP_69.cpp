8441cc3d6ef504d8a25734cd4f8bc2cf519c15f8
beep boop <githubbot@vadim.me>
2024-07-06 10:37:58 +0200

    Solution 51 of CPP/69, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/qp49zkht Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/utility:76,                  from /usr/include/c++/11/algorithm:60,                  from 7da4ebf2-f7a9-40bb-901d-b44cbed66540.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_69.cpp
+++ CPP_69.cpp
@@ -3,16 +3,7 @@
 using namespace std;
 
 int main() {
-    vector<int> vec;
-    int n;
-    cout << "Enter the number of elements: ";
-    cin >> n;
-    vec.resize(n);
-    
-    cout << "Enter the elements: ";
-    for(int i = 0; i < n; i++)
-        cin >> vec[i];
-    
+    vector<int> vec(vec.begin(), vec.end());
     assert(std::search(vec.begin(), vec.end(), vec.begin(), vec.end()) == vec.end());
     return 0;
 }
