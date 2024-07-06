c20ee4695bd79344082403d7d1991f7a414d7627
beep boop <githubbot@vadim.me>
2024-07-06 03:02:10 +0200

    Solution 25 of CPP/68, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/mcs7y2vf Prompt:       |           ^~~~~~ In file included from 7d9880d1-f5ab-40b5-8a97-f65dac8fa560.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 7d9880d1-f5ab-40b5-8a97-f65dac8fa560.cpp:33:34: error: expected primary-expressi on before ‘int’    33 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 7d9880d1-f5ab-40b5-8a97-f65dac8fa560.cpp:33:39: error: expression list treated a s compound expression in initializer [-fpermissive]    33 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 7d9880d1-f5ab-40b5-8a97-f65dac8fa560.cpp:32: 7d9880d1-f5ab-40b5-8a97-f65dac8fa560.cpp: In function ‘int main()’: 7d9880d1-f5ab-40b5-8a97-f65dac8fa560.cpp:42:20: error: ‘pluck’ was not declared in this scope    42 |     assert (issame(pluck({7, 9, 7, 1}) , {}));       |                    ^~~~~ 7d9880d1-f5ab-40b5-8a97-f65dac8fa560.cpp:42:19: error: ‘issame’ cannot be used a s a function    42 |     assert (issame(pluck({7, 9, 7, 1}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_68.cpp
+++ CPP_68.cpp
@@ -2,12 +2,12 @@
 #include <algorithm>
 #include <utility>
 
-bool issame(std::vector<int> a, std::vector<int> b) {
+bool issame(vector<int> a, vector<int> b) {
     return a == b;
 }
 
-std::vector<int> pluck(std::vector<int> arr) {
-    std::vector<std::pair<int, int>> nodes;
+vector<int> pluck(vector<int> arr) {
+    vector<pair<int, int>> nodes;
     for (int i = 0; i < arr.size(); i++) {
         if (arr[i] % 2 == 0) {
             nodes.push_back({arr[i], i});
@@ -18,9 +18,9 @@ std::vector<int> pluck(std::vector<int> arr) {
         return {};
     }
     
-    std::sort(nodes.begin(), nodes.end());
+    sort(nodes.begin(), nodes.end());
     
-    std::vector<int> result;
+    vector<int> result;
     for (int i = 1; i < nodes.size(); i++) {
         if (issame({nodes[i-1].first, nodes[i-1].second}, {nodes[i].first, nodes[i].second})) {
             return {};
