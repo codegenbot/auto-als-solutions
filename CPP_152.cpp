a3d9007786c12725fa32699814587b1154d49219
beep boop <githubbot@vadim.me>
2024-07-06 12:56:51 +0200

    Solution 86 of CPP/152, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/iiv03sks Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from fb7b17bf-b7bc-4cce-a4a8-86db3415d160.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_152.cpp
+++ CPP_152.cpp
@@ -2,27 +2,17 @@
 #include <vector>
 #include <algorithm>
 
-bool isSame(int a, int b) {
+bool issame(int a, int b) {
     return a == b;
-}
-
-bool areEqualVectors(std::vector<int> v1, std::vector<int> v2) {
-    if (v1.size() != v2.size()) 
-        return false;  
-    for (int i = 0; i < v1.size(); i++) {
-        if (!isSame(v1[i], v2[i])) 
-            return false;
-    } 
-    return true;
 }
 
 std::vector<std::vector<int>> compare(std::vector<int> game, std::vector<int> guess) {
     std::vector<std::vector<int>> result(game.size());
     for (int i = 0; i < game.size(); i++) {
         if (game[i] == guess[i]) {
-            result[i] = {{0}};
+            result[i] = {1};
         } else {
-            result[i] = {{std::abs(game[i] - guess[i])}};
+            result[i] = {std::abs(game[i] - guess[i])};
         }
     }
     return result;
@@ -31,6 +21,6 @@ std::vector<std::vector<int>> compare(std::vector<int> game, std::vector<int> gu
 int main_function() {
     std::vector<int> game = {1,2,3,5};
     std::vector<int> guess = {-1,2,3,4};
-    assert(std::equal(compare(game,guess).begin(), compare(game,guess).end(), {{0},{0,{0}}}));
+    assert(std::equal(compare(game,guess).begin(), compare(game,guess).end(), {{0},{0},{0,{0}}});
     return 0;
 }
