db277fb9e0c14d2427a7bb74d871416a46b76669
beep boop <githubbot@vadim.me>
2024-07-06 07:32:41 +0200

    Solution 9 of CPP/133, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/nokt3pfm Prompt: 6033bac4-d20f-4462-9e5a-ece7b7d39a9f.cpp:1:1: error: ‘Here’ does not name a type     1 | Here is the completed code:       | ^~~~ In file included from 6033bac4-d20f-4462-9e5a-ece7b7d39a9f.cpp:12: 6033bac4-d20f-4462-9e5a-ece7b7d39a9f.cpp: In function ‘int main()’: 6033bac4-d20f-4462-9e5a-ece7b7d39a9f.cpp:14:13: error: ‘sum_squares’ was not dec lared in this scope    14 |     assert (sum_squares({-1,1,0})==2);       |             ^~~~~~~~~~~

--- CPP_133.cpp
+++ CPP_133.cpp
@@ -1,10 +1,13 @@
+```cpp
 // Here is the completed code:
+#include <vector>
+#include <cmath>
 
-int sum_squares(vector<float> lst){
+int sum_squares(std::vector<float> lst){
     int sum = 0;
     for(float i : lst){
-        int ceil_i = ceil(i);
-        sum += pow(ceil_i,2);
+        int ceil_i = std::ceil(i);
+        sum += std::pow(ceil_i,2);
     }
     return sum;
 }
