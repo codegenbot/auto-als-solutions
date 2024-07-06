d8507a5123db2c3954ba3694b9750bb0b4f32297
beep boop <githubbot@vadim.me>
2024-07-06 02:15:28 +0200

    Solution 26 of CPP/63, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/gzcd2ovm Prompt: a7c224ad-7cd9-4f37-99b8-026bb2d926c2.cpp:1:1: error: stray ‘`’ in program     1 | ```       | ^ a7c224ad-7cd9-4f37-99b8-026bb2d926c2.cpp:1:2: error: stray ‘`’ in program     1 | ```       |  ^ a7c224ad-7cd9-4f37-99b8-026bb2d926c2.cpp:1:3: error: stray ‘`’ in program     1 | ```       |   ^ a7c224ad-7cd9-4f37-99b8-026bb2d926c2.cpp:23:1: warning: ISO C++ forbids declarat ion of ‘main’ with no type [-Wreturn-type]    23 | main() {       | ^~~~ a7c224ad-7cd9-4f37-99b8-026bb2d926c2.cpp:32:5: error: redefinition of ‘int main( )’    32 | int main(){       |     ^~~~ a7c224ad-7cd9-4f37-99b8-026bb2d926c2.cpp:23:1: note: ‘int main()’ previously def ined here    23 | main() {       | ^~~~

--- CPP_63.cpp
+++ CPP_63.cpp
@@ -1,4 +1,3 @@
-#include <cstdio>
 #include <iostream>
 using namespace std;
 
@@ -13,7 +12,7 @@ int fibfib(int n) {
             int temp = a + b + c;
             a = b;
             b = c;
-            c = temp % 2 == 0 ? c : c - ((c/2)*(c/2));
+            c = temp;
         }
         return c;
     }
