712fb1ebfd75fca7252f938c0b1644433fa9d67d
beep boop <githubbot@vadim.me>
2024-07-07 01:02:30 +0200

    Solution 19 of CPP/43, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/gx7bp187 Prompt: Program failed

--- CPP_43.cpp
+++ CPP_43.cpp
@@ -9,7 +9,7 @@ bool pairs_sum_to_zero(std::vector<int> l) {
         }
     }
     for(int i=0; i<l.size(); i++){
-        if(std::find(l.begin()+i+1, l.end(), -std::abs(l[i])) != l.end())
+        if(std::find(l.begin(), l.end(), -std::abs(l[i])) != l.end())
             return true;
     }
     return false;
