e9393cb05d10852bf1229c9fcc785099ad29df1e
beep boop <githubbot@vadim.me>
2024-07-06 19:12:29 +0200

    Solution 16 of CPP/34, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/yxw68pfq Prompt: /usr/include/c++/11/pstl/glue_algorithm_defs.h:243:1: note: candidate: ‘template <class _ExecutionPolicy, class _ForwardIterator1, class _ForwardIterator2> __pst l::__internal::__enable_if_execution_policy<_ExecutionPolicy, _ForwardIterator2>  std::unique_copy(_ExecutionPolicy&&, _ForwardIterator1, _ForwardIterator1, _For wardIterator2)’   243 | unique_copy(_ExecutionPolicy&& __exec, _ForwardIterator1 __first, _Forwa rdIterator1 __last, _ForwardIterator2 __result);       | ^~~~~~~~~~~ /usr/include/c++/11/pstl/glue_algorithm_defs.h:243:1: note:   template argument deduction/substitution failed: a3b4563d-939f-4f46-b7be-4cd2d7e880ec.cpp:7:46: note:   candidate expects 4 argum ents, 2 provided     7 |     vector<int> result(l.begin(), unique_copy(l.begin(), l.end()));       |                                   ~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~ a3b4563d-939f-4f46-b7be-4cd2d7e880ec.cpp: At global scope: a3b4563d-939f-4f46-b7be-4cd2d7e880ec.cpp:37:5: error: redefinition of ‘int main( )’    37 | int main(){       |     ^~~~ a3b4563d-939f-4f46-b7be-4cd2d7e880ec.cpp:12:5: note: ‘int main()’ previously def ined here    12 | int main() {       |     ^~~~

--- CPP_34.cpp
+++ CPP_34.cpp
@@ -1,21 +1,26 @@
+#include <iostream>
+#include <vector>
 #include <algorithm>
+using namespace std;
 
 vector<int> unique(vector<int> l) {
-    vector<int> result(l.begin(), unique_copy(l.begin(), l.end()).end());
+    vector<int> result(l.begin(), l.end());
+    sort(result.begin(), result.end());
     return result;
 }
 
-vector<int>::iterator unique_copy(iterator start, iterator end) {
-    sort(start, end);
-    iterator i = start;
-    for (++i; i != end; ++i) {
-        if (*i == *(i - 1)) {
-            *i = INT_MIN;
-        }
+int main() {
+    vector<int> v = {5, 3, 5, 2, 3, 3, 9, 0, 123};
+    for (int x : v) {
+        cout << x << " ";
     }
-    for (iterator j = start; j != end; ++j) {
-        if (*j == INT_MIN) continue;
-        *j = *(i - 1);
+    cout << endl;
+    
+    vector<int> unique_v = unique(v);
+    for (int x : unique_v) {
+        cout << x << " ";
     }
-    return i;
+    cout << endl;
+
+    return 0;
 }
