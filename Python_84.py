a417be6b3cc354492f0d8655d911f7f663b5324d
beep boop <githubbot@vadim.me>
2024-07-06 12:52:31 +0200

    Solution 12 of Python/84, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/e1jdvdtr Prompt: Complete the following code given the task description and function signature.

--- Python_84.py
+++ Python_84.py
@@ -1,5 +1,2 @@
 def solve(N):
-    total = 0
-    for i in bin(N)[2:]:
-        total += int(i)
-    return str(total) if N > 1 else str(total)
+    return bin(sum(int(i) for i in bin(N)[2:].replace(" ",""))[2:])
