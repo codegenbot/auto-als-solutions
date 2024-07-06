516a723d0dc9f932023684b735124f2cbf6d28f6
beep boop <githubbot@vadim.me>
2024-07-06 10:51:55 +0200

    Solution 54 of CPP/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/99ilqzbl Prompt:     6 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 18e3c8a2-7401-4d50-ae2c-3c43823e3002.cpp:6:34: error: expected primary-expressio n before ‘int’     6 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 18e3c8a2-7401-4d50-ae2c-3c43823e3002.cpp:6:39: error: expression list treated as  compound expression in initializer [-fpermissive]     6 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 18e3c8a2-7401-4d50-ae2c-3c43823e3002.cpp:5: 18e3c8a2-7401-4d50-ae2c-3c43823e3002.cpp: In function ‘int main()’: 18e3c8a2-7401-4d50-ae2c-3c43823e3002.cpp:15:27: error: ‘minPath’ cannot be used as a function    15 |     assert (issame(minPath({{1, 3}, {3, 2}}, 10) , {1, 3, 1, 3, 1, 3, 1,  3, 1, 3}));       |                    ~~~~~~~^~~~~~~~~~~~~~~~~~~~~~ 18e3c8a2-7401-4d50-ae2c-3c43823e3002.cpp:15:19: error: ‘issame’ cannot be used a s a function    15 |     assert (issame(minPath({{1, 3}, {3, 2}}, 10) , {1, 3, 1, 3, 1, 3, 1,  3, 1, 3}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~

--- CPP_129.cpp
+++ CPP_129.cpp
@@ -1,3 +1,8 @@
-#include <utility>
-int minPath(vector<vector<char>> grid, int x, int y, vector<pair<int, int>>& path) {
-    ...
+bool checkSame(vector<int> a, vector<int>b){
+    if(a.size()!=b.size())
+        return false;
+    for(int i=0; i<a.size(); i++)
+        if(a[i]!=b[i])
+            return false;
+    return true;
+}
