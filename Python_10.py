4d62ae5ae09f34d3734e26e1ec1c9b0d9c1c3026
beep boop <githubbot@vadim.me>
2024-07-06 13:07:09 +0200

    Solution 73 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/pm26cptm Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/37d53b27-5ef6-45d2-bb98-efd8269c4 d43.py", line 18, in <module>     check(make_palindrome)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/37d53b27-5ef6-45d2-bb98-efd8269c4 d43.py", line 15, in check     assert make_palindrome("jerry") == "jerryrrej"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_10.py
+++ Python_10.py
@@ -4,6 +4,6 @@ def make_palindrome(string: str) -> str:
     for i in range(len(s), 0, -1):
         if s[:i] == s[:i][::-1]:
             if string.islower():
-                return string + s[:i][::-1]
+                return string + s[:i][[::-1]]
             else:
                 return string + s[:i][::-1].capitalize()
