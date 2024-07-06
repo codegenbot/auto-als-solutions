03d6115d5b4fa74602e4e0a9fe2ba79fcd0f16ed
beep boop <githubbot@vadim.me>
2024-07-06 07:25:26 +0200

    Solution 82 of CPP/80, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/yra2qaiq Prompt:       | ^~~~~~~~~~~~~ f453659a-c4f3-4544-81ab-1ca775a1d33a.cpp:37:9: warning: empty parentheses were d isambiguated as a function declaration [-Wvexing-parse]    37 | int main(){       |         ^~ f453659a-c4f3-4544-81ab-1ca775a1d33a.cpp:37:9: note: remove parentheses to defau lt-initialize a variable    37 | int main(){       |         ^~       |         -- f453659a-c4f3-4544-81ab-1ca775a1d33a.cpp:37:9: note: or replace parentheses with  braces to value-initialize a variable f453659a-c4f3-4544-81ab-1ca775a1d33a.cpp:37:11: error: a function-definition is not allowed here before ‘{’ token    37 | int main(){       |           ^ f453659a-c4f3-4544-81ab-1ca775a1d33a.cpp:39:2: error: expected ‘}’ at end of inp ut    39 | }       |  ^ f453659a-c4f3-4544-81ab-1ca775a1d33a.cpp:26:12: note: to match this ‘{’    26 | int main() {       |            ^

--- CPP_80.cpp
+++ CPP_80.cpp
@@ -1,3 +1,27 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+
+using namespace std;
+
+bool is_happy(string s){
+    if(s.length() < 3) return false;
+    for(int i = 0; i <= s.length()-3; i++){
+        char cstr[4] = {0}; 
+        memcpy(cstr, s.substr(i, 3).c_str(), 3);
+        bool unique = true;
+        for(char d : s){ 
+            if(strchr(cstr, d) != NULL) {
+                unique = false;
+                break;
+            }
+        }
+        if(!unique) return false;
+    }
+    return true;
+
+}
+
 int main
 {
     string s;
