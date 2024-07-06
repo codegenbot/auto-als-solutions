80ada3a749ddbe84e4f4eb9f3b2217b1bfe6639c
beep boop <githubbot@vadim.me>
2024-07-06 23:15:58 +0200

    Solution 8 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/4p7xu0od Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,28 +1,48 @@
+Here is the completed code:
+
+```cpp
+#include <boost/any.hpp>
 #include <string>
 #include <algorithm>
-#include <boost/any.hpp>
-#include <boost/lexical_cast.hpp>
 
 using namespace std;
-using namespace boost;
 
 boost::any compare_one(boost::any a, boost::any b) {
-    if (is_any_of<a>(double())) {
-        if (is_any_of<b>(double())) {
-            double da = boost::any_cast<double>(a);
-            double db = boost::any_cast<double>(b);
-            return da > db ? a : (da == db ? "None" : b);
-        } else {
-            string sa = boost::any_cast<string>(a);
-            double db = boost::any_cast<double>(b);
-            if (db > stod(sa))
-                return a;
-            else
-                return b;
-        }
-    } else {
-        string sa = boost::any_cast<string>(a);
-        string sb = boost::any_cast<string>(b);
-        return stod(sb) > stod(sa) ? b : (stod(sa) == stod(sb) ? "None" : a);
+    if (a.type() == typeid(int) && b.type() == typeid(double)) {
+        return max(a.convert_to<int>(), b.convert_to<double>());
     }
+    else if (a.type() == typeid(double) && b.type() == typeid(int)) {
+        return max(a, boost::any(b.convert_to<int>()));
+    }
+    else if (a.type() == typeid(string) && b.type() == typeid(double)) {
+        return (boost::any(max((stoi(get<string>(a)).convert_to<double>(), b.convert_to<double>()))));
+    }
+    else if (a.type() == typeid(double) && b.type() == typeid(string)) {
+        return (boost::any(max(a, boost::any(stoi(get<string>(b))))));
+    }
+    else if (a.type() == typeid(string) && b.type() == typeid(string)) {
+        if (get<string>(a) > get<string>(b))
+            return a;
+        else if (get<string>(a) < get<string>(b))
+            return b;
+        else
+            return boost::any("None");
+    }
+    else if (a.type() == typeid(int) && b.type() == typeid(string)) {
+        if (stoi(get<string>(b)) > a.convert_to<int>())
+            return boost::any(stoi(get<string>(b)));
+        else if (stoi(get<string>(b)) < a)
+            return a;
+        else
+            return boost::any("None");
+    }
+    else if (a.type() == typeid(string) && b.type() == typeid(int)) {
+        if (stoi(get<string>(a)) > b.convert_to<int>())
+            return a;
+        else if (stoi(get<string>(a)) < b)
+            return boost::any(b);
+        else
+            return boost::any("None");
+    }
+    return boost::any("None");
 }
