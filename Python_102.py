8ab39a5a315401cedf4b2f63da1acadd1edeef27
beep boop <githubbot@vadim.me>
2024-07-06 02:39:13 +0200

    Solution 4 of Python/102, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/v7mdesiz Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d0ea05b4-0c76-4a9f-bf8d-8e6dd18aa cb9.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_102.py
+++ Python_102.py
@@ -1,7 +1,8 @@
 def choose_num(x, y):
-    if x < y:
-        return y
-    elif x > y:
-        return x
+    if x % 2 == 0 and y % 2 == 0:
+        return min(y, max(x, 2))
     else:
-        return "The numbers are equal"
+        for i in range(max(x, 2), min(y+1, y)):
+            if i % 2 == 0:
+                return i
+        return -1
