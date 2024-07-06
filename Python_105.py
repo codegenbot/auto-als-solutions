748648b417122b447c6e5e8ff6b65611138ce847
beep boop <githubbot@vadim.me>
2024-07-06 03:19:03 +0200

    Solution 43 of Python/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/ubalkc5i Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/dd33e8e8-bdfb-4495-abb1-af5991dfc d15.py", line 10, in <module>     check(by_length)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/dd33e8e8-bdfb-4495-abb1-af5991dfc d15.py", line 7, in check     assert by_length([9, 4, 8]) == ["Nine", "Eight", "Four"]            ^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/dd33e8e8-bdfb-4495-abb1-af5991dfc d15.py", line 3, in by_length     return [name for num, name in zip(map(str, arr), [mapping[x] for x in map(st r, arr)]) if set(str(num))[0] == "1"]                                                      ^^^^^^^^^^^^^^^^^^^^^^^^^^^ ^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/dd33e8e8-bdfb-4495-abb1-af5991dfc d15.py", line 3, in <listcomp>     return [name for num, name in zip(map(str, arr), [mapping[x] for x in map(st r, arr)]) if set(str(num))[0] == "1"]                                                       ~~~~~~~^^^ KeyError: '9'

--- Python_105.py
+++ Python_105.py
@@ -1,4 +1,4 @@
 ```
 def by_length(arr):
     mapping = {"One": 1, "Two": 2, "Three": 3, "Four": 4, "Five": 5, "Six": 6, "Seven": 7, "Eight": 8, "Nine": 9}
-    return [name for num, name in zip(map(str, arr), map(str, arr)) if str(num).startswith('1')]
+    return [name for num, name in zip(map(str, arr), map(lambda x: mapping[str(x)], arr)) if str(num).startswith('1')]
