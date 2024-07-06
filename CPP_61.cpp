c44f302bdb0bd313bd8c8aeab29acf22dc9aff39
beep boop <githubbot@vadim.me>
2024-07-06 03:05:15 +0200

    Solution 15 of CPP/61, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/xmu7e595 Prompt: b81f5830-e011-451f-aca5-7bda6141cf69.cpp:3:25: error: ‘string’ was not declared in this scope     3 | bool correct_bracketing(string brackets) {       |                         ^~~~~~ b81f5830-e011-451f-aca5-7bda6141cf69.cpp:3:25: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from b81f5830-e011-451f-aca5-7bda6141cf69.cpp:1: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from b81f5830-e011-451f-aca5-7bda6141cf69.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ In file included from b81f5830-e011-451f-aca5-7bda6141cf69.cpp:12: b81f5830-e011-451f-aca5-7bda6141cf69.cpp: In function ‘int main()’: b81f5830-e011-451f-aca5-7bda6141cf69.cpp:14:36: error: ‘correct_bracketing’ cann ot be used as a function    14 |     assert (not (correct_bracketing("()()(()())()))()")));       |                  ~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~

--- CPP_61.cpp
+++ CPP_61.cpp
@@ -1,5 +1,6 @@
 #include <string>
-bool correct_bracketing(std::string brackets){
+
+bool correct_bracketing(std::string brackets) {
     int count = 0;
     for(char c : brackets){
         if(c == '(')count++;
