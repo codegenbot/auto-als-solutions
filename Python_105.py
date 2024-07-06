36f1e87a5bf0b8632919df0d7681f9aba39ddf2a
beep boop <githubbot@vadim.me>
2024-07-06 03:37:10 +0200

    Solution 64 of Python/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/ubalkc5i Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f62914c9-64b2-4c9e-ac76-73b74e436 4b3.py", line 10, in <module>     check(by_length)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f62914c9-64b2-4c9e-ac76-73b74e436 4b3.py", line 7, in check     assert by_length([9, 4, 8]) == ["Nine", "Eight", "Four"]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_105.py
+++ Python_105.py
@@ -1,17 +1,4 @@
 def by_length(arr):
-    mapping = {
-        "One": 1,
-        "Two": 2,
-        "Three": 3,
-        "Four": 4,
-        "Five": 5,
-        "Six": 6,
-        "Seven": 7,
-        "Eight": 8,
-        "Nine": 9,
-    }
-    return [
-        mapping.get(str(i), str(i))
-        for i in arr
-        if "One" <= mapping.get(str(i), str(i)) <= "Nine"
-    ]
+    name_number = {1: "One", 2: "Two", 3: "Three", 4: "Four", 5: "Five", 
+                   6: "Six", 7: "Seven", 8: "Eight", 9: "Nine"}
+    return [name_number[i] for i in arr if str(i).startswith('1')]
