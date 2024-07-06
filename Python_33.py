a6bc09779dddad9495204f136cb5d4adbad31b67
beep boop <githubbot@vadim.me>
2024-07-06 03:07:46 +0200

    Solution 77 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/03tao3n9 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/eacf5880-68e7-4a52-9fcc-e5c515104 711.py", line 9, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/eacf5880-68e7-4a52-9fcc-e5c515104 711.py", line 4, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/eacf5880-68e7-4a52-9fcc-e5c515104 711.py", line 2, in sort_third     return sorted((x[2], x) for x in lst)            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/eacf5880-68e7-4a52-9fcc-e5c515104 711.py", line 2, in <genexpr>     return sorted((x[2], x) for x in lst)                    ~^^^ TypeError: 'int' object is not subscriptable

--- Python_33.py
+++ Python_33.py
@@ -1,3 +1,2 @@
-```
-def sort_third(lst):
-    return sorted((x[2], x) for x in lst if isinstance(x, list))
+```def sort_third(lst):
+    return sorted((x[2], x) for x in lst if isinstance(x, list))```
