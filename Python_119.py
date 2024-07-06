19a76001911a0d7188295505f1f39321aa42f875
beep boop <githubbot@vadim.me>
2024-07-06 04:18:25 +0200

    Solution 21 of Python/119, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/629l2qg8 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/fd58a446-a535-45dd-97c5-3144feb91 9ec.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_119.py
+++ Python_119.py
@@ -1,5 +1,3 @@
-input_list = [
-    input("Enter string {}: ".format(i + 1))
-    for i in range(int(input("Number of strings: ")))
-]
+```
+input_list = [input("Enter string {}: ".format(i+1)) for i in range(int(input("Number of strings: ")))]
 print(match_parens([s.strip() for s in input_list]))
