0e8db66067aa9955e15a58d251e0854cc574f6dd
beep boop <githubbot@vadim.me>
2024-07-06 02:49:29 +0200

    Solution 58 of Python/12, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/7h4skt8d Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c6e1bf78-e840-4c45-9d12-a1a3c078e 709.py", line 1, in <module>     longest(["this", "is", "the", "example"])     ^^^^^^^ NameError: name 'longest' is not defined

--- Python_12.py
+++ Python_12.py
@@ -1,13 +1,2 @@
-```
-from typing import List, Optional
-
-def longest(strings: List[str]) -> Optional[str]:
-    if not strings:
-        return None
-    max_len = 0
-    result = ""
-    for string in strings:
-        if len(string) > max_len:
-            max_len = len(string)
-            result = string
-    return result
+def longest(lst):
+    return max(lst, key=len)
