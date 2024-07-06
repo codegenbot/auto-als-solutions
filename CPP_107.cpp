8ffa6e4ff008a0b24390bfbf9ae67fdbd51d2b2b
beep boop <githubbot@vadim.me>
2024-07-06 04:37:05 +0200

    Solution 0 of CPP/107, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/fhz9lftu Prompt: Complete the following code given the task description and function signature.

---
+++ CPP_107.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+using namespace std;
+
+vector<int> even_odd_palindrome(int n) {
+    vector<int> result(2);
+    for (int i = 1; i <= n; ++i) {
+        string str = to_string(i);
+        if (str == reverse(str).s) {
+            if (i % 2 == 0)
+                result[0]++;
+            else
+                result[1]++;
+        }
+    }
+    return result;
+}
