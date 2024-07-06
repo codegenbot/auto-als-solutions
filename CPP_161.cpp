bd48439a3823b25c0467168a652d30b497613adb
beep boop <githubbot@vadim.me>
2024-07-07 01:29:19 +0200

    Solution 31 of CPP/161, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/qu95vsas Prompt: f1ce6896-806e-42e6-b6e6-81d2ecc7ac0d.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string solve(string s) {       | ^~~~~~ In file included from f1ce6896-806e-42e6-b6e6-81d2ecc7ac0d.cpp:26: f1ce6896-806e-42e6-b6e6-81d2ecc7ac0d.cpp: In function ‘int main()’: f1ce6896-806e-42e6-b6e6-81d2ecc7ac0d.cpp:28:13: error: ‘solve’ was not declared in this scope    28 |     assert (solve("#ccc") == "#CCC");       |             ^~~~~

--- CPP_161.cpp
+++ CPP_161.cpp
@@ -19,7 +19,7 @@ std::string solve(std::string s) {
     }
 
     if (!foundLetter) {
-        reverse(result.begin(), result.end());
+        std::reverse(result.begin(), result.end());
     }
 
     return result;
