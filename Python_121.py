c7a2bd8d583d20f5eeac49c22259820e93096a1c
beep boop <githubbot@vadim.me>
2024-07-06 03:21:14 +0200

    Solution 7 of Python/121, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/1eafl0mm Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ec5d4608-0427-4b04-bd52-8f9b65bd5 005.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_121.py
+++ Python_121.py
@@ -1,3 +1,5 @@
+````
 def solution(lst):
-    """Complete this code to sum all odd numbers starting from second element of the list."""
-    return sum(i for i in lst[1:] if i % 2 != 0)
+"""Complete this code to sum all odd numbers starting from second element of the list."""
+return sum(i for i in lst[1::2] if i % 2 != 0)
+```
