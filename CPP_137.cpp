42f09937ee40e41c276977446b5ff1994e00f05a
beep boop <githubbot@vadim.me>
2024-07-06 23:59:15 +0200

    Solution 52 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/4p7xu0od Prompt: 8aede7b1-2178-4453-9570-3cb8c35de70a.cpp:1:1: error: ‘boost’ does not name a typ e     1 | boost::any compare_one(boost::any a, boost::any b) {       | ^~~~~ In file included from 8aede7b1-2178-4453-9570-3cb8c35de70a.cpp:32: 8aede7b1-2178-4453-9570-3cb8c35de70a.cpp: In function ‘int main()’: 8aede7b1-2178-4453-9570-3cb8c35de70a.cpp:34:13: error: ‘boost’ has not been decl ared    34 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ 8aede7b1-2178-4453-9570-3cb8c35de70a.cpp:34:29: error: ‘string’ was not declared  in this scope    34 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ 8aede7b1-2178-4453-9570-3cb8c35de70a.cpp:34:37: error: ‘compare_one’ was not dec lared in this scope    34 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                     ^~~~~~~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -9,24 +9,17 @@ boost::any compare_one(boost::any a, boost::any b) {
         return (float)boost::any_cast<int>(b) > (float)boost::any_cast<int>(a) ? b : a;
     }
     else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-        string s1 = boost::any_cast<string>(a);
-        string s2 = boost::any_cast<string>(b);
-        if ((s1 == "None" || s2 == "None") && s1 != s2)
-            return s1 > s2 ? a : b;
-        else
-            return "None";
+        string s1 = boost::any_cast<string>(a), s2 = boost::any_cast<string>(b);
+        return s1 > s2 ? a : b;
     }
     else if (a.type() == typeid(string)) {
         string s = boost::any_cast<string>(a);
-        if (s == "None" || boost::any_cast<string>(b) == "None")
-            return "None";
-        else
-            return s > boost::any_cast<string>(b) ? a : b;
+        return (s == "None" || boost::any_cast<string>(b) == "None") ? "None" : (s > boost::any_cast<string>(b) ? a : b);
     }
-    else if (boost::any_cast<string>(a) == "None" || boost::any_cast<string>(b) == "None") {
+    else if (boost::any_cast<string>(a) == "None") {
         return "None";
     }
     else {
-        return boost::any_cast<string>(a) > boost::any_cast<string>(b) ? a : b;
+        return a;
     }
 }
