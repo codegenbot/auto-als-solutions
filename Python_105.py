42349affae27c775919857c886fb2cf09e6d0595
beep boop <githubbot@vadim.me>
2024-07-06 02:59:36 +0200

    Solution 18 of Python/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/ubalkc5i Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1553d3dd-afd8-451b-9208-94dd75f4a 94b.py", line 9, in <module>     check(by_length)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1553d3dd-afd8-451b-9208-94dd75f4a 94b.py", line 6, in check     assert by_length([9, 4, 8]) == ["Nine", "Eight", "Four"]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_105.py
+++ Python_105.py
@@ -1,8 +1,2 @@
 def by_length(arr):
-    result = sorted([i for i in arr if 1 <= i <= 9])
-    return [
-        name
-        for name in ["One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"]
-        for num in result
-        if str(num) == str(name)[0]
-    ]
+    return [name for num, name in zip(map(str, arr), ["One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"]) if set(name[0]) == {"1"}]
