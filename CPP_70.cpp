b58c936f4b0cbb91336cd11702188f482be2e3df
beep boop <githubbot@vadim.me>
2024-07-06 07:54:09 +0200

    Solution 90 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/xmlozhdh Prompt: 62b0fced-626b-485a-add8-0a952e1ae79a.cpp:36:5: note: ‘int main()’ previously def ined here    36 | int main() {       |     ^~~~ In file included from 62b0fced-626b-485a-add8-0a952e1ae79a.cpp:43: 62b0fced-626b-485a-add8-0a952e1ae79a.cpp: In function ‘int main()’: 62b0fced-626b-485a-add8-0a952e1ae79a.cpp:53:37: error: cannot bind non-const lva lue reference of type ‘std::vector<int>&’ to an rvalue of type ‘std::vector<int> ’    53 |     assert (issame(strange_sort_list({111111}) , {111111}));       |                    ~~~~~~~~~~~~~~~~~^~~~~~~~~~ In file included from /usr/include/c++/11/vector:67,                  from 62b0fced-626b-485a-add8-0a952e1ae79a.cpp:1: /usr/include/c++/11/bits/stl_vector.h:625:7: note:   after user-defined conversi on: ‘std::vector<_Tp, _Alloc>::vector(std::initializer_list<_Tp>, const allocato r_type&) [with _Tp = int; _Alloc = std::allocator<int>; std::vector<_Tp, _Alloc> ::allocator_type = std::allocator<int>]’   625 |       vector(initializer_list<value_type> __l,       |       ^~~~~~ 62b0fced-626b-485a-add8-0a952e1ae79a.cpp:13:42: note:   initializing argument 1 of ‘void strange_sort_list(std::vector<int>&)’    13 | void strange_sort_list(std::vector<int>& lst) {       |                        ~~~~~~~~~~~~~~~~~~^~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -1,8 +1,41 @@
-void strange_sort_list(std::vector<int>& lst) {
+#include <vector>
+#include <algorithm>
+
+bool issame(std::vector<int> a, std::vector<int> b) {
+    if (a.size() != b.size())
+        return false;
+    for (int i = 0; i < a.size(); i++)
+        if (a[i] != b[i])
+            return false;
+    return true;
+}
+
+std::vector<int> strange_sort_list(std::vector<int>& lst) {
     std::vector<int> result;
     while (!lst.empty()) {
         int min_val = *std::min_element(lst.begin(), lst.end());
         result.push_back(min_val);
-        lst.erase(std::remove(lst.begin(), lst.end(), min_val), lst.end());
+        lst.erase(std::remove_if(lst.begin(), lst.end(), [min_val](int i){ return i == min_val; }), lst.end());
+
+        if (!lst.empty()) {
+            int max_val = *std::max_element(lst.begin(), lst.end());
+            std::vector<int> temp;
+            for (int i : lst) {
+                if (i != max_val) {
+                    temp.push_back(i);
+                }
+            }
+            result.insert(result.end(), temp.begin(), temp.end());
+        } else {
+            int min_val2 = *std::min_element(lst.begin(), lst.end());
+            result.push_back(min_val2);
+        }
     }
+    return result;
+}
+
+int main() {
+    std::vector<int> lst = {1, 2, 3};
+    assert(issame(strange_sort_list(lst), {111111}));
+    return 0;
 }
