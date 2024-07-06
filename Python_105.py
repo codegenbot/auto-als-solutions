b6447e7378647746d69741c635ffacfa47f0573d
beep boop <githubbot@vadim.me>
2024-07-06 03:22:20 +0200

    Solution 46 of Python/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/ubalkc5i Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a67762f8-1b6a-416f-b0e0-7bd6b2cb7 0ef.py", line 10, in <module>     check(by_length)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a67762f8-1b6a-416f-b0e0-7bd6b2cb7 0ef.py", line 7, in check     assert by_length([9, 4, 8]) == ["Nine", "Eight", "Four"]            ^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a67762f8-1b6a-416f-b0e0-7bd6b2cb7 0ef.py", line 3, in by_length     return [name for num, name in zip(map(str, arr), [mapping[x] for x in map(st r, arr)]) if set(str(num))[0] == "1"]                                                      ^^^^^^^^^^^^^^^^^^^^^^^^^^^ ^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a67762f8-1b6a-416f-b0e0-7bd6b2cb7 0ef.py", line 3, in <listcomp>     return [name for num, name in zip(map(str, arr), [mapping[x] for x in map(st r, arr)]) if set(str(num))[0] == "1"]                                                       ~~~~~~~^^^ KeyError: '9'

--- Python_105.py
+++ Python_105.py
@@ -1,4 +1,6 @@
+def get_name(x):
+    mapping = {"1": "One", "2": "Two", "3": "Three", "4": "Four", "5": "Five", "6": "Six", "7": "Seven", "8": "Eight", "9": "Nine"}
+    return mapping[str(x)]
+
 def by_length(arr):
-    mapping = {"1": "One", "2": "Two", "3": "Three", "4": "Four", "5": "Five", 
-               "6": "Six", "7": "Seven", "8": "Eight", "9": "Nine"}
-    return [mapping[str(num)] for num in arr if str(num)[0] == "1"]
+    return [name for num, name in zip(map(str, arr), [get_name(x) for x in arr]) if set(str(num))[0] == "1"]
