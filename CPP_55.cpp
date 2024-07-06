076d4211f0992b56bee40a0a3261a1da217b7309
beep boop <githubbot@vadim.me>
2024-07-06 04:39:41 +0200

    Solution 84 of CPP/55, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/sa7iw70j Prompt: d3c03ebb-4cbf-458a-b8ec-36518f9616d3.cpp:28:5: error: redefinition of ‘int main( )’    28 | int main(){       |     ^~~~ d3c03ebb-4cbf-458a-b8ec-36518f9616d3.cpp:18:5: note: ‘int main()’ previously def ined here    18 | int main()       |     ^~~~

--- CPP_55.cpp
+++ CPP_55.cpp
@@ -16,11 +16,10 @@ int fib(int n) {
     return result;
 }
 
-int main() 
+void main() 
 {
     int n;
     cout << "Enter the value of n: ";
     cin >> n;
     cout << "The " << n << "-th Fibonacci number is: " << fib(n) << endl;
-    return 0;
 }
