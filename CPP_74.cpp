638d91a42c140d2dfa95cddac56c06303d9ff474
beep boop <githubbot@vadim.me>
2024-07-06 03:56:33 +0200

    Solution 36 of CPP/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/k9lvoh5f Prompt:       |                                      ^~~~~~ 887def59-d00c-4243-ae64-090c4047fb9f.cpp:5:38: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from 887def59-d00c-4243-ae64-090c4047fb9f.cpp:3: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 887def59-d00c-4243-ae64-090c4047fb9f.cpp:3: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 887def59-d00c-4243-ae64-090c4047fb9f.cpp:5:46: error: ‘b’ was not declared in th is scope     5 | bool issame(vector<string> a, vector<string> b) {       |                                              ^ 887def59-d00c-4243-ae64-090c4047fb9f.cpp:5:47: error: expression list treated as  compound expression in initializer [-fpermissive]     5 | bool issame(vector<string> a, vector<string> b) {       |                                               ^ 887def59-d00c-4243-ae64-090c4047fb9f.cpp:9:1: error: ‘vector’ does not name a ty pe     9 | vector<string> total_match(vector<string> lst1, vector<string> lst2) {       | ^~~~~~

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -1,41 +1,43 @@
+```
 #include <vector>
 #include <algorithm>
 #include <string>
 
-bool issame(std::vector<std::string> a) {
-    return true;
+bool issame(vector<string> a, vector<string> b) {
+    return a == b;
 }
 
-std::vector<std::string> total_match(std::vector<std::string> lst1, std::vector<std::string> lst2) {
+vector<vector<string>> total_match(vector<vector<string>> lst1, vector<vector<string>> lst2) {
     int sum1 = 0;
-    for (const auto& str : lst1) {
+    for (const auto& str : lst1[0]) {
         sum1 += str.length();
     }
     
     int sum2 = 0;
-    for (const auto& str : lst2) {
+    for (const auto& str : lst2[0]) {
         sum2 += str.length();
     }
 
-    if (sum1 < sum2) {
+    if (issame(lst1, lst2)) {
+        return lst1;
+    } else if (sum1 < sum2) {
         return lst1;
     } else if (sum1 > sum2) {
         return lst2;
     }
     
     int sum1lst = 0, sum2lst = 0;
-    for (const auto& str : lst1) {
+    for (const auto& str : lst1[0]) {
         sum1lst += str.length();
         for (char c : str) {
             sum1lst += 1;
         }
     }
 
-    for (const auto& str : lst2) {
+    for (const auto& str : lst2[0]) {
         sum2lst += str.length();
         for (char c : str) {
             sum2lst += 1;
-        }
     }
 
     if (sum1lst < sum2lst) {
@@ -43,14 +45,14 @@ std::vector<std::string> total_match(std::vector<std::string> lst1, std::vector<
     } else if (sum1lst > sum2lst) {
         return lst2;
     } else {
-        for (const auto& str : lst1) {
-            for (const auto& sub_str : lst2) {
+        for (const auto& str : lst1[0]) {
+            for (const auto& sub_str : lst2[0]) {
                 if (str.find(sub_str) != std::string::npos || sub_str.find(str) != std::string::npos) {
                     return lst1;
                 }
             }
         }
 
-        return lst1.size() < lst2.size() ? lst1 : lst2;
+        return lst1[0].size() < lst2[0].size() ? lst1 : lst2;
     }
 }
