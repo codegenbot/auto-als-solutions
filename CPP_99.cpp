c0a64878c53323b5a51b3243c486deac05ee81f5
beep boop <githubbot@vadim.me>
2024-07-06 22:26:59 +0200

    Solution 13 of CPP/99, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/tlovojjh Prompt: Complete the following code given the task description and function signature.

--- CPP_99.cpp
+++ CPP_99.cpp
@@ -1,8 +1,2 @@
-int closest_integer(string value) {
-    double num = stod(value);
-    if (num - static_cast<int>(num) < static_cast<int>(num) - (num + 1)) {
-        return static_cast<int>(num);
-    } else {
-        return static_cast<int>(num) + 1;
-    }
-}
+double num = stod(value);
+return (num >= 0) ? ceil(num) : floor(num);
