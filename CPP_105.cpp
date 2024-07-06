943bac1e4f8133663971ce334f63c4e3a2f884a9
beep boop <githubbot@vadim.me>
2024-07-06 22:37:38 +0200

    Solution 17 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/3uehbgph Prompt:       |                              ^~~~~~ 60bcd31c-8581-479c-8a95-0997245f52d5.cpp:51:37: error: ‘string’ was not declared  in this scope    51 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 60bcd31c-8581-479c-8a95-0997245f52d5.cpp:51:44: error: ‘b’ was not declared in t his scope    51 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 60bcd31c-8581-479c-8a95-0997245f52d5.cpp:51:45: error: expression list treated a s compound expression in initializer [-fpermissive]    51 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 60bcd31c-8581-479c-8a95-0997245f52d5.cpp:50: 60bcd31c-8581-479c-8a95-0997245f52d5.cpp: In function ‘int main()’: 60bcd31c-8581-479c-8a95-0997245f52d5.cpp:60:20: error: ‘by_length’ was not decla red in this scope    60 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ 60bcd31c-8581-479c-8a95-0997245f52d5.cpp:60:19: error: ‘issame’ cannot be used a s a function    60 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -10,49 +10,16 @@ bool issame(vector<string> a, vector<string> b) {
     return true;
 }
 
-vector<vector<string>> group_by_length(vector<int> arr) {
-    vector<vector<string>> result;
-    map<int, vector<string>> mp;
-
+vector<string> by_length(vector<int> arr) {
+    vector<string> numbers;
     for (int num : arr) {
         if (num >= 1 && num <= 9) {
-            string str = "";
-            switch (num) {
-                case 1:
-                    str = "One";
-                    break;
-                case 2:
-                    str = "Two";
-                    break;
-                case 3:
-                    str = "Three";
-                    break;
-                case 4:
-                    str = "Four";
-                    break;
-                case 5:
-                    str = "Five";
-                    break;
-                case 6:
-                    str = "Six";
-                    break;
-                case 7:
-                    str = "Seven";
-                    break;
-                case 8:
-                    str = "Eight";
-                    break;
-                case 9:
-                    str = "Nine";
-                    break;
-            }
-            mp[str.length()].push_back(str);
+            numbers.push_back(to_string(num));
         }
     }
 
-    for (auto it = mp.begin(); it != mp.end(); ++it) {
-        result.push_back(it->second);
-    }
+    sort(numbers.begin(), numbers.end());
+    reverse(numbers.begin(), numbers.end());
 
-    return result;
+    return numbers;
 }
