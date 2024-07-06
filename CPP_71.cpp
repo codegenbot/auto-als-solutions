5c7be80ba8fc4026a5178990a6a03654978b3582
beep boop <githubbot@vadim.me>
2024-07-06 20:38:56 +0200

    Solution 27 of CPP/71, ratio 0.8888888888888888 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/3skxldrd Prompt: Program failed

--- CPP_71.cpp
+++ CPP_71.cpp
@@ -4,8 +4,7 @@ using namespace std;
 
 float triangle_area(float a, float b, float c) {
     if (a + b > c && a + c > b && b + c > a) {
-        float s = (a + b + c) / 2.0;
-        return round((sqrt(s * (s - a) * (s - b) * (s - c))) * 100.0) / 100.0;
+        return (a * b * c) ? sqrt(float(a)*float(b)*float(c)/4.0) : -1.0f;
     } else {
         return -1.0f;
     }
