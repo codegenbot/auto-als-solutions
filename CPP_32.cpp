b200c4e4daaa41fcd70103b2f2907066bf679a65
beep boop <githubbot@vadim.me>
2024-07-06 23:57:53 +0200

    Solution 29 of CPP/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/0ujhsbw6 Prompt: ed928f6f-281d-43e8-9a78-19f5d86d8218.cpp:22:13: error: ‘coeffs’ was not declared  in this scope; did you mean ‘coeff’?    22 |             coeffs.push_back(coeff);       |             ^~~~~~       |             coeff ed928f6f-281d-43e8-9a78-19f5d86d8218.cpp:25:30: error: ‘coeffs’ was not declared  in this scope; did you mean ‘ncoeff’?    25 |         solution = find_zero(coeffs);       |                              ^~~~~~       |                              ncoeff ed928f6f-281d-43e8-9a78-19f5d86d8218.cpp:25:36: error: ‘find_zero’ cannot be use d as a function    25 |         solution = find_zero(coeffs);       |                                    ^ In file included from ed928f6f-281d-43e8-9a78-19f5d86d8218.cpp:9: ed928f6f-281d-43e8-9a78-19f5d86d8218.cpp:26:21: error: ‘poly’ was not declared i n this scope    26 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                     ^~~~ ed928f6f-281d-43e8-9a78-19f5d86d8218.cpp:26:17: error: ‘abs’ was not declared in  this scope    26 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                 ^~~

--- CPP_32.cpp
+++ CPP_32.cpp
@@ -1,19 +1,19 @@
-vector<double> coeffs(xs.begin() + 1, xs.end());
+vector<double> coeffs;
+int ncoeff;
 double poly(double x) {
     double sum = 0;
-    for (int i = 0; i < coeffs.size(); i++) {
+    for (int i = 0; i < ncoeff; i++) {
         sum += coeffs[i] * pow(x, i);
     }
     return sum;
 }
+
 double find_zero(vector<double> xs){
-    double ncoeff = 1;
-    for (int i = 2; i < xs.size(); i+=2) {
-        ncoeff *= -xs[i-1];
+    int ncoeff = xs.size();
+    coeffs = xs;
+    double x = 1.0;
+    for (int i = 2; i < ncoeff; i+=2) {
+        x = -poly(x) / poly(1);
     }
-    double solution = 0;
-    for (int i = 2; i < xs.size(); i+=2) {
-        solution += xs[i] * pow(-solution / xs[i-1], i);
-    }
-    return round(solution, 10);
+    return round(x, 10);
 }
