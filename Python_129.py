d80d2bfcade5ce4ec995406efba73ea2a64e03cf
beep boop <githubbot@vadim.me>
2024-07-06 05:48:34 +0200

    Solution 63 of Python/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/antrahxj Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/efb18664-0dc0-4334-952c-0de8e9f37 aaf.py", line 49, in <module>     check(minPath)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/efb18664-0dc0-4334-952c-0de8e9f37 aaf.py", line 46, in check     assert minPath([[1, 3], [3, 2]], 10) == [1, 3, 1, 3, 1, 3, 1, 3, 1, 3]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/efb18664-0dc0-4334-952c-0de8e9f37 aaf.py", line 35, in minPath     res = [p for p in [dfs(i, j, [m[i][j]], k)] if p]                                   ~^^^ TypeError: list indices must be integers or slices, not tuple

--- Python_129.py
+++ Python_129.py
@@ -9,9 +9,7 @@ def minPath(grid, k):
         if (i, j) in visited:
             return None
         if 0 <= i < n and 0 <= j < n and grid[i][j] == k:
-            new_sum = sum_path + k
-            new_path = path + [m[i][j]]
-            return ((new_path), new_sum)
+            return ((path + [m[i][j]]), sum_path + k)
         if 0 <= i < n and 0 <= j < n and grid[i][j] != k:
             return None
         for x, y in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
@@ -19,7 +17,7 @@ def minPath(grid, k):
             if 0 <= ni < n and 0 <= nj < n:
                 new_path, new_sum = dfs(ni, nj, path + [m[ni][nj]], sum_path + k)
                 if new_path:
-                    res.append(new_path[0])
+                    res.append(new_path)
         return (
             min([p for p in set(tuple(x) for x in res) if p], key=lambda x: len(x))
             or []
@@ -35,8 +33,7 @@ def minPath(grid, k):
     for i, j in boundary_cells:
         visited.add((i, j))
         res = dfs(i, j, [], k)
-        if res[0]:
-            min_path = res[0]
+        if res:
+            min_path = min([path for path in set(tuple(x) for x in res)], key=lambda x: len(x))
             break
-
     return min_path if min_path else []
