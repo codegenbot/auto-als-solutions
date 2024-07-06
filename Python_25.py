b1168710b0a1be2aa3e1c032de61ec31fcdbc95c
beep boop <githubbot@vadim.me>
2024-07-06 14:26:25 +0200

    Solution 21 of Python/25, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/or3xldlf Prompt: Program did not receive expected input

--- Python_25.py
+++ Python_25.py
@@ -1,10 +1,12 @@
-```
+```Python
 from typing import List
 import math
 
 
 def factorize(n: int) -> List[int]:
-    if n < 2:
+    if n <= 0:
+        raise ValueError("Input should be greater than zero.")
+    elif n < 2:
         return [n]
     factors = []
     i = 2
