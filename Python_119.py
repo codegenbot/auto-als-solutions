5c260fe73f79cee3bfb363834928752267d2a851
beep boop <githubbot@vadim.me>
2024-07-06 05:01:01 +0200

    Solution 44 of Python/119, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/629l2qg8 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ea675885-2c60-4ea7-b66d-12cbf90c6 2ba.py", line 25, in <module>     check(match_parens)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ea675885-2c60-4ea7-b66d-12cbf90c6 2ba.py", line 22, in check     assert match_parens([")", "("]) == "Yes"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_119.py
+++ Python_119.py
@@ -3,17 +3,13 @@ def match_parens(lst):
     for string in lst:
         open_count = 0
         close_count = 0
-        matching = True
-        
         for char in string:
             if char == '(':
                 open_count += 1
             elif char == ')':
-                close_count += 1
-        
-        if open_count != close_count:
-            matching = False
-        
-        if not matching:
+                if open_count == 0:
+                    return "No"
+                open_count -= 1
+        if open_count != 0:
             return "No"
     return "Yes"
