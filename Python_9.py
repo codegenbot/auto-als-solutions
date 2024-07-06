f4ab3dd027a7a580c85ffb81e30e165856147e8c
beep boop <githubbot@vadim.me>
2024-07-06 03:47:18 +0200

    Solution 87 of Python/9, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/u5llv5d8 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/90a8fb7f-4829-416c-8259-605cad177 733.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_9.py
+++ Python_9.py
@@ -2,8 +2,8 @@ def max_rolling_sum(lst):
     if len(lst) == 0:
         return 0
     current_max = max_window = sum(lst[:1])
-    for i in range(1, len(lst)):
-        current_max = max(current_max, sum(lst[i-1:i+1]))
+    for window in zip(*[iter(lst)]*2):
+        current_max = max(current_max, sum(window))
     return current_max
 
 print(max_rolling_sum([1, 2]))
