716dc038bfb1c024bf15034a0caaa44d06ce0201
beep boop <githubbot@vadim.me>
2024-07-06 07:16:05 +0200

    Solution 63 of Python/103, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/fw0szpvy Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/bbd61828-894e-4688-9d66-f5ccb3b30 766.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_103.py
+++ Python_103.py
@@ -1,6 +1,9 @@
 def rounded_avg():
     n = int(input("Enter the first number: "))
-    avg1 = round(n / 2)
+    avg1 = round(n / 1)
     m = int(input("Enter the second number: "))
-    avg2 = round(m / 2)
-    return bin(int(avg1 + avg2))[2:]
+    avg2 = round(m / 1)
+    total_avg = (avg1 + avg2) / 2
+    return bin(int(total_avg))[2:]
+
+print(rounded_avg())
