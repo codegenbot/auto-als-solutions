baedfd0694128c860b2ad961dbbe8d3b4ab2afe6
beep boop <githubbot@vadim.me>
2024-07-06 19:06:33 +0200

    Solution 62 of CPP/20, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/p6t224qv Prompt:    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 999a6670-be77-43cb-b5f2-83edde8eafbb.cpp:21:36: error: expected primary-expressi on before ‘float’    21 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 999a6670-be77-43cb-b5f2-83edde8eafbb.cpp:21:43: error: expression list treated a s compound expression in initializer [-fpermissive]    21 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 999a6670-be77-43cb-b5f2-83edde8eafbb.cpp:20: 999a6670-be77-43cb-b5f2-83edde8eafbb.cpp: In function ‘int main()’: 999a6670-be77-43cb-b5f2-83edde8eafbb.cpp:30:20: error: ‘find_closest_elements’ w as not declared in this scope    30 |     assert (issame(find_closest_elements({1.1, 2.2, 3.1, 4.1, 5.1}) , {2 .2, 3.1}));       |                    ^~~~~~~~~~~~~~~~~~~~~ 999a6670-be77-43cb-b5f2-83edde8eafbb.cpp:30:19: error: ‘issame’ cannot be used a s a function    30 |     assert (issame(find_closest_elements({1.1, 2.2, 3.1, 4.1, 5.1}) , {2 .2, 3.1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~

--- CPP_20.cpp
+++ CPP_20.cpp
@@ -1,28 +1,6 @@
-#include <algorithm>
-#include <numeric>
-
 bool issame(vector<float> a,vector<float>b){
     return (a==b);
-}
-
-vector<float> find_closest_elements(vector<float> numbers) {
-    sort(numbers.begin(), numbers.end());
-    float minDiff = numeric_limits<float>::max();
-    pair<float, float> closestPair;
-
-    for (int i = 0; i < numbers.size() - 1; ++i) {
-        float diff = numbers[i + 1] - numbers[i];
-        if (diff < minDiff) {
-            minDiff = diff;
-            closestPair.first = numbers[i];
-            closestPair.second = numbers[i + 1];
-        }
-    }
-
-    return {closestPair.first, closestPair.second};
-}
 
 int main() {
     assert(find_closest_elements({1.1, 2.2, 3.1, 4.1, 5.1}) == vector<float>{2.2, 3.1});
-    // rest of your code
 }
