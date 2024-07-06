27a6419c3eb918225108481b3534024b8252f95a
beep boop <githubbot@vadim.me>
2024-07-06 07:54:14 +0200

    Solution 32 of CPP/125, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/g4r9w7pd Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from d1b8a721-b79d-4c7f-b771-52d1f0065fc4.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_125.cpp
+++ CPP_125.cpp
@@ -1,29 +1,22 @@
+```cpp
+#include <vector>
+#include <string>
+#include <initializer_list>
+
 bool issame(const std::vector<std::string>& a, const std::vector<std::string>& b) {
-    return a.size() == b.size() && std::equal(a.begin(), a.end(), b.begin());
+    return a == b;
 }
 
-std::vector<std::vector<std::string>> split_words(string txt) {
-    vector<vector<string>> result = {};
+std::vector<std::string> split_words(string txt) {
+    vector<string> result = {};
     size_t pos = 0;
     while ((pos = txt.find(' ')) != string::npos) {
-        vector<string> temp = {};
-        for(int i = 0; i <= pos; i++) {
-            if(i < txt.length()) {
-                temp.push_back(txt.substr(0, i+1));
-                txt.erase(0, i+1);
-            }
-        }
-        result.push_back(temp);
+        result.push_back(txt.substr(0, pos));
+        txt.erase(0, pos + 1);
     }
-    if (!txt.empty()) {
-        vector<string> temp = {};
-        for(int i = 0; i <= txt.length(); i++) {
-            if(i < txt.length()) {
-                temp.push_back(txt.substr(0, i+1));
-                txt.erase(0, i+1);
-            }
-        }
-        result.push_back(temp);
+    if (txt.empty()) {
+        return result;
     }
+    result.push_back(txt);
     return result;
 }
