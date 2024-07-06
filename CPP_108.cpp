450c1609ab3674cb0d36ccd1f816e7a0f8b724b1
beep boop <githubbot@vadim.me>
2024-07-06 05:59:27 +0200

    Solution 20 of CPP/108, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/ic7n1rvg Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from a8db1b9a-8d58-4afc-a661-244854d40bd0.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_108.cpp
+++ CPP_108.cpp
@@ -1,19 +1,22 @@
 #include <initializer_list>
+#include <vector>
 
 int count_nums(std::vector<int> nums) {
     int count = 0;
-    for (int num : nums) {
+    std::size_t _M_len;
+    constexpr std::size_t size() const { return _M_len; }
+    for (std::size_t i = 0; i < size(); ++i) {
         bool has_positive_sum = false;
-        if (num < 0) {
-            num = -num;
+        if (nums[i] < 0) {
+            nums[i] = -nums[i];
         }
-        while (num > 0) {
-            int digit = num % 10;
+        while (nums[i] > 0) {
+            int digit = nums[i] % 10;
             if (digit != 0) {
                 has_positive_sum = true;
                 break;
             }
-            num /= 10;
+            nums[i] /= 10;
         }
         if (has_positive_sum) {
             count++;
