3b3f20054e69b11f941f5e0a721ebd80c0021dac
beep boop <githubbot@vadim.me>
2024-07-07 01:12:32 +0200

    Solution 20 of CPP/58, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/s2umepel Prompt:       |         ^~~ 07a16d1c-ca30-4ccf-92a8-82a4949fdea4.cpp:8:9: error: expected primary-expression  before ‘int’     8 |     set<int> s2(l2.begin(), l2.end());       |         ^~~ 07a16d1c-ca30-4ccf-92a8-82a4949fdea4.cpp:10:9: error: expected primary-expressio n before ‘int’    10 |     set<int> intersection;       |         ^~~ 07a16d1c-ca30-4ccf-92a8-82a4949fdea4.cpp:11:22: error: ‘s1’ was not declared in this scope; did you mean ‘l1’?    11 |     set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(),       |                      ^~       |                      l1 07a16d1c-ca30-4ccf-92a8-82a4949fdea4.cpp:11:44: error: ‘s2’ was not declared in this scope; did you mean ‘l2’?    11 |     set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(),       |                                            ^~       |                                            l2 07a16d1c-ca30-4ccf-92a8-82a4949fdea4.cpp:12:33: error: ‘intersection’ was not de clared in this scope    12 |                        inserter(intersection, intersection.begin()));       |                                 ^~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
+#include <set>
 #include <algorithm>
+
 using namespace std;
 
 vector<int> common(vector<int> l1, vector<int> l2) {
@@ -8,8 +10,8 @@ vector<int> common(vector<int> l1, vector<int> l2) {
     set<int> s2(l2.begin(), l2.end());
 
     set<int> intersection;
-    set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(),
-                       inserter(intersection, intersection.begin()));
+    set_difference(s2.begin(), s2.end(), s1.begin(), s1.end(),
+                    inserter(intersection, intersection.begin()));
 
     vector<int> result(intersection.begin(), intersection.end());
     return result;
