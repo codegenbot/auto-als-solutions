79d045fc3fdfa6945ed1daea77b9778ed9933a5f
beep boop <githubbot@vadim.me>
2024-07-06 20:30:15 +0200

    Solution 39 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/t59aw96g Prompt:                  from /usr/include/c++/11/algorithm:74,                  from cf425b02-094b-4229-90d5-ad38c640edb8.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ cf425b02-094b-4229-90d5-ad38c640edb8.cpp:18:34: error: expected primary-expressi on before ‘int’    18 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ cf425b02-094b-4229-90d5-ad38c640edb8.cpp:18:39: error: expression list treated a s compound expression in initializer [-fpermissive]    18 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from cf425b02-094b-4229-90d5-ad38c640edb8.cpp:17: cf425b02-094b-4229-90d5-ad38c640edb8.cpp: In function ‘int main()’: cf425b02-094b-4229-90d5-ad38c640edb8.cpp:27:20: error: ‘strange_sort_list’ was n ot declared in this scope    27 |     assert (issame(strange_sort_list({111111}) , {111111}));       |                    ^~~~~~~~~~~~~~~~~ cf425b02-094b-4229-90d5-ad38c640edb8.cpp:27:19: error: ‘issame’ cannot be used a s a function    27 |     assert (issame(strange_sort_list({111111}) , {111111}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -6,7 +6,7 @@ bool issame(const std::vector<int>& a, const std::vector<int>& b) {
 }
 
 std::vector<int> strange_sort_list(std::vector<int> lst) {
-    std::vector<int> result;
+    vector<int> result;
     while (!lst.empty()) {
         int min_val = *min_element(lst.begin(), lst.end());
         int max_val = *max_element(lst.begin(), lst.end());
@@ -17,15 +17,4 @@ std::vector<int> strange_sort_list(std::vector<int> lst) {
         lst.erase(std::remove(lst.begin(), lst.end(), max_val), lst.end());
     }
     return result;
-}
-
-int main() {
-    std::vector<int> input = {1, 3, 2};
-    std::vector<int> output = strange_sort_list(input);
-    if (issame(output, {1, 3, 2})) {
-        std::cout << "Strange Sort List works correctly." << std::endl;
-    } else {
-        std::cout << "Strange Sort List does not work correctly." << std::endl;
-    }
-    return 0;
 }
