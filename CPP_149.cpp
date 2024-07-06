83bc1e71108141f13d77b79552a3376242987c89
beep boop <githubbot@vadim.me>
2024-07-06 10:22:16 +0200

    Solution 32 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/tenvpvw5 Prompt: r> >)’ previously defined here     8 | bool issame(vector<string> a, vector<string> b) {       |      ^~~~~~ aee347d9-3dcb-4893-a991-8caea36add18.cpp:52:5: error: redefinition of ‘int std:: main()’    52 | int main(){       |     ^~~~ aee347d9-3dcb-4893-a991-8caea36add18.cpp:38:5: note: ‘int std::main()’ previousl y defined here    38 | int main() {       |     ^~~~ aee347d9-3dcb-4893-a991-8caea36add18.cpp:54:2: error: expected ‘}’ at end of inp ut    54 | }       |  ^ aee347d9-3dcb-4893-a991-8caea36add18.cpp:5:15: note: to match this ‘{’     5 | namespace std {       |               ^ aee347d9-3dcb-4893-a991-8caea36add18.cpp: In function ‘int std::main()’: aee347d9-3dcb-4893-a991-8caea36add18.cpp:54:1: warning: control reaches end of n on-void function [-Wreturn-type]    54 | }       | ^

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -3,7 +3,8 @@
 #include <string>
 
 namespace std {
-using namespace std;
+    using namespace std;
+}
 
 bool issame(vector<string> a, vector<string> b) {
     if (a.size() != b.size())
@@ -35,7 +36,7 @@ vector<string> sorted_list_sum(vector<string> lst) {
     return result;
 }
 
-}int main() {
-    assert(issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}), {"cc", "dd", "aaaa", "bbbb"}));
+int main() {
+    vector<string> lst = {"aaaa", "bbbb", "dd", "cc"};
+    assert(issame(sorted_list_sum(lst), {"cc", "dd", "aaaa", "bbbb"}));
     return 0;
-}
