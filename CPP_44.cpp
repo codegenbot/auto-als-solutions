89da63c72596f22645972e87f65dc7d44fefc8bb
beep boop <githubbot@vadim.me>
2024-07-06 02:04:42 +0200

    Solution 62 of CPP/44, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/vm7zlok5 Prompt: 148cc097-5719-4478-8028-21389e5af55c.cpp:1:1: error: stray ‘`’ in program     1 | ```       | ^ 148cc097-5719-4478-8028-21389e5af55c.cpp:1:2: error: stray ‘`’ in program     1 | ```       |  ^ 148cc097-5719-4478-8028-21389e5af55c.cpp:1:3: error: stray ‘`’ in program     1 | ```       |   ^ 148cc097-5719-4478-8028-21389e5af55c.cpp:26:5: error: redefinition of ‘int main( )’    26 | int main(){       |     ^~~~ 148cc097-5719-4478-8028-21389e5af55c.cpp:16:5: note: ‘int main()’ previously def ined here    16 | int main() {       |     ^~~~

--- CPP_44.cpp
+++ CPP_44.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 
 string change_base(int x, int base) {
-    string s = string();
+    string res = "";
     while (x > 0) {
         int rem = x % base;
-        char c = rem < 10 ? '0' + rem : rem > 9 ? 'A' + rem - 10 : '-';
-        s.insert(0,1,c);
+        if(rem >= 10) res.insert(0, 1, (char)(rem - 55));
+        else res.insert(0, 1, '0' + rem);
         x /= base;
     }
-    return s;
+    return res;
 }
 
 int main() {
