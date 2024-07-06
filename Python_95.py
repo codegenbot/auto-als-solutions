1b143ceb888a0b5c2f7b10f60c8e459d0e30f71c
beep boop <githubbot@vadim.me>
2024-07-06 12:55:36 +0200

    Solution 3 of Python/95, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/vtmlyt3f Prompt: Complete the following code given the task description and function signature.

--- Python_95.py
+++ Python_95.py
@@ -2,7 +2,7 @@
 def check_dict_case(dict):
     if len(dict) == 0:
         return False
-    keys = [key for key in dict.keys()]
+    keys = [key for key in dict]
     all_lower = all(key.islower() for key in keys)
     all_upper = all(key.isupper() for key in keys)
     return all_lower or all_upper
