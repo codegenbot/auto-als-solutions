05505c0656dd01e34781eb728dd931eb302620a6
beep boop <githubbot@vadim.me>
2024-07-07 00:01:26 +0200

    Solution 44 of CPP/27, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/7te09xjm Prompt: 2eefed1c-850e-4485-b022-ad205a1280ce.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string flip_case(string str){       | ^~~~~~ In file included from 2eefed1c-850e-4485-b022-ad205a1280ce.cpp:13: 2eefed1c-850e-4485-b022-ad205a1280ce.cpp: In function ‘int main()’: 2eefed1c-850e-4485-b022-ad205a1280ce.cpp:15:13: error: ‘filp_case’ was not decla red in this scope    15 |     assert (filp_case("These violent delights have violent ends") == "tH ESE VIOLENT DELIGHTS HAVE VIOLENT ENDS");       |             ^~~~~~~~~

--- CPP_27.cpp
+++ CPP_27.cpp
@@ -1,7 +1,7 @@
 #include <string>
 
-string flip_case(string str){
-    string result = "";
+std::string flip_case(std::string str){
+    std::string result = "";
     for(char c : str){
         if(islower(c)){
             result += toupper(c);
