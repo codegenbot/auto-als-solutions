017467c6da75e2a5c1fe2abaf26c4105af628584
beep boop <githubbot@vadim.me>
2024-07-06 22:48:26 +0200

    Solution 39 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/3uehbgph Prompt:       |                              ^~~~~~ 60bcd31c-8581-479c-8a95-0997245f52d5.cpp:51:37: error: ‘string’ was not declared  in this scope    51 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 60bcd31c-8581-479c-8a95-0997245f52d5.cpp:51:44: error: ‘b’ was not declared in t his scope    51 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 60bcd31c-8581-479c-8a95-0997245f52d5.cpp:51:45: error: expression list treated a s compound expression in initializer [-fpermissive]    51 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 60bcd31c-8581-479c-8a95-0997245f52d5.cpp:50: 60bcd31c-8581-479c-8a95-0997245f52d5.cpp: In function ‘int main()’: 60bcd31c-8581-479c-8a95-0997245f52d5.cpp:60:20: error: ‘by_length’ was not decla red in this scope    60 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ 60bcd31c-8581-479c-8a95-0997245f52d5.cpp:60:19: error: ‘issame’ cannot be used a s a function    60 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -4,73 +4,18 @@ bool issame(vector<string> a, vector<string> b) {
 
 int main() {
     int b;
-    cin >> b;
-
     vector<int> arr;
-    for (int i = 0; i < b; ++i) {
-        int num;
-        cin >> num;
-        arr.push_back(num);
+    cout << "Enter the numbers: ";
+    for (int i = 0; i < 9; i++) {
+        cin >> b;
+        arr.push_back(b);
     }
-
-    vector<string> by_length(vector<int> arr) {
-        vector<int> numbers;
-        for (int num : arr) {
-            if (num >= 1 && num <= 9) {
-                numbers.push_back(num);
-            }
-        }
-
-        sort(numbers.begin(), numbers.end());
-        reverse(numbers.begin(), numbers.end());
-
-        vector<string> result;
-        for (int num : numbers) {
-            string str = "";
-            switch (num) {
-                case 1:
-                    str = "One";
-                    break;
-                case 2:
-                    str = "Two";
-                    break;
-                case 3:
-                    str = "Three";
-                    break;
-                case 4:
-                    str = "Four";
-                    break;
-                case 5:
-                    str = "Five";
-                    break;
-                case 6:
-                    str = "Six";
-                    break;
-                case 7:
-                    str = "Seven";
-                    break;
-                case 8:
-                    str = "Eight";
-                    break;
-                case 9:
-                    str = "Nine";
-                    break;
-            }
-            result.push_back(str);
-        }
-
-        return result;
+    
+    vector<string> result = by_length(arr);
+    
+    cout << "The numbers in descending order with their names are: ";
+    for (string str : result) {
+        cout << str << " ";
     }
-
-    vector<string> res = by_length(arr);
-
-    bool same = issame(res, {"One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"});
-
-    if (same) {
-        cout << "The output is the same as expected." << endl;
-    } else {
-        cout << "The output is different from expected." << endl;
-    }
-
     return 0;
 }
