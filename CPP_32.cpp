07eac69f37b9bed480cf90e5200f578b47201b6a
beep boop <githubbot@vadim.me>
2024-07-06 23:52:11 +0200

    Solution 5 of CPP/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/0ujhsbw6 Prompt: Complete the following code given the task description and function signature.

--- CPP_32.cpp
+++ CPP_32.cpp
@@ -1,20 +1,7 @@
-#include<stdio.h>
-#include<math.h>
-#include<vector>
-using namespace std;
-
-double poly(vector<double> xs, double x){
-    double sum=0;
-    int i;
-    for (i=0;i<xs.size();i++)
-    {
-        sum+=xs[i]*pow(x,i);
-    }
-    return sum;
-}
-
 double find_zero(vector<double> xs){
-    if(xs.size() % 2 != 0 || abs(xs[0]) < 1e-6) return -1; // Check for even number of coefficients and largest non zero coefficient
-    double x = (-xs[1]/xs[3]); 
-    return round(x, 2);
+    double sum = 0;
+    for(int i=1; i<xs.size();i+=2){
+        sum += (-1) * xs[i]/(double)i * pow(xs[0], (double)(i-1));
+    }
+    return -sum;
 }
