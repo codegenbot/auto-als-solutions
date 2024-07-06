e50653fe6d8690899d6afd35666a3145fdeb9aff
beep boop <githubbot@vadim.me>
2024-07-06 14:54:52 +0200

    Solution 12 of Python/55, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/2yonuxz1 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/888962b7-93d6-44a0-bcf4-91769865d 2a6.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_55.py
+++ Python_55.py
@@ -1,13 +1,12 @@
-def find_fibonacci():
+```
+def get_fibonacci():
     n = int(input("Enter a number: "))
     if n <= 0:
         print("Please enter a positive integer.")
     elif n == 1 or n == 2:
-        print("The Fibonacci sequence for", n, "is", 1)
+        print("Fibonacci sequence for", n, "is:", 1)
     else:
         a, b = 1, 1
-        fib_sequence = [1]
-        while len(fib_sequence) < n:
+        for _ in range(2, n):
             a, b = b, a + b
-            fib_sequence.append(a)
-        print("The Fibonacci sequence for", n, "is", fib_sequence)
+        print("Fibonacci sequence for", n, "is:", b)
