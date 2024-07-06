9a3ff1d921985eceed7b2f91fcf990602e103e32
beep boop <githubbot@vadim.me>
2024-07-06 06:21:38 +0200

    Solution 4 of CPP/127, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/o7kskobq Prompt: c89e72b4-96b5-4774-a042-10645a8e5a9f.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string intersection(vector<int> interval1, vector<int> interval2) {       | ^~~~~~ In file included from c89e72b4-96b5-4774-a042-10645a8e5a9f.cpp:20: c89e72b4-96b5-4774-a042-10645a8e5a9f.cpp: In function ‘int main()’: c89e72b4-96b5-4774-a042-10645a8e5a9f.cpp:22:13: error: ‘intersection’ was not de clared in this scope    22 |     assert (intersection({-2, -2}, {-3, -2}) == "NO");       |             ^~~~~~~~~~~~

--- CPP_127.cpp
+++ CPP_127.cpp
@@ -1,7 +1,6 @@
 #include <string>
-#include <vector>
 
-std::string intersection(std::vector<int> interval1, std::vector<int> interval2) {
+string intersection(vector<int> interval1, vector<int> interval2) {
     int start = max(interval1[0], interval2[0]);
     int end = min(interval1[1], interval2[1]);
 
@@ -18,9 +17,4 @@ std::string intersection(std::vector<int> interval1, std::vector<int> interval2)
     }
 
     return isPrime ? "YES" : "NO";
-}
-
-int main() {
-    assert (intersection({-2, -2}, {-3, -2}) == "NO");
-    return 0;
 }
