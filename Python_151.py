412b67a2b94e1a2fbc24a94208128ff462b1d4c4
beep boop <githubbot@vadim.me>
2024-07-06 04:34:10 +0200

    Solution 22 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/es6naen8 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d894d5c2-6ae2-45b8-bd77-db89b877e 0ca.py", line 14, in <module>     check(double_the_difference)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d894d5c2-6ae2-45b8-bd77-db89b877e 0ca.py", line 10, in check     double_the_difference(lst) == odd_sum                           ^^^ NameError: name 'lst' is not defined. Did you mean: 'list'?

--- Python_151.py
+++ Python_151.py
@@ -1,6 +1,8 @@
 def double_the_difference(lst):
+    even_sum = sum(i for i in lst if isinstance(i, int) and i % 2 == 0)
     odd_sum = sum(i for i in lst if isinstance(i, int) and i % 2 != 0)
-    return abs(sum(i**2 for i in lst if isinstance(i, int) and i > 0) - (2 * odd_sum))
+    
+    return abs(sum(i**2 for i in lst if isinstance(i, int)) - (2 * even_sum)) if odd_sum == 0 else abs(even_sum - (2 * odd_sum))
 
-input_list = [1, 2, 3, 4, 5]
+input_list = [1, 2, 3, 4]
 print(double_the_difference(input_list))
