1ac9a949014093c00ec7d2e45f318afa93d934c4
beep boop <githubbot@vadim.me>
2024-07-06 13:23:14 +0200

    Solution 4 of Python/130, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/sl1cdaq4 Prompt: Complete the following code given the task description and function signature.

--- Python_130.py
+++ Python_130.py
@@ -6,12 +6,11 @@ def tri(n):
     elif n == 2:
         return [3, 2]
     else:
-        sequence = [3, 2]
+        result = [3, 2]
         for i in range(2, n):
             if i % 2 == 0:
-                sequence.append(int((i + 1) / 2))
+                result.append(1 + i / 2)
             else:
-                last_two = sequence[-2:]
-                next_value = sum(last_two)
-                sequence.append(next_value)
-        return sequence
+                last_three = result[-3:]
+                result.append(sum(last_three))
+        return result
