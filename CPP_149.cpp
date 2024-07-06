f413fae816b5b6df9844ca54a7f3a227f57be3c3
beep boop <githubbot@vadim.me>
2024-07-06 13:10:35 +0200

    Solution 80 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/tenvpvw5 Prompt: In file included from 391d74f5-d8ca-4f4f-87ea-dd877f7e686a.cpp:3: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 391d74f5-d8ca-4f4f-87ea-dd877f7e686a.cpp:43:44: error: ‘b’ was not declared in t his scope    43 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 391d74f5-d8ca-4f4f-87ea-dd877f7e686a.cpp:51:5: error: redefinition of ‘int main( )’    51 | int main(){       |     ^~~~ 391d74f5-d8ca-4f4f-87ea-dd877f7e686a.cpp:35:5: note: ‘int main()’ previously def ined here    35 | int main() {       |     ^~~~ In file included from 391d74f5-d8ca-4f4f-87ea-dd877f7e686a.cpp:42: 391d74f5-d8ca-4f4f-87ea-dd877f7e686a.cpp: In function ‘int main()’: 391d74f5-d8ca-4f4f-87ea-dd877f7e686a.cpp:52:20: error: ‘sorted_list_sum’ was not  declared in this scope; did you mean ‘sortedListSum’?    52 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -2,7 +2,7 @@
 #include <algorithm>
 #include <string>
 
-bool issame(std::vector<std::string> a, std::vector<std::string> b) {
+bool vectorEqual(std::vector<std::string> a, std::vector<std::string> b) {
     if (a.size() != b.size())
         return false;
 
@@ -32,9 +32,9 @@ std::vector<std::string> sortedListSum(std::vector<std::string> lst) {
     return result;
 }
 
-int main(int argc, char** argv) {
+int main() {
     std::vector<std::string> vec1 = {"aaaa", "bbbb", "dd", "cc"};
-    std::vector<std::string> vec2 = {"cc", "dd", "aaaa", "bbbb"};
-    assert(issame(sortedListSum(vec1), vec2));
+    std::vector<std::string> vec2 = sortedListSum(vec1);
+    assert(vectorEqual(vec2, {"cc", "dd", "aaaa", "bbbb"}));
     return 0;
 }
