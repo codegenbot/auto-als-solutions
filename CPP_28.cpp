24d74c03a894f4e3d2a6e99e4f68873222db6e96
beep boop <githubbot@vadim.me>
2024-07-06 23:49:52 +0200

    Solution 20 of CPP/28, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/5qq2jnha Prompt: 732ce1d3-f3b7-42fd-acdb-825489f00b5f.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string concatenate(vector<string> strings){       | ^~~~~~ In file included from 732ce1d3-f3b7-42fd-acdb-825489f00b5f.cpp:9: 732ce1d3-f3b7-42fd-acdb-825489f00b5f.cpp: In function ‘int main()’: 732ce1d3-f3b7-42fd-acdb-825489f00b5f.cpp:11:13: error: ‘concatenate’ was not dec lared in this scope    11 |     assert (concatenate({"x", "y", "z", "w", "k"}) == "xyzwk");       |             ^~~~~~~~~~~

--- CPP_28.cpp
+++ CPP_28.cpp
@@ -1,15 +1,9 @@
 #include <string>
+#include <vector>
 
-string concatenate(vector<string> strings){
-    string result = "";
+std::string concatenate(std::vector<std::string> strings){
+    std::string result = "";
     for(string s : strings) {
         result += s;
     }
     return result;
-
-}
-
-int main() {
-    assert (concatenate({"x", "y", "z", "w", "k"}) == "xyzwk");
-    return 0;
-}
