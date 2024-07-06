ad804e126d7228ef997c3501f4adc22de04c302c
beep boop <githubbot@vadim.me>
2024-07-06 02:42:51 +0200

    Solution 4 of Python/104, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/jsnklyvz Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e78e7e7b-6e3e-4cce-b13b-6f3634ac4 07e.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_104.py
+++ Python_104.py
@@ -1,3 +1,4 @@
-```
 def unique_digits(x):
-    return sorted([i for i in x if not any(int(digit) % 2 == 0 for digit in str(i))])
+    return sorted(
+        [i for i in range(1, x + 1) if not any(int(digit) % 2 == 0 for digit in str(i))]
+    )
