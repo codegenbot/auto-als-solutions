09203419b05de1fc4d31a1f55ead5afd5c67ec1a
beep boop <githubbot@vadim.me>
2024-07-07 00:52:23 +0200

    Solution 0 of CPP/49, ratio 0.7142857142857143 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/dxgt1y9t Prompt: Complete the following code given the task description and function signature.

---
+++ CPP_49.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+using namespace std;
+
+int modp(int n, int p) {
+    if (n == 0)
+        return 1;
+    long long res = 1;
+    for (int i = 1; i <= n; i++) {
+        res = (res * i) % p;
+    }
+    return res;
+}
