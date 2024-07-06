e1133cd42048ef6ddaf159eda767adeabb0801a8
beep boop <githubbot@vadim.me>
2024-07-06 07:10:21 +0200

    Solution 74 of CPP/91, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/rwutwl42 Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- CPP_91.cpp
+++ CPP_91.cpp
@@ -4,13 +4,13 @@
 int is_bored(std::string S) {
     int count = 0;
     size_t pos = 0;
-    while ((pos = S.find("I", pos)) != stdstring::npos) {
-        if (S.find(".", pos) == stdstring::npos && S.find("?", pos) == stdstring::npos && S.find("!", pos) == stdstring::npos)
-            count++; 
+    while ((pos = S.find("I", pos)) != std::string::npos) {
+        if (pos == S.find(".", pos) || pos == S.find("?", pos) || pos == S.find("!", pos))
+            pos = S.find("I", pos + 1);
         else
-            pos = S.find('.', pos) + 1;
+            count++;
     }
-    return count; 
+    return count;
 
 int main() {
     std::string input;
@@ -19,3 +19,4 @@ int main() {
     int result = is_bored(input);
     std::cout << "Number of 'I' without punctuation: " << result << "\n";
     return 0;
+}
