a01330d904fc49f363873715e5f6a03e9695e9c7
beep boop <githubbot@vadim.me>
2024-07-06 10:27:22 +0200

    Solution 16 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/mfo3ikld Prompt: in this scope    19 |     assert(int_to_mini_roman(1000) == "M");       |     ^~~~~~ 6b8c3174-1c5b-4e10-9cb9-2a8258ac05db.cpp:3:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     2 | #include <vector>   +++ |+#include <cassert>     3 | #include <string> 6b8c3174-1c5b-4e10-9cb9-2a8258ac05db.cpp: At global scope: 6b8c3174-1c5b-4e10-9cb9-2a8258ac05db.cpp:25:5: error: redefinition of ‘int main( )’    25 | int main(){       |     ^~~~ 6b8c3174-1c5b-4e10-9cb9-2a8258ac05db.cpp:18:5: note: ‘int main()’ previously def ined here    18 | int main() {       |     ^~~~ In file included from 6b8c3174-1c5b-4e10-9cb9-2a8258ac05db.cpp:24: 6b8c3174-1c5b-4e10-9cb9-2a8258ac05db.cpp: In function ‘int main()’: 6b8c3174-1c5b-4e10-9cb9-2a8258ac05db.cpp:26:13: error: ‘int_to_mini_romank’ was not declared in this scope; did you mean ‘int_to_mini_roman’?    26 |     assert (int_to_mini_romank(1000) == "m");       |             ^~~~~~~~~~~~~~~~~~

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -5,7 +5,7 @@
 
 std::vector<std::string> romanNumerals = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
 int numerals[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-std::string int_to_mini_romank(int number) {
+std::string int_to_mini_roman(int number) {
     std::string roman = "";
     for (int i = 0; i < sizeof(numerals)/sizeof(int); i++) {
         while (number >= numerals[i]) {
@@ -17,6 +17,6 @@ std::string int_to_mini_romank(int number) {
 }
 
 int main() {
-    assert(int_to_mini_romank(1000) == "M");
+    assert(int_to_mini_roman(1000) == "M");
     return 0;
 }
