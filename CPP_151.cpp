cea48b947db8c80a11254e4819264ebecbb22eda
beep boop <githubbot@vadim.me>
2024-07-06 13:04:23 +0200

    Solution 77 of CPP/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/2iqw0vuf Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from dd6178b8-d1f4-4021-b99e-120a3b65af1a.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_151.cpp
+++ CPP_151.cpp
@@ -2,7 +2,6 @@
 #include <cmath>
 #include <iostream>
 #include <initializer_list>
-
 using namespace std;
 
 long long double_the_difference(vector<double> lst) {
@@ -16,9 +15,8 @@ long long double_the_difference(vector<double> lst) {
 }
 
 int main() {
-    vector<double> lst({1.5, 2.5, -3.4, 4.7});
+    vector<double> lst(vector<double>({1.5, 2.5, -3.4, 4.7}));
     long long odd_sum = double_the_difference(lst);
-    assert(double_the_difference(lst) == odd_sum); 
     cout << odd_sum;
     return 0;
 }
