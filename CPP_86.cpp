74b5f58ae37e82594138ba73815c56b5aa50314c
beep boop <githubbot@vadim.me>
2024-07-06 21:45:49 +0200

    Solution 3 of CPP/86, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/ai2cx1mu Prompt: Complete the following code given the task description and function signature.

--- CPP_86.cpp
+++ CPP_86.cpp
@@ -3,22 +3,21 @@ using namespace std;
 
 string anti_shuffle(string s) {
     string result = "";
-    for (const auto& word : split(s, ' ')) {
-        for (char c : word) {
-            result += (c <= 'z' && c >= 'a') ? c : ' ';
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] == ' ') {
+            result += " ";
+        } else {
+            char temp = s[i];
+            while (i + 1 < s.length() && s[i+1] == ' ') {
+                i++;
+            }
+            for (int j = i; j >= 0 && s[j] != ' '; j--) {
+                if (s[j] > temp) {
+                    temp = s[j];
+                }
+            }
+            result += temp;
         }
-        result += " ";
     }
-    return result;
-}
-
-string split(const string& str, char delimiter) {
-    string result = "";
-    size_t pos = 0;
-    while ((pos = str.find(delimiter)) != std::string::npos) {
-        result += str.substr(0, pos);
-        str.erase(0, pos + 1);
-    }
-    result += str;
     return result;
 }
