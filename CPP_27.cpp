f9f778d3bc139988c0b51ca7ed3f67ec9b8a9d50
beep boop <githubbot@vadim.me>
2024-07-06 19:24:18 +0200

    Solution 84 of CPP/27, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/9n7jt4bs Prompt: d81cac63-684f-42a9-86b0-46113209c023.cpp:2:1: error: ‘string’ does not name a ty pe     2 | string filp_case(string str){       | ^~~~~~ In file included from d81cac63-684f-42a9-86b0-46113209c023.cpp:14: d81cac63-684f-42a9-86b0-46113209c023.cpp: In function ‘int main()’: d81cac63-684f-42a9-86b0-46113209c023.cpp:16:13: error: ‘filp_case’ was not decla red in this scope    16 |     assert (filp_case("These violent delights have violent ends") == "tH ESE VIOLENT DELIGHTS HAVE VIOLENT ENDS");       |             ^~~~~~~~~

--- CPP_27.cpp
+++ CPP_27.cpp
@@ -5,15 +5,13 @@ int main(){
     std::string str;
     std::cout << "Enter a string: ";
     std::cin >> str;
-
     for(int i = 0; i < str.length(); i++){
         if(str[i] >= 'a' && str[i] <= 'z'){
-            str[i] = (char)(str[i]-32);
+            str[i] = (char)toupper((int)str[i]);
         }else if(str[i] >= 'A' && str[i] <= 'Z'){
-            str[i] = (char)(str[i]+32);
+            str[i] = (char)tolower((int)str[i]);
         }
     }
-
-    std::cout << "Flipped case: " << str << std::endl;
+    std::cout << "Flipped case string: " << str << std::endl;
     return 0;
 }
