758cdcb26ba389097d09725ebf388c1c355cd314
beep boop <githubbot@vadim.me>
2024-07-06 23:54:43 +0200

    Solution 12 of CPP/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/0ujhsbw6 Prompt: ed928f6f-281d-43e8-9a78-19f5d86d8218.cpp:22:13: error: ‘coeffs’ was not declared  in this scope; did you mean ‘coeff’?    22 |             coeffs.push_back(coeff);       |             ^~~~~~       |             coeff ed928f6f-281d-43e8-9a78-19f5d86d8218.cpp:25:30: error: ‘coeffs’ was not declared  in this scope; did you mean ‘ncoeff’?    25 |         solution = find_zero(coeffs);       |                              ^~~~~~       |                              ncoeff ed928f6f-281d-43e8-9a78-19f5d86d8218.cpp:25:36: error: ‘find_zero’ cannot be use d as a function    25 |         solution = find_zero(coeffs);       |                                    ^ In file included from ed928f6f-281d-43e8-9a78-19f5d86d8218.cpp:9: ed928f6f-281d-43e8-9a78-19f5d86d8218.cpp:26:21: error: ‘poly’ was not declared i n this scope    26 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                     ^~~~ ed928f6f-281d-43e8-9a78-19f5d86d8218.cpp:26:17: error: ‘abs’ was not declared in  this scope    26 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                 ^~~

--- CPP_32.cpp
+++ CPP_32.cpp
@@ -1,16 +1,14 @@
-vector<double> coeffs;
-double find_zero(vector<double> xs){
-    vector<double> poly = {1.0};
+double find_zero(vector<double> coeffs){
     double sum = 0;
-    for (int i = 2; i < xs.size(); i+=2) {
-        vector<double> newPoly;
-        for(int j=0;j<poly.size();j++){
-            newPoly.push_back(poly[j]);
+    vector<double> poly(1);
+    for (int i = 0; i < coeffs.size(); i++) {
+        if(i % 2 == 0) {
+            poly.push_back(coeffs[i]);
+        } else {
+            for(int j = 0; j <= i/2; j++) {
+                poly[j] += (-1) * pow(poly, i);
+            }
         }
-        newPoly.push_back(0);
-        newPoly.back() += -sum / xs[i-1] * poly.back();
-        sum = 0;
-        poly = newPoly;
     }
-    return round(sum, 10);
+    return round(poly[0], 10);
 }
