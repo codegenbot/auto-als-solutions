8cf22fbfb3d369f2eea822771b2641aea6c278af
beep boop <githubbot@vadim.me>
2024-07-06 14:29:49 +0200

    Solution 53 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/mfo3ikld Prompt: in this scope    19 |     assert(miniRom(1000) == "M");       |     ^~~~~~ 142c9819-aa68-45fb-9c96-5356b24d92ef.cpp:3:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     2 | #include <vector>   +++ |+#include <cassert>     3 | #include <string> 142c9819-aa68-45fb-9c96-5356b24d92ef.cpp: At global scope: 142c9819-aa68-45fb-9c96-5356b24d92ef.cpp:25:5: error: redefinition of ‘int main( )’    25 | int main(){       |     ^~~~ 142c9819-aa68-45fb-9c96-5356b24d92ef.cpp:18:5: note: ‘int main()’ previously def ined here    18 | int main() {       |     ^~~~ In file included from 142c9819-aa68-45fb-9c96-5356b24d92ef.cpp:24: 142c9819-aa68-45fb-9c96-5356b24d92ef.cpp: In function ‘int main()’: 142c9819-aa68-45fb-9c96-5356b24d92ef.cpp:26:13: error: ‘int_to_mini_romank’ was not declared in this scope    26 |     assert (int_to_mini_romank(1000) == "m");       |             ^~~~~~~~~~~~~~~~~~

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -5,7 +5,7 @@
 
 std::vector<std::string> romanNumerals = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
 int numerals[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-std::string romToMini(int number) {
+std::string romanize(int number) {
     std::string result = "";
     for (int i = 0; i < sizeof(numerals)/sizeof(int); i++) {
         while (number >= numerals[i]) {
@@ -17,7 +17,7 @@ std::string romToMini(int number) {
 }
 
 int main() {
-    assert(romToMini(1000) == "M");
-    std::cout << romToMini(2023);
+    assert(romanize(1000) == "M");
+    std::cout << romanize(2023);
     return 0;
 }
