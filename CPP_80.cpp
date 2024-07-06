fd2db8a19134237cee3149181fc05ef93707d0de
beep boop <githubbot@vadim.me>
2024-07-06 22:30:01 +0200

    Solution 80 of CPP/80, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/9pm4s1va Prompt: d54b4e6a-7c65-4067-996c-a5cc35b05a8b.cpp:1:15: error: ‘string’ was not declared in this scope     1 | bool is_happy(string s) {       |               ^~~~~~ In file included from d54b4e6a-7c65-4067-996c-a5cc35b05a8b.cpp:23: d54b4e6a-7c65-4067-996c-a5cc35b05a8b.cpp: In function ‘int main()’: d54b4e6a-7c65-4067-996c-a5cc35b05a8b.cpp:25:21: error: ‘is_happy’ cannot be used  as a function    25 |     assert (is_happy("iopaxioi") == false );       |             ~~~~~~~~^~~~~~~~~~~~

--- CPP_80.cpp
+++ CPP_80.cpp
@@ -1,26 +1,21 @@
-#include <stdio.h>
 #include <string>
-using namespace std;
-
-bool is_happy(string s) {
+bool is_happy(std::string s) {
     if (s.length() < 3)
         return false;
     for (int i = 0; i <= s.length() - 3; i++) {
-        string t = s.substr(i, 3);
-        bool duplicate = false;
-        for (char c : t) {
+        std::string sub = s.substr(i, 3);
+        bool unique = true;
+        for (char c : sub) {
             int count = 0;
-            for (int j = i; j < i + 3 && j < s.length(); j++) {
-                if (s[j] == c) {
+            for (int j = i; j < i + 3; j++)
+                if (s[j] == c)
                     count++;
-                }
-            }
             if (count > 1) {
-                duplicate = true;
+                unique = false;
                 break;
             }
         }
-        if (!duplicate)
+        if (!unique)
             return false;
     }
     return true;
