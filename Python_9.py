3a888996ec143694b4fb62f6da97094cb37574d6
beep boop <githubbot@vadim.me>
2024-07-06 03:25:57 +0200

    Solution 66 of Python/9, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/u5llv5d8 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ce7a81af-4ed0-4f06-8eff-a6351fdba 641.py", line 18, in <module>     check(rolling_max)           ^^^^^^^^^^^ NameError: name 'rolling_max' is not defined

--- Python_9.py
+++ Python_9.py
@@ -1,12 +1,1 @@
-def max_rolling_sum(numbers: list[int]) -> int:
-    if not numbers:
-        return 0
-
-    current_sum = max_sum = start = numbers[0]
-
-    for num in numbers[1:]:
-        current_sum = max(num, current_sum + num)
-        max_sum = max(max_sum, current_sum)
-        start = min(start, current_sum - num)
-
-    return max_sum
+print(max_rolling_sum([1, 2, 3, 4, 5]))
