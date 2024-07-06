84013c73639749736e66d8705b7f5ae56f47cdf0
beep boop <githubbot@vadim.me>
2024-07-07 01:13:32 +0200

    Solution 48 of CPP/153, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/7ij0la7r Prompt:       |                         ^~~~~~ d2d80c3d-f954-46a6-8e3f-16f55f5c0b5c.cpp:1:43: error: ‘vector’ was not declared in this scope     1 | int Strongest_Extension(string class_name,vector<string> extensions){       |                                           ^~~~~~ d2d80c3d-f954-46a6-8e3f-16f55f5c0b5c.cpp:1:50: error: ‘string’ was not declared in this scope     1 | int Strongest_Extension(string class_name,vector<string> extensions){       |                                                  ^~~~~~ d2d80c3d-f954-46a6-8e3f-16f55f5c0b5c.cpp:1:58: error: ‘extensions’ was not decla red in this scope     1 | int Strongest_Extension(string class_name,vector<string> extensions){       |                                                          ^~~~~~~~~~ d2d80c3d-f954-46a6-8e3f-16f55f5c0b5c.cpp:1:68: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | int Strongest_Extension(string class_name,vector<string> extensions){       |                                                                    ^ In file included from d2d80c3d-f954-46a6-8e3f-16f55f5c0b5c.cpp:21: d2d80c3d-f954-46a6-8e3f-16f55f5c0b5c.cpp: In function ‘int main()’: d2d80c3d-f954-46a6-8e3f-16f55f5c0b5c.cpp:23:32: error: ‘Strongest_Extension’ can not be used as a function    23 |     assert (Strongest_Extension("Sp", {"671235", "Bb"}) == "Sp.671235");       |             ~~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_153.cpp
+++ CPP_153.cpp
@@ -1,28 +1,23 @@
 #include <iostream>
-#include <string>
 #include <vector>
+#include <string>
 
-std::string Strongest_Extension(std::string class_name, std::vector<std::string> extensions) {
+int Strongest_Extension(std::string class_name, std::vector<std::string> extensions) {
     int max_strength = 0;
     std::string strongest_extension;
-
-    for(auto extension : extensions){
-        int cap = 0, sm = 0;
-        for(char c : extension){
-            if(std::isupper(c)) cap++;
-            else if(std::islower(c)) sm++;
+    
+    for (std::string extension : extensions) {
+        int strength = 0;
+        for (char c : extension) {
+            if (std::isupper(c)) strength++;
+            else if (std::islower(c)) strength--;
         }
-        int strength = cap - sm;
-        if(strength > max_strength || (strength == max_strength && extension < strongest_extension)){
+        
+        if (strength > max_strength) {
             max_strength = strength;
             strongest_extension = extension;
         }
     }
-
+    
     return class_name + "." + strongest_extension;
-}
-
-int main() {
-    assert(Strongest_Extension("Sp", {"671235", "Bb"}) == "Sp.671235");
-    return 0;
 }
