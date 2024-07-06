982eb24e6ea21be8426d301f916957b95d1b95a3
beep boop <githubbot@vadim.me>
2024-07-06 10:39:40 +0200

    Solution 44 of CPP/145, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/pmi4gz2f Prompt:    29 | bool issame(vector<int> a,vector<int>b){       |                                       ^ a9ac5db4-4935-4f27-8e2f-438635bd55ab.cpp:37:5: error: redefinition of ‘int main( )’    37 | int main(){       |     ^~~~ a9ac5db4-4935-4f27-8e2f-438635bd55ab.cpp:23:5: note: ‘int main()’ previously def ined here    23 | int main() {       |     ^~~~ In file included from a9ac5db4-4935-4f27-8e2f-438635bd55ab.cpp:28: a9ac5db4-4935-4f27-8e2f-438635bd55ab.cpp: In function ‘int main()’: a9ac5db4-4935-4f27-8e2f-438635bd55ab.cpp:38:20: error: ‘order_by_points’ was not  declared in this scope    38 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |                    ^~~~~~~~~~~~~~~ a9ac5db4-4935-4f27-8e2f-438635bd55ab.cpp:38:19: error: ‘issame’ cannot be used a s a function    38 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~

--- CPP_145.cpp
+++ CPP_145.cpp
@@ -1,12 +1,11 @@
 #include <algorithm>
 #include <vector>
-#include <initializer_list>
 
 bool same(std::vector<int> a, std::vector<int> b) {
     return (a.size() == b.size()) && (std::equal(a.begin(), a.end(), b.begin()));
 }
 
-std::vector<int> sortNumbersByPoints(std::vector<int> nums) {
+std::vector<int> order_by_points(std::vector<int> nums) {
     auto comp = [&nums](int a, int b) {
         int sumA = 0, sumB = 0;
         for (char c : std::to_string(a)) sumA += c - '0';
@@ -17,9 +16,3 @@ std::vector<int> sortNumbersByPoints(std::vector<int> nums) {
     };
     std::sort(nums.begin(), nums.end(), comp);
     return nums;
-}
-
-int main() {
-    assert(same(sortNumbersByPoints({0,6,6,-76,-21,23,4}), {-76, -21, 0, 4, 23, 6, 6}));
-    return 0;
-}
