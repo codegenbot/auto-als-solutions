ad8e709eac7cbd10978ad4447596e40f533c623d
beep boop <githubbot@vadim.me>
2024-07-06 08:02:21 +0200

    Solution 92 of CPP/80, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/yra2qaiq Prompt: fore ‘if’    30 |     if(is_happy(s))       |     ^~ 36b6a393-d6a6-4357-8cb9-0a6121a579d5.cpp:32:5: error: expected unqualified-id be fore ‘else’    32 |     else       |     ^~~~ 36b6a393-d6a6-4357-8cb9-0a6121a579d5.cpp:34:5: error: expected unqualified-id be fore ‘return’    34 |     return 0;       |     ^~~~~~ 36b6a393-d6a6-4357-8cb9-0a6121a579d5.cpp:35:1: error: expected declaration befor e ‘}’ token    35 | }       | ^ 36b6a393-d6a6-4357-8cb9-0a6121a579d5.cpp:38:10: error: ‘int main()’ redeclared a s different kind of entity    38 | int main(){       |          ^ 36b6a393-d6a6-4357-8cb9-0a6121a579d5.cpp:25:5: note: previous declaration ‘int m ain’    25 | int main       |     ^~~~

--- CPP_80.cpp
+++ CPP_80.cpp
@@ -1,4 +1,3 @@
-```cpp
 #include <iostream>
 #include <string>
 #include <cstring>
@@ -23,11 +22,11 @@ bool is_happy(string s){
 
 }
 
-int main(){
+void main() {
     string s;
     cout << "Enter a string: ";
     getline(cin, s);
-    if((is_happy(s)))
+    if(is_happy(s))
         cout << "The given string is happy." << endl;
     else
         cout << "The given string is not happy." << endl;
