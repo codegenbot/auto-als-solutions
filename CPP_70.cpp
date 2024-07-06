d8c12dbe8cfda088cfae9aa09454ee6abe0c5012
beep boop <githubbot@vadim.me>
2024-07-06 06:28:00 +0200

    Solution 71 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/xmlozhdh Prompt: 7f696d6a-33ba-467b-a1a5-3c259fccf282.cpp:35:5: note: ‘int main()’ previously def ined here    35 | int main() {       |     ^~~~ In file included from 7f696d6a-33ba-467b-a1a5-3c259fccf282.cpp:43: 7f696d6a-33ba-467b-a1a5-3c259fccf282.cpp: In function ‘int main()’: 7f696d6a-33ba-467b-a1a5-3c259fccf282.cpp:53:37: error: cannot bind non-const lva lue reference of type ‘std::vector<int>&’ to an rvalue of type ‘std::vector<int> ’    53 |     assert (issame(strange_sort_list({111111}) , {111111}));       |                    ~~~~~~~~~~~~~~~~~^~~~~~~~~~ In file included from /usr/include/c++/11/vector:67,                  from 7f696d6a-33ba-467b-a1a5-3c259fccf282.cpp:2: /usr/include/c++/11/bits/stl_vector.h:625:7: note:   after user-defined conversi on: ‘std::vector<_Tp, _Alloc>::vector(std::initializer_list<_Tp>, const allocato r_type&) [with _Tp = int; _Alloc = std::allocator<int>; std::vector<_Tp, _Alloc> ::allocator_type = std::allocator<int>]’   625 |       vector(initializer_list<value_type> __l,       |       ^~~~~~ 7f696d6a-33ba-467b-a1a5-3c259fccf282.cpp:9:42: note:   initializing argument 1 o f ‘void strange_sort_list(std::vector<int>&)’     9 | void strange_sort_list(std::vector<int>& lst) {       |                        ~~~~~~~~~~~~~~~~~~^~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -1,38 +1,3 @@
-```cpp
-#include <iostream>
-#include <vector>
-#include <algorithm>
-
-bool issame(std::vector<int> a, std::vector<int> b) {
-    return a == b;
-}
-
-void strange_sort_list(const std::vector<int>& lst) {
-    std::vector<int> result;
-    while (!lst.empty()) {
-        int min_val = *std::min_element(lst.begin(), lst.end());
-        result.push_back(min_val);
-        lst.erase(std::remove(lst.begin(), lst.end(), min_val), lst.end());
-
-        if (!lst.empty()) {
-            int max_val = *std::max_element(lst.begin(), lst.end());
-            std::vector<int> temp;
-            for (int i : lst) {
-                if (i == max_val) {
-                    lst.erase(std::remove(lst.begin(), lst.end(), i), lst.end());
-                    break;
-                } else {
-                    temp.push_back(i);
-                }
-            }
-            result.insert(result.end(), temp.begin(), temp.end());
-        } else {
-            int min_val2 = *std::min_element(lst.begin(), lst.end());
-            result.push_back(min_val2);
-        }
-    }
-}
-
 int main() {
     std::vector<int> input = {1, 1, 1, 1, 1};
     
