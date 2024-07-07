f89384a50ac05f1777d3365f44ca5accd537c888
beep boop <githubbot@vadim.me>
2024-07-07 16:15:56 +0200

    Solution 8 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/utgpeiz7 Prompt: Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,18 +1,60 @@
 #include <vector>
 using namespace std;
 
-vector<vector<int>> cutVector(vector<int>& nums) {
-    int min_diff = INT_MAX;
-    int index = -1;
-    for (int i = 0; i < nums.size() - 1; i++) {
-        int diff = abs(nums[i] - nums[i + 1]);
-        if (diff <= min_diff) {
-            min_diff = diff;
-            index = i;
+vector<vector<int>> cutVector(vector<int> vec) {
+    int n = vec.size();
+    vector<vector<int>> res(2);
+    for (int i = 0; i < n; i++) {
+        if (i == 0 || i == n - 1) {
+            res[0].push_back(vec[i]);
+            res[1].push_back(vec[i]);
+        } else {
+            int left_sum = 0, right_sum = 0;
+            for (int j = 0; j < i; j++) {
+                left_sum += vec[j];
+            }
+            for (int j = i; j < n; j++) {
+                right_sum += vec[j];
+            }
+            if (left_sum == right_sum) {
+                res[0].push_back(vec[i]);
+                res[1].push_back(vec[i]);
+            } else {
+                int min_diff = abs(left_sum - right_sum);
+                for (int j = 0; j < i; j++) {
+                    left_sum -= vec[j];
+                    if (abs(left_sum - right_sum) < min_diff) {
+                        res[0].clear();
+                        res[1].clear();
+                        res[0].push_back(vec[j]);
+                        res[1].push_back(vec[i]);
+                        for (int k = 0; k < j; k++) {
+                            res[0].push_back(vec[k]);
+                        }
+                        for (int k = i + 1; k < n; k++) {
+                            res[1].push_back(vec[k]);
+                        }
+                        min_diff = abs(left_sum - right_sum);
+                    }
+                }
+                for (int j = i; j < n; j++) {
+                    right_sum -= vec[j];
+                    if (abs(left_sum - right_sum) < min_diff) {
+                        res[0].clear();
+                        res[1].clear();
+                        res[0].push_back(vec[i]);
+                        res[1].push_back(vec[j]);
+                        for (int k = 0; k < i; k++) {
+                            res[0].push_back(vec[k]);
+                        }
+                        for (int k = j + 1; k < n; k++) {
+                            res[1].push_back(vec[k]);
+                        }
+                        min_diff = abs(left_sum - right_sum);
+                    }
+                }
+            }
         }
     }
-    vector<vector<int>> result(2);
-    result[0].insert(result[0].end(), nums.begin(), nums.begin() + index + 1);
-    result[1].insert(result[1].begin(), nums.begin() + index + 1, nums.end());
-    return result;
+    return res;
 }
