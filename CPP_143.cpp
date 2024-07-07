8b0f21898d30e645344812d025349c2032d46c9e
beep boop <githubbot@vadim.me>
2024-07-07 03:54:23 +0200

    Solution 94 of CPP/143, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/x3m2x9qi Prompt: 0a202f69-2c30-4ae9-9006-986caf75d294.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string words_in_sentence(string sentence) {       | ^~~~~~ 0a202f69-2c30-4ae9-9006-986caf75d294.cpp:24:1: error: ‘vector’ does not name a t ype    24 | vector<string> split(const string& str, char ch) {       | ^~~~~~ In file included from 0a202f69-2c30-4ae9-9006-986caf75d294.cpp:35: 0a202f69-2c30-4ae9-9006-986caf75d294.cpp: In function ‘int main()’: 0a202f69-2c30-4ae9-9006-986caf75d294.cpp:37:13: error: ‘words_in_sentence’ was n ot declared in this scope    37 |     assert (words_in_sentence("here is") == "is");       |             ^~~~~~~~~~~~~~~~~

--- CPP_143.cpp
+++ CPP_143.cpp
@@ -18,7 +18,7 @@ std::string words_in_sentence(std::string sentence) {
 
     std::vector<std::string> words = split(sentence, ' ');
     for (std::string word : words) {
-        if (find(primeNumbers.begin(), primeNumbers.end(), word.length()) != primeNumbers.end())
+        if (find(primeNumbers.begin(), primeNumbers.end(), std::stoi(word)) != primeNumbers.end())
             result += word + " ";
     }
     return result.substr(0, result.size() - 1);
@@ -33,6 +33,7 @@ std::vector<std::string> split(const std::string& str, char ch) {
     }
     tokens.push_back(str);
     return tokens;
+
 }
 
 int main() {
