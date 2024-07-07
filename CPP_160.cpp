53ae83f41cbf65d2600295d503b071ae537c0d4d
beep boop <githubbot@vadim.me>
2024-07-07 03:35:42 +0200

    Solution 51 of CPP/160, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/7o99s3y4 Prompt:       |                       ^~~~~~ 57d07364-fb1c-4527-acc7-414490878c25.cpp:1:31: error: ‘operator_’ was not declar ed in this scope     1 | int do_algebra(vector<string> operator_, vector<int> operands_) {       |                               ^~~~~~~~~ 57d07364-fb1c-4527-acc7-414490878c25.cpp:1:42: error: ‘vector’ was not declared in this scope     1 | int do_algebra(vector<string> operator_, vector<int> operands_) {       |                                          ^~~~~~ 57d07364-fb1c-4527-acc7-414490878c25.cpp:1:49: error: expected primary-expressio n before ‘int’     1 | int do_algebra(vector<string> operator_, vector<int> operands_) {       |                                                 ^~~ 57d07364-fb1c-4527-acc7-414490878c25.cpp:1:63: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | int do_algebra(vector<string> operator_, vector<int> operands_) {       |                                                               ^ In file included from 57d07364-fb1c-4527-acc7-414490878c25.cpp:31: 57d07364-fb1c-4527-acc7-414490878c25.cpp: In function ‘int main()’: 57d07364-fb1c-4527-acc7-414490878c25.cpp:33:23: error: ‘do_algebra’ cannot be us ed as a function    33 |     assert (do_algebra({"//", "*"}, {7, 3, 4}) == 8);       |             ~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_160.cpp
+++ CPP_160.cpp
@@ -9,12 +9,12 @@ int do_algebra(vector<string> operator_, vector<int> operands_) {
         } else if (operator_[i] == "-") {
             result -= operands_[i];
         } else if (operator_[i] == "*") {
-            int temp = 1;
+            int temp = 0;
             for (int j = i; j < operands_.size(); j++) {
                 temp *= operands_[j];
             }
             result += temp;
-        } else if (operator_[i] == "/*" || operator_[i] == "//") {
+        } else if (operator_[i] == "/" || operator_[i] == "//") {
             int temp = operands_[i];
             for (int j = i + 1; j < operands_.size(); j++) {
                 temp /= operands_[j];
