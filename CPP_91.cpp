1d5725d8b582ab7e008422994a1e295adf5dcf50
beep boop <githubbot@vadim.me>
2024-07-07 02:19:41 +0200

    Solution 50 of CPP/91, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/7ezv9ye1 Prompt: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~ In file included from /usr/include/c++/11/bits/stl_algobase.h:67,                  from /usr/include/c++/11/bits/char_traits.h:39,                  from /usr/include/c++/11/string:40,                  from 96c8a1a1-2bb2-44ea-9da9-9310be8db937.cpp:4: /usr/include/c++/11/bits/stl_iterator.h: In instantiation of ‘class __gnu_cxx::_ _normal_iterator<const char*, std::__cxx11::basic_string<char> >’: /usr/include/c++/11/bits/basic_string.tcc:225:25:   required from ‘void std::__c xx11::basic_string<_CharT, _Traits, _Alloc>::_M_construct(_InIterator, _InIterat or, std::forward_iterator_tag) [with _FwdIterator = const char*; _CharT = char; _Traits = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:539:14:   required from ‘std::__cxx11::b asic_string<_CharT, _Traits, _Alloc>::basic_string(const _CharT*, const _Alloc&)  [with <template-parameter-2-1> = std::allocator<char>; _CharT = char; _Traits =  std::char_traits<char>; _Alloc = std::allocator<char>]’ 96c8a1a1-2bb2-44ea-9da9-9310be8db937.cpp:25:5:   required from here /usr/include/c++/11/bits/stl_iterator.h:1015:57: error: no type named ‘differenc e_type’ in ‘struct std::iterator_traits<const char*>’  1015 |       typedef typename __traits_type::difference_type   difference_type;       |                                                         ^~~~~~~~~~~~~~~

--- CPP_91.cpp
+++ CPP_91.cpp
@@ -1,13 +1,19 @@
+#include <string>
+using namespace std;
+
 int is_bored(string S) {
-    int count = 0;
-    size_t pos = 0;
-    while ((pos = S.find('.', pos)) != string::npos || 
-           (pos = S.find('?', pos)) != string::npos || 
-           (pos = S.find('!', pos)) != string::npos) {
-        if (S.substr(0, 2).compare("I ") == 0) {
-            count++;
+    int boredoms = 0;
+    string sentence;
+    const char* p = S.c_str();
+    for (; *p != '\0'; ++p) {
+        if (*p == '.' || *p == '?' || *p == '!') {
+            if (sentence.length() > 2 && sentence.substr(0, 2) == "I ") {
+                boredoms++;
+            }
+            sentence.clear();
+        } else {
+            sentence += *p;
         }
-        pos++;
     }
-    return count;
+    return boredoms;
 }
