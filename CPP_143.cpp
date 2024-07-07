9320bdd6655bc801c0fc9c105b2d9d9dd2a8bb81
beep boop <githubbot@vadim.me>
2024-07-07 03:51:26 +0200

    Solution 89 of CPP/143, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/x3m2x9qi Prompt:     7 |     vector<string> words = split(sentence, ' ');       |                            ^~~~~ fe9b6dc4-3f1e-4647-8d2d-d8f7c90b10a4.cpp:10:13: error: ‘is_prime’ was not declar ed in this scope    10 |         if (is_prime(word.length())) {       |             ^~~~~~~~ fe9b6dc4-3f1e-4647-8d2d-d8f7c90b10a4.cpp: In function ‘std::vector<std::__cxx11: :basic_string<char> > split(const string&, char)’: fe9b6dc4-3f1e-4647-8d2d-d8f7c90b10a4.cpp:30:18: error: passing ‘const string’ {a ka ‘const std::__cxx11::basic_string<char>’} as ‘this’ argument discards qualifi ers [-fpermissive]    30 |         str.erase(0, pos + 1);       |         ~~~~~~~~~^~~~~~~~~~~~ In file included from /usr/include/c++/11/string:55,                  from fe9b6dc4-3f1e-4647-8d2d-d8f7c90b10a4.cpp:2: /usr/include/c++/11/bits/basic_string.h:1827:7: note:   in call to ‘std::__cxx11 ::basic_string<_CharT, _Traits, _Alloc>& std::__cxx11::basic_string<_CharT, _Tra its, _Alloc>::erase(std::__cxx11::basic_string<_CharT, _Traits, _Alloc>::size_ty pe, std::__cxx11::basic_string<_CharT, _Traits, _Alloc>::size_type) [with _CharT  = char; _Traits = std::char_traits<char>; _Alloc = std::allocator<char>; std::_ _cxx11::basic_string<_CharT, _Traits, _Alloc>::size_type = long unsigned int]’  1827 |       erase(size_type __pos = 0, size_type __n = npos)       |       ^~~~~

--- CPP_143.cpp
+++ CPP_143.cpp
@@ -1,3 +1,4 @@
+```
 #include <vector>
 #include<string>
 
@@ -11,17 +12,6 @@ bool is_prime(int n) {
     return true;
 }
 
-string words_in_sentence(string sentence) {
-    vector<string> words = split(sentence, ' ');
-    string result;
-    for (const auto& word : words) {
-        if (is_prime(word.length())) {
-            result += word + " ";
-        }
-    }
-    return result.substr(0, result.size() - 1);
-}
-
 vector<string> split(const string& str, char delimiter) {
     vector<string> tokens;
     size_t pos = 0;
@@ -31,4 +21,15 @@ vector<string> split(const string& str, char delimiter) {
     }
     tokens.push_back(str);
     return tokens;
+}
+
+string words_in_sentence(string sentence) {
+    vector<string> words = split(sentence, ' ');
+    string result;
+    for (const auto& word : words) {
+        if (is_prime(word.length())) {
+            result += word + " ";
+        }
+    }
+    return result.substr(0, result.size() - 1);
 }
