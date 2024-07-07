356df7d3829338eb532977255f6c9654f38fe7c1
beep boop <githubbot@vadim.me>
2024-07-07 04:07:21 +0200

    Solution 90 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/9fpokkdy Prompt: 4d8ffae9-67eb-44bb-ba54-da99aad067e7.cpp:3:1: error: ‘string’ does not name a ty pe; did you mean ‘stdin’?     3 | string string_to_md5(string text) {       | ^~~~~~       | stdin In file included from 4d8ffae9-67eb-44bb-ba54-da99aad067e7.cpp:26: 4d8ffae9-67eb-44bb-ba54-da99aad067e7.cpp: In function ‘int main()’: 4d8ffae9-67eb-44bb-ba54-da99aad067e7.cpp:28:13: error: ‘string_to_md5’ was not d eclared in this scope    28 |     assert (string_to_md5("password") == "5f4dcc3b5aa765d61d8327deb882cf 99");       |             ^~~~~~~~~~~~~

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -1,21 +1,25 @@
+#include <openssl/evp.h>
 #include <string>
-#include <openssl/md5.h>
-#include <sstream>
-#include <iomanip>
 
-using namespace std;
-
-string string_to_md5(string text) {
+std::string string_to_md5(std::string text) {
     if (text.empty()) return "";
-    
-    unsigned char md5[MD5_DIGEST_LENGTH];
-    MD5((const unsigned char*)text.c_str(), text.size(), md5);
-    
-    stringstream ss;
-    ss << hex;  // specify the base
-    for (int i = 0; i < MD5_DIGEST_LENGTH; i++) {
-        ss << setfill('0') << setw(2) << (int)md5[i];
+
+    unsigned char md[MD5_DIGEST_LENGTH];
+    MD5_CTX ctx;
+    MD5_Init(&ctx);
+    const char* ptr = text.c_str();
+    while (*ptr) {
+        MD5_Update(&ctx, ptr, 1);
+        ptr++;
     }
-    
-    return ss.str();
+    MD5_Final(md, &ctx);
+
+    std::string result;
+    for (int i = 0; i < MD5_DIGEST_LENGTH; i++) {
+        char buff[3];
+        sprintf(buff, "%02x", md[i]);
+        result += buff;
+    }
+
+    return result;
 }
