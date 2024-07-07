173ebeea18df10d55ea26629f7ea5168f280ff9f
beep boop <githubbot@vadim.me>
2024-07-07 14:15:02 +0200

    Solution 17 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/h176ktrr Prompt: ype     1 | int bowlingScore(const string& input) {       |                        ^~~~~~ 5303f25c-9136-4109-9748-4de6fadeb8f7.cpp: In function ‘int bowlingScore(const in t&)’: 5303f25c-9136-4109-9748-4de6fadeb8f7.cpp:5:19: error: ‘begin’ was not declared i n this scope     5 |     for (char c : input) {       |                   ^~~~~ 5303f25c-9136-4109-9748-4de6fadeb8f7.cpp:5:19: error: ‘end’ was not declared in this scope 5303f25c-9136-4109-9748-4de6fadeb8f7.cpp:7:22: error: ‘min’ was not declared in this scope     7 |             score += min(10, prevRolls);       |                      ^~~ 5303f25c-9136-4109-9748-4de6fadeb8f7.cpp:9:20: error: ‘isdigit’ was not declared  in this scope     9 |         } else if (isdigit(c)) {       |                    ^~~~~~~ 5303f25c-9136-4109-9748-4de6fadeb8f7.cpp:19:18: error: ‘min’ was not declared in  this scope    19 |         score += min(10, prevRolls);       |                  ^~~

--- bowling.cpp
+++ bowling.cpp
@@ -5,13 +5,13 @@ int bowlingScore(const string& input) {
     int score = 0;
     int roll1, roll2, prevRolls = 0;
 
-    for (auto it = input.begin(); it != input.end(); ++it) {
-        if (*it == '/') {
+    for (char c : input) {
+        if (c == '/') {
             score += std::min(10, prevRolls);
             prevRolls = 0;
-        } else if (std::isdigit(*it)) {
+        } else if (::isdigit(c)) {
             prevRolls *= 10;
-            prevRolls += *it - '0';
+            prevRolls += c - '0';
         } else { // X
             score += 10;
             prevRolls = 0;
@@ -23,3 +23,4 @@ int bowlingScore(const string& input) {
     }
 
     return score;
+}
