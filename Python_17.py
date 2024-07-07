b38c1448f78c1ea7b667a85b72a72c1803a97d6c
beep boop <githubbot@vadim.me>
2024-07-07 03:34:12 +0200

    Solution 55 of Python/17, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/nqzqbtai Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4980d8d9-87d5-4613-9b61-447c7fc94 99a.py", line 8, in <module>     check(parse_music)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4980d8d9-87d5-4613-9b61-447c7fc94 99a.py", line 5, in check     assert parse_music("o| .| o| .| o o| o o|") == [2, 1, 2, 1, 4, 2, 4, 2]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ TypeError: parse_music() takes 0 positional arguments but 1 was given

--- Python_17.py
+++ Python_17.py
@@ -1,1 +1,25 @@
+```
 def parse_music(input_string):
+    note = ""
+    if "o|" in input_string:
+        note += "1"
+    elif ".":
+        note += "0"
+    output = []
+    i = 0
+    while i < len(input_string):
+        if input_string[i] == "o":
+            j = i + 3
+            while i < j and i < len(input_string) - 1 and input_string[i+1] != "|":
+                i += 1
+            note_length = int(input_string[i+1])
+            for _ in range(note_length):
+                if input_string[j] == "o":
+                    output.append(2)
+                elif input_string[j] == ".":
+                    output.append(1)
+                j += 1
+        elif input_string[i] == ".":
+            output.append(0)
+        i += 1
+    return [int(i) for i in note.split()]
