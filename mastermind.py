0e9f744fa0fcaccb8784ddb0b1682a528025510c
beep boop <githubbot@vadim.me>
2024-07-07 22:12:03 +0200

    Solution 38 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/l009th00 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1de2bb8f-b7ef-4193-a0f4-9d7a9419d 752.py", line 1     ```     ^ SyntaxError: invalid syntax

--- mastermind.py
+++ mastermind.py
@@ -1,7 +1,7 @@
 ```
 def mastermind(code, guess):
     correct_colors = [c1 == c2 for c1, c2 in zip(code, guess)]
-    black_pegs = sum([c1 == c2 and i == j for i, (c1, c2) in enumerate(zip(code, guess))])
+    black_pegs = sum([c1 == c2 and idx + 1 == i for i, (c1, c2) in enumerate(zip(code, guess))])
     
     white_pegs = sum(correct_colors) - black_pegs
     return str(white_pegs) + "\n" + str(black_pegs)
