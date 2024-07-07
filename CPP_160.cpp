0bf31d985284d012c0dd8dda4890567accdd3871
beep boop <githubbot@vadim.me>
2024-07-07 03:42:07 +0200

    Solution 77 of CPP/160, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/7o99s3y4 Prompt:    49 |             int b = st.top();       |                     ^~       |                     s 3d27a58c-323c-4e17-b737-045e39d52d8b.cpp:55:21: error: ‘st’ was not declared in this scope; did you mean ‘s’?    55 |             int b = st.top();       |                     ^~       |                     s 3d27a58c-323c-4e17-b737-045e39d52d8b.cpp:61:21: error: ‘st’ was not declared in this scope; did you mean ‘s’?    61 |             int b = st.top();       |                     ^~       |                     s 3d27a58c-323c-4e17-b737-045e39d52d8b.cpp:67:21: error: ‘st’ was not declared in this scope; did you mean ‘s’?    67 |             int b = st.top();       |                     ^~       |                     s 3d27a58c-323c-4e17-b737-045e39d52d8b.cpp:75:12: error: ‘st’ was not declared in this scope; did you mean ‘s’?    75 |     return st.top();       |            ^~       |            s

--- CPP_160.cpp
+++ CPP_160.cpp
@@ -28,8 +28,10 @@ int do_algebra(vector<string> operato, vector<int> operand) {
 }
 
 int evaluate_expression(string s) {
-    stack<int> st;
     int res = 0;
+    stack<int> st;
+    stack<string> ops;
+
     for (int i = 0; i < s.length(); i++) {
         if (isdigit(s[i])) {
             int num = 0;
@@ -38,32 +40,46 @@ int evaluate_expression(string s) {
                 i++;
             }
             st.push(num);
-        } else if (s[i] == '+') {
-            int b = st.top();
-            st.pop();
-            int a = st.top();
-            st.pop();
-            st.push(a + b);
-        } else if (s[i] == '-') {
-            int b = st.top();
-            st.pop();
-            int a = st.top();
-            st.pop();
-            st.push(a - b);
-        } else if (s[i] == '*') {
-            int b = st.top();
-            st.pop();
-            int a = st.top();
-            st.pop();
-            st.push(a * b);
-        } else if (s[i] == '/') {
-            int b = st.top();
-            st.pop();
-            int a = st.top();
-            st.pop();
-            st.push(a / b);
+        } else if (s[i] == '+' || s[i] == '-' || s[i] == '*' || s[i] == '/') {
+            while (!ops.empty() && priority(ops.top()) >= priority(s[i])) {
+                applyOp(st, ops);
+            }
+            ops.push(string(1, s[i]));
         }
     }
-    
+
+    while (!ops.empty()) {
+        applyOp(st, ops);
+    }
+
     return st.top();
+}
+
+int priority(char op) {
+    if (op == '+' || op == '-') {
+        return 1;
+    } else if (op == '*' || op == '/') {
+        return 2;
+    }
+    return 0;
+}
+
+void applyOp(stack<int>& st, stack<string>& ops) {
+    int b = st.top();
+    st.pop();
+    int a = st.top();
+    st.pop();
+
+    string op = ops.top();
+    ops.pop();
+
+    if (op == "+") {
+        st.push(a + b);
+    } else if (op == "-") {
+        st.push(a - b);
+    } else if (op == "*") {
+        st.push(a * b);
+    } else if (op == "/") {
+        st.push(a / b);
+    }
 }
