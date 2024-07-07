4557746eb94924b72ddc7e2ee7d786923246f65d
beep boop <githubbot@vadim.me>
2024-07-07 02:17:28 +0200

    Solution 70 of CPP/77, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/nhvf1o87 Prompt: 9ecfb1de-0b1f-41be-a134-199eb59a4e3d.cpp: In function ‘bool iscuber(int)’: 9ecfb1de-0b1f-41be-a134-199eb59a4e3d.cpp:2:19: error: ‘cbrt’ was not declared in  this scope     2 |     int c = round(cbrt(a));       |                   ^~~~ 9ecfb1de-0b1f-41be-a134-199eb59a4e3d.cpp:2:13: error: ‘round’ was not declared i n this scope     2 |     int c = round(cbrt(a));       |             ^~~~~

--- CPP_77.cpp
+++ CPP_77.cpp
@@ -1,9 +1,4 @@
 bool iscuber(int a){
-    int c = (int)cbrt(a);
-    return (c*c*c) == a;
-}
-
-int main() {
-    assert(iscuber(1729) == false); 
-    return 0;
+    int c = cbrt(a);
+    return c*c*c == a;
 }
