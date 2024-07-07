ba2bb2a2e6febd04e789f6e09d4ed7548f8bbf53
beep boop <githubbot@vadim.me>
2024-07-07 03:32:47 +0200

    Solution 40 of CPP/160, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/7o99s3y4 Prompt:       |                       ^~~~~~ 79e3bd01-a898-4369-a5d1-dfbcf0c76598.cpp:1:31: error: ‘operato’ was not declared  in this scope     1 | int do_algebra(vector<string> operato, vector<int> operand) {       |                               ^~~~~~~ 79e3bd01-a898-4369-a5d1-dfbcf0c76598.cpp:1:40: error: ‘vector’ was not declared in this scope     1 | int do_algebra(vector<string> operato, vector<int> operand) {       |                                        ^~~~~~ 79e3bd01-a898-4369-a5d1-dfbcf0c76598.cpp:1:47: error: expected primary-expressio n before ‘int’     1 | int do_algebra(vector<string> operato, vector<int> operand) {       |                                               ^~~ 79e3bd01-a898-4369-a5d1-dfbcf0c76598.cpp:1:59: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | int do_algebra(vector<string> operato, vector<int> operand) {       |                                                           ^ In file included from 79e3bd01-a898-4369-a5d1-dfbcf0c76598.cpp:19: 79e3bd01-a898-4369-a5d1-dfbcf0c76598.cpp: In function ‘int main()’: 79e3bd01-a898-4369-a5d1-dfbcf0c76598.cpp:21:23: error: ‘do_algebra’ cannot be us ed as a function    21 |     assert (do_algebra({"//", "*"}, {7, 3, 4}) == 8);       |             ~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_160.cpp
+++ CPP_160.cpp
@@ -1,20 +1,30 @@
+#include <iostream>
 #include <vector>
-using namespace std;
+#include <cmath>
 
 int do_algebra(vector<string> operato, vector<int> operand) {
     int result = operand[0];
     for (int i = 0; i < operato.size(); i++) {
         if (operato[i] == "+") {
-            result += operand[i + 1];
+            if(i+1 < operand.size())
+                result += operand[i + 1];
         } else if (operato[i] == "-") {
-            result -= operand[i + 1];
+            if(i+1 < operand.size())
+                result -= operand[i + 1];
         } else if (operato[i] == "*") {
-            result *= operand[i + 1];
+            if(i+1 < operand.size())
+                result *= operand[i + 1];
         } else if (operato[i] == "//") {
-            result = result / operand[i + 1];
+            if(i+1 < operand.size())
+                result = result / operand[i + 1];
         } else if (operato[i] == "**") {
-            result = pow(result, operand[i + 1]);
+            if(i+1 < operand.size())
+                result = pow(result, operand[i + 1]);
         }
     }
     return result;
+}
+
+int main() {
+    assert (do_algebra({"//", "*"}, {7, 3, 4}) == 8);
 }
