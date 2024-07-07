70965d03693e78ee67005d82d52062452670f943
beep boop <githubbot@vadim.me>
2024-07-07 02:59:15 +0200

    Solution 5 of CPP/135, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/s21y658c Prompt: Complete the following code given the task description and function signature.

--- CPP_135.cpp
+++ CPP_135.cpp
@@ -1,9 +1,8 @@
-int can_arrange(vector<int> arr){
-    int max_index = -1;
-    for(int i=1; i<arr.size(); i++){
-        if(arr[i-1] <= arr[i]){
-            max_index = i-1;
+int can_arrange(vector<int> arr) {
+    for (int i = 1; i < arr.size(); i++) {
+        if (arr[i] <= arr[i - 1]) {
+            return i;
         }
     }
-    return max_index;
+    return -1;
 }
