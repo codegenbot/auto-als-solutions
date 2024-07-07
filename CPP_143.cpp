b70e821a54355bc63edd15713dd4c56b5989cd53
beep boop <githubbot@vadim.me>
2024-07-07 03:40:55 +0200

    Solution 68 of CPP/143, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/x3m2x9qi Prompt: bfaa7002-8d8f-4cd6-9188-37a138cda5f8.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string words_in_sentence(string sentence){       | ^~~~~~ In file included from bfaa7002-8d8f-4cd6-9188-37a138cda5f8.cpp:22: bfaa7002-8d8f-4cd6-9188-37a138cda5f8.cpp: In function ‘int main()’: bfaa7002-8d8f-4cd6-9188-37a138cda5f8.cpp:24:13: error: ‘words_in_sentence’ was n ot declared in this scope    24 |     assert (words_in_sentence("here is") == "is");       |             ^~~~~~~~~~~~~~~~~

--- CPP_143.cpp
+++ CPP_143.cpp
@@ -1,31 +1,27 @@
-```c++
-#include <iostream>
 #include <string>
+using namespace std;
 
-std::string words_in_sentence(std::string sentence) {
-    std::string result = "";
-    int count = 0;
+string word;
+word = words_in_sentence("here is");
+return 0;
+
+string words_in_sentence(string sentence){
+    string result = "";
+    int count = 1;
     for(int i=0; i<sentence.size(); i++){
         if(sentence[i] == ' '){
             count++;
             continue;
         }
         bool isPrime = true;
-        for(int j=2; j*j<=count+1; j++){
-            if((count+1)%j==0){
+        for(int j=2; j*j<=count; j++){
+            if((count)%j==0){
                 isPrime = false;
                 break;
             }
         }
-        if(isPrime) result += sentence.substr(i-count, 1)+ " ";
+        if(isPrime) result += sentence.substr(i-count+1, 1)+ " ";
         count++;
     }
     return result;
-}
-
-int main() {
-    std::string word;
-    word = words_in_sentence("here is");
-    assert(word == "is");
-    return 0;
 }
