559d5fc5d4733e7324d0b03708e422c0cb431fd6
beep boop <githubbot@vadim.me>
2024-07-07 03:17:16 +0200

    Solution 57 of CPP/126, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/6lf0vkgh Prompt: 6194c8b3-8ff2-4038-82f0-73549bf602a3.cpp:1:16: error: ‘vector’ was not declared in this scope     1 | bool is_sorted(vector<int> lst){       |                ^~~~~~ 6194c8b3-8ff2-4038-82f0-73549bf602a3.cpp:1:23: error: expected primary-expressio n before ‘int’     1 | bool is_sorted(vector<int> lst){       |                       ^~~ In file included from 6194c8b3-8ff2-4038-82f0-73549bf602a3.cpp:24: 6194c8b3-8ff2-4038-82f0-73549bf602a3.cpp: In function ‘int main()’: 6194c8b3-8ff2-4038-82f0-73549bf602a3.cpp:26:22: error: ‘is_sorted’ cannot be use d as a function    26 |     assert (is_sorted({1, 2, 3, 4}) == true);       |             ~~~~~~~~~^~~~~~~~~~~~~~

--- CPP_126.cpp
+++ CPP_126.cpp
@@ -1,10 +1,24 @@
-bool is_sorted(vector<int> lst){
+#include <vector>
+
+bool is_sorted(std::vector<int> lst){
+    if(lst.size() <= 1) return true;
     for(int i = 1; i < lst.size(); i++){
-        if(lst[i-1] >= lst[i]){
-            vector<int>::iterator it = unique(lst.begin(), lst.end());
-            lst.erase(it, lst.end());
-            return false;
+        if(lst[i] >= lst[i-1]){
+            continue;
+        }
+        else{
+            int count = 0;
+            for(int j = i; j < lst.size(); j++){
+                if(lst[j] == lst[i]){
+                    count++;
+                }
+                else{
+                    break;
+                }
+            }
+            if(count > 1) return false;
+            else return true;
         }
     }
-    return true;
+    return false;
 }
