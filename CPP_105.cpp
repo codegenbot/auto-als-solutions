d96e492d25ed9751c6b40728634ec984b7e12b78
beep boop <githubbot@vadim.me>
2024-07-07 02:38:35 +0200

    Solution 70 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/4yrb3a76 Prompt:       |                              ^~~~~~ 3578ae5c-dc5b-4d70-b1a4-c70dabafc2ec.cpp:21:37: error: ‘string’ was not declared  in this scope    21 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 3578ae5c-dc5b-4d70-b1a4-c70dabafc2ec.cpp:21:44: error: ‘b’ was not declared in t his scope    21 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 3578ae5c-dc5b-4d70-b1a4-c70dabafc2ec.cpp:21:45: error: expression list treated a s compound expression in initializer [-fpermissive]    21 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 3578ae5c-dc5b-4d70-b1a4-c70dabafc2ec.cpp:20: 3578ae5c-dc5b-4d70-b1a4-c70dabafc2ec.cpp: In function ‘int main()’: 3578ae5c-dc5b-4d70-b1a4-c70dabafc2ec.cpp:30:20: error: ‘by_length’ was not decla red in this scope    30 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ 3578ae5c-dc5b-4d70-b1a4-c70dabafc2ec.cpp:30:19: error: ‘issame’ cannot be used a s a function    30 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,14 +1,36 @@
-using namespace std;
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <map>
 
-vector<string> by_length(vector<int> arr){
-    vector<int> nums;
-    for(auto x:arr){
-        if(x>=1 && x<=9)nums.push_back(x);
+bool issame(vector<string> a, vector<string> b) {
+    if (a.size() != b.size()) return false;
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) return false;
     }
-    sort(nums.begin(),nums.end());
-    reverse(nums.begin(),nums.end());
+    return true;
+}
+
+vector<string> by_length(vector<int> arr) {
+    vector<int> numbers;
+    for (int num : arr) {
+        if (num >= 1 && num <= 9) {
+            numbers.push_back(num);
+        }
+    }
+    sort(numbers.begin(), numbers.end());
+    reverse(numbers.begin(), numbers.end());
     vector<string> result;
-    map<int,string> numNames = {{1,"One"},{2,"Two"},{3,"Three"},{4,"Four"},{5,"Five"},{6,"Six"},{7,"Seven"},{8,"Eight"},{9,"Nine"}; 
-    for(auto x:nums)result.push_back(numNames[x]);
+    map<int, string> numberNames = {{1, "One"}, {2, "Two"}, {3, "Three"},
+                                     {4, "Four"}, {5, "Five"}, {6, "Six"},
+                                     {7, "Seven"}, {8, "Eight"}, {9, "Nine"}};
+    for (int num : numbers) {
+        result.push_back(numberNames[num]);
+    }
     return result;
+}
+
+int main() {
+    assert(issame(by_length({9, 4, 8}), {"Nine", "Four", "Eight"}));
+    // rest of your code
 }
