f40bb3fb9dfad3d65d5b648f6b2496157dfb64f4
beep boop <githubbot@vadim.me>
2024-07-08 00:30:57 +0200

    Solution 48 of spin-words, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/4mbzio5l Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from b8fee560-ed1f-4956-93d3-5d047a828a8e.cpp:3: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- spin-words.cpp
+++ spin-words.cpp
@@ -1,21 +1,35 @@
-std::string spinWords(std::string sentence) {
-    std::vector<std::string> words;
-    std::istringstream iss(sentence);
-    std::string word;
+#include <vector>
+#include <iostream>
+#include <string>
 
-    while (iss >> word) {
-        if (word.length() >= 5)
-            word = std::string(word.rbegin(), word.rend());
-        words.push_back(word);
-    }
+using namespace std;
 
-    sentence = ""; 
-    for (int i = 0; i < words.size(); ++i) {
-        sentence += words.back();
-        if (i != words.size() - 1)
-            sentence += " "; 
-        else
-            words.pop_back(); 
+string spinWords(string str) {
+    string result = "";
+    int i = 0;
+    
+    while(i < str.length()) {
+        if(str[i] != ' ') {
+            if(str.substr(i).size() >= 5) {
+                for(int j=str.substr(i).size()-1; j>=0; --j)
+                    result += str[i+j];
+                result += " ";
+            } else 
+                result += str[i];
+            i++;
+        } else {
+            result += ' ';
+            i++;
+        }
     }
-    return sentence;
+    
+    return result.substr(0, result.length()-1);
+}
+
+int main() {
+    string input;
+    while (cin >> input) {
+        cout << spinWords(input) << endl;
+    }
+    return 0;
 }
