e2c480024eb08c265c0f230dc434639793812204
beep boop <githubbot@vadim.me>
2024-07-07 03:10:24 +0200

    Solution 0 of CPP/144, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/k4wf49uc Prompt: Complete the following code given the task description and function signature.

---
+++ CPP_144.cpp
@@ -0,0 +1,26 @@
+#include <string>
+using namespace std;
+
+bool simplify(string x, string n) {
+    int numerator1 = 0, denominator1 = 0;
+    int numerator2 = 0, denominator2 = 0;
+
+    size_t pos = x.find('/');
+    numerator1 = stoi(x.substr(0, pos));
+    denominator1 = stoi(x.substr(pos + 1));
+
+    pos = n.find('/');
+    numerator2 = stoi(n.substr(0, pos));
+    denominator2 = stoi(n.substr(pos + 1));
+
+    if (denominator1 != denominator2)
+        return false;
+
+    int newNumerator = numerator1 * denominator2;
+    int newDenominator = denominator1;
+
+    if (newNumerator % newDenominator != 0)
+        return false;
+
+    return true;
+}
