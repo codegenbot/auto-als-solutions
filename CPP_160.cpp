16050c1feed5a363d289fdf5d235900cf84ee833
beep boop <githubbot@vadim.me>
2024-07-07 03:22:20 +0200

    Solution 2 of CPP/160, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/7o99s3y4 Prompt: Complete the following code given the task description and function signature.

--- CPP_160.cpp
+++ CPP_160.cpp
@@ -1,16 +1,28 @@
-int do_algebra(vector<string> operato, vector<int> operand) {
-    int result = operand[0];
-    for (int i = 0; i < operato.size(); i++) {
-        if (operato[i] == "+") {
-            result += operand[i + 1];
-        } else if (operato[i] == "-") {
-            result -= operand[i + 1];
-        } else if (operato[i] == "*") {
-            result *= operand[i + 1];
-        } else if (operato[i] == "//") {
-            result = result / operand[i + 1];
-        } else if (operato[i] == "**") {
-            result = pow(result, operand[i + 1]);
+int do_algebra(vector<string> operator_, vector<int> operands_) {
+    int result = 0;
+    for (int i = 0; i < operator_.size(); i++) {
+        if (operator_[i] == "+") {
+            result += operands_[i];
+        } else if (operator_[i] == "-") {
+            result -= operands_[i];
+        } else if (operator_[i] == "*") {
+            int temp = 0;
+            for (int j = i; j < operands_.size(); j++) {
+                temp *= operands_[j];
+            }
+            result += temp;
+        } else if (operator_[i] == "/*" || operator_[i] == "//") {
+            int temp = operands_[i] / 1;
+            for (int j = i + 1; j < operands_.size(); j++) {
+                temp /= operands_[j];
+            }
+            result += temp;
+        } else if (operator_[i] == "**") {
+            int temp = 1;
+            for (int j = i; j < operands_.size(); j++) {
+                temp *= pow(operands_[j], 1);
+            }
+            result += temp;
         }
     }
     return result;
