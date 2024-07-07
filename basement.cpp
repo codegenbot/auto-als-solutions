8e400c87a6ffa2b10767897752e57e58248e8750
beep boop <githubbot@vadim.me>
2024-07-07 16:50:25 +0200

    Solution 80 of basement, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/ui5djyyu Prompt: this scope    17 |     cin >> n;       |     ^~~ 502642cb-500d-4d21-8caa-1e8b204f6b5b.cpp:2:1: note: ‘std::cin’ is defined in hea der ‘<iostream>’; did you forget to ‘#include <iostream>’?     1 | #include <vector>   +++ |+#include <iostream>     2 | using namespace std; 502642cb-500d-4d21-8caa-1e8b204f6b5b.cpp:23:5: error: ‘cout’ was not declared in  this scope    23 |     cout << basementIndex(nums) << endl;       |     ^~~~ 502642cb-500d-4d21-8caa-1e8b204f6b5b.cpp:23:5: note: ‘std::cout’ is defined in h eader ‘<iostream>’; did you forget to ‘#include <iostream>’? 502642cb-500d-4d21-8caa-1e8b204f6b5b.cpp:23:36: error: ‘endl’ was not declared i n this scope    23 |     cout << basementIndex(nums) << endl;       |                                    ^~~~ 502642cb-500d-4d21-8caa-1e8b204f6b5b.cpp:2:1: note: ‘std::endl’ is defined in he ader ‘<ostream>’; did you forget to ‘#include <ostream>’?     1 | #include <vector>   +++ |+#include <ostream>     2 | using namespace std;

--- basement.cpp
+++ basement.cpp
@@ -1,12 +1,26 @@
 #include <vector>
+#include <iostream>
 using namespace std;
 
-int basement(vector<int>& nums) {
+int basementIndex(vector<int> &nums) {
     int sum = 0;
     for (int i = 0; i < nums.size(); i++) {
         sum += nums[i];
         if (sum < 0)
             return i;
     }
-    return -1; // not found
+    return -1;
+}
+
+int main() {
+    vector<int> nums;
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        nums.push_back(x);
+    }
+    cout << basementIndex(nums) << endl;
+    return 0;
 }
