32bd1bf937bead5bd41871d1a54a6807d770066e
beep boop <githubbot@vadim.me>
2024-07-07 14:27:17 +0200

    Solution 27 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/zp5pwy07 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b36e4e10-e50d-40f7-a21e-cb2bbb6b4 6e8.py", line 1     ```     ^ SyntaxError: invalid syntax

--- bowling.py
+++ bowling.py
@@ -2,14 +2,14 @@ def bowling_score(frames):
     score = 0
     frames = frames.split(' ')
     for i in range(0, len(frames), 2):
-        if '/' in frames[i]:
+        if '/' not in frames[i]:
+            score += int(frames[i])
+        else:
             split_frame = list(map(int, frames[i].split('/')))
             if sum(split_frame) == 10:
-                score += 10 + sum(split_frame)
-            elif max(split_frame) == 10:
-                score += 10 + sum(split_frame)
+                score += 10
+            elif split_frame[0] + split_frame[1] == 10:
+                score += 10
             else:
                 score += sum(split_frame)
-        else:
-            score += int(frames[i])
     return score
