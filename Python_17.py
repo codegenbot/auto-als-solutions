8ced403da24c43d9b540a7fa811291680bdbdd49
beep boop <githubbot@vadim.me>
2024-07-07 03:32:46 +0200

    Solution 53 of Python/17, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/nqzqbtai Prompt: Program did not receive expected input

--- Python_17.py
+++ Python_17.py
@@ -4,16 +4,14 @@ def parse_music(music_string: str) -> list:
     music_list = []
     i = 0
     while i < len(music_string):
-        if i + 1 <= len(music_string) and music_string[i:i+2] == 'o|':
-            note_duration = 4
-            i += 2
-        elif i + 2 <= len(music_string) and music_string[i:i+3] == '.||':
+        if i+3 <= len(music_string) and music_string[i:i+3] == '.||':
             note_duration = 1
             i += 3
+        elif i+2 <= len(music_string) and music_string[i:i+2] in ['o|', 'O|']:
+            note_duration = 4
+            i += 2
         else:
             i += 1
-            continue
-        note_name = music_string[i]
-        if note_name in notes:
-            music_list.append((notes[note_name], note_duration))
+        if i < len(music_string) and music_string[i] in notes:
+            music_list.append((notes[music_string[i]], note_duration))
     return music_list
