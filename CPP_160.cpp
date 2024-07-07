920085ae6ca58938ddb8e5aaea2deeba6a03c35b
beep boop <githubbot@vadim.me>
2024-07-07 03:38:24 +0200

    Solution 61 of CPP/160, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/7o99s3y4 Prompt:       |                       ^~~~~~ 57d07364-fb1c-4527-acc7-414490878c25.cpp:1:31: error: ‘operator_’ was not declar ed in this scope     1 | int do_algebra(vector<string> operator_, vector<int> operands_) {       |                               ^~~~~~~~~ 57d07364-fb1c-4527-acc7-414490878c25.cpp:1:42: error: ‘vector’ was not declared in this scope     1 | int do_algebra(vector<string> operator_, vector<int> operands_) {       |                                          ^~~~~~ 57d07364-fb1c-4527-acc7-414490878c25.cpp:1:49: error: expected primary-expressio n before ‘int’     1 | int do_algebra(vector<string> operator_, vector<int> operands_) {       |                                                 ^~~ 57d07364-fb1c-4527-acc7-414490878c25.cpp:1:63: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | int do_algebra(vector<string> operator_, vector<int> operands_) {       |                                                               ^ In file included from 57d07364-fb1c-4527-acc7-414490878c25.cpp:31: 57d07364-fb1c-4527-acc7-414490878c25.cpp: In function ‘int main()’: 57d07364-fb1c-4527-acc7-414490878c25.cpp:33:23: error: ‘do_algebra’ cannot be us ed as a function    33 |     assert (do_algebra({"//", "*"}, {7, 3, 4}) == 8);       |             ~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_160.cpp
+++ CPP_160.cpp
@@ -6,7 +6,6 @@ int do_algebra(vector<pair<char, int>> expressions) {
     for (const auto& expression : expressions) {
         char operator_;
         int operand_;
-
         std::tie(operator_, operand_) = expression;
 
         if (operator_ == '+') {
@@ -14,16 +13,16 @@ int do_algebra(vector<pair<char, int>> expressions) {
         } else if (operator_ == '-') {
             result -= operand_;
         } else if (operator_ == '*') {
-            int temp = 1;
-            for (int i = 0; i < expressions.size(); i++) {
+            int temp = 0;
+            for (int i = expressions.size() - 1; i >= 0; --i) {
                 if (expressions[i].first == '*') {
                     temp *= expressions[i].second;
                 }
             }
-            result += temp * operand_;
+            result += temp;
         } else if (operator_ == "/") {
             int temp = operand_;
-            for (int i = 0; i < expressions.size(); i++) {
+            for (int i = expressions.size() - 1; i >= 0; --i) {
                 if (expressions[i].first == '/') {
                     temp /= expressions[i].second;
                 }
@@ -31,12 +30,12 @@ int do_algebra(vector<pair<char, int>> expressions) {
             result += temp;
         } else if (operator_ == "**") {
             int temp = 1;
-            for (int i = 0; i < expressions.size(); i++) {
+            for (int i = expressions.size() - 1; i >= 0; --i) {
                 if (expressions[i].first == '**') {
-                    temp *= pow(expressions[i].second, 1.0);
+                    temp *= pow(expressions[i].second, 1);
                 }
             }
-            result += temp * operand_;
+            result += temp;
         }
     }
     return result;
