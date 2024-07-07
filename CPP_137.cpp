61b3aba8ab53c4ac1794bd3b2f298f2e1da3bbb2
beep boop <githubbot@vadim.me>
2024-07-07 03:51:48 +0200

    Solution 98 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/1t9tb84a Prompt: de718a88-6372-48c6-8a36-01ddea3f2a26.cpp:1:1: error: ‘boost’ does not name a typ e     1 | boost::any compare_one(boost::any a, boost::any b) {       | ^~~~~ In file included from de718a88-6372-48c6-8a36-01ddea3f2a26.cpp:28: de718a88-6372-48c6-8a36-01ddea3f2a26.cpp: In function ‘int main()’: de718a88-6372-48c6-8a36-01ddea3f2a26.cpp:30:13: error: ‘boost’ has not been decl ared    30 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ de718a88-6372-48c6-8a36-01ddea3f2a26.cpp:30:29: error: ‘string’ was not declared  in this scope    30 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ de718a88-6372-48c6-8a36-01ddea3f2a26.cpp:30:37: error: ‘compare_one’ was not dec lared in this scope    30 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                     ^~~~~~~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -5,24 +5,24 @@
 
 boost::any compare_one(boost::any a, boost::any b) {
     if (a.type() == typeid(int) && b.type() == typeid(float)) {
-        return boost::any((int)b > (int)a ? b : a);
+        return boost::any_cast<int>(b) > boost::any_cast<int>(a) ? b : a;
     }
     else if (a.type() == typeid(int) && b.type() == typeid(double)) {
-        return boost::any((int)b > (int)a ? b : a);
+        return boost::any_cast<double>(b) > boost::any_cast<int>(a) ? b : a;
     }
     else if (a.type() == typeid(int) && b.type() == typeid(std::string)) {
-        double val = std::stod(b.convert<std::string>().c_str());
-        return val > (double)a ? b : a;
+        double val = std::stod(boost::any_cast<std::string>(b).c_str());
+        return val > boost::any_cast<int>(a) ? b : a;
     }
     else if (a.type() == typeid(float) && b.type() == typeid(double)) {
-        return boost::any((double)b > (float)a ? b : a);
+        return boost::any_cast<double>(b) > boost::any_cast<float>(a) ? b : a;
     }
     else if (a.type() == typeid(std::string) && b.type() == typeid(double)) {
-        double val = std::stod(b.convert<std::string>().c_str());
-        return val > std::stod(a.convert<std::string>().c_str()) ? b : a;
+        double val = std::stod(boost::any_cast<std::string>(b).c_str());
+        return val > std::stod(boost::any_cast<std::string>(a).c_str()) ? b : a;
     }
     else if (a.type() == typeid(std::string) && b.type() == typeid(std::string)) {
-        double val1 = std::stod(a.convert<std::string>().c_str()), val2 = std::stod(b.convert<std::string>().c_str());
+        double val1 = std::stod(boost::any_cast<std::string>(a).c_str()), val2 = std::stod(boost::any_cast<std::string>(b).c_str());
         return val2 > val1 ? b : a;
     }
     else {
