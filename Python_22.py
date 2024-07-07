46b65aa53732adb70d20ea998741d46734cc4349
beep boop <githubbot@vadim.me>
2024-07-07 02:49:52 +0200

    Solution 23 of Python/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/991lhs6q Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d9d9e9bc-e6db-4407-bcb3-13402be97 2c0.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_22.py
+++ Python_22.py
@@ -1,10 +1,11 @@
 from typing import List
 
+
 def filter_integers() -> List[int]:
     values = []
     while True:
         user_input = input("Enter a value (or 'stop' to finish): ")
-        if user_input.lower() == 'stop':
+        if user_input.lower() == "stop":
             break
         try:
             value = int(user_input)
@@ -12,5 +13,6 @@ def filter_integers() -> List[int]:
         except ValueError:
             print(f"Invalid input '{user_input}'! Please enter an integer.")
     return values
+
 
 print(filter_integers())
