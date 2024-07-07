2742367c41d8c03674a4540f5dcf6b5c2b7fddfc
beep boop <githubbot@vadim.me>
2024-07-07 03:22:49 +0200

    Solution 40 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/1t9tb84a Prompt: a9ba005a-d6c0-486d-8900-48b10cfd12f9.cpp:3:10: fatal error: boost/any.hpp: No su ch file or directory     3 | #include <boost/any.hpp>       |          ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -4,11 +4,12 @@
 #include <boost/lexical_cast.hpp>
 
 using namespace std;
+
 boost::any compare_one(boost::any a, boost::any b) {
     if (a.type() == typeid(int) && b.type() == typeid(int)) {
-        return max(a, b);
+        return boost::any(max(boost::any_cast<int>(a), boost::any_cast<int>(b)));
     } else if (a.type() == typeid(float) && b.type() == typeid(float)) {
-        return max(a, b);
+        return boost::any(max(boost::any_cast<float>(a), boost::any_cast<float>(b)));
     } else if ((a.type() == typeid(string) || a.type() == typeid(double)) &&
                (b.type() == typeid(string) || b.type() == typeid(double))) {
         string strA = boost::any_cast<string>(a);
@@ -33,7 +34,7 @@ boost::any compare_one(boost::any a, boost::any b) {
     }
 
     if (boost::any_cast<int>(a) == boost::any_cast<int>(b)) {
-        return boost::any("None");
+        return boost::any("Equal");
     } else if (boost::any_cast<double>(a) > boost::any_cast<double>(b)) {
         return a;
     } else {
@@ -42,10 +43,10 @@ boost::any compare_one(boost::any a, boost::any b) {
 }
 
 int main() {
-    cout << boost::any_cast<string>(compare_one(boost::any(1), boost::any(2.5))) << endl;
-    cout << boost::any_cast<string>(compare_one(boost::any(1), boost::any("2,3"))) << endl;
-    cout << boost::any_cast<string>(compare_one(boost::any("5,1"), boost::any("6"))) << endl;
-    cout << boost::any_cast<string>(compare_one(boost::any("1"), boost::any(1))) << endl;
+    cout << boost::any_cast<string>(compare_one(1, 2.5)) << endl;
+    cout << boost::any_cast<string>(compare_one(1, "2,3")) << endl;
+    cout << boost::any_cast<string>(compare_one("5,1", "6")) << endl;
+    cout << boost::any_cast<string>(compare_one("1", 1)) << endl;
 
     return 0;
 }
