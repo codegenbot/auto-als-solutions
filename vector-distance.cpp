acaad615b16f387a08893233cb0681703ea7c13f
beep boop <githubbot@vadim.me>
2024-07-08 01:09:23 +0200

    Solution 93 of vector-distance, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/0xykrijb Prompt: 7316130d-506f-41e7-a65d-bcb5c92e4aed.cpp: In function ‘double vectorDistance(std ::vector<float>, std::vector<float>)’: 7316130d-506f-41e7-a65d-bcb5c92e4aed.cpp:8:16: error: ‘pow’ was not declared in this scope     8 |         sum += pow(v1[i] - v2[i], 2);       |                ^~~ 7316130d-506f-41e7-a65d-bcb5c92e4aed.cpp:10:12: error: ‘sqrt’ was not declared i n this scope    10 |     return sqrt(sum);       |            ^~~~ 7316130d-506f-41e7-a65d-bcb5c92e4aed.cpp: In function ‘int main()’: 7316130d-506f-41e7-a65d-bcb5c92e4aed.cpp:23:13: error: ‘setprecision’ was not de clared in this scope    23 |     cout << setprecision(10) << fixed << vectorDistance(v1, v2) << endl;       |             ^~~~~~~~~~~~

--- vector-distance.cpp
+++ vector-distance.cpp
@@ -15,13 +15,14 @@ double vectorDistance(vector<float> v1, vector<float> v2) {
 int main() {
     int n;
     cin >> n;
-
+    
     vector<float> v1(n), v2(n);
-
+    
     for (int i = 0; i < n; i++) {
         cin >> v1[i] >> v2[i];
     }
-
+    
     cout << setprecision(10) << fixed << vectorDistance(v1, v2) << endl;
-
+    
     return 0;
+}
