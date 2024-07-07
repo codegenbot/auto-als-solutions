fa9ac38fafb5e9a7c2c0b377340c6d0ffe3470ed
beep boop <githubbot@vadim.me>
2024-07-07 04:03:36 +0200

    Solution 81 of Python/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/57cd07pw Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b9a44403-7342-42d9-b84a-97db42808 81e.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_26.py
+++ Python_26.py
@@ -1,6 +1,8 @@
-def remove_duplicates() -> List[int]:
+def unique_numbers() -> List[int]:
     user_input = input("Enter numbers separated by space: ")
+    
     if not user_input:
         return []
+    
     numbers = list(map(int, user_input.split()))
     return sorted(list(set(numbers)))
