7b711237edaf511b0d96a746ece9b3b40efbed4b
beep boop <githubbot@vadim.me>
2024-07-07 15:30:35 +0200

    Solution 76 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/zp5pwy07 Prompt: it must return 77 for input 22417-7/8-3217354352, but it returns .

--- bowling.py
+++ bowling.py
@@ -3,35 +3,52 @@ def bowling_score(s):
     score = 0
     i = 0
     while i < len(s):
-        if s[i] == "X":
-            score += 10
-            i += 1
-            if i < len(s) and s[i] in "12345678":
-                first_roll = int(s[i])
-                if i + 1 < len(s) and s[i + 1].isdigit():
-                    second_roll = int(s[i + 1])
+        if s[i].isdigit():
+            first_roll = int(s[i])
+            if i + 1 < len(s) and s[i+1].isdigit():
+                second_roll = int(s[i+1])
+                if first_roll + second_roll == 10:
+                    score += 10
+                    i += 2
+                else:
+                    score += first_roll + second_roll
+                    i += 2
+            elif s[i+1] == "X":
+                score += 10
+                i += 2
+            else:
+                if s[i+1].isdigit():
+                    second_roll = int(s[i+1])
                     if first_roll + second_roll == 10:
                         score += 10
+                        i += 2
                     else:
                         score += first_roll + second_roll
-                elif s[i + 1] == "X":
+                        i += 2
+                elif s[i+1] == "X":
                     score += 10
-            i += 1
-        elif s[i].isdigit() and (i + 1 < len(s) and s[i + 1].isdigit()):
-            first_roll = int(s[i])
-            second_roll = int(s[i + 1])
-            if first_roll + second_roll == 10:
-                score += 10
+                    i += 1
+                else:
+                    if first_roll == 10:
+                        bonus = int(s[i+2])
+                        score += 10 + bonus
+                        i += 3
+                    else:
+                        i += 2
+        elif s[i] == "X":
+            if i != len(s) - 1 and s[i+1].isdigit():
+                second_roll = int(s[i+1])
+                score += 10 + second_roll
+                i += 2
             else:
-                score += first_roll + second_roll
-            i += 2
+                score += 10
+                i += 1
         elif s[i] == "/":
-            first_roll = int(s[i - 1])
-            second_roll = int(s[i + 1])
-            score += first_roll + second_roll
-            i += 2
-        else:
-            if s[i].isdigit():
-                score += int(s[i])
-            i += 1
+            if i != len(s) - 1 and s[i+1].isdigit():
+                second_roll = int(s[i+1])
+                first_roll = 10 - second_roll
+                score += first_roll + second_roll
+                i += 2
+            else:
+                i += 1
     return score
