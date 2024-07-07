ea8da9d3d0c0b56de54703d709a6db01c0ada0a2
beep boop <githubbot@vadim.me>
2024-07-07 04:03:31 +0200

    Solution 84 of CPP/153, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/yyuya9y6 Prompt:       |                         ^~~~~~ 378e734c-deca-4965-9750-ac170dc04757.cpp:1:43: error: ‘vector’ was not declared in this scope     1 | int Strongest_Extension(string class_name,vector<string> extensions){       |                                           ^~~~~~ 378e734c-deca-4965-9750-ac170dc04757.cpp:1:50: error: ‘string’ was not declared in this scope     1 | int Strongest_Extension(string class_name,vector<string> extensions){       |                                                  ^~~~~~ 378e734c-deca-4965-9750-ac170dc04757.cpp:1:58: error: ‘extensions’ was not decla red in this scope     1 | int Strongest_Extension(string class_name,vector<string> extensions){       |                                                          ^~~~~~~~~~ 378e734c-deca-4965-9750-ac170dc04757.cpp:1:68: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | int Strongest_Extension(string class_name,vector<string> extensions){       |                                                                    ^ In file included from 378e734c-deca-4965-9750-ac170dc04757.cpp:23: 378e734c-deca-4965-9750-ac170dc04757.cpp: In function ‘int main()’: 378e734c-deca-4965-9750-ac170dc04757.cpp:25:32: error: ‘Strongest_Extension’ can not be used as a function    25 |     assert (Strongest_Extension("Sp", {"671235", "Bb"}) == "Sp.671235");       |             ~~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_153.cpp
+++ CPP_153.cpp
@@ -1,11 +1,11 @@
 #include <string>
 #include <vector>
 
-int Strongest_Extension(std::string class_name, std::vector<std::string> extensions){
+int Strongest_Extension(string class_name, vector<string> extensions){
     int max_strength = 0;
-    std::string strongest_extension;
+    string strongest_extension;
 
-    for (std::string extension : extensions) {
+    for (string extension : extensions) {
         int cap = 0, sm = 0;
         for (char c : extension) {
             if (isupper(c)) cap++;
