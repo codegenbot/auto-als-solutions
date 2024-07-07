760559316570dfb20438e4b827aa0e17a400e04a
beep boop <githubbot@vadim.me>
2024-07-07 03:29:56 +0200

    Solution 31 of CPP/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/u90fhiul Prompt: babb7eab-f09d-40ac-8cc0-3b556f1dca3b.cpp:1:1: error: ‘Here’ does not name a type     1 | Here is the completed code:       | ^~~~ In file included from babb7eab-f09d-40ac-8cc0-3b556f1dca3b.cpp:15: babb7eab-f09d-40ac-8cc0-3b556f1dca3b.cpp: In function ‘int main()’: babb7eab-f09d-40ac-8cc0-3b556f1dca3b.cpp:17:35: error: ‘lst’ was not declared in  this scope    17 |     assert (double_the_difference(lst) == odd_sum );       |                                   ^~~ babb7eab-f09d-40ac-8cc0-3b556f1dca3b.cpp:17:13: error: ‘double_the_difference’ w as not declared in this scope    17 |     assert (double_the_difference(lst) == odd_sum );       |             ^~~~~~~~~~~~~~~~~~~~~ babb7eab-f09d-40ac-8cc0-3b556f1dca3b.cpp:17:43: error: ‘odd_sum’ was not declare d in this scope    17 |     assert (double_the_difference(lst) == odd_sum );       |                                           ^~~~~~~

--- CPP_151.cpp
+++ CPP_151.cpp
@@ -6,9 +6,8 @@ long long double_the_difference(vector<float> lst) {
     long long sum = 0;
     for (float num : lst) {
         if (num > 0 && floor(num) == num) { 
-            if (num % 2 != 0) { 
+            if (num % 2 != 0) 
                 sum += pow(num, 2); 
-            }
         }
     }
     return sum;
