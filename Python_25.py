596359d55b8cc89db390487cb359ce233249ae49
beep boop <githubbot@vadim.me>
2024-07-07 03:43:54 +0200

    Solution 36 of Python/25, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/z9kplh9m Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d6c0d4f5-edf4-40e6-93f0-8b299e12b 8be.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_25.py
+++ Python_25.py
@@ -1,10 +1,9 @@
-```
 def find_primes():
     num = int(input("Enter a number: "))
     factors = [i for i in range(1, num + 1) if num % i == 0]
     prime_factors = [
         factor for factor in set(factors) if all(num % i > 0 for i in range(2, int(factor ** 0.5) + 1))
     ]
-    return list(set(prime_factors))
+    return prime_factors
 
 print(find_primes())
