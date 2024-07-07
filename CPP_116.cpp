874a18cd33028a2e8b9172db0d5caa236b508dbb
beep boop <githubbot@vadim.me>
2024-07-07 02:40:34 +0200

    Solution 41 of CPP/116, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/j8vzgzam Prompt:       |                    ^~~ 256ac92c-5032-455d-a08f-397d49c224e2.cpp:24:27: error: ‘vector’ was not declared  in this scope    24 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 256ac92c-5032-455d-a08f-397d49c224e2.cpp:24:34: error: expected primary-expressi on before ‘int’    24 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 256ac92c-5032-455d-a08f-397d49c224e2.cpp:24:39: error: expression list treated a s compound expression in initializer [-fpermissive]    24 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 256ac92c-5032-455d-a08f-397d49c224e2.cpp:23: 256ac92c-5032-455d-a08f-397d49c224e2.cpp: In function ‘int main()’: 256ac92c-5032-455d-a08f-397d49c224e2.cpp:33:20: error: ‘sort_array’ was not decl ared in this scope    33 |     assert (issame(sort_array({2,4,8,16,32}) , {2, 4, 8, 16, 32}));       |                    ^~~~~~~~~~ 256ac92c-5032-455d-a08f-397d49c224e2.cpp:33:19: error: ‘issame’ cannot be used a s a function    33 |     assert (issame(sort_array({2,4,8,16,32}) , {2, 4, 8, 16, 32}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_116.cpp
+++ CPP_116.cpp
@@ -1,22 +1,3 @@
-vector<int> sort_array(vector<int> arr){
-    vector<int> result;
-    for(int i: arr) {
-        int ones = 0;
-        int num = i;
-        while(num > 0) {
-            if((num & 1) == 1)
-                ones++;
-            num >>= 1;
-        }
-        result.push_back({ones, i});
-    }
-    sort(result.begin(), result.end());
-    vector<int> res;
-    for(auto x: result) {
-        res.push_back(x.second);
-    }
-    return res;
-}
 bool issame(vector<vector<int>> a,vector<vector<int>> b){
     if(a.size()!=b.size()) return false;
     for(int i=0; i<a.size();i++){
