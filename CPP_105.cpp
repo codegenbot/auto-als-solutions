f77570f017a877f9af73863010161d2a726efcf2
beep boop <githubbot@vadim.me>
2024-07-07 02:10:40 +0200

    Solution 10 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/4yrb3a76 Prompt:       |                              ^~~~~~ b6da8999-1079-4023-9a36-37962387ee3a.cpp:27:37: error: ‘string’ was not declared  in this scope    27 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ b6da8999-1079-4023-9a36-37962387ee3a.cpp:27:44: error: ‘b’ was not declared in t his scope    27 | bool issame(vector<string> a,vector<string>b){       |                                            ^ b6da8999-1079-4023-9a36-37962387ee3a.cpp:27:45: error: expression list treated a s compound expression in initializer [-fpermissive]    27 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from b6da8999-1079-4023-9a36-37962387ee3a.cpp:26: b6da8999-1079-4023-9a36-37962387ee3a.cpp: In function ‘int main()’: b6da8999-1079-4023-9a36-37962387ee3a.cpp:36:20: error: ‘by_length’ was not decla red in this scope    36 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ b6da8999-1079-4023-9a36-37962387ee3a.cpp:36:19: error: ‘issame’ cannot be used a s a function    36 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,48 +1,32 @@
+#include <vector>
+#include <string>
+
+bool issame(vector<string> a, vector<string> b) {
+    if (a.size() != b.size()) return false;
+    for (int i = 0; i < a.size(); ++i)
+        if (a[i] != b[i]) return false;
+    return true;
+}
+
 vector<string> by_length(vector<int> arr) {
-    vector<int> tempArr;
-    for (int num : arr) {
-        if (num >= 1 && num <= 9) {
-            tempArr.push_back(num);
-        }
-    }
-
-    sort(tempArr.begin(), tempArr.end());
-    reverse(tempArr.begin(), tempArr.end());
-
     vector<string> result;
-    for (int num : tempArr) {
-        string str = "";
-        switch (num) {
-            case 1:
-                str = "One";
-                break;
-            case 2:
-                str = "Two";
-                break;
-            case 3:
-                str = "Three";
-                break;
-            case 4:
-                str = "Four";
-                break;
-            case 5:
-                str = "Five";
-                break;
-            case 6:
-                str = "Six";
-                break;
-            case 7:
-                str = "Seven";
-                break;
-            case 8:
-                str = "Eight";
-                break;
-            case 9:
-                str = "Nine";
-                break;
+    for (int i : arr) {
+        switch (i) {
+            case 1: result.push_back("One"); break;
+            case 2: result.push_back("Two"); break;
+            case 3: result.push_back("Three"); break;
+            case 4: result.push_back("Four"); break;
+            case 5: result.push_back("Five"); break;
+            case 6: result.push_back("Six"); break;
+            case 7: result.push_back("Seven"); break;
+            case 8: result.push_back("Eight"); break;
+            case 9: result.push_back("Nine"); break;
         }
-        result.push_back(str);
     }
-
     return result;
+}
+
+int main() {
+    assert(issame(by_length({9, 4, 8}), {"Nine", "Four", "Eight"}));
+    return 0;
 }
