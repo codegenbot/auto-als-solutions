8cfdbe7d7fd25d9ebb656232f3f2f5876dae02df
beep boop <githubbot@vadim.me>
2024-07-07 21:32:06 +0200

    Solution 77 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/utgpeiz7 Prompt: 558bffe3-1bb2-4de4-9f98-759d7172d6a6.cpp: In function ‘std::vector<int> cutVecto r(std::vector<int>&)’: 558bffe3-1bb2-4de4-9f98-759d7172d6a6.cpp:6:19: error: ‘INT_MAX’ was not declared  in this scope     6 |     int minDiff = INT_MAX;       |                   ^~~~~~~ 558bffe3-1bb2-4de4-9f98-759d7172d6a6.cpp:2:1: note: ‘INT_MAX’ is defined in head er ‘<climits>’; did you forget to ‘#include <climits>’?     1 | #include <vector>   +++ |+#include <climits>     2 | using namespace std; 558bffe3-1bb2-4de4-9f98-759d7172d6a6.cpp:10:20: error: ‘abs’ was not declared in  this scope    10 |         int diff = abs(nums[i-1] - nums[i]);       |                    ^~~ 558bffe3-1bb2-4de4-9f98-759d7172d6a6.cpp:20:24: error: could not convert ‘{left,  right}’ from ‘<brace-enclosed initializer list>’ to ‘std::vector<int>’    20 |     return {left, right};       |                        ^       |                        |       |                        <brace-enclosed initializer list>

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -22,8 +22,8 @@ Result cutVector(vector<int>& nums) {
         }
     }
 
-    Result result;
-    result.left = vector<int>(nums.begin(), nums.begin() + cutIndex);
-    result.right = vector<int>(nums.begin() + cutIndex, nums.end());
+    vector<int> left = vector<int>(nums.begin(), nums.begin() + cutIndex);
+    vector<int> right = vector<int>(nums.begin() + cutIndex, nums.end());
 
-    return result;
+    return Result{left, right};
+}
