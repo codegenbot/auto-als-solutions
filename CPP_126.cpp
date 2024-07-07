ebea04c29ac2ac7c91c5f7eead27151f1ac7b519
beep boop <githubbot@vadim.me>
2024-07-07 02:52:22 +0200

    Solution 20 of CPP/126, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/6lf0vkgh Prompt: 146c27cc-1615-4f14-bf70-764de008d8f1.cpp:1:16: error: ‘vector’ was not declared in this scope     1 | bool is_sorted(vector<int> lst){       |                ^~~~~~ 146c27cc-1615-4f14-bf70-764de008d8f1.cpp:1:23: error: expected primary-expressio n before ‘int’     1 | bool is_sorted(vector<int> lst){       |                       ^~~ In file included from 146c27cc-1615-4f14-bf70-764de008d8f1.cpp:12: 146c27cc-1615-4f14-bf70-764de008d8f1.cpp: In function ‘int main()’: 146c27cc-1615-4f14-bf70-764de008d8f1.cpp:14:22: error: ‘is_sorted’ cannot be use d as a function    14 |     assert (is_sorted({1, 2, 3, 4}) == true);       |             ~~~~~~~~~^~~~~~~~~~~~~~

--- CPP_126.cpp
+++ CPP_126.cpp
@@ -1,10 +1,17 @@
+#include <iostream>
 #include <vector>
+#include <algorithm>
 
-bool is_sorted(vector<int> lst){
+bool is_sorted(std::vector<int> lst){
     for(int i = 1; i < lst.size(); i++){
         if(lst[i-1] >= lst[i]){
             return false;
         }
     }
     return true;
+}
+
+int main() {
+    assert(is_sorted({1, 2, 3, 4}) == true);
+    return 0;
 }
