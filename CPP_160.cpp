0c50cf689dce478c446ac2b22b94237cf02afec0
beep boop <githubbot@vadim.me>
2024-07-07 03:38:39 +0200

    Solution 63 of CPP/160, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/7o99s3y4 Prompt:       |                       ^~~~~~ 57d07364-fb1c-4527-acc7-414490878c25.cpp:1:31: error: ‘operator_’ was not declar ed in this scope     1 | int do_algebra(vector<string> operator_, vector<int> operands_) {       |                               ^~~~~~~~~ 57d07364-fb1c-4527-acc7-414490878c25.cpp:1:42: error: ‘vector’ was not declared in this scope     1 | int do_algebra(vector<string> operator_, vector<int> operands_) {       |                                          ^~~~~~ 57d07364-fb1c-4527-acc7-414490878c25.cpp:1:49: error: expected primary-expressio n before ‘int’     1 | int do_algebra(vector<string> operator_, vector<int> operands_) {       |                                                 ^~~ 57d07364-fb1c-4527-acc7-414490878c25.cpp:1:63: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | int do_algebra(vector<string> operator_, vector<int> operands_) {       |                                                               ^ In file included from 57d07364-fb1c-4527-acc7-414490878c25.cpp:31: 57d07364-fb1c-4527-acc7-414490878c25.cpp: In function ‘int main()’: 57d07364-fb1c-4527-acc7-414490878c25.cpp:33:23: error: ‘do_algebra’ cannot be us ed as a function    33 |     assert (do_algebra({"//", "*"}, {7, 3, 4}) == 8);       |             ~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_160.cpp
+++ CPP_160.cpp
@@ -3,40 +3,36 @@
 
 int do_algebra(vector<pair<char, int>> expressions) {
     int result = 0;
-    for (const auto& expression : expressions) {
-        switch (expression.first) {
-            case '+':
-                result += expression.second;
-                break;
-            case '-':
-                result -= expression.second;
-                break;
-            case '*':
-                for (int i = expressions.size() - 1; i >= 0; --i) {
-                    if (expressions[i].first == '*') {
-                        result *= expressions[i].second;
-                    }
-                }
-                break;
-            case '/':
-            case '//':
-                int temp = expression.second;
-                for (int i = expressions.size() - 1; i >= 0; --i) {
-                    if ((expressions[i].first == '/') || (expressions[i].first == '//')) {
-                        temp /= expressions[i].second;
-                    }
-                }
-                result = temp;
-                break;
-            case '^':
-                int temp2 = 1;
-                for (int i = expressions.size() - 1; i >= 0; --i) {
-                    if (expressions[i].first == '^') {
-                        temp2 = pow(temp2, expressions[i].second);
-                    }
-                }
-                result = temp2;
-                break;
+    for (auto& expression : expressions) {
+        char operator_;
+        int operand;
+        std::tie(operator_, operand) = expression;
+
+        if (operator_ == '+') {
+            result += operand;
+        } else if (operator_ == '-') {
+            result -= operand;
+        } else if (operator_ == '*') {
+            int temp = 1;
+            for (int i = 0; i < expressions.size(); i++) {
+                if (expressions[i].first == '*')
+                    temp *= expressions[i].second;
+            }
+            result += temp * operand;
+        } else if (operator_ == '/') {
+            int temp = 1;
+            for (int i = 0; i < expressions.size(); i++) {
+                if (expressions[i].first == '/')
+                    temp /= expressions[i].second;
+            }
+            result += temp / operand;
+        } else if (operator_ == '**') {
+            int temp = 1;
+            for (int i = 0; i < expressions.size(); i++) {
+                if (expressions[i].first == '**')
+                    temp *= pow(expressions[i].second, 1);
+            }
+            result += temp * pow(operand, 1);
         }
     }
     return result;
