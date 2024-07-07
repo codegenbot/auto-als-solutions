657d25e7eb2d3b4847b2f789a2f60ac4b3ed3759
beep boop <githubbot@vadim.me>
2024-07-07 03:23:33 +0200

    Solution 0 of CPP/161, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/n95r81ax Prompt: Complete the following code given the task description and function signature.

---
+++ CPP_161.cpp
@@ -0,0 +1,16 @@
+string solve(string s){
+    string result = "";
+    for(int i=0; i<s.length();i++){
+        if(isalpha(s[i])){
+            if(islower(s[i]))
+                result += toupper(s[i]);
+            else
+                result += tolower(s[i]);
+        }
+        else
+            result += s[i];
+    }
+    if(result.empty())
+        return string(1, result[0]).append(string(result.length()-1, char(result[0])));
+    return result;
+}
