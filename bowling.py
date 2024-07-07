4c706daf71aedb201c5bf2c8595040f9eb4dc9a3
beep boop <githubbot@vadim.me>
2024-07-07 14:33:09 +0200

    Solution 31 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/zp5pwy07 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b36e4e10-e50d-40f7-a21e-cb2bbb6b4 6e8.py", line 1     ```     ^ SyntaxError: invalid syntax

--- bowling.py
+++ bowling.py
@@ -1,16 +1,13 @@
 def bowling_score(frames):
     score = 0
-    frames_list = frames.split(' ')
-    for i in range(10):
-        if '/' in frames_list[i]:
-            split_frame = list(map(int, frames_list[i].split('/')))
+    frame_list = frames.split()
+    for i in range(0, len(frame_list), 2):
+        if '/' in frame_list[i]:
+            split_frame = list(map(int, frame_list[i].split('/')))
             if sum(split_frame) == 10:
                 score += 10 + sum(split_frame)
             else:
-                score += sum(split_frame)
+                score += 10 + min(split_frame)
         else:
-            score += int(frames_list[i])
+            score += sum(map(int, (frame_list[i], frame_list[i+1])))
     return score
-
-frames = "5/5 3/- 1/2 7/3 5/- 4/- 8/1 0/6 2/- 3/-"
-print(bowling_score(frames))
