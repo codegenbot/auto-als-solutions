c9b6f9b17586f0ee2d0a04992630a104932641a3
beep boop <githubbot@vadim.me>
2024-07-07 03:41:43 +0200

    Solution 75 of CPP/160, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/7o99s3y4 Prompt:    49 |             int b = st.top();       |                     ^~       |                     s 3d27a58c-323c-4e17-b737-045e39d52d8b.cpp:55:21: error: ‘st’ was not declared in this scope; did you mean ‘s’?    55 |             int b = st.top();       |                     ^~       |                     s 3d27a58c-323c-4e17-b737-045e39d52d8b.cpp:61:21: error: ‘st’ was not declared in this scope; did you mean ‘s’?    61 |             int b = st.top();       |                     ^~       |                     s 3d27a58c-323c-4e17-b737-045e39d52d8b.cpp:67:21: error: ‘st’ was not declared in this scope; did you mean ‘s’?    67 |             int b = st.top();       |                     ^~       |                     s 3d27a58c-323c-4e17-b737-045e39d52d8b.cpp:75:12: error: ‘st’ was not declared in this scope; did you mean ‘s’?    75 |     return st.top();       |            ^~       |            s

--- CPP_160.cpp
+++ CPP_160.cpp
@@ -1,3 +1,4 @@
+```
 int do_algebra(vector<string> operato, vector<int> operand) {
     int result = 0;
     int i = 0;
@@ -28,7 +29,6 @@ int do_algebra(vector<string> operato, vector<int> operand) {
 
 int evaluate_expression(string s) {
     stack<int> st;
-    int res = 0;
     for (int i = 0; i < s.length(); i++) {
         if (isdigit(s[i])) {
             int num = 0;
