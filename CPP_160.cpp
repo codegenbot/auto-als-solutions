37a6a901c7a8deb8582e457b2bc96b2073c9fb5e
beep boop <githubbot@vadim.me>
2024-07-07 03:48:05 +0200

    Solution 90 of CPP/160, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/7o99s3y4 Prompt:       |             ^~~~~~~~~~ 519cdace-7ccf-40a9-88de-454237368c76.cpp:102:27: error: ‘valueStack’ was not dec lared in this scope   102 |             long long b = valueStack.top();       |                           ^~~~~~~~~~ 519cdace-7ccf-40a9-88de-454237368c76.cpp:119:32: error: ‘LONG_LONG_MAX’ was not declared in this scope   119 |                         return LONG_LONG_MAX; // invalid operation       |                                ^~~~~~~~~~~~~ 519cdace-7ccf-40a9-88de-454237368c76.cpp:123:25: error: ‘pow’ was not declared i n this scope   123 |                     v = pow(a, b);       |                         ^~~ 519cdace-7ccf-40a9-88de-454237368c76.cpp:132:12: error: ‘valueStack’ was not dec lared in this scope   132 |     return valueStack.top();       |            ^~~~~~~~~~ In file included from 519cdace-7ccf-40a9-88de-454237368c76.cpp:135: 519cdace-7ccf-40a9-88de-454237368c76.cpp: In function ‘int main()’: 519cdace-7ccf-40a9-88de-454237368c76.cpp:137:23: error: ‘do_algebra’ cannot be u sed as a function   137 |     assert (do_algebra({"//", "*"}, {7, 3, 4}) == 8);       |             ~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_160.cpp
+++ CPP_160.cpp
@@ -1,69 +1,148 @@
-```
-int do_algebra(vector<string> operato, vector<int> operand) {
-    int result = 0;
-    int i = 0;
-    string temp = "";
-    
-    for (i = 0; i < operato.size(); i++) {
-        temp += operato[i];
-        if (i < operato.size() - 1) {
-            // Add the current operand to the expression
-            temp += to_string(operand[i]) + " ";
-        }
+```c++
+#include <stack>
+#include <limits>
+
+using namespace std;
+
+long long LONG_LONG_MAX = numeric_limits<long long>::max();
+
+double pow(long long a, long long b) {
+    double result = 1;
+    for (int i = 0; i < b; i++) {
+        result *= a;
     }
-    
-    int j = 0;
-    for (j = 0; j < operand.size(); j++) {
-        if (j == operand.size() - 1) {
-            // Add the last operand to the expression
-            temp += to_string(operand[j]);
-        } else {
-            // Add the current operand to the expression
-            temp += to_string(operand[j]) + " ";
-        }
-    }
-    
-    int res = evaluate_expression(temp);
-    return res;
+    return result;
 }
 
-int evaluate_expression(string s) {
-    int res = 0;
-    stack<int> s;
-    for (int i = 0; i < s.length(); i++) {
-        if (isdigit(s[i])) {
-            int num = 0;
-            while (i < s.length() && isdigit(s[i])) {
-                num = num * 10 + (s[i] - '0');
-                i++;
+int do_algebra(vector<string> operato, vector<int> operand) {
+    string expression = "";
+    for (int i = 0; i < operato.size(); i++) {
+        expression += to_string(operand[i]);
+        expression += operato[i];
+    }
+    expression += to_string(operand.back());
+    
+    int result = eval(expression.c_str());
+    
+    return result;
+}
+
+long long eval(const char* pExpr)
+{
+    char buff[256];
+    const char* pToken;
+    long long v;
+
+    // Create a new stack
+    stack<long long> valueStack;
+
+    // Process the input string one token at a time:
+    while (*pExpr) {
+        // Find the next token that begins with * or is a valid number
+        pToken = pExpr;
+        if (strncasecmp(pToken, "pow(", 4) == 0)
+            pToken += 4;
+        else if (*pToken == '(') {
+            int count = 1;
+            while (++pToken, *pToken != ')') {
+                if (*pToken == '(')
+                    ++count;
             }
-            s.push(num);
-        } else if (s[i] == '+') {
-            int b = s.top();
-            s.pop();
-            int a = s.top();
-            s.pop();
-            s.push(a + b);
-        } else if (s[i] == '-') {
-            int b = s.top();
-            s.pop();
-            int a = s.top();
-            s.pop();
-            s.push(a - b);
-        } else if (s[i] == '*') {
-            int b = s.top();
-            s.pop();
-            int a = s.top();
-            s.pop();
-            s.push(a * b);
-        } else if (s[i] == '/') {
-            int b = s.top();
-            s.pop();
-            int a = s.top();
-            s.pop();
-            s.push(a / b);
+            pToken++;
+            // Now process the expression inside the parentheses
+            v = eval(pToken);
+            for (int i = 0; i < count; i++)
+                valueStack.push(v);
+        } else if (*pToken == '-') {
+            v = -1;
+            while (*++pToken != ' ') {
+                if (*pToken >= '0' && *pToken <= '9')
+                    v = v * 10 + (long long)(*pToken - '0');
+                else
+                    break;
+            }
+        } else if (*pToken == '+') {
+            v = 1;
+            while (*++pToken != ' ') {
+                if (*pToken >= '0' && *pToken <= '9')
+                    v = v * 10 + (long long)(*pToken - '0');
+                else
+                    break;
+            }
+        } else if (*pToken == '.') {
+            v = 0;
+            while (*++pToken != ' ') {
+                if (*pToken >= '0' && *pToken <= '9')
+                    v = v * 10 + (long long)(*pToken - '0');
+                else
+                    break;
+            }
+        } else if (*pToken == 'x') {
+            v = 1;
+            while (*++pToken != ' ') {
+                if (*pToken >= '0' && *pToken <= '9')
+                    v = v * 10 + (long long)(*pToken - '0');
+                else
+                    break;
+            }
+        } else {
+            pToken++;
+            // Skip any spaces or parentheses:
+            while (*pToken == ' ')
+                ++pToken;
+            if (*pToken == '(')
+                pToken++;
+            v = 0;
+            while (*pToken >= '0' && *pToken <= '9') {
+                v = v * 10 + (long long)(*pToken - '0');
+                ++pToken;
+            }
+        }
+
+        // Now process the token:
+        if (*pToken == ')')
+            pToken++;
+        else
+            while (*pToken != ' ')
+                ++pToken;
+
+        // If this is a value, push it onto the stack:
+        if (v > 0)
+            valueStack.push(v);
+
+        // If this is an operator, pop two operands and apply the operation:
+        if (*pToken == '+' || *pToken == '-' ||
+            *pToken == '*' || *pToken == '/' ||
+            *pToken == '^') {
+            int op = *pToken;
+            long long b = valueStack.top();
+            valueStack.pop();
+            long long a = valueStack.top();
+            valueStack.pop();
+
+            switch (op) {
+                case '+':
+                    v = a + b;
+                    break;
+                case '-':
+                    v = a - b;
+                    break;
+                case '*':
+                    v = a * b;
+                    break;
+                case '/':
+                    if (b == 0)
+                        return LONG_LONG_MAX;
+                    v = a / b;
+                    break;
+                case '^':
+                    v = pow(a, b);
+                    break;
+            }
+            valueStack.push(v);
         }
     }
-    
-    return s.top();
+
+    // Return the final result
+    return valueStack.top();
 }
