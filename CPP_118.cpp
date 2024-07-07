12d7221cf206d32c82f10c81053353b528e5fef7
beep boop <githubbot@vadim.me>
2024-07-07 02:56:48 +0200

    Solution 46 of CPP/118, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/o5kxl6gq Prompt: d3d12525-3425-4337-a238-d44637b42942.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string get_closest_vowel(string word){       | ^~~~~~ d3d12525-3425-4337-a238-d44637b42942.cpp: In function ‘bool isvowel(char)’: d3d12525-3425-4337-a238-d44637b42942.cpp:16:9: error: ‘tolower’ was not declared  in this scope    16 |     c = tolower(c);       |         ^~~~~~~ In file included from d3d12525-3425-4337-a238-d44637b42942.cpp:20: d3d12525-3425-4337-a238-d44637b42942.cpp: In function ‘int main()’: d3d12525-3425-4337-a238-d44637b42942.cpp:22:13: error: ‘get_closest_vowel’ was n ot declared in this scope    22 |     assert (get_closest_vowel("Above") == "o");       |             ^~~~~~~~~~~~~~~~~

--- CPP_118.cpp
+++ CPP_118.cpp
@@ -1,7 +1,7 @@
 #include <string>
 #include <cctype>
 
-std::string get_closest_vowel(std::string word){
+std::string get_closest_vowel(std::string word) {
     int left = 0, right = word.size() - 1;
     while(left < right){
         if(!isvowel(word[left])){
