0db063b1f9f725e0e40059a714302887c6145a72
beep boop <githubbot@vadim.me>
2024-07-07 03:30:26 +0200

    Solution 44 of CPP/143, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/x3m2x9qi Prompt: ed684153-aed8-4b63-b19f-a509e99c8e26.cpp:4:1: error: ‘string’ does not name a ty pe     4 | string words_in_sentence(string sentence) {       | ^~~~~~ ed684153-aed8-4b63-b19f-a509e99c8e26.cpp:25:1: error: ‘vector’ does not name a t ype    25 | vector<string> split(const string& str, char c) {       | ^~~~~~ In file included from ed684153-aed8-4b63-b19f-a509e99c8e26.cpp:36: ed684153-aed8-4b63-b19f-a509e99c8e26.cpp: In function ‘int main()’: ed684153-aed8-4b63-b19f-a509e99c8e26.cpp:38:13: error: ‘words_in_sentence’ was n ot declared in this scope    38 |     assert (words_in_sentence("here is") == "is");       |             ^~~~~~~~~~~~~~~~~

--- CPP_143.cpp
+++ CPP_143.cpp
@@ -1,7 +1,8 @@
-using namespace std;
-
 #include <vector>
 #include <algorithm>
+#include <string>
+
+using namespace std;
 
 string words_in_sentence(string sentence) {
     vector<string> words = split(sentence, ' ');
@@ -33,3 +34,4 @@ vector<string> split(const string& str, char c) {
     }
     tokens.push_back(str);
     return tokens;
+}
