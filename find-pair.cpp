94990fb5ee978a88c3cd900b1aa16cf9a3b3ef2d
beep boop <githubbot@vadim.me>
2024-07-07 22:06:56 +0200

    Solution 75 of find-pair, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/hvrce79t Prompt: this scope    18 |     cin >> n;       |     ^~~ 2cacd913-031c-4b1c-b342-4f0c86ef3d78.cpp:2:1: note: ‘std::cin’ is defined in hea der ‘<iostream>’; did you forget to ‘#include <iostream>’?     1 | #include <vector>   +++ |+#include <iostream>     2 | using namespace std; 2cacd913-031c-4b1c-b342-4f0c86ef3d78.cpp:26:5: error: ‘cout’ was not declared in  this scope    26 |     cout << result.first << " " << result.second << endl;       |     ^~~~ 2cacd913-031c-4b1c-b342-4f0c86ef3d78.cpp:26:5: note: ‘std::cout’ is defined in h eader ‘<iostream>’; did you forget to ‘#include <iostream>’? 2cacd913-031c-4b1c-b342-4f0c86ef3d78.cpp:26:53: error: ‘endl’ was not declared i n this scope    26 |     cout << result.first << " " << result.second << endl;       |                                                     ^~~~ 2cacd913-031c-4b1c-b342-4f0c86ef3d78.cpp:2:1: note: ‘std::endl’ is defined in he ader ‘<ostream>’; did you forget to ‘#include <ostream>’?     1 | #include <vector>   +++ |+#include <ostream>     2 | using namespace std;

--- find-pair.cpp
+++ find-pair.cpp
@@ -1,5 +1,4 @@
-```c++
-#include <iostream> 
+#include <iostream>
 #include <vector>
 using namespace std;
 
@@ -12,7 +11,7 @@ pair<int, int> findPair(vector<int>& nums, int target) {
         }
         numMap[nums[i]] = i;
     }
-    return {-1, -1}; 
+    return {-1, -1};
 }
 
 int main() {
