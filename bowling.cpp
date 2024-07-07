e671e47602918e9391801831bada2b6c9bb99744
beep boop <githubbot@vadim.me>
2024-07-07 15:29:46 +0200

    Solution 41 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/h176ktrr Prompt: ype     1 | int bowlingScore(const string& input) {       |                        ^~~~~~ 5303f25c-9136-4109-9748-4de6fadeb8f7.cpp: In function ‘int bowlingScore(const in t&)’: 5303f25c-9136-4109-9748-4de6fadeb8f7.cpp:5:19: error: ‘begin’ was not declared i n this scope     5 |     for (char c : input) {       |                   ^~~~~ 5303f25c-9136-4109-9748-4de6fadeb8f7.cpp:5:19: error: ‘end’ was not declared in this scope 5303f25c-9136-4109-9748-4de6fadeb8f7.cpp:7:22: error: ‘min’ was not declared in this scope     7 |             score += min(10, prevRolls);       |                      ^~~ 5303f25c-9136-4109-9748-4de6fadeb8f7.cpp:9:20: error: ‘isdigit’ was not declared  in this scope     9 |         } else if (isdigit(c)) {       |                    ^~~~~~~ 5303f25c-9136-4109-9748-4de6fadeb8f7.cpp:19:18: error: ‘min’ was not declared in  this scope    19 |         score += min(10, prevRolls);       |                  ^~~

--- bowling.cpp
+++ bowling.cpp
@@ -12,7 +12,7 @@ int bowlingScore(const string& input) {
         } else if (::isdigit(c)) {
             prevRolls *= 10;
             prevRolls += c - '0';
-        } else { 
+        } else { // X
             score += 10;
             prevRolls = 0;
         }
