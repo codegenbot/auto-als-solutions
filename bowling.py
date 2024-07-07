9d8c4de60feb3db2eb3fe9e8b63bfa9236e6ef27
beep boop <githubbot@vadim.me>
2024-07-07 14:09:04 +0200

    Solution 14 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/zp5pwy07 Prompt: Given a string representing the individual bowls in a 10-frame round of 10 pin bowling, return the score of that round.

--- bowling.py
+++ bowling.py
@@ -1,16 +1,25 @@
-def bowling_game(bowling_score):
+```
+def bowling_score(frames):
     score = 0
-    for i in range(0, len(bowling_score), 2):
-        if bowling_score[i] == "X":
-            score += 30
-        elif bowling_score[i].isdigit() and len(str(int(bowling_score[i]))) == 1:
-            if (
-                bowling_score[i + 1].isdigit()
-                and len(str(int(bowling_score[i + 1]))) == 1
-            ):
-                score += int(bowling_score[i]) + int(bowling_score[i + 1])
-            else:
-                score += int(bowling_score[i])
-        elif bowling_score[i].isdigit() and len(str(int(bowling_score[i]))) > 1:
-            score += int(bowling_score[i : i + 2])
+    roll_count = [0] * 10
+    
+    for frame in frames.split('/'):
+        if len(frame) > 1:
+            rolls = list(map(int, frame))
+            roll_count[9 - int(''.join(map(str, rolls)))] += 2
+            if rolls[0] == 10 and len(rolls) == 2:
+                roll_count[9 - '10'[0]] += 1
+        else:
+            score += int(frame)
+    
+    for i in range(9):
+        if roll_count[i] > 1:
+            score += 20
+        elif roll_count[i] == 1:
+            score += 10
+        if i < 8 and (roll_count[i] > 0 and roll_count[i+1] > 0):
+            if roll_count[i] == 2 and int(frames.split('/')[i].split('X')[0]) + 10 == int(frames.split('/')[i+1]):
+                score += 30
+            elif sum(map(int, frames.split('/')[i].split('-')[:2])) == 10:
+                score += 20
     return score
