78421a381b9753898d47494c16696a2a799c92c8
beep boop <githubbot@vadim.me>
2024-07-07 03:07:09 +0200

    Solution 39 of Python/17, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/nqzqbtai Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/70db5a27-cee3-4ebe-8539-c3e03c810 378.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_17.py
+++ Python_17.py
@@ -1,15 +1,19 @@
 def parse_music(music_string: str) -> list:
     music_list = []
-    notes = {'o': 4, 'o|': 2, '.||': 1}
     i = 0
     while i < len(music_string):
-        if music_string[i:i+2] in notes:
-            if music_string[i-1] == '.' and music_string[i:i+2] == 'o||':
+        if music_string[i:i+2] == 'o|':
+            if music_string[i-1] == '.':
                 music_list.append(8)
             else:
-                music_list.append(notes[music_string[i:i+2]])
+                music_list.append(2)
             i += 2
-        elif music_string[i] == 'o' and music_string[i+1] in ['o', '|']:
+        elif music_string[i] == 'o':
+            if music_string[i+1] in ['o', '|']:
+                continue
             music_list.append(4)
             i += 1
+        elif music_string[i:i+3] == '.||':
+            music_list.append(1)
+            i += 3
     return music_list
