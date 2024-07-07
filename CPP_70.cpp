d3a6c50da051f3be4f1b7ec3920cb6f0eb107d08
beep boop <githubbot@vadim.me>
2024-07-07 02:10:24 +0200

    Solution 53 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/bx78xaav Prompt: eed54535-f386-4aed-b4d2-2d4781a990d7.cpp:62:5: error: redefinition of ‘int main( )’    62 | int main(){       |     ^~~~ eed54535-f386-4aed-b4d2-2d4781a990d7.cpp:23:5: note: ‘int main()’ previously def ined here    23 | int main() {       |     ^~~~ In file included from eed54535-f386-4aed-b4d2-2d4781a990d7.cpp:53: eed54535-f386-4aed-b4d2-2d4781a990d7.cpp: In function ‘int main()’: eed54535-f386-4aed-b4d2-2d4781a990d7.cpp:63:20: error: ‘strange_sort_list’ was n ot declared in this scope; did you mean ‘strange_sort_vector’?    63 |     assert (issame(strange_sort_list({111111}) , {111111}));       |                    ^~~~~~~~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -34,7 +34,6 @@ int main() {
     vector<int> result1 = strange_sort_vector(v1);
     vector<int> result2 = strange_sort_vector(v2);
     vector<int> result3 = strange_sort_vector(result1);
-    vector<int> result4 = strange_sort_vector(v4);
 
     cout << "Strange sorted Vector: " << endl;
     for (int x : result1) {
@@ -44,6 +43,12 @@ int main() {
 
     cout << "Strange sorted Vector: " << endl;
     for (int x : result2) {
+        cout << x << " ";
+    }
+    cout << endl;
+
+    cout << "Strange sorted Vector: " << endl;
+    for (int x : result3) {
         cout << x << " ";
     }
     cout << endl;
