07a23f84f6f1b29bd8cfb58775ef609a53d125e5
beep boop <githubbot@vadim.me>
2024-07-08 09:21:56 +0200

    Solution 10 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/gugttc1g Prompt: d99764b0-b041-408d-9fdf-b2695d2f2566.cpp:3:18: error: ‘string’ was not declared in this scope     3 | int bowlingScore(string s) {       |                  ^~~~~~ d99764b0-b041-408d-9fdf-b2695d2f2566.cpp:3:18: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from d99764b0-b041-408d-9fdf-b2695d2f2566.cpp:1: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from d99764b0-b041-408d-9fdf-b2695d2f2566.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~

--- bowling.cpp
+++ bowling.cpp
@@ -1,37 +1,22 @@
-int bowlingScore(std::string s) {
+```
+#include <string>
+
+int bowlingScore(string s) {
     int score = 0;
-    bool inFrame = true;
-    int currentRolls = 0;
-    int currentFrameScore = 0;
-
+    int currentRoll = 0;
     for (char c : s) {
-        if (c == 'X') {
-            score += 10 + (inFrame ? 10 : 20);
-            inFrame = false;
-            currentRolls++;
-            currentFrameScore = 0;
-        } else if (c == '/') {
-            int firstRoll = currentRolls % 2 == 0 ? 10 - std::stoi(std::string(1, c)) : std::stoi(std::string(1, c));
-            score += firstRoll;
-            inFrame = false;
-            currentRolls++;
-            currentFrameScore = 0;
-        } else if (c >= '0' && c <= '9') {
-            int roll = std::stoi(std::string(1, c));
-            currentRolls++;
-            currentFrameScore += roll;
-
-            if (currentRolls == 2) {
-                inFrame = true;
-                if (currentFrameScore < 10) {
-                    score += currentFrameScore;
-                } else if (currentFrameScore == 10) {
-                    score += 10;
-                }
-                currentFrameScore = 0;
+        if (c == '/') {
+            if (currentRoll < 2) {
+                score += 10 - (10 - currentRoll);
             }
+            currentRoll = 0;
+        } else if (c == 'X') {
+            score += 10;
+            currentRoll = 0;
+        } else {
+            currentRoll++;
+            score += c - '0';
         }
     }
-
     return score;
 }
