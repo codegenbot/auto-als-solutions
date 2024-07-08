02972221e6452f61defe9703f24ea5e1ac148e29
beep boop <githubbot@vadim.me>
2024-07-08 13:32:21 +0200

    Solution 24 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/wc42nvz7 Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from 584a7ef4-8ca8-4ac5-9ac7-0c367e3fcc58.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,28 +1,55 @@
+#include <vector>
+#include <iostream>
+#include <climits>
+using namespace std;
+
 pair<vector<int>, vector<int>> cutVector(vector<int> v) {
     int minDiff = INT_MAX;
     int cutIndex = -1;
-
+    
     for (int i = 0; i < v.size() - 1; i++) {
         int sumLeft = 0, sumRight = 0;
-
+        
         for (int j = 0; j <= i; j++) {
             sumLeft += v[j];
         }
-
+        
         for (int j = i + 1; j < v.size(); j++) {
             sumRight += v[j];
         }
-
+        
         int diff = abs(sumLeft - sumRight);
-
+        
         if (diff < minDiff) {
             minDiff = diff;
             cutIndex = i;
         }
     }
-
+    
     vector<int> left(v.begin(), v.begin() + cutIndex + 1);
     vector<int> right(v.begin() + cutIndex, v.end());
+    
+    return {left, right};
+}
 
-    return {vector<int>(left), vector<int>(right)};
+int main() {
+    int n; cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++) cin >> v[i];
+    
+    pair<vector<int>, vector<int>> result = cutVector(v);
+    
+    cout << "[";
+    for (int num : result.first) {
+        cout << num << " ";
+    }
+    cout << "]" << endl;
+    
+    cout << "[";
+    for (int num : result.second) {
+        cout << num << " ";
+    }
+    cout << "0]" << endl;
+    
+    return 0;
 }
