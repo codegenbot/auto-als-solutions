12305a6b8c69f67cbbdcd4056e219988121577bf
beep boop <githubbot@vadim.me>
2024-07-08 09:50:31 +0200

    Solution 18 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/gugttc1g Prompt: bfb8928f-e02d-43f8-9613-cc89e49cf4b5.cpp:1:1: error: stray ‘`’ in program     1 | ```       | ^ bfb8928f-e02d-43f8-9613-cc89e49cf4b5.cpp:1:2: error: stray ‘`’ in program     1 | ```       |  ^ bfb8928f-e02d-43f8-9613-cc89e49cf4b5.cpp:1:3: error: stray ‘`’ in program     1 | ```       |   ^ bfb8928f-e02d-43f8-9613-cc89e49cf4b5.cpp:4:18: error: ‘string’ was not declared in this scope     4 | int bowlingScore(string s) {       |                  ^~~~~~ bfb8928f-e02d-43f8-9613-cc89e49cf4b5.cpp:4:18: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from bfb8928f-e02d-43f8-9613-cc89e49cf4b5.cpp:2: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from bfb8928f-e02d-43f8-9613-cc89e49cf4b5.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~

--- bowling.cpp
+++ bowling.cpp
@@ -1,11 +1,9 @@
-```c++
 #include <string>
-
-using namespace std;
 
 int bowlingScore(std::string s) {
     int score = 0;
     int currentRoll = 0;
+    bool spareOrStrike = false;
     for (char c : s) {
         if (c == '/') {
             if (currentRoll < 2) {
@@ -15,10 +13,16 @@ int bowlingScore(std::string s) {
         } else if (c == 'X') {
             score += 10;
             currentRoll = 0;
+            spareOrStrike = true;
         } else {
             currentRoll++;
-            score += c - '0';
+            if (c == '0' || c == '1' || c == '2' || c == '3' || c == '4' || c == '5' || c == '6' || c == '7' || c == '8' || c == '9') {
+                score += c - '0';
+            } else if (c == '+' && spareOrStrike) {
+                score += 10;
+                currentRoll = 0;
+                spareOrStrike = false;
+            }
         }
     }
     return score;
-}
