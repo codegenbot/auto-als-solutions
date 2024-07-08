292d7e0612b728614068e657c85521191cc4bd5c
beep boop <githubbot@vadim.me>
2024-07-08 15:14:51 +0200

    Solution 50 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/wc42nvz7 Prompt: In file included from 7f29d1fa-047a-4a88-a15a-ce62df69da94.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 7f29d1fa-047a-4a88-a15a-ce62df69da94.cpp:3:21: error: wrong number of template a rguments (1, should be 2)     3 | std::pair<vector<int>, vector<int>> cutVector(vector<int> v) {       |                     ^ In file included from /usr/include/c++/11/bits/stl_algobase.h:64,                  from /usr/include/c++/11/vector:60,                  from 7f29d1fa-047a-4a88-a15a-ce62df69da94.cpp:1: /usr/include/c++/11/bits/stl_pair.h:211:12: note: provided for ‘template<class _ T1, class _T2> struct std::pair’   211 |     struct pair       |            ^~~~ 7f29d1fa-047a-4a88-a15a-ce62df69da94.cpp:3:22: error: expected unqualified-id be fore ‘,’ token     3 | std::pair<vector<int>, vector<int>> cutVector(vector<int> v) {       |                      ^ 7f29d1fa-047a-4a88-a15a-ce62df69da94.cpp:3:30: error: expected initializer befor e ‘<’ token     3 | std::pair<vector<int>, vector<int>> cutVector(vector<int> v) {       |                              ^

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,31 +1,32 @@
 #include <vector>
 #include <utility>
-#include <climits>
-#include <cmath>
 
 std::pair<std::vector<int>, std::vector<int>> cutVector(std::vector<int> v) {
     int minDiff = INT_MAX;
     int cutIndex = -1;
-    
+
     for (int i = 0; i < v.size() - 1; i++) {
         int sumLeft = 0, sumRight = 0;
-        
+
         for (int j = 0; j <= i; j++) {
             sumLeft += v[j];
         }
-        
+
         for (int j = i + 1; j < v.size(); j++) {
             sumRight += v[j];
         }
-        
-        if (abs(sumLeft - sumRight) < minDiff || abs(sumLeft - sumRight) == 0) {
+
+        if (abs(sumLeft - sumRight) < minDiff) {
+            minDiff = abs(sumLeft - sumRight);
+            cutIndex = i;
+        } else if (abs(sumLeft - sumRight) == 0 && cutIndex == -1) {
             minDiff = abs(sumLeft - sumRight);
             cutIndex = i;
         }
     }
-    
+
     std::vector<int> left(v.begin(), v.begin() + cutIndex + 1);
     std::vector<int> right(v.begin() + cutIndex, v.end());
-    
+
     return {left, right};
 }
