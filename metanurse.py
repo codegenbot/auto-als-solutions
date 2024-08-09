2e30d41527ff01be64220ea8b56b6717d643f1ce
beep boop <githubbot@vadim.me>
2024-08-09 18:25:38 +0200

    57 (0.0). One of the ED nurses has called you to see a patient that she is worried about.
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    What's more, if John's sats fall below 65% or if his mean arterial pressure falls below 20mmHg, he will go into cardiac arrest.
    
    Good luck! 
    DoNothing
    Finish
    Thanks for playing! Here's how you did:
    
    Sadly, you didn't manage to stabilise John on this occasion.  He is hypotensive.  He has an unstable tachyarrhythmia, which is an indication for cardioversion. 
    
    Don't worry if you're not perfect yet, though. That's exactly what we're here for! 
    
    Final reward: 0.0

--- metanurse.py
+++ metanurse.py
@@ -1,5 +1,4 @@
 import sys
-import math
 
 
 def parse_input():
@@ -20,23 +19,17 @@ def main():
             return
 
         # ABCDE Assessment and Stabilization
-        if any(event_relevances[i] == 0 for i in [3, 4, 5]):  # Airway issues
+        if any(event_relevances[i] > 0 for i in [3, 4, 5]):  # Airway issues
             print(3)  # ExamineAirway
-        elif any(event_relevances[i] == 0 for i in [6, 7, 8]):  # Breathing issues
+        elif any(event_relevances[i] > 0 for i in [6, 7, 8]) or (
+            vital_signs_relevances[6] > 0 and vital_signs_measurements[6] < 8
+        ):  # Breathing issues
             print(4)  # ExamineBreathing
-        elif (
-            vital_signs_relevances[4] == 0 or vital_signs_measurements[4] < 60
-        ):  # Circulation issues (MAP)
+        elif (vital_signs_relevances[4] > 0 and vital_signs_measurements[4] < 60) or (
+            vital_signs_relevances[5] > 0 and vital_signs_measurements[5] < 88
+        ):  # Circulation issues
             print(5)  # ExamineCirculation
-        elif (
-            vital_signs_relevances[5] == 0 or vital_signs_measurements[5] < 88
-        ):  # Circulation issues (Sats)
-            print(5)  # ExamineCirculation
-        elif (
-            vital_signs_relevances[6] == 0 or vital_signs_measurements[6] < 8
-        ):  # Breathing issues (Resps)
-            print(4)  # ExamineBreathing
-        elif any(event_relevances[i] == 0 for i in range(3)):  # Response issues
+        elif any(event_relevances[i] > 0 for i in range(3)):  # Response issues
             print(8)  # ExamineResponse
         else:
             # Check for stabilization and take appropriate actions
