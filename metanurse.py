c296b8f5e63dc1b65f201ae53bb54e3148c630a4
beep boop <githubbot@vadim.me>
2024-08-10 23:36:43 +0200

    1553 (0.0). One of the ED nurses has called you to see a patient that she is worried about.
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    What's more, if John's sats fall below 65% or if his mean arterial pressure falls below 20mmHg, he will go into cardiac arrest.
    
    Good luck! 
    DoNothing
    Finish
    Thanks for playing! Here's how you did:
    
    Sadly, you didn't manage to stabilise John on this occasion.  He is hypotensive.  He has an unstable tachyarrhythmia, which is an indication for cardioversion. 
    
    Don't worry if you're not perfect yet, though. That's exactly what we're here for! 
    
    Final reward: 0.0

--- metanurse.py
+++ metanurse.py
@@ -14,32 +14,38 @@ def main():
         vital_signs_measurements = observations[40:]
 
         if vital_signs_measurements[5] < 65 or vital_signs_measurements[4] < 20:
-            print(48)
+            print(48)  # Finish due to cardiac arrest
             return
 
-        if vital_signs_relevances[4] > 0 and vital_signs_measurements[4] < 60:
-            print(20)
-            continue
-
-        if vital_signs_relevances[5] > 0 and vital_signs_measurements[5] < 88:
-            print(30)
-            continue
-
-        if vital_signs_relevances[6] > 0 and vital_signs_measurements[6] < 8:
-            print(29)
-            continue
-
+        # A - Airway
         if not any(event_relevances[i] > 0 for i in [3, 4, 5]):
-            print(36)
-        elif not any(event_relevances[i] > 0 for i in range(3)):
-            print(8)
-        elif not any(event_relevances[i] > 0 for i in range(26, 33)):
-            print(7)
-        else:
-            if all(vital_signs_measurements[i] >= [60, 88, 8][i] for i in range(3)):
-                print(48)
-            else:
-                print(0)
+            print(36)  # PerformHeadTiltChinLift
+            continue
+
+        # B - Breathing
+        if vital_signs_relevances[6] > 0 and vital_signs_measurements[6] < 8:
+            print(29)  # UseBagValveMask
+            continue
+
+        # C - Circulation
+        if vital_signs_relevances[4] > 0 and vital_signs_measurements[4] < 60:
+            print(20)  # OpenCirculationDrawer
+            continue
+
+        # D - Disability
+        if not any(event_relevances[i] > 0 for i in range(26, 33)):
+            print(7)  # ExamineExposure
+            continue
+
+        # E - Exposure
+        if not any(event_relevances[i] > 0 for i in range(3)):
+            print(8)  # ExamineResponse
+            continue
+
+        # Check if stabilized
+        if all(vital_signs_measurements[i] >= [60, 88, 8][i] for i in range(3)):
+            print(48)  # Finish
+            return
 
         steps += 1
 
