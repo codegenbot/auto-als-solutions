4c9abdac183dfd4e0afe47d15328cabe659cf60b
beep boop <githubbot@vadim.me>
2024-08-10 14:28:40 +0200

    1195 (0.0). One of the ED nurses has called you to see a patient that she is worried about.
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    What's more, if John's sats fall below 65% or if his mean arterial pressure falls below 20mmHg, he will go into cardiac arrest.
    
    Good luck! 
    DoNothing
    Finish
    Thanks for playing! Here's how you did:
    
    Sadly, you didn't manage to stabilise John on this occasion.  He is hypotensive.  He has an unstable tachyarrhythmia, which is an indication for cardioversion. 
    
    Don't worry if you're not perfect yet, though. That's exactly what we're here for! 
    
    Final reward: 0.0

--- metanurse.py
+++ metanurse.py
@@ -20,17 +20,17 @@ def main():
 
         # ABCDE Assessment and Stabilization
         if any(event_relevances[i] > 0 for i in [3, 4, 5]):  # Airway issues
-            print(3)  # ExamineAirway
+            print(36)  # PerformHeadTiltChinLift or 37 PerformJawThrust
         elif any(event_relevances[i] > 0 for i in [6, 7, 8]) or (
             vital_signs_relevances[6] > 0 and vital_signs_measurements[6] < 8
         ):  # Breathing issues
-            print(4)  # ExamineBreathing
+            print(29)  # UseBagValveMask or 30 UseNonRebreatherMask
         elif (vital_signs_relevances[4] > 0 and vital_signs_measurements[4] < 60) or (
             vital_signs_relevances[5] > 0 and vital_signs_measurements[5] < 88
         ):  # Circulation issues
-            print(5)  # ExamineCirculation
+            print(20)  # OpenCirculationDrawer or 16 ViewMonitor
         elif any(event_relevances[i] > 0 for i in range(3)):  # Response issues
-            print(8)  # ExamineResponse
+            print(8)  # ExamineResponse or 6 ExamineDisability
         elif any(event_relevances[i] > 0 for i in range(26, 33)):  # Exposure issues
             print(7)  # ExamineExposure
         else:
