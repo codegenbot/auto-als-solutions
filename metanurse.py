70bd05dbda8d67359d3038c685d76d0a2293d2f6
beep boop <githubbot@vadim.me>
2024-08-10 18:42:07 +0200

    1355 (0.0). One of the ED nurses has called you to see a patient that she is worried about.
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    What's more, if John's sats fall below 65% or if his mean arterial pressure falls below 20mmHg, he will go into cardiac arrest.
    
    Good luck! 
    DoNothing
    Finish
    Thanks for playing! Here's how you did:
    
    Sadly, you didn't manage to stabilise John on this occasion.  He is hypotensive.  He has an unstable tachyarrhythmia, which is an indication for cardioversion. 
    
    Don't worry if you're not perfect yet, though. That's exactly what we're here for! 
    
    Final reward: 0.0

--- metanurse.py
+++ metanurse.py
@@ -7,10 +7,6 @@ def parse_input():
 
 def main():
     steps = 0
-    checked_airway = (
-        checked_breathing
-    ) = checked_circulation = checked_disability = False
-
     while steps < 350:
         observations = parse_input()
         event_relevances = observations[:33]
@@ -18,56 +14,27 @@ def main():
         vital_signs_measurements = observations[40:]
 
         if vital_signs_measurements[5] < 65 or vital_signs_measurements[4] < 20:
-            print(48)  # Finish due to cardiac arrest
+            print(48)
             return
 
-        # A - Airway
-        if not checked_airway:
-            if event_relevances[3] == 0:  # AirwayClear not observed
-                print(36)  # PerformHeadTiltChinLift
+        if not any(event_relevances[i] > 0 for i in [3, 4, 5]):
+            print(36)
+        elif vital_signs_relevances[6] > 0 and vital_signs_measurements[6] < 8:
+            print(29)
+        elif vital_signs_relevances[4] > 0 and vital_signs_measurements[4] < 60:
+            print(20)
+        elif vital_signs_relevances[5] > 0 and vital_signs_measurements[5] < 88:
+            print(30)
+        elif not any(event_relevances[i] > 0 for i in range(3)):
+            print(8)
+        elif not any(event_relevances[i] > 0 for i in range(26, 33)):
+            print(7)
+        else:
+            if all(vital_signs_measurements[i] >= [60, 88, 8][i] for i in range(3)):
+                print(48)
             else:
-                for i in [4, 5]:  # AirwayVomit, AirwayBlood
-                    if event_relevances[i] > 0:
-                        print(31)  # UseYankeurSucionCatheter
-                        break
-            checked_airway = True
-            steps += 1
-            continue
+                print(0)
 
-        # B - Breathing
-        if not checked_breathing:
-            if vital_signs_relevances[6] > 0 and vital_signs_measurements[6] < 8:
-                print(29)  # UseBagValveMask
-            elif vital_signs_relevances[5] > 0 and vital_signs_measurements[5] < 88:
-                print(30)  # UseNonRebreatherMask
-            checked_breathing = True
-            steps += 1
-            continue
-
-        # C - Circulation
-        if not checked_circulation:
-            if vital_signs_relevances[4] > 0 and vital_signs_measurements[4] < 60:
-                print(20)  # OpenCirculationDrawer
-            checked_circulation = True
-            steps += 1
-            continue
-
-        # D - Disability
-        if not checked_disability:
-            if (
-                vital_signs_relevances[2] > 0 and vital_signs_measurements[2] < 4
-            ):  # Hypoglycemia
-                print(15)  # GiveFluids
-            checked_disability = True
-            steps += 1
-            continue
-
-        # Stabilization Check
-        if all(vital_signs_measurements[i] >= [60, 88, 8][i] for i in range(3)):
-            print(48)  # Finish
-            return
-
-        print(0)  # DoNothing if no action is required
         steps += 1
 
 
