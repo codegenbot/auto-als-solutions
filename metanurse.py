458fb5c02ab71b228e32bbbca008a33d580f5602
beep boop <githubbot@vadim.me>
2024-08-10 16:40:18 +0200

    1291 (0.0). One of the ED nurses has called you to see a patient that she is worried about.
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    What's more, if John's sats fall below 65% or if his mean arterial pressure falls below 20mmHg, he will go into cardiac arrest.
    
    Good luck! 
    DoNothing
    Finish
    Thanks for playing! Here's how you did:
    
    Sadly, you didn't manage to stabilise John on this occasion.  He is hypotensive.  He has an unstable tachyarrhythmia, which is an indication for cardioversion. 
    
    Don't worry if you're not perfect yet, though. That's exactly what we're here for! 
    
    Final reward: 0.0

--- metanurse.py
+++ metanurse.py
@@ -19,31 +19,23 @@ def main():
             return
 
         # ABCDE Assessment and Stabilization
-        if not any(event_relevances[i] > 0 for i in [3, 4, 5]):  # No Airway issues
+        if any(event_relevances[i] > 0 for i in [3, 4, 5]):  # Airway issues
             print(36)  # PerformHeadTiltChinLift or 37 PerformJawThrust
-        elif (
+        elif any(event_relevances[i] > 0 for i in [6, 7, 8]) or (
             vital_signs_relevances[6] > 0 and vital_signs_measurements[6] < 8
         ):  # Breathing issues
-            print(29)  # UseBagValveMask
-        elif (
-            vital_signs_relevances[4] > 0 and vital_signs_measurements[4] < 60
-        ):  # Circulation issues (MAP)
-            print(20)  # OpenCirculationDrawer
-        elif (
+            print(29)  # UseBagValveMask or 30 UseNonRebreatherMask
+        elif (vital_signs_relevances[4] > 0 and vital_signs_measurements[4] < 60) or (
             vital_signs_relevances[5] > 0 and vital_signs_measurements[5] < 88
-        ):  # Circulation issues (Sats)
-            print(30)  # UseNonRebreatherMask
+        ):  # Circulation issues
+            print(20)  # OpenCirculationDrawer or 16 ViewMonitor
         elif any(event_relevances[i] > 0 for i in range(3)):  # Response issues
             print(8)  # ExamineResponse or 6 ExamineDisability
         elif any(event_relevances[i] > 0 for i in range(26, 33)):  # Exposure issues
             print(7)  # ExamineExposure
         else:
             # Check for stabilization and take appropriate actions
-            if all(
-                vital_signs_relevances[i] > 0
-                and vital_signs_measurements[i] >= [60, 88, 8][i]
-                for i in range(3)
-            ):
+            if all(vital_signs_measurements[i] >= [60, 88, 8][i] for i in range(3)):
                 print(48)  # Finish if stabilized
             else:
                 # Prioritize actions based on current status
