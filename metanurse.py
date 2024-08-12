9098d2ae655e1ab1c0ee6b27e1080d561f849370
beep boop <githubbot@vadim.me>
2024-08-13 01:32:17 +0200

    223 (0.0). One of the ED nurses has called you to see a patient that she is worried about.
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    What's more, if John's sats fall below 65% or if his mean arterial pressure falls below 20mmHg, he will go into cardiac arrest.
    
    Good luck! 
    DoNothing
    ExamineAirway
    John is conscious and talking. The airway seems to be fine. 
    AirwayClear
    OpenBreathingDrawer
    UseSatsProbe
    ViewMonitor
    MeasuredSats: 92
    Finish
    Thanks for playing! Here's how you did:
    
    Sadly, you didn't manage to stabilise John on this occasion.  He is hypotensive.  He has an unstable tachyarrhythmia, which is an indication for cardioversion. 
    
    Don't worry if you're not perfect yet, though. That's exactly what we're here for! 
    
    Final reward: 0.0

--- metanurse.py
+++ metanurse.py
@@ -38,7 +38,7 @@ def main():
             )
         }
 
-        # Examine airway if not clear
+        # Examine airway
         if not events[3]:
             print(3)
             continue
@@ -81,23 +81,19 @@ def main():
             if not used_methods["OpenedCirculationDrawer"]:
                 print(20)
                 used_methods["OpenedCirculationDrawer"] = True
-                continue
             elif not used_methods["UsedMonitorPads"]:
                 print(24)
                 used_methods["UsedMonitorPads"] = True
-                continue
             elif not used_methods["UsedBP_Cuff"]:
                 print(27)
                 used_methods["UsedBP_Cuff"] = True
-                continue
             elif not used_methods["UsedA_Line"]:
                 print(26)
                 used_methods["UsedA_Line"] = True
-                continue
             elif not used_methods["GivenFluids"]:
                 print(15)
                 used_methods["GivenFluids"] = True
-                continue
+            continue
 
         # Check heart rate conditions and take appropriate action
         if vitals["HeartRate"]:
@@ -110,16 +106,9 @@ def main():
             elif vitals["HeartRate"] > 150:
                 print(28)
                 continue
-        
-        # If stabilized, finish
-        if (
-            (events[3] > 0) and
-            (vitals["Sats"] and vitals["Sats"] >= 88) and
-            (vitals["RespRate"] and vitals["RespRate"] >= 8) and
-            (vitals["MAP"] and vitals["MAP"] >= 60)
-        ):
-            print(48)
-            return
+
+        # Do nothing to proceed through steps
+        print(0)
 
     print(48)
 
