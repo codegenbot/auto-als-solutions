fa5adf2d979b8d1fd99a4c995ef7cf0173b27323
beep boop <githubbot@vadim.me>
2024-08-12 22:08:00 +0200

    77 (0.0). One of the ED nurses has called you to see a patient that she is worried about.
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    What's more, if John's sats fall below 65% or if his mean arterial pressure falls below 20mmHg, he will go into cardiac arrest.
    
    Good luck! 
    DoNothing
    ExamineAirway
    John is conscious and talking. The airway seems to be fine. 
    AirwayClear
    OpenBreathingDrawer
    UseSatsProbe
    ViewMonitor
    MeasuredSats: 92
    Finish
    Thanks for playing! Here's how you did:
    
    Sadly, you didn't manage to stabilise John on this occasion.  He is hypotensive.  He has an unstable tachyarrhythmia, which is an indication for cardioversion. 
    
    Don't worry if you're not perfect yet, though. That's exactly what we're here for! 
    
    Final reward: 0.0

--- metanurse.py
+++ metanurse.py
@@ -2,7 +2,7 @@ import sys
 
 def main():
     max_steps = 350
-    opened_breathing_drawer = used_pulse_oximeter = viewed_monitor = False
+    opened_breathing_drawer = used_sats_probe = viewed_monitor = used_blood_pressure_cuff = False
     
     for step in range(max_steps):
         observations = list(map(float, input().strip().split()))
@@ -10,6 +10,7 @@ def main():
         vital_signs_times = observations[33:40]
         vital_signs_values = observations[40:]
 
+        # Extract vitals if measured
         sats = vital_signs_values[5] if vital_signs_times[5] > 0 else None
         map_value = vital_signs_values[4] if vital_signs_times[4] > 0 else None
         resp_rate = vital_signs_values[1] if vital_signs_times[1] > 0 else None
@@ -17,48 +18,49 @@ def main():
 
         if events[3]:  # AirwayClear
             if not opened_breathing_drawer:
-                print(19)
+                print(19)  # OpenBreathingDrawer
                 opened_breathing_drawer = True
                 continue
 
-            if not used_pulse_oximeter:
-                print(25)
-                used_pulse_oximeter = True
+            if not used_sats_probe:
+                print(25)  # UseSatsProbe
+                used_sats_probe = True
+                continue
+
+            if not used_blood_pressure_cuff:
+                print(27)  # UseBloodPressureCuff
+                used_blood_pressure_cuff = True
                 continue
 
             if not viewed_monitor:
-                print(16)
+                print(16)  # ViewMonitor
                 viewed_monitor = True
                 continue
 
             if (sats is not None and sats < 65) or (map_value is not None and map_value < 20):
-                print(17)
+                print(17)  # StartChestCompression
                 continue
 
             if sats is not None and sats < 88:
-                print(30)
+                print(30)  # UseNonRebreatherMask
                 continue
 
             if resp_rate is not None and resp_rate < 8:
-                print(29)
+                print(29)  # UseBagValveMask
                 continue
 
             if map_value is not None and map_value < 60:
-                print(15)
+                print(15)  # GiveFluids
                 continue
 
-            if heart_rate is not None:
-                if heart_rate < 50:
-                    print(12)
-                    continue
-                elif heart_rate > 100:
-                    print(9)
-                    continue
+            if heart_rate is not None and heart_rate > 100: 
+                print(24)  # UseMonitorPads
+                continue
 
-            print(48)
+            print(48)  # Finish
             return
 
-        print(3)
+        print(3)  # ExamineAirway
 
 if __name__ == "__main__":
     main()
