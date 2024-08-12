6d36b775a9b17d5fe763f6742d1877a5ccb6f828
beep boop <githubbot@vadim.me>
2024-08-13 00:15:23 +0200

    136 (0.0). One of the ED nurses has called you to see a patient that she is worried about.
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    What's more, if John's sats fall below 65% or if his mean arterial pressure falls below 20mmHg, he will go into cardiac arrest.
    
    Good luck! 
    DoNothing
    ExamineAirway
    John is conscious and talking. The airway seems to be fine. 
    AirwayClear
    OpenBreathingDrawer
    UseSatsProbe
    ViewMonitor
    MeasuredSats: 92
    Finish
    Thanks for playing! Here's how you did:
    
    Sadly, you didn't manage to stabilise John on this occasion.  He is hypotensive.  He has an unstable tachyarrhythmia, which is an indication for cardioversion. 
    
    Don't worry if you're not perfect yet, though. That's exactly what we're here for! 
    
    Final reward: 0.0

--- metanurse.py
+++ metanurse.py
@@ -14,57 +14,59 @@ def main():
                       ["HeartRate", "RespRate", "CapillaryGlucose", "Temperature", "MAP", "Sats", "Resps"])}
         
         if not events[3]:
-            print(3)
+            print(3)  # ExamineAirway
             continue
         
         if not opened_drawers[19]:
-            print(19)
+            print(19)  # OpenBreathingDrawer
             opened_drawers[19] = True
             continue
 
         if not used_methods['UsedSatsProbe']:
-            print(25)
+            print(25)  # UseSatsProbe
             used_methods['UsedSatsProbe'] = True
             continue
 
         if not used_methods['ViewedMonitor']:
-            print(16)
+            print(16)  # ViewMonitor
             used_methods['ViewedMonitor'] = True
             continue
         
         if (vitals["Sats"] and vitals["Sats"] < 65) or (vitals["MAP"] and vitals["MAP"] < 20):
-            print(17)
+            print(17)  # Start chest compressions
             continue
 
         if vitals["Sats"] and vitals["Sats"] < 88:
-            print(30)
+            print(30)  # Use non-rebreather mask
             continue
 
         if vitals["RespRate"] and vitals["RespRate"] < 8:
-            print(29)
+            print(29)  # Use bag valve mask
             continue
 
         if vitals["MAP"] and vitals["MAP"] < 60:
             if not used_methods['BP_Cuff']:
-                print(27)
+                print(27)  # Apply BP cuff
                 used_methods['BP_Cuff'] = True
             elif not used_methods['A_Line']:
-                print(26)
+                print(26)  # Use arterial line
                 used_methods['A_Line'] = True
             elif not used_methods['Fluids']:
-                print(15)
+                print(15)  # Give fluids
                 used_methods['Fluids'] = True
             continue
 
         if vitals["HeartRate"]:
             if vitals["HeartRate"] < 50:
-                print(12)
+                print(12)  # Give Atropine
                 continue
             elif vitals["HeartRate"] > 150:
-                print(2)
+                print(2)  # Check rhythm
+                print(10)  # Give adrenaline - stabilization after checking
                 continue
             elif 100 < vitals["HeartRate"] <= 150:
-                print(2)
+                print(2)  # Check rhythm
+                print(9)  # Give adenosine - stabilization after checking
                 continue
         
         print(48)
