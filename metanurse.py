8ab731db83a88f5eae9546d4b1232b0c0d4ac99b
beep boop <githubbot@vadim.me>
2024-08-12 23:29:05 +0200

    108 (0.0). One of the ED nurses has called you to see a patient that she is worried about.
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    What's more, if John's sats fall below 65% or if his mean arterial pressure falls below 20mmHg, he will go into cardiac arrest.
    
    Good luck! 
    DoNothing
    ExamineAirway
    John is conscious and talking. The airway seems to be fine. 
    AirwayClear
    OpenBreathingDrawer
    UseSatsProbe
    UseBloodPressureCuff
    ViewMonitor
    MeasuredMAP: 36
    MeasuredSats: 92
    Finish
    Thanks for playing! Here's how you did:
    
    Sadly, you didn't manage to stabilise John on this occasion.  He is hypotensive.  He has an unstable tachyarrhythmia, which is an indication for cardioversion. 
    
    Don't worry if you're not perfect yet, though. That's exactly what we're here for! 
    
    Final reward: -1.0

--- metanurse.py
+++ metanurse.py
@@ -1,11 +1,8 @@
 import sys
 
-
 def main():
     max_steps = 350
-    opened_breathing_drawer = (
-        used_sats_probe
-    ) = viewed_monitor = used_blood_pressure_cuff = False
+    opened_breathing_drawer = used_sats_probe = viewed_monitor = used_blood_pressure_cuff = False
 
     for step in range(max_steps):
         observations = list(map(float, input().strip().split()))
@@ -13,6 +10,7 @@ def main():
         vital_signs_times = observations[33:40]
         vital_signs_values = observations[40:]
 
+        # Extract vitals if measured
         sats = vital_signs_values[5] if vital_signs_times[5] > 0 else None
         map_value = vital_signs_values[4] if vital_signs_times[4] > 0 else None
         resp_rate = vital_signs_values[1] if vital_signs_times[1] > 0 else None
@@ -39,9 +37,7 @@ def main():
                 viewed_monitor = True
                 continue
 
-            if (sats is not None and sats < 65) or (
-                map_value is not None and map_value < 20
-            ):
+            if (sats is not None and sats < 65) or (map_value is not None and map_value < 20):
                 print(17)  # StartChestCompression
                 continue
 
@@ -57,12 +53,7 @@ def main():
                 print(15)  # GiveFluids
                 continue
 
-            if (
-                heart_rate is not None
-                and heart_rate > 100
-                and map_value is not None
-                and map_value < 60
-            ):
+            if heart_rate is not None and heart_rate > 100 and map_value < 60:
                 print(24)  # UseMonitorPads (potential start cardioversion)
                 continue
 
@@ -70,7 +61,6 @@ def main():
             return
 
         print(3)  # ExamineAirway
-
 
 if __name__ == "__main__":
     main()
