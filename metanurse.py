2e3e94da713ca984e7c5cd52bbb7ffa19464604a
beep boop <githubbot@vadim.me>
2024-08-13 00:48:38 +0200

    156 (0.0). One of the ED nurses has called you to see a patient that she is worried about.
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    What's more, if John's sats fall below 65% or if his mean arterial pressure falls below 20mmHg, he will go into cardiac arrest.
    
    Good luck! 
    DoNothing
    ExamineAirway
    John is conscious and talking. The airway seems to be fine. 
    AirwayClear
    OpenBreathingDrawer
    UseSatsProbe
    ViewMonitor
    MeasuredSats: 92
    Finish
    Thanks for playing! Here's how you did:
    
    Sadly, you didn't manage to stabilise John on this occasion.  He is hypotensive.  He has an unstable tachyarrhythmia, which is an indication for cardioversion. 
    
    Don't worry if you're not perfect yet, though. That's exactly what we're here for! 
    
    Final reward: 0.0

--- metanurse.py
+++ metanurse.py
@@ -1,116 +1,85 @@
 import sys
 
-
 def main():
     max_steps = 350
     opened_drawers = {19: False, 20: False, 21: False}
-    used_methods = {
-        "UsedSatsProbe": False,
-        "ViewedMonitor": False,
-        "BP_Cuff": False,
-        "A_Line": False,
-        "Fluids": False,
-        "DefibPads": False,
-    }
+    used_methods = {'UsedSatsProbe': False, 'ViewedMonitor': False, 
+                    'BP_Cuff': False, 'A_Line': False, 'Fluids': False,
+                    'DefibPads': False}
     for step in range(max_steps):
         observations = list(map(float, input().strip().split()))
-        events, vital_signs_times, vital_signs_values = (
-            observations[:33],
-            observations[33:40],
-            observations[40:],
-        )
-
-        vitals = {
-            name: value if time > 0 else None
-            for value, time, name in zip(
-                vital_signs_values,
-                vital_signs_times,
-                [
-                    "HeartRate",
-                    "RespRate",
-                    "CapillaryGlucose",
-                    "Temperature",
-                    "MAP",
-                    "Sats",
-                    "Resps",
-                ],
-            )
-        }
-
+        events, vital_signs_times, vital_signs_values = observations[:33], observations[33:40], observations[40:]
+        
+        vitals = {name: value if time > 0 else None for value, time, name in 
+                  zip(vital_signs_values, vital_signs_times, 
+                      ["HeartRate", "RespRate", "CapillaryGlucose", "Temperature", "MAP", "Sats", "Resps"])}
+        
         if not events[3]:
             print(3)
             continue
-
+        
         if not opened_drawers[19]:
             print(19)
             opened_drawers[19] = True
             continue
-
-        if not used_methods["UsedSatsProbe"]:
+        
+        if not used_methods['UsedSatsProbe']:
             print(25)
-            used_methods["UsedSatsProbe"] = True
+            used_methods['UsedSatsProbe'] = True
             continue
-
-        if not used_methods["ViewedMonitor"]:
+        
+        if not used_methods['ViewedMonitor']:
             print(16)
-            used_methods["ViewedMonitor"] = True
+            used_methods['ViewedMonitor'] = True
             continue
-
-        if (vitals["Sats"] and vitals["Sats"] < 65) or (
-            vitals["MAP"] and vitals["MAP"] < 20
-        ):
+        
+        if (vitals["Sats"] and vitals["Sats"] < 65) or (vitals["MAP"] and vitals["MAP"] < 20):
             print(17)
             continue
-
+        
         if vitals["Sats"] and vitals["Sats"] < 88:
             print(30)
             continue
-
+        
         if vitals["RespRate"] and vitals["RespRate"] < 8:
             print(29)
             continue
-
+        
         if vitals["MAP"] and vitals["MAP"] < 60:
-            if not used_methods["BP_Cuff"]:
+            if not used_methods['BP_Cuff']:
                 print(27)
-                used_methods["BP_Cuff"] = True
-            elif not used_methods["A_Line"]:
+                used_methods['BP_Cuff'] = True
+            elif not used_methods['A_Line']:
                 print(26)
-                used_methods["A_Line"] = True
-            elif not used_methods["Fluids"]:
+                used_methods['A_Line'] = True
+            elif not used_methods['Fluids']:
                 print(15)
-                used_methods["Fluids"] = True
+                used_methods['Fluids'] = True
             continue
-
+        
         if vitals["HeartRate"]:
             if vitals["HeartRate"] < 50:
                 print(12)
                 continue
             elif vitals["HeartRate"] > 150:
-                if not used_methods["DefibPads"]:
+                if not used_methods['DefibPads']:
                     print(24)
-                    used_methods["DefibPads"] = True
+                    used_methods['DefibPads'] = True
                 print(2)
                 continue
             elif 100 < vitals["HeartRate"] <= 150:
                 print(2)
                 continue
-
-        if all(
-            vitals[name]
-            and (60 <= vitals["HeartRate"] <= 100)
-            and (88 <= vitals["Sats"])
-            and (8 <= vitals["RespRate"])
-            and (60 <= vitals["MAP"])
-            for name in ["HeartRate", "Sats", "RespRate", "MAP"]
-        ):
+        
+        if all(vitals[name] and (60 <= vitals["HeartRate"] <= 100) and
+               (88 <= vitals["Sats"]) and (8 <= vitals["RespRate"]) and (60 <= vitals["MAP"])
+               for name in ["HeartRate", "Sats", "RespRate", "MAP"]):
             print(48)
             return
-
+        
         print(0)
 
     print(48)
-
 
 if __name__ == "__main__":
     main()
