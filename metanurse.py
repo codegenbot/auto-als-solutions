89c17792d3384185e7d4cd2012272c755007d706
beep boop <githubbot@vadim.me>
2024-08-13 01:05:42 +0200

    188 (0.0). One of the ED nurses has called you to see a patient that she is worried about.
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    What's more, if John's sats fall below 65% or if his mean arterial pressure falls below 20mmHg, he will go into cardiac arrest.
    
    Good luck! 
    DoNothing
    ExamineAirway
    John is conscious and talking. The airway seems to be fine. 
    AirwayClear
    OpenBreathingDrawer
    UseSatsProbe
    ViewMonitor
    MeasuredSats: 92
    Finish
    Thanks for playing! Here's how you did:
    
    Sadly, you didn't manage to stabilise John on this occasion.  He is hypotensive.  He has an unstable tachyarrhythmia, which is an indication for cardioversion. 
    
    Don't worry if you're not perfect yet, though. That's exactly what we're here for! 
    
    Final reward: 0.0

--- metanurse.py
+++ metanurse.py
@@ -1,106 +1,78 @@
 import sys
 
-
 def main():
     max_steps = 350
     opened_drawers = {19: False, 20: False}
-    used_methods = {
-        "UsedSatsProbe": False,
-        "ViewedMonitor": False,
-        "BP_Cuff": False,
-        "A_Line": False,
-        "Fluids": False,
-    }
-
+    used_methods = {'UsedSatsProbe': False, 'ViewedMonitor': False, 
+                    'BP_Cuff': False, 'A_Line': False, 'Fluids': False}
+    
     for step in range(max_steps):
         observations = list(map(float, input().strip().split()))
-        events, vital_signs_times, vital_signs_values = (
-            observations[:33],
-            observations[33:40],
-            observations[40:],
-        )
-
-        vitals = {
-            name: value if time > 0 else None
-            for value, time, name in zip(
-                vital_signs_values,
-                vital_signs_times,
-                [
-                    "HeartRate",
-                    "RespRate",
-                    "CapillaryGlucose",
-                    "Temperature",
-                    "MAP",
-                    "Sats",
-                    "Resps",
-                ],
-            )
-        }
-
-        if not events[3]:
-            print(3)  # ExamineAirway
+        events, vital_signs_times, vital_signs_values = observations[:33], observations[33:40], observations[40:]
+        
+        vitals = {name: value if time > 0 else None for value, time, name in 
+                  zip(vital_signs_values, vital_signs_times, 
+                      ["HeartRate", "RespRate", "CapillaryGlucose", "Temperature", "MAP", "Sats", "Resps"])}
+        
+        if not events[3]: 
+            print(3)
             continue
-
-        if not opened_drawers[19]:
-            print(19)  # OpenBreathingDrawer
+        
+        if not opened_drawers[19]: 
+            print(19) 
             opened_drawers[19] = True
             continue
 
-        if not used_methods["UsedSatsProbe"]:
-            print(25)  # UseSatsProbe
-            used_methods["UsedSatsProbe"] = True
+        if not used_methods['UsedSatsProbe']: 
+            print(25)
+            used_methods['UsedSatsProbe'] = True
             continue
 
-        if not used_methods["ViewedMonitor"]:
-            print(16)  # ViewMonitor
-            used_methods["ViewedMonitor"] = True
+        if not used_methods['ViewedMonitor']: 
+            print(16)
+            used_methods['ViewedMonitor'] = True
+            continue
+        
+        if (vitals["Sats"] is not None and vitals["Sats"] < 65) or (vitals["MAP"] is not None and vitals["MAP"] < 20): 
+            print(17)
             continue
 
-        if (vitals["Sats"] is not None and vitals["Sats"] < 65) or (
-            vitals["MAP"] is not None and vitals["MAP"] < 20
-        ):
-            print(17)  # StartChestCompression
+        if vitals["Sats"] is not None and vitals["Sats"] < 88: 
+            print(30)
             continue
 
-        if vitals["Sats"] is not None and vitals["Sats"] < 88:
-            print(30)  # UseNonRebreatherMask
+        if vitals["RespRate"] is not None and vitals["RespRate"] < 8: 
+            print(29)
             continue
 
-        if vitals["RespRate"] is not None and vitals["RespRate"] < 8:
-            print(29)  # UseBagValveMask
+        if vitals["MAP"] is not None and vitals["MAP"] < 60: 
+            if not used_methods['BP_Cuff']: 
+                print(27)
+                used_methods['BP_Cuff'] = True
+            elif not used_methods['A_Line']: 
+                print(26)
+                used_methods['A_Line'] = True
+            elif not used_methods['Fluids']: 
+                print(15)
+                used_methods['Fluids'] = True
             continue
 
-        if vitals["MAP"] is not None and vitals["MAP"] < 60:
-            if not used_methods["BP_Cuff"]:
-                print(27)  # UseBloodPressureCuff
-                used_methods["BP_Cuff"] = True
-            elif not used_methods["A_Line"]:
-                print(26)  # UseAline
-                used_methods["A_Line"] = True
-            elif not used_methods["Fluids"]:
-                print(15)  # GiveFluids
-                used_methods["Fluids"] = True
+        if vitals["HeartRate"] is not None and vitals["HeartRate"] > 100: 
+            print(2)
+            print(10)
+            continue
+        
+        if vitals["HeartRate"] is not None and vitals["HeartRate"] < 50: 
+            print(12)
             continue
 
-        if vitals["HeartRate"] is not None and vitals["HeartRate"] > 100:
-            print(2)  # CheckRhythm
-            print(10)  # GiveAdrenaline
-            continue
-
-        if vitals["HeartRate"] is not None and vitals["HeartRate"] < 50:
-            print(12)  # GiveAtropine
-            continue
-
-        if (
-            (vitals["Sats"] is not None and vitals["Sats"] >= 88)
-            and (vitals["RespRate"] is not None and vitals["RespRate"] >= 8)
-            and (vitals["MAP"] is not None and vitals["MAP"] >= 60)
-        ):
-            print(48)  # Finish
+        if (vitals["Sats"] is not None and vitals["Sats"] >= 88) and 
+           (vitals["RespRate"] is not None and vitals["RespRate"] >= 8) and 
+           (vitals["MAP"] is not None and vitals["MAP"] >= 60):
+            print(48)
             return
 
-    print(48)  # Finish
-
+    print(48)
 
 if __name__ == "__main__":
     main()
