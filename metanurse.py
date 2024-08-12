045b7b81a07f65246d4112c8e3ff0d859822fb81
beep boop <githubbot@vadim.me>
2024-08-13 00:53:52 +0200

    168 (0.0). One of the ED nurses has called you to see a patient that she is worried about.
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    What's more, if John's sats fall below 65% or if his mean arterial pressure falls below 20mmHg, he will go into cardiac arrest.
    
    Good luck! 
    DoNothing
    ExamineAirway
    John is conscious and talking. The airway seems to be fine. 
    AirwayClear
    OpenBreathingDrawer
    UseSatsProbe
    ViewMonitor
    MeasuredSats: 92
    Finish
    Thanks for playing! Here's how you did:
    
    Sadly, you didn't manage to stabilise John on this occasion.  He is hypotensive.  He has an unstable tachyarrhythmia, which is an indication for cardioversion. 
    
    Don't worry if you're not perfect yet, though. That's exactly what we're here for! 
    
    Final reward: 0.0

--- metanurse.py
+++ metanurse.py
@@ -1,94 +1,76 @@
 import sys
 
-
 def main():
     max_steps = 350
     actions_taken = {
-        "AirwayClear": False,
-        "BreathingDrawer": False,
-        "SatsProbe": False,
-        "Monitor": False,
-        "BP_Cuff": False,
-        "Fluids": False,
-        "A_Line": False,
+        'AirwayClear': False, 'BreathingDrawer': False, 'SatsProbe': False,
+        'Monitor': False, 'BP_Cuff': False, 'Fluids': False, 'A_Line': False
     }
-
+    
     for step in range(max_steps):
         observations = list(map(float, input().strip().split()))
         events = observations[:33]
         vital_signs_times = observations[33:40]
         vital_signs_values = observations[40:]
 
-        vitals = {
-            name: value if time > 0 else None
-            for value, time, name in zip(
-                vital_signs_values,
-                vital_signs_times,
-                [
-                    "HeartRate",
-                    "RespRate",
-                    "CapillaryGlucose",
-                    "Temperature",
-                    "MAP",
-                    "Sats",
-                    "Resps",
-                ],
-            )
-        }
-
-        if not actions_taken["AirwayClear"]:
+        vitals = {name: value if time > 0 else None for value, time, name in zip(vital_signs_values, vital_signs_times, 
+                        ["HeartRate", "RespRate", "CapillaryGlucose", "Temperature", "MAP", "Sats", "Resps"])}
+        
+        # AIRWAY
+        if not actions_taken['AirwayClear']:
             print(3)  # ExamineAirway
-            actions_taken["AirwayClear"] = True
+            actions_taken['AirwayClear'] = True
             continue
-
-        if not actions_taken["BreathingDrawer"]:
+        
+        # BREATHING
+        if not actions_taken['BreathingDrawer']:
             print(19)  # OpenBreathingDrawer
-            actions_taken["BreathingDrawer"] = True
+            actions_taken['BreathingDrawer'] = True
             continue
-
-        if not actions_taken["SatsProbe"]:
+        
+        if not actions_taken['SatsProbe']:
             print(25)  # UseSatsProbe
-            actions_taken["SatsProbe"] = True
+            actions_taken['SatsProbe'] = True
             continue
-
-        if not actions_taken["Monitor"]:
+        
+        if not actions_taken['Monitor']:
             print(16)  # ViewMonitor
-            actions_taken["Monitor"] = True
+            actions_taken['Monitor'] = True
             continue
-
-        if (vitals["Sats"] and vitals["Sats"] < 65) or (
-            vitals["MAP"] and vitals["MAP"] < 20
-        ):
+        
+        if (vitals["Sats"] and vitals["Sats"] < 65) or (vitals["MAP"] and vitals["MAP"] < 20):
             print(17)  # StartChestCompression
             continue
-
+        
         if vitals["Sats"] and vitals["Sats"] < 88:
             print(30)  # UseNonRebreatherMask
             continue
-
+        
         if vitals["RespRate"] and vitals["RespRate"] < 8:
             print(29)  # UseBagValveMask
             continue
-
-        if not actions_taken["BP_Cuff"]:
+        
+        # CIRCULATION
+        if not actions_taken['BP_Cuff']:
             print(27)  # UseBloodPressureCuff
-            actions_taken["BP_Cuff"] = True
+            actions_taken['BP_Cuff'] = True
             continue
-
-        if not actions_taken["Fluids"]:
+        
+        if not actions_taken['Fluids']:
             print(15)  # GiveFluids
-            actions_taken["Fluids"] = True
+            actions_taken['Fluids'] = True
             continue
-
-        if not actions_taken["A_Line"]:
+        
+        if not actions_taken['A_Line']:
             print(26)  # UseAline
-            actions_taken["A_Line"] = True
+            actions_taken['A_Line'] = True
             continue
 
         if vitals["MAP"] and vitals["MAP"] < 60:
             print(15)  # GiveFluids
             continue
 
+        # Check Heart Rate for pharmacological intervention
         if vitals["HeartRate"]:
             if vitals["HeartRate"] > 150:
                 print(10)  # GiveAdrenaline
@@ -99,12 +81,11 @@ def main():
             elif vitals["HeartRate"] < 50:
                 print(12)  # GiveAtropine
                 continue
-
+            
         print(48)  # Finish
         return
 
     print(48)
-
 
 if __name__ == "__main__":
     main()
