2524c54ddf3aecf3c19e24371ee0f823cdd959e0
beep boop <githubbot@vadim.me>
2024-08-13 02:33:30 +0200

    269 (0.0). One of the ED nurses has called you to see a patient that she is worried about.
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    What's more, if John's sats fall below 65% or if his mean arterial pressure falls below 20mmHg, he will go into cardiac arrest.
    
    Good luck! 
    DoNothing
    ExamineAirway
    John is conscious and talking. The airway seems to be fine. 
    AirwayClear
    OpenBreathingDrawer
    UseSatsProbe
    ViewMonitor
    MeasuredSats: 92
    Finish
    Thanks for playing! Here's how you did:
    
    Sadly, you didn't manage to stabilise John on this occasion.  He is hypotensive.  He has an unstable tachyarrhythmia, which is an indication for cardioversion. 
    
    Don't worry if you're not perfect yet, though. That's exactly what we're here for! 
    
    Final reward: 0.0

--- metanurse.py
+++ metanurse.py
@@ -2,17 +2,18 @@ import sys
 
 def main():
     max_steps = 350
-    used_methods = {
-        "UsedSatsProbe": False,
-        "ViewedMonitor": False,
-        "OpenedBreathingDrawer": False,
-        "OpenedCirculationDrawer": False,
-        "UsedMonitorPads": False,
-        "UsedBP_Cuff": False,
-        "UsedA_Line": False,
-        "GivenFluids": False,
-        "UsedDefibPads": False,
-        "DefibrillatorCharged": False,
+    steps = {
+        "examine_airway": False,
+        "open_breathing_drawer": False,
+        "use_sats_probe": False,
+        "view_monitor": False,
+        "open_circulation_drawer": False,
+        "use_monitor_pads": False,
+        "use_bp_cuff": False,
+        "use_a_line": False,
+        "give_fluids": False,
+        "use_defib_pads": False,
+        "defibrillator_charged": False,
     }
 
     for step in range(max_steps):
@@ -24,85 +25,103 @@ def main():
         )
 
         vitals = {
-            name: value if times > 0 else None
-            for times, value, name in zip(
-                vital_signs_times,
+            name: value if time > 0 else None
+            for value, time, name in zip(
                 vital_signs_values,
+                vital_signs_times,
                 [
-                    "HeartRate", "RespRate", "CapillaryGlucose",
-                    "Temperature", "MAP", "Sats", "Resps"
+                    "HeartRate",
+                    "RespRate",
+                    "CapillaryGlucose",
+                    "Temperature",
+                    "MAP",
+                    "Sats",
+                    "Resps",
                 ],
             )
         }
 
-        if not events[3]:  # AirwayClear
-            print(3)  # ExamineAirway
+        # Airway
+        if not steps["examine_airway"]:
+            print(3)
+            steps["examine_airway"] = True
             continue
 
-        if not used_methods["OpenedBreathingDrawer"]:
-            print(19)  # OpenBreathingDrawer
-            used_methods["OpenedBreathingDrawer"] = True
+        if 3 in events:
+            steps["examine_airway"] = True
+
+        # Breathing
+        if not steps["open_breathing_drawer"]:
+            print(19)
+            steps["open_breathing_drawer"] = True
             continue
 
-        if not used_methods["UsedSatsProbe"]:
-            print(25)  # UseSatsProbe
-            used_methods["UsedSatsProbe"] = True
+        if not steps["use_sats_probe"]:
+            print(25)
+            steps["use_sats_probe"] = True
             continue
 
-        if not used_methods["ViewedMonitor"]:
-            print(16)  # ViewMonitor
-            used_methods["ViewedMonitor"] = True
+        # Circulation
+        if not steps["view_monitor"]:
+            print(16)
+            steps["view_monitor"] = True
             continue
 
-        if (vitals["Sats"] and vitals["Sats"] < 65) or (vitals["MAP"] and vitals["MAP"] < 20):
-            print(17)  # StartChestCompression
+        if (vitals["Sats"] and vitals["Sats"] < 65) or (
+            vitals["MAP"] and vitals["MAP"] < 20
+        ):
+            print(17)
             continue
 
         if vitals["Sats"] and vitals["Sats"] < 88:
-            print(30)  # UseNonRebreatherMask
+            print(30)
             continue
 
         if vitals["RespRate"] and vitals["RespRate"] < 8:
-            print(29)  # UseBagValveMask
+            print(29)
             continue
 
         if vitals["MAP"] and vitals["MAP"] < 60:
-            if not used_methods["OpenedCirculationDrawer"]:
-                print(20)  # OpenCirculationDrawer
-                used_methods["OpenedCirculationDrawer"] = True
-                continue
-            if not used_methods["UsedBP_Cuff"]:
-                print(27)  # UseBloodPressureCuff
-                used_methods["UsedBP_Cuff"] = True
-                continue
-            if not used_methods["GivenFluids"]:
-                print(15)  # GiveFluids
-                used_methods["GivenFluids"] = True
-                continue
+            if not steps["open_circulation_drawer"]:
+                print(20)
+                steps["open_circulation_drawer"] = True
+            elif not steps["use_monitor_pads"]:
+                print(24)
+                steps["use_monitor_pads"] = True
+            elif not steps["use_bp_cuff"]:
+                print(27)
+                steps["use_bp_cuff"] = True
+            elif not steps["use_a_line"]:
+                print(26)
+                steps["use_a_line"] = True
+            elif not steps["give_fluids"]:
+                print(15)
+                steps["give_fluids"] = True
+            continue
 
         if vitals["HeartRate"]:
             if vitals["HeartRate"] < 50:
-                print(12)  # GiveAtropine
+                print(12)
                 continue
             elif 100 < vitals["HeartRate"] <= 150:
-                print(2)  # CheckRhythm
+                print(2)
                 continue
             elif vitals["HeartRate"] > 150:
-                if not used_methods["UsedDefibPads"]:
-                    print(28)  # AttachDefibPads
-                    used_methods["UsedDefibPads"] = True
+                if not steps["use_defib_pads"]:
+                    print(28)
+                    steps["use_defib_pads"] = True
                     continue
-                if not used_methods["DefibrillatorCharged"]:
-                    print(40)  # DefibrillatorCharge
-                    used_methods["DefibrillatorCharged"] = True
+                if not steps["defibrillator_charged"]:
+                    print(40)
+                    steps["defibrillator_charged"] = True
                     continue
-                print(44)  # DefibrillatorPacePause
+                print(44)
                 continue
 
-        print(48)  # Finish
+        print(48)
         return
 
-    print(48)  # Finish
+    print(48)
 
 if __name__ == "__main__":
     main()
