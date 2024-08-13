e6e0cbd47ec795cc6d23dfdab898c87a06e69c82
beep boop <githubbot@vadim.me>
2024-08-13 11:18:42 +0200

    805 (0.0). One of the ED nurses has called you to see a patient that she is worried about.
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    What's more, if John's sats fall below 65% or if his mean arterial pressure falls below 20mmHg, he will go into cardiac arrest.
    
    Good luck! 
    DoNothing
    ExamineAirway
    John is conscious and talking. The airway seems to be fine. 
    AirwayClear
    UseSatsProbe
    UseBloodPressureCuff
    ViewMonitor
    MeasuredMAP: 37
    MeasuredSats: 92
    Finish
    Thanks for playing! Here's how you did:
    
    Sadly, you didn't manage to stabilise John on this occasion.  He is hypotensive.  He has an unstable tachyarrhythmia, which is an indication for cardioversion. 
    
    Don't worry if you're not perfect yet, though. That's exactly what we're here for! 
    
    Final reward: 0.0

--- metanurse.py
+++ metanurse.py
@@ -4,45 +4,83 @@ def main():
     max_steps = 350
     used_methods = set()
     initial_examine = False
+    step_counter = 0
 
-    for step in range(max_steps):
+    def next_action(actions):
+        for action in actions:
+            if action not in used_methods:
+                used_methods.add(action)
+                return action
+        return 0
+
+    while step_counter < max_steps:
         observations = list(map(float, input().strip().split()))
         events, vital_signs_times, vital_signs_values = observations[:33], observations[33:40], observations[40:]
         vitals = {name: value if time > 0 else None for value, time, name in zip(vital_signs_values, vital_signs_times, [
             "HeartRate", "RespRate", "CapillaryGlucose", "Temperature", "MAP", "Sats", "Resps"
         ])}
 
-        if step == 0 or not initial_examine:
-            action = [3, 4, 5][step % 3]
-            print(action)
-            initial_examine = True
-            continue
+        if not initial_examine:
+            actions = [3, 4, 5, 6, 7, 8]
+            action = next_action(actions)
+            if action:
+                initial_examine = True
+                step_counter += 1
+                print(action)
+                continue
 
         if "UseSatsProbe" not in used_methods:
             print(25)
             used_methods.add("UseSatsProbe")
+            step_counter += 1
             continue
 
         if "UseBloodPressureCuff" not in used_methods:
             print(27)
             used_methods.add("UseBloodPressureCuff")
+            step_counter += 1
             continue
 
         if "ViewMonitor" not in used_methods:
             print(16)
             used_methods.add("ViewMonitor")
+            step_counter += 1
             continue
 
         if vitals["Sats"] and (vitals["Sats"] < 65 or (vitals["MAP"] is not None and vitals["MAP"] < 20)):
             print(17)
+            step_counter += 1
             continue
         
         if vitals["Sats"] and vitals["Sats"] < 88:
             print(30)
+            step_counter += 1
             continue
         
+        if vitals["RespRate"] and vitals["RespRate"] < 8:
+            print(29)
+            step_counter += 1
+            continue
+
         if vitals["MAP"] and vitals["MAP"] < 60:
+            if vitals["HeartRate"] and (vitals["HeartRate"] > 150 or vitals["HeartRate"] < 50):
+                if "TurnOnDefibrillator" not in used_methods:
+                    print(39)
+                    used_methods.add("TurnOnDefibrillator")
+                    step_counter += 1
+                    continue
+                if "DefibrillatorCharge" not in used_methods:
+                    print(40)
+                    used_methods.add("DefibrillatorCharge")
+                    step_counter += 1
+                    continue
+                if "DefibrillatorPace" not in used_methods:
+                    print(43)
+                    used_methods.add("DefibrillatorPace")
+                    step_counter += 1
+                    continue
             print(15)
+            step_counter += 1
             continue
 
         if all(vital is not None and vital >= threshold for vital, threshold in zip(
@@ -51,6 +89,7 @@ def main():
             print(48)
             return
         
+        step_counter += 1
         print(48)
         return
 
