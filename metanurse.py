60f02f91257e1b5456d212f574341fcc4cf550ee
beep boop <githubbot@vadim.me>
2024-08-13 16:13:06 +0200

    952 (0.0). One of the ED nurses has called you to see a patient that she is worried about.
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    What's more, if John's sats fall below 65% or if his mean arterial pressure falls below 20mmHg, he will go into cardiac arrest.
    
    Good luck! 
    DoNothing
    ExamineAirway
    John is conscious and talking. The airway seems to be fine. 
    AirwayClear
    UseSatsProbe
    UseBloodPressureCuff
    ViewMonitor
    MeasuredMAP: 37
    MeasuredSats: 92
    ExamineAirway
    MeasuredMAP: 36
    MeasuredSats: 92
    ExamineBreathing
    MeasuredMAP: 35
    MeasuredSats: 92
    ExamineCirculation
    MeasuredMAP: 34
    MeasuredSats: 92
    Finish
    Thanks for playing! Here's how you did:
    
    Sadly, you didn't manage to stabilise John on this occasion.  He is hypotensive.  He has an unstable tachyarrhythmia, which is an indication for cardioversion. 
    
    Don't worry if you're not perfect yet, though. That's exactly what we're here for! 
    
    Final reward: -1.0

--- metanurse.py
+++ metanurse.py
@@ -3,8 +3,7 @@ import sys
 def stabilize():
     max_steps = 350
 
-    # Status flags
-    first_examine = use_sats_probe = use_blood_pressure_cuff = view_monitor = False
+    first_examine = use_sats_probe = use_blood_pressure_cuff = use_monitor = False
     airway_examined = breathing_examined = circulation_examined = False
 
     for step in range(max_steps):
@@ -33,51 +32,51 @@ def stabilize():
 
         if not first_examine:
             first_examine = True
-            print(3)  # ExamineAirway
+            print(3)
             continue
 
         if not use_sats_probe:
-            print(25)  # UseSatsProbe
+            print(25)
             use_sats_probe = True
             continue
 
         if not use_blood_pressure_cuff:
-            print(27)  # UseBloodPressureCuff
+            print(27)
             use_blood_pressure_cuff = True
             continue
 
-        if not view_monitor:
-            print(16)  # ViewMonitor
-            view_monitor = True
+        if not use_monitor:
+            print(16)
+            use_monitor = True
             continue
 
         if not circulation_examined:
-            print(5)  # ExamineCirculation
+            print(5)
             circulation_examined = True
             continue 
         
         if vitals["Sats"] is not None and vitals["Sats"] < 65:
-            print(17)  # StartChestCompression
+            print(17)
             continue
 
         if vitals["MAP"] is not None and vitals["MAP"] < 20:
-            print(17)  # StartChestCompression
+            print(17)
             continue
         
         if vitals["MAP"] is not None and vitals["MAP"] < 60:
-            print(15)  # GiveFluids
+            print(15)
             continue
 
         if vitals["HeartRate"] is not None and (vitals["HeartRate"] > 150 or vitals["HeartRate"] < 50):
-            print(41)  # DefibrillatorCurrentUp
+            print(41)
             continue
 
         if vitals["Sats"] is not None and vitals["Sats"] < 88:
-            print(30)  # UseNonRebreatherMask
+            print(30)
             continue
 
         if vitals["RespRate"] is not None and vitals["RespRate"] < 8:
-            print(29)  # UseBagValveMask
+            print(29)
             continue
 
         if all(
@@ -86,10 +85,10 @@ def stabilize():
                 [vitals["Sats"], vitals["RespRate"], vitals["MAP"]], [88, 8, 60]
             )
         ):
-            print(48)  # Finish
+            print(48)
             return
 
-        print(48)  # Finish
+        print(48)
         return
 
 if __name__ == "__main__":
