f29b9fbd36254fa29e2b813375035fc043f3f5e8
beep boop <githubbot@vadim.me>
2024-08-13 15:16:33 +0200

    930 (0.0). One of the ED nurses has called you to see a patient that she is worried about.
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    What's more, if John's sats fall below 65% or if his mean arterial pressure falls below 20mmHg, he will go into cardiac arrest.
    
    Good luck! 
    DoNothing
    ExamineAirway
    John is conscious and talking. The airway seems to be fine. 
    AirwayClear
    ExamineBreathing
    John is breathing at a rate of 20 breaths per minute. Chest exapansion is equal. There are bibasal crepitations. 
    
    (Select the finger probe from the breathing drawer and hit "View Monitor" to see sats.)
    BreathingEqualChestExpansion
    BreathingBibasalCrepitations
    MeasuredRespRate: 20
    ExamineCirculation
    The radial pulse is too weak to palpate. The carotid pulse is palpable. The heart rate is 190bpm. Heart sounds are normal. 
    
    (To check the BP, choose the pressure cuff or the arterial line from the circulation drawer and hit "View Monitor".)
    RadialPulseNonPalpable
    HeartSoundsNormal
    MeasuredHeartRate: 190.4614
    UseSatsProbe
    UseBloodPressureCuff
    ViewMonitor
    MeasuredMAP: 35
    MeasuredSats: 92
    Finish
    Thanks for playing! Here's how you did:
    
    Sadly, you didn't manage to stabilise John on this occasion.  He is hypotensive.  He has an unstable tachyarrhythmia, which is an indication for cardioversion. 
    
    Don't worry if you're not perfect yet, though. That's exactly what we're here for! 
    
    Final reward: -1.0

--- metanurse.py
+++ metanurse.py
@@ -1,108 +1,83 @@
 import sys
 
-
 def stabilize():
     max_steps = 350
-    examine_airway = (
-        examine_breathing
-    ) = examine_circulation = examine_disability = False
+    examine_airway = examine_breathing = examine_circulation = examine_disability = False
     use_sats_probe = use_blood_pressure_cuff = view_monitor = False
 
     for step in range(max_steps):
         observations = list(map(float, input().strip().split()))
-        events, vital_signs_times, vital_signs_values = (
-            observations[:33],
-            observations[33:40],
-            observations[40:],
-        )
-        vitals = {
-            name: value if time > 0 else None
-            for value, time, name in zip(
-                vital_signs_values,
-                vital_signs_times,
-                [
-                    "HeartRate",
-                    "RespRate",
-                    "CapillaryGlucose",
-                    "Temperature",
-                    "MAP",
-                    "Sats",
-                    "Resps",
-                ],
-            )
-        }
+        events, vital_signs_times, vital_signs_values = observations[:33], observations[33:40], observations[40:]
+        vitals = {name: value if time > 0 else None for value, time, name in zip(vital_signs_values, vital_signs_times, [
+            "HeartRate", "RespRate", "CapillaryGlucose", "Temperature", "MAP", "Sats", "Resps"
+        ])}
 
         if not examine_airway:
             examine_airway = True
-            print(3)
+            print(3)  # Examine Airway
             continue
-
+        
         if not examine_breathing:
             examine_breathing = True
-            print(4)
+            print(4)  # Examine Breathing
             continue
-
+        
         if not examine_circulation:
             examine_circulation = True
-            print(5)
+            print(5)  # Examine Circulation
             continue
 
         if vitals["Sats"] is None and not use_sats_probe:
-            print(25)
+            print(25)  # Use Sats Probe
             use_sats_probe = True
             continue
 
         if vitals["MAP"] is None and not use_blood_pressure_cuff:
-            print(27)
+            print(27)  # Use Blood Pressure Cuff
             use_blood_pressure_cuff = True
             continue
 
         if not view_monitor:
             view_monitor = True
-            print(16)
+            print(16)  # View Monitor
             continue
 
         if vitals["Sats"] is not None and vitals["Sats"] < 65:
-            print(17)
+            print(17)  # Start Chest Compression
             continue
-
+        
         if vitals["MAP"] is not None and vitals["MAP"] < 20:
-            print(17)
+            print(17)  # Start Chest Compression
             continue
 
         if vitals["MAP"] is not None and vitals["MAP"] < 60:
-            print(15)
+            print(15)  # Give Fluids
             continue
 
         if vitals["Sats"] is not None and vitals["Sats"] < 88:
-            print(30)
+            print(30)  # Use Non Rebreather Mask
             continue
 
         if vitals["RespRate"] is not None and vitals["RespRate"] < 8:
-            print(29)
+            print(29)  # Use Bag Valve Mask
             continue
-
+        
         if vitals["HeartRate"] is not None and vitals["HeartRate"] > 150:
-            print(43)
+            print(43)  # Defibrillator Pace
             continue
 
         if vitals["HeartRate"] is not None and vitals["HeartRate"] < 50:
-            print(43)
+            print(43)  # Defibrillator Pace
             continue
 
-        if all(
-            [
-                vitals["Sats"] is not None and vitals["Sats"] >= 88,
+        if all([vitals["Sats"] is not None and vitals["Sats"] >= 88,
                 vitals["RespRate"] is not None and vitals["RespRate"] >= 8,
-                vitals["MAP"] is not None and vitals["MAP"] >= 60,
-            ]
-        ):
-            print(48)
+                vitals["MAP"] is not None and vitals["MAP"] >= 60]):
+            print(48)  # Finish
             return
 
-        print(48)
+        print(48)  # Finish
         return
-
 
 if __name__ == "__main__":
     stabilize()
