2f67a0368571032d2bbafca213725e96117086ce
beep boop <githubbot@vadim.me>
2024-08-13 12:15:36 +0200

    836 (0.0). One of the ED nurses has called you to see a patient that she is worried about.
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    What's more, if John's sats fall below 65% or if his mean arterial pressure falls below 20mmHg, he will go into cardiac arrest.
    
    Good luck! 
    DoNothing
    ExamineAirway
    John is conscious and talking. The airway seems to be fine. 
    AirwayClear
    UseSatsProbe
    UseBloodPressureCuff
    ViewMonitor
    MeasuredMAP: 37
    MeasuredSats: 92
    Finish
    Thanks for playing! Here's how you did:
    
    Sadly, you didn't manage to stabilise John on this occasion.  He is hypotensive.  He has an unstable tachyarrhythmia, which is an indication for cardioversion. 
    
    Don't worry if you're not perfect yet, though. That's exactly what we're here for! 
    
    Final reward: 0.0

--- metanurse.py
+++ metanurse.py
@@ -3,9 +3,7 @@ import sys
 
 def stabilize():
     max_steps = 350
-    used_probes = set(
-        ["ExamineAirway", "UseSatsProbe", "UseBloodPressureCuff", "ViewMonitor"]
-    )
+    used_probes = set()
 
     for step in range(max_steps):
         observations = list(map(float, input().strip().split()))
@@ -32,51 +30,51 @@ def stabilize():
         }
 
         if "ExamineAirway" not in used_probes:
-            print(3)
+            print(3)  # ExamineAirway
             used_probes.add("ExamineAirway")
             continue
 
         if "UseSatsProbe" not in used_probes:
-            print(25)
+            print(25)  # UseSatsProbe
             used_probes.add("UseSatsProbe")
             continue
 
         if "UseBloodPressureCuff" not in used_probes:
-            print(27)
+            print(27)  # UseBloodPressureCuff
             used_probes.add("UseBloodPressureCuff")
             continue
 
         if "ViewMonitor" not in used_probes:
-            print(16)
+            print(16)  # ViewMonitor
             used_probes.add("ViewMonitor")
             continue
 
         if vitals["Sats"] is not None and vitals["Sats"] < 65:
-            print(17)
+            print(17)  # StartChestCompression
             continue
 
         if vitals["MAP"] is not None and vitals["MAP"] < 20:
-            print(17)
+            print(17)  # StartChestCompression
             continue
 
         if vitals["Sats"] is not None and vitals["Sats"] < 88:
-            print(30)
+            print(30)  # UseNonRebreatherMask
             continue
 
         if vitals["RespRate"] is not None and vitals["RespRate"] < 8:
-            print(29)
+            print(29)  # UseBagValveMask
             continue
 
         if vitals["MAP"] is not None and vitals["MAP"] < 60:
-            print(15)
+            print(15)  # GiveFluids
             continue
 
         if vitals["HeartRate"] is not None:
             if vitals["HeartRate"] > 150 or vitals["HeartRate"] < 50:
-                print(39)
+                print(39)  # TurnOnDefibrillator
                 continue
             if 60 <= vitals["HeartRate"] <= 100:
-                print(43)
+                print(43)  # DefibrillatorPace
                 continue
 
         if all(
@@ -85,10 +83,10 @@ def stabilize():
                 [vitals["Sats"], vitals["RespRate"], vitals["MAP"]], [88, 8, 60]
             )
         ):
-            print(48)
+            print(48)  # Finish
             return
 
-        print(1)
+        print(1)  # DoNothing
 
 
 if __name__ == "__main__":
