5c91c08197dd73710e2bbf7f244d1eaac7f3efda
beep boop <githubbot@vadim.me>
2024-08-13 17:05:43 +0200

    980 (0.0). One of the ED nurses has called you to see a patient that she is worried about.
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    What's more, if John's sats fall below 65% or if his mean arterial pressure falls below 20mmHg, he will go into cardiac arrest.
    
    Good luck! 
    DoNothing
    ExamineAirway
    John is conscious and talking. The airway seems to be fine. 
    AirwayClear
    UseSatsProbe
    UseBloodPressureCuff
    ViewMonitor
    MeasuredMAP: 37
    MeasuredSats: 92
    Finish
    Thanks for playing! Here's how you did:
    
    Sadly, you didn't manage to stabilise John on this occasion.  He is hypotensive.  He has an unstable tachyarrhythmia, which is an indication for cardioversion. 
    
    Don't worry if you're not perfect yet, though. That's exactly what we're here for! 
    
    Final reward: 0.0

--- metanurse.py
+++ metanurse.py
@@ -17,45 +17,48 @@ def stabilize():
             )
         }
 
-        if step == 0: actions_taken.add(25); print(25); continue
-        if step == 1: actions_taken.add(27); print(27); continue
-        if step == 2: actions_taken.add(16); print(16); continue
+        # Initial examinations
+        if step == 0: actions_taken.add(25); print(25); continue  # UseSatsProbe
+        if step == 1: actions_taken.add(27); print(27); continue  # UseBloodPressureCuff
+        if step == 2: actions_taken.add(16); print(16); continue  # ViewMonitor
         if all(a in actions_taken for a in [25, 27, 16]):
             if "AirwayClear" not in actions_taken:
-                print(3)
+                print(3)  # ExamineAirway
                 actions_taken.add("AirwayClear")
                 continue
 
+        # Interventions
         if vitals["Sats"] and vitals["Sats"] < 65:
-            print(17)
+            print(17)  # StartChestCompression
             continue
-        
+
         if vitals["MAP"] and vitals["MAP"] < 20:
-            print(17)
+            print(17)  # StartChestCompression
             continue
-        
+
         if vitals["MAP"] and vitals["MAP"] < 60:
-            print(15)
+            print(15)  # GiveFluids
             continue
-        
+
         if vitals["Sats"] and vitals["Sats"] < 88:
-            print(30)
+            print(30)  # UseNonRebreatherMask
             continue
-        
+
         if vitals["RespRate"] and vitals["RespRate"] < 8:
-            print(29)
+            print(29)  # UseBagValveMask
             continue
-        
+
+        # End criteria
         if all(
             vital is not None and vital >= threshold
             for vital, threshold in zip(
                 [vitals["Sats"], vitals["RespRate"], vitals["MAP"]], [88, 8, 60]
             )
         ):
-            print(48)
+            print(48)  # Finish
             return
-        
-        print(48)
+
+        print(48)  # Finish
         return
 
 if __name__ == "__main__":
