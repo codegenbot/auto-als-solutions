26a72da16a560d332feab8f4d6ed07a70863b0ff
beep boop <githubbot@vadim.me>
2024-08-13 05:30:29 +0200

    485 (0.0). One of the ED nurses has called you to see a patient that she is worried about.
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    What's more, if John's sats fall below 65% or if his mean arterial pressure falls below 20mmHg, he will go into cardiac arrest.
    
    Good luck! 
    DoNothing
    ExamineAirway
    John is conscious and talking. The airway seems to be fine. 
    AirwayClear
    UseSatsProbe
    UseBloodPressureCuff
    ViewMonitor
    MeasuredMAP: 37
    MeasuredSats: 92
    Finish
    Thanks for playing! Here's how you did:
    
    Sadly, you didn't manage to stabilise John on this occasion.  He is hypotensive.  He has an unstable tachyarrhythmia, which is an indication for cardioversion. 
    
    Don't worry if you're not perfect yet, though. That's exactly what we're here for! 
    
    Final reward: 0.0

--- metanurse.py
+++ metanurse.py
@@ -4,7 +4,7 @@ def main():
     max_steps = 350
     used_methods = set()
     initial_examine = False
-    
+
     for step in range(max_steps):
         observations = list(map(float, input().strip().split()))
         events, vital_signs_times, vital_signs_values = (
@@ -29,69 +29,69 @@ def main():
                 ],
             )
         }
-        
+
         if step == 0 or not initial_examine:
             print(3)  # ExamineAirway
             initial_examine = True
             continue
-        
+
         if not events[3]:  # AirwayClear
             print(35)  # PerformAirwayManoeuvres
             continue
-        
+
         if (vitals["Sats"] and vitals["Sats"] < 65) or (
             vitals["MAP"] and vitals["MAP"] < 20
         ):
             print(17)  # StartChestCompression
             continue
-        
+
         if "UseSatsProbe" not in used_methods:
             print(25)  # UseSatsProbe
             used_methods.add("UseSatsProbe")
             continue
-        
+
         if "UseBloodPressureCuff" not in used_methods:
             print(27)  # UseBloodPressureCuff
             used_methods.add("UseBloodPressureCuff")
             continue
-        
+
         if "ViewMonitor" not in used_methods:
             print(16)  # ViewMonitor
             used_methods.add("ViewMonitor")
             continue
-        
-        if vitals["MAP"] and vitals["MAP"] < 60:
-            print(15)  # GiveFluids
-            continue
-        
-        if (vitals["HeartRate"] and vitals["HeartRate"] > 150) or events[27]:  # HeartRhythmSVT
-            if "TurnOnDefibrillator" not in used_methods:
-                print(39)  # TurnOnDefibrillator
-                used_methods.add("TurnOnDefibrillator")
-                continue
-            elif "DefibrillatorCharge" not in used_methods:
-                print(40)  # DefibrillatorCharge
-                used_methods.add("DefibrillatorCharge")
-                continue
-            elif "DefibrillatorSync" not in used_methods:
-                print(47)  # DefibrillatorSync
-                used_methods.add("DefibrillatorSync")
-                continue
-            else:
-                print(43)  # DefibrillatorPace
-                continue
-        
+
         if vitals["Sats"] and vitals["Sats"] < 88:
             print(30)  # UseNonRebreatherMask
             continue
-        
+
         if vitals["RespRate"] and vitals["RespRate"] < 8:
             print(29)  # UseBagValveMask
             continue
 
+        if vitals["MAP"] and vitals["MAP"] < 60:
+            if vitals["HeartRate"] and vitals["HeartRate"] > 150:
+                if "TurnOnDefibrillator" not in used_methods:
+                    print(39)  # TurnOnDefibrillator
+                    used_methods.add("TurnOnDefibrillator")
+                    continue
+                elif "DefibrillatorCharge" not in used_methods:
+                    print(40)  # DefibrillatorCharge
+                    used_methods.add("DefibrillatorCharge")
+                    continue
+                elif "DefibrillatorSync" not in used_methods:
+                    print(47)  # DefibrillatorSync
+                    used_methods.add("DefibrillatorSync")
+                    continue
+                else:
+                    print(43)  # DefibrillatorPace
+                    continue
+            else:
+                print(15)  # GiveFluids
+                continue
+
         print(48)  # Finish
         return
-    
+
     print(48)
 
 if __name__ == "__main__":
