c3e960091ddc1b96f59c54ef49a0c8d02db8624b
beep boop <githubbot@vadim.me>
2024-08-13 02:50:47 +0200

    307 (0.0). One of the ED nurses has called you to see a patient that she is worried about.
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    What's more, if John's sats fall below 65% or if his mean arterial pressure falls below 20mmHg, he will go into cardiac arrest.
    
    Good luck! 
    DoNothing
    ExamineAirway
    John is conscious and talking. The airway seems to be fine. 
    AirwayClear
    OpenBreathingDrawer
    UseSatsProbe
    ViewMonitor
    MeasuredSats: 92
    Finish
    Thanks for playing! Here's how you did:
    
    Sadly, you didn't manage to stabilise John on this occasion.  He is hypotensive.  He has an unstable tachyarrhythmia, which is an indication for cardioversion. 
    
    Don't worry if you're not perfect yet, though. That's exactly what we're here for! 
    
    Final reward: 0.0

--- metanurse.py
+++ metanurse.py
@@ -2,7 +2,14 @@ import sys
 
 def main():
     max_steps = 350
-    used_methods = set()
+    used_methods = {
+        "UsedSatsProbe": False,
+        "ViewedMonitor": False,
+        "OpenedBreathingDrawer": False,
+        "OpenedCirculationDrawer": False,
+        "UsedBP_Cuff": False,
+        "GivenFluids": False,
+    }
 
     for step in range(max_steps):
         observations = list(map(float, input().strip().split()))
@@ -28,58 +35,57 @@ def main():
             )
         }
 
-        if "ExamineAirway" not in used_methods:
+        if vitals["Sats"] and vitals["Sats"] < 65:
+            print(17)
+            continue
+        if vitals["MAP"] and vitals["MAP"] < 20:
+            print(17)
+            continue
+
+        if not events[3]:
             print(3)
-            used_methods.add("ExamineAirway")
-            continue
-        
-        if not events[3] and "PerformAirwayManoeuvres" not in used_methods:
-            print(35)
-            used_methods.add("PerformAirwayManoeuvres")
             continue
 
-        if "OpenBreathingDrawer" not in used_methods:
+        if not used_methods["OpenedBreathingDrawer"]:
             print(19)
-            used_methods.add("OpenBreathingDrawer")
+            used_methods["OpenedBreathingDrawer"] = True
             continue
-
-        if "UseSatsProbe" not in used_methods:
+        if not used_methods["UsedSatsProbe"]:
             print(25)
-            used_methods.add("UseSatsProbe")
+            used_methods["UsedSatsProbe"] = True
             continue
-
-        if "ViewMonitor" not in used_methods:
+        if not used_methods["ViewedMonitor"]:
             print(16)
-            used_methods.add("ViewMonitor")
-            continue
-
-        if (vitals["Sats"] and vitals["Sats"] < 65) or (
-            vitals["MAP"] and vitals["MAP"] < 20
-        ):
-            print(23)
+            used_methods["ViewedMonitor"] = True
             continue
 
         if vitals["Sats"] and vitals["Sats"] < 88:
             print(30)
             continue
-
         if vitals["RespRate"] and vitals["RespRate"] < 8:
             print(29)
             continue
 
         if vitals["MAP"] and vitals["MAP"] < 60:
-            print(15)
-            continue
+            if not used_methods["OpenedCirculationDrawer"]:
+                print(20)
+                used_methods["OpenedCirculationDrawer"] = True
+                continue
+            if not used_methods["UsedBP_Cuff"]:
+                print(27)
+                used_methods["UsedBP_Cuff"] = True
+                continue
+            if not used_methods["GivenFluids"]:
+                print(15)
+                used_methods["GivenFluids"] = True
+                continue
 
         if vitals["HeartRate"]:
+            if vitals["HeartRate"] > 150:
+                print(40)
+                continue
             if vitals["HeartRate"] < 50:
                 print(12)
-                continue
-            elif 100 < vitals["HeartRate"] <= 150:
-                print(2)
-                continue
-            elif vitals["HeartRate"] > 150:
-                print(9)
                 continue
 
         print(48)
