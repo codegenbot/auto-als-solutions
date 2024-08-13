2b28eb508c7fc404a44b5701f85ea7c1486048b4
beep boop <githubbot@vadim.me>
2024-08-13 05:29:50 +0200

    483 (0.0). One of the ED nurses has called you to see a patient that she is worried about.
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    What's more, if John's sats fall below 65% or if his mean arterial pressure falls below 20mmHg, he will go into cardiac arrest.
    
    Good luck! 
    DoNothing
    ExamineAirway
    John is conscious and talking. The airway seems to be fine. 
    AirwayClear
    UseSatsProbe
    UseBloodPressureCuff
    ViewMonitor
    MeasuredMAP: 37
    MeasuredSats: 92
    Finish
    Thanks for playing! Here's how you did:
    
    Sadly, you didn't manage to stabilise John on this occasion.  He is hypotensive.  He has an unstable tachyarrhythmia, which is an indication for cardioversion. 
    
    Don't worry if you're not perfect yet, though. That's exactly what we're here for! 
    
    Final reward: 0.0

--- metanurse.py
+++ metanurse.py
@@ -1,5 +1,6 @@
 import sys
 
+
 def main():
     max_steps = 350
     used_methods = set()
@@ -14,72 +15,88 @@ def main():
         )
 
         vitals = {
-            "HeartRate": vital_signs_values[0] if vital_signs_times[0] != 0 else None,
-            "RespRate": vital_signs_values[1] if vital_signs_times[1] != 0 else None,
-            "CapillaryGlucose": vital_signs_values[2] if vital_signs_times[2] != 0 else None,
-            "Temperature": vital_signs_values[3] if vital_signs_times[3] != 0 else None,
-            "MAP": vital_signs_values[4] if vital_signs_times[4] != 0 else None,
-            "Sats": vital_signs_values[5] if vital_signs_times[5] != 0 else None,
-            "Resps": vital_signs_values[6] if vital_signs_times[6] != 0 else None,
+            name: value if time > 0 else None
+            for value, time, name in zip(
+                vital_signs_values,
+                vital_signs_times,
+                [
+                    "HeartRate",
+                    "RespRate",
+                    "CapillaryGlucose",
+                    "Temperature",
+                    "MAP",
+                    "Sats",
+                    "Resps",
+                ],
+            )
         }
 
         if step == 0 or not initial_examine:
-            print(3)
+            print(3)  # ExamineAirway
             initial_examine = True
             continue
-        
-        if not events[3]:
-            print(35)
+
+        if not events[3]:  # AirwayClear
+            print(35)  # PerformAirwayManoeuvres
             continue
 
-        if (vitals["Sats"] and vitals["Sats"] < 65) or (vitals["MAP"] and vitals["MAP"] < 20):
-            print(17)
+        if (vitals["Sats"] and vitals["Sats"] < 65) or (
+            vitals["MAP"] and vitals["MAP"] < 20
+        ):
+            print(17)  # StartChestCompression
             continue
 
         if "UseSatsProbe" not in used_methods:
-            print(25)
+            print(25)  # UseSatsProbe
             used_methods.add("UseSatsProbe")
             continue
 
         if "UseBloodPressureCuff" not in used_methods:
-            print(27)
+            print(27)  # UseBloodPressureCuff
             used_methods.add("UseBloodPressureCuff")
             continue
 
         if "ViewMonitor" not in used_methods:
-            print(16)
+            print(16)  # ViewMonitor
             used_methods.add("ViewMonitor")
             continue
 
-        if vitals["Sats"] and vitals["Sats"] < 88:
-            print(30)
-            continue
-
-        if vitals["RespRate"] and vitals["RespRate"] < 8:
-            print(29)
-            continue
-
         if vitals["MAP"] and vitals["MAP"] < 60:
-            print(15)
+            print(15)  # GiveFluids
             continue
 
-        if (vitals["HeartRate"] and vitals["HeartRate"] > 150) or any(events[i] for i in range(28, 39)):
+        if (vitals["HeartRate"] and vitals["HeartRate"] > 150) or events[
+            27
+        ]:  # HeartRhythmSVT
             if "TurnOnDefibrillator" not in used_methods:
-                print(39)
+                print(39)  # TurnOnDefibrillator
                 used_methods.add("TurnOnDefibrillator")
                 continue
             elif "DefibrillatorCharge" not in used_methods:
-                print(40)
+                print(40)  # DefibrillatorCharge
                 used_methods.add("DefibrillatorCharge")
                 continue
+            elif "DefibrillatorSync" not in used_methods:
+                print(47)  # DefibrillatorSync
+                used_methods.add("DefibrillatorSync")
+                continue
             else:
-                print(43)
+                print(43)  # DefibrillatorPace
                 continue
 
-        print(48)
+        if vitals["Sats"] and vitals["Sats"] < 88:
+            print(30)  # UseNonRebreatherMask
+            continue
+
+        if vitals["RespRate"] and vitals["RespRate"] < 8:
+            print(29)  # UseBagValveMask
+            continue
+
+        print(48)  # Finish
         return
 
     print(48)
+
 
 if __name__ == "__main__":
     main()
