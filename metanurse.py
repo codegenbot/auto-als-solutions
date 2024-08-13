e87633a78980e038ffe25fd160578a7b00f762a4
beep boop <githubbot@vadim.me>
2024-08-14 01:12:48 +0200

    1227 (0.0). One of the ED nurses has called you to see a patient that she is worried about.
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    What's more, if John's sats fall below 65% or if his mean arterial pressure falls below 20mmHg, he will go into cardiac arrest.
    
    Good luck! 
    DoNothing
    UseSatsProbe
    UseBloodPressureCuff
    ViewMonitor
    MeasuredMAP: 37
    MeasuredSats: 92
    ExamineAirway
    MeasuredMAP: 37
    MeasuredSats: 92
    ExamineBreathing
    MeasuredMAP: 36
    MeasuredSats: 92
    ExamineCirculation
    MeasuredMAP: 35
    MeasuredSats: 92
    ExamineResponse
    MeasuredMAP: 34
    MeasuredSats: 92
    CheckRhythm
    MeasuredMAP: 34
    MeasuredSats: 92
    Finish
    Thanks for playing! Here's how you did:
    
    Sadly, you didn't manage to stabilise John on this occasion.  He is hypotensive.  He has an unstable tachyarrhythmia, which is an indication for cardioversion. 
    
    Don't worry if you're not perfect yet, though. That's exactly what we're here for! 
    
    Final reward: -1.0

--- metanurse.py
+++ metanurse.py
@@ -29,16 +29,6 @@ def stabilize():
             )
         }
 
-        unstable_tachyarrhythmia = events[29] > 0 or events[30] > 0 or events[32] > 0
-        
-        if vitals["MAP"] and vitals["MAP"] < 20:
-            print(17)  # StartChestCompression
-            continue
-        
-        if vitals["Sats"] and vitals["Sats"] < 65:
-            print(22)  # Bag During CPR
-            continue
-        
         if 25 not in actions_taken:
             actions_taken.add(25)
             print(25)  # UseSatsProbe
@@ -79,9 +69,24 @@ def stabilize():
             print(2)  # CheckRhythm
             continue
 
-        if unstable_tachyarrhythmia:
-            print(40)  # DefibrillatorCharge
+        unstable_tachyarrhythmia = events[29] > 0 or events[30] > 0 or events[32] > 0
+
+        if vitals["MAP"] and vitals["MAP"] < 20:
+            print(17)  # StartChestCompression
             continue
+        
+        if vitals["Sats"] and vitals["Sats"] < 65:
+            print(22)  # BagDuringCPR
+            continue
+        
+        if unstable_tachyarrhythmia:
+            if 40 not in actions_taken:
+                actions_taken.add(40)
+                print(40)  # DefibrillatorCharge
+                continue
+            else:
+                print(24)  # UseMonitorPads
+                continue
         
         if vitals["MAP"] and vitals["MAP"] < 60:
             print(15)  # GiveFluids
