153d66f0a75f2372237ab406ed4c8f3c3866b018
beep boop <githubbot@vadim.me>
2024-08-13 14:29:23 +0200

    905 (0.0). One of the ED nurses has called you to see a patient that she is worried about.
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    What's more, if John's sats fall below 65% or if his mean arterial pressure falls below 20mmHg, he will go into cardiac arrest.
    
    Good luck! 
    DoNothing
    ExamineAirway
    John is conscious and talking. The airway seems to be fine. 
    AirwayClear
    UseSatsProbe
    UseBloodPressureCuff
    ViewMonitor
    MeasuredMAP: 37
    MeasuredSats: 92
    Finish
    Thanks for playing! Here's how you did:
    
    Sadly, you didn't manage to stabilise John on this occasion.  He is hypotensive.  He has an unstable tachyarrhythmia, which is an indication for cardioversion. 
    
    Don't worry if you're not perfect yet, though. That's exactly what we're here for! 
    
    Final reward: 0.0

--- metanurse.py
+++ metanurse.py
@@ -2,7 +2,7 @@ import sys
 
 def stabilize():
     max_steps = 350
-    examine_airway = examine_breathing = examine_circulation = examine_disability = False
+    first_examine = False
     use_sats_probe = use_blood_pressure_cuff = view_monitor = False
 
     for step in range(max_steps):
@@ -12,68 +12,58 @@ def stabilize():
             "HeartRate", "RespRate", "CapillaryGlucose", "Temperature", "MAP", "Sats", "Resps"
         ])}
 
-        if not examine_airway:
-            examine_airway = True
-            print(3)  # Examine Airway
+        if step == 0 or not first_examine:
+            first_examine = True
+            print(3)
             continue
         
-        if not examine_breathing:
-            examine_breathing = True
-            print(4)  # Examine Breathing
-            continue
-        
-        if not examine_circulation:
-            examine_circulation = True
-            print(5)  # Examine Circulation
-            continue
-
-        if vitals["Sats"] is None and not use_sats_probe:
-            print(25)  # Use Sats Probe
+        if not use_sats_probe:
+            print(25)
             use_sats_probe = True
             continue
 
-        if vitals["MAP"] is None and not use_blood_pressure_cuff:
-            print(27)  # Use Blood Pressure Cuff
+        if not use_blood_pressure_cuff:
+            print(27)
             use_blood_pressure_cuff = True
             continue
 
         if not view_monitor:
+            print(16)
             view_monitor = True
-            print(16)  # View Monitor
-            continue
-
-        if vitals["Sats"] is not None and vitals["Sats"] < 65:
-            print(17)  # Start Chest Compression
             continue
         
-        if vitals["MAP"] is not None and vitals["MAP"] < 20:
-            print(17)  # Start Chest Compression
-            continue
-
-        if vitals["MAP"] is not None and vitals["MAP"] < 60:
-            print(15)  # Give Fluids
-            continue
-
-        if vitals["Sats"] is not None and vitals["Sats"] < 88:
-            print(30)  # Use Non Rebreather Mask
-            continue
-
-        if vitals["RespRate"] is not None and vitals["RespRate"] < 8:
-            print(29)  # Use Bag Valve Mask
+        if vitals["Sats"] and vitals["Sats"] < 65:
+            print(17)
             continue
         
-        if vitals["HeartRate"] is not None and (vitals["HeartRate"] > 150 or vitals["HeartRate"] < 50):
-            print(43)  # Defibrillator Pace
+        if vitals["MAP"] and vitals["MAP"] < 20:
+            print(17)
+            continue
+        
+        if vitals["MAP"] and vitals["MAP"] < 60:
+            print(15)
+            continue
+        
+        if vitals["Sats"] and vitals["Sats"] < 88:
+            print(30)
             continue
 
-        if all([vitals["Sats"] is not None and vitals["Sats"] >= 88,
-                vitals["RespRate"] is not None and vitals["RespRate"] >= 8,
-                vitals["MAP"] is not None and vitals["MAP"] >= 60]):
-            print(48)  # Finish
+        if vitals["RespRate"] and vitals["RespRate"] < 8:
+            print(29)
+            continue
+        
+        if vitals["HeartRate"] and (vitals["HeartRate"] > 150 or vitals["HeartRate"] < 50):
+            print(40)
+            continue
+
+        if all([vitals["Sats"] and vitals["Sats"] >= 88,
+                vitals["RespRate"] and vitals["RespRate"] >= 8,
+                vitals["MAP"] and vitals["MAP"] >= 60]):
+            print(48)
             return
-
-        print(48)  # Finish
-        return
+    
+    print(48)
+    return
 
 if __name__ == "__main__":
     stabilize()
