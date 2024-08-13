52663dc6c34b836ba07bc91eec30aeecd07e69fd
beep boop <githubbot@vadim.me>
2024-08-14 01:58:04 +0200

    1261 (0.0). One of the ED nurses has called you to see a patient that she is worried about.
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    What's more, if John's sats fall below 65% or if his mean arterial pressure falls below 20mmHg, he will go into cardiac arrest.
    
    Good luck! 
    DoNothing
    UseSatsProbe
    UseBloodPressureCuff
    ViewMonitor
    MeasuredMAP: 38
    MeasuredSats: 92
    ExamineAirway
    MeasuredMAP: 37
    MeasuredSats: 92
    ExamineBreathing
    MeasuredMAP: 36
    MeasuredSats: 92
    ExamineCirculation
    MeasuredMAP: 35
    MeasuredSats: 92
    ExamineResponse
    MeasuredMAP: 35
    MeasuredSats: 92
    CheckRhythm
    MeasuredMAP: 34
    MeasuredSats: 92
    Finish
    Thanks for playing! Here's how you did:
    
    Sadly, you didn't manage to stabilise John on this occasion.  He is hypotensive.  He has an unstable tachyarrhythmia, which is an indication for cardioversion. 
    
    Don't worry if you're not perfect yet, though. That's exactly what we're here for! 
    
    Final reward: -1.0

--- metanurse.py
+++ metanurse.py
@@ -3,7 +3,7 @@ import sys
 def stabilize():
     max_steps = 350
     actions_taken = set()
-
+    
     for step in range(max_steps):
         observations = list(map(float, input().strip().split()))
         events, vital_signs_times, vital_signs_values = (
@@ -28,17 +28,17 @@ def stabilize():
                 ]
             )
         }
-
+        
         if 25 not in actions_taken:
             actions_taken.add(25)
             print(25)
             continue
-
+        
         if 27 not in actions_taken:
             actions_taken.add(27)
             print(27)
             continue
-
+        
         if 16 not in actions_taken:
             actions_taken.add(16)
             print(16)
@@ -48,32 +48,36 @@ def stabilize():
             actions_taken.add(3)
             print(3)
             continue
-
+        
         if 4 not in actions_taken:
             actions_taken.add(4)
             print(4)
             continue
-
+        
         if 5 not in actions_taken:
             actions_taken.add(5)
             print(5)
             continue
-
-        if 8 not in actions_taken:
-            actions_taken.add(8)
-            print(8)
+        
+        if 6 not in actions_taken:
+            actions_taken.add(6)
+            print(6)
             continue
-
+        
+        if 7 not in actions_taken:
+            actions_taken.add(7)
+            print(7)
+            continue
+        
         if vitals["MAP"] and vitals["MAP"] < 20:
             print(17)
             continue
-
+        
         if vitals["Sats"] and vitals["Sats"] < 65:
             print(22)
             continue
         
         unstable_tachyarrhythmia = events[29] > 0 or events[30] > 0 or events[31] > 0
-        
         if unstable_tachyarrhythmia:
             if 28 not in actions_taken:
                 actions_taken.add(28)
@@ -89,11 +93,11 @@ def stabilize():
         if vitals["MAP"] and vitals["MAP"] < 60:
             print(15)
             continue
-
+        
         if vitals["Sats"] and vitals["Sats"] < 88:
             print(30)
             continue
-
+        
         if vitals["RespRate"] and vitals["RespRate"] < 8:
             print(29)
             continue
@@ -107,7 +111,7 @@ def stabilize():
         ):
             print(48)
             return
-
+        
         print(48)
         return
 
