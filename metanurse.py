afbc74ca5b7385bd168a7b156a8d81b7f24c5411
beep boop <githubbot@vadim.me>
2024-08-13 23:32:12 +0200

    1161 (0.0). One of the ED nurses has called you to see a patient that she is worried about.
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    What's more, if John's sats fall below 65% or if his mean arterial pressure falls below 20mmHg, he will go into cardiac arrest.
    
    Good luck! 
    DoNothing
    UseSatsProbe
    UseBloodPressureCuff
    ViewMonitor
    MeasuredMAP: 37
    MeasuredSats: 92
    ExamineCirculation
    MeasuredMAP: 37
    MeasuredSats: 92
    ExamineAirway
    MeasuredMAP: 36
    MeasuredSats: 92
    ExamineBreathing
    MeasuredMAP: 35
    MeasuredSats: 92
    ExamineResponse
    MeasuredMAP: 34
    MeasuredSats: 92
    CheckRhythm
    MeasuredMAP: 34
    MeasuredSats: 92
    Finish
    Thanks for playing! Here's how you did:
    
    Sadly, you didn't manage to stabilise John on this occasion.  He is hypotensive.  He has an unstable tachyarrhythmia, which is an indication for cardioversion. 
    
    Don't worry if you're not perfect yet, though. That's exactly what we're here for! 
    
    Final reward: -1.0

--- metanurse.py
+++ metanurse.py
@@ -9,8 +9,9 @@ def stabilize():
         events, vital_signs_times, vital_signs_values = (
             observations[:33],
             observations[33:40],
-            observations[40:],
+            observations[40:]
         )
+        
         vitals = {
             name: value if time > 0 else None
             for value, time, name in zip(
@@ -23,8 +24,8 @@ def stabilize():
                     "Temperature",
                     "MAP",
                     "Sats",
-                    "Resps",
-                ],
+                    "Resps"
+                ]
             )
         }
 
@@ -32,54 +33,62 @@ def stabilize():
             actions_taken.add(25)
             print(25)  # UseSatsProbe
             continue
+        
         if 27 not in actions_taken:
             actions_taken.add(27)
             print(27)  # UseBloodPressureCuff
             continue
+        
         if 16 not in actions_taken:
             actions_taken.add(16)
             print(16)  # ViewMonitor
             continue
-        if 5 not in actions_taken:
-            actions_taken.add(5)
-            print(5)  # ExamineCirculation
-            continue
+        
         if 3 not in actions_taken:
             actions_taken.add(3)
             print(3)  # ExamineAirway
             continue
+        
         if 4 not in actions_taken:
             actions_taken.add(4)
             print(4)  # ExamineBreathing
             continue
+        
+        if 5 not in actions_taken:
+            actions_taken.add(5)
+            print(5)  # ExamineCirculation
+            continue
+        
         if 8 not in actions_taken:
             actions_taken.add(8)
             print(8)  # ExamineResponse
             continue
+        
         if 2 not in actions_taken:
             actions_taken.add(2)
             print(2)  # CheckRhythm
             continue
-
+        
         if vitals["MAP"] and vitals["MAP"] < 20:
             print(17)  # StartChestCompression
             continue
+        
         if vitals["Sats"] and vitals["Sats"] < 65:
             print(22)  # Bag During CPR
             continue
-        if events[29] > 0 or events[30] > 0:
-            print(24)  # UseMonitorPads
-            continue
+        
         if vitals["MAP"] and vitals["MAP"] < 60:
             print(15)  # GiveFluids
             continue
+        
         if vitals["Sats"] and vitals["Sats"] < 88:
             print(30)  # UseNonRebreatherMask
             continue
+        
         if vitals["RespRate"] and vitals["RespRate"] < 8:
             print(29)  # UseBagValveMask
             continue
-
+        
         if all(
             vital is not None and vital >= threshold
             for vital, threshold in zip(
@@ -89,8 +98,9 @@ def stabilize():
         ):
             print(48)  # Finish
             return
-
-    print(48)  # Finish
+        
+        print(48)  # Finish
+        return
 
 if __name__ == "__main__":
     stabilize()
