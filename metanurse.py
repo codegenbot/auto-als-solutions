0cd24c91f5c0a78a361a62fc2d13cda717665f84
beep boop <githubbot@vadim.me>
2024-08-13 03:12:46 +0200

    335 (0.0). One of the ED nurses has called you to see a patient that she is worried about.
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    What's more, if John's sats fall below 65% or if his mean arterial pressure falls below 20mmHg, he will go into cardiac arrest.
    
    Good luck! 
    DoNothing
    ExamineAirway
    John is conscious and talking. The airway seems to be fine. 
    AirwayClear
    OpenBreathingDrawer
    UseSatsProbe
    ViewMonitor
    MeasuredSats: 92
    Finish
    Thanks for playing! Here's how you did:
    
    Sadly, you didn't manage to stabilise John on this occasion.  He is hypotensive.  He has an unstable tachyarrhythmia, which is an indication for cardioversion. 
    
    Don't worry if you're not perfect yet, though. That's exactly what we're here for! 
    
    Final reward: 0.0

--- metanurse.py
+++ metanurse.py
@@ -32,11 +32,17 @@ def main():
             print(3)
             used_methods.add("ExamineAirway")
             continue
-        
-        if not events[3] and "PerformAirwayManoeuvres" not in used_methods:
-            print(35)
-            used_methods.add("PerformAirwayManoeuvres")
-            continue
+
+        if not events[3]:  # Airway not clear
+            if events[5] or events[6]:  # Vomit or Blood in airway
+                print(31)  # Use yankeur suction catheter
+                continue
+            elif events[8]:  # Tongue obstructing
+                print(36)  # Perform head tilt chin lift
+                continue
+            else:
+                print(35)  # Perform airway manoeuvres
+                continue
 
         if "OpenBreathingDrawer" not in used_methods:
             print(19)
@@ -56,35 +62,15 @@ def main():
         if (vitals["Sats"] and vitals["Sats"] < 65) or (
             vitals["MAP"] and vitals["MAP"] < 20
         ):
-            print(23)
+            print(17)  # Start chest compression
             continue
 
         if vitals["Sats"] and vitals["Sats"] < 88:
-            print(30)
+            print(30)  # Use non-rebreather mask
             continue
 
         if vitals["RespRate"] and vitals["RespRate"] < 8:
-            print(29)
-            continue
-
-        if vitals["MAP"] and vitals["MAP"] < 60:
-            print(15)
-            continue
-
-        if vitals["HeartRate"]:
-            if vitals["HeartRate"] < 50:
-                print(12)
-                continue
-            elif 100 < vitals["HeartRate"] <= 150:
-                print(2)
-                continue
-            elif vitals["HeartRate"] > 150:
-                print(9)
-                continue
-
-        if "ExamineBreathing" not in used_methods:
-            print(4)
-            used_methods.add("ExamineBreathing")
+            print(29)  # Use bag valve mask
             continue
 
         if "ExamineCirculation" not in used_methods:
@@ -92,10 +78,39 @@ def main():
             used_methods.add("ExamineCirculation")
             continue
 
-        print(48)
+        if vitals["MAP"] and vitals["MAP"] < 60:
+            print(15)  # Give fluids
+            continue
+
+        if "ExamineResponse" not in used_methods:
+            print(8)
+            used_methods.add("ExamineResponse")
+            continue
+
+        if "CheckRhythm" not in used_methods:
+            print(2)
+            used_methods.add("CheckRhythm")
+            continue
+
+        if vitals["HeartRate"] is not None:
+            if vitals["HeartRate"] < 50:
+                print(12)  # Give atropine
+                continue
+            elif 100 < vitals["HeartRate"] <= 150:
+                print(2)  # Check rhythm
+                continue
+            elif vitals["HeartRate"] > 150:
+                print(9)  # Give adenosine (assume SVT)
+                continue
+
+        if vitals["HeartRate"] is None and events[18]:  # No radial pulse
+            print(17)  # Start chest compression
+            continue
+
+        print(48)  # Finish if patient is stable
         return
 
-    print(48)
+    print(48)  # Finish after max_steps
 
 if __name__ == "__main__":
     main()
