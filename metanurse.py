4345a56aa0f5301151a0edd52e2a13dd153bdb2a
beep boop <githubbot@vadim.me>
2024-08-13 04:42:55 +0200

    421 (0.0). One of the ED nurses has called you to see a patient that she is worried about.
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    What's more, if John's sats fall below 65% or if his mean arterial pressure falls below 20mmHg, he will go into cardiac arrest.
    
    Good luck! 
    DoNothing
    ExamineAirway
    John is conscious and talking. The airway seems to be fine. 
    AirwayClear
    UseSatsProbe
    UseBloodPressureCuff
    ViewMonitor
    MeasuredMAP: 37
    MeasuredSats: 92
    Finish
    Thanks for playing! Here's how you did:
    
    Sadly, you didn't manage to stabilise John on this occasion.  He is hypotensive.  He has an unstable tachyarrhythmia, which is an indication for cardioversion. 
    
    Don't worry if you're not perfect yet, though. That's exactly what we're here for! 
    
    Final reward: 0.0

--- metanurse.py
+++ metanurse.py
@@ -1,16 +1,17 @@
 import sys
 
+
 def main():
     max_steps = 350
     used_methods = set()
     initial_examine = False
-    
+
     for step in range(max_steps):
         observations = list(map(float, input().strip().split()))
         events, vital_signs_times, vital_signs_values = (
             observations[:33],
             observations[33:40],
-            observations[40:]
+            observations[40:],
         )
         vitals = {
             name: value if time > 0 else None
@@ -25,16 +26,16 @@ def main():
                     "MAP",
                     "Sats",
                     "Resps",
-                ]
+                ],
             )
         }
-        
+
         if not initial_examine:
             print(3)  # ExamineAirway
             initial_examine = True
             continue
-        
-        if not events[3]:  # AirwayClear
+
+        if not events[3]:  # Airway not clear
             print(35)  # PerformAirwayManoeuvres
             continue
 
@@ -53,14 +54,16 @@ def main():
             used_methods.add("ViewMonitor")
             continue
 
-        if (vitals["Sats"] and vitals["Sats"] < 65) or (vitals["MAP"] and vitals["MAP"] < 20):
+        if (vitals["Sats"] and vitals["Sats"] < 65) or (
+            vitals["MAP"] and vitals["MAP"] < 20
+        ):
             print(17)  # StartChestCompression
             continue
-        
+
         if vitals["Sats"] and vitals["Sats"] < 88:
             print(30)  # UseNonRebreatherMask
             continue
-        
+
         if vitals["RespRate"] and vitals["RespRate"] < 8:
             print(29)  # UseBagValveMask
             continue
@@ -69,14 +72,20 @@ def main():
             print(15)  # GiveFluids
             continue
 
-        if (vitals["HeartRate"] and vitals["HeartRate"] > 150):
+        if (
+            (vitals["HeartRate"] and vitals["HeartRate"] > 150)
+            or events[30]
+            or events[31]
+            or events[32]
+        ):
             print(40)  # DefibrillatorCharge
             continue
 
         print(48)  # Finish
         return
-    
+
     print(48)
+
 
 if __name__ == "__main__":
     main()
