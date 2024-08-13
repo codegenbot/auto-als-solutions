f69fae4cf3a0b86ca9b7eabe2c78512e03a259a6
beep boop <githubbot@vadim.me>
2024-08-13 23:39:58 +0200

    1171 (0.0). One of the ED nurses has called you to see a patient that she is worried about.
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    What's more, if John's sats fall below 65% or if his mean arterial pressure falls below 20mmHg, he will go into cardiac arrest.
    
    Good luck! 
    DoNothing
    UseSatsProbe
    UseBloodPressureCuff
    ViewMonitor
    MeasuredMAP: 37
    MeasuredSats: 92
    ExamineAirway
    MeasuredMAP: 37
    MeasuredSats: 92
    ExamineBreathing
    MeasuredMAP: 36
    MeasuredSats: 92
    ExamineCirculation
    MeasuredMAP: 35
    MeasuredSats: 92
    Finish
    Thanks for playing! Here's how you did:
    
    Sadly, you didn't manage to stabilise John on this occasion.  He is hypotensive.  He has an unstable tachyarrhythmia, which is an indication for cardioversion. 
    
    Don't worry if you're not perfect yet, though. That's exactly what we're here for! 
    
    Final reward: -1.0

--- metanurse.py
+++ metanurse.py
@@ -1,102 +1,78 @@
 import sys
 
-
 def stabilize():
     max_steps = 350
+    steps = 0
     actions_taken = set()
-
-    for step in range(max_steps):
+    
+    while steps < max_steps:
         observations = list(map(float, input().strip().split()))
-        events, vital_signs_times, vital_signs_values = (
-            observations[:33],
-            observations[33:40],
-            observations[40:],
-        )
+        events, vital_signs_times, vital_signs_values = observations[:33], observations[33:40], observations[40:]
         vitals = {
             name: value if time > 0 else None
             for value, time, name in zip(
-                vital_signs_values,
-                vital_signs_times,
-                [
-                    "HeartRate",
-                    "RespRate",
-                    "CapillaryGlucose",
-                    "Temperature",
-                    "MAP",
-                    "Sats",
-                    "Resps",
-                ],
+                vital_signs_values, vital_signs_times,
+                ["HeartRate", "RespRate", "CapillaryGlucose", "Temperature", "MAP", "Sats", "Resps"]
             )
         }
 
-        if 25 not in actions_taken:
-            actions_taken.add(25)
-            print(25)
-            continue
-        if 27 not in actions_taken:
-            actions_taken.add(27)
-            print(27)
-            continue
-        if 16 not in actions_taken:
-            actions_taken.add(16)
-            print(16)
-            continue
-        if 5 not in actions_taken:
-            actions_taken.add(5)
-            print(5)
-            continue
-        if 3 not in actions_taken:
-            actions_taken.add(3)
-            print(3)
-            continue
-        if 4 not in actions_taken:
-            actions_taken.add(4)
-            print(4)
-            continue
-        if 8 not in actions_taken:
-            actions_taken.add(8)
-            print(8)
-            continue
-        if 2 not in actions_taken:
-            actions_taken.add(2)
-            print(2)
-            continue
+        actions = [
+            (25, "UseSatsProbe"),
+            (27, "UseBloodPressureCuff"),
+            (16, "ViewMonitor"),
+            (3, "ExamineAirway"),
+            (4, "ExamineBreathing"),
+            (5, "ExamineCirculation"),
+            (6, "ExamineDisability"),
+            (7, "ExamineExposure"),
+            (8, "ExamineResponse"),
+        ]
+        
+        for action, action_name in actions:
+            if action not in actions_taken:
+                actions_taken.add(action)
+                print(action)
+                steps += 1
+                break
+        else:
+            if vitals["Sats"] and vitals["Sats"] < 65:
+                print(22)  # Bag During CPR
+                steps += 1
+                continue
+            if vitals["MAP"] and vitals["MAP"] < 20:
+                print(15)  # GiveFluids
+                steps += 1
+                continue
 
-        if vitals["MAP"] is not None and vitals["MAP"] < 20:
-            print(17)
-            continue
-        if vitals["Sats"] is not None and vitals["Sats"] < 65:
-            print(22)
-            continue
+            if vitals["MAP"] and vitals["MAP"] < 60:
+                if events[29] > 0 or events[30] > 0:  # HeartRhythmSVT or HeartRhythmAF
+                    print(10)  # GiveAdrenaline
+                else:
+                    print(15)  # GiveFluids
+                steps += 1
+                continue
 
-        if vitals["MAP"] is not None and vitals["MAP"] < 60 and 15 not in actions_taken:
-            actions_taken.add(15)
-            print(15)
-            continue
-        if (
-            vitals["Sats"] is not None
-            and vitals["Sats"] < 88
-            and 30 not in actions_taken
-        ):
-            actions_taken.add(30)
-            print(30)
-            continue
-        if vitals["RespRate"] is not None and vitals["RespRate"] < 8:
-            print(29)
-            continue
+            if vitals["Sats"] and vitals["Sats"] < 88:
+                print(30)  # UseNonRebreatherMask
+                steps += 1
+                continue
 
-        if all(
-            vital is not None and vital >= threshold
-            for vital, threshold in zip(
-                [vitals["Sats"], vitals["RespRate"], vitals["MAP"]], [88, 8, 60]
-            )
-        ):
-            print(48)
+            if vitals["RespRate"] and vitals["RespRate"] < 8:
+                print(29)  # UseBagValveMask
+                steps += 1
+                continue
+
+            if all(
+                vital is not None and vital >= threshold
+                for vital, threshold in zip(
+                    [vitals["Sats"], vitals["RespRate"], vitals["MAP"]], [88, 8, 60]
+                )
+            ):
+                print(48)  # Finish
+                return
+            
+            print(48)  # Finish
             return
-
-        print(48)
-        return
-
 
 if __name__ == "__main__":
     stabilize()
