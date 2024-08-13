d6c884aa2dc4539475eea54618f2ed92ae837d83
beep boop <githubbot@vadim.me>
2024-08-13 12:24:59 +0200

    847 (0.0). One of the ED nurses has called you to see a patient that she is worried about.
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    What's more, if John's sats fall below 65% or if his mean arterial pressure falls below 20mmHg, he will go into cardiac arrest.
    
    Good luck! 
    DoNothing
    ExamineAirway
    John is conscious and talking. The airway seems to be fine. 
    AirwayClear
    ExamineBreathing
    John is breathing at a rate of 20 breaths per minute. Chest exapansion is equal. There are bibasal crepitations. 
    
    (Select the finger probe from the breathing drawer and hit "View Monitor" to see sats.)
    BreathingEqualChestExpansion
    BreathingBibasalCrepitations
    MeasuredRespRate: 20
    UseSatsProbe
    UseBloodPressureCuff
    ViewMonitor
    MeasuredMAP: 36
    MeasuredSats: 92
    Finish
    Thanks for playing! Here's how you did:
    
    Sadly, you didn't manage to stabilise John on this occasion.  He is hypotensive.  He has an unstable tachyarrhythmia, which is an indication for cardioversion. 
    
    Don't worry if you're not perfect yet, though. That's exactly what we're here for! 
    
    Final reward: -1.0

--- metanurse.py
+++ metanurse.py
@@ -2,66 +2,92 @@ import sys
 
 def stabilize():
     max_steps = 350
-    examined, use_sats_probe, use_blood_pressure_cuff, view_monitor = False, False, False, False
-    
+
+    use_sats_probe = use_blood_pressure_cuff = view_monitor = False
+    examined_airway = examined_breathing = False
+
     for step in range(max_steps):
         observations = list(map(float, input().strip().split()))
-        events, vital_signs_times, vital_signs_values = observations[:33], observations[33:40], observations[40:]
-        vitals = {name: value if time > 0 else None for value, time, name in zip(vital_signs_values, vital_signs_times, [
-            "HeartRate", "RespRate", "CapillaryGlucose", "Temperature", "MAP", "Sats", "Resps"
-        ])}
+        events, vital_signs_times, vital_signs_values = (
+            observations[:33],
+            observations[33:40],
+            observations[40:],
+        )
+        vitals = {
+            name: value if time > 0 else None
+            for value, time, name in zip(
+                vital_signs_values,
+                vital_signs_times,
+                [
+                    "HeartRate",
+                    "RespRate",
+                    "CapillaryGlucose",
+                    "Temperature",
+                    "MAP",
+                    "Sats",
+                    "Resps",
+                ],
+            )
+        }
 
-        if not examined:
-            print(3)  # ExamineAirway
-            examined = True
+        if not examined_airway:
+            examined_airway = True
+            print(3)
+            continue
+
+        if not examined_breathing:
+            examined_breathing = True
+            print(4)
             continue
 
         if not use_sats_probe:
-            print(25)  # UseSatsProbe
+            print(25)
             use_sats_probe = True
             continue
 
         if not use_blood_pressure_cuff:
-            print(27)  # UseBloodPressureCuff
+            print(27)
             use_blood_pressure_cuff = True
             continue
 
         if not view_monitor:
-            print(16)  # ViewMonitor
+            print(16)
             view_monitor = True
             continue
 
         if vitals["Sats"] is not None and vitals["Sats"] < 65:
-            print(17)  # StartChestCompression
+            print(17)
             continue
 
         if vitals["MAP"] is not None and vitals["MAP"] < 20:
-            print(17)  # StartChestCompression
+            print(17)
             continue
 
         if vitals["Sats"] is not None and vitals["Sats"] < 88:
-            print(30)  # UseNonRebreatherMask
+            print(30)
             continue
 
         if vitals["RespRate"] is not None and vitals["RespRate"] < 8:
-            print(29)  # UseBagValveMask
+            print(29)
             continue
 
         if vitals["MAP"] is not None and vitals["MAP"] < 60:
-            if vitals["HeartRate"] is not None:
-                if vitals["HeartRate"] > 150 or vitals["HeartRate"] < 50:
-                    print(39)  # TurnOnDefibrillator
-                    continue
-            print(15)  # GiveFluids
+            if vitals["HeartRate"] is not None and vitals["HeartRate"] > 150:
+                print(40)
+                continue
+            print(15)
             continue
-        
-        if all(vital is not None and vital >= threshold for vital, threshold in zip(
-                [vitals["Sats"], vitals["RespRate"], vitals["MAP"]],
-                [88, 8, 60])):
-            print(48)  # Finish
+
+        if all(
+            vital is not None and vital >= threshold
+            for vital, threshold in zip(
+                [vitals["Sats"], vitals["RespRate"], vitals["MAP"]], [88, 8, 60]
+            )
+        ):
+            print(48)
             return
 
-        print(48)  # Finish
+        print(48)
         return
 
 if __name__ == "__main__":
