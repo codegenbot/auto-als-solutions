3a7f8c3181db5f43fc26b7942c7942af39090472
beep boop <githubbot@vadim.me>
2024-08-13 22:31:25 +0200

    1134 (0.0). One of the ED nurses has called you to see a patient that she is worried about.
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    What's more, if John's sats fall below 65% or if his mean arterial pressure falls below 20mmHg, he will go into cardiac arrest.
    
    Good luck! 
    DoNothing
    UseSatsProbe
    UseBloodPressureCuff
    ViewMonitor
    MeasuredMAP: 37
    MeasuredSats: 92
    ExamineAirway
    MeasuredMAP: 37
    MeasuredSats: 92
    ExamineBreathing
    MeasuredMAP: 36
    MeasuredSats: 92
    ExamineCirculation
    MeasuredMAP: 35
    MeasuredSats: 92
    ExamineDisability
    MeasuredMAP: 35
    MeasuredSats: 92
    ExamineExposure
    MeasuredMAP: 34
    MeasuredSats: 92
    Finish
    Thanks for playing! Here's how you did:
    
    Sadly, you didn't manage to stabilise John on this occasion.  He is hypotensive.  He has an unstable tachyarrhythmia, which is an indication for cardioversion. 
    
    Don't worry if you're not perfect yet, though. That's exactly what we're here for! 
    
    Final reward: -1.0

--- metanurse.py
+++ metanurse.py
@@ -1,5 +1,6 @@
 import sys
 
+
 def stabilize():
     max_steps = 350
     assessed_sections = set()
@@ -8,72 +9,74 @@ def stabilize():
     for step in range(max_steps):
         observations = list(map(float, input().strip().split()))
         events, vital_signs_times, vital_signs_values = (
-            observations[:33], observations[33:40], observations[40:]
+            observations[:33],
+            observations[33:40],
+            observations[40:],
         )
         vitals = {
             name: value if time > 0 else None
             for value, time, name in zip(
-                vital_signs_values, vital_signs_times,
-                ["HeartRate", "RespRate", "CapillaryGlucose", "Temperature",
-                 "MAP", "Sats", "Resps"]
+                vital_signs_values,
+                vital_signs_times,
+                [
+                    "HeartRate",
+                    "RespRate",
+                    "CapillaryGlucose",
+                    "Temperature",
+                    "MAP",
+                    "Sats",
+                    "Resps",
+                ],
             )
         }
-        
+
         sequence = [
-            (25, "UseSatsProbe"), (27, "UseBloodPressureCuff"), (16, "ViewMonitor"),
-            (3, "ExamineAirway"), (4, "ExamineBreathing"), (5, "ExamineCirculation"), 
-            (6, "ExamineDisability"), (7, "ExamineExposure")
+            (25, "UseSatsProbe"),
+            (27, "UseBloodPressureCuff"),
+            (16, "ViewMonitor"),
+            (3, "ExamineAirway"),
+            (4, "ExamineBreathing"),
+            (5, "ExamineCirculation"),
+            (6, "ExamineDisability"),
+            (7, "ExamineExposure"),
         ]
-        
+
         for action, action_name in sequence:
             if action not in actions_taken:
                 actions_taken.add(action)
                 print(action)
-                continue
+                break
+        else:
+            if vitals["Sats"] is not None and vitals["Sats"] < 65:
+                print(22)
+            elif vitals["MAP"] is not None and vitals["MAP"] < 20:
+                print(15)
+            elif vitals["MAP"] is not None and vitals["MAP"] < 60:
+                if events[29] > 0 or events[30] > 0:
+                    if 39 not in actions_taken:
+                        actions_taken.add(39)
+                        print(39)
+                    elif 40 not in actions_taken:
+                        actions_taken.add(40)
+                        print(40)
+                    else:
+                        print(47)
+                else:
+                    print(15)
+            elif vitals["Sats"] is not None and vitals["Sats"] < 88:
+                print(30)
+            elif vitals["RespRate"] is not None and vitals["RespRate"] < 8:
+                print(29)
+            elif all(
+                vitals[name] is not None and vitals[name] >= threshold
+                for name, threshold in zip(["Sats", "RespRate", "MAP"], [88, 8, 60])
+            ):
+                print(48)
+                return
+            else:
+                print(48)
+                return
 
-        if vitals["Sats"] is not None and vitals["Sats"] < 65:
-            print(22)  # Bag During CPR
-            continue
-
-        if vitals["MAP"] is not None and vitals["MAP"] < 20:
-            print(15)  # Give Fluids
-            continue
-
-        if vitals["MAP"] is not None and vitals["MAP"] < 60:
-            if events[29] > 0 or events[30] > 0:  # SVT or AF
-                if 39 not in actions_taken:
-                    actions_taken.add(39)
-                    print(39)  # TurnOnDefibrillator
-                    continue
-                if 40 not in actions_taken:
-                    actions_taken.add(40)
-                    print(40)  # DefibrillatorCharge
-                    continue
-                print(47)  # DefibrillatorSync
-                continue
-
-            print(15)  # Give Fluids
-            continue
-
-        if vitals["Sats"] is not None and vitals["Sats"] < 88:
-            print(30)  # UseNonRebreatherMask
-            continue
-
-        if vitals["RespRate"] is not None and vitals["RespRate"] < 8:
-            print(29)  # UseBagValveMask
-            continue
-
-        if all(
-            vitals[name] is not None and vitals[name] >= threshold
-            for name, threshold in zip(
-                ["Sats", "RespRate", "MAP"], [88, 8, 60]
-            )
-        ):
-            print(48)  # Finish
-            return
-
-        print(48)  # Finish
-        return
 
 if __name__ == "__main__":
     stabilize()
