81e5a6a900c71a58c8c3046f46297f13f551a0d2
beep boop <githubbot@vadim.me>
2024-08-13 16:12:56 +0200

    951 (0.0). One of the ED nurses has called you to see a patient that she is worried about.
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    What's more, if John's sats fall below 65% or if his mean arterial pressure falls below 20mmHg, he will go into cardiac arrest.
    
    Good luck! 
    DoNothing
    ExamineAirway
    John is conscious and talking. The airway seems to be fine. 
    AirwayClear
    UseSatsProbe
    UseBloodPressureCuff
    ViewMonitor
    MeasuredMAP: 37
    MeasuredSats: 92
    ExamineAirway
    MeasuredMAP: 36
    MeasuredSats: 92
    ExamineBreathing
    MeasuredMAP: 35
    MeasuredSats: 92
    ExamineCirculation
    MeasuredMAP: 34
    MeasuredSats: 92
    Finish
    Thanks for playing! Here's how you did:
    
    Sadly, you didn't manage to stabilise John on this occasion.  He is hypotensive.  He has an unstable tachyarrhythmia, which is an indication for cardioversion. 
    
    Don't worry if you're not perfect yet, though. That's exactly what we're here for! 
    
    Final reward: -1.0

--- metanurse.py
+++ metanurse.py
@@ -2,32 +2,17 @@ import sys
 
 def stabilize():
     max_steps = 350
-    steps_taken = 0
-    
-    # Flags for each examination step
-    examined = {'Airway': False, 'Breathing': False, 'Circulation': False, 'Disability': False, 'Exposure': False}
-    
-    # Initial steps to probe and measure vitals
-    def initial_probing():
-        nonlocal steps_taken
-        steps_list = [25, 27, 16, 3]
-        for step in steps_list:
-            print(step)
-            steps_taken += 1
-            if steps_taken >= max_steps:
-                print(48)  # Finish
-                return True
-        return False
-    
-    if initial_probing():
-        return
 
-    while steps_taken < max_steps:
+    # Status flags
+    first_examine = use_sats_probe = use_blood_pressure_cuff = view_monitor = False
+    airway_examined = breathing_examined = circulation_examined = False
+
+    for step in range(max_steps):
         observations = list(map(float, input().strip().split()))
         events, vital_signs_times, vital_signs_values = (
             observations[:33],
             observations[33:40],
-            observations[40:]
+            observations[40:],
         )
         vitals = {
             name: value if time > 0 else None
@@ -35,78 +20,70 @@ def stabilize():
                 vital_signs_values,
                 vital_signs_times,
                 [
-                    'HeartRate', 'RespRate', 'CapillaryGlucose',
-                    'Temperature', 'MAP', 'Sats', 'Resps'
-                ]
+                    "HeartRate",
+                    "RespRate",
+                    "CapillaryGlucose",
+                    "Temperature",
+                    "MAP",
+                    "Sats",
+                    "Resps",
+                ],
             )
         }
 
-        # Perform ABCDE examinations
-        if not examined['Airway']:
+        if not first_examine:
+            first_examine = True
             print(3)  # ExamineAirway
-            examined['Airway'] = True
-            steps_taken += 1
             continue
-        
-        if not examined['Breathing']:
-            print(4)  # ExamineBreathing
-            examined['Breathing'] = True
-            steps_taken += 1
+
+        if not use_sats_probe:
+            print(25)  # UseSatsProbe
+            use_sats_probe = True
             continue
-        
-        if not examined['Circulation']:
+
+        if not use_blood_pressure_cuff:
+            print(27)  # UseBloodPressureCuff
+            use_blood_pressure_cuff = True
+            continue
+
+        if not view_monitor:
+            print(16)  # ViewMonitor
+            view_monitor = True
+            continue
+
+        if not circulation_examined:
             print(5)  # ExamineCirculation
-            examined['Circulation'] = True
-            steps_taken += 1
+            circulation_examined = True
+            continue 
+        
+        if vitals["Sats"] is not None and vitals["Sats"] < 65:
+            print(17)  # StartChestCompression
+            continue
+
+        if vitals["MAP"] is not None and vitals["MAP"] < 20:
+            print(17)  # StartChestCompression
             continue
         
-        if not examined['Disability']:
-            print(6)  # ExamineDisability
-            examined['Disability'] = True
-            steps_taken += 1
-            continue
-        
-        if not examined['Exposure']:
-            print(7)  # ExamineExposure
-            examined['Exposure'] = True
-            steps_taken += 1
-            continue
-
-        # Treatment steps based on vitals
-        if vitals['Sats'] is not None and vitals['Sats'] < 65:
-            print(17)  # StartChestCompression
-            steps_taken += 1
-            continue
-
-        if vitals['MAP'] is not None and vitals['MAP'] < 20:
-            print(17)  # StartChestCompression
-            steps_taken += 1
-            continue
-
-        if vitals['HeartRate'] is not None and (vitals['HeartRate'] > 150 or vitals['HeartRate'] < 50):
-            print(41)  # DefibrillatorCurrentUp
-            steps_taken += 1
-            continue
-
-        if vitals['MAP'] is not None and vitals['MAP'] < 60:
+        if vitals["MAP"] is not None and vitals["MAP"] < 60:
             print(15)  # GiveFluids
-            steps_taken += 1
             continue
 
-        if vitals['Sats'] is not None and vitals['Sats'] < 88:
+        if vitals["HeartRate"] is not None and (vitals["HeartRate"] > 150 or vitals["HeartRate"] < 50):
+            print(41)  # DefibrillatorCurrentUp
+            continue
+
+        if vitals["Sats"] is not None and vitals["Sats"] < 88:
             print(30)  # UseNonRebreatherMask
-            steps_taken += 1
             continue
 
-        if vitals['RespRate'] is not None and vitals['RespRate'] < 8:
+        if vitals["RespRate"] is not None and vitals["RespRate"] < 8:
             print(29)  # UseBagValveMask
-            steps_taken += 1
             continue
 
         if all(
             vital is not None and vital >= threshold
             for vital, threshold in zip(
-                [vitals['Sats'], vitals['RespRate'], vitals['MAP']], [88, 8, 60]
+                [vitals["Sats"], vitals["RespRate"], vitals["MAP"]], [88, 8, 60]
             )
         ):
             print(48)  # Finish
