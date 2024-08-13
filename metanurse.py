72099f9e2e4308f7fd532525b10c027a963663e9
beep boop <githubbot@vadim.me>
2024-08-13 05:46:33 +0200

    514 (0.0). One of the ED nurses has called you to see a patient that she is worried about.
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    What's more, if John's sats fall below 65% or if his mean arterial pressure falls below 20mmHg, he will go into cardiac arrest.
    
    Good luck! 
    DoNothing
    ExamineAirway
    John is conscious and talking. The airway seems to be fine. 
    AirwayClear
    UseSatsProbe
    UseBloodPressureCuff
    ViewMonitor
    MeasuredMAP: 37
    MeasuredSats: 92
    Finish
    Thanks for playing! Here's how you did:
    
    Sadly, you didn't manage to stabilise John on this occasion.  He is hypotensive.  He has an unstable tachyarrhythmia, which is an indication for cardioversion. 
    
    Don't worry if you're not perfect yet, though. That's exactly what we're here for! 
    
    Final reward: 0.0

--- metanurse.py
+++ metanurse.py
@@ -1,5 +1,6 @@
 import sys
 
+
 def main():
     max_steps = 350
     used_methods = set()
@@ -38,7 +39,7 @@ def main():
         if not events[3]:  # AirwayClear
             print(35)  # PerformAirwayManoeuvres
             continue
-    
+
         if "UseSatsProbe" not in used_methods:
             print(25)  # UseSatsProbe
             used_methods.add("UseSatsProbe")
@@ -54,7 +55,12 @@ def main():
             used_methods.add("ViewMonitor")
             continue
 
-        if vitals["Sats"] and vitals["Sats"] < 65 or vitals["MAP"] and vitals["MAP"] < 20:
+        if (
+            vitals["Sats"]
+            and vitals["Sats"] < 65
+            or vitals["MAP"]
+            and vitals["MAP"] < 20
+        ):
             print(17)  # StartChestCompression
             continue
 
@@ -91,6 +97,7 @@ def main():
         return
 
     print(48)
+
 
 if __name__ == "__main__":
     main()
