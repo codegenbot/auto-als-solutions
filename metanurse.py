d1586940f525ce8b286fa0f08234f4b6bd7d556d
beep boop <githubbot@vadim.me>
2024-08-14 02:00:08 +0200

    1263 (0.0). One of the ED nurses has called you to see a patient that she is worried about.
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    What's more, if John's sats fall below 65% or if his mean arterial pressure falls below 20mmHg, he will go into cardiac arrest.
    
    Good luck! 
    DoNothing
    UseSatsProbe
    UseBloodPressureCuff
    ViewMonitor
    MeasuredMAP: 37
    MeasuredSats: 92
    ExamineAirway
    MeasuredMAP: 37
    MeasuredSats: 92
    ExamineBreathing
    MeasuredMAP: 36
    MeasuredSats: 92
    ExamineCirculation
    MeasuredMAP: 35
    MeasuredSats: 92
    ExamineResponse
    MeasuredMAP: 35
    MeasuredSats: 92
    CheckRhythm
    MeasuredMAP: 34
    MeasuredSats: 92
    Finish
    Thanks for playing! Here's how you did:
    
    Sadly, you didn't manage to stabilise John on this occasion.  He is hypotensive.  He has an unstable tachyarrhythmia, which is an indication for cardioversion. 
    
    Don't worry if you're not perfect yet, though. That's exactly what we're here for! 
    
    Final reward: -1.0

--- metanurse.py
+++ metanurse.py
@@ -28,56 +28,41 @@ def stabilize():
                 ]
             )
         }
-        
+
         if 25 not in actions_taken:
             actions_taken.add(25)
             print(25)
             continue
-        
         if 27 not in actions_taken:
             actions_taken.add(27)
             print(27)
             continue
-        
         if 16 not in actions_taken:
             actions_taken.add(16)
             print(16)
             continue
-        
         if 3 not in actions_taken:
             actions_taken.add(3)
             print(3)
             continue
-        
         if 4 not in actions_taken:
             actions_taken.add(4)
             print(4)
             continue
-        
+        if vitals["Sats"] and vitals["Sats"] < 88:
+            print(30)
+            continue
+        if vitals["RespRate"] and vitals["RespRate"] < 8:
+            print(29)
+            continue
         if 5 not in actions_taken:
             actions_taken.add(5)
             print(5)
             continue
-        
-        if 6 not in actions_taken:
-            actions_taken.add(6)
-            print(6)
-            continue
-        
-        if 7 not in actions_taken:
-            actions_taken.add(7)
-            print(7)
-            continue
-
+        unstable_tachyarrhythmia = events[29] > 0 or events[30] > 0 or events[31] > 0
         if vitals["MAP"] and vitals["MAP"] < 20:
             print(17)
             continue
-        
-        if vitals["Sats"] and vitals["Sats"] < 65:
-            print(22)
-            continue
-        
-        unstable_tachyarrhythmia = events[29] > 0 or events[30] > 0 or events[31] > 0
         if unstable_tachyarrhythmia:
             if 28 not in actions_taken:
                 actions_taken.add(28)
@@ -89,19 +74,18 @@ def stabilize():
                 continue
             print(41)
             continue
-
-        if vitals["MAP"] and vitals["MAP"] < 60:
+        if vitals["MAP"] and vitals["MAP"] < 60 and not unstable_tachyarrhythmia:
             print(15)
             continue
-        
-        if vitals["Sats"] and vitals["Sats"] < 88:
-            print(30)
+        if 6 not in actions_taken:
+            actions_taken.add(6)
+            print(6)
+            continue
+        if 7 not in actions_taken:
+            actions_taken.add(7)
+            print(7)
             continue
         
-        if vitals["RespRate"] and vitals["RespRate"] < 8:
-            print(29)
-            continue
-
         if all(
             vital is not None and vital >= threshold
             for vital, threshold in zip(
