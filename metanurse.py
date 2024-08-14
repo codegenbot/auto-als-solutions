02742d6eb49da3c80fe4afc21d31da1c9d5f1235
beep boop <githubbot@vadim.me>
2024-08-14 17:50:45 +0200

    2021 (0.0). One of the ED nurses has called you to see a patient that she is worried about.
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    What's more, if John's sats fall below 65% or if his mean arterial pressure falls below 20mmHg, he will go into cardiac arrest.
    
    Good luck! 
    DoNothing
    ViewMonitor
    UseSatsProbe
    UseBloodPressureCuff
    MeasuredMAP: 37
    MeasuredSats: 92
    ExamineAirway
    MeasuredMAP: 37
    MeasuredSats: 92
    Finish
    Thanks for playing! Here's how you did:
    
    Sadly, you didn't manage to stabilise John on this occasion.  He is hypotensive.  He has an unstable tachyarrhythmia, which is an indication for cardioversion. 
    
    Don't worry if you're not perfect yet, though. That's exactly what we're here for! 
    
    Final reward: 0.0

--- metanurse.py
+++ metanurse.py
@@ -10,20 +10,18 @@ def stabilize():
 
     required_measurements = {25, 27, 16, 3}  # SATs Probe, BP Cuff, Monitor, Airway
 
+    def need_measurements():
+        return not required_measurements.issubset(actions_taken)
+
+    def need_measurement_action():
+        for action in required_measurements:
+            if action not in actions_taken:
+                return action
+
     for step in range(max_steps):
         observations = list(map(float, input().strip().split()))
-        events = observations[:33]
-        vital_signs_times = observations[33:40]
-        vital_signs_values = observations[40:]
-
-        def need_measurements():
-            return not required_measurements.issubset(actions_taken)
-
-        def need_measurement_action():
-            for action in required_measurements:
-                if action not in actions_taken:
-                    return action
-
+        events, vital_signs_times, vital_signs_values = (observations[:33], observations[33:40], observations[40:])
+        
         vitals = {
             "RespRate": vital_signs_values[1] if vital_signs_times[1] > 0 else None,
             "MAP": vital_signs_values[4] if vital_signs_times[4] > 0 else None,
@@ -39,7 +37,7 @@ def stabilize():
             continue
 
         if vitals["MAP"] is not None and vitals["MAP"] < 60:
-            if events[29] > 0:  # Signs of unstable tachyarrhythmia (SVT)
+            if any(events[i] > 0 for i in [24, 28, 33, 34, 35, 36, 37, 38]):  # Significant arrhythmia
                 take_action(24)  # Use Monitor Pads
             else:
                 take_action(15)  # Give Fluids
@@ -53,25 +51,15 @@ def stabilize():
             take_action(29)  # Use Bag-Valve Mask
             continue
 
-        if events[5] > 0:
+        if any(events[i] > 0 for i in [4, 5]):
             take_action(31)  # Use Yankeur Suction Catheter
             continue
 
-        if events[6] > 0:
-            take_action(36)  # Perform Head-Tilt Chin-Lift
-            continue
-
-        if any(events[i] > 0 for i in [7, 10, 11, 12, 13, 14]):
+        if any(events[i] > 0 for i in [1, 2, 7, 10, 11, 12, 13, 14]):
             take_action(29)  # Use Bag-Valve Mask
             continue
 
-        if all([
-            vitals["MAP"] is not None and vitals["MAP"] >= 60,
-            vitals["Sats"] is not None and vitals["Sats"] >= 88,
-            vitals["RespRate"] is not None and vitals["RespRate"] >= 8
-        ]):
-            take_action(48)  # Finish if stable
-            break
+        take_action(48)  # Finish if stable
 
 if __name__ == "__main__":
     stabilize()
