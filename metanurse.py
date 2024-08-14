789bbacd7dab34a3ff6e20196ebd95ffadf3f8c3
beep boop <githubbot@vadim.me>
2024-08-14 22:52:15 +0200

    2297 (0.0). One of the ED nurses has called you to see a patient that she is worried about.
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    What's more, if John's sats fall below 65% or if his mean arterial pressure falls below 20mmHg, he will go into cardiac arrest.
    
    Good luck! 
    DoNothing
    ViewMonitor
    UseSatsProbe
    UseBloodPressureCuff
    MeasuredMAP: 37
    MeasuredSats: 92
    UseMonitorPads
    MeasuredMAP: 37
    MeasuredSats: 92
    Finish
    Thanks for playing! Here's how you did:
    
    Sadly, you didn't manage to stabilise John on this occasion.  He is hypotensive.  He has an unstable tachyarrhythmia, which is an indication for cardioversion. 
    
    Don't worry if you're not perfect yet, though. That's exactly what we're here for! 
    
    Final reward: 0.0

--- metanurse.py
+++ metanurse.py
@@ -8,7 +8,7 @@ def stabilize():
         print(action)
         actions_taken.add(action)
 
-    required_measurements = {25, 27, 24}
+    required_measurements = {25, 27, 16, 24}
 
     def needs_measurements():
         return not required_measurements.issubset(actions_taken)
@@ -19,7 +19,7 @@ def stabilize():
                 return action
 
     def has_unstable_tachyarrhythmia(events):
-        arrhythmia_events = [31, 32, 33, 34, 35, 36, 37, 38, 39]
+        arrhythmia_events = [31, 32, 33, 34, 35, 36, 37, 38]
         return any(events[i] > 0 for i in arrhythmia_events)
     
     for step in range(max_steps):
@@ -50,11 +50,10 @@ def stabilize():
                     take_action(40)
                 elif 47 not in actions_taken:
                     take_action(47)
-                else:
-                    take_action(48)
+                continue
             else:
                 take_action(15)
-            continue
+                continue
 
         if vitals["Sats"] is not None and vitals["Sats"] < 88:
             take_action(30)
