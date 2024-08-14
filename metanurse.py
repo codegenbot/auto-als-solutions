29f402a4045186eb2ad4c7b0812969b7cc2ac0f0
beep boop <githubbot@vadim.me>
2024-08-14 11:55:52 +0200

    1757 (0.0). One of the ED nurses has called you to see a patient that she is worried about.
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    One of the ED nurses has called you to see a patient that she is worried about.
    
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    One of the ED nurses has called you to see a patient that she is worried about.
    
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    One of the ED nurses has called you to see a patient that she is worried about.
    
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    One of the ED nurses has called you to see a patient that she is worried about.
    
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    What's more, if John's sats fall below 65% or if his mean arterial pressure falls below 20mmHg, he will go into cardiac arrest.
    
    Good luck! 
    DoNothing
    UseSatsProbe
    UseBloodPressureCuff
    ViewMonitor
    MeasuredMAP: 37
    MeasuredSats: 92
    ExamineAirway
    MeasuredMAP: 37
    MeasuredSats: 92
    Finish
    Thanks for playing! Here's how you did:
    
    Sadly, you didn't manage to stabilise John on this occasion.  He is hypotensive.  He has an unstable tachyarrhythmia, which is an indication for cardioversion. 
    
    Don't worry if you're not perfect yet, though. That's exactly what we're here for! 
    
    Final reward: 0.0

--- metanurse.py
+++ metanurse.py
@@ -2,15 +2,45 @@ import sys
 
 def stabilize():
     max_steps = 350
+    actions_taken = set()
+    done = False
 
     def take_action(action):
+        nonlocal done
+        actions_taken.add(action)
         print(action)
         if action == 48:
-            sys.exit()
+            done = True
+
+    def need_measurements():
+        return not all(measured(vital_sign) for vital_sign in [25, 27, 16, 3])
+
+    def measured(vital_sign):
+        return vital_sign in actions_taken
+
+    def need_measurement_action():
+        for action in [25, 27, 16, 3]:
+            if action not in actions_taken:
+                return action
+
+    actions_needed = {
+        'ExamineAirway': 3, 'UseSatsProbe': 25, 'UseBloodPressureCuff': 27, 'ViewMonitor': 16
+    }
 
     for step in range(max_steps):
+        if done:
+            break
+
         observations = list(map(float, input().strip().split()))
-        events, vital_signs_times, vital_signs_values = (observations[:33], observations[33:40], observations[40:])
+        events, vital_signs_times, vital_signs_values = (
+            observations[:33],
+            observations[33:40],
+            observations[40:],
+        )
+
+        if need_measurements():
+            take_action(need_measurement_action())
+            continue
 
         vitals = {
             "HeartRate": vital_signs_values[0] if vital_signs_times[0] > 0 else None,
@@ -19,53 +49,46 @@ def stabilize():
             "Sats": vital_signs_values[5] if vital_signs_times[5] > 0 else None,
         }
 
-        if vitals["MAP"] is None or vitals["Sats"] is None or vitals["RespRate"] is None:
-            if vital_signs_times[5] == 0:
-                take_action(25)
-            elif vital_signs_times[4] == 0:
-                take_action(27)
-            elif vital_signs_times[1] == 0:
-                take_action(16)
-            else:
-                take_action(1)
+        if events[4] > 0 or events[5] > 0:
+            take_action(31)
             continue
 
-        if events[4] > 0 or events[5] > 0 or events[6] > 0:
-            take_action(31 if (events[4] > 0 or events[5] > 0) else 36)
+        if events[6] > 0:
+            take_action(36)
             continue
 
         if events[7] > 0:
             take_action(29)
             continue
 
-        if vitals["MAP"] and vitals["MAP"] < 20:
-            take_action(17)
-            continue
-
-        if vitals["Sats"] and vitals["Sats"] < 65:
-            take_action(22)
-            continue
-
-        if vitals["MAP"] and vitals["MAP"] < 60:
-            take_action(15)
-            continue
-
-        if vitals["Sats"] and vitals["Sats"] < 88:
-            take_action(30)
-            continue
-
-        if vitals["RespRate"] and vitals["RespRate"] < 8:
-            take_action(29)
-            continue
-
         if any(events[i] > 0 for i in range(28, 33)):
-            if 28 not in set(range(3, 33)):
+            if 28 not in actions_taken:
                 take_action(28)
                 continue
             take_action(40)
             continue
 
-        if step == max_steps - 1:
+        if vitals["MAP"] is not None and vitals["MAP"] < 20:
+            take_action(17)
+            continue
+
+        if vitals["Sats"] is not None and vitals["Sats"] < 65:
+            take_action(22)
+            continue
+
+        if vitals["MAP"] is not None and vitals["MAP"] < 60:
+            take_action(15)
+            continue
+
+        if vitals["Sats"] is not None and vitals["Sats"] < 88:
+            take_action(30)
+            continue
+
+        if vitals["RespRate"] is not None and vitals["RespRate"] < 8:
+            take_action(29)
+            continue
+
+        if actions_taken == set(actions_needed.values()):
             take_action(48)
 
 if __name__ == "__main__":
