3e2708239f7c99e578b99b78e8b73adfddcbf6fc
beep boop <githubbot@vadim.me>
2024-08-14 03:19:46 +0200

    1312 (0.0). One of the ED nurses has called you to see a patient that she is worried about.
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    What's more, if John's sats fall below 65% or if his mean arterial pressure falls below 20mmHg, he will go into cardiac arrest.
    
    Good luck! 
    DoNothing
    UseSatsProbe
    UseBloodPressureCuff
    ViewMonitor
    MeasuredMAP: 37
    MeasuredSats: 92
    ExamineAirway
    MeasuredMAP: 37
    MeasuredSats: 92
    ExamineBreathing
    MeasuredMAP: 36
    MeasuredSats: 92
    ExamineCirculation
    MeasuredMAP: 35
    MeasuredSats: 92
    ExamineResponse
    MeasuredMAP: 35
    MeasuredSats: 92
    Finish
    Thanks for playing! Here's how you did:
    
    Sadly, you didn't manage to stabilise John on this occasion.  He is hypotensive.  He has an unstable tachyarrhythmia, which is an indication for cardioversion. 
    
    Don't worry if you're not perfect yet, though. That's exactly what we're here for! 
    
    Final reward: -1.0

--- metanurse.py
+++ metanurse.py
@@ -24,8 +24,8 @@ def stabilize():
                     "Temperature",
                     "MAP",
                     "Sats",
-                    "Resps"
-                ]
+                    "Resps",
+                ],
             )
         }
 
@@ -33,49 +33,49 @@ def stabilize():
             actions_taken.add(action)
             print(action)
 
-        if 25 not in actions_taken:
+        if 25 not in actions_taken:  # UseSatsProbe
             take_action(25)
             continue
-        if 27 not in actions_taken:
+        if 27 not in actions_taken:  # UseBloodPressureCuff
             take_action(27)
             continue
-        if 16 not in actions_taken:
+        if 16 not in actions_taken:  # ViewMonitor
             take_action(16)
             continue
 
-        necessary_examinations = [3, 4, 5, 8]
+        necessary_examinations = [3, 4, 5, 8]  # ExamineAirway, ExamineBreathing, ExamineCirculation, ExamineResponse
         for exam in necessary_examinations:
             if exam not in actions_taken:
                 take_action(exam)
                 continue
 
         unstable_tachyarrhythmia = any(events[i] > 0 for i in (29, 30, 31))
-        
+
         if vitals["MAP"] is not None and vitals["MAP"] < 20:
-            take_action(17)
+            take_action(17)  # StartChestCompression
             continue
 
         if vitals["Sats"] is not None and vitals["Sats"] < 65:
-            take_action(22)
+            take_action(22)  # BagDuringCPR
             continue
 
         if vitals["MAP"] is not None and vitals["MAP"] < 60:
-            take_action(15)
+            take_action(15)  # GiveFluids
             continue
 
         if vitals["Sats"] is not None and vitals["Sats"] < 88:
-            take_action(30)
+            take_action(30)  # UseNonRebreatherMask
             continue
 
         if vitals["RespRate"] is not None and vitals["RespRate"] < 8:
-            take_action(29)
+            take_action(29)  # UseBagValveMask
             continue
 
         if unstable_tachyarrhythmia:
-            if 28 not in actions_taken:
+            if 28 not in actions_taken:  # AttachDefibPads
                 take_action(28)
                 continue
-            take_action(40)
+            take_action(40)  # DefibrillatorCharge
             continue
         
         if all(
@@ -84,10 +84,10 @@ def stabilize():
                 [vitals["Sats"], vitals["RespRate"], vitals["MAP"]], [88, 8, 60]
             )
         ):
-            take_action(48)
+            take_action(48)  # Finish
             return
 
-        take_action(48)
+        take_action(48)  # If unable to stabilize, finish
         return
 
 if __name__ == "__main__":
