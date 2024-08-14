0d2305a4d34903ee9dbc143f7b66b8bb1bbede91
beep boop <githubbot@vadim.me>
2024-08-14 03:29:00 +0200

    1325 (0.0). One of the ED nurses has called you to see a patient that she is worried about.
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    What's more, if John's sats fall below 65% or if his mean arterial pressure falls below 20mmHg, he will go into cardiac arrest.
    
    Good luck! 
    DoNothing
    UseSatsProbe
    UseBloodPressureCuff
    ViewMonitor
    MeasuredMAP: 37
    MeasuredSats: 92
    ExamineAirway
    MeasuredMAP: 37
    MeasuredSats: 92
    Finish
    Thanks for playing! Here's how you did:
    
    Sadly, you didn't manage to stabilise John on this occasion.  He is hypotensive.  He has an unstable tachyarrhythmia, which is an indication for cardioversion. 
    
    Don't worry if you're not perfect yet, though. That's exactly what we're here for! 
    
    Final reward: 0.0

--- metanurse.py
+++ metanurse.py
@@ -3,34 +3,21 @@ import sys
 def stabilize():
     max_steps = 350
     actions_taken = set()
-    
+
     def take_action(action):
         actions_taken.add(action)
         print(action)
         sys.stdout.flush()
-    
+
     for step in range(max_steps):
         observations = list(map(float, input().strip().split()))
-        events, vital_signs_times, vital_signs_values = (
-            observations[:33],
-            observations[33:40],
-            observations[40:]
-        )
+        events, vital_signs_times, vital_signs_values = (observations[:33], observations[33:40], observations[40:])
 
         vitals = {
             name: value if time > 0 else None
             for value, time, name in zip(
-                vital_signs_values,
-                vital_signs_times,
-                [
-                    "HeartRate",
-                    "RespRate",
-                    "CapillaryGlucose",
-                    "Temperature",
-                    "MAP",
-                    "Sats",
-                    "Resps"
-                ]
+                vital_signs_values, vital_signs_times,
+                ["HeartRate", "RespRate", "CapillaryGlucose", "Temperature", "MAP", "Sats", "Resps"]
             )
         }
 
@@ -43,33 +30,25 @@ def stabilize():
         if 16 not in actions_taken:
             take_action(16)
             continue
-        if 3 not in actions_taken:
-            take_action(3)
-            continue
-        if 4 not in actions_taken:
-            take_action(4)
-            continue
-        if 5 not in actions_taken:
-            take_action(5)
-            continue
 
-        if vitals["MAP"] is not None and vitals["MAP"] < 20:
+        examine_actions = [3, 4, 5]
+        for action in examine_actions:
+            if action not in actions_taken:
+                take_action(action)
+                break
+
+        unstable_tachyarrhythmia = any(events[i] > 0 for i in [29, 30, 31])
+
+        if vitals["MAP"] is not None and vitals["MAP"] < 20 or vitals["Sats"] is not None and vitals["Sats"] < 65:
             take_action(17)
             continue
 
-        if vitals["Sats"] is not None and vitals["Sats"] < 65:
-            take_action(22)
-            continue
-        
-        unstable_tachyarrhythmia = events[29] > 0 or events[30] > 0 or events[31] > 0
         if vitals["MAP"] is not None and vitals["MAP"] < 60 and not unstable_tachyarrhythmia:
             take_action(15)
             continue
-        
         if vitals["Sats"] is not None and vitals["Sats"] < 88:
             take_action(30)
             continue
-        
         if vitals["RespRate"] is not None and vitals["RespRate"] < 8:
             take_action(29)
             continue
@@ -91,8 +70,7 @@ def stabilize():
             take_action(48)
             return
 
-        take_action(48)
-        return
+    take_action(48)
 
 if __name__ == "__main__":
     stabilize()
