bb4121747380abb4f84ffc996361a96810ca71a5
beep boop <githubbot@vadim.me>
2024-08-14 08:40:08 +0200

    1580 (0.0). One of the ED nurses has called you to see a patient that she is worried about.
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    What's more, if John's sats fall below 65% or if his mean arterial pressure falls below 20mmHg, he will go into cardiac arrest.
    
    Good luck! 
    DoNothing
    UseSatsProbe
    UseBloodPressureCuff
    ViewMonitor
    MeasuredMAP: 37
    MeasuredSats: 92
    ExamineAirway
    MeasuredMAP: 37
    MeasuredSats: 92
    Finish
    Thanks for playing! Here's how you did:
    
    Sadly, you didn't manage to stabilise John on this occasion.  He is hypotensive.  He has an unstable tachyarrhythmia, which is an indication for cardioversion. 
    
    Don't worry if you're not perfect yet, though. That's exactly what we're here for! 
    
    Final reward: 0.0

--- metanurse.py
+++ metanurse.py
@@ -13,7 +13,7 @@ def stabilize():
             done = True
 
     def need_examination():
-        return 25 not in actions_taken or 27 not in actions_taken or 16 not in actions_taken or 3 not in actions_taken
+        return 25 not in actions_taken or 27 not in actions_taken or 16 not in actions_taken or 3 not in actions_taken or 4 not in actions_taken or 5 not in actions_taken or 6 not in actions_taken or 7 not in actions_taken or 8 not in actions_taken
 
     for step in range(max_steps):
         if done:
@@ -49,7 +49,22 @@ def stabilize():
             if 3 not in actions_taken:
                 take_action(3)
                 continue
-
+            if 4 not in actions_taken:
+                take_action(4)
+                continue
+            if 5 not in actions_taken:
+                take_action(5)
+                continue
+            if 6 not in actions_taken:
+                take_action(6)
+                continue
+            if 7 not in actions_taken:
+                take_action(7)
+                continue
+            if 8 not in actions_taken:
+                take_action(8)
+                continue
+        
         if events[4] > 0 or events[5] > 0:
             take_action(31)
             continue
