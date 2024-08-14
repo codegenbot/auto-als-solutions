1fc3c1e23f72301cc577388f8ebda58afb0d5e3f
beep boop <githubbot@vadim.me>
2024-08-14 23:13:08 +0200

    2319 (0.0). One of the ED nurses has called you to see a patient that she is worried about.
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    What's more, if John's sats fall below 65% or if his mean arterial pressure falls below 20mmHg, he will go into cardiac arrest.
    
    Good luck! 
    DoNothing
    UseMonitorPads
    UseSatsProbe
    UseBloodPressureCuff
    Finish
    Thanks for playing! Here's how you did:
    
    Sadly, you didn't manage to stabilise John on this occasion.  He is hypotensive.  He has an unstable tachyarrhythmia, which is an indication for cardioversion. 
    
    Don't worry if you're not perfect yet, though. That's exactly what we're here for! 
    
    Final reward: 0.0

--- metanurse.py
+++ metanurse.py
@@ -8,7 +8,7 @@ def stabilize():
         print(action)
         actions_taken.add(action)
 
-    required_measurements = {25, 27, 24}
+    required_measurements = {25, 27, 24, 3, 4, 5, 6}
 
     def needs_measurements():
         return not required_measurements.issubset(actions_taken)
@@ -22,21 +22,12 @@ def stabilize():
         arrhythmia_events = [31, 32, 33, 34, 35, 36, 37, 38, 39]
         return any(events[i] > 0 for i in arrhythmia_events)
     
-    def perform_cardioversion():
-        if 24 not in actions_taken:
-            take_action(24)
-        elif 40 not in actions_taken:
-            take_action(40)
-        elif 47 not in actions_taken:
-            take_action(47)
-        else:
-            take_action(48)
-
     for step in range(max_steps):
         observations = list(map(float, input().strip().split()))
         events, vital_signs_times, vital_signs_values = observations[:33], observations[33:40], observations[40:]
 
         vitals = {
+            "HeartRate": vital_signs_values[0] if vital_signs_times[0] > 0 else None,
             "RespRate": vital_signs_values[1] if vital_signs_times[1] > 0 else None,
             "MAP": vital_signs_values[4] if vital_signs_times[4] > 0 else None,
             "Sats": vital_signs_values[5] if vital_signs_times[5] > 0 else None,
@@ -54,7 +45,14 @@ def stabilize():
 
         if vitals["MAP"] is not None and vitals["MAP"] < 60:
             if has_unstable_tachyarrhythmia(events):
-                perform_cardioversion()
+                if 24 not in actions_taken:
+                    take_action(24)
+                elif 40 not in actions_taken:
+                    take_action(40)
+                elif 47 not in actions_taken:
+                    take_action(47)
+                else:
+                    take_action(48)
             else:
                 take_action(15)
             continue
@@ -67,7 +65,7 @@ def stabilize():
             take_action(29)
             continue
 
-        if any(events[i] > 0 for i in [4, 5]):
+        if events[4] > 0 or events[5] > 0:
             take_action(31)
             continue
 
