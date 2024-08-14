f98bd03a44aadc467a2bd3b30f644facc69078d9
beep boop <githubbot@vadim.me>
2024-08-14 21:42:55 +0200

    2227 (0.0). One of the ED nurses has called you to see a patient that she is worried about.
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    One of the ED nurses has called you to see a patient that she is worried about.
    
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    What's more, if John's sats fall below 65% or if his mean arterial pressure falls below 20mmHg, he will go into cardiac arrest.
    
    Good luck! 
    DoNothing
    ViewMonitor
    UseSatsProbe
    UseBloodPressureCuff
    MeasuredMAP: 37
    MeasuredSats: 92
    ExamineAirway
    MeasuredMAP: 37
    MeasuredSats: 92
    Finish
    Thanks for playing! Here's how you did:
    
    Sadly, you didn't manage to stabilise John on this occasion.  He is hypotensive.  He has an unstable tachyarrhythmia, which is an indication for cardioversion. 
    
    Don't worry if you're not perfect yet, though. That's exactly what we're here for! 
    
    Final reward: 0.0

--- metanurse.py
+++ metanurse.py
@@ -8,10 +8,10 @@ def stabilize():
         print(action)
         actions_taken.add(action)
 
-    required_measurements = [25, 27, 16, 3]  # SATs Probe, BP Cuff, View Monitor, Examine Airway
+    required_measurements = {25, 27, 16, 3}  # SATs Probe, BP Cuff, View Monitor, Examine Airway
 
     def needs_measurements():
-        return not set(required_measurements).issubset(actions_taken)
+        return not required_measurements.issubset(actions_taken)
 
     def next_measurement_action():
         for action in required_measurements:
@@ -43,11 +43,9 @@ def stabilize():
         if has_unstable_tachyarrhythmia(events):
             if 24 not in actions_taken:
                 take_action(24)  # Use Monitor Pads
-                continue
-            if 40 not in actions_taken:
+            else:
                 take_action(40)  # Defibrillator Charge
-                continue
-            take_action(48)  # Finish if ready to defibrillate
+            continue
 
         if vitals["MAP"] is not None and vitals["MAP"] < 60:
             take_action(15)  # Give Fluids
@@ -61,20 +59,8 @@ def stabilize():
             take_action(29)  # Use Bag-Valve Mask
             continue
 
-        if any(events[i] > 0 for i in [4, 5]):
+        if any(events[i] > 0 for i in [4, 5, 6]):
             take_action(31)  # Use Yankeur Suction Catheter
-            continue
-
-        if events[6] > 0:
-            take_action(36)  # Perform Head-Tilt Chin-Lift
-            continue
-
-        if any(events[i] > 0 for i in [7, 10, 11, 12, 13, 14]):
-            take_action(29)  # Use Bag-Valve Mask
-            continue
-
-        if any(events[i] > 0 for i in range(1, 4)):
-            take_action(8)  # Examine Response
             continue
 
         take_action(48)  # Finish if stable
