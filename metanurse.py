2ef1ca116ebc1b6dc40ed3fe48035b583c419921
beep boop <githubbot@vadim.me>
2024-08-14 08:50:40 +0200

    1601 (0.0). One of the ED nurses has called you to see a patient that she is worried about.
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    What's more, if John's sats fall below 65% or if his mean arterial pressure falls below 20mmHg, he will go into cardiac arrest.
    
    Good luck! 
    DoNothing
    UseSatsProbe
    UseBloodPressureCuff
    ViewMonitor
    MeasuredMAP: 37
    MeasuredSats: 92
    ExamineAirway
    MeasuredMAP: 37
    MeasuredSats: 92
    Finish
    Thanks for playing! Here's how you did:
    
    Sadly, you didn't manage to stabilise John on this occasion.  He is hypotensive.  He has an unstable tachyarrhythmia, which is an indication for cardioversion. 
    
    Don't worry if you're not perfect yet, though. That's exactly what we're here for! 
    
    Final reward: 0.0

--- metanurse.py
+++ metanurse.py
@@ -50,14 +50,14 @@ def stabilize():
                 take_action(3)
                 continue
 
-        if events[4] > 0 or events[5] > 0 or events[6] > 0:
+        if events[4] > 0 or events[5] > 0:
             take_action(31)
             continue
-        
-        if events[7] > 0:
+
+        if events[6] > 0:
             take_action(36)
             continue
-        
+
         unstable_tachyarrhythmia = (events[29] > 0 or events[30] > 0 or 
                                     events[31] > 0 or events[32] > 0)
         if unstable_tachyarrhythmia:
@@ -66,27 +66,27 @@ def stabilize():
                 continue
             take_action(40)
             continue
-        
+
         if vitals["MAP"] is not None and vitals["MAP"] < 20:
             take_action(17)
             continue
-        
+
         if vitals["Sats"] is not None and vitals["Sats"] < 65:
             take_action(22)
             continue
-        
+
         if vitals["MAP"] is not None and vitals["MAP"] < 60:
             take_action(15)
             continue
-        
+
         if vitals["Sats"] is not None and vitals["Sats"] < 88:
             take_action(30)
             continue
-        
+
         if vitals["RespRate"] is not None and vitals["RespRate"] < 8:
             take_action(29)
             continue
-        
+
         take_action(48)
 
 if __name__ == "__main__":
