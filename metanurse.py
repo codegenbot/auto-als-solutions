d95eeb5417133471b4673182b477a4d53a273b0e
beep boop <githubbot@vadim.me>
2024-08-14 10:10:41 +0200

    1677 (0.0). One of the ED nurses has called you to see a patient that she is worried about.
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    What's more, if John's sats fall below 65% or if his mean arterial pressure falls below 20mmHg, he will go into cardiac arrest.
    
    Good luck! 
    DoNothing
    UseSatsProbe
    UseBloodPressureCuff
    ViewMonitor
    MeasuredMAP: 37
    MeasuredSats: 92
    ExamineAirway
    MeasuredMAP: 37
    MeasuredSats: 92
    Finish
    Thanks for playing! Here's how you did:
    
    Sadly, you didn't manage to stabilise John on this occasion.  He is hypotensive.  He has an unstable tachyarrhythmia, which is an indication for cardioversion. 
    
    Don't worry if you're not perfect yet, though. That's exactly what we're here for! 
    
    Final reward: 0.0

--- metanurse.py
+++ metanurse.py
@@ -5,13 +5,12 @@ def stabilize():
     actions_taken = set()
     done = False
 
-    def take_action(action, break_action=True):
+    def take_action(action):
         nonlocal done
         actions_taken.add(action)
         print(action)
         if action == 48:
             done = True
-        return break_action
 
     def need_measurements():
         return (
@@ -46,8 +45,8 @@ def stabilize():
         if need_measurements():
             measurement_action = need_measurement_action()
             if measurement_action is not None:
-                if take_action(measurement_action):
-                    continue
+                take_action(measurement_action)
+                continue
 
         vitals = {
             "HeartRate": vital_signs_values[0] if vital_signs_times[0] > 0 else None,
@@ -57,45 +56,47 @@ def stabilize():
         }
 
         if events[4] > 0 or events[5] > 0:  # Vomit, Blood in Airway
-            if take_action(31):
-                continue  # UseSuction
+            take_action(31)  # UseSuction
+            continue
 
         if events[6] > 0:  # Tongue Obstruction
-            if take_action(36):
-                continue  # HeadTiltChinLift
+            take_action(36)  # HeadTiltChinLift
+            continue
 
         if events[7] > 0:  # No Breathing
-            if 29 not in actions_taken and take_action(29):
-                continue  # UseBagValveMask
+            if 29 not in actions_taken:
+                take_action(29)  # UseBagValveMask
+            continue
 
-        if any(events[i] > 0 for i in range(28, 33)):  # Unstable Tachyarrhythmia
-            if 28 not in actions_taken and take_action(28):
-                continue  # AttachDefibPads
-            if take_action(40):  # DefibrillatorCharge
+        unstable_tachyarrhythmia = any(events[i] > 0 for i in range(29, 35))
+        if unstable_tachyarrhythmia:
+            if 28 not in actions_taken:
+                take_action(28)  # AttachDefibPads
                 continue
+            take_action(40)  # DefibrillatorCharge
+            continue
 
         if vitals["MAP"] is not None and vitals["MAP"] < 20:
-            if take_action(17):
-                continue  # StartChestCompression
+            take_action(17)  # StartChestCompression
+            continue
 
         if vitals["Sats"] is not None and vitals["Sats"] < 65:
-            if take_action(22):
-                continue  # BagDuringCPR
+            take_action(22)  # BagDuringCPR
+            continue
 
         if vitals["MAP"] is not None and vitals["MAP"] < 60:
-            if take_action(15):
-                continue  # GiveFluids
+            take_action(15)  # GiveFluids
+            continue
 
         if vitals["Sats"] is not None and vitals["Sats"] < 88:
-            if take_action(30):
-                continue  # UseNonRebreatherMask
+            take_action(30)  # UseNonRebreatherMask
+            continue
 
         if vitals["RespRate"] is not None and vitals["RespRate"] < 8:
-            if take_action(29):
-                continue  # UseBagValveMask
+            take_action(29)  # UseBagValveMask
+            continue
 
-        if take_action(48, False):  # Finish
-            break
+        take_action(48)  # Finish
 
 if __name__ == "__main__":
     stabilize()
