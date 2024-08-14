7beba87e1e26693a70c267db54062b43a6bf3867
beep boop <githubbot@vadim.me>
2024-08-14 03:23:31 +0200

    1317 (0.0). One of the ED nurses has called you to see a patient that she is worried about.
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    What's more, if John's sats fall below 65% or if his mean arterial pressure falls below 20mmHg, he will go into cardiac arrest.
    
    Good luck! 
    DoNothing
    UseSatsProbe
    UseBloodPressureCuff
    ViewMonitor
    MeasuredMAP: 37
    MeasuredSats: 92
    ExamineAirway
    MeasuredMAP: 37
    MeasuredSats: 92
    Finish
    Thanks for playing! Here's how you did:
    
    Sadly, you didn't manage to stabilise John on this occasion.  He is hypotensive.  He has an unstable tachyarrhythmia, which is an indication for cardioversion. 
    
    Don't worry if you're not perfect yet, though. That's exactly what we're here for! 
    
    Final reward: 0.0

--- metanurse.py
+++ metanurse.py
@@ -1,15 +1,21 @@
 import sys
 
+
 def stabilize():
     max_steps = 350
     actions_taken = set()
-    
+
+    def take_action(action):
+        actions_taken.add(action)
+        print(action)
+        sys.stdout.flush()
+
     for step in range(max_steps):
         observations = list(map(float, input().strip().split()))
         events, vital_signs_times, vital_signs_values = (
             observations[:33],
             observations[33:40],
-            observations[40:]
+            observations[40:],
         )
 
         vitals = {
@@ -24,14 +30,10 @@ def stabilize():
                     "Temperature",
                     "MAP",
                     "Sats",
-                    "Resps"
-                ]
+                    "Resps",
+                ],
             )
         }
-
-        def take_action(action):
-            actions_taken.add(action)
-            print(action)
 
         if 25 not in actions_taken:
             take_action(25)
@@ -43,26 +45,13 @@ def stabilize():
             take_action(16)
             continue
 
-        if 3 not in actions_taken:
-            take_action(3)
-            continue
-        if 4 not in actions_taken:
-            take_action(4)
-            continue
-        if 5 not in actions_taken:
-            take_action(5)
-            continue
+        necessary_examinations = [3, 4, 5, 8, 2]
+        for exam in necessary_examinations:
+            if exam not in actions_taken:
+                take_action(exam)
+                break
 
         unstable_tachyarrhythmia = events[29] > 0 or events[30] > 0 or events[31] > 0
-
-        if vitals["MAP"] is not None and vitals["MAP"] < 20:
-            take_action(17)
-            continue
-        
-        if vitals["Sats"] is not None and vitals["Sats"] < 65:
-            take_action(22)
-            continue
-
         if unstable_tachyarrhythmia:
             if 28 not in actions_taken:
                 take_action(28)
@@ -70,14 +59,19 @@ def stabilize():
             take_action(40)
             continue
 
-        if vitals["MAP"] is not None and vitals["MAP"] < 60 and not unstable_tachyarrhythmia:
+        if vitals["MAP"] is not None and vitals["MAP"] < 20:
+            take_action(17)
+            continue
+        if vitals["Sats"] is not None and vitals["Sats"] < 65:
+            take_action(22)
+            continue
+
+        if vitals["MAP"] is not None and vitals["MAP"] < 60:
             take_action(15)
             continue
-        
         if vitals["Sats"] is not None and vitals["Sats"] < 88:
             take_action(30)
             continue
-        
         if vitals["RespRate"] is not None and vitals["RespRate"] < 8:
             take_action(29)
             continue
@@ -85,15 +79,12 @@ def stabilize():
         if all(
             vital is not None and vital >= threshold
             for vital, threshold in zip(
-                [vitals["Sats"], vitals["RespRate"], vitals["MAP"]],
-                [88, 8, 60]
+                [vitals["Sats"], vitals["RespRate"], vitals["MAP"]], [88, 8, 60]
             )
         ):
             take_action(48)
             return
-        
-        take_action(48)
-        return
+
 
 if __name__ == "__main__":
     stabilize()
