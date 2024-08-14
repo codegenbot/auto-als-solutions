bb8b1906ca3b34fa9e4948b61bdcae6d34ce272c
beep boop <githubbot@vadim.me>
2024-08-14 06:29:51 +0200

    1426 (0.0). One of the ED nurses has called you to see a patient that she is worried about.
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    What's more, if John's sats fall below 65% or if his mean arterial pressure falls below 20mmHg, he will go into cardiac arrest.
    
    Good luck! 
    DoNothing
    UseSatsProbe
    UseBloodPressureCuff
    ViewMonitor
    MeasuredMAP: 37
    MeasuredSats: 92
    ExamineAirway
    MeasuredMAP: 37
    MeasuredSats: 92
    Finish
    Thanks for playing! Here's how you did:
    
    Sadly, you didn't manage to stabilise John on this occasion.  He is hypotensive.  He has an unstable tachyarrhythmia, which is an indication for cardioversion. 
    
    Don't worry if you're not perfect yet, though. That's exactly what we're here for! 
    
    Final reward: 0.0

--- metanurse.py
+++ metanurse.py
@@ -35,52 +35,52 @@ def stabilize():
         # Examining Airway
         if any(events[idx] > 0 for idx in [3, 4, 5, 6, 7]):
             if events[7] > 0:
-                take_action(29)
+                take_action(29)  # Use Bag Valve Mask if breathing is insufficient
             elif events[4] > 0:
-                take_action(31)
+                take_action(31)  # Use Yankeur Suction Catheter for vomit
             elif events[6] > 0:
-                take_action(36)
+                take_action(36)  # Head Tilt Chin Lift if tongue obstructing
             continue
         
         # Examining Breathing
         if any(events[idx] > 0 for idx in [8, 9, 10, 11, 12, 13, 14]):
             if events[8] > 0:
-                take_action(29)
+                take_action(29)  # Use Bag Valve Mask if no breathing
             elif events[13] > 0:
-                take_action(30)
+                take_action(30)  # Use Non-Rebreather Mask for low oxygen
             continue
         
         # Examining Circulation
         if any(events[idx] > 0 for idx in [15, 16, 17, 18, 19, 30, 31, 32, 33, 34, 35, 36, 37, 38]):
             if vitals["MAP"] is not None and vitals["MAP"] < 60:
-                take_action(15)
+                take_action(15)  # Give Fluids for hypotension
 
             if vitals["MAP"] is not None and vitals["MAP"] < 20:
-                take_action(17)
+                take_action(17)  # Start Chest Compressions
 
             if vitals["Sats"] is not None and vitals["Sats"] < 65:
-                take_action(22)
+                take_action(22)  # Bag During CPR
             continue
 
         if any(events[idx] > 0 for idx in [20, 21, 22, 23, 24]):
-            take_action(6)
+            take_action(6)  # Examine Disability
             continue
 
         if any(events[idx] > 0 for idx in [25, 26, 27, 28]):
-            take_action(7)
+            take_action(7)  # Examine Exposure
             continue
 
         if 25 not in actions_taken:
-            take_action(25)
+            take_action(25)  # Use Sats Probe
             continue
         if 27 not in actions_taken:
-            take_action(27)
+            take_action(27)  # Use Blood Pressure Cuff
             continue
         if 16 not in actions_taken:
-            take_action(16)
+            take_action(16)  # View Monitor
             continue
         if 3 not in actions_taken:
-            take_action(3)
+            take_action(3)  # Examine Airway
             continue
 
         if all(vital is not None and vital >= threshold for vital, threshold in zip(
