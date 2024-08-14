50bf0c80ce78d1052c6a3332281c76c8f308457e
beep boop <githubbot@vadim.me>
2024-08-14 23:20:42 +0200

    2320 (0.0). One of the ED nurses has called you to see a patient that she is worried about.
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    What's more, if John's sats fall below 65% or if his mean arterial pressure falls below 20mmHg, he will go into cardiac arrest.
    
    Good luck! 
    DoNothing
    UseMonitorPads
    UseSatsProbe
    UseBloodPressureCuff
    Finish
    Thanks for playing! Here's how you did:
    
    Sadly, you didn't manage to stabilise John on this occasion.  He is hypotensive.  He has an unstable tachyarrhythmia, which is an indication for cardioversion. 
    
    Don't worry if you're not perfect yet, though. That's exactly what we're here for! 
    
    Final reward: 0.0

--- metanurse.py
+++ metanurse.py
@@ -1,5 +1,6 @@
 import sys
 
+
 def stabilize():
     max_steps = 350
     actions_taken = set()
@@ -12,7 +13,7 @@ def stabilize():
 
     def needs_measurements():
         return not required_measurements.issubset(actions_taken)
-    
+
     def next_measurement_action():
         for action in required_measurements:
             if action not in actions_taken:
@@ -21,10 +22,14 @@ def stabilize():
     def has_unstable_tachyarrhythmia(events):
         arrhythmia_events = [31, 32, 33, 34, 35, 36, 37, 38, 39]
         return any(events[i] > 0 for i in arrhythmia_events)
-    
+
     for step in range(max_steps):
         observations = list(map(float, input().strip().split()))
-        events, vital_signs_times, vital_signs_values = observations[:33], observations[33:40], observations[40:]
+        events, vital_signs_times, vital_signs_values = (
+            observations[:33],
+            observations[33:40],
+            observations[40:],
+        )
 
         vitals = {
             "HeartRate": vital_signs_values[0] if vital_signs_times[0] > 0 else None,
@@ -82,6 +87,7 @@ def stabilize():
             continue
 
         take_action(48)
+
 
 if __name__ == "__main__":
     stabilize()
