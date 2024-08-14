759502626f5b85e74830ca11b9c585652e7927fe
beep boop <githubbot@vadim.me>
2024-08-14 09:05:10 +0200

    1619 (0.0). One of the ED nurses has called you to see a patient that she is worried about.
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    What's more, if John's sats fall below 65% or if his mean arterial pressure falls below 20mmHg, he will go into cardiac arrest.
    
    Good luck! 
    DoNothing
    UseSatsProbe
    UseBloodPressureCuff
    ViewMonitor
    MeasuredMAP: 37
    MeasuredSats: 92
    ExamineAirway
    MeasuredMAP: 37
    MeasuredSats: 92
    Finish
    Thanks for playing! Here's how you did:
    
    Sadly, you didn't manage to stabilise John on this occasion.  He is hypotensive.  He has an unstable tachyarrhythmia, which is an indication for cardioversion. 
    
    Don't worry if you're not perfect yet, though. That's exactly what we're here for! 
    
    Final reward: 0.0

--- metanurse.py
+++ metanurse.py
@@ -14,10 +14,10 @@ def stabilize():
 
     def need_measurements():
         return (
-            25 not in actions_taken or
-            27 not in actions_taken or
-            16 not in actions_taken or
-            3 not in actions_taken
+            25 not in actions_taken
+            or 27 not in actions_taken
+            or 16 not in actions_taken
+            or 3 not in actions_taken
         )
 
     def need_measurement_action():
@@ -36,7 +36,11 @@ def stabilize():
             break
 
         observations = list(map(float, input().strip().split()))
-        events, vital_signs_times, vital_signs_values = observations[:33], observations[33:40], observations[40:]
+        events, vital_signs_times, vital_signs_values = (
+            observations[:33],
+            observations[33:40],
+            observations[40:],
+        )
 
         if need_measurements():
             measurement_action = need_measurement_action()
@@ -52,47 +56,46 @@ def stabilize():
         }
 
         if events[4] > 0 or events[5] > 0:
-            take_action(31)
+            take_action(31)  # Use suction for vomit or blood
             continue
 
         if events[6] > 0:
-            take_action(36)
+            take_action(36)  # Perform head tilt chin lift for tongue obstruction
             continue
 
         if events[7] > 0:
-            take_action(35)
-            continue
-
-        if 28 not in actions_taken:
-            take_action(28)
+            take_action(35)  # Perform airway manoeuvres for no breathing
             continue
 
         unstable_tachyarrhythmia = any(events[i] > 0 for i in range(29, 32))
         if unstable_tachyarrhythmia:
-            take_action(40)
+            if 28 not in actions_taken:
+                take_action(28)  # Attach defib pads
+                continue
+            take_action(40)  # Defibrillator charge
             continue
 
         if vitals["MAP"] is not None and vitals["MAP"] < 20:
-            take_action(17)
+            take_action(17)  # Start chest compression
             continue
 
         if vitals["Sats"] is not None and vitals["Sats"] < 65:
-            take_action(22)
+            take_action(22)  # Bag during CPR
             continue
 
         if vitals["MAP"] is not None and vitals["MAP"] < 60:
-            take_action(15)
+            take_action(15)  # Give fluids
             continue
 
         if vitals["Sats"] is not None and vitals["Sats"] < 88:
-            take_action(30)
+            take_action(30)  # Use nonrebreather mask
             continue
 
         if vitals["RespRate"] is not None and vitals["RespRate"] < 8:
-            take_action(29)
+            take_action(29)  # Use bag-valve mask
             continue
 
-        take_action(48)
-
+        take_action(48)  # Finish
+    
 if __name__ == "__main__":
     stabilize()
