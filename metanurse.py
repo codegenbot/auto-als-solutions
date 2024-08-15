73d94bafce0e19b722548eeb5607cf7845df79f2
beep boop <githubbot@vadim.me>
2024-08-16 00:41:59 +0200

    3051 (0.0). One of the ED nurses has called you to see a patient that she is worried about.
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    What's more, if John's sats fall below 65% or if his mean arterial pressure falls below 20mmHg, he will go into cardiac arrest.
    
    Good luck! 
    DoNothing
    UseBloodPressureCuff
    UseSatsProbe
    ViewMonitor
    MeasuredMAP: 37
    MeasuredSats: 92
    TakeBloodPressure
    MeasuredMAP: 37
    MeasuredSats: 92
    Finish
    Thanks for playing! Here's how you did:
    
    Sadly, you didn't manage to stabilise John on this occasion.  He is hypotensive.  He has an unstable tachyarrhythmia, which is an indication for cardioversion. 
    
    Don't worry if you're not perfect yet, though. That's exactly what we're here for! 
    
    Final reward: 0.0

--- metanurse.py
+++ metanurse.py
@@ -23,11 +23,10 @@ def stabilize():
             "MAP": values[4] if times[4] > 0 else None,
             "Sats": values[5] if times[5] > 0 else None,
         }
-        
-        heart_rhythm_indices = [27, 28, 29, 30, 31, 32]
 
         if (vitals["Sats"] is not None and vitals["Sats"] < 65) or (
-            vitals["MAP"] is not None and vitals["MAP"] < 20):
+            vitals["MAP"] is not None and vitals["MAP"] < 20
+        ):
             take_action(17)
             continue
 
@@ -40,11 +39,14 @@ def stabilize():
             continue
 
         if vitals["MAP"] is not None and vitals["MAP"] < 60:
-            take_action(15)
-            continue
-
-        if any(events[i] > 0 for i in heart_rhythm_indices):
-            take_action(24)
+            if 15 not in actions_taken:
+                actions_taken.add(15)
+                take_action(15)
+                continue
+            if any(events[i] > 0 for i in range(26, 33)):
+                take_action(2)
+                continue
+            take_action(9)
             continue
 
         if 27 not in actions_taken:
