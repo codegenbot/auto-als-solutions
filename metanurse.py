189c87ebdd8f84505013bc5fd7e651a75215d1a9
beep boop <githubbot@vadim.me>
2024-08-15 20:02:33 +0200

    2899 (0.0). One of the ED nurses has called you to see a patient that she is worried about.
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    What's more, if John's sats fall below 65% or if his mean arterial pressure falls below 20mmHg, he will go into cardiac arrest.
    
    Good luck! 
    DoNothing
    UseBloodPressureCuff
    Finish
    Thanks for playing! Here's how you did:
    
    Sadly, you didn't manage to stabilise John on this occasion.  He is hypotensive.  He has an unstable tachyarrhythmia, which is an indication for cardioversion. 
    
    Don't worry if you're not perfect yet, though. That's exactly what we're here for! 
    
    Final reward: 0.0

--- metanurse.py
+++ metanurse.py
@@ -6,15 +6,8 @@ def stabilize():
         sys.stdout.flush()
 
     actions_taken = set()
-    checks = [27, 25]  # UseBloodPressureCuff, UseSatsProbe
-
-    ABCDE_steps = [
-        {"action": 3, "conditions": [3, 4, 5, 6]},  # Airway
-        {"action": 4, "conditions": [7, 8, 9, 10, 11, 12, 13, 14]},  # Breathing
-        {"action": 5, "conditions": [15, 16, 17, 18, 19]},  # Circulation
-        {"action": 6, "conditions": [20, 21, 22, 23, 24, 25]},  # Disability
-        {"action": 7, "conditions": [26, 27, 28, 29, 30, 31, 32]}  # Exposure
-    ]
+    first_checks = [27, 25]  # UseBloodPressureCuff, UseSatsProbe
+    examine_order = [3, 4, 5, 6, 7, 8]
 
     for step in range(350):
         observations = list(map(float, input().strip().split()))
@@ -37,35 +30,67 @@ def stabilize():
             take_action(17)  # StartChestCompression
             continue
 
-        for check in checks:
+        if vitals["Sats"] is not None and vitals["Sats"] < 88:
+            take_action(30)  # UseNonRebreatherMask
+            continue
+
+        if vitals["RR"] is not None and vitals["RR"] < 8:
+            take_action(29)  # UseBagValveMask
+            continue
+
+        if vitals["MAP"] is not None and vitals["MAP"] < 60:
+            take_action(15)  # GiveFluids
+            continue
+
+        for check in first_checks:
             if check not in actions_taken:
                 actions_taken.add(check)
                 take_action(check)
                 break
         else:
-            for step in ABCDE_steps:
-                action = step["action"]
-                conditions = step["conditions"]
-                if any(events[i] > 0 for i in conditions):
-                    take_action(action)
+            for exam in examine_order:
+                if exam not in actions_taken:
+                    actions_taken.add(exam)
+                    take_action(exam)
                     break
 
-            if vitals["Sats"] is not None and vitals["Sats"] < 88:
-                take_action(30)  # UseNonRebreatherMask
-                continue
+        if any(events[i] > 0 for i in range(3, 7)):  # Airway events
+            take_action(3)  # ExamineAirway
+            continue
 
-            if vitals["RR"] is not None and vitals["RR"] < 8:
-                take_action(29)  # UseBagValveMask
-                continue
+        if events[5] > 0:  # AirwayVomit
+            take_action(31)  # UseYankeurSuctionCatheter
+            continue
+        if events[6] > 0:  # AirwayTongue
+            take_action(32)  # UseGuedelAirway
+            continue
 
-            if vitals["MAP"] is not None and vitals["MAP"] < 60:
-                take_action(15)  # GiveFluids
-                continue
+        if any(events[i] > 0 for i in range(7, 15)):  # Breathing events
+            take_action(4)  # ExamineBreathing
+            continue
+        if events[7] > 0:  # BreathingNone
+            take_action(29)  # UseBagValveMask
+            continue
+        if events[14] > 0:  # BreathingPneumothoraxSymptoms
+            take_action(19)  # OpenBreathingDrawer
+            continue
 
-            if vitals["HR"] is not None and vitals["HR"] > 150:
-                take_action(24)  # UseMonitorPads (for synchronized cardioversion)
-                continue
-    
+        if vitals["HR"] is not None and vitals["HR"] > 150:  # Tachyarrhythmia
+            take_action(24)  # UseMonitorPads (for synchronized cardioversion)
+            continue
+
+        if any(events[i] > 0 for i in range(15, 20)):  # Circulation events
+            take_action(5)  # ExamineCirculation
+            continue
+
+        if any(events[i] > 0 for i in range(20, 26)):  # Disability events
+            take_action(6)  # ExamineDisability
+            continue
+
+        if any(events[i] > 0 for i in range(26, 33)):  # Exposure events
+            take_action(7)  # ExamineExposure
+            continue
+
         take_action(48)  # Finish
         break
 
