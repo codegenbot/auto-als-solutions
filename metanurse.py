01169f234f8a346755021700032e629137906966
beep boop <githubbot@vadim.me>
2024-08-15 07:29:33 +0200

    2584 (0.0). One of the ED nurses has called you to see a patient that she is worried about.
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    What's more, if John's sats fall below 65% or if his mean arterial pressure falls below 20mmHg, he will go into cardiac arrest.
    
    Good luck! 
    DoNothing
    UseMonitorPads
    Finish
    Thanks for playing! Here's how you did:
    
    Sadly, you didn't manage to stabilise John on this occasion.  He is hypotensive.  He has an unstable tachyarrhythmia, which is an indication for cardioversion. 
    
    Don't worry if you're not perfect yet, though. That's exactly what we're here for! 
    
    Final reward: 0.0

--- metanurse.py
+++ metanurse.py
@@ -28,21 +28,21 @@ def stabilize():
         if (vitals["MAP"] is not None and vitals["MAP"] < 20) or (
             vitals["Sats"] is not None and vitals["Sats"] < 65
         ):
-            take_action(17)
+            take_action(17)  # Start chest compressions
             continue
 
         if not any(events[3:7]):
-            take_action(3)
+            take_action(3)  # Examine airway
             continue
         elif events[4] > 0:
-            take_action(31)
+            take_action(31)  # Use suction catheter
             continue
         elif events[6] > 0:
-            take_action(32)
+            take_action(32)  # Use guedel airway
             continue
 
         if not any([vitals["Sats"], vitals["RR"], vitals["MAP"]]):
-            for action in [27, 25, 38]:
+            for action in [27, 25, 38]:  # Attach BP cuff, Use Sats probe, Check BP
                 if action not in actions_taken:
                     take_action(action)
                     actions_taken.add(action)
@@ -50,20 +50,20 @@ def stabilize():
             continue
 
         if vitals["Sats"] is not None and vitals["Sats"] < 88:
-            take_action(30)
+            take_action(30)  # Use non-rebreather mask
             continue
 
         if vitals["RR"] is not None and vitals["RR"] < 8:
-            take_action(29)
+            take_action(29)  # Use bag-valve mask
             continue
 
         if vitals["MAP"] is not None and vitals["MAP"] < 60:
-            take_action(15)
+            take_action(15)  # Give fluids
             continue
 
         if vitals["HR"] is not None and (vitals["HR"] < 60 or vitals["HR"] > 100):
-            take_action(24)
-            take_action(43)
+            take_action(24)  # Examine monitor
+            take_action(43)  # Use defibrillator pace
             continue
         
         take_action(48)
