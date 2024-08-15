784a9783ed1034329bfaec1754811e0a4cf114a9
beep boop <githubbot@vadim.me>
2024-08-15 13:55:47 +0200

    2707 (0.0). One of the ED nurses has called you to see a patient that she is worried about.
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    What's more, if John's sats fall below 65% or if his mean arterial pressure falls below 20mmHg, he will go into cardiac arrest.
    
    Good luck! 
    DoNothing
    UseMonitorPads
    UseSatsProbe
    UseBloodPressureCuff
    UseAline
    Finish
    Thanks for playing! Here's how you did:
    
    Sadly, you didn't manage to stabilise John on this occasion.  He is hypotensive.  He has an unstable tachyarrhythmia, which is an indication for cardioversion. 
    
    Don't worry if you're not perfect yet, though. That's exactly what we're here for! 
    
    Final reward: 0.0

--- metanurse.py
+++ metanurse.py
@@ -1,5 +1,6 @@
 import sys
 
+
 def stabilize():
     max_steps = 350
     actions_taken = set()
@@ -24,7 +25,11 @@ def stabilize():
         if len(observations) != 53:
             continue
 
-        events, vital_signs_times, vital_signs_values = observations[:33], observations[33:40], observations[40:]
+        events, vital_signs_times, vital_signs_values = (
+            observations[:33],
+            observations[33:40],
+            observations[40:],
+        )
 
         vitals = {
             "HR": vital_signs_values[0] if vital_signs_times[0] > 0 else None,
@@ -33,31 +38,30 @@ def stabilize():
             "Sats": vital_signs_values[5] if vital_signs_times[5] > 0 else None,
         }
 
-        if (vitals["MAP"] is not None and vitals["MAP"] < 20) or (vitals["Sats"] is not None and vitals["Sats"] < 65):
-            take_action(17)
+        if (vitals["MAP"] is not None and vitals["MAP"] < 20) or (
+            vitals["Sats"] is not None and vitals["Sats"] < 65
+        ):
+            take_action(23 if 23 not in actions_taken else 17)
             continue
 
         if needs_initial_measurements():
             take_action(next_initial_measurement_action())
             continue
 
-        if any(events[i] > 0 for i in range(3, 7)):
-            take_action(3)
-            if events[5] > 0:
-                take_action(31)
-            elif events[6] > 0:
-                take_action(32)
+        if events[3] + events[4] + events[5] + events[6] > 0:
+            take_action(18 if 18 not in actions_taken else 3)
+            continue
+
+        if any(events[i] > 0 for i in range(7, 15)):
+            take_action(19 if 19 not in actions_taken else 4)
             continue
 
         if vitals["Sats"] is not None and vitals["Sats"] < 88:
             take_action(30)
             continue
+
         if vitals["RR"] is not None and vitals["RR"] < 8:
             take_action(29)
-            continue
-
-        if any(events[i] > 0 for i in range(7, 15)):
-            take_action(4)
             continue
 
         if vitals["MAP"] is not None and vitals["MAP"] < 60:
@@ -78,6 +82,7 @@ def stabilize():
 
         take_action(48)
         break
+
 
 if __name__ == "__main__":
     stabilize()
