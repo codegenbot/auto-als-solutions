696719b4a68601bb69fbd5eb90f09a11eea62a6a
beep boop <githubbot@vadim.me>
2024-08-15 12:51:45 +0200

    2677 (0.0). One of the ED nurses has called you to see a patient that she is worried about.
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    What's more, if John's sats fall below 65% or if his mean arterial pressure falls below 20mmHg, he will go into cardiac arrest.
    
    Good luck! 
    DoNothing
    UseMonitorPads
    UseSatsProbe
    UseBloodPressureCuff
    UseAline
    Finish
    Thanks for playing! Here's how you did:
    
    Sadly, you didn't manage to stabilise John on this occasion.  He is hypotensive.  He has an unstable tachyarrhythmia, which is an indication for cardioversion. 
    
    Don't worry if you're not perfect yet, though. That's exactly what we're here for! 
    
    Final reward: 0.0

--- metanurse.py
+++ metanurse.py
@@ -6,6 +6,7 @@ def stabilize():
 
     def take_action(action):
         print(action)
+        sys.stdout.flush()
         actions_taken.add(action)
 
     initial_measurements = [24, 25, 27, 26]
@@ -23,11 +24,7 @@ def stabilize():
         if len(observations) != 53:
             continue
 
-        events, vital_signs_times, vital_signs_values = (
-            observations[:33],
-            observations[33:40],
-            observations[40:],
-        )
+        events, vital_signs_times, vital_signs_values = observations[:33], observations[33:40], observations[40:]
 
         vitals = {
             "HR": vital_signs_values[0] if vital_signs_times[0] > 0 else None,
@@ -36,11 +33,9 @@ def stabilize():
             "Sats": vital_signs_values[5] if vital_signs_times[5] > 0 else None,
         }
 
-        if (vitals["MAP"] is not None and vitals["MAP"] < 20) or (
-            vitals["Sats"] is not None and vitals["Sats"] < 65
-        ):
-            take_action(17) # StartChestCompression
-            take_action(23) # ResumeCPR
+        if (vitals["MAP"] is not None and vitals["MAP"] < 20) or (vitals["Sats"] is not None and vitals["Sats"] < 65):
+            take_action(17)
+            take_action(23)
             continue
 
         if needs_initial_measurements():
@@ -48,39 +43,36 @@ def stabilize():
             continue
 
         if any(events[i] > 0 for i in range(3, 7)):
-            take_action(3) # ExamineAirway
+            take_action(3)
             if events[5] > 0:
-                take_action(31) # UseYankeurSucionCatheter
+                take_action(31)
             if events[6] > 0:
-                take_action(32) # UseGuedelAirway
+                take_action(32)
             continue
 
-        if vitals["Sats"] and vitals["Sats"] < 88:
-            take_action(30) # UseNonRebreatherMask
+        if vitals["Sats"] is not None and vitals["Sats"] < 88:
+            take_action(30)
             continue
-
-        if vitals["RR"] and vitals["RR"] < 8:
-            take_action(29) # UseBagValveMask
+        if vitals["RR"] is not None and vitals["RR"] < 8:
+            take_action(29)
             continue
-
         if any(events[i] > 0 for i in range(7, 15)):
-            take_action(4) # ExamineBreathing
+            take_action(4)
             continue
 
-        if vitals["MAP"] and vitals["MAP"] < 60:
-            take_action(15) # GiveFluids
+        if vitals["MAP"] is not None and vitals["MAP"] < 60:
+            take_action(15)
             continue
-
         if any(events[i] > 0 for i in range(15, 20)):
-            take_action(5) # ExamineCirculation
+            take_action(5)
             continue
 
         if any(events[i] > 0 for i in range(20, 26)):
-            take_action(6) # ExamineDisability
+            take_action(6)
             continue
 
         if any(events[i] > 0 for i in range(26, 33)):
-            take_action(7) # ExamineExposure
+            take_action(7)
             continue
 
         take_action(48)
