64792c7e037fad56f0322663489a4d5b03956a70
beep boop <githubbot@vadim.me>
2024-08-15 22:30:23 +0200

    2972 (0.0). One of the ED nurses has called you to see a patient that she is worried about.
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    What's more, if John's sats fall below 65% or if his mean arterial pressure falls below 20mmHg, he will go into cardiac arrest.
    
    Good luck! 
    DoNothing
    UseBloodPressureCuff
    UseSatsProbe
    ExamineAirway
    John is conscious and talking. The airway seems to be fine. 
    AirwayClear
    ExamineBreathing
    John is breathing at a rate of 20 breaths per minute. Chest exapansion is equal. There are bibasal crepitations. 
    
    (Hit "View Monitor" to see sats.)
    BreathingEqualChestExpansion
    BreathingBibasalCrepitations
    MeasuredRespRate: 20
    ExamineCirculation
    The radial pulse is too weak to palpate. The carotid pulse is palpable. The heart rate is 190bpm. Heart sounds are normal. 
    
    Hit "View Monitor" to see BP.
    
    (You can attach the cardiac monitor and defib pads to see the rhythm if you wish.)
    RadialPulseNonPalpable
    HeartSoundsNormal
    MeasuredHeartRate: 190.4614
    ExamineDisability
    AVPU score = V. Pupils equal and reactive. The capillary glucose is 8.
    
    John cannot remember what regular medication he takes. 
    AVPU_V
    PupilsNormal
    MeasuredCapillaryGlucose: 7.99199
    ExamineExposure
    John appears peripherally shut-down. 
    ExposurePeripherallyShutdown
    MeasuredTemperature: 37
    ExamineResponse
    You: "How you feeling John?"
    
    .John: "On a scale from one to punching someone in the face? A three."
    ResponseVerbal
    Finish
    Thanks for playing! Here's how you did:
    
    Sadly, you didn't manage to stabilise John on this occasion.  He is hypotensive.  He has an unstable tachyarrhythmia, which is an indication for cardioversion. 
    
    Don't worry if you're not perfect yet, though. That's exactly what we're here for! 
    
    Final reward: -1.0

--- metanurse.py
+++ metanurse.py
@@ -33,7 +33,8 @@ def stabilize():
 
         # Critical conditions
         if (vitals["Sats"] is not None and vitals["Sats"] < 65) or (
-            vitals["MAP"] is not None and vitals["MAP"] < 20):
+            vitals["MAP"] is not None and vitals["MAP"] < 20
+        ):
             take_action(17)  # Start Chest Compression
             continue
 
