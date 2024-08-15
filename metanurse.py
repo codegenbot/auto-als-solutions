489fc290c0a25e33f540b302e9d211b0d30bfbec
beep boop <githubbot@vadim.me>
2024-08-15 02:00:16 +0200

    2369 (0.0). One of the ED nurses has called you to see a patient that she is worried about.
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    What's more, if John's sats fall below 65% or if his mean arterial pressure falls below 20mmHg, he will go into cardiac arrest.
    
    Good luck! 
    DoNothing
    ViewMonitor
    UseSatsProbe
    UseBloodPressureCuff
    MeasuredMAP: 37
    MeasuredSats: 92
    Finish
    Thanks for playing! Here's how you did:
    
    Sadly, you didn't manage to stabilise John on this occasion.  He is hypotensive.  He has an unstable tachyarrhythmia, which is an indication for cardioversion. 
    
    Don't worry if you're not perfect yet, though. That's exactly what we're here for! 
    
    Final reward: 0.0

--- metanurse.py
+++ metanurse.py
@@ -1,7 +1,7 @@
 import sys
 
 def stabilize():
-    max_steps = 350
+    max_steps = 350 
     actions_taken = set()
 
     def take_action(action):
@@ -9,7 +9,7 @@ def stabilize():
         sys.stdout.flush()
         actions_taken.add(action)
 
-    required_measurements = {16, 25, 27}
+    required_measurements = {25, 27, 16}
 
     def needs_measurements():
         return not required_measurements.issubset(actions_taken)
