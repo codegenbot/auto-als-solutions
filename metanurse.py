e71f48d16d3878f09bb6589d97493068c42d5b1d
beep boop <githubbot@vadim.me>
2024-08-15 12:15:10 +0200

    2657 (0.0). One of the ED nurses has called you to see a patient that she is worried about.
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    What's more, if John's sats fall below 65% or if his mean arterial pressure falls below 20mmHg, he will go into cardiac arrest.
    
    Good luck! 
    DoNothing
    UseMonitorPads
    UseSatsProbe
    UseBloodPressureCuff
    UseAline
    OpenAirwayDrawer
    OpenBreathingDrawer
    OpenCirculationDrawer
    OpenDrugsDrawer
    PerformJawThrust
    ... this action is not available in current state ...
    Finish
    Thanks for playing! Here's how you did:
    
    Sadly, you didn't manage to stabilise John on this occasion.  He is hypotensive.  He has an unstable tachyarrhythmia, which is an indication for cardioversion. 
    
    Don't worry if you're not perfect yet, though. That's exactly what we're here for! 
    
    Final reward: -2.0

--- metanurse.py
+++ metanurse.py
@@ -1,52 +1,51 @@
 import sys
 
+
 def stabilize():
     max_steps = 350
-    steps_taken = 0
     actions_taken = set()
-    measured = {"HR": False, "RR": False, "MAP": False, "Sats": False}
-    
+
     def take_action(action):
-        nonlocal steps_taken
         print(action)
-        steps_taken += 1
         actions_taken.add(action)
 
-    actions_order = [24, 25, 27, 26, 18, 19, 20, 21, 35, 36, 37]
+    initial_measurements = [24, 25, 27, 26]
 
-    while steps_taken < max_steps:
+    def next_initial_measurement_action():
+        for action in initial_measurements:
+            if action not in actions_taken:
+                return action
+
+    def needs_initial_measurements():
+        return not all(action in actions_taken for action in initial_measurements)
+
+    for step in range(max_steps):
         observations = list(map(float, input().strip().split()))
         if len(observations) != 53:
             continue
 
-        events, vitals_time, vitals_values = observations[:33], observations[33:40], observations[40:]
+        events, vital_signs_times, vital_signs_values = (
+            observations[:33],
+            observations[33:40],
+            observations[40:],
+        )
+
         vitals = {
-            "HR": vitals_values[0] if vitals_time[0] > 0 else None,
-            "RR": vitals_values[1] if vitals_time[1] > 0 else None,
-            "MAP": vitals_values[4] if vitals_time[4] > 0 else None,
-            "Sats": vitals_values[5] if vitals_time[5] > 0 else None,
+            "HR": vital_signs_values[0] if vital_signs_times[0] > 0 else None,
+            "RR": vital_signs_values[1] if vital_signs_times[1] > 0 else None,
+            "MAP": vital_signs_values[4] if vital_signs_times[4] > 0 else None,
+            "Sats": vital_signs_values[5] if vital_signs_times[5] > 0 else None,
         }
 
-        if any(vitals[m] is None for m in measured) and steps_taken < len(actions_order):
-            if actions_order[steps_taken] not in actions_taken:
-                take_action(actions_order[steps_taken])
-            continue
-
-        if (vitals["MAP"] is not None and vitals["MAP"] < 20) or (vitals["Sats"] is not None and vitals["Sats"] < 65):
+        if (vitals["MAP"] is not None and vitals["MAP"] < 20) or (
+            vitals["Sats"] is not None and vitals["Sats"] < 65
+        ):
             take_action(17)
             take_action(23)
             continue
 
-        if vitals["MAP"] and vitals["MAP"] < 60:
-            take_action(15)
-            continue
-
-        if vitals["Sats"] and vitals["Sats"] < 88:
-            take_action(30)
-            continue
-
-        if vitals["RR"] and vitals["RR"] < 8:
-            take_action(29)
+        if needs_initial_measurements():
+            take_action(next_initial_measurement_action())
             continue
 
         if any(events[i] > 0 for i in range(3, 7)):
@@ -57,8 +56,20 @@ def stabilize():
                 take_action(32)
             continue
 
+        if vitals["Sats"] and vitals["Sats"] < 88:
+            take_action(30)
+            continue
+
+        if vitals["RR"] and vitals["RR"] < 8:
+            take_action(29)
+            continue
+
         if any(events[i] > 0 for i in range(7, 15)):
             take_action(4)
+            continue
+
+        if vitals["MAP"] and vitals["MAP"] < 60:
+            take_action(15)
             continue
 
         if any(events[i] > 0 for i in range(15, 20)):
@@ -75,6 +86,7 @@ def stabilize():
 
         take_action(48)
         break
+
 
 if __name__ == "__main__":
     stabilize()
