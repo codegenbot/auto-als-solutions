f18601386fd833100e9aa0158cb9fc835a586420
beep boop <githubbot@vadim.me>
2024-08-16 23:38:21 +0200

    4107 (0.0). One of the ED nurses has called you to see a patient that she is worried about.
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    One of the ED nurses has called you to see a patient that she is worried about.
    
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    What's more, if John's sats fall below 65% or if his mean arterial pressure falls below 20mmHg, he will go into cardiac arrest.
    
    Good luck! 
    DoNothing
    ExamineAirway
    John is conscious and talking. The airway seems to be fine. 
    AirwayClear
    ExamineBreathing
    John is breathing at a rate of 20 breaths per minute. Chest exapansion is equal. There are bibasal crepitations. 
    
    (Select the finger probe from the breathing drawer and hit "View Monitor" to see sats.)
    BreathingEqualChestExpansion
    BreathingBibasalCrepitations
    MeasuredRespRate: 20
    ExamineCirculation
    The radial pulse is too weak to palpate. The carotid pulse is palpable. The heart rate is 190bpm. Heart sounds are normal. 
    
    (To check the BP, choose the pressure cuff or the arterial line from the circulation drawer and hit "View Monitor".)
    RadialPulseNonPalpable
    HeartSoundsNormal
    MeasuredHeartRate: 190.4614
    ViewMonitor
    Finish
    Thanks for playing! Here's how you did:
    
    Sadly, you didn't manage to stabilise John on this occasion.  He is hypotensive.  He has an unstable tachyarrhythmia, which is an indication for cardioversion. 
    
    Don't worry if you're not perfect yet, though. That's exactly what we're here for! 
    
    Final reward: 0.0

--- metanurse.py
+++ metanurse.py
@@ -1,5 +1,6 @@
 import sys
 
+
 def stabilize():
     def take_action(action):
         print(action)
@@ -13,70 +14,97 @@ def stabilize():
             examined.add("Monitor")
             return
         if "SatsProbe" not in examined:
+            examine_drawers("Breathing")
             take_action(25)
             examined.add("SatsProbe")
             return
         if "BP" not in examined:
-            take_action(27)
+            examine_drawers("Circulation")
+            take_action(38)
             examined.add("BP")
             return
 
-    for step in range(350):
+    def examine_drawers(system):
+        if system == "Airway":
+            take_action(18)
+        elif system == "Breathing":
+            take_action(19)
+        elif system == "Circulation":
+            take_action(20)
+        elif system == "Drugs":
+            take_action(21)
+
+    for _ in range(350):
         observations = list(map(float, input().strip().split()))
         if len(observations) != 53:
             take_action(0)
             continue
 
-        events = observations[:33]
-        times = observations[33:40]
-        values = observations[40:]
+        events, times, values = (
+            observations[:33],
+            observations[33:40],
+            observations[40:],
+        )
 
-        vitals = dict()
-        vitals["HR"] = values[0] if times[0] > 0 else None
-        vitals["RR"] = values[1] if times[1] > 0 else None
-        vitals["Glucose"] = values[2] if times[2] > 0 else None
-        vitals["Temp"] = values[3] if times[3] > 0 else None
-        vitals["MAP"] = values[4] if times[4] > 0 else None
-        vitals["Sats"] = values[5] if times[5] > 0 else None
-        vitals["Resps"] = values[6] if times[6] > 0 else None
+        vitals = {
+            "HR": values[0] if times[0] > 0 else None,
+            "RR": values[1] if times[1] > 0 else None,
+            "Glucose": values[2] if times[2] > 0 else None,
+            "Temp": values[3] if times[3] > 0 else None,
+            "MAP": values[4] if times[4] > 0 else None,
+            "Sats": values[5] if times[5] > 0 else None,
+            "Resps": values[6] if times[6] > 0 else None,
+        }
 
-        if (vitals["Sats"] and vitals["Sats"] < 65) or (vitals["MAP"] and vitals["MAP"] < 20):
-            take_action(17)
+        if (vitals["Sats"] and vitals["Sats"] < 65) or (
+            vitals["MAP"] and vitals["MAP"] < 20
+        ):
+            take_action(17)  # StartChestCompression
             continue
 
-        if events[3] == 0 and "Airway" not in examined:
-            take_action(3)
+        if not "Airway" in examined:
+            take_action(3)  # ExamineAirway
             examined.add("Airway")
             continue
-
-        if events[9] == 0 and "Breathing" not in examined:
-            take_action(4)
+        if not "Breathing" in examined:
+            take_action(4)  # ExamineBreathing
             examined.add("Breathing")
+            continue
+        if not "Circulation" in examined:
+            take_action(5)  # ExamineCirculation
+            examined.add("Circulation")
             continue
 
         examine_vitals()
 
         if vitals["MAP"] and vitals["MAP"] < 60:
-            take_action(15)
+            take_action(15)  # GiveFluids
             continue
 
         if vitals["Sats"] and vitals["Sats"] < 88:
-            take_action(30)
+            take_action(30)  # UseNonRebreatherMask
             continue
 
         if vitals["RR"] and vitals["RR"] < 8:
-            take_action(29)
+            take_action(29)  # UseBagValveMask
             continue
 
         if vitals["HR"]:
-            if vitals["HR"] > 150 or vitals["HR"] < 50:
-                take_action(2)
+            if vitals["HR"] > 150:
+                take_action(24)  # UseMonitorPads
+                continue
+            elif vitals["HR"] > 100:
+                take_action(11)  # GiveAmiodarone
+                continue
+            elif vitals["HR"] < 50:
+                take_action(12)  # GiveAtropine
                 continue
 
-        take_action(48)
+        take_action(48)  # Finish
         break
     else:
-        take_action(48)
+        take_action(48)  # Finish after max steps
+
 
 if __name__ == "__main__":
     stabilize()
