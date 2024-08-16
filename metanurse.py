517934f00e082986fe1671eb2a14bd621d73cf77
beep boop <githubbot@vadim.me>
2024-08-16 16:30:38 +0200

    3711 (0.0). One of the ED nurses has called you to see a patient that she is worried about.
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    What's more, if John's sats fall below 65% or if his mean arterial pressure falls below 20mmHg, he will go into cardiac arrest.
    
    Good luck! 
    DoNothing
    ExamineAirway
    John is conscious and talking. The airway seems to be fine. 
    AirwayClear
    UseSatsProbe
    ExamineBreathing
    John is breathing at a rate of 20 breaths per minute. Chest exapansion is equal. There are bibasal crepitations. 
    
    (Hit "View Monitor" to see sats.)
    BreathingEqualChestExpansion
    BreathingBibasalCrepitations
    MeasuredRespRate: 20
    UseBloodPressureCuff
    UseMonitorPads
    Finish
    Thanks for playing! Here's how you did:
    
    Sadly, you didn't manage to stabilise John on this occasion.  He is hypotensive.  He has an unstable tachyarrhythmia, which is an indication for cardioversion. 
    
    Don't worry if you're not perfect yet, though. That's exactly what we're here for! 
    
    Final reward: -1.0

--- metanurse.py
+++ metanurse.py
@@ -6,6 +6,8 @@ def stabilize():
         sys.stdout.flush()
 
     examined = set()
+    Sats_taken, BP_taken = False, False
+
     for step in range(350):
         observations = list(map(float, input().strip().split()))
         if len(observations) != 53:
@@ -32,36 +34,43 @@ def stabilize():
             take_action(17)
             continue
 
-        if "airway" not in examined:
-            take_action(3)
-            examined.add("airway")
-            continue
-        if "Sats" not in examined:
-            take_action(25)
-            examined.add("Sats")
-            continue
-        if "RR" not in examined:
-            take_action(4)
-            examined.add("RR")
-            continue
-        if "BP" not in examined:
-            take_action(27)
-            examined.add("BP")
-            continue
-        if "HR" not in examined:
-            take_action(24)
-            examined.add("HR")
-            continue
-
         if vitals["MAP"] is not None and vitals["MAP"] < 60:
             take_action(15)
             continue
+
         if vitals["Sats"] is not None and vitals["Sats"] < 88:
             take_action(30)
             continue
+
         if vitals["RR"] is not None and vitals["RR"] < 8:
             take_action(29)
             continue
+
+        if not Sats_taken:
+            take_action(25)
+            Sats_taken = True
+            continue
+
+        if not BP_taken:
+            take_action(27)
+            BP_taken = True
+            continue
+
+        if not all(k in examined for k in ["airway", "breathing", "circulation"]):
+            if "airway" not in examined:
+                take_action(3)
+                examined.add("airway")
+                continue
+
+            if vitals["RR"] is None and "RR" not in examined:
+                take_action(4)
+                examined.add("RR")
+                continue
+
+            if vitals["HR"] is None and "HR" not in examined:
+                take_action(24)
+                examined.add("HR")
+                continue
 
         if any(events[i] > 0 for i in range(20, 26)) and "disability" not in examined:
             take_action(6)
