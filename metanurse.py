c9dda0e3047b2d0458396b691ddad3bf1d674e2f
beep boop <githubbot@vadim.me>
2024-08-16 14:23:36 +0200

    3649 (0.0). One of the ED nurses has called you to see a patient that she is worried about.
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    What's more, if John's sats fall below 65% or if his mean arterial pressure falls below 20mmHg, he will go into cardiac arrest.
    
    Good luck! 
    DoNothing
    UseSatsProbe
    ExamineBreathing
    John is breathing at a rate of 20 breaths per minute. Chest exapansion is equal. There are bibasal crepitations. 
    
    (Hit "View Monitor" to see sats.)
    BreathingEqualChestExpansion
    BreathingBibasalCrepitations
    MeasuredRespRate: 20
    UseBloodPressureCuff
    UseMonitorPads
    Finish
    Thanks for playing! Here's how you did:
    
    Sadly, you didn't manage to stabilise John on this occasion.  He is hypotensive.  He has an unstable tachyarrhythmia, which is an indication for cardioversion. 
    
    Don't worry if you're not perfect yet, though. That's exactly what we're here for! 
    
    Final reward: 0.0

--- metanurse.py
+++ metanurse.py
@@ -5,7 +5,7 @@ def stabilize():
         print(action)
         sys.stdout.flush()
 
-    examined_vitals = set()
+    examined = set()
     for step in range(350):
         observations = list(map(float, input().strip().split()))
         if len(observations) != 53:
@@ -15,51 +15,78 @@ def stabilize():
         events = observations[:33]
         times = observations[33:40]
         values = observations[40:]
-        
-        if times[0] == 0: 
-            take_action(24)
-            continue
-        if times[1] == 0: 
-            take_action(4)
-            continue
-        if times[4] == 0: 
-            take_action(27)
-            continue
-        if times[5] == 0: 
-            take_action(25)
-            continue
-        
+
         vitals = {
-            "HR": values[0],
-            "RR": values[1],
-            "Glucose": values[2],
-            "Temp": values[3],
-            "MAP": values[4],
-            "Sats": values[5],
-            "Resps": values[6],
+            "HR": values[0] if times[0] != 0 else None,
+            "RR": values[1] if times[1] != 0 else None,
+            "Glucose": values[2] if times[2] != 0 else None,
+            "Temp": values[3] if times[3] != 0 else None,
+            "MAP": values[4] if times[4] != 0 else None,
+            "Sats": values[5] if times[5] != 0 else None,
+            "Resps": values[6] if times[6] != 0 else None,
         }
 
-        if vitals["Sats"] < 65 or vitals["MAP"] < 20:
+        if (vitals["Sats"] is not None and vitals["Sats"] < 65) or (
+            vitals["MAP"] is not None and vitals["MAP"] < 20
+        ):
             take_action(17)
             continue
 
-        if events[3] == 0 and events[8] == 0:
-            take_action(3)
+        if not all(k in examined for k in ["airway", "breathing", "circulation"]):
+            if any(events[i] > 0 for i in range(3, 7)) and "airway" not in examined:
+                take_action(3)
+                examined.add("airway")
+                continue
+
+            if vitals["Sats"] is None and "Sats" not in examined:
+                take_action(25)
+                examined.add("Sats")
+                continue
+
+            if vitals["RR"] is None and "RR" not in examined:
+                take_action(4)
+                examined.add("RR")
+                continue
+
+            if vitals["MAP"] is None and "BP" not in examined:
+                take_action(27)
+                examined.add("BP")
+                continue
+            elif vitals["MAP"] is None and times[4] > 0:
+                take_action(16)
+                continue
+
+            if vitals["HR"] is None and "HR" not in examined:
+                take_action(24)
+                examined.add("HR")
+                continue
+            elif vitals["HR"] is None and times[0] > 0:
+                take_action(16)
+                continue
+
+        if any(events[i] > 0 for i in range(20, 26)) and "disability" not in examined:
+            take_action(6)
+            examined.add("disability")
             continue
-        
-        if vitals["Sats"] < 88: 
+
+        if any(events[i] > 0 for i in range(26, 33)) and "exposure" not in examined:
+            take_action(7)
+            examined.add("exposure")
+            continue
+
+        if vitals["Sats"] is not None and vitals["Sats"] < 88:
             take_action(30)
             continue
-        
-        if vitals["RR"] < 8:
+
+        if vitals["RR"] is not None and vitals["RR"] < 8:
             take_action(29)
             continue
-        
-        if vitals["MAP"] < 60:
+
+        if vitals["MAP"] is not None and vitals["MAP"] < 60:
             take_action(15)
             continue
-        
-        if vitals["HR"] < 60 or vitals["HR"] > 150:
+
+        if vitals["HR"] is not None and (vitals["HR"] < 60 or vitals["HR"] > 150):
             take_action(9)
             continue
 
