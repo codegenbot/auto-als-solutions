cd5d5a127f8620d362587b371ade524f09325a88
beep boop <githubbot@vadim.me>
2024-08-16 19:10:18 +0200

    3848 (0.0). One of the ED nurses has called you to see a patient that she is worried about.
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    What's more, if John's sats fall below 65% or if his mean arterial pressure falls below 20mmHg, he will go into cardiac arrest.
    
    Good luck! 
    DoNothing
    ExamineAirway
    John is conscious and talking. The airway seems to be fine. 
    AirwayClear
    ViewMonitor
    Finish
    Thanks for playing! Here's how you did:
    
    Sadly, you didn't manage to stabilise John on this occasion.  He is hypotensive.  He has an unstable tachyarrhythmia, which is an indication for cardioversion. 
    
    Don't worry if you're not perfect yet, though. That's exactly what we're here for! 
    
    Final reward: 0.0

--- metanurse.py
+++ metanurse.py
@@ -1,33 +1,34 @@
 import sys
 
+
 def stabilize():
     def take_action(action):
         print(action)
         sys.stdout.flush()
 
     examined = set()
-    
+
     def examine_airway():
         if "airway" not in examined:
             take_action(3)
             examined.add("airway")
             return True
         return False
-    
+
     def examine_breathing():
         if "breathing" not in examined:
             take_action(4)
             examined.add("breathing")
             return True
         return False
-    
+
     def examine_circulation():
         if "circulation" not in examined:
             take_action(5)
             examined.add("circulation")
             return True
         return False
-    
+
     def measure_all_vitals():
         if "Monitor" not in examined:
             take_action(16)
@@ -66,46 +67,47 @@ def stabilize():
             "Sats": values[5] if times[5] != 0 else None,
             "Resps": values[6] if times[6] != 0 else None,
         }
-        
+
         if (vitals["Sats"] is not None and vitals["Sats"] < 65) or (
             vitals["MAP"] is not None and vitals["MAP"] < 20
         ):
-            take_action(17)
+            take_action(17)  # Cardiac arrest, start CPR
             continue
-        
+
         if examine_airway():
             continue
-        
-        if events[3] > 0:
+
+        if events[3] > 0:  # AirwayClear
             examined.add("airway")
 
         if measure_all_vitals():
             continue
-        
+
         if vitals["MAP"] is not None and vitals["MAP"] < 60:
-            take_action(15)
+            take_action(15)  # GiveFluids
             continue
 
         if vitals["Sats"] is not None and vitals["Sats"] < 88:
-            take_action(30)
+            take_action(30)  # UseNonRebreatherMask
             continue
 
         if vitals["RR"] is not None and vitals["RR"] < 8:
-            take_action(29)
+            take_action(29)  # UseBagValveMask
             continue
-        
+
         if examine_breathing() or examine_circulation():
             continue
 
         if any([events[28], events[29], events[30], events[31], events[32]]):
-            take_action(40)
+            take_action(40)  # Prepare for defibrillation
             take_action(41)
             take_action(43)
 
-        take_action(48)
+        take_action(48)  # Finish
         break
     else:
-        take_action(48)
+        take_action(48)  # Finish
+
 
 if __name__ == "__main__":
     stabilize()
