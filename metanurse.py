870c1c1a0cc7efd24aa6bbe0d813dbf955d01bc8
beep boop <githubbot@vadim.me>
2024-08-16 06:39:00 +0200

    3328 (0.0). One of the ED nurses has called you to see a patient that she is worried about.
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    What's more, if John's sats fall below 65% or if his mean arterial pressure falls below 20mmHg, he will go into cardiac arrest.
    
    Good luck! 
    DoNothing
    ViewMonitor
    UseBloodPressureCuff
    MeasuredMAP: 38
    UseSatsProbe
    MeasuredMAP: 37
    MeasuredSats: 92
    ExamineBreathing
    MeasuredMAP: 37
    MeasuredSats: 92
    Finish
    Thanks for playing! Here's how you did:
    
    Sadly, you didn't manage to stabilise John on this occasion.  He is hypotensive.  He has an unstable tachyarrhythmia, which is an indication for cardioversion. 
    
    Don't worry if you're not perfect yet, though. That's exactly what we're here for! 
    
    Final reward: 0.0

--- metanurse.py
+++ metanurse.py
@@ -25,37 +25,38 @@ def stabilize():
         }
 
         if (vitals["Sats"] is not None and vitals["Sats"] < 65) or (
-            vitals["MAP"] is not None and vitals["MAP"] < 20):
-            take_action(17) # Start CPR
+            vitals["MAP"] is not None and vitals["MAP"] < 20
+        ):
+            take_action(17)  # Start CPR
             continue
 
         if "monitor" not in examined_vitals:
-            take_action(16) # View monitor
+            take_action(16)  # View monitor
             examined_vitals.add("monitor")
             continue
 
         if vitals["Sats"] is None and "SatsProbe" not in examined_vitals:
-            take_action(25) # Use sats probe
+            take_action(25)  # Use sats probe
             examined_vitals.add("SatsProbe")
             continue
 
         if vitals["MAP"] is None and "BPCuff" not in examined_vitals:
-            take_action(27) # Use blood pressure cuff
+            take_action(27)  # Use blood pressure cuff
             examined_vitals.add("BPCuff")
             continue
 
         if any(events[i] > 0 for i in range(3, 7)) and "airway" not in examined_vitals:
-            take_action(3) # Examine airway
+            take_action(3)  # Examine airway
             examined_vitals.add("airway")
             continue
 
         if any(events[i] > 0 for i in range(7, 15)) and "breathing" not in examined_vitals:
-            take_action(4) # Examine breathing
+            take_action(4)  # Examine breathing
             examined_vitals.add("breathing")
             continue
 
         if vitals["MAP"] is not None and vitals["MAP"] < 60:
-            take_action(15) # Give fluids
+            take_action(15)  # Give fluids
             continue
 
         if vitals["Sats"] is not None and vitals["Sats"] < 88:
@@ -64,10 +65,10 @@ def stabilize():
             continue
 
         if vitals["RR"] is not None and vitals["RR"] < 8:
-            take_action(29) # Use bag valve mask
+            take_action(29)  # Use bag valve mask
             continue
 
-        take_action(48) # Finish
+        take_action(48)  # Finish
         break
 
 if __name__ == "__main__":
