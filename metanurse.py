fe49dadfbf26f1e9b8edb7db8cc2cf641f5ac06f
beep boop <githubbot@vadim.me>
2024-08-16 03:28:10 +0200

    3201 (0.0). One of the ED nurses has called you to see a patient that she is worried about.
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    What's more, if John's sats fall below 65% or if his mean arterial pressure falls below 20mmHg, he will go into cardiac arrest.
    
    Good luck! 
    DoNothing
    UseBloodPressureCuff
    UseSatsProbe
    ExamineBreathing
    John is breathing at a rate of 20 breaths per minute. Chest exapansion is equal. There are bibasal crepitations. 
    
    (Hit "View Monitor" to see sats.)
    BreathingEqualChestExpansion
    BreathingBibasalCrepitations
    MeasuredRespRate: 20
    ExamineBreathing
    John is breathing at a rate of 20 breaths per minute. Chest exapansion is equal. There are bibasal crepitations. 
    
    (Hit "View Monitor" to see sats.)
    BreathingEqualChestExpansion
    BreathingBibasalCrepitations
    MeasuredRespRate: 20
    Finish
    Thanks for playing! Here's how you did:
    
    Sadly, you didn't manage to stabilise John on this occasion.  He is hypotensive.  He has an unstable tachyarrhythmia, which is an indication for cardioversion. 
    
    Don't worry if you're not perfect yet, though. That's exactly what we're here for! 
    
    Final reward: 0.0

--- metanurse.py
+++ metanurse.py
@@ -6,9 +6,7 @@ def stabilize():
         print(action)
         sys.stdout.flush()
 
-    actions_taken = set()
     examined_vitals = set()
-    has_viewed_monitor = False
 
     for step in range(350):
         observations = list(map(float, input().strip().split()))
@@ -33,50 +31,19 @@ def stabilize():
             take_action(17)
             continue
 
-        if not has_viewed_monitor:
-            take_action(16)
-            has_viewed_monitor = True
+        if vitals["MAP"] is None and 27 not in examined_vitals:
+            take_action(27)
+            examined_vitals.add(27)
             continue
 
-        if 3 not in actions_taken:
-            take_action(3)
-            actions_taken.add(3)
+        if vitals["Sats"] is None and 25 not in examined_vitals:
+            take_action(25)
+            examined_vitals.add(25)
             continue
 
-        if 4 not in actions_taken:
+        if vitals["RR"] is None and 4 not in examined_vitals:
             take_action(4)
-            actions_taken.add(4)
-            continue
-
-        if 5 not in actions_taken:
-            take_action(5)
-            actions_taken.add(5)
-            continue
-
-        if 6 not in actions_taken:
-            take_action(6)
-            actions_taken.add(6)
-            continue
-
-        if 7 not in actions_taken:
-            take_action(7)
-            actions_taken.add(7)
-            continue
-
-        if vitals["MAP"] is None:
-            if 27 not in examined_vitals:
-                take_action(27)
-                examined_vitals.add(27)
-                continue
-
-        if vitals["Sats"] is None:
-            if 25 not in examined_vitals:
-                take_action(25)
-                examined_vitals.add(25)
-                continue
-
-        if vitals["RR"] is None:
-            take_action(4)
+            examined_vitals.add(4)
             continue
 
         if vitals["MAP"] is not None and vitals["MAP"] < 60:
@@ -84,20 +51,36 @@ def stabilize():
             continue
 
         if vitals["Sats"] is not None and vitals["Sats"] < 88:
-            take_action(30 if 30 not in actions_taken else 29)
-            actions_taken.add(30)
+            take_action(30 if 30 not in examined_vitals else 29)
+            examined_vitals.add(30)
             continue
 
         if vitals["RR"] is not None and vitals["RR"] < 8:
             take_action(29)
             continue
 
-        if (
-            any(events[i] > 0 for i in range(26, 33))
-            or vitals["HR"] is not None
-            and (vitals["HR"] < 60 or vitals["HR"] > 100)
-        ):
-            take_action(2)
+        if any(events[i] > 0 for i in range(3, 7)) and 3 not in examined_vitals:
+            take_action(3)
+            examined_vitals.add(3)
+            continue
+
+        if any(events[i] > 0 for i in range(7, 15)) and 4 not in examined_vitals:
+            take_action(4)
+            examined_vitals.add(4)
+            continue
+
+        if any(events[i] > 0 for i in range(15, 20)) and 5 not in examined_vitals:
+            take_action(5)
+            examined_vitals.add(5)
+            continue
+
+        if any(events[i] > 0 for i in range(20, 26)) and 6 not in examined_vitals:
+            take_action(6)
+            examined_vitals.add(6)
+            continue
+
+        if any(events[i] > 0 for i in range(26, 33)):
+            take_action(7)
             continue
 
         take_action(48)
