4ec84f801885def619feced5115509322dd04bb9
beep boop <githubbot@vadim.me>
2024-08-16 03:28:59 +0200

    3203 (0.0). One of the ED nurses has called you to see a patient that she is worried about.
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    What's more, if John's sats fall below 65% or if his mean arterial pressure falls below 20mmHg, he will go into cardiac arrest.
    
    Good luck! 
    DoNothing
    UseBloodPressureCuff
    UseSatsProbe
    Finish
    Thanks for playing! Here's how you did:
    
    Sadly, you didn't manage to stabilise John on this occasion.  He is hypotensive.  He has an unstable tachyarrhythmia, which is an indication for cardioversion. 
    
    Don't worry if you're not perfect yet, though. That's exactly what we're here for! 
    
    Final reward: 0.0

--- metanurse.py
+++ metanurse.py
@@ -5,7 +5,7 @@ def stabilize():
         print(action)
         sys.stdout.flush()
 
-    examined_vitals = set()
+    actions_taken = set()
 
     for step in range(350):
         observations = list(map(float, input().strip().split()))
@@ -16,7 +16,7 @@ def stabilize():
         events = observations[:33]
         times = observations[33:40]
         values = observations[40:]
-
+        
         vitals = {
             "HR": values[0] if times[0] > 0 else None,
             "RR": values[1] if times[1] > 0 else None,
@@ -28,56 +28,46 @@ def stabilize():
             take_action(17)
             continue
 
-        if vitals["MAP"] is None and 27 not in examined_vitals:
+        if 27 not in actions_taken:
             take_action(27)
-            examined_vitals.add(27)
+            actions_taken.add(27)
             continue
-
-        if vitals["Sats"] is None and 25 not in examined_vitals:
+        
+        if 25 not in actions_taken:
             take_action(25)
-            examined_vitals.add(25)
-            continue
-
-        if vitals["RR"] is None and 4 not in examined_vitals:
-            take_action(4)
-            examined_vitals.add(4)
+            actions_taken.add(25)
             continue
 
         if vitals["MAP"] is not None and vitals["MAP"] < 60:
             take_action(15)
             continue
-
         if vitals["Sats"] is not None and vitals["Sats"] < 88:
-            take_action(30 if 30 not in examined_vitals else 29)
-            examined_vitals.add(30)
+            take_action(29 if 30 in actions_taken else 30)
+            actions_taken.add(30)
             continue
-
         if vitals["RR"] is not None and vitals["RR"] < 8:
             take_action(29)
             continue
-
-        if any(events[i] > 0 for i in range(3, 7)) and 3 not in examined_vitals:
+        
+        if 3 not in actions_taken:
             take_action(3)
-            examined_vitals.add(3)
+            actions_taken.add(3)
             continue
-
-        if any(events[i] > 0 for i in range(7, 15)) and 4 not in examined_vitals:
+        if 4 not in actions_taken:
             take_action(4)
-            examined_vitals.add(4)
+            actions_taken.add(4)
             continue
-
-        if any(events[i] > 0 for i in range(15, 20)) and 5 not in examined_vitals:
+        if 5 not in actions_taken:
             take_action(5)
-            examined_vitals.add(5)
+            actions_taken.add(5)
             continue
-
-        if any(events[i] > 0 for i in range(20, 26)) and 6 not in examined_vitals:
+        if 6 not in actions_taken:
             take_action(6)
-            examined_vitals.add(6)
+            actions_taken.add(6)
             continue
-
-        if any(events[i] > 0 for i in range(26, 33)):
+        if 7 not in actions_taken:
             take_action(7)
+            actions_taken.add(7)
             continue
 
         take_action(48)
