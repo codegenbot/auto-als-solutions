a84da9b158dbc79e421cabd54535c4b29dfd8e1d
beep boop <githubbot@vadim.me>
2024-08-16 03:02:32 +0200

    3189 (0.0). One of the ED nurses has called you to see a patient that she is worried about.
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    What's more, if John's sats fall below 65% or if his mean arterial pressure falls below 20mmHg, he will go into cardiac arrest.
    
    Good luck! 
    DoNothing
    UseBloodPressureCuff
    UseSatsProbe
    ViewMonitor
    MeasuredMAP: 37
    MeasuredSats: 92
    TakeBloodPressure
    MeasuredMAP: 37
    MeasuredSats: 92
    Finish
    Thanks for playing! Here's how you did:
    
    Sadly, you didn't manage to stabilise John on this occasion.  He is hypotensive.  He has an unstable tachyarrhythmia, which is an indication for cardioversion. 
    
    Don't worry if you're not perfect yet, though. That's exactly what we're here for! 
    
    Final reward: 0.0

--- metanurse.py
+++ metanurse.py
@@ -7,8 +7,7 @@ def stabilize():
 
     actions_taken = set()
     given_fluids = False
-    checked_monitor = False
-
+    
     for step in range(350):
         observations = list(map(float, input().strip().split()))
         if len(observations) != 53:
@@ -30,14 +29,13 @@ def stabilize():
             take_action(17)
             continue
 
-        if any(events[i] > 0 for i in range(3, 7)):
-            if 3 not in actions_taken:
-                take_action(3)
-                actions_taken.add(3)
-                continue
-            if events[7] > 0:
-                take_action(29)
-                continue
+        if vitals["MAP"] is not None and vitals["MAP"] < 60:
+            if not given_fluids:
+                take_action(15)
+                given_fluids = True
+            else:
+                take_action(16)
+            continue
 
         if vitals["Sats"] is not None and vitals["Sats"] < 88:
             if 25 not in actions_taken:
@@ -50,57 +48,54 @@ def stabilize():
                 continue
             take_action(29)
             continue
+
         if vitals["RR"] is not None and vitals["RR"] < 8:
             take_action(29)
             continue
 
-        if vitals["MAP"] is not None and vitals["MAP"] < 60:
-            if not given_fluids:
-                take_action(15)
-                given_fluids = True
-                continue
-            if not checked_monitor:
-                take_action(16)
-                checked_monitor = True
-                continue
-
-        if events[21] > 0:
-            take_action(6)
-            continue
-        if events[22] > 0:
-            take_action(6)
+        if any(events[i] > 0 for i in range(3, 7)) and 3 not in actions_taken:
+            take_action(3)
+            actions_taken.add(3)
             continue
 
-        if any(events[i] > 0 for i in range(26, 33)):
-            take_action(7)
-            continue
-
-        if 1 not in actions_taken:
-            take_action(1)
-            actions_taken.add(1)
-            continue
-        if 2 not in actions_taken:
-            take_action(2)
-            actions_taken.add(2)
-            continue
         if 27 not in actions_taken:
             take_action(27)
             actions_taken.add(27)
             continue
+
         if 25 not in actions_taken:
             take_action(25)
             actions_taken.add(25)
             continue
+
         if 16 not in actions_taken:
             take_action(16)
             actions_taken.add(16)
             continue
+
         if 38 not in actions_taken:
             take_action(38)
             actions_taken.add(38)
             continue
+        
+        if any(events[i] > 0 for i in range(3, 7)):
+            take_action(3)
+            continue
+        if any(events[i] > 0 for i in range(7, 15)):
+            take_action(4)
+            continue
+        if any(events[i] > 0 for i in range(15, 20)):
+            take_action(5)
+            continue
+        if any(events[i] > 0 for i in range(20, 26)):
+            take_action(6)
+            continue
+        if any(events[i] > 0 for i in range(26, 33)):
+            take_action(7)
+            continue
 
-        take_action(0)
+        take_action(48)
+        break
 
 if __name__ == "__main__":
     stabilize()
