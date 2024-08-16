36bd7ca32ee0944ad476d5b9138cd9dc79033951
beep boop <githubbot@vadim.me>
2024-08-16 21:18:58 +0200

    3949 (0.0). One of the ED nurses has called you to see a patient that she is worried about.
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    What's more, if John's sats fall below 65% or if his mean arterial pressure falls below 20mmHg, he will go into cardiac arrest.
    
    Good luck! 
    DoNothing
    ExamineAirway
    John is conscious and talking. The airway seems to be fine. 
    AirwayClear
    ViewMonitor
    UseBloodPressureCuff
    MeasuredMAP: 37
    Finish
    Thanks for playing! Here's how you did:
    
    Sadly, you didn't manage to stabilise John on this occasion.  He is hypotensive.  He has an unstable tachyarrhythmia, which is an indication for cardioversion. 
    
    Don't worry if you're not perfect yet, though. That's exactly what we're here for! 
    
    Final reward: 0.0

--- metanurse.py
+++ metanurse.py
@@ -54,18 +54,16 @@ def stabilize():
         if "airway" in examined and events[3] > 0:
             examined.add("airway_clear")
         elif "airway_clear" not in examined:
-            take_action(3)  # Re-examineAirway if not clear
+            take_action(35)  # PerformAirwayManoeuvres if not clear
             continue
 
         measure_vitals()
 
         if vitals["MAP"] is not None:
-            if vitals["MAP"] < 60 and "fluids_given" not in examined:
-                take_action(15)  # GiveFluids
-                examined.add("fluids_given")
-                continue
-            elif vitals["MAP"] < 60:
-                take_action(28)  # AttachDefibPads (considering circulatory support)
+            if vitals["MAP"] < 60:
+                if "IVFluid" not in examined:
+                    take_action(15)  # GiveFluids
+                    examined.add("IVFluid")
                 continue
             else:
                 examined.add("stable_MAP")
@@ -78,10 +76,9 @@ def stabilize():
             take_action(29)  # UseBagValveMask
             continue
 
-        if vitals["HR"] is not None:
-            if vitals["HR"] > 150 or vitals["HR"] < 50:
-                take_action(28)  # AttachDefibPads
-                continue
+        if vitals["HR"] is not None and (vitals["HR"] > 150 or vitals["HR"] < 50):
+            take_action(28)  # AttachDefibPads
+            continue
 
         if "all_vitals_checked" not in examined:
             examined.add("all_vitals_checked")
