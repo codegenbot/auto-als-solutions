57fa5a9afa6ba0fdf079e6dcafa36d8674660df7
beep boop <githubbot@vadim.me>
2024-08-17 01:25:20 +0200

    4186 (0.0). One of the ED nurses has called you to see a patient that she is worried about.
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    What's more, if John's sats fall below 65% or if his mean arterial pressure falls below 20mmHg, he will go into cardiac arrest.
    
    Good luck! 
    DoNothing
    ExamineAirway
    John is conscious and talking. The airway seems to be fine. 
    AirwayClear
    ViewMonitor
    Finish
    Thanks for playing! Here's how you did:
    
    Sadly, you didn't manage to stabilise John on this occasion.  He is hypotensive.  He has an unstable tachyarrhythmia, which is an indication for cardioversion. 
    
    Don't worry if you're not perfect yet, though. That's exactly what we're here for! 
    
    Final reward: 0.0

--- metanurse.py
+++ metanurse.py
@@ -1,6 +1,5 @@
 import sys
 
-
 def stabilize():
     def take_action(action):
         print(action)
@@ -42,9 +41,7 @@ def stabilize():
             "Resps": values[6] if times[6] > 0 else None,
         }
 
-        if (vitals["Sats"] and vitals["Sats"] < 65) or (
-            vitals["MAP"] and vitals["MAP"] < 20
-        ):
+        if (vitals["Sats"] and vitals["Sats"] < 65) or (vitals["MAP"] and vitals["MAP"] < 20):
             take_action(17)
             continue
 
@@ -58,7 +55,7 @@ def stabilize():
                 take_action(4)
                 examined.add("Breathing")
                 continue
-
+        
         if "BreathingSnoring" in events:
             take_action(36)
             continue
@@ -79,18 +76,17 @@ def stabilize():
         if vitals["RR"] and vitals["RR"] < 8:
             take_action(29)
             continue
-
+        
         if vitals["HR"]:
             if vitals["HR"] > 150:
                 take_action(24)
-                continue
             elif vitals["HR"] > 100:
                 take_action(9)
                 continue
             elif vitals["HR"] < 50:
                 take_action(12)
                 continue
-
+            
             if "ViewMonitor" not in examined:
                 take_action(16)
                 examined.add("ViewMonitor")
@@ -100,13 +96,11 @@ def stabilize():
                 take_action(40)
                 take_action(41)
                 take_action(43)
-                continue
 
         take_action(48)
         break
     else:
         take_action(48)
-
 
 if __name__ == "__main__":
     stabilize()
