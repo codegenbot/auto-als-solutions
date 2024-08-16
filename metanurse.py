719553615f4fd8860c00641ffd842e6d00cc6c3e
beep boop <githubbot@vadim.me>
2024-08-16 23:51:05 +0200

    4115 (0.0). One of the ED nurses has called you to see a patient that she is worried about.
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    What's more, if John's sats fall below 65% or if his mean arterial pressure falls below 20mmHg, he will go into cardiac arrest.
    
    Good luck! 
    DoNothing
    ExamineAirway
    John is conscious and talking. The airway seems to be fine. 
    AirwayClear
    ViewMonitor
    Finish
    Thanks for playing! Here's how you did:
    
    Sadly, you didn't manage to stabilise John on this occasion.  He is hypotensive.  He has an unstable tachyarrhythmia, which is an indication for cardioversion. 
    
    Don't worry if you're not perfect yet, though. That's exactly what we're here for! 
    
    Final reward: 0.0

--- metanurse.py
+++ metanurse.py
@@ -6,7 +6,30 @@ def stabilize():
         sys.stdout.flush()
 
     examined = set()
-    
+
+    def examine_step():
+        if "Airway" not in examined:
+            take_action(3)
+            examined.add("Airway")
+            return True
+        if "Breathing" not in examined:
+            take_action(4)
+            examined.add("Breathing")
+            return True
+        if "Circulation" not in examined:
+            take_action(5)
+            examined.add("Circulation")
+            return True
+        if "Disability" not in examined:
+            take_action(6)
+            examined.add("Disability")
+            return True
+        if "Exposure" not in examined:
+            take_action(7)
+            examined.add("Exposure")
+            return True
+        return False
+
     def examine_vitals():
         if "Monitor" not in examined:
             take_action(16)
@@ -17,7 +40,7 @@ def stabilize():
             examined.add("SatsProbe")
             return
         if "BP" not in examined:
-            take_action(38)
+            take_action(27)
             examined.add("BP")
             return
 
@@ -31,32 +54,21 @@ def stabilize():
         times = observations[33:40]
         values = observations[40:]
 
-        vitals = dict()
-        vitals["HR"] = values[0] if times[0] > 0 else None
-        vitals["RR"] = values[1] if times[1] > 0 else None
-        vitals["Glucose"] = values[2] if times[2] > 0 else None
-        vitals["Temp"] = values[3] if times[3] > 0 else None
-        vitals["MAP"] = values[4] if times[4] > 0 else None
-        vitals["Sats"] = values[5] if times[5] > 0 else None
-        vitals["Resps"] = values[6] if times[6] > 0 else None
+        vitals = {
+            "HR": values[0] if times[0] > 0 else None,
+            "RR": values[1] if times[1] > 0 else None,
+            "Glucose": values[2] if times[2] > 0 else None,
+            "Temp": values[3] if times[3] > 0 else None,
+            "MAP": values[4] if times[4] > 0 else None,
+            "Sats": values[5] if times[5] > 0 else None,
+            "Resps": values[6] if times[6] > 0 else None,
+        }
 
         if (vitals["Sats"] and vitals["Sats"] < 65) or (vitals["MAP"] and vitals["MAP"] < 20):
             take_action(17)
             continue
 
-        if events[3] == 0 and "Airway" not in examined:
-            take_action(3)
-            examined.add("Airway")
-            continue
-
-        if events[9] == 0 and "Breathing" not in examined:
-            take_action(4)
-            examined.add("Breathing")
-            continue
-
-        if "Circulation" not in examined:
-            take_action(5)
-            examined.add("Circulation")
+        if examine_step():
             continue
 
         examine_vitals()
@@ -77,8 +89,8 @@ def stabilize():
             if vitals["HR"] > 150:
                 take_action(24)
                 continue
-            elif vitals["HR"] >= 100:
-                take_action(11)
+            elif vitals["HR"] > 100:
+                take_action(9)
                 continue
             elif vitals["HR"] < 50:
                 take_action(12)
