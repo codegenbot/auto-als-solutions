5b0f7365b3d5438474c349a839c4e7f73784d90e
beep boop <githubbot@vadim.me>
2024-08-16 03:04:19 +0200

    3194 (0.0). One of the ED nurses has called you to see a patient that she is worried about.
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    What's more, if John's sats fall below 65% or if his mean arterial pressure falls below 20mmHg, he will go into cardiac arrest.
    
    Good luck! 
    DoNothing
    UseBloodPressureCuff
    UseSatsProbe
    ViewMonitor
    MeasuredMAP: 37
    MeasuredSats: 92
    TakeBloodPressure
    MeasuredMAP: 37
    MeasuredSats: 92
    Finish
    Thanks for playing! Here's how you did:
    
    Sadly, you didn't manage to stabilise John on this occasion.  He is hypotensive.  He has an unstable tachyarrhythmia, which is an indication for cardioversion. 
    
    Don't worry if you're not perfect yet, though. That's exactly what we're here for! 
    
    Final reward: 0.0

--- metanurse.py
+++ metanurse.py
@@ -29,12 +29,12 @@ def stabilize():
             take_action(17)
             continue
 
-        if 25 not in actions_taken:
+        if events[4] > 0 and 25 not in actions_taken:
             take_action(25)
             actions_taken.add(25)
             continue
 
-        if 27 not in actions_taken:
+        if events[4] > 0 and 27 not in actions_taken:
             take_action(27)
             actions_taken.add(27)
             continue
@@ -42,15 +42,13 @@ def stabilize():
         if vitals["MAP"] is not None and vitals["MAP"] < 60:
             take_action(15)
             continue
-
+        
         if vitals["Sats"] is not None and vitals["Sats"] < 88:
             if 30 not in actions_taken:
                 take_action(30)
                 actions_taken.add(30)
                 continue
-            take_action(29)
-            continue
-
+        
         if vitals["RR"] is not None and vitals["RR"] < 8:
             take_action(29)
             continue
@@ -60,6 +58,26 @@ def stabilize():
             actions_taken.add(3)
             continue
 
+        if 25 not in actions_taken:
+            take_action(25)
+            actions_taken.add(25)
+            continue
+        if 27 not in actions_taken:
+            take_action(27)
+            actions_taken.add(27)
+            continue
+        if 16 not in actions_taken:
+            take_action(16)
+            actions_taken.add(16)
+            continue
+        if 38 not in actions_taken:
+            take_action(38)
+            actions_taken.add(38)
+            continue
+
+        if any(events[i] > 0 for i in range(3, 7)):
+            take_action(3)
+            continue
         if any(events[i] > 0 for i in range(7, 15)):
             take_action(4)
             continue
