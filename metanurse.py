ee9f67aef49acff5ff5ae637bf17e2eb2c7ede49
beep boop <githubbot@vadim.me>
2024-08-16 12:27:00 +0200

    3579 (0.0). One of the ED nurses has called you to see a patient that she is worried about.
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    What's more, if John's sats fall below 65% or if his mean arterial pressure falls below 20mmHg, he will go into cardiac arrest.
    
    Good luck! 
    DoNothing
    UseSatsProbe
    UseBloodPressureCuff
    Finish
    Thanks for playing! Here's how you did:
    
    Sadly, you didn't manage to stabilise John on this occasion.  He is hypotensive.  He has an unstable tachyarrhythmia, which is an indication for cardioversion. 
    
    Don't worry if you're not perfect yet, though. That's exactly what we're here for! 
    
    Final reward: 0.0

--- metanurse.py
+++ metanurse.py
@@ -27,46 +27,27 @@ def stabilize():
             "Resps": values[6] if times[6] != 0 else None,
         }
 
-        if (vitals["Sats"] is not None and vitals["Sats"] < 65) or (vitals["MAP"] is not None and vitals["MAP"] < 20):
+        if (vitals["Sats"] is not None and vitals["Sats"] < 65) or (
+            vitals["MAP"] is not None and vitals["MAP"] < 20
+        ):
             take_action(17)
             continue
-        
-        if "airway" not in examined_vitals:
-            if any(events[i] > 0 for i in [3, 7, 8, 9]):
-                take_action(3)
-                examined_vitals.add("airway")
-                continue
-            if any(events[i] > 0 for i in [5, 6, 10]):
-                take_action(18)
-                continue
-            take_action(35)
-            continue
 
+        if "airway" not in examined_vitals:
+            take_action(3)
+            examined_vitals.add("airway")
+            continue
+        
         if "breathing" not in examined_vitals:
-            if vitals["Sats"] is None:
-                take_action(25)
-                continue
-            if vitals["Sats"] < 88:
-                take_action(30)
-                continue
-            if vitals["RR"] < 8:
-                take_action(29)
-                continue
             take_action(4)
             examined_vitals.add("breathing")
             continue
 
         if "circulation" not in examined_vitals:
-            if vitals["MAP"] is None:
-                take_action(27)
-                continue
-            if vitals["MAP"] < 60:
-                take_action(15)
-                continue
             take_action(5)
             examined_vitals.add("circulation")
             continue
-
+        
         if "disability" not in examined_vitals:
             take_action(6)
             examined_vitals.add("disability")
@@ -76,7 +57,35 @@ def stabilize():
             take_action(7)
             examined_vitals.add("exposure")
             continue
-            
+
+        if vitals["Sats"] is None:
+            take_action(25)
+            continue
+
+        if vitals["Sats"] is not None and vitals["Sats"] < 88:
+            take_action(30)
+            continue
+
+        if vitals["MAP"] is None:
+            take_action(27)
+            continue
+
+        if vitals["MAP"] is not None and vitals["MAP"] < 60:
+            take_action(15)
+            continue
+
+        if vitals["RR"] is not None and vitals["RR"] < 8:
+            take_action(29)
+            continue
+
+        if vitals["HR"] is None or (vitals["HR"] < 60 or vitals["HR"] > 100):
+            take_action(2)
+            continue
+
+        if events[28] > 0 or events[30] > 0:
+            take_action(9)
+            continue
+
         take_action(48)
         break
 
