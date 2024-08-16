96070710e5318262996f4f144422ca9bb158dd31
beep boop <githubbot@vadim.me>
2024-08-17 01:53:29 +0200

    4201 (0.0). One of the ED nurses has called you to see a patient that she is worried about.
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    What's more, if John's sats fall below 65% or if his mean arterial pressure falls below 20mmHg, he will go into cardiac arrest.
    
    Good luck! 
    DoNothing
    ExamineAirway
    John is conscious and talking. The airway seems to be fine. 
    AirwayClear
    ExamineBreathing
    John is breathing at a rate of 20 breaths per minute. Chest exapansion is equal. There are bibasal crepitations. 
    
    (Select the finger probe from the breathing drawer and hit "View Monitor" to see sats.)
    BreathingEqualChestExpansion
    BreathingBibasalCrepitations
    MeasuredRespRate: 20
    ViewMonitor
    Finish
    Thanks for playing! Here's how you did:
    
    Sadly, you didn't manage to stabilise John on this occasion.  He is hypotensive.  He has an unstable tachyarrhythmia, which is an indication for cardioversion. 
    
    Don't worry if you're not perfect yet, though. That's exactly what we're here for! 
    
    Final reward: 0.0

--- metanurse.py
+++ metanurse.py
@@ -7,6 +7,10 @@ def stabilize():
 
     examined = set()
     def examine_vitals():
+        if "BPCuff" not in examined:
+            take_action(27)  # Use Blood Pressure Cuff
+            examined.add("BPCuff")
+            return
         if "Monitor" not in examined:
             take_action(16)  # View Monitor
             examined.add("Monitor")
@@ -14,10 +18,6 @@ def stabilize():
         if "SatsProbe" not in examined:
             take_action(25)  # Use Sats Probe
             examined.add("SatsProbe")
-            return
-        if "BPCuff" not in examined:
-            take_action(27)  # Use Blood Pressure Cuff
-            examined.add("BPCuff")
             return
 
     for step in range(350):
@@ -44,51 +44,53 @@ def stabilize():
             continue
 
         if not any(events[3:7]):
-            take_action(3)  # Examine Airway
+            take_action(3)  # ExamineAirway
             continue
 
         if events[3]:  # AirwayClear
             if not any(events[7:15]):
-                take_action(4)  # Examine Breathing
+                take_action(4)  # ExamineBreathing
                 continue
 
         if events[8]:  # BreathingSnoring
-            take_action(36)  # Perform Head Tilt Chin Lift
+            take_action(36)  # PerformHeadTiltChinLift
+            continue
+
+        if vitals["Sats"] is None or vitals["MAP"] is None or vitals["RR"] is None:
+            examine_vitals()
+            continue
+
+        if vitals["MAP"] and vitals["MAP"] < 60:
+            take_action(15)  # GiveFluids
             continue
 
         if vitals["Sats"] and vitals["Sats"] < 88:
-            take_action(30)  # Use Non Rebreather Mask
-            continue
-
-        examine_vitals()
-
-        if vitals["MAP"] and vitals["MAP"] < 60:
-            take_action(15)  # Give Fluids
+            take_action(30)  # UseNonRebreatherMask
             continue
 
         if vitals["RR"] and vitals["RR"] < 8:
-            take_action(29)  # Use Bag Valve Mask
+            take_action(29)  # UseBagValveMask
             continue
 
         if any(events[i] for i in range(28, 33)):
-            take_action(24)  # Attach Defib Pads
+            take_action(24)  # UseMonitorPads
             continue
 
         if vitals["HR"]:
             if vitals["HR"] > 150:
-                take_action(24)  # Attach Defib Pads
+                take_action(24)  # UseMonitorPads
                 continue
             elif vitals["HR"] > 100:
-                take_action(9)  # Give Adenosine
+                take_action(9)  # GiveAdenosine
                 continue
             elif vitals["HR"] < 50:
-                take_action(12)  # Give Atropine
+                take_action(12)  # GiveAtropine
                 continue
 
-        take_action(48)
+        take_action(48)  # Finish
         break
     else:
-        take_action(48)
+        take_action(48)  # Finish
 
 if __name__ == "__main__":
     stabilize()
