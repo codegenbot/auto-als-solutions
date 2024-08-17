9f58bca1a4d229d1a174aacdd74e4a4973707f95
beep boop <githubbot@vadim.me>
2024-08-17 07:52:46 +0200

    4495 (0.0). One of the ED nurses has called you to see a patient that she is worried about.
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    What's more, if John's sats fall below 65% or if his mean arterial pressure falls below 20mmHg, he will go into cardiac arrest.
    
    Good luck! 
    DoNothing
    ExamineAirway
    John is conscious and talking. The airway seems to be fine. 
    AirwayClear
    UseSatsProbe
    UseBloodPressureCuff
    ExamineBreathing
    John is breathing at a rate of 20 breaths per minute. Chest exapansion is equal. There are bibasal crepitations. 
    
    (Hit "View Monitor" to see sats.)
    BreathingEqualChestExpansion
    BreathingBibasalCrepitations
    MeasuredRespRate: 20
    Finish
    Thanks for playing! Here's how you did:
    
    Sadly, you didn't manage to stabilise John on this occasion.  He is hypotensive.  He has an unstable tachyarrhythmia, which is an indication for cardioversion. 
    
    Don't worry if you're not perfect yet, though. That's exactly what we're here for! 
    
    Final reward: 0.0

--- metanurse.py
+++ metanurse.py
@@ -1,89 +1,97 @@
 import sys
 
-(
-    DO_NOTHING,
-    EXAMINE_AIRWAY,
-    EXAMINE_BREATHING,
-    EXAMINE_CIRCULATION,
-    EXAMINE_DISABILITY,
-    EXAMINE_EXPOSURE,
-    USE_BP_CUFF,
-    USE_SATS_PROBE,
-    USE_NON_REBREATHER_MASK,
-    GIVE_FLUIDS,
-    FINISH,
-) = (0, 3, 4, 5, 6, 7, 27, 25, 30, 15, 48)
-
 def stabilize():
     def take_action(action):
         print(action)
         sys.stdout.flush()
 
-    steps, examined = 350, set()
-    for step in range(steps):
+    def get_observations():
         try:
-            observations = list(map(float, input().strip().split()))
+            return list(map(float, input().strip().split()))
         except:
-            take_action(FINISH)
-            return
+            take_action(48)
+            sys.exit()
 
+    steps, examined = 350, set()
+
+    for _ in range(steps):
+        observations = get_observations()
         if len(observations) != 53:
-            take_action(DO_NOTHING)
+            take_action(0)
             continue
 
-        events, measuring_times, measurements = (
-            observations[:33],
-            observations[33:40],
-            observations[46:],
-        )
-        vitals = ["HR", "RR", "Glucose", "Temp", "MAP", "Sats", "Resps"]
-        vitals_dict = {
-            vitals[i]: measurements[i] if measuring_times[i] > 0 else None
-            for i in range(7)
+        events = observations[:33]
+        measured_recent = observations[33:40]
+        measurements = observations[46:]
+
+        vitals = {
+            "HR": measurements[0] if measured_recent[0] > 0 else None,
+            "RR": measurements[1] if measured_recent[1] > 0 else None,
+            "Glucose": measurements[2] if measured_recent[2] > 0 else None,
+            "Temp": measurements[3] if measured_recent[3] > 0 else None,
+            "MAP": measurements[4] if measured_recent[4] > 0 else None,
+            "Sats": measurements[5] if measured_recent[5] > 0 else None,
+            "Resps": measurements[6] if measured_recent[6] > 0 else None,
         }
 
-        if (vitals_dict["Sats"] and vitals_dict["Sats"] < 65) or (
-            vitals_dict["MAP"] and vitals_dict["MAP"] < 20
+        if (vitals["Sats"] is not None and vitals["Sats"] < 65) or (
+            vitals["MAP"] is not None and vitals["MAP"] < 20
         ):
-            take_action(FINISH)
+            take_action(17)
             continue
 
-        if not events[3] and "Airway" not in examined:
-            take_action(EXAMINE_AIRWAY)
+        if not any(events[3:7]) and "Airway" not in examined:
+            take_action(3)
             examined.add("Airway")
             continue
 
-        if "Sats" not in examined and vitals_dict["Sats"] is None:
-            take_action(USE_SATS_PROBE)
+        if vitals["Sats"] is None and "Sats" not in examined:
+            take_action(25)
             examined.add("Sats")
             continue
 
-        if "MAP" not in examined and vitals_dict["MAP"] is None:
-            take_action(USE_BP_CUFF)
+        if vitals["MAP"] is None and "MAP" not in examined:
+            take_action(27)
             examined.add("MAP")
             continue
 
         if "Breathing" not in examined:
-            take_action(EXAMINE_BREATHING)
+            take_action(4)
             examined.add("Breathing")
             continue
 
-        if vitals_dict["Sats"] and vitals_dict["Sats"] < 88:
-            take_action(USE_NON_REBREATHER_MASK)
+        if vitals["Sats"] is not None and vitals["Sats"] < 88:
+            take_action(30)
             continue
 
-        if vitals_dict["MAP"] and vitals_dict["MAP"] < 60:
-            take_action(GIVE_FLUIDS)
+        if vitals["MAP"] is not None and vitals["MAP"] < 60:
+            take_action(15)
             continue
 
-        if vitals_dict["RR"] and vitals_dict["RR"] < 8:
-            take_action(USE_NON_REBREATHER_MASK)
+        if vitals["RR"] is not None and vitals["RR"] < 8:
+            take_action(29)
             continue
 
-        take_action(FINISH)
+        rhythm_events = [events[i] for i in range(27, 33)]
+        if any(rhythm_events):
+            take_action(24)
+            continue
+
+        if vitals["HR"]:
+            if vitals["HR"] > 150:
+                take_action(24)
+                continue
+            elif vitals["HR"] < 50:
+                take_action(12)
+                continue
+            elif vitals["HR"] > 100:
+                take_action(9)
+                continue
+
+        take_action(48)
         break
     else:
-        take_action(FINISH)
+        take_action(48)
 
 if __name__ == "__main__":
     stabilize()
