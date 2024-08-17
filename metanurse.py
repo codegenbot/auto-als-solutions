d65d9fdccd05e31f3ba472e9419e9106dc49ea59
beep boop <githubbot@vadim.me>
2024-08-17 11:16:08 +0200

    4667 (0.0). One of the ED nurses has called you to see a patient that she is worried about.
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    What's more, if John's sats fall below 65% or if his mean arterial pressure falls below 20mmHg, he will go into cardiac arrest.
    
    Good luck! 
    DoNothing
    ExamineAirway
    John is conscious and talking. The airway seems to be fine. 
    AirwayClear
    UseSatsProbe
    UseBloodPressureCuff
    ExamineBreathing
    John is breathing at a rate of 20 breaths per minute. Chest exapansion is equal. There are bibasal crepitations. 
    
    (Hit "View Monitor" to see sats.)
    BreathingEqualChestExpansion
    BreathingBibasalCrepitations
    MeasuredRespRate: 20
    Finish
    Thanks for playing! Here's how you did:
    
    Sadly, you didn't manage to stabilise John on this occasion.  He is hypotensive.  He has an unstable tachyarrhythmia, which is an indication for cardioversion. 
    
    Don't worry if you're not perfect yet, though. That's exactly what we're here for! 
    
    Final reward: 0.0

--- metanurse.py
+++ metanurse.py
@@ -47,26 +47,30 @@ def stabilize():
             take_action(25)
             examined.add("Sats")
             continue
-        
-        if vitals["Sats"] is not None and vitals["Sats"] < 88:
-            take_action(30)
-            continue
 
         if vitals["MAP"] is None and "MAP" not in examined:
             take_action(27)
             examined.add("MAP")
             continue
 
-        if vitals["MAP"] is not None and vitals["MAP"] < 60:
-            take_action(15)
-            continue
-
-        if vitals["RR"] is None and "Breathing" not in examined:
+        if "Breathing" not in examined:
             take_action(4)
             examined.add("Breathing")
             continue
 
-        if vitals["RR"] is not None and vitals["RR"] < 8:
+        if events[12] > 0:
+            take_action(29)
+            continue
+
+        if (vitals["Sats"] is not None and vitals["Sats"] < 88):
+            take_action(30)
+            continue
+
+        if (vitals["MAP"] is not None and vitals["MAP"] < 60):
+            take_action(15)
+            continue
+
+        if (vitals["RR"] is not None and vitals["RR"] < 8):
             take_action(29)
             continue
 
@@ -74,9 +78,9 @@ def stabilize():
             take_action(24)
             continue
 
-        if vitals["HR"] is not None:
+        if vitals["HR"]:
             if vitals["HR"] > 150:
-                take_action(9)
+                take_action(9)  # Use adenosine for tachyarrhythmia
                 continue
             elif vitals["HR"] < 50:
                 take_action(12)
