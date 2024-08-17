c30f0590092236beba6958c5bbf692ed452d9cbb
beep boop <githubbot@vadim.me>
2024-08-17 06:08:50 +0200

    4407 (0.0). One of the ED nurses has called you to see a patient that she is worried about.
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    What's more, if John's sats fall below 65% or if his mean arterial pressure falls below 20mmHg, he will go into cardiac arrest.
    
    Good luck! 
    DoNothing
    ExamineAirway
    John is conscious and talking. The airway seems to be fine. 
    AirwayClear
    UseBloodPressureCuff
    UseSatsProbe
    ExamineBreathing
    John is breathing at a rate of 20 breaths per minute. Chest exapansion is equal. There are bibasal crepitations. 
    
    (Hit "View Monitor" to see sats.)
    BreathingEqualChestExpansion
    BreathingBibasalCrepitations
    MeasuredRespRate: 20
    Finish
    Thanks for playing! Here's how you did:
    
    Sadly, you didn't manage to stabilise John on this occasion.  He is hypotensive.  He has an unstable tachyarrhythmia, which is an indication for cardioversion. 
    
    Don't worry if you're not perfect yet, though. That's exactly what we're here for! 
    
    Final reward: 0.0

--- metanurse.py
+++ metanurse.py
@@ -12,19 +12,19 @@ def stabilize():
         if len(observations) != 53:
             take_action(0)  # DoNothing
             continue
-
+        
         events = observations[:33]
         measurement_times = observations[33:40]
         measurements = observations[40:]
 
         vitals = {
-            "HR": measurements[0] if measurement_times[0] > 0 else None,
-            "RR": measurements[1] if measurement_times[1] > 0 else None,
-            "Glucose": measurements[2] if measurement_times[2] > 0 else None,
-            "Temp": measurements[3] if measurement_times[3] > 0 else None,
-            "MAP": measurements[4] if measurement_times[4] > 0 else None,
-            "Sats": measurements[5] if measurement_times[5] > 0 else None,
-            "Resps": measurements[6] if measurement_times[6] > 0 else None,
+            "HR": measurements[0] if measurement_times[0] else None,
+            "RR": measurements[1] if measurement_times[1] else None,
+            "Glucose": measurements[2] if measurement_times[2] else None,
+            "Temp": measurements[3] if measurement_times[3] else None,
+            "MAP": measurements[4] if measurement_times[4] else None,
+            "Sats": measurements[5] if measurement_times[5] else None,
+            "Resps": measurements[6] if measurement_times[6] else None,
         }
 
         if (vitals["Sats"] and vitals["Sats"] < 65) or (vitals["MAP"] and vitals["MAP"] < 20):
@@ -36,49 +36,40 @@ def stabilize():
             examined.add("Airway")
             continue
 
-        if "Sats" not in examined and vitals["Sats"] is None:
-            take_action(25)  # Use Sats Probe
-            examined.add("Sats")
-            continue
-
         if "MAP" not in examined and vitals["MAP"] is None:
             take_action(27)  # Use Blood Pressure Cuff
             examined.add("MAP")
             continue
 
-        if any(events[3:7]) and "Breathing" not in examined:
+        if "Sats" not in examined and vitals["Sats"] is None:
+            take_action(25)  # Use Sats Probe
+            examined.add("Sats")
+            continue
+
+        if "Breathing" not in examined:
             take_action(4)  # Examine Breathing
             examined.add("Breathing")
             continue
         
-        if "HR" not in examined and vitals["HR"] is None:
-            take_action(2)  # Check Rhythm
-            examined.add("HR")
-            continue
-
-        if vitals["Sats"] and vitals["Sats"] < 88:
+        if vitals["Sats"] is not None and vitals["Sats"] < 88:
             take_action(30)  # Use Non Rebreather Mask
             continue
-
-        if vitals["MAP"] and vitals["MAP"] < 60:
+        
+        if vitals["MAP"] is not None and vitals["MAP"] < 60:
             take_action(15)  # Give Fluids
             continue
 
-        if vitals["RR"] and vitals["RR"] < 8:
+        if vitals["RR"] is not None and vitals["RR"] < 8:
             take_action(29)  # Use Bag-Valve Mask
             continue
-
-        if vitals["HR"] and vitals["HR"] > 150:
-            take_action(24)  # Use Monitor Pads (for cardioversion)
-            continue
-
-        if vitals["RR"] and vitals["RR"] > 20:
-            take_action(29)  # Use Bag-Valve Mask
-            continue
-
-        if vitals["HR"] and vitals["HR"] < 50:
-            take_action(12)  # Give Atropine
-            continue
+        
+        if vitals["HR"] is not None:
+            if vitals["HR"] > 150:
+                take_action(24)  # Use Monitor Pads (for cardioversion)
+                continue
+            elif vitals["HR"] < 50:
+                take_action(12)  # Give Atropine
+                continue
 
         take_action(48)  # Finish
         break
