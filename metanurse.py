a5988c476f6f111598ebacd057dd0c562d44ad6e
beep boop <githubbot@vadim.me>
2024-08-17 07:35:51 +0200

    4466 (0.0). One of the ED nurses has called you to see a patient that she is worried about.
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    What's more, if John's sats fall below 65% or if his mean arterial pressure falls below 20mmHg, he will go into cardiac arrest.
    
    Good luck! 
    DoNothing
    ExamineAirway
    John is conscious and talking. The airway seems to be fine. 
    AirwayClear
    UseSatsProbe
    UseBloodPressureCuff
    ExamineBreathing
    John is breathing at a rate of 20 breaths per minute. Chest exapansion is equal. There are bibasal crepitations. 
    
    (Hit "View Monitor" to see sats.)
    BreathingEqualChestExpansion
    BreathingBibasalCrepitations
    MeasuredRespRate: 20
    Finish
    Thanks for playing! Here's how you did:
    
    Sadly, you didn't manage to stabilise John on this occasion.  He is hypotensive.  He has an unstable tachyarrhythmia, which is an indication for cardioversion. 
    
    Don't worry if you're not perfect yet, though. That's exactly what we're here for! 
    
    Final reward: 0.0

--- metanurse.py
+++ metanurse.py
@@ -1,17 +1,6 @@
 import sys
 
-(
-    DO_NOTHING,
-    EXAMINE_AIRWAY,
-    EXAMINE_BREATHING,
-    USE_BP_CUFF,
-    USE_SATS_PROBE,
-    USE_NON_REBREATHER_MASK,
-    GIVE_FLUIDS,
-    PERFORM_CARDIOVERSION,
-    FINISH,
-) = (0, 3, 4, 27, 25, 30, 15, 24, 48)
-
+DO_NOTHING, EXAMINE_AIRWAY, EXAMINE_BREATHING, EXAMINE_CIRCULATION, USE_BP_CUFF, USE_SATS_PROBE, USE_NON_REBREATHER_MASK, GIVE_FLUIDS, PERFORM_CARDIOVERSION, FINISH = 0, 3, 4, 5, 27, 25, 30, 15, 24, 48
 
 def stabilize():
     def take_action(action):
@@ -30,20 +19,11 @@ def stabilize():
             take_action(DO_NOTHING)
             continue
 
-        events, measuring_times, measurements = (
-            observations[:33],
-            observations[33:40],
-            observations[46:],
-        )
+        events, measuring_times, measurements = observations[:33], observations[33:40], observations[46:]
         vitals = ["HR", "RR", "Glucose", "Temp", "MAP", "Sats", "Resps"]
-        vitals_dict = {
-            vitals[i]: measurements[i] if measuring_times[i] > 0 else None
-            for i in range(7)
-        }
+        vitals_dict = {vitals[i]: measurements[i] if measuring_times[i] > 0 else None for i in range(7)}
 
-        if (vitals_dict["Sats"] and vitals_dict["Sats"] < 65) or (
-            vitals_dict["MAP"] and vitals_dict["MAP"] < 20
-        ):
+        if (vitals_dict["Sats"] and vitals_dict["Sats"] < 65) or (vitals_dict["MAP"] and vitals_dict["MAP"] < 20):
             take_action(PERFORM_CARDIOVERSION)
             continue
 
@@ -83,7 +63,6 @@ def stabilize():
         break
     else:
         take_action(FINISH)
-
 
 if __name__ == "__main__":
     stabilize()
