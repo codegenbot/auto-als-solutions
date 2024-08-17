1b0b06457a7515ca4ac9be2bcf047d5b9fb38f2a
beep boop <githubbot@vadim.me>
2024-08-17 09:11:06 +0200

    4559 (0.0). One of the ED nurses has called you to see a patient that she is worried about.
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    What's more, if John's sats fall below 65% or if his mean arterial pressure falls below 20mmHg, he will go into cardiac arrest.
    
    Good luck! 
    DoNothing
    ExamineAirway
    John is conscious and talking. The airway seems to be fine. 
    AirwayClear
    UseSatsProbe
    UseBloodPressureCuff
    ExamineBreathing
    John is breathing at a rate of 20 breaths per minute. Chest exapansion is equal. There are bibasal crepitations. 
    
    (Hit "View Monitor" to see sats.)
    BreathingEqualChestExpansion
    BreathingBibasalCrepitations
    MeasuredRespRate: 20
    Finish
    Thanks for playing! Here's how you did:
    
    Sadly, you didn't manage to stabilise John on this occasion.  He is hypotensive.  He has an unstable tachyarrhythmia, which is an indication for cardioversion. 
    
    Don't worry if you're not perfect yet, though. That's exactly what we're here for! 
    
    Final reward: 0.0

--- metanurse.py
+++ metanurse.py
@@ -1,5 +1,6 @@
 import sys
 
+
 def stabilize():
     def take_action(action):
         print(action)
@@ -60,6 +61,10 @@ def stabilize():
             examined.add("Breathing")
             continue
 
+        if events[12] > 0:
+            take_action(29)
+            continue
+
         if vitals["Sats"] is not None and vitals["Sats"] < 88:
             take_action(30)
             continue
@@ -72,27 +77,37 @@ def stabilize():
             take_action(29)
             continue
 
-        if "Circulation" not in examined:
-            take_action(5)
-            examined.add("Circulation")
-            continue
-
-        if set(events[27:33]):
-            take_action(16)
+        if set(events[27:33]) & {i for i in range(27, 33)}:
+            take_action(24)
             continue
 
         if vitals["HR"]:
             if vitals["HR"] > 150:
-                take_action(16)
+                take_action(24)
                 continue
             elif vitals["HR"] < 50:
                 take_action(12)
                 continue
 
+        if not set(examined).intersection({"Monitor"}):
+            take_action(16)
+            examined.add("Monitor")
+            continue
+
+        if not set(examined).intersection({"MAP"}):
+            take_action(38)
+            examined.add("MAP")
+            continue
+
+        if not set(examined).intersection({"SatsProbe"}):
+            take_action(25)
+            continue
+
         take_action(48)
         break
     else:
         take_action(48)
+
 
 if __name__ == "__main__":
     stabilize()
