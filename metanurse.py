5b5636e48f977b0903dd9b8221ec96e4060561ca
beep boop <githubbot@vadim.me>
2024-08-17 07:51:22 +0200

    4492 (0.0). One of the ED nurses has called you to see a patient that she is worried about.
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    What's more, if John's sats fall below 65% or if his mean arterial pressure falls below 20mmHg, he will go into cardiac arrest.
    
    Good luck! 
    DoNothing
    ExamineAirway
    John is conscious and talking. The airway seems to be fine. 
    AirwayClear
    UseSatsProbe
    UseBloodPressureCuff
    ExamineBreathing
    John is breathing at a rate of 20 breaths per minute. Chest exapansion is equal. There are bibasal crepitations. 
    
    (Hit "View Monitor" to see sats.)
    BreathingEqualChestExpansion
    BreathingBibasalCrepitations
    MeasuredRespRate: 20
    Finish
    Thanks for playing! Here's how you did:
    
    Sadly, you didn't manage to stabilise John on this occasion.  He is hypotensive.  He has an unstable tachyarrhythmia, which is an indication for cardioversion. 
    
    Don't worry if you're not perfect yet, though. That's exactly what we're here for! 
    
    Final reward: 0.0

--- metanurse.py
+++ metanurse.py
@@ -2,53 +2,14 @@ import sys
 
 ACTIONS = {
     "DoNothing": 0,
-    "CheckSignsOfLife": 1,
-    "CheckRhythm": 2,
     "ExamineAirway": 3,
     "ExamineBreathing": 4,
-    "ExamineCirculation": 5,
-    "ExamineDisability": 6,
-    "ExamineExposure": 7,
-    "ExamineResponse": 8,
-    "GiveAdenosine": 9,
-    "GiveAdrenaline": 10,
-    "GiveAmiodarone": 11,
-    "GiveAtropine": 12,
-    "GiveMidazolam": 13,
-    "UseVenflonIVCatheter": 14,
-    "GiveFluids": 15,
-    "ViewMonitor": 16,
-    "StartChestCompression": 17,
-    "OpenAirwayDrawer": 18,
-    "OpenBreathingDrawer": 19,
-    "OpenCirculationDrawer": 20,
-    "OpenDrugsDrawer": 21,
-    "BagDuringCPR": 22,
-    "ResumeCPR": 23,
-    "UseMonitorPads": 24,
-    "UseSatsProbe": 25,
-    "UseAline": 26,
     "UseBloodPressureCuff": 27,
-    "AttachDefibPads": 28,
-    "UseBagValveMask": 29,
+    "UseSatsProbe": 25,
     "UseNonRebreatherMask": 30,
-    "UseYankeurSucionCatheter": 31,
-    "UseGuedelAirway": 32,
-    "TakeBloodForArtherialBloodGas": 33,
-    "TakeRoutineBloods": 34,
-    "PerformAirwayManoeuvres": 35,
-    "PerformHeadTiltChinLift": 36,
-    "PerformJawThrust": 37,
-    "TakeBloodPressure": 38,
-    "TurnOnDefibrillator": 39,
-    "DefibrillatorCharge": 40,
-    "DefibrillatorCurrentUp": 41,
-    "DefibrillatorCurrentDown": 42,
-    "DefibrillatorPace": 43,
-    "DefibrillatorPacePause": 44,
-    "DefibrillatorRateUp": 45,
-    "DefibrillatorRateDown": 46,
-    "DefibrillatorSync": 47,
+    "GiveFluids": 15,
+    "StartChestCompression": 17,
+    "UseBagValveMask": 29,
     "Finish": 48
 }
 
@@ -74,7 +35,7 @@ def stabilize():
         measurements = observations[46:]
 
         vitals = ["HeartRate", "RespRate", "Glucose", "Temp", "MAP", "Sats", "Resps"]
-        vitals_dict = {vitals[i]: measurements[i] if measuring_times[i] > 0 else None for i in range(7)}
+        vitals_dict = {vitals[i]: measurements[i] if measuring_times[i] > 0 else None for i in range(7)]
 
         if (vitals_dict["Sats"] is not None and vitals_dict["Sats"] < 65) or (vitals_dict["MAP"] is not None and vitals_dict["MAP"] < 20):
             take_action(ACTIONS["StartChestCompression"])
