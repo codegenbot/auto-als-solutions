6ada76081d9fd1fc76d57a023326794337491b26
beep boop <githubbot@vadim.me>
2024-08-17 10:01:31 +0200

    4585 (0.0). One of the ED nurses has called you to see a patient that she is worried about.
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    What's more, if John's sats fall below 65% or if his mean arterial pressure falls below 20mmHg, he will go into cardiac arrest.
    
    Good luck! 
    DoNothing
    ExamineAirway
    John is conscious and talking. The airway seems to be fine. 
    AirwayClear
    UseSatsProbe
    UseBloodPressureCuff
    ExamineBreathing
    John is breathing at a rate of 20 breaths per minute. Chest exapansion is equal. There are bibasal crepitations. 
    
    (Hit "View Monitor" to see sats.)
    BreathingEqualChestExpansion
    BreathingBibasalCrepitations
    MeasuredRespRate: 20
    Finish
    Thanks for playing! Here's how you did:
    
    Sadly, you didn't manage to stabilise John on this occasion.  He is hypotensive.  He has an unstable tachyarrhythmia, which is an indication for cardioversion. 
    
    Don't worry if you're not perfect yet, though. That's exactly what we're here for! 
    
    Final reward: 0.0

--- metanurse.py
+++ metanurse.py
@@ -23,7 +23,7 @@ def stabilize():
         events = observations[:33]
         measured_recent = observations[33:40]
         measurements = observations[46:]
-
+    
         vitals = {
             "HR": measurements[0] if measured_recent[0] > 0 else None,
             "RR": measurements[1] if measured_recent[1] > 0 else None,
@@ -35,14 +35,18 @@ def stabilize():
         }
 
         if (vitals["Sats"] is not None and vitals["Sats"] < 65) or (
-            vitals["MAP"] is not None and vitals["MAP"] < 20
-        ):
+            vitals["MAP"] is not None and vitals["MAP"] < 20):
             take_action(17)
             continue
 
         if not any(events[3:7]) and "Airway" not in examined:
             take_action(3)
             examined.add("Airway")
+            continue
+
+        if "Breathing" not in examined:
+            take_action(4)
+            examined.add("Breathing")
             continue
 
         if vitals["Sats"] is None and "Sats" not in examined:
@@ -53,11 +57,6 @@ def stabilize():
         if vitals["MAP"] is None and "MAP" not in examined:
             take_action(27)
             examined.add("MAP")
-            continue
-
-        if "Breathing" not in examined:
-            take_action(4)
-            examined.add("Breathing")
             continue
 
         if vitals["Sats"] is not None and vitals["Sats"] < 88:
@@ -77,7 +76,7 @@ def stabilize():
             continue
 
         if vitals["HR"]:
-            if vitals["HR"] > 150 and events[28] == 0:
+            if vitals["HR"] > 150:
                 take_action(17)
                 continue
             elif vitals["HR"] < 50:
