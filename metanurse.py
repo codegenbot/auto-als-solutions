1e162774898f1ae369ef1b3634efdfe7833fe0a4
beep boop <githubbot@vadim.me>
2024-08-17 03:34:01 +0200

    4273 (0.0). One of the ED nurses has called you to see a patient that she is worried about.
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    What's more, if John's sats fall below 65% or if his mean arterial pressure falls below 20mmHg, he will go into cardiac arrest.
    
    Good luck! 
    DoNothing
    ViewMonitor
    UseSatsProbe
    UseBloodPressureCuff
    MeasuredMAP: 37
    MeasuredSats: 92
    ExamineAirway
    MeasuredMAP: 37
    MeasuredSats: 92
    Finish
    Thanks for playing! Here's how you did:
    
    Sadly, you didn't manage to stabilise John on this occasion.  He is hypotensive.  He has an unstable tachyarrhythmia, which is an indication for cardioversion. 
    
    Don't worry if you're not perfect yet, though. That's exactly what we're here for! 
    
    Final reward: 0.0

--- metanurse.py
+++ metanurse.py
@@ -6,14 +6,14 @@ def stabilize():
         sys.stdout.flush()
 
     steps, examined, current_action_idx = 350, set(), 0
-    basic_actions = [16, 25, 27]
+    actions = [16, 25, 27]
 
     for step in range(steps):
         observations = list(map(float, input().strip().split()))
         if len(observations) != 53:
             take_action(0)
             continue
-
+        
         events = observations[:33]
         recent_measurements = observations[33:40]
         measurements = observations[40:]
@@ -32,59 +32,40 @@ def stabilize():
             take_action(17)
             continue
 
+        if (vitals["Sats"] and vitals["Sats"] < 88) or not vitals["Sats"]:
+            take_action(30)
+            continue
+
+        if (vitals["MAP"] and vitals["MAP"] < 60) or not vitals["MAP"]:
+            take_action(15)
+            continue
+
+        if (vitals["RR"] and vitals["RR"] < 8) or not vitals["RR"]:
+            take_action(29)
+            continue
+
+        if current_action_idx < len(actions):
+            take_action(actions[current_action_idx])
+            current_action_idx += 1
+            continue
+
         if not any(events[3:7]) and "Airway" not in examined:
             take_action(3)
             examined.add("Airway")
             continue
 
-        if "Airway" in examined:
-            if any(events[7:15]):
-                if events[7]:
-                    take_action(29)
-                elif events[9]:
-                    take_action(36)
-                continue
-            if not any(events[0:3]) and "Response" not in examined:
-                take_action(8)
-                examined.add("Response")
-                continue
-            if not any(events[15:17]) and "Circulation" not in examined:
-                take_action(5)
-                examined.add("Circulation")
-                continue
-            if not any(events[21:24]) and "Disability" not in examined:
-                take_action(6)
-                examined.add("Disability")
-                continue
-
-        if vitals["MAP"] and vitals["MAP"] < 60:
-            take_action(15)
-            continue
-
-        if vitals["Sats"] and vitals["Sats"] < 88:
-            take_action(30)
-            continue
-
-        if vitals["RR"] and vitals["RR"] < 8:
-            take_action(29)
-            continue
-
-        if any(events[28:33]) or (vitals["HR"] and vitals["HR"] > 150):
-            take_action(24)
+        if events[3] and not any(events[7:15]) and "Breathing" not in examined:
+            take_action(4)
+            examined.add("Breathing")
             continue
 
         if vitals["HR"]:
-            if vitals["HR"] > 100 and vitals["MAP"] < 60:
+            if vitals["HR"] > 150:
                 take_action(24)
                 continue
             elif vitals["HR"] < 50:
                 take_action(12)
                 continue
-
-        if current_action_idx < len(basic_actions):
-            take_action(basic_actions[current_action_idx])
-            current_action_idx += 1
-            continue
 
         take_action(48)
         break
