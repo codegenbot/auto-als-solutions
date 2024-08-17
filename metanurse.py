4ebb0873a8b9bbac346a0aa6f95351b142fb3ec9
beep boop <githubbot@vadim.me>
2024-08-17 07:10:26 +0200

    4450 (0.0). One of the ED nurses has called you to see a patient that she is worried about.
    John is a fit and well 45-year-old man who has been feeling breathless since this morning. Please perform an ABCDE assessment and stabilise him to complete the scenario.
    For the purposes of this exercise, being "stabilised" means that John will have a clear airway, oxygen saturation of at least 88%, a respiratory rate of at least 8 breaths per minute and a mean arterial pressure of at least 60mmHg. 
    What's more, if John's sats fall below 65% or if his mean arterial pressure falls below 20mmHg, he will go into cardiac arrest.
    
    Good luck! 
    DoNothing
    ExamineAirway
    John is conscious and talking. The airway seems to be fine. 
    AirwayClear
    UseSatsProbe
    UseBloodPressureCuff
    ExamineBreathing
    John is breathing at a rate of 20 breaths per minute. Chest exapansion is equal. There are bibasal crepitations. 
    
    (Hit "View Monitor" to see sats.)
    BreathingEqualChestExpansion
    BreathingBibasalCrepitations
    MeasuredRespRate: 20
    Finish
    Thanks for playing! Here's how you did:
    
    Sadly, you didn't manage to stabilise John on this occasion.  He is hypotensive.  He has an unstable tachyarrhythmia, which is an indication for cardioversion. 
    
    Don't worry if you're not perfect yet, though. That's exactly what we're here for! 
    
    Final reward: 0.0

--- metanurse.py
+++ metanurse.py
@@ -1,6 +1,27 @@
 import sys
 
-DO_NOTHING, EXAMINE_AIRWAY, EXAMINE_BREATHING, USE_BP_CUFF, USE_SATS_PROBE, USE_NON_REBREATHER_MASK, GIVE_FLUIDS, PERFORM_CARDIOVERSION, FINISH = 0, 3, 4, 27, 25, 30, 15, 24, 48
+(
+    DO_NOTHING,
+    CHECK_LIFE_SIGNS,
+    CHECK_RHYTHM,
+    EXAMINE_AIRWAY,
+    EXAMINE_BREATHING,
+    EXAMINE_CIRCULATION,
+) = (0, 1, 2, 3, 4, 5)
+EXAMINE_DISABILITY, EXAMINE_EXPOSURE, EXAMINE_RESPONSE = 6, 7, 8
+GIVE_ADENOSINE, GIVE_ADRENALINE, GIVE_AMIODARONE, GIVE_ATROPINE = 9, 10, 11, 12
+GIVE_MIDAZOLAM, USE_VENFLON, GIVE_FLUIDS, VIEW_MONITOR = 13, 14, 15, 16
+START_CHEST_COMPRESSION, OPEN_AIRWAY_DRAWER, OPEN_BREATHING_DRAWER = 17, 18, 19
+OPEN_CIRCULATION_DRAWER, OPEN_DRUGS_DRAWER, BAG_DURING_CPR = 20, 21, 22
+RESUME_CPR, USE_MONITOR_PADS, USE_SATS_PROBE, USE_ALINE = 23, 24, 25, 26
+USE_BP_CUFF, ATTACH_DEFIB_PADS, USE_BAG_VALVE_MASK = 27, 28, 29
+USE_NON_REBREATHER_MASK, USE_YANKEUR_SUCTION, USE_GUEDEL_AIRWAY = 30, 31, 32
+TAKE_BLOOD_GAS, TAKE_BLOODS, PERFORM_AIRWAY_MANEUVER = 33, 34, 35
+PERFORM_HEAD_TILT, PERFORM_JAW_THRUST, TAKE_BP = 36, 37, 38
+TURN_ON_DEFIB, DEFIB_CHARGE, DEFIB_CURRENT_UP = 39, 40, 41
+DEFIB_CURRENT_DOWN, DEFIB_PACE, DEFIB_PACE_PAUSE = 42, 43, 44
+DEFIB_RATE_UP, DEFIB_RATE_DOWN, DEFIB_SYNC, FINISH = 45, 46, 47, 48
+
 
 def stabilize():
     def take_action(action):
@@ -18,12 +39,21 @@ def stabilize():
             take_action(DO_NOTHING)
             continue
 
-        events, measuring_times, measurements = observations[:33], observations[33:40], observations[46:]
+        events, measuring_times, measurements = (
+            observations[:33],
+            observations[33:40],
+            observations[46:],
+        )
         vitals = ["HR", "RR", "Glucose", "Temp", "MAP", "Sats", "Resps"]
-        vitals_dict = {vitals[i]: measurements[i] if measuring_times[i] > 0 else None for i in range(7)}
+        vitals_dict = {
+            vitals[i]: measurements[i] if measuring_times[i] > 0 else None
+            for i in range(7)
+        }
 
-        if (vitals_dict["Sats"] and vitals_dict["Sats"] < 65) or (vitals_dict["MAP"] and vitals_dict["MAP"] < 20):
-            take_action(PERFORM_CARDIOVERSION)
+        if (vitals_dict["Sats"] and vitals_dict["Sats"] < 65) or (
+            vitals_dict["MAP"] and vitals_dict["MAP"] < 20
+        ):
+            take_action(START_CHEST_COMPRESSION)
             continue
 
         if not any(events[3:7]) and "Airway" not in examined:
@@ -55,21 +85,25 @@ def stabilize():
             continue
 
         if vitals_dict["RR"] and vitals_dict["RR"] < 8:
-            take_action(USE_NON_REBREATHER_MASK)
+            take_action(USE_BAG_VALVE_MASK)
             continue
 
         if vitals_dict["HR"]:
             if vitals_dict["HR"] > 150:
-                take_action(PERFORM_CARDIOVERSION)
+                take_action(USE_MONITOR_PADS)
                 continue
             elif vitals_dict["HR"] < 50:
-                take_action(GIVE_FLUIDS)
+                take_action(GIVE_ATROPINE)
+                continue
+            elif vitals_dict["HR"] > 100:
+                take_action(GIVE_ADENOSINE)
                 continue
 
         take_action(FINISH)
         break
     else:
         take_action(FINISH)
+
 
 if __name__ == "__main__":
     stabilize()
